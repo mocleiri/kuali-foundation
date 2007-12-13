a3a1de3787e4253d302e1d182f1a4ba8be6d28d5
abyrne <abyrne@kuali.org>
2007-12-12 21:54:17 -0500

    *** empty log message ***
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5260 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-daily-updates.sh
+++ kfs/do-daily-updates.sh
@@ -4,10 +4,6 @@
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
-kexport dba
-kdailytag kfs-release-2-1-br
-kupdate unt true true false
-kupdate cnv true false true
 kpurge $LOGS_DIRECTORY 7
 
 kend 0
