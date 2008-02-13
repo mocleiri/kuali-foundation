6ef3217b5bdf7e24bb9f9c94ef8da685c18ab961
thrclark <thrclark@kuali.org>
2008-02-13 12:18:15 -0500

    *** empty log message ***
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/mocks/trunk@6036 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ menu/kra_iframes/noaccess2_files/UserService.js
@@ -0,0 +1,7 @@
+
+function UserService() { }
+UserService._path = '/kuali-reg/dwr';
+
+UserService.getUniversalUserByAuthenticationUserId = function(p0, callback) {
+    DWREngine._execute(UserService._path, 'UserService', 'getUniversalUserByAuthenticationUserId', p0, callback);
+}
