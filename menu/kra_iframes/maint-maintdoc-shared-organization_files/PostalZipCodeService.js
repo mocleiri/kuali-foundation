fc9617b648854a9c229aa086a7f84739fd4e950e
mraising <mraising@kuali.org>
2008-02-25 17:09:44 -0500

    Changes as per Lora's Mail dated Feb 22nd.(Mostly in the Menu folder)
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/mocks/trunk@6054 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ menu/kra_iframes/maint-maintdoc-shared-organization_files/PostalZipCodeService.js
@@ -0,0 +1,7 @@
+
+function PostalZipCodeService() { }
+PostalZipCodeService._path = '/kuali-cnv/dwr';
+
+PostalZipCodeService.getByPrimaryId = function(p0, callback) {
+    DWREngine._execute(PostalZipCodeService._path, 'PostalZipCodeService', 'getByPrimaryId', p0, callback);
+}
