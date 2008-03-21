4b033a981ce5b9ee1f8dd96f8d62739a908d9120
thrclark <thrclark@kuali.org>
2008-03-21 15:49:04 -0400

    *** empty log message ***
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/mocks/trunk@6101 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- menu/kra_iframes/maint-maintdoc-pro-RateType_files/CustomAttributeService.js
+++
@@ -1,7 +0,0 @@
-
-function CustomAttributeService() { }
-CustomAttributeService._path = '/kra-cnv/dwr';
-
-CustomAttributeService.getLookupReturnsForAjaxCall = function(p0, callback) {
-    DWREngine._execute(CustomAttributeService._path, 'CustomAttributeService', 'getLookupReturnsForAjaxCall', p0, callback);
-}
--- menu/kra_iframes/maint-maintdoc-shared-organization_files/PostalZipCodeService.js
+++
@@ -1,7 +0,0 @@
-
-function PostalZipCodeService() { }
-PostalZipCodeService._path = '/kuali-cnv/dwr';
-
-PostalZipCodeService.getByPrimaryId = function(p0, callback) {
-    DWREngine._execute(PostalZipCodeService._path, 'PostalZipCodeService', 'getByPrimaryId', p0, callback);
-}
