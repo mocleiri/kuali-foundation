56cd44890ae674e63eac2ad57ab324c7383f93fc
abyrne <abyrne@kuali.org>
2008-04-02 18:32:45 -0400

    removing cnv and reg for now and updating the stg db
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5299 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-daily-updates.sh
+++ kfs/do-daily-updates.sh
@@ -7,8 +7,7 @@
 kexport dba
 kdailytag HEAD
 kupdate unt true true false
-kupdate cnv true true true
-kupdate stg false false true
+kupdate stg false true true
 kpurge $LOGS_DIRECTORY 7
 
 sh ~j2eemgr/kuali/kfs/check-modularization.sh
--- kfs/do-weekly-updates.sh
+++ kfs/do-weekly-updates.sh
@@ -4,6 +4,4 @@
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
-kupdate reg true true true
-
 kend 0
