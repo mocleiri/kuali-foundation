18cdd79ea2487bec75a0d139fd9b300893c28801
gmcgrego <gmcgrego@kuali.org>
2008-09-05 17:19:07 -0400

    KRACOEUS-1849
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5349 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kra/do-weekly-updates.sh
+++ kra/do-weekly-updates.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-. ~j2eemgr/kuali/cvs-svn-staging/kra/settings
-. ~j2eemgr/kuali/cvs-svn-staging/shared-settings
-. ~j2eemgr/kuali/cvs-svn-staging/shared-functions
+. ~j2eemgr/kuali/kra/settings
+. ~j2eemgr/kuali/shared-settings
+. ~j2eemgr/kuali/shared-functions
 
 kupdate reg true true true
 # kmail kuali-rice-dev@googlegroups.com "weekly kra reg build completed" "weekly kra reg build completed, all clear to deploy rice snapshot"
