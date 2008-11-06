1ae24c7754ebb4b4f1adad177add18147eb7d168
ewestfal <ewestfal@kuali.org>
2008-11-06 12:46:01 -0500

    KULRICE-1859 - Committed a rice directory and daily update script to kul-cfg-envs
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5385 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-updates.sh
+++ rice/do-daily-updates.sh
@@ -5,11 +5,5 @@
 . ~j2eemgr/kuali/shared-functions
 
 kexport dba branches/rice-release-0-9-4-br
-kdailytag trunk
-kupdate unt standard true false Oracle9i
-kupdate cnv standard true true Oracle9i
-kpurge $LOGS_DIRECTORY 7
-
-sh ~j2eemgr/kuali/kfs/check-modularization.sh
 
 kend 0
