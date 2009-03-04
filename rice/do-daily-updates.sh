cf905e35c49de0fef1250fe9113a764d410ed7d4
caseyhb <caseyhb@kuali.org>
2009-03-04 11:20:30 -0500

    OPEN - issue KULRICE-2764: Deploy Rice standalone server to our test environments https://test.kuali.org/jira/browse/KULRICE-2764
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5404 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-updates.sh
+++ rice/do-daily-updates.sh
@@ -5,7 +5,17 @@
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
-kexport dba branches/rice-release-0-9-4-br
-kdailytag branches/rice-release-0-9-4-br
+kexport dba $APPLICATION_INFRASTRUCTURE_VERSION
+kdailytag $APPLICATION_INFRASTRUCTURE_VERSION
+
+##
+# Args to kupdate
+# 	1) environment ("unt","cnv","stg")
+#   2) name ("standard" == use project/artifact version)
+#	3) update database (boolean)
+# 	4) deploy war (boolean)
+#   5) database platform ("Oracle9i")
+##
+kupdate stg standard true true Oracle9i
 
 kend 0
