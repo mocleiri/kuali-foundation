95f183ade7fcb89966d50e1fca5fd7cfbec3f886
ewestfal <ewestfal@kuali.org>
2009-03-25 14:19:09 -0400

    KULCFG-374 - updated shared-settings file so that the ability to execute concurrent builds should be fully enabled now.  Also made some updates and additions to the various rice update scripts.
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5416 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ rice/do-daily-kuali-client-updates.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#do-daily-updates.sh
+
+. ~j2eemgr/kuali/rice/common-settings
+. ~j2eemgr/kuali/rice/1.0.0-kuali-client-settings
+. ~j2eemgr/kuali/shared-settings
+. ~j2eemgr/kuali/shared-functions
+
+kexport dba $APPLICATION_INFRASTRUCTURE_VERSION
+# may need to update kdailytag, it assumes we have one database project per application project!
+# kdailytag $APPLICATION_INFRASTRUCTURE_VERSION
+
+##
+# Args to kupdate
+# 	1) environment ("unt","cnv","stg")
+#   2) name ("standard" -- do not use an arbitrary value)
+#	3) update database (boolean)
+# 	4) deploy war (boolean)
+#   5) database platform ("Oracle9i" -- do not use an arbitrary value)
+##
+kupdate cnv standard true true Oracle9i
+#kpurge $LOGS_DIRECTORY 7
+
+kend 0
