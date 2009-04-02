c0dc0e4d10c364917e8b09095af877251aa847e4
aakers <aakers@kuali.org>
2009-04-02 12:42:55 -0400

    Updating weekly and adding daily
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5420 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kra/do-weekly-updates.sh
+++ kra/do-weekly-updates.sh
@@ -5,7 +5,6 @@
 . ~j2eemgr/kuali/shared-functions
 
 kupdate reg true true true
-# kmail kuali-rice-dev@googlegroups.com "weekly kra reg build completed" "weekly kra reg build completed, all clear to deploy rice snapshot"
-kmail kra-developers-l@indiana.edu "weekly kra reg build completed" "weekly kra reg build completed, it's ok to test on it."
+kmail kc.dev@kuali.org "weekly kra reg build completed" "weekly kra reg build completed, it's ok to test on it."
 
 kend 0
