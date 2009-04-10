16ac2197246cae7383c3c625286f4f9c44c7fee7
aakers <aakers@kuali.org>
2009-04-10 12:41:39 -0400

    updated kupdate to new parameter format
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5422 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kra/do-weekly-updates.sh
+++ kra/do-weekly-updates.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 . ~j2eemgr/kuali/kra/settings
+. ~j2eemgr/kuali/kra/kra-functions
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
-kupdate reg true true true
+kupdate reg standard true true Oracle9i
 kmail kc.dev@kuali.org "weekly kra reg build completed" "weekly kra reg build completed, it's ok to test on it."
 
 kend 0
