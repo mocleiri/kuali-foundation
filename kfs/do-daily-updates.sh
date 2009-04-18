ef9025c14e067bfb789e20af197bf583a3efc6fe
abyrne <abyrne@kuali.org>
2009-04-18 16:04:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5425 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-daily-updates.sh
+++ kfs/do-daily-updates.sh
@@ -6,7 +6,7 @@
 
 kexport dba trunk
 kdailytag trunk
-kupdate cnv standard true true MySQL
+kupdate cnv standard true true Oracle9i
 kupdate unt standard true false Oracle9i
 kpurge $LOGS_DIRECTORY 7
 
