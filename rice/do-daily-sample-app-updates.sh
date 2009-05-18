dd8a2c7ed0a27cd8b8bc1b807695bc6db90d0b4e
ewestfal <ewestfal@kuali.org>
2009-05-18 16:07:24 -0400

    Made some fixes to sample app update script
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5435 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-sample-app-updates.sh
+++ rice/do-daily-sample-app-updates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 . ~j2eemgr/kuali/rice/common-settings
-. ~j2eemgr/kuali/rice/1.0.0-kuali-client-settings
+. ~j2eemgr/kuali/rice/1.0.0-sample-app-settings
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
