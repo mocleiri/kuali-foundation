aa6991fe3d3146e9cc53440c4799848039264551
ewestfal <ewestfal@kuali.org>
2009-05-27 15:35:04 -0400

    KULRICE-2858 - Added daily update scripts for CAS
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5452 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ cas/do-daily-updates.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+. ~j2eemgr/kuali/cas/common-settings
+. ~j2eemgr/kuali/cas/1.0.0-settings
+. ~j2eemgr/kuali/shared-settings
+. ~j2eemgr/kuali/shared-functions
+
+kdailytag $APPLICATION_INFRASTRUCTURE_VERSION
+
+##
+# Args to kupdate
+# 	1) environment ("unt","cnv","stg")
+#   2) name ("standard" -- do not use an arbitrary value)
+#	3) update database (boolean)
+# 	4) deploy war (boolean)
+#   5) database platform ("Oracle9i" -- do not use an arbitrary value)
+##
+kupdate cnv standard false true Oracle9i
+
+kpurge $LOGS_DIRECTORY 7
+
+kend 0
