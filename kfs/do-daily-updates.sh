3706a8d12c40012c2ae6bbda0a78c965c0a9e05d
abyrne <abyrne@kuali.org>
2009-05-28 13:21:14 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5453 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-daily-updates.sh
+++ kfs/do-daily-updates.sh
@@ -4,11 +4,10 @@
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
-kexport dba branches/kfs-200907-implementations-br
-kdailytag branches/kfs-200907-implementations-br
+kexport dba trunk
+kdailytag trunk
 kupdate cnv standard true true Oracle9i
 kupdate unt standard true false Oracle9i
-kupdate stg trunk true true Oracle9i
 kpurge $LOGS_DIRECTORY 7
 
 #sh ~j2eemgr/kuali/kfs/check-modularization.sh
