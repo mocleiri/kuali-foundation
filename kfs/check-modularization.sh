3f3dceae965314982d47afdeed81d4935f3ae3b1
jkeller <jkeller@kuali.org>
2009-06-03 12:37:37 -0400

    KFSMI-3059 : additional greps for modularization issues which may not be caught by other tests
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5464 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/check-modularization.sh
+++ kfs/check-modularization.sh
@@ -4,6 +4,47 @@
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
+PROJECT_DIRECTORY=$APPLICATION_DIRECTORY/$APPLICATION; typeset -rx PROJECT_DIRECTORY
+SOURCE_DIRECTORY=$PROJECT_DIRECTORY/work/src; typeset -rx SOURCE_DIRECTORY
+
+function kgrepcode {
+	{
+	typeset directory=$1
+	typeset pattern=$2
+	klog "Testing path [$directory] for pattern [$pattern]"	
+	grep -rnE --include=*.java --include=*.xml "$pattern" $directory
+	typeset return_status=$?
+	if [[ "$return_status" != "0" ]]; then
+	    echo "NO MATCHES"
+	fi
+	echo "\n"
+	} >> $SCRIPT_LOG 2>&1
+}
+typeset -xf kgrepcode
+
 kcheckmodularization unt trunk Oracle9i
+
+# these grep tests catch code references that the verify-design task can not
+
+# check core for reference to the external modules
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/coa "org\.kuali\.kfs\.module\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/fp "org\.kuali\.kfs\.module\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/gl "org\.kuali\.kfs\.module\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/pdp "org\.kuali\.kfs\.module\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/sys "org\.kuali\.kfs\.module\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/vnd "org\.kuali\.kfs\.module\."
+
+# check the integration package for references to the external modules
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/integration "org\.kuali\.kfs\.module\."
+
+# check the optional modules for references to other optional modules
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/module/ar "org\.kuali\.kfs\.module\.(bc|cab|cam|cg|ec|ld|purap)\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/module/bc "org\.kuali\.kfs\.module\.(ar|cab|cam|cg|ec|ld|purap)\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/module/cab "org\.kuali\.kfs\.module\.(ar|bc|cg|ec|ld)\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/module/cam "org\.kuali\.kfs\.module\.(ar|bc|cab|cg|ec|ld|purap)\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/module/cg "org\.kuali\.kfs\.module\.(ar|bc|cab|cam|ec|ld|purap)\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/module/ec "org\.kuali\.kfs\.module\.(ar|bc|cab|cam|cg|ld|purap)\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/module/ld "org\.kuali\.kfs\.module\.(ar|bc|cab|cam|cg|ec|purap)\."
+kgrepcode $SOURCE_DIRECTORY/org/kuali/kfs/module/purap "org\.kuali\.kfs\.module\.(ar|bc|cab|cam|cg|ec|ld)\."
 
 kend 0
