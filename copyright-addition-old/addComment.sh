82f635063ce7a53d38ad768fcc204bb5c58cc6f1
caseyhb <caseyhb@kuali.org>
2009-06-04 14:48:00 -0400

    OPEN - issue KULRICE-3094: put tlds and js files in special web/lib dir / update license header generation script to work with svn and ignore the dir with the tlds and js files that we didn't author https://test.kuali.org/jira/browse/KULRICE-3094
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5466 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ copyright-addition-old/addComment.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+#jbmorris October/November, 2006
+#modified in December to work with relative path to CVS directory, on all files
+#therein, instead of working only on committed files via commitinfo
+
+# caseyhb 1June2009
+# modified to work with SVN
+
+function usage()
+{
+    local printDefault=true
+    echo "addComment.sh [url] [branch] (excluded files) (excluded dirs)\n"
+    echo "where 'url' is the Subversion repository URL of the project\n"
+    echo "      'branch' is the project branch (default 'trunk')\n"
+    echo "      'excluded files' is a file of newline delimited filenames to be excluded\n"
+    echo "      'excluded dirs' is a file of newline-delimited directory names to be excluded\n"
+    exit 1
+}
+
+# check args
+if [ $# -lt 4 ]; then
+    usage()
+fi
+#elif (( $# -lt 2 )); then
+#    usage()
+
+cd "$1"  #get into directory we're going to process
+typeset Programdir=`dirname "$0"`
+typeset Directory="$CVSROOT/$1"
+typeset Branch=$2
+typeset File
+typeset -l Extension  #ignore case
+typeset Style
+typeset Message=""
+typeset ReturnStatus
+ 
+function ignore_log 
+{
+  #this used to echo to a log file, so was a little more useful as a function then
+  echo "IGNORING: $*"
+}
+
+function add_to_message {
+  typeset NewMessage=""
+  case $2 in
+    -1) NewMessage="unknown extension" ;;
+    -2) NewMessage="no extension" ;;
+    -3) NewMessage="space in filename" ;;
+    -4) NewMessage="space in directory name" ;;
+    1) NewMessage="perl - style not known" ;;
+    2) NewMessage="perl - unrecognized cvs log output" ;;
+    3) NewMessage="perl - too few arguments" ;;
+    4) NewMessage="perl - too many arguments" ;;
+    5) NewMessage="perl - invalid tag specification" ;;
+    *) NewMessage="unknown error $2" ;;
+  esac
+  Message="$Message\n$Directory/$1: $NewMessage"
+}
+
+#traverse the directory tree from current up to root, checking each one
+#if find a directory whose name begins with . don't do license processing
+#(e.g. .settings)
+#OR if directory is in ignore.dirs file, don't do license processing
+while [[ $Directory != $CVSROOT && $Directory != "/" ]]; do
+  if [[ ${Directory#* } != $Directory ]]; then  #contains space
+    add_to_message "" -4
+  fi
+  if [[ ${Directory#.} != $Directory ]]; then
+    ignore_log $1
+    exit 0
+  elif ( grep -q "^`basename "$Directory"`\$" "$Programdir/ignore.dirs" ); then
+    ignore_log $1
+    exit 0
+  else
+    Directory=`dirname "$Directory"`
+  fi
+done
+
+Directory="$CVSROOT/$1"  #get back to the correct value for use in add_to_message
+#now examine all the files in the directory one by one
+#ignore files in ignore.files, or if they have no extension, or if their
+# extension is in the ignore list
+for File in `ls`; do
+  if [[ -f $File ]]; then  #if not a regular file (e.g. directory, link, pipe) it's no need to do anything with it
+    Extension=${File##*.}
+    if [[ ${File#* } != $File ]]; then  #contains space
+      add_to_message "$File" -3
+    fi
+    if [[ ${File#.} != $File ]]; then  #begins with .
+      ignore_log $File
+    elif ( grep -q "^$File\$" "$Programdir/ignore.files" ); then
+      ignore_log $File
+    elif [[ $Extension = $File ]]; then  #no extension
+      add_to_message "$File" -2
+    else
+      case $Extension in
+        java|js|ddl|table|indexes|sql|css) Style="C" ;;
+        html|htm|xml|xsd|xsl|jmx|tld|wsdd|dia|dtd) Style="HTML" ;;
+        jsp|tag) Style="JSP" ;;
+        vm) Style="VM" ;;
+        sh|pl) Style="SHELL" ;;
+        bat) Style="DOS" ;;
+        ctl) Style="SQLLDR" ;;
+        sqr|h) Style="SQR" ;;
+        jpg|gif|zip|dmp|ico|war|jar|pdf|xls|png|ois|project|properties|jj|log|sla|tex|tsv|csv|txt|eps|fb|prefs|xml) Style="" ;;
+        *) Style="" ; add_to_message "$File" -1 ;;
+      esac
+      if [[ $Style != "" ]]; then
+        "$Programdir/addComment.pl" "$File" $Style "$Branch"
+        ReturnStatus=$?
+        if (( $ReturnStatus != 0 )); then
+          add_to_message "$File" $ReturnStatus
+        fi
+      fi
+    fi
+  fi
+done
+
+if [[ $Message != "" ]]; then
+  echo $Message | mail -s "$0 WARNINGS/ERRORS" kfcm-l@indiana.edu
+fi
+exit
