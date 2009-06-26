e19ba0a7c653ff3d2e6372912c18aea3662d324e
caseyhb <caseyhb@kuali.org>
2009-06-26 01:17:57 -0400

    OPEN - issue KULCFG-387: rice-cfg-dbs and kr-cnv not being processed/deployed nightly https://test.kuali.org/jira/browse/KULCFG-387
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5483 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-updates.sh
+++ rice/do-daily-updates.sh
@@ -1,27 +1,44 @@
 #!/bin/sh
 
-. ~j2eemgr/kuali/rice/do-daily-kuali-client-updates.sh
-
 . ~j2eemgr/kuali/rice/common-settings
-. ~j2eemgr/kuali/rice/1.0.0-settings
+. ~j2eemgr/kuali/rice/1.0.0-kuali-client-settings
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
 kexport dba $APPLICATION_INFRASTRUCTURE_VERSION
 kdailytag $APPLICATION_INFRASTRUCTURE_VERSION
 
+. ~j2eemgr/kuali/rice/common-settings
+. ~j2eemgr/kuali/rice/1.0.0-settings
+
+kexport dba $APPLICATION_INFRASTRUCTURE_VERSION
+kdailytag $APPLICATION_INFRASTRUCTURE_VERSION
+
 ##
 # Args to kupdate
-# 	1) environment ("unt","cnv","stg")
+#   1) environment ("unt","cnv","stg")
 #   2) name ("standard" -- do not use an arbitrary value)
-#	3) update database (boolean)
-# 	4) deploy war (boolean)
+#   3) update database (boolean)
+#   4) deploy war (boolean)
 #   5) database platform ("Oracle9i" -- do not use an arbitrary value)
 ##
 kupdate cnv standard true true Oracle9i
 
-. ~j2eemgr/kuali/rice/do-daily-sample-app-updates.sh
+. ~j2eemgr/kuali/rice/common-settings
+. ~j2eemgr/kuali/rice/1.0.0-sample-app-settings
+. ~j2eemgr/kuali/rice/common-functions
+
+kexport client $APPLICATION_INFRASTRUCTURE_VERSION
+
+APPLICATION_JAVA_PROJECT=rice-sample-apps/recipe; typeset -x APPLICATION_JAVA_PROJECT
+
+sampleappdailytag $APPLICATION_INFRASTRUCTURE_VERSION
+
+DATABASE_APPLICATION_CODE=ricesamp094; typeset -x DATABASE_APPLICATION_CODE
+
+kupdate cnv standard true true Oracle9i
 
 kpurge $LOGS_DIRECTORY 7
 
 kend 0
+
--- rice/do-weekly-updates.sh
+++ rice/do-weekly-updates.sh
@@ -7,8 +7,24 @@
 . ~j2eemgr/kuali/shared-functions
 
 kupdate stg standard true true Oracle9i
-. ~j2eemgr/kuali/rice/do-weekly-sample-app-updates.sh
-. ~j2eemgr/kuali/rice/do-weekly-kuali-client-updates.sh
+
+#chb: 25Jun2009: from do-weekly-sample-app-updates.sh
+. ~j2eemgr/kuali/rice/common-settings
+. ~j2eemgr/kuali/rice/1.0.0-sample-app-settings
+. ~j2eemgr/kuali/shared-settings
+. ~j2eemgr/kuali/rice/common-functions
+
+APPLICATION_JAVA_PROJECT=rice-sample-apps/recipe; typeset -x APPLICATION_JAVA_PROJECT
+
+DATABASE_APPLICATION_CODE=ricesamp094; typeset -x DATABASE_APPLICATION_CODE
+
+kupdate stg standard true true Oracle9i
+
+#chb: 25Jun2009: from do-weekly-kuali-client-updates.sh
+. ~j2eemgr/kuali/rice/common-settings
+. ~j2eemgr/kuali/rice/1.0.0-kuali-client-settings
+
+#kupdate stg standard true true Oracle9i
 
 kpurge $LOGS_DIRECTORY 7
 
