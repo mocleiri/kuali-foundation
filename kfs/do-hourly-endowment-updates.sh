0def05f32e5cb641dc656fad3a80626762ef943d
abyrne <abyrne@kuali.org>
2009-08-05 12:35:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5514 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-hourly-endowment-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-. ~j2eemgr/kuali/kfs/settings
-. ~j2eemgr/kuali/shared-settings
-. ~j2eemgr/kuali/shared-functions
-
-# kupdate dev2 branches/endowment-build-1219 false true Oracle9i
-
-kend 0
--- kfs/do-hourly-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-. ~j2eemgr/kuali/kfs/settings
-. ~j2eemgr/kuali/shared-settings
-. ~j2eemgr/kuali/shared-functions
-
-sh ~j2eemgr/kuali/kfs/do-hourly-endowment-updates.sh
-
-kend 0
