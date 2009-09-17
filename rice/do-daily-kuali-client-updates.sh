10924706befeb4e6a2c280a1b38603afbfb095dd
ewestfal <ewestfal@kuali.org>
2009-09-17 16:01:29 -0400

    Restored do-daily-kuali-client-updates.sh
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5531 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ rice/do-daily-kuali-client-updates.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+. ~j2eemgr/kuali/rice/common-settings
+. ~j2eemgr/kuali/rice/1.0.1-kuali-client-settings
+. ~j2eemgr/kuali/shared-settings
+. ~j2eemgr/kuali/shared-functions
+
+kexport dba $APPLICATION_INFRASTRUCTURE_VERSION
+kdailytag $APPLICATION_INFRASTRUCTURE_VERSION
+
+##
+# Args to kupdate
+# 	1) environment ("unt","cnv","stg")
+#   2) name ("standard" -- do not use an arbitrary value)
+#	3) update database (boolean)
+# 	4) deploy war (boolean)
+#   5) database platform ("Oracle9i" -- do not use an arbitrary value)
+##
+kupdate cnv standard true true Oracle9i
+kpurge $LOGS_DIRECTORY 7
+
+kend 0
