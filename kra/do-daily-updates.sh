0432001bc2a9793698ef757109660129dd173f37
aakers <aakers@kuali.org>
2009-10-02 12:03:53 -0400

    updated rebuild of dev and dev2
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5546 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kra/do-daily-updates.sh
+++ kra/do-daily-updates.sh
@@ -8,15 +8,18 @@
 . ~j2eemgr/kuali/shared-functions
 
 buildsvnwebapp dev2
-buildsvnwebapp dev
+
 kexport stg trunk
 kdailytag trunk
-#APPLICATION_SERVER_NAME=kuali15; typeset -rx APPLICATION_SERVER_NAME
+
 kupdate cnv standard true false Oracle9i
 buildsvnwebapp cnv
 runS2sScripts cnv
-#APPLICATION_SERVER_NAME=kuali5; typeset -rx APPLICATION_SERVER_NAME
-#kupdate stg standard false true Oracle9i
+
+kupdate dev standard true false Oracle9i
+buildsvnwebapp dev
+runS2sScripts dev
+
 buildsvnwebapp stg
 kpurge $LOGS_DIRECTORY 7
 
