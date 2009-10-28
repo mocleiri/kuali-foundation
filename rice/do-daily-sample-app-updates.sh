3cd35117f1b7bdda605c57a0f58e01bcfea09965
ewestfal <ewestfal@kuali.org>
2009-10-28 01:54:36 -0400

    Added back in sample app updates so we can get the nightly export of the rice101client database
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5582 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ rice/do-daily-sample-app-updates.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. ~j2eemgr/kuali/rice/common-settings
+. ~j2eemgr/kuali/rice/1.0.1-sample-app-settings
+. ~j2eemgr/kuali/shared-settings
+. ~j2eemgr/kuali/shared-functions
+# build number needs to be different for each project
+BUILD_NUMBER_FILE=$APPLICATION_DIRECTORY/$JAVA_APPLICATION_CODE-build-number.txt; typeset -rx BUILD_NUMBER_FILE
+
+kexport client $APPLICATION_INFRASTRUCTURE_SRCCTRL_VERSION
+kdailytag $APPLICATION_INFRASTRUCTURE_SRCCTRL_VERSION
+
+kpurge $LOGS_DIRECTORY 15
+
+kend 0
--- rice/do-daily-updates.sh
+++ rice/do-daily-updates.sh
@@ -2,3 +2,4 @@
 
 sh ~j2eemgr/kuali/rice/do-daily-kuali-client-updates.sh
 sh ~j2eemgr/kuali/rice/do-daily-standard-updates.sh
+sh ~j2eemgr/kuali/rice/do-daily-sample-app-updates.sh
