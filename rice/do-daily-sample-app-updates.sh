6bd7b857a8d2e215594edbdff79cf8c8529436a9
caseyhb <caseyhb@kuali.org>
2009-12-04 00:34:27 -0500

    Adding 1.0.1.1 to routine
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5589 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-sample-app-updates.sh
+++ rice/do-daily-sample-app-updates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 . ~j2eemgr/kuali/rice/common-settings
-. ~j2eemgr/kuali/rice/1.0.1-sample-app-settings
+. ~j2eemgr/kuali/rice/1.0.1.1-sample-app-settings
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 # build number needs to be different for each project
