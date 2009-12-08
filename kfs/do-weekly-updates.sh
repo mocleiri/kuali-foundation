fd7cf867a6143cd5dd168496ab8dce4d9261667b
jkeller <jkeller@kuali.org>
2009-12-08 12:32:04 -0500

    Updates to functions to support deployment of the patch branch from rice for the KFS STG instance.
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5593 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-weekly-updates.sh
+++ kfs/do-weekly-updates.sh
@@ -4,8 +4,8 @@
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
-typeset -x APPLICATION_SERVER_NAME=kuali22
-kupdate reg standard true true MySQL false
+#typeset -x APPLICATION_SERVER_NAME=kuali22
+#kupdate reg standard true true MySQL false
 
 sh ~j2eemgr/kuali/kfs/do-weekly-endowment-updates.sh
 
