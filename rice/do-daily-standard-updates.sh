cfb3b63010dedb3eab102c189305ff4630c66dd6
caseyhb <caseyhb@kuali.org>
2010-01-13 18:35:21 -0500

    Creating scripts for simultaneous deployment of both 1.0.1.1 and 1.1.0
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5599 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-standard-updates.sh
+++ rice/do-daily-standard-updates.sh
@@ -21,6 +21,15 @@ kdailytag $APPLICATION_INFRASTRUCTURE_SRCCTRL_VERSION
 # uncomment this when we get a 101 cnv env setup
 kupdate cnv standard true true Oracle9i false
 
+. ~j2eemgr/kuali/rice/1.1.0-settings
+. ~j2eemgr/kuali/shared-settings
+. ~j2eemgr/kuali/shared-functions
+BUILD_NUMBER_FILE=$APPLICATION_DIRECTORY/$JAVA_APPLICATION_CODE-build-number.txt; typeset -rx BUILD_NUMBER_FILE
+kexport dba $APPLICATION_INFRASTRUCTURE_SRCCTRL_VERSION
+kdailytag $APPLICATION_INFRASTRUCTURE_SRCCTRL_VERSION
+kupdate cnv standard true true Oracle9i false
+
+
 kpurge $LOGS_DIRECTORY 15
 
 kend 0
