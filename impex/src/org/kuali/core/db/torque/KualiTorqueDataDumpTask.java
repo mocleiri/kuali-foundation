65fbdd710df3e869749ef3685b57f5835f583fc6
jkeller <jkeller@kuali.org>
2010-01-21 23:32:02 -0500

    Updates to allow filtering of the tables exported.
    git-svn-id: https://svn.kuali.org/repos/foundation/db-utils/branches/export-filter@7143 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- impex/src/org/kuali/core/db/torque/KualiTorqueDataDumpTask.java
+++ impex/src/org/kuali/core/db/torque/KualiTorqueDataDumpTask.java
@@ -13,11 +13,16 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.filters.TokenFilter.StringTokenizer;
 import org.apache.torque.engine.platform.Platform;
 import org.apache.torque.engine.platform.PlatformFactory;
 import org.apache.xerces.dom.DocumentImpl;
@@ -47,6 +52,9 @@ public class KualiTorqueDataDumpTask extends Task {
 	
 	private String databaseType;
 	private String tableName;
+	private String excludedTables;
+	private List<String> excludedTableList = new ArrayList<String>();
+	private String startTableName;
 	
 	/** The database connection used to retrieve the data to dump. */
 	private Connection conn;
@@ -299,11 +307,31 @@ public class KualiTorqueDataDumpTask extends Task {
 		ResultSet tableNames = null;
 		// these are the entity types we want from the database
 		String[] types = { "TABLE" }; // JHK: removed views from list
+		List<Pattern> exclusionPatterns = new ArrayList<Pattern>( excludedTableList.size() );
+		for ( String t : excludedTableList ) {
+			exclusionPatterns.add( Pattern.compile(t) );
+		}
+		
 		try {
 			tableNames = dbMeta.getTables( null, databaseSchema.toUpperCase(), tableName,
 					types ); // JHK: upper-cased schema name (required by Oracle)
 			while ( tableNames.next() ) {
 				String name = tableNames.getString( 3 );
+				if ( startTableName != null && !startTableName.equals("") ) { 	
+					if ( name.compareTo(startTableName) < 0 ) {
+						continue;
+					}
+				}
+				boolean skip = false;
+				for ( Pattern p : exclusionPatterns ) {
+					if ( p.matcher(name).matches() ) {
+						skip = true;
+						break;
+					}
+				}
+				if ( skip ) {
+					continue;
+				}
 				tables.add( name );
 			}
 		} finally {
@@ -337,5 +365,23 @@ public class KualiTorqueDataDumpTask extends Task {
 
 	public void setTableName(String tableName) {
 		this.tableName = tableName;
+	}
+
+	public void setExcludedTables(String excludedTables) {
+		this.excludedTables = excludedTables;
+		excludedTableList = Arrays.asList( StringUtils.split(excludedTables, ',') );
+	}
+
+	public String getExcludedTables() {
+		return excludedTables;
+	}
+
+	public void setStartTableName(String startTableName) {
+		this.startTableName = startTableName;
+	}
+
+	public String getStartTableName() {
+		return startTableName;
 	}	
+	
 }
