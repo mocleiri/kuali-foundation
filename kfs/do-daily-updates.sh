70dfb22b0008cbe9ad61ca31771e778337fbefb3
jkeller <jkeller@kuali.org>
2010-02-05 16:56:12 -0500

    Restored CNV and REG builds
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5616 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-daily-updates.sh
+++ kfs/do-daily-updates.sh
@@ -7,17 +7,15 @@
 kexport dba trunk
 kdailytag trunk
 
-#typeset -x APPLICATION_SERVER_NAME=kuali23
-#kupdate cnv standard true true Oracle9i true
+typeset -x APPLICATION_SERVER_NAME=kuali23
+kupdate cnv standard true true Oracle9i true
 
-typeset -x APPLICATION_SERVER_NAME=kuali2
-typeset -x APPLICATION_INFRASTRUCTURE_VERSION=1.0.1.1
-kupdate stg branches/release-3-0-1 true true Oracle9i true branches/rice-release-1-0-1-1-br
+#typeset -x APPLICATION_SERVER_NAME=kuali2
+#typeset -x APPLICATION_INFRASTRUCTURE_VERSION=1.0.1.1
+#kupdate stg branches/release-3-0-1 true true Oracle9i true branches/rice-release-1-0-1-1-br
 
-# take from thunk
-#kupdate unt standard true false Oracle9i true
-# update using the 3.0.1 patch dataset
-kupdate unt branches/release-3-0-1 true false Oracle9i true branches/rice-release-1-0-1-1-br
+# take from trunk
+kupdate unt standard true false Oracle9i true
 
 sh ~j2eemgr/kuali/kfs/do-daily-endowment-updates.sh
 
