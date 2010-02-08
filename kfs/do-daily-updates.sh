60b7695cac724a906b9535df9b7baef184441a71
jkeller <jkeller@kuali.org>
2010-02-08 12:22:38 -0500

    Restored STG deployment to branch 3.0.1.1
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5617 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-daily-updates.sh
+++ kfs/do-daily-updates.sh
@@ -10,9 +10,9 @@ kdailytag trunk
 typeset -x APPLICATION_SERVER_NAME=kuali23
 kupdate cnv standard true true Oracle9i true
 
-#typeset -x APPLICATION_SERVER_NAME=kuali2
-#typeset -x APPLICATION_INFRASTRUCTURE_VERSION=1.0.1.1
-#kupdate stg branches/release-3-0-1 true true Oracle9i true branches/rice-release-1-0-1-1-br
+typeset -x APPLICATION_SERVER_NAME=kuali2
+typeset -x APPLICATION_INFRASTRUCTURE_VERSION=1.0.1.1
+kupdate stg branches/release-3-0-1-1 true true Oracle9i true branches/rice-release-1-0-1-1-br
 
 # take from trunk
 kupdate unt standard true false Oracle9i true
