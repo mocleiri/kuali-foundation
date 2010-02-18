6483d7c52cc201cd9e03bebbc6951816eb347557
huangb <huangb@kuali.org>
2010-02-17 21:36:07 -0500

    turn on the daily deployment
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5632 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-daily-endowment-updates.sh
+++ kfs/do-daily-endowment-updates.sh
@@ -5,8 +5,8 @@
 . ~j2eemgr/kuali/shared-functions
 
 #kbranch standard endowment
-#typeset -x APPLICATION_SERVER_NAME=$DEV2_APPLICATION_SERVER_NAME
-#typeset -x APPLICATION_INFRASTRUCTURE_VERSION=1.0.1
-#kupdate dev2 branches/endowment-build-1304 false true Oracle9i true branches/rice-release-1-0-1-br
+typeset -x APPLICATION_SERVER_NAME=$DEV2_APPLICATION_SERVER_NAME
+typeset -x APPLICATION_INFRASTRUCTURE_VERSION=1.0.1
+kupdate dev2 branches/endowment-build-1304 false true Oracle9i true branches/rice-release-1-0-1-br
 
 kend 0
