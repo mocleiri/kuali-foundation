01554df1e6ea19abdd709e4d85b8759ec39807e6
huangb <huangb@kuali.org>
2010-02-18 14:34:54 -0500

    turn off the hourly deployment since it is out of synch with do-hourly-updates.sh.
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5637 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kfs/do-hourly-endowment-updates.sh
+++ kfs/do-hourly-endowment-updates.sh
@@ -4,9 +4,9 @@
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 
-typeset -x APPLICATION_SERVER_NAME=$DEV2_APPLICATION_SERVER_NAME
-typeset -x APPLICATION_INFRASTRUCTURE_VERSION=1.0.1
-kupdate dev2 branches/endowment-build-1304 false true Oracle9i true branches/rice-release-1-0-1-br
+#typeset -x APPLICATION_SERVER_NAME=$DEV2_APPLICATION_SERVER_NAME
+#typeset -x APPLICATION_INFRASTRUCTURE_VERSION=1.0.1
+#kupdate dev2 branches/endowment-build-1304 false true Oracle9i true branches/rice-release-1-0-1-br
 
 
 kend 0
