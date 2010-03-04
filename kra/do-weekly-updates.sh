425e1c3830aaf707cb8eacc88cb3e7670eb95a1e
aakers <aakers@kuali.org>
2010-03-04 18:34:04 -0500

    Commenting out kra-reg deployment for now
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5645 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kra/do-weekly-updates.sh
+++ kra/do-weekly-updates.sh
@@ -6,7 +6,7 @@
 . ~j2eemgr/kuali/shared-functions
 
 kupdate reg standard true false Oracle9i true
-buildsvnwebapp reg
-kmail 'kc.dev@kuali.org,kcqa@kuali.org' "weekly kra reg build completed" "weekly kra reg build completed, it's ok to test on it."
+#buildsvnwebapp reg
+#kmail 'kc.dev@kuali.org,kcqa@kuali.org' "weekly kra reg build completed" "weekly kra reg build completed, it's ok to test on it."
 
 kend 0
