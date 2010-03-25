2d2306872f79fb69e5100382cbde2fb2cd7c74c1
caseyhb <caseyhb@kuali.org>
2010-03-25 17:07:06 -0400

    KULCFG-437
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5650 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-standard-110-updates.sh
+++ rice/do-daily-standard-110-updates.sh
@@ -5,10 +5,13 @@
 . ~j2eemgr/kuali/shared-settings
 . ~j2eemgr/kuali/shared-functions
 BUILD_NUMBER_FILE=$APPLICATION_DIRECTORY/$JAVA_APPLICATION_CODE-build-number.txt; typeset -rx BUILD_NUMBER_FILE
+JDK_HOME=/opt/cm-tools/java/jdk6
+JAVA_HOME=/opt/cm-tools/java/jdk6
+PATH=$JAVA_HOME/bin:$PATH
+
 kexport dba $APPLICATION_INFRASTRUCTURE_SRCCTRL_VERSION
 kdailytag $APPLICATION_INFRASTRUCTURE_SRCCTRL_VERSION
 kupdate cnv standard true true Oracle9i false
-
 
 kpurge $LOGS_DIRECTORY 15
 
