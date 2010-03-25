c6589fa11e23fa04d664cb428161c2ad284f42ca
caseyhb <caseyhb@kuali.org>
2010-03-25 13:18:16 -0400

    KULRICE-4053
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5649 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-updates.sh
+++ rice/do-daily-updates.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sh ~j2eemgr/kuali/rice/do-daily-kuali-client-updates.sh
-sh ~j2eemgr/kuali/rice/do-daily-standard-updates.sh
+sh ~j2eemgr/kuali/rice/do-daily-standard-1011-updates.sh
+sh ~j2eemgr/kuali/rice/do-daily-standard-110-updates.sh
 sh ~j2eemgr/kuali/rice/do-daily-kc-updates.sh
 sh ~j2eemgr/kuali/rice/do-daily-sample-app-updates.sh
