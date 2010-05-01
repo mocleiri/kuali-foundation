6805f60339d626bc7bbaea92170d602b04a73054
caseyhb <caseyhb@kuali.org>
2010-05-01 04:07:14 -0400

    database changes anticipated prior to release.
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy/cfg-envs/trunk@5655 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- rice/do-daily-updates.sh
+++ rice/do-daily-updates.sh
@@ -3,5 +3,5 @@
 sh ~j2eemgr/kuali/rice/do-daily-kuali-client-updates.sh
 sh ~j2eemgr/kuali/rice/do-daily-standard-1011-updates.sh
 sh ~j2eemgr/kuali/rice/do-daily-standard-110-updates.sh
-sh ~j2eemgr/kuali/rice/do-daily-kc-updates.sh
+#sh ~j2eemgr/kuali/rice/do-daily-kc-updates.sh
 sh ~j2eemgr/kuali/rice/do-daily-sample-app-updates.sh
