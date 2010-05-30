b022e6ba47e72e1f7d0199ce84cea399e5c02a09
jkeller <jkeller@kuali.org>
2010-05-30 19:59:11 -0400

    Restored some support for restarts and export data filters.
    git-svn-id: https://svn.kuali.org/repos/foundation/db-utils/branches/clover-integration@7185 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- clover/src/org/kuali/impex/ant/RunExportGraphs.java
+++ clover/src/org/kuali/impex/ant/RunExportGraphs.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Vector;
 import java.util.concurrent.Future;
+import java.util.regex.Pattern;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.DirectoryScanner;
@@ -21,7 +22,8 @@ import org.kuali.impex.ETLHelper;
 public class RunExportGraphs extends EtlJdbcTask {
 
 	public boolean verbose = false;
-	
+	public String graphExclusionRegex = ""; 
+	public Pattern graphExclusionPattern;
 	public Vector<FileSet> graphFiles = new Vector<FileSet>();
 	
 	public void addFileSet( FileSet fileSet ) {
@@ -39,16 +41,20 @@ public class RunExportGraphs extends EtlJdbcTask {
 			File dir = ds.getBasedir();
 			String[] filesInSet = ds.getIncludedFiles();
 			for ( String fileName : filesInSet ) {
-				if ( !ETLHelper.isTableEmpty(getConnection(), getSchemaName(), fileName.replace("_export.grf.xml", "" ) ) ) {
-					File file = new File( dir, fileName );
-					log( "Running graph: " + fileName );
-					try {
-						runGraph(file,graphReader);
-					} catch ( Exception ex ) {
-						throw new BuildException( file.toString(), ex );
+				if ( graphExclusionPattern != null && !graphExclusionPattern.matcher(fileName).matches() ) {
+					if ( !ETLHelper.isTableEmpty(getConnection(), getSchemaName(), fileName.replace("_export.grf.xml", "" ) ) ) {
+						File file = new File( dir, fileName );
+						log( "Running graph: " + fileName );
+						try {
+							runGraph(file,graphReader);
+						} catch ( Exception ex ) {
+							throw new BuildException( file.toString(), ex );
+						}
+					} else {
+						log( "Skipping graph: " + fileName + " -- Table Empty" );
 					}
 				} else {
-					log( "Skipping graph: " + fileName + " -- Table Empty" );
+					log( "Skipping graph: " + fileName + " excluded by Regex" );
 				}
 			}
 		}
@@ -86,5 +92,14 @@ public class RunExportGraphs extends EtlJdbcTask {
 
 	public void setVerbose(boolean verbose) {
 		this.verbose = verbose;
+	}
+
+	public String getGraphExclusionRegex() {
+		return graphExclusionRegex;
+	}
+
+	public void setGraphExclusionRegex(String graphExclusionRegex) {
+		this.graphExclusionRegex = graphExclusionRegex;
+		graphExclusionPattern = Pattern.compile(graphExclusionRegex, Pattern.CASE_INSENSITIVE);
 	}
 }
