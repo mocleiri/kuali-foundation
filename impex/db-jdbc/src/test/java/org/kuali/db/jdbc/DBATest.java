598c0c0b3ab8cf1961f1355774f29e911b62dae3
jcaddel <jcaddel@kuali.org>
2011-06-03 19:58:37 -0400

    More unit testing
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@1043 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- impex/db-jdbc/src/test/java/org/kuali/db/jdbc/DBATest.java
+++ impex/db-jdbc/src/test/java/org/kuali/db/jdbc/DBATest.java
@@ -2,27 +2,14 @@ package org.kuali.db.jdbc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.kuali.spring.util.PropertiesPlaceholderConfigurer;
-import org.kuali.spring.util.PropertyLogger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class DBATest {
-	@Autowired
-	private ApplicationContext applicationContext;
 
 	@Test
 	public void importTest() {
-		PropertyLogger plogger = (PropertyLogger) applicationContext.getBean("kuali.plogger");
-		System.out.println(plogger.isFlattenPropertyValues());
-		PropertiesPlaceholderConfigurer pphc = (PropertiesPlaceholderConfigurer) applicationContext
-				.getBean("kuali.pconfigurer");
-		PropertyLogger plogger2 = pphc.getLoader().getPlogger();
-		System.out.println(plogger2.isFlattenPropertyValues());
-
 	}
 }
