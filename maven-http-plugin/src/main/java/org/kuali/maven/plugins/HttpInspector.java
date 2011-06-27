b959f43c37dd3f74b0cd5f3037b2f2c3a954a882
jcaddel <jcaddel@kuali.org>
2011-06-27 00:51:56 -0400

    Initial commit
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@1082 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-http-plugin/src/main/java/org/kuali/maven/plugins/HttpInspector.java
@@ -0,0 +1,139 @@
+package org.kuali.maven.plugins;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpMethodRetryHandler;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpClientParams;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpInspector {
+	private final Logger logger = LoggerFactory.getLogger(HttpInspector.class);
+	String dateFormat = "yyyy-MM-dd HH:mm:ss z";
+	List<Integer> successCodes = new ArrayList<Integer>();
+	int requestTimeout = 3000;
+	int sleepInterval = 3000;
+	int timeout = 180;
+
+	public void setDateFormat(String dateFormat) {
+		this.dateFormat = dateFormat;
+	}
+
+	protected boolean isSuccess(int resultCode) {
+		for (int successCode : successCodes) {
+			if (resultCode == successCode) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected String getMsg(String msg, SimpleDateFormat sdf) {
+		return getMsg(msg, -1, sdf);
+	}
+
+	protected String getMsg(String msg, long l, SimpleDateFormat sdf) {
+		StringBuilder sb = new StringBuilder();
+		if (l != -1) {
+			sb.append(l + " - ");
+		}
+		sb.append(msg);
+		sb.append(" - ");
+		sb.append(sdf.format(new Date()));
+		return sb.toString();
+	}
+
+	public boolean wait(String url) {
+		SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+		long now = System.currentTimeMillis();
+		long end = now + (timeout * 1000);
+		HttpClient client = new HttpClient();
+		HttpClientParams clientParams = client.getParams();
+		HttpMethodRetryHandler retryHandler = new DefaultHttpMethodRetryHandler(0, false);
+		clientParams.setParameter(HttpMethodParams.RETRY_HANDLER, retryHandler);
+		clientParams.setParameter(HttpMethodParams.SO_TIMEOUT, requestTimeout);
+		logger.info(getMsg("Determining status for '" + url + "'", sdf));
+		for (;;) {
+			long secondsRemaining = (long) Math.ceil((end - System.currentTimeMillis()) / 1000D);
+			try {
+				HttpMethod method = new GetMethod(url);
+				client.executeMethod(method);
+				int statusCode = method.getStatusCode();
+				String statusText = method.getStatusText();
+				boolean success = isSuccess(statusCode);
+				if (success) {
+					logger.info(getMsg("Status for '" + url + "' is '" + statusCode + ":" + statusText + "'", sdf));
+					return true;
+				} else {
+					logger.info(getMsg("Status for '" + url + "' is '" + statusCode + ":" + statusText + "'",
+							secondsRemaining, sdf));
+				}
+			} catch (IOException e) {
+				logger.info(getMsg("Status for '" + url + "' is '" + e.getMessage() + "'", secondsRemaining, sdf));
+			}
+			sleep(sleepInterval);
+			if (System.currentTimeMillis() > end) {
+				logger.info("Timed out waiting for response from '" + url + "'");
+				break;
+			}
+		}
+		return false;
+	}
+
+	protected void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public int getRequestTimeout() {
+		return requestTimeout;
+	}
+
+	public void setRequestTimeout(int requestTimeout) {
+		this.requestTimeout = requestTimeout;
+	}
+
+	public int getSleepInterval() {
+		return sleepInterval;
+	}
+
+	public void setSleepInterval(int sleepInterval) {
+		this.sleepInterval = sleepInterval;
+	}
+
+	public int getTimeout() {
+		return timeout;
+	}
+
+	public void setTimeout(int waitTimeout) {
+		this.timeout = waitTimeout;
+	}
+
+	public Logger getLogger() {
+		return logger;
+	}
+
+	public String getDateFormat() {
+		return dateFormat;
+	}
+
+	public List<Integer> getSuccessCodes() {
+		return successCodes;
+	}
+
+	public void setSuccessCodes(List<Integer> successCodes) {
+		this.successCodes = successCodes;
+	}
+}
---
+++ maven-http-plugin/src/main/java/org/kuali/maven/plugins/WaitMojo.java
@@ -0,0 +1,138 @@
+package org.kuali.maven.plugins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Repeatedly contact the specified HTTP URL until a valid HTTP response code is returned or the maximum wait timeout is
+ * exceeded, whichever comes first.
+ * 
+ * @author Jeff Caddel
+ * @goal wait
+ * @execute phase="validate"
+ * @since 1.0
+ */
+public class WaitMojo extends AbstractMojo {
+
+	/**
+	 * The url to contact
+	 * 
+	 * @parameter expression="${http.url}" default-value="http://localhost"
+	 */
+	private String url;
+
+	/**
+	 * The maximum number of seconds to wait for the url to respond correctly
+	 * 
+	 * @parameter expression="${http.timeout}" default-value="180"
+	 */
+	private int timeout;
+
+	/**
+	 * The maximum number of milliseconds to wait for an individual HTTP request to complete
+	 * 
+	 * @parameter expression="${http.requestTimeout}" default-value="3000"
+	 */
+	private int requestTimeout;
+
+	/**
+	 * The number of milliseconds to sleep in between HTTP requests
+	 * 
+	 * @parameter expression="${http.sleepInterval}" default-value="3000"
+	 */
+	private int sleepInterval;
+
+	/**
+	 * Comma separated list of HTTP status codes that represent success
+	 * 
+	 * @parameter expression="${http.successCodes}" default-value="200,302,304"
+	 */
+	private String httpSuccessCodes;
+
+	/**
+	 * The format to use when displaying dates in the log messages
+	 * 
+	 * @parameter expression="${http.dateFormat}" default-value="yyyy-MM-dd HH:mm:ss z"
+	 */
+	private String dateFormat;
+
+	protected HttpInspector getHttpInspector() throws MojoExecutionException {
+		HttpInspector inspector = new HttpInspector();
+		try {
+			BeanUtils.copyProperties(inspector, this);
+		} catch (Exception e) {
+			throw new MojoExecutionException("Error copying properties", e);
+		}
+		String[] successCodeStrings = StringUtils.splitByWholeSeparator(httpSuccessCodes, ",");
+		List<Integer> successCodeList = new ArrayList<Integer>();
+		for (String successCodeString : successCodeStrings) {
+			successCodeList.add(new Integer(successCodeString));
+		}
+		inspector.setSuccessCodes(successCodeList);
+		return inspector;
+	}
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		HttpInspector inspector = getHttpInspector();
+		boolean success = inspector.wait(url);
+		if (!success) {
+			throw new MojoExecutionException("Waiting for a response from '" + url + "' was not successful");
+		}
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getTimeout() {
+		return timeout;
+	}
+
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
+
+	public int getRequestTimeout() {
+		return requestTimeout;
+	}
+
+	public void setRequestTimeout(int requestTimeout) {
+		this.requestTimeout = requestTimeout;
+	}
+
+	public int getSleepInterval() {
+		return sleepInterval;
+	}
+
+	public void setSleepInterval(int sleepInterval) {
+		this.sleepInterval = sleepInterval;
+	}
+
+	public String getDateFormat() {
+		return dateFormat;
+	}
+
+	public void setDateFormat(String dateFormat) {
+		this.dateFormat = dateFormat;
+	}
+
+	public String getHttpSuccessCodes() {
+		return httpSuccessCodes;
+	}
+
+	public void setHttpSuccessCodes(String httpSuccessCodes) {
+		this.httpSuccessCodes = httpSuccessCodes;
+	}
+
+}
