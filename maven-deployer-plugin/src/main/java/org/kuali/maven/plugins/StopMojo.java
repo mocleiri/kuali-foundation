a20a96ffbc2f0ad0503bdfab2c61fb218833bf4c
jcaddel <jcaddel@kuali.org>
2011-09-30 22:53:09 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@1987 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-deployer-plugin/src/main/java/org/kuali/maven/plugins/StopMojo.java
@@ -0,0 +1,18 @@
+package org.kuali.maven.plugins;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ *
+ * @author Jeff Caddel
+ * @goal stop
+ */
+public class StopMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+    }
+
+}
