69b78af701aaebe3f5817e6b8cb2788066505a76
jcaddel <jcaddel@kuali.org>
2011-10-02 13:45:10 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2069 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-oracle-plugin/src/main/java/org/kuali/maven/plugins/DeployJarsMojo.java
@@ -0,0 +1,17 @@
+package org.kuali.maven.plugins;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @author Jeff Caddel
+ * @goal deployjars
+ */
+public class DeployJarsMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+    }
+
+}
