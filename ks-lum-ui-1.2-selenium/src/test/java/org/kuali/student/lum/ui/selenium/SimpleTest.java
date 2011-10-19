f6f36cc9ccf6824927bc29e35503d217024af07a
jcaddel <jcaddel@kuali.org>
2011-10-19 16:29:30 -0400

    Version 2.8.0 of seleniumhq
    git-svn-id: https://svn.kuali.org/repos/foundation/ks@2359 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/SimpleTest.java
+++ ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/SimpleTest.java
@@ -12,41 +12,45 @@ public class SimpleTest {
 
     @Test
     public void googleTest() {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-        WebDriver driver = new FirefoxDriver();
+        try {
+            // Create a new instance of the Firefox driver
+            // Notice that the remainder of the code relies on the interface,
+            // not the implementation.
+            WebDriver driver = new FirefoxDriver();
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+            // And now use this to visit Google
+            driver.get("http://www.google.com");
+            // Alternatively the same thing can be done like this
+            // driver.navigate().to("http://www.google.com");
 
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
+            // Find the text input element by its name
+            WebElement element = driver.findElement(By.name("q"));
 
-        // Enter something to search for
-        element.sendKeys("Cheese!");
+            // Enter something to search for
+            element.sendKeys("Cheese!");
 
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
+            // Now submit the form. WebDriver will find the form for us from the element
+            element.submit();
 
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
+            // Check the title of the page
+            System.out.println("Page title is: " + driver.getTitle());
 
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            @Override
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("cheese!");
-            }
-        });
+            // Google's search is rendered dynamically with JavaScript.
+            // Wait for the page to load, timeout after 10 seconds
+            (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+                @Override
+                public Boolean apply(WebDriver d) {
+                    return d.getTitle().toLowerCase().startsWith("cheese!");
+                }
+            });
 
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
+            // Should see: "cheese! - Google Search"
+            System.out.println("Page title is: " + driver.getTitle());
 
-        // Close the browser
-        driver.quit();
+            // Close the browser
+            driver.quit();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 }
