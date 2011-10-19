e9058fb59ebfa99a8468b7141470625e83237c56
jcaddel <jcaddel@kuali.org>
2011-10-19 18:48:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/ks@2379 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/LoginNewTest.java
+++ ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/LoginNewTest.java
@@ -1,24 +1,43 @@
 package org.kuali.student.lum.ui.selenium;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginNewTest {
 
     @Test
     public void simpleTest() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("http://dev.ks.kuali.org/login.jsp");
-        WebElement logonUserName = driver.findElement(By.id("j_username"));
-        typeValue(logonUserName, "admin");
-        WebElement logonPassword = driver.findElement(By.id("j_password"));
-        typeValue(logonPassword, "admin");
-        logonPassword.submit();
-        String title = driver.getTitle();
-        driver.quit();
+        WebDriver driver = null;
+        try {
+            driver = new FirefoxDriver();
+            driver.get("http://dev.ks.kuali.org/login.jsp");
+            WebElement logonUserName = driver.findElement(By.id("j_username"));
+            WebElement logonPassword = driver.findElement(By.id("j_password"));
+            typeValue(logonUserName, "admin");
+            typeValue(logonPassword, "admin");
+            logonPassword.submit();
+            Wait<WebDriver> wait = new WebDriverWait(driver, 10);
+            ExpectedCondition<Boolean> condition = new TitlePresentCondition("Kuali Student: Home");
+            Assert.assertTrue(wait.until(condition));
+        } finally {
+            quitQuietly(driver);
+        }
+    }
+
+    private void quitQuietly(WebDriver driver) {
+        if (driver == null) {
+            return;
+        } else {
+            driver.quit();
+        }
     }
 
     private void typeValue(final WebElement webElement, CharSequence... keysToSend) {
