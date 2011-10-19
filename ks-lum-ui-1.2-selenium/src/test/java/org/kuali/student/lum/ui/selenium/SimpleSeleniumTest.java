47ed75a351f1e1fcdffa315001acb79c13149aaf
jcaddel <jcaddel@kuali.org>
2011-10-19 17:34:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/ks@2366 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/SimpleSeleniumTest.java
+++ ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/SimpleSeleniumTest.java
@@ -10,28 +10,11 @@ import com.thoughtworks.selenium.Selenium;
 public class SimpleSeleniumTest {
 
     @Test
-    public void testSomethingSimple() throws Exception {
+    public void googleTest() throws Exception {
         Selenium selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://www.google.com");
         selenium.start();
-
-        //
-        // This is an exmaple of testing the Apache Geroniom Welcome page for specific text
-        //
-
-        selenium.click("link=JVM");
         selenium.waitForPageToLoad("30000");
-        selenium.click("link=Welcome");
-        selenium.waitForPageToLoad("30000");
-        Assert.assertEquals("Geronimo Console", selenium.getTitle());
-        Assert.assertEquals(
-                "Welcome",
-                selenium.getText("xpath=/html/body/table[@id='rootfragment']/tbody/tr[2]/td/table/tbody/tr[2]/td[4]/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/table/tbody/tr/td[2]/table/tbody/tr/td[1]/strong"));
-
-        // Test help link
-        selenium.click("link=help");
-        selenium.waitForPageToLoad("30000");
-        selenium.isTextPresent("This is the help for the Geronimo Administration Console Welcome.");
-
+        Assert.assertEquals("Google", selenium.getTitle());
         selenium.stop();
     }
 }
