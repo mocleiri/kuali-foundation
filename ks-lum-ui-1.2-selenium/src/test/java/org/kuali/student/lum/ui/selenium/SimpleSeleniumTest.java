da93a5ccec887d800b0f67a47deb16b40654ea30
jcaddel <jcaddel@kuali.org>
2011-10-19 17:44:23 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/ks@2370 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/SimpleSeleniumTest.java
+++ ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/SimpleSeleniumTest.java
@@ -2,16 +2,24 @@ package org.kuali.student.lum.ui.selenium;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.Selenium;
 
 public class SimpleSeleniumTest {
+    Selenium selenium;
+
+    @Before
+    public void setUp() throws Exception {
+        selenium = new DefaultSelenium("localhost", 4444, "*firefox", System.getProperty("remote.public.url"));
+        selenium.start();
+    }
 
     @Test
     public void googleTest() throws Exception {
-        Selenium selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://www.google.com");
+        selenium.open("/");
         selenium.start();
         selenium.waitForPageToLoad("30000");
         Assert.assertEquals("Google", selenium.getTitle());
