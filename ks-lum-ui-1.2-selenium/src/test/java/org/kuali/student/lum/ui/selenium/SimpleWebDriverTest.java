df0837eca0b9496a8caea88222428c67a3e04b43
jcaddel <jcaddel@kuali.org>
2011-10-19 17:59:05 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/ks@2374 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/SimpleWebDriverTest.java
+++ ks-lum-ui-1.2-selenium/src/test/java/org/kuali/student/lum/ui/selenium/SimpleWebDriverTest.java
@@ -1,54 +1,19 @@
 package org.kuali.student.lum.ui.selenium;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SimpleWebDriverTest {
 
     @Test
     public void googleTest() {
-        try {
-            // Create a new instance of the Firefox driver
-            // Notice that the remainder of the code relies on the interface,
-            // not the implementation.
-            WebDriver driver = new FirefoxDriver();
-
-            // And now use this to visit Google
-            driver.get("http://www.google.com");
-            // Alternatively the same thing can be done like this
-            // driver.navigate().to("http://www.google.com");
-
-            // Find the text input element by its name
-            WebElement element = driver.findElement(By.name("q"));
-
-            // Enter something to search for
-            element.sendKeys("Cheese!");
-
-            // Now submit the form. WebDriver will find the form for us from the element
-            element.submit();
-
-            // Check the title of the page
-            System.out.println("Page title is: " + driver.getTitle());
-
-            // Google's search is rendered dynamically with JavaScript.
-            // Wait for the page to load, timeout after 10 seconds
-            Wait<WebDriver> wait = new WebDriverWait(driver, 10);
-            ExpectedCondition<Boolean> condition = new CheeseTitlePresentCondition();
-            wait.until(condition);
-
-            // Should see: "cheese! - Google Search"
-            System.out.println("Page title is: " + driver.getTitle());
-
-            // Close the browser
-            driver.quit();
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        String title = driver.getTitle();
+        Assert.assertEquals("Google", title);
+        driver.quit();
     }
 }
