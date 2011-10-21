d90304276d1a8692bac3065ef4c276cb8961ce81
jcaddel <jcaddel@kuali.org>
2011-10-21 01:33:14 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2444 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/config/SandboxConfig.java
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/config/SandboxConfig.java
@@ -5,6 +5,6 @@ public class SandboxConfig extends DefaultConfig {
         super();
         setApiKey("549de3da-8eae-4350-b20e-3d5c31f7117e");
         setSecretKey("58dca3a2-650e-4919-bcfb-d6e6e61f4fc2");
-        setBaseUrl("http://api.sandbox.dnsmadeeasy.com/V1.2/domains/");
+        setBaseUrl("http://api.sandbox.dnsmadeeasy.com/V1.2/domains/kuali.org/records/");
     }
 }
