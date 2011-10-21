3e0fc128638f188bc7895337d2aab0ec48263bc0
jcaddel <jcaddel@kuali.org>
2011-10-20 23:05:14 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2434 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/ProductionConfig.java
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/ProductionConfig.java
@@ -5,6 +5,6 @@ public class ProductionConfig extends DefaultConfig {
         super();
         setApiKey("454f2836-81c0-4379-b8dd-2cc6495131b5");
         setSecretKey("5fc3245f-78b5-4c92-9053-be0b5c64a680");
-        setBaseUrl("http://api.dnsmadeeasy.com/V1.2/domains/");
+        setBaseUrl("http://api.dnsmadeeasy.com/V1.2/domains");
     }
 }
--- maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/SandboxConfig.java
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/SandboxConfig.java
@@ -3,8 +3,8 @@ package org.kuali.maven.plugins;
 public class SandboxConfig extends DefaultConfig {
     public SandboxConfig() {
         super();
-        setApiKey("454f2836-81c0-4379-b8dd-2cc6495131b5");
-        setSecretKey("5fc3245f-78b5-4c92-9053-be0b5c64a680");
+        setApiKey("549de3da-8eae-4350-b20e-3d5c31f7117e");
+        setSecretKey("58dca3a2-650e-4919-bcfb-d6e6e61f4fc2");
         setBaseUrl("http://api.dnsmadeeasy.com/V1.2/domains/");
     }
 }
