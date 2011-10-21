fb7084048169b247f02a8befc828a5e1dc2ec556
jcaddel <jcaddel@kuali.org>
2011-10-21 14:53:04 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2460 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/config/PersonalConfig.java
@@ -0,0 +1,10 @@
+package org.kuali.maven.plugins.dnsme.config;
+
+public class PersonalConfig extends DefaultConfig {
+    public PersonalConfig() {
+        super();
+        setApiKey("bad6fd4f-1ca1-4e5d-880e-585afe7d070e");
+        setSecretKey("856ce7ab-8a2b-4a81-b1ea-b1e9f2b04618");
+        setBaseUrl("http://api.sandbox.dnsmadeeasy.com/V1.2/domains/");
+    }
+}
