aa1d3c80ee05ab7218242e62e4289a36aedea4b6
jcaddel <jcaddel@kuali.org>
2011-10-21 16:23:51 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2461 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/test/java/org/kuali/maven/plugins/ApiTest.java
+++ maven-dnsme-plugin/src/test/java/org/kuali/maven/plugins/ApiTest.java
@@ -6,6 +6,7 @@ import org.apache.commons.httpclient.HttpClient;
 import org.junit.Assert;
 import org.junit.Test;
 import org.kuali.maven.plugins.dnsme.config.Config;
+import org.kuali.maven.plugins.dnsme.config.PersonalConfig;
 import org.kuali.maven.plugins.dnsme.config.ProductionConfig;
 import org.kuali.maven.plugins.dnsme.config.SampleConfig;
 import org.kuali.maven.plugins.dnsme.config.SandboxConfig;
@@ -17,13 +18,36 @@ public class ApiTest {
      * http://cp.dnsmadeeasy.com/enterprisedns/api.html
      */
     @Test
-    public void testHeaderValues() {
+    public void testSampleMatch() {
         String date = "Sat, 12 Feb 2011 20:59:04 GMT";
         Config config = new SampleConfig();
         Api api = new Api(config);
         String hmac = api.getHMACSHA1Hash(date);
         String expectedHmac = "b3502e6116a324f3cf4a8ed693d78bcee8d8fe3c";
         Assert.assertEquals(expectedHmac, hmac);
+    }
+
+    @Test
+    public void testPerlMatch() {
+        String date = "Fri, 21 Oct 2011 20:05:09 GMT";
+        Config config = new SandboxConfig();
+        Api api = new Api(config);
+        String hmac = api.getHMACSHA1Hash(date);
+        String expectedHmac = "33235fef00493d52ca9200da7ef2fbadd379ef2e";
+        Assert.assertEquals(expectedHmac, hmac);
+    }
+
+    @Test
+    public void testPersonalConfig() {
+        try {
+            HttpInspector inspector = new HttpInspector();
+            HttpClient client = inspector.getDefaultHttpClient();
+            Config personal = new PersonalConfig();
+            HttpRequestResult result = inspector.doDNSMERequest(client, personal);
+            inspector.log(personal.getBaseUrl(), result, -1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // @Test
