6008da0eff6db35bf51d3497fc95ff25b5bb837a
jcaddel <jcaddel@kuali.org>
2011-10-23 02:47:45 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2488 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/Domain.java
@@ -0,0 +1,34 @@
+package org.kuali.maven.plugins.dnsme;
+
+import java.util.List;
+
+public class Domain {
+    String name;
+    List<String> nameServers;
+    Boolean gtdEnabled;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getNameServers() {
+        return nameServers;
+    }
+
+    public void setNameServers(List<String> nameServers) {
+        this.nameServers = nameServers;
+    }
+
+    public Boolean getGtdEnabled() {
+        return gtdEnabled;
+    }
+
+    public void setGtdEnabled(Boolean gtdEnabled) {
+        this.gtdEnabled = gtdEnabled;
+    }
+
+}
---
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/Record.java
@@ -0,0 +1,112 @@
+package org.kuali.maven.plugins.dnsme;
+
+public class Record {
+    int id;
+    String name;
+    String type;
+    String data;
+    int ttl;
+    String gtdLocation;
+    String password;
+    String description;
+    String keywords;
+    String title;
+    String redirectType;
+    Boolean hardLink;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(int ttl) {
+        this.ttl = ttl;
+    }
+
+    public String getGtdLocation() {
+        return gtdLocation;
+    }
+
+    public void setGtdLocation(String gtdLocation) {
+        this.gtdLocation = gtdLocation;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRedirectType() {
+        return redirectType;
+    }
+
+    public void setRedirectType(String redirectType) {
+        this.redirectType = redirectType;
+    }
+
+    public Boolean getHardLink() {
+        return hardLink;
+    }
+
+    public void setHardLink(Boolean hardLink) {
+        this.hardLink = hardLink;
+    }
+}
