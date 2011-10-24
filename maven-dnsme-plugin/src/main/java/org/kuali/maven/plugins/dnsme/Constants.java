5e38b1dfa287e9992817cc0c7ea5d3859b3392ef
jcaddel <jcaddel@kuali.org>
2011-10-24 11:34:55 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2510 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/Constants.java
@@ -0,0 +1,6 @@
+package org.kuali.maven.plugins.dnsme;
+
+public class Constants {
+    public static final String PRODUCTION_URL = "http://api.dnsmadeeasy.com/V1.2";
+    public static final String SANDBOX_URL = "http://api.sandbox.dnsmadeeasy.com/V1.2";
+}
