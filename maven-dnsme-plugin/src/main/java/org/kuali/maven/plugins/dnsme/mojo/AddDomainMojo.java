18b6a29fd0e39b5ea3feacca9f5b6eba13a6ab59
jcaddel <jcaddel@kuali.org>
2011-10-25 18:29:29 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2573 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/mojo/AddDomainMojo.java
@@ -0,0 +1,30 @@
+package org.kuali.maven.plugins.dnsme.mojo;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.kuali.maven.plugins.dnsme.DNSMEClient;
+import org.kuali.maven.plugins.dnsme.beans.Domain;
+
+/**
+ * @author Jeff Caddel
+ * @goal adddomain
+ */
+public class AddDomainMojo extends BaseDNSMEMojo {
+
+    /**
+     * The url for the DNSME Rest API
+     *
+     * @parameter expression="${dnsme.domainName}"
+     * @required
+     */
+    String domainName;
+
+    @Override
+    public void performTasks(DNSMEClient client) throws MojoExecutionException, MojoFailureException {
+        Domain domain = new Domain();
+        domain.setName(domainName);
+        Domain addedDomain = client.addDomain(domain);
+        getLog().info("Added: " + addedDomain.getName());
+    }
+
+}
