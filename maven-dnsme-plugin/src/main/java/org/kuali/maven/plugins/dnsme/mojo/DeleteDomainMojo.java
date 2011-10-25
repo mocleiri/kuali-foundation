03e204857686273f14ee376a594c3c5aa9512797
jcaddel <jcaddel@kuali.org>
2011-10-25 18:32:47 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2574 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/mojo/DeleteDomainMojo.java
@@ -0,0 +1,30 @@
+package org.kuali.maven.plugins.dnsme.mojo;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.kuali.maven.plugins.dnsme.DNSMEClient;
+import org.kuali.maven.plugins.dnsme.beans.Domain;
+
+/**
+ * @author Jeff Caddel
+ * @goal deletedomain
+ */
+public class DeleteDomainMojo extends BaseDNSMEMojo {
+
+    /**
+     * The url for the DNSME Rest API
+     *
+     * @parameter expression="${dnsme.domainName}"
+     * @required
+     */
+    String domainName;
+
+    @Override
+    public void performTasks(DNSMEClient client) throws MojoExecutionException, MojoFailureException {
+        Domain domain = new Domain();
+        domain.setName(domainName);
+        client.deleteDomain(domain);
+        getLog().info("Deleted: " + domainName);
+    }
+
+}
