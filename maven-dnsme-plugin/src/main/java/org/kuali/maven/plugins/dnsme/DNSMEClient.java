2b75432bd5e69ba856790760644b950d1230d0f8
jcaddel <jcaddel@kuali.org>
2011-10-24 20:56:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2529 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/DNSMEClient.java
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/DNSMEClient.java
@@ -1,8 +1,13 @@
 package org.kuali.maven.plugins.dnsme;
 
+import com.google.gson.Gson;
+
 public class DNSMEClient {
 
     String url;
+    Gson gson = new Gson();
+    HttpUtil http = new HttpUtil();
+    DNSMEUtil dnsme = new DNSMEUtil();
 
     public static DNSMEClient getInstance(Environment env) {
         return new DNSMEClient(env.getUrl());
@@ -13,4 +18,9 @@ public class DNSMEClient {
         this.url = url;
     }
 
+    public List<Domain> getDomains() {
+        String url = this.url + "/domains";
+        Method method = dnsme.get 
+        
+    }
 }
