110d28b528bd44c37457e6b06f7252fdee603599
jcaddel <jcaddel@kuali.org>
2011-10-25 13:33:37 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2551 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/test/java/org/kuali/maven/plugins/dnsme/DNSMEClientTest.java
+++ maven-dnsme-plugin/src/test/java/org/kuali/maven/plugins/dnsme/DNSMEClientTest.java
@@ -2,6 +2,8 @@ package org.kuali.maven.plugins.dnsme;
 
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.kuali.maven.plugins.dnsme.accounts.SandboxAccount;
 import org.kuali.maven.plugins.dnsme.beans.Account;
@@ -12,36 +14,26 @@ public class DNSMEClientTest {
 
     @Test
     public void testAddRecord() {
-        try {
-            Account account = new SandboxAccount();
-            String restApiUrl = Constants.SANDBOX_URL;
-            DNSMEClient client = DNSMEClient.getInstance(account, restApiUrl);
-            Domain domain = new Domain();
-            domain.setName("foomanchu.com");
-            Record record = new Record();
-            record.setName("foo.bar");
-            record.setTtl(60);
-            record.setData("foo.aws.amazon.com");
-            record.setType(RecordType.CNAME);
-            Record resultRecord = client.addRecord(domain, record);
-            System.out.println(resultRecord.getId());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Account account = new SandboxAccount();
+        String restApiUrl = Constants.SANDBOX_URL;
+        DNSMEClient client = DNSMEClient.getInstance(account, restApiUrl);
+        Domain domain = new Domain();
+        domain.setName("foomanchu.com");
+        Record record = new Record();
+        record.setName("foo.bar");
+        record.setTtl(60);
+        record.setData("foo.aws.amazon.com");
+        record.setType(RecordType.CNAME);
+        Record resultRecord = client.addRecord(domain, record);
+        Assert.assertNotNull(resultRecord.getId());
     }
 
     // @Test
     public void testGetDomains() {
-        try {
-            Account account = new SandboxAccount();
-            String restApiUrl = Constants.SANDBOX_URL;
-            DNSMEClient client = DNSMEClient.getInstance(account, restApiUrl);
-            List<Domain> domains = client.getDomains();
-            for (Domain domain : domains) {
-                System.out.println(domain.getName());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Account account = new SandboxAccount();
+        String restApiUrl = Constants.SANDBOX_URL;
+        DNSMEClient client = DNSMEClient.getInstance(account, restApiUrl);
+        List<Domain> domains = client.getDomains();
+        Assert.assertTrue(domains.size() > 0);
     }
 }
