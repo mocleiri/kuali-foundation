2eaff726d31c737d3be5bee120ab3efd7f96fcf6
jcaddel <jcaddel@kuali.org>
2011-10-24 21:27:47 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2536 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/test/java/org/kuali/maven/plugins/dnsme/DNSMEClientTest.java
+++ maven-dnsme-plugin/src/test/java/org/kuali/maven/plugins/dnsme/DNSMEClientTest.java
@@ -1,23 +1,26 @@
 package org.kuali.maven.plugins.dnsme;
 
-import java.security.GeneralSecurityException;
 import java.util.List;
 
 import org.junit.Test;
-import org.kuali.maven.plugins.dnsme.accounts.ProductionAccount;
+import org.kuali.maven.plugins.dnsme.accounts.SandboxAccount;
 import org.kuali.maven.plugins.dnsme.beans.Account;
 import org.kuali.maven.plugins.dnsme.beans.Domain;
 
 public class DNSMEClientTest {
 
     @Test
-    public void testGetDomains() throws GeneralSecurityException {
-        Account account = new ProductionAccount();
-        String baseUrl = Constants.PRODUCTION_URL;
-        DNSMEClient client = DNSMEClient.getInstance(account, baseUrl);
-        List<Domain> domains = client.getDomains();
-        for (Domain domain : domains) {
-            System.out.println(domain.getName());
+    public void testGetDomains() {
+        try {
+            Account account = new SandboxAccount();
+            String baseUrl = Constants.SANDBOX_URL;
+            DNSMEClient client = DNSMEClient.getInstance(account, baseUrl);
+            List<Domain> domains = client.getDomains();
+            for (Domain domain : domains) {
+                System.out.println(domain.getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
