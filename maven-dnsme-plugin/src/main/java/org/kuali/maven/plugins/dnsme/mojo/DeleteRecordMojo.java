390c52f136ec2ad6f5d29ed018f1d16e46096daa
jcaddel <jcaddel@kuali.org>
2011-10-25 19:20:27 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2577 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/mojo/DeleteRecordMojo.java
@@ -0,0 +1,75 @@
+package org.kuali.maven.plugins.dnsme.mojo;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.kuali.maven.plugins.dnsme.DNSMEClient;
+import org.kuali.maven.plugins.dnsme.beans.Domain;
+import org.kuali.maven.plugins.dnsme.beans.Record;
+
+/**
+ * @author Jeff Caddel
+ * @goal deleterecord
+ */
+public class DeleteRecordMojo extends BaseDNSMEMojo {
+    /**
+     * The domain to show records for
+     *
+     * @parameter expression="${dnsme.domainName}"
+     * @required
+     */
+    String domainName;
+
+    /**
+     * Matches a single record with this exact name
+     *
+     * @parameter expression="${dnsme.recordName}"
+     */
+    String recordName;
+
+    /**
+     * Matches a single record with this exact name
+     *
+     * @parameter expression="${dnsme.recordId}"
+     */
+    Integer recordId;
+
+    @Override
+    public void performTasks(DNSMEClient client) throws MojoExecutionException, MojoFailureException {
+        Domain domain = client.getDomain(domainName);
+        Record record = getRecord(client, domain);
+        getLog().info("Deleting a record from '" + domainName + "'");
+        getLog().info("[" + getLog(record) + "]");
+        client.deleteRecord(domain, record.getId());
+    }
+
+    protected Record getRecord(DNSMEClient client, Domain domain) throws MojoExecutionException {
+        if (recordId != null) {
+            client.getRecord(domain, recordId);
+        }
+        if (recordName != null) {
+            return client.getRecord(domain, recordName);
+        }
+        throw new MojoExecutionException("Must supply a value for either recordName or recordId");
+    }
+
+    protected String getCompactLog(Record record) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(record.getName());
+        sb.append("->" + record.getData());
+        sb.append("," + record.getType());
+        sb.append(",ttl=" + record.getTtl() + "s");
+        return sb.toString();
+    }
+
+    protected String getLog(Record record) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(record.getId());
+        sb.append("," + record.getName());
+        sb.append("->" + record.getData());
+        sb.append("," + record.getType());
+        sb.append(",ttl=" + record.getTtl() + "s");
+        sb.append(",gtd=" + record.getGtdLocation());
+        return sb.toString();
+    }
+
+}
