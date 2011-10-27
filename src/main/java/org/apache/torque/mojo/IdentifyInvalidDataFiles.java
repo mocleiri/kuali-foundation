55c046a68c6b0f4b28f71f0f52acca00a548b81b
jcaddel <jcaddel@kuali.org>
2011-10-26 23:42:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/maven-impex-plugin-1.0@2656 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/apache/torque/mojo/IdentifyInvalidDataFiles.java
+++ src/main/java/org/apache/torque/mojo/IdentifyInvalidDataFiles.java
@@ -1,11 +1,13 @@
 package org.apache.torque.mojo;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.tools.ant.DirectoryScanner;
@@ -24,155 +26,173 @@ import org.kuali.core.db.torque.Utils;
  * @goal id-invalid-data-files
  */
 public class IdentifyInvalidDataFiles extends BaseMojo {
-	private static final String FS = System.getProperty("file.separator");
+    private static final String FS = System.getProperty("file.separator");
 
-	/**
-	 * @parameter expression="${extension}" default-value=".xml"
-	 * @required
-	 */
-	private String extension;
+    /**
+     * @parameter expression="${extension}" default-value=".xml"
+     * @required
+     */
+    private String extension;
 
-	/**
-	 * @parameter expression="${dataDir}" default-value="${project.basedir}/src/main/impex"
-	 * @required
-	 */
-	private File dataDir;
+    /**
+     * @parameter expression="${dataDir}" default-value="${project.basedir}/src/main/impex"
+     * @required
+     */
+    private File dataDir;
 
-	/**
-	 * @parameter expression="${dataDirIncludes}" default-value="*.xml"
-	 */
-	private String dataDirIncludes;
+    /**
+     * @parameter expression="${dataDirIncludes}" default-value="*.xml"
+     */
+    private String dataDirIncludes;
 
-	/**
-	 * @parameter expression="${dataDirExcludes}" default-value="schema.xml"
-	 */
-	private String dataDirExcludes;
+    /**
+     * @parameter expression="${dataDirExcludes}" default-value="schema.xml"
+     */
+    private String dataDirExcludes;
 
-	/**
-	 * @parameter expression="${schemaXMLFile}" default-value="src/main/impex/schema.xml"
-	 */
-	private String schemaXMLFile;
+    /**
+     * @parameter expression="${schemaXMLFile}" default-value="src/main/impex/schema.xml"
+     */
+    private String schemaXMLFile;
 
-	/**
-	 * @parameter expression="${targetDatabase}" default-value="oracle"
-	 */
-	private String targetDatabase;
+    /**
+     * @parameter expression="${targetDatabase}" default-value="oracle"
+     */
+    private String targetDatabase;
 
-	@Override
-	protected void executeMojo() throws MojoExecutionException, MojoFailureException {
-		Utils utils = new Utils();
-		try {
-			getLog().info("Examining " + dataDir.getAbsolutePath());
-			Database db = utils.getDatabase(schemaXMLFile, targetDatabase);
-			DirectoryScanner ds = getDirectoryScanner();
-			Set<File> existing = getExistingFiles(ds);
-			Set<File> allowed = getDatabaseFiles(db);
-			Set<File> invalid = SetUtils.difference(existing, allowed);
-			getLog().info(existing.size() + " data files currently exist");
-			getLog().info(invalid.size() + " of those are invalid");
-			StringBuilder sb = new StringBuilder();
-			int count = 0;
-			for (File file : invalid) {
-				if (count != 0) {
-					sb.append(",");
-				}
-				sb.append("**/src/main/impex/" + file.getName());
-				getLog().info("Marked for removal: " + file.getName());
-				count++;
-			}
-			Properties properties = getProject().getProperties();
-			properties.setProperty("impex.data.invalid", sb.toString());
-		} catch (Exception e) {
-			throw new MojoExecutionException("Error executing mojo", e);
-		}
-	}
+    /**
+     * Any invalid files are listed in this file. One file per line
+     * 
+     * @parameter expression="${impex.markedForRemoval}" default-value="${project.build.directory}/markedForRemoval.txt"
+     */
+    private File markedForRemoval;
 
-	protected Set<File> getDatabaseFiles(Database db) {
-		List<?> tables = db.getTables();
-		Set<File> files = new TreeSet<File>();
-		for (Object object : tables) {
-			Table table = (Table) object;
-			String tableName = table.getName();
-			String filename = dataDir.getAbsolutePath() + FS + tableName + extension;
-			File file = new File(filename);
-			files.add(file);
-		}
-		return files;
-	}
+    @Override
+    protected void executeMojo() throws MojoExecutionException, MojoFailureException {
+        Utils utils = new Utils();
+        try {
+            getLog().info("Examining " + dataDir.getAbsolutePath());
+            Database db = utils.getDatabase(schemaXMLFile, targetDatabase);
+            DirectoryScanner ds = getDirectoryScanner();
+            Set<File> existing = getExistingFiles(ds);
+            Set<File> allowed = getDatabaseFiles(db);
+            Set<File> invalid = SetUtils.difference(existing, allowed);
+            getLog().info(existing.size() + " data files currently exist");
+            getLog().info(invalid.size() + " of those are invalid");
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+            StringBuilder sb2 = new StringBuilder();
+            for (File file : invalid) {
+                if (count != 0) {
+                    sb.append(",");
+                }
+                sb.append("**/src/main/impex/" + file.getName());
+                sb2.append(file.getCanonicalPath() + "\n");
+                getLog().info("Marked for removal: " + file.getName());
+                count++;
+            }
+            Properties properties = getProject().getProperties();
+            properties.setProperty("impex.data.invalid", sb.toString());
+            IOUtils.write(sb2.toString(), new FileOutputStream(markedForRemoval));
+        } catch (Exception e) {
+            throw new MojoExecutionException("Error executing mojo", e);
+        }
+    }
 
-	protected Set<File> getExistingFiles(DirectoryScanner ds) {
-		ds.scan();
-		String[] relativeFilenames = ds.getIncludedFiles();
-		Set<File> files = new TreeSet<File>();
-		for (int i = 0; i < relativeFilenames.length; i++) {
-			String filename = ds.getBasedir().getAbsolutePath() + FS + relativeFilenames[i];
-			File file = new File(filename);
-			files.add(file);
-		}
-		return files;
-	}
+    protected Set<File> getDatabaseFiles(Database db) {
+        List<?> tables = db.getTables();
+        Set<File> files = new TreeSet<File>();
+        for (Object object : tables) {
+            Table table = (Table) object;
+            String tableName = table.getName();
+            String filename = dataDir.getAbsolutePath() + FS + tableName + extension;
+            File file = new File(filename);
+            files.add(file);
+        }
+        return files;
+    }
 
-	protected DirectoryScanner getDirectoryScanner() {
-		DirectoryScanner ds = new DirectoryScanner();
-		ds.setBasedir(dataDir);
-		ds.setIncludes(new String[] { dataDirIncludes });
-		ds.setExcludes(new String[] { dataDirExcludes });
-		return ds;
-	}
+    protected Set<File> getExistingFiles(DirectoryScanner ds) {
+        ds.scan();
+        String[] relativeFilenames = ds.getIncludedFiles();
+        Set<File> files = new TreeSet<File>();
+        for (int i = 0; i < relativeFilenames.length; i++) {
+            String filename = ds.getBasedir().getAbsolutePath() + FS + relativeFilenames[i];
+            File file = new File(filename);
+            files.add(file);
+        }
+        return files;
+    }
 
-	protected File getFile(Table table) {
-		String tableName = table.getName();
-		String filename = dataDir.getAbsolutePath() + FS + tableName + extension;
-		File file = new File(filename);
-		return file;
-	}
+    protected DirectoryScanner getDirectoryScanner() {
+        DirectoryScanner ds = new DirectoryScanner();
+        ds.setBasedir(dataDir);
+        ds.setIncludes(new String[] { dataDirIncludes });
+        ds.setExcludes(new String[] { dataDirExcludes });
+        return ds;
+    }
 
-	public File getDataDir() {
-		return dataDir;
-	}
+    protected File getFile(Table table) {
+        String tableName = table.getName();
+        String filename = dataDir.getAbsolutePath() + FS + tableName + extension;
+        File file = new File(filename);
+        return file;
+    }
 
-	public void setDataDir(File dataDir) {
-		this.dataDir = dataDir;
-	}
+    public File getDataDir() {
+        return dataDir;
+    }
 
-	public String getDataDirIncludes() {
-		return dataDirIncludes;
-	}
+    public void setDataDir(File dataDir) {
+        this.dataDir = dataDir;
+    }
 
-	public void setDataDirIncludes(String dataDirIncludes) {
-		this.dataDirIncludes = dataDirIncludes;
-	}
+    public String getDataDirIncludes() {
+        return dataDirIncludes;
+    }
 
-	public String getDataDirExcludes() {
-		return dataDirExcludes;
-	}
+    public void setDataDirIncludes(String dataDirIncludes) {
+        this.dataDirIncludes = dataDirIncludes;
+    }
 
-	public void setDataDirExcludes(String dataDirExcludes) {
-		this.dataDirExcludes = dataDirExcludes;
-	}
+    public String getDataDirExcludes() {
+        return dataDirExcludes;
+    }
 
-	public String getSchemaXMLFile() {
-		return schemaXMLFile;
-	}
+    public void setDataDirExcludes(String dataDirExcludes) {
+        this.dataDirExcludes = dataDirExcludes;
+    }
 
-	public void setSchemaXMLFile(String schemaXMLFile) {
-		this.schemaXMLFile = schemaXMLFile;
-	}
+    public String getSchemaXMLFile() {
+        return schemaXMLFile;
+    }
 
-	public String getExtension() {
-		return extension;
-	}
+    public void setSchemaXMLFile(String schemaXMLFile) {
+        this.schemaXMLFile = schemaXMLFile;
+    }
 
-	public void setExtension(String extension) {
-		this.extension = extension;
-	}
+    public String getExtension() {
+        return extension;
+    }
 
-	public String getTargetDatabase() {
-		return targetDatabase;
-	}
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
 
-	public void setTargetDatabase(String targetDatabase) {
-		this.targetDatabase = targetDatabase;
-	}
+    public String getTargetDatabase() {
+        return targetDatabase;
+    }
+
+    public void setTargetDatabase(String targetDatabase) {
+        this.targetDatabase = targetDatabase;
+    }
+
+    public File getMarkedForRemoval() {
+        return markedForRemoval;
+    }
+
+    public void setMarkedForRemoval(File markedForRemoval) {
+        this.markedForRemoval = markedForRemoval;
+    }
 
 }
