3926c39d297ee2e06e725e951484d967d09e01c3
jcaddel <jcaddel@kuali.org>
2011-10-31 14:27:26 -0400

    Remvoe .txt from the logs directory
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2690 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2admin/src/main/resources/apache-tomcat/root/bin/cleanup.sh
+++ ec2admin/src/main/resources/apache-tomcat/root/bin/cleanup.sh
@@ -8,17 +8,20 @@ fi
 OUT=$CATALINA_BASE/logs/*.out
 LOG=$CATALINA_BASE/logs/*.log
 TLOG=$CATALINA_BASE/logs/*.tlog
+TXT=$CATALINA_BASE/logs/*.txt
 PID=$CATALINA_BASE/logs/catalina.pid
 WORK=$CATALINA_BASE/work
 
 echo Removing $OUT
 echo Removing $LOG
 echo Removing $TLOG
+echo Removing $TXT
 echo Removing $PID
 echo Removing $WORK
 
 rm -f $OUT
 rm -f $LOG
 rm -f $TLOG
+rm -f $TXT
 rm -f $PID
 rm -rf $WORK
