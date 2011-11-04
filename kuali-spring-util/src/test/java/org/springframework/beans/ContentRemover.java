d5791ef3d31226b949bcbc4b3fd48485b2ea0266
jcaddel <jcaddel@kuali.org>
2011-11-04 11:48:27 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2856 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-spring-util/src/test/java/org/springframework/beans/ContentRemover.java
@@ -0,0 +1,50 @@
+package org.springframework.beans;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
+
+public class ContentRemover {
+
+    public void removeContent(File file, String[] contentsToRemove) {
+        String content = getExistingContent(file);
+        for (int i = 0; i < contentsToRemove.length; i++) {
+            String contentToRemove = contentsToRemove[i];
+            int pos = content.indexOf(contentToRemove);
+            if (pos != -1) {
+                System.out.println("Matched " + i + " replacing");
+                content = content.replace(contentToRemove, "");
+            }
+        }
+        write(file, content);
+    }
+
+    protected void write(File file, String content) {
+        OutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            IOUtils.write(content.getBytes(), out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            IOUtils.closeQuietly(out);
+        }
+    }
+
+    protected String getExistingContent(File file) {
+        InputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            return IOUtils.toString(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
+    }
+}
