4f3b272261bcb531b34836354c576cb94e7121f6
jcaddel <jcaddel@kuali.org>
2011-11-04 15:30:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2870 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-spring-util/src/test/java/org/springframework/beans/MultipleCopyrightIgnores.java
@@ -0,0 +1,17 @@
+package org.springframework.beans;
+
+import java.io.File;
+
+public class MultipleCopyrightIgnores extends CommonIgnoresFilter {
+
+    @Override
+    public boolean accept(File file) {
+        String path = file.getAbsolutePath();
+
+        if (path.endsWith(".xsd") && contains(path, "/impl/src/main/resources/schema/")) {
+            return false;
+        } else {
+            return super.accept(file);
+        }
+    }
+}
