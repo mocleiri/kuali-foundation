2521cb03ad32ae686b2ca51b7386d73b324f15bd
jcaddel <jcaddel@kuali.org>
2011-11-04 13:22:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2863 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-spring-util/src/test/java/org/springframework/beans/CopyrightHandler.java
+++ kuali-spring-util/src/test/java/org/springframework/beans/CopyrightHandler.java
@@ -1,8 +1,11 @@
 package org.springframework.beans;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -15,8 +18,59 @@ import org.springframework.core.io.ResourceLoader;
 
 public class CopyrightHandler {
 
+    public static final String ECL_CR = "${ecl.cr}";
+    public static final String ECL_LF = "${ecl.lf}";
+    public static final String CR = "\r";
+    public static final String LF = "\n";
+
     public static void main(String[] args) {
         new CopyrightHandler().exec(args);
+    }
+
+    protected void write(File file, String content) {
+        OutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            IOUtils.write(content.getBytes(), out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            IOUtils.closeQuietly(out);
+        }
+    }
+
+    protected String getExistingContent(File file) {
+        InputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            return IOUtils.toString(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
+    }
+
+    public static final String expand(String s) {
+        s = s.replace(ECL_CR, CR);
+        s = s.replace(ECL_LF, LF);
+        return s;
+    }
+
+    public static final String flatten(String s) {
+        s = s.replace(CR, ECL_CR);
+        s = s.replace(LF, ECL_LF);
+        return s;
+    }
+
+    protected void copy(File file) {
+        String content = getExistingContent(file);
+        String flat = flatten(content);
+        String filename = file.getName();
+        File newFile1 = new File("C:/temp/ecl/" + filename);
+        File newFile2 = new File("C:/temp/ecl/" + filename + ".flat");
+        write(newFile1, content);
+        write(newFile2, flat);
     }
 
     protected void exec(String[] args) {
@@ -33,12 +87,19 @@ public class CopyrightHandler {
             }
             Properties invalidEcl = getXMLProperties("invalid-ecl-headers.xml");
             List<String> contentsToRemove = getValues(invalidEcl);
+            File foo = new File("c:/temp/ecl/invalid.txt");
+            StringBuilder sb = new StringBuilder();
+            for (String s : contentsToRemove) {
+                String flat = flatten(s);
+                sb.append(flat + "\n\n");
+            }
+            write(foo, sb.toString());
 
             ContentRemover remover = new ContentRemover();
             for (File file : files) {
                 String name = file.getName();
                 if (name.equals("EDLSpringBeans.xml")) {
-                    System.out.println("foo");
+                    copy(file);
                 }
                 remover.removeContent(file, contentsToRemove);
             }
@@ -53,6 +114,9 @@ public class CopyrightHandler {
         Set<String> keys = properties.stringPropertyNames();
         for (String key : keys) {
             String value = properties.getProperty(key);
+
+            value = value.replace("${ecl.cr}", "\r");
+            value = value.replace("${ecl.lf}", "\n");
 
             // If it has cr+linefeed change it to just linefeed
             String lf = value.replace("\r\n", "\n");
