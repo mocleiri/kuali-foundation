f2d845ee6f9aa55332ac1df1f51da5f546b7c72d
jcaddel <jcaddel@kuali.org>
2011-11-04 10:55:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2849 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-spring-util/src/test/java/org/springframework/beans/ProblemFileDetector.java
+++ kuali-spring-util/src/test/java/org/springframework/beans/ProblemFileDetector.java
@@ -7,38 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProblemFileDetector {
-
     public static void main(String[] args) {
         new ProblemFileDetector().exec(args);
     }
 
     protected void exec(String[] args) {
         try {
-            File baseDir = new File("c:/eclipse/3.6.2/r11/eclipse/ws/rice-2.0-trunk");
-            FileFilter exclude = new CommonIgnoresFilter();
-            FileFilter include = new FilenameContainsDotXFilter();
-            // FileFilter problem = new MisplacedXMLPrologFilter();
-            FileFilter problem = new MultipleCopyrightFilter();
-            execute(baseDir, exclude, include, problem);
+            String basedir = "c:/eclipse/3.6.2/r11/eclipse/ws/rice-2.0-trunk";
+            ProblemFileContext context = new MisplacedXMLPrologContext(basedir);
+            List<File> files = getProblemFiles(context);
+            for (File file : files) {
+                System.out.println(file.getAbsolutePath());
+            }
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 
-    protected void execute(File baseDir, FileFilter exclude, FileFilter include, FileFilter problem) throws IOException {
-        long beg = System.currentTimeMillis();
+    public List<File> getProblemFiles(ProblemFileContext context) throws IOException {
+        File baseDir = context.getBaseDir();
+        FileFilter exclude = context.getExclude();
+        FileFilter include = context.getInclude();
+        FileFilter problem = context.getProblem();
         List<File> files = getFiles(baseDir, exclude, include);
-        System.out.println(files.size());
         List<File> problemFiles = getProblemFiles(files, problem);
-        System.out.println(problemFiles.size());
-        for (int i = 0; i < problemFiles.size(); i++) {
-            File problemFile = problemFiles.get(i);
-            System.out.println(problemFile.getAbsolutePath());
-        }
-        long end = System.currentTimeMillis();
-        long elapsed = end - beg;
-        double seconds = elapsed / 1000D;
-        System.out.println(seconds);
+        return problemFiles;
     }
 
     protected List<File> getProblemFiles(List<File> files, FileFilter problem) throws IOException {
