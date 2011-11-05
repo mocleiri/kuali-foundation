7865947557a61d9bac7e773035d2a0b31776b81c
jcaddel <jcaddel@kuali.org>
2011-11-05 08:23:36 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2902 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-ecl-plugin/src/main/java/org/kuali/maven/plugins/ecl/filter/MultipleCopyrightFilter.java
+++ maven-ecl-plugin/src/main/java/org/kuali/maven/plugins/ecl/filter/MultipleCopyrightFilter.java
@@ -3,28 +3,36 @@ package org.kuali.maven.plugins.ecl.filter;
 import java.util.List;
 
 public class MultipleCopyrightFilter extends CheckLinesFileFilter {
-    String target = "copyright";
+    String[] targets = { "copyright", "kuali", "foundation" };
 
     @Override
     protected boolean checkLines(List<String> lines) {
         int count = 0;
         for (int i = 0; i < lines.size(); i++) {
             String s = lines.get(i);
-            String lowerCase = s.toLowerCase();
-            int pos = lowerCase.indexOf(target);
-            if (pos != -1) {
+            if (contains(s.toLowerCase(), targets)) {
                 count++;
             }
         }
         return count > 1;
     }
 
-    public String getTarget() {
-        return target;
+    protected boolean contains(String s, String[] targets) {
+        for (String target : targets) {
+            int pos = s.indexOf(target);
+            if (pos == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public void setTarget(String target) {
-        this.target = target;
+    public String[] getTargets() {
+        return targets;
+    }
+
+    public void setTargets(String[] targets) {
+        this.targets = targets;
     }
 
 }
