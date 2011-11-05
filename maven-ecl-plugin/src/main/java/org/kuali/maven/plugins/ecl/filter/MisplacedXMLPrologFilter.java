c6b517c04a3fa60e352b634ac05de0364c168556
jcaddel <jcaddel@kuali.org>
2011-11-05 08:17:57 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@2899 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-ecl-plugin/src/main/java/org/kuali/maven/plugins/ecl/filter/MisplacedXMLPrologFilter.java
+++ maven-ecl-plugin/src/main/java/org/kuali/maven/plugins/ecl/filter/MisplacedXMLPrologFilter.java
@@ -1,39 +1,22 @@
 package org.kuali.maven.plugins.ecl.filter;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
+public class MisplacedXMLPrologFilter extends CheckLinesFileFilter {
 
-public class MisplacedXMLPrologFilter implements FileFilter {
+    String prologStart = "<?xml";
+    String prologStop = "?>";
 
     @Override
-    public boolean accept(File file) {
-        List<String> lines = getLines(file);
+    public boolean checkLines(List<String> lines) {
         int prologIndex = getPrologIndex(lines);
         return prologIndex > 0;
     }
 
-    protected List<String> getLines(File file) {
-        InputStream in = null;
-        try {
-            in = new FileInputStream(file);
-            return IOUtils.readLines(in);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            IOUtils.closeQuietly(in);
-        }
-    }
-
     protected boolean isProlog(String line) {
         String lowerCase = line.toLowerCase();
-        int pos1 = lowerCase.indexOf("<?xml");
-        int pos2 = lowerCase.indexOf("?>");
+        int pos1 = lowerCase.indexOf(prologStart);
+        int pos2 = lowerCase.indexOf(prologStop);
         boolean flag1 = pos1 != -1 && pos2 != -1;
         boolean flag2 = pos2 > pos1;
         return flag1 && flag2;
@@ -47,6 +30,22 @@ public class MisplacedXMLPrologFilter implements FileFilter {
             }
         }
         return -1;
+    }
+
+    public String getPrologStart() {
+        return prologStart;
+    }
+
+    public void setPrologStart(String prologStart) {
+        this.prologStart = prologStart;
+    }
+
+    public String getPrologStop() {
+        return prologStop;
+    }
+
+    public void setPrologStop(String prologStop) {
+        this.prologStop = prologStop;
     }
 
 }
