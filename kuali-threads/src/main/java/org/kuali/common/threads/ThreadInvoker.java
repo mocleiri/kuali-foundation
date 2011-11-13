6a8c94ee67d344c6aa0dd3bed09987f462ef885c
jcaddel <jcaddel@kuali.org>
2011-11-13 11:58:32 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@3076 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-threads/src/main/java/org/kuali/common/threads/ThreadInvoker.java
+++ kuali-threads/src/main/java/org/kuali/common/threads/ThreadInvoker.java
@@ -9,24 +9,32 @@ import org.slf4j.LoggerFactory;
 public class ThreadInvoker {
     private final Logger logger = LoggerFactory.getLogger(ThreadHandlerFactory.class);
 
-    public <T> ExecutionStatistics invokeThreads(int max, int divisor, ElementHandler<T> elementHandler, List<T> list) {
+    public <T> ExecutionStatistics invokeThreads(int max, int min, int divisor, ThreadHandlerContext<T> context) {
+        if (context.getHandler() == null || context.getList() == null) {
+            throw new IllegalArgumentException("Neither elementHandler nor list can be null");
+        }
         ThreadHandlerFactory factory = new ThreadHandlerFactory();
         ThreadHandlerContext<T> thc = new ThreadHandlerContext<T>();
         thc.setMax(max);
-        thc.setHandler(elementHandler);
-        thc.setList(list);
+        thc.setMin(min);
         thc.setDivisor(divisor);
-        thc.setListener(new ConsoleListener<T>());
+        if (thc.getListener() == null) {
+            thc.setListener(new ConsoleListener<T>());
+        }
         ThreadHandler<T> handler = factory.getThreadHandler(thc);
         int elementsPerThread = handler.getElementsPerThread();
-        int size = list.size();
+        int size = thc.getList().size();
         logger.info("Executing [t:" + handler.getThreadCount() + " e:" + elementsPerThread + " s:" + size + "]");
         handler.executeThreads();
         return handler.getExecutionStatistics();
     }
 
-    public <T> void invokeThreads(int max, ElementHandler<T> elementHandler, List<T> list) {
-        invokeThreads(max, 0, elementHandler, list);
+    public <T> ExecutionStatistics invokeThreads(int max, ElementHandler<T> elementHandler, List<T> list) {
+        ThreadHandlerContext<T> thc = new ThreadHandlerContext<T>();
+        thc.setMax(max);
+        thc.setHandler(elementHandler);
+        thc.setList(list);
+        return invokeThreads(max, 0, 0, thc);
     }
 
 }
