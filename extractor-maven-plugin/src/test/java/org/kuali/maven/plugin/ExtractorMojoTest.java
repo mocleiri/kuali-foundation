368c17734135c39cc378750e2dac937820f68138
jcaddel <jcaddel@kuali.org>
2011-11-25 19:06:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@3678 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- extractor-maven-plugin/src/test/java/org/kuali/maven/plugin/ExtractorMojoTest.java
+++ extractor-maven-plugin/src/test/java/org/kuali/maven/plugin/ExtractorMojoTest.java
@@ -1,12 +1,22 @@
 package org.kuali.maven.plugin;
 
+import junit.framework.Assert;
+
+import org.apache.maven.model.Scm;
+import org.junit.Test;
 
 /**
  *
  */
 public class ExtractorMojoTest {
+
+    @Test
     public void testGetScmUrl() {
         ExtractorMojo mojo = new ExtractorMojo();
         String dev = "scm:git:git@github.com:jcaddel/maven-s3-wagon.git";
+        Scm scm = new Scm();
+        scm.setDeveloperConnection(dev);
+        String scmUrl = mojo.getScmUrl(scm);
+        Assert.assertEquals("git@github.com:jcaddel/maven-s3-wagon.git", scmUrl);
     }
 }
