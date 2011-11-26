bc1096bef01fdaae2e559e469332dd668c27777f
jcaddel <jcaddel@kuali.org>
2011-11-25 19:57:52 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@3698 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/mojo/AddRecordMojo.java
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/mojo/AddRecordMojo.java
@@ -39,7 +39,7 @@ public class AddRecordMojo extends BaseDNSMEMojo {
     RecordType recordType;
 
     /**
-     * The record name eg "myurl.kuali.org"
+     * The record name eg "myurl.mykualiproject". "kuali.org" is appended to this value
      *
      * @parameter expression="${dnsme.recordName}"
      * @required
