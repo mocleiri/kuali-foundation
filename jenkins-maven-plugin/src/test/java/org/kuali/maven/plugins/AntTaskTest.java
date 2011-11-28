4c5c38a42d817fe9923b72fa886bc54611e85918
jcaddel <jcaddel@kuali.org>
2011-11-28 15:56:03 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@3898 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- jenkins-maven-plugin/src/test/java/org/kuali/maven/plugins/AntTaskTest.java
+++ jenkins-maven-plugin/src/test/java/org/kuali/maven/plugins/AntTaskTest.java
@@ -1,6 +1,7 @@
 package org.kuali.maven.plugins;
 
 import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
 import org.junit.Test;
 
 public class AntTaskTest {
@@ -8,6 +9,8 @@ public class AntTaskTest {
 	@Test
 	public void testForking() {
 		Project project = new Project();
-
+		Task task = new HelloWorldTask();
+		task.setProject(project);
+		task.execute();
 	}
 }
