3bba46cd1bd41332702b4a5b04564feb07d045f5
jcaddel <jcaddel@kuali.org>
2011-11-29 10:55:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@3952 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-kuali-common/src/test/java/org/kuali/maven/common/PropertiesUtilsTest.java
+++ maven-kuali-common/src/test/java/org/kuali/maven/common/PropertiesUtilsTest.java
@@ -1,7 +1,5 @@
 package org.kuali.maven.common;
 
-import static org.junit.Assert.*;
-
 import java.util.Properties;
 
 import junit.framework.Assert;
@@ -11,19 +9,19 @@ import org.springframework.util.PropertyPlaceholderHelper;
 
 public class PropertiesUtilsTest {
 
-	@Test
-	public void test() {
-		Properties properties = new Properties();
-		properties.setProperty("a", "1");
-		properties.setProperty("b", "2");
-		properties.setProperty("c", "3");
-		properties.setProperty("foo", "${a}");
+    @Test
+    public void test() {
+        Properties properties = new Properties();
+        properties.setProperty("a", "1");
+        properties.setProperty("b", "2");
+        properties.setProperty("c", "3");
+        properties.setProperty("foo", "${a}");
 
-		String s = "The value is ${foo}";
-		PropertyPlaceholderHelper helper = new PropertyPlaceholderHelper("${", "}");
-		String expected = "The value is 1";
-		String resolved = helper.replacePlaceholders(s, properties);
-		Assert.assertEquals(expected, resolved);
-	}
+        String s = "The value is ${foo}";
+        PropertyPlaceholderHelper helper = new PropertyPlaceholderHelper("${", "}");
+        String expected = "The value is 1";
+        String resolved = helper.replacePlaceholders(s, properties);
+        Assert.assertEquals(expected, resolved);
+    }
 
 }
