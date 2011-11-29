c90ead1ff74d83504da0bc5f6cf69d6ea460fe19
jcaddel <jcaddel@kuali.org>
2011-11-28 19:25:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@3925 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/CreateJobFromTemplate.java
@@ -0,0 +1,49 @@
+package org.kuali.maven.mojo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.kuali.maven.common.Extractor;
+
+public class CreateJobFromTemplate extends AbstractMojo {
+	Extractor extractor = new Extractor();
+
+	/**
+	 * The Maven project this plugin runs in.
+	 * 
+	 * @parameter expression="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		String scmType = extractor.getScmType(project.getScm());
+		String scmUrl = extractor.getScmUrl(project.getScm());
+		String majorVersion = extractor.getMajorVersion(project.getVersion());
+
+		Properties properties = project.getProperties();
+		properties.setProperty("jenkins.project.scmType", scmType);
+		properties.setProperty("jenkins.project.scmUrl", scmUrl);
+		properties.setProperty("jenkins.project.majorVersion", majorVersion);
+
+		List<String> locations = new ArrayList<String>();
+		locations.add("classpath:org/kuali/cm/jenkins/kuali.properties");
+		locations.add("classpath:org/kuali/cm/jenkins/jenkins.properties");
+		locations.add("classpath:org/kuali/cm/jenkins/jobs/properties/common.xml");
+	}
+
+	public MavenProject getProject() {
+		return project;
+	}
+
+	public void setProject(MavenProject project) {
+		this.project = project;
+	}
+}
