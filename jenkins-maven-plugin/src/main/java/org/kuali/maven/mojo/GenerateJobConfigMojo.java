b3323ddd469701c86a21320dd4362378982999e8
jcaddel <jcaddel@kuali.org>
2011-11-29 13:33:37 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@3982 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/GenerateJobConfigMojo.java
@@ -0,0 +1,70 @@
+package org.kuali.maven.mojo;
+
+import java.io.IOException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * @goal generatejobconfig
+ */
+public class GenerateJobConfigMojo extends AbstractMojo {
+	Generator generator = new Generator();
+
+	/**
+	 * The Maven project this plugin runs in.
+	 * 
+	 * @parameter expression="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
+
+	/**
+	 * @parameter expression="${jenkins.type}" default-value="publish"
+	 * @required
+	 */
+	private String type;
+
+	/**
+	 * @parameter expression="${jenkins.filename}"
+	 */
+	private String filename;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		try {
+			JobContext context = generator.getJobContext(project, filename, type);
+			getLog().info("Generating: " + context.getFilename());
+			generator.generate(context);
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unexpected error", e);
+		}
+	}
+
+	public MavenProject getProject() {
+		return project;
+	}
+
+	public void setProject(MavenProject project) {
+		this.project = project;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
--- jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/Generator.java
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/Generator.java
@@ -11,101 +11,126 @@ import java.util.Properties;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.StringUtils;
 import org.kuali.maven.common.Extractor;
 import org.kuali.maven.common.PropertiesUtils;
 
 public class Generator {
-    Extractor extractor = new Extractor();
-    PropertiesUtils pu = new PropertiesUtils();
-    private static final String FS = System.getProperty("file.separator");
+	Extractor extractor = new Extractor();
+	PropertiesUtils pu = new PropertiesUtils();
+	private static final String FS = System.getProperty("file.separator");
 
-    public String getDefaultFilename(JobContext context) {
-        String majorVersion = context.getMajorVersion();
-        MavenProject project = context.getProject();
-        String artifactId = project.getArtifactId();
-        String buildDir = project.getBuild().getDirectory();
-        StringBuilder sb = new StringBuilder();
-        sb.append(buildDir);
-        sb.append(FS);
-        sb.append("jenkins");
-        sb.append(FS);
-        sb.append(artifactId);
-        sb.append("-");
-        sb.append(majorVersion);
-        sb.append("-");
-        sb.append(context.getJobType());
-        sb.append(".xml");
-        return sb.toString();
-    }
+	protected JobContext getJobContext(MavenProject project, String filename, String type) {
+		Extractor extractor = new Extractor();
+		String scmType = extractor.getScmType(project.getScm());
+		String scmUrl = extractor.getScmUrl(project.getScm());
+		String majorVersion = extractor.getMajorVersion(project.getVersion());
 
-    public void generate(JobContext context) throws IOException {
-        String filename = context.getFilename();
-        Properties properties = getProperties(context);
-        String xml = read("classpath:org/kuali/jenkins/jobs/templates/jenkins.xml");
-        String resolvedXml = pu.getResolvedValue(xml, properties);
-        write(filename, resolvedXml);
-    }
+		JobContext context = new JobContext();
+		context.setFilename(filename);
+		context.setProject(project);
+		context.setJobType(type);
+		context.setScmType(scmType);
+		context.setScmUrl(scmUrl);
+		context.setMajorVersion(majorVersion);
 
-    protected void write(String filename, String contents) throws IOException {
-        File file = new File(filename);
-        OutputStream out = null;
-        try {
-            out = FileUtils.openOutputStream(file);
-            IOUtils.write(contents, out);
-        } finally {
-            IOUtils.closeQuietly(out);
-        }
+		if (StringUtils.isEmpty(filename)) {
+			String defaultFilename = getDefaultFilename(context);
+			context.setFilename(defaultFilename);
+		} else {
+			context.setFilename(filename);
+		}
 
-    }
+		return context;
+	}
 
-    protected String read(String location) throws IOException {
-        InputStream in = null;
-        try {
-            in = pu.getInputStream(location);
-            return IOUtils.toString(in);
-        } finally {
-            IOUtils.closeQuietly(in);
-        }
-    }
+	public String getDefaultFilename(JobContext context) {
+		String majorVersion = context.getMajorVersion();
+		MavenProject project = context.getProject();
+		String artifactId = project.getArtifactId();
+		String buildDir = project.getBuild().getDirectory();
+		StringBuilder sb = new StringBuilder();
+		sb.append(buildDir);
+		sb.append(FS);
+		sb.append("jenkins");
+		sb.append(FS);
+		sb.append(artifactId);
+		sb.append("-");
+		sb.append(majorVersion);
+		sb.append("-");
+		sb.append(context.getJobType());
+		sb.append(".xml");
+		return sb.toString();
+	}
 
-    protected Properties getProperties(JobContext context) throws IOException {
-        MavenProject project = context.getProject();
+	public void generate(JobContext context) throws IOException {
+		String filename = context.getFilename();
+		Properties properties = getProperties(context);
+		String xml = read("classpath:org/kuali/jenkins/jobs/templates/jenkins.xml");
+		String resolvedXml = pu.getResolvedValue(xml, properties);
+		write(filename, resolvedXml);
+	}
 
-        List<String> locations = getLocations(context);
-        Properties resourceProperties = pu.getProperties(locations);
-        Properties jenkinsProperties = getJenkinsProperties(context);
-        Properties projectProperties = project.getProperties();
-        Properties environmentProperties = pu.getEnvironmentProperties();
-        Properties systemProperties = System.getProperties();
+	protected void write(String filename, String contents) throws IOException {
+		File file = new File(filename);
+		OutputStream out = null;
+		try {
+			out = FileUtils.openOutputStream(file);
+			IOUtils.write(contents, out);
+		} finally {
+			IOUtils.closeQuietly(out);
+		}
 
-        Properties properties = new Properties();
-        properties.putAll(resourceProperties);
-        properties.putAll(jenkinsProperties);
-        properties.putAll(projectProperties);
-        properties.putAll(environmentProperties);
-        properties.putAll(systemProperties);
-        return properties;
-    }
+	}
 
-    protected Properties getJenkinsProperties(JobContext context) {
-        MavenProject project = context.getProject();
-        Properties properties = new Properties();
-        properties.setProperty("jenkins.project.scmType", context.getScmType());
-        properties.setProperty("jenkins.project.scmUrl", context.getScmUrl());
-        properties.setProperty("jenkins.project.majorVersion", context.getMajorVersion());
-        properties.setProperty("jenkins.project.groupId", project.getGroupId());
-        properties.setProperty("jenkins.project.artifactId", project.getArtifactId());
-        return properties;
-    }
+	protected String read(String location) throws IOException {
+		InputStream in = null;
+		try {
+			in = pu.getInputStream(location);
+			return IOUtils.toString(in);
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+	}
 
-    protected List<String> getLocations(JobContext context) {
-        List<String> locations = new ArrayList<String>();
-        locations.add("classpath:org/kuali/jenkins/kuali.properties");
-        locations.add("classpath:org/kuali/jenkins/jenkins.properties");
-        locations.add("classpath:org/kuali/jenkins/jobs/properties/common.xml");
-        locations.add("classpath:org/kuali/jenkins/jobs/properties/" + context.getScmType() + ".xml");
-        locations.add("classpath:org/kuali/jenkins/jobs/properties/types/" + context.getJobType() + ".xml");
-        return locations;
-    }
+	protected Properties getProperties(JobContext context) throws IOException {
+		MavenProject project = context.getProject();
+
+		List<String> locations = getLocations(context);
+		Properties resourceProperties = pu.getProperties(locations);
+		Properties jenkinsProperties = getJenkinsProperties(context);
+		Properties projectProperties = project.getProperties();
+		Properties environmentProperties = pu.getEnvironmentProperties();
+		Properties systemProperties = System.getProperties();
+
+		Properties properties = new Properties();
+		properties.putAll(resourceProperties);
+		properties.putAll(jenkinsProperties);
+		properties.putAll(projectProperties);
+		properties.putAll(environmentProperties);
+		properties.putAll(systemProperties);
+		return properties;
+	}
+
+	protected Properties getJenkinsProperties(JobContext context) {
+		MavenProject project = context.getProject();
+		Properties properties = new Properties();
+		properties.setProperty("jenkins.project.scmType", context.getScmType());
+		properties.setProperty("jenkins.project.scmUrl", context.getScmUrl());
+		properties.setProperty("jenkins.project.majorVersion", context.getMajorVersion());
+		properties.setProperty("jenkins.project.groupId", project.getGroupId());
+		properties.setProperty("jenkins.project.artifactId", project.getArtifactId());
+		return properties;
+	}
+
+	protected List<String> getLocations(JobContext context) {
+		List<String> locations = new ArrayList<String>();
+		locations.add("classpath:org/kuali/jenkins/kuali.properties");
+		locations.add("classpath:org/kuali/jenkins/jenkins.properties");
+		locations.add("classpath:org/kuali/jenkins/jobs/properties/common.xml");
+		locations.add("classpath:org/kuali/jenkins/jobs/properties/" + context.getScmType() + ".xml");
+		locations.add("classpath:org/kuali/jenkins/jobs/properties/types/" + context.getJobType() + ".xml");
+		return locations;
+	}
 
 }
