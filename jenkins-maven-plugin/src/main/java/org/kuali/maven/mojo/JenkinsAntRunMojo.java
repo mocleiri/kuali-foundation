66e966f5dc3f86ab284fba42204620f99fa99962
jcaddel <jcaddel@kuali.org>
2011-11-29 16:03:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4010 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/JenkinsAntRunMojo.java
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/JenkinsAntRunMojo.java
@@ -9,23 +9,23 @@ import org.codehaus.plexus.configuration.PlexusConfigurationException;
 /**
  * @goal antrun
  */
-public class KualiAntRunMojo extends AbstractAntRunMojo {
+public class JenkinsAntRunMojo extends AbstractAntRunMojo {
 	Generator generator = new Generator();
 
 	/**
-	 * @parameter expression="${jenkins.cliWrapper}" default-value="classpath:org/kuali/jenkins/ant/cli-wrapper.xml"
+	 * @parameter expression="${jenkins.ant.location}" default-value="classpath:org/kuali/jenkins/ant/cli-wrapper.xml"
 	 * @required
 	 */
-	private String cliWrapper;
+	private String location;
 
-	private File cliWrapperFile;
+	private File localFile;
 
 	@Override
 	public void execute() throws MojoExecutionException {
 		try {
 			super.setAntTargetName("main");
-			cliWrapperFile = new File(getProject().getBuild().getDirectory() + "/antrun/cli-wrapper.xml");
-			generator.copy(cliWrapper, cliWrapperFile.getAbsolutePath());
+			localFile = new File(getProject().getBuild().getDirectory() + "/antrun/local-file.xml");
+			generator.copy(location, localFile.getAbsolutePath());
 			super.execute();
 		} catch (IOException e) {
 			throw new MojoExecutionException("Unexpected error", e);
@@ -37,10 +37,10 @@ public class KualiAntRunMojo extends AbstractAntRunMojo {
 		String filename = getProject().getBuild().getDirectory() + "/antrun/build-main.xml";
 		StringBuilder sb = new StringBuilder();
 		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
-		sb.append("<project name=\"maven-antrun-\" default=\"main\"  >\n");
+		sb.append("<project name=\"maven-antrun-\" default=\"main\">\n");
 		sb.append("<target name=\"main\">\n");
 		sb.append("  <property name=\"maven.plugin.classpath\" refid=\"maven.plugin.classpath\"/>\n");
-		sb.append("  <ant antfile=\"" + cliWrapperFile.getAbsolutePath() + "\" target=\"noop\"/>\n");
+		sb.append("  <ant antfile=\"" + localFile.getAbsolutePath() + "\" target=\"noop\"/>\n");
 		sb.append("</target>\n");
 		sb.append("</project>\n");
 		generator.write(filename, sb.toString());
