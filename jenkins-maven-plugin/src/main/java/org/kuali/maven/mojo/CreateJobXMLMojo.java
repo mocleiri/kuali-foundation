56e9212b199402addf1aa1c9e43f0e6b8b8ecb5a
jcaddel <jcaddel@kuali.org>
2011-11-29 11:17:15 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@3953 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/CreateJobXMLMojo.java
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/CreateJobXMLMojo.java
@@ -1,14 +1,12 @@
 package org.kuali.maven.mojo;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -17,87 +15,100 @@ import org.kuali.maven.common.Extractor;
 import org.kuali.maven.common.PropertiesUtils;
 
 public class CreateJobXMLMojo extends AbstractMojo {
-	Extractor extractor = new Extractor();
-	PropertiesUtils pu = new PropertiesUtils();
+    Extractor extractor = new Extractor();
+    PropertiesUtils pu = new PropertiesUtils();
 
-	/**
-	 * The Maven project this plugin runs in.
-	 * 
-	 * @parameter expression="${project}"
-	 * @required
-	 * @readonly
-	 */
-	private MavenProject project;
+    /**
+     * The Maven project this plugin runs in.
+     * 
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
 
-	/**
-	 * 
-	 * @parameter expression="${jenkins.template}" default-value="PUBLISH"
-	 */
-	private Template template;
+    /**
+     * 
+     * @parameter expression="${jenkins.template}" default-value="PUBLISH"
+     */
+    private Template template;
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-	}
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            Properties properties = getProperties();
+            String xml = getContent("classpath:org/kuali/cm/jenkins/jobs/templates/jenkins.xml");
+            String resolvedXml = pu.getResolvedValue(xml, properties);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Unexpected error", e);
+        }
+    }
 
-	protected Properties getProperties() throws MojoExecutionException {
-		try {
-			String scmType = extractor.getScmType(project.getScm()).toLowerCase();
-			String scmUrl = extractor.getScmUrl(project.getScm());
-			String majorVersion = extractor.getMajorVersion(project.getVersion());
+    protected String getContent(String location) throws IOException {
+        InputStream in = null;
+        try {
+            in = pu.getInputStream(location);
+            return IOUtils.toString(in);
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
+    }
 
-			List<String> locations = getLocations(scmType, scmUrl, template);
-			Properties resourceProperties = pu.getProperties(locations);
-			Properties jenkinsProperties = getJenkinsProperties(scmType, scmUrl, majorVersion, project);
-			Properties projectProperties = project.getProperties();
-			Properties environmentProperties = pu.getEnvironmentProperties();
-			Properties systemProperties = System.getProperties();
+    protected Properties getProperties() throws IOException {
+        String scmType = extractor.getScmType(project.getScm()).toLowerCase();
+        String scmUrl = extractor.getScmUrl(project.getScm());
+        String majorVersion = extractor.getMajorVersion(project.getVersion());
 
-			Properties properties = new Properties();
-			properties.putAll(resourceProperties);
-			properties.putAll(jenkinsProperties);
-			properties.putAll(projectProperties);
-			properties.putAll(environmentProperties);
-			properties.putAll(systemProperties);
-			return properties;
-		} catch (IOException e) {
-			throw new MojoExecutionException("Error loading properties", e);
-		}
-	}
+        List<String> locations = getLocations(scmType, scmUrl, template);
+        Properties resourceProperties = pu.getProperties(locations);
+        Properties jenkinsProperties = getJenkinsProperties(scmType, scmUrl, majorVersion, project);
+        Properties projectProperties = project.getProperties();
+        Properties environmentProperties = pu.getEnvironmentProperties();
+        Properties systemProperties = System.getProperties();
 
-	protected Properties getJenkinsProperties(String scmType, String scmUrl, String majorVersion, MavenProject project) {
-		Properties properties = new Properties();
-		properties.setProperty("jenkins.project.scmType", scmType);
-		properties.setProperty("jenkins.project.scmUrl", scmUrl);
-		properties.setProperty("jenkins.project.majorVersion", majorVersion);
-		properties.setProperty("jenkins.project.groupId", project.getGroupId());
-		properties.setProperty("jenkins.project.artifactId", project.getArtifactId());
-		return properties;
-	}
+        Properties properties = new Properties();
+        properties.putAll(resourceProperties);
+        properties.putAll(jenkinsProperties);
+        properties.putAll(projectProperties);
+        properties.putAll(environmentProperties);
+        properties.putAll(systemProperties);
+        return properties;
+    }
 
-	protected List<String> getLocations(String scmType, String scmUrl, Template template) {
-		List<String> locations = new ArrayList<String>();
-		locations.add("classpath:org/kuali/cm/jenkins/kuali.properties");
-		locations.add("classpath:org/kuali/cm/jenkins/jenkins.properties");
-		locations.add("classpath:org/kuali/cm/jenkins/jobs/properties/common.xml");
-		locations.add("classpath:org/kuali/cm/jenkins/jobs/properties/" + scmType + ".xml");
-		String lowerCase = template.toString().toLowerCase();
-		locations.add("classpath:org/kuali/cm/jenkins/jobs/properties/types/" + lowerCase + ".xml");
-		return locations;
-	}
+    protected Properties getJenkinsProperties(String scmType, String scmUrl, String majorVersion, MavenProject project) {
+        Properties properties = new Properties();
+        properties.setProperty("jenkins.project.scmType", scmType);
+        properties.setProperty("jenkins.project.scmUrl", scmUrl);
+        properties.setProperty("jenkins.project.majorVersion", majorVersion);
+        properties.setProperty("jenkins.project.groupId", project.getGroupId());
+        properties.setProperty("jenkins.project.artifactId", project.getArtifactId());
+        return properties;
+    }
 
-	public MavenProject getProject() {
-		return project;
-	}
+    protected List<String> getLocations(String scmType, String scmUrl, Template template) {
+        List<String> locations = new ArrayList<String>();
+        locations.add("classpath:org/kuali/cm/jenkins/kuali.properties");
+        locations.add("classpath:org/kuali/cm/jenkins/jenkins.properties");
+        locations.add("classpath:org/kuali/cm/jenkins/jobs/properties/common.xml");
+        locations.add("classpath:org/kuali/cm/jenkins/jobs/properties/" + scmType + ".xml");
+        String lowerCase = template.toString().toLowerCase();
+        locations.add("classpath:org/kuali/cm/jenkins/jobs/properties/types/" + lowerCase + ".xml");
+        return locations;
+    }
 
-	public void setProject(MavenProject project) {
-		this.project = project;
-	}
+    public MavenProject getProject() {
+        return project;
+    }
 
-	public Template getTemplate() {
-		return template;
-	}
+    public void setProject(MavenProject project) {
+        this.project = project;
+    }
 
-	public void setTemplate(Template template) {
-		this.template = template;
-	}
+    public Template getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(Template template) {
+        this.template = template;
+    }
 }
