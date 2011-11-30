4156c7ec43257e6e89b183cb55cd667c30d98702
jcaddel <jcaddel@kuali.org>
2011-11-29 20:22:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4048 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/JenkinsCLIMojo.java
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/JenkinsCLIMojo.java
@@ -1,18 +1,31 @@
 package org.kuali.maven.mojo;
 
 import org.apache.maven.plugin.MojoExecutionException;
-import org.kuali.maven.common.PropertiesUtils;
 
+/**
+ * @goal cli
+ */
+public class JenkinsCLIMojo extends AntMojo {
 
-public class JenkinsCLIMojo extends JenkinsAntRunMojo {
-	PropertiesUtils pu = new PropertiesUtils();
+	/**
+	 * The location of the Ant build.xml file. This can be a file on the file system, a file on the classpath, or any
+	 * URL that Spring's resource loading can understand
+	 * 
+	 * @parameter expression="${jenkins.cliWrapper}"
+	 *            default-value="classpath:org/kuali/jenkins/jobs/ant/cli-wrapper.xml"
+	 */
+	private String cliWrapper;
 
 	@Override
 	public void execute() throws MojoExecutionException {
-		String location = "classpath:org/kuali/jenkins/jenkins.properties";
-		Properties properties = pu.getProperties(location);
-		
-		super.execute();
+	}
+
+	public String getCliWrapper() {
+		return cliWrapper;
+	}
+
+	public void setCliWrapper(String cliWrapper) {
+		this.cliWrapper = cliWrapper;
 	}
 
 }
