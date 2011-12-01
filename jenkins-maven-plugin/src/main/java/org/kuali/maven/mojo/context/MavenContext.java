a186ebef469258f05a09e3a0e4aca9c67c0a91a4
jcaddel <jcaddel@kuali.org>
2011-12-01 08:14:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4164 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/mojo/context/MavenContext.java
@@ -0,0 +1,43 @@
+package org.kuali.maven.mojo.context;
+
+import org.apache.maven.project.MavenProject;
+
+public class MavenContext {
+	MavenProject mavenProject;
+	String scmType;
+	String scmUrl;
+	String majorVersion;
+
+	public MavenProject getMavenProject() {
+		return mavenProject;
+	}
+
+	public void setMavenProject(MavenProject mavenProject) {
+		this.mavenProject = mavenProject;
+	}
+
+	public String getScmType() {
+		return scmType;
+	}
+
+	public void setScmType(String scmType) {
+		this.scmType = scmType;
+	}
+
+	public String getScmUrl() {
+		return scmUrl;
+	}
+
+	public void setScmUrl(String scmUrl) {
+		this.scmUrl = scmUrl;
+	}
+
+	public String getMajorVersion() {
+		return majorVersion;
+	}
+
+	public void setMajorVersion(String majorVersion) {
+		this.majorVersion = majorVersion;
+	}
+
+}
