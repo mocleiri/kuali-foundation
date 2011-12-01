bdf47bed90c9a2860e1382b4f480bb7e24044c7c
jcaddel <jcaddel@kuali.org>
2011-12-01 10:35:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4182 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-kuali-common/src/test/java/org/kuali/maven/common/ExtractorTest.java
+++ maven-kuali-common/src/test/java/org/kuali/maven/common/ExtractorTest.java
@@ -8,10 +8,9 @@ public class ExtractorTest {
 	Extractor extractor = new Extractor();
 
 	@Test
-	public void test() {
+	public void testGetMajorVersion() {
 		String version = "1.0.0";
 		String majorVersion = extractor.getMajorVersion(version);
-		System.out.println(majorVersion);
 		Assert.assertEquals("1.0", majorVersion);
 	}
 
