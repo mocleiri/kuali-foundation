df365a89f8611ac3723f9071213052f72de86397
jcaddel <jcaddel@kuali.org>
2011-12-04 11:08:09 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4413 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- crlf/src/main/java/org/kuali/common/util/crlf/CRLF.java
+++ crlf/src/main/java/org/kuali/common/util/crlf/CRLF.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.common.util.strip;
+package org.kuali.common.util.crlf;
 
 import java.io.File;
 import java.io.FileInputStream;
