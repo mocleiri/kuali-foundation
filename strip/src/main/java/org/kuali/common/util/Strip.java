fad731f9a794f33471bb794407d4321b3930ce00
jcaddel <jcaddel@kuali.org>
2011-12-04 07:23:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4376 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ strip/src/main/java/org/kuali/common/util/Strip.java
@@ -0,0 +1,16 @@
+package org.kuali.common.util;
+
+public class Strip {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			System.out.println("hello world");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
