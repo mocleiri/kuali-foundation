8aadbd3c040fed809f681a88d28da50b1b9006f4
jcaddel <jcaddel@kuali.org>
2011-12-04 15:19:46 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4436 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ crlf/src/main/resources/example.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+M2_REPO=~/.m2/repository
+GROUPID=org/kuali/common/util
+ARTIFACTID=crlf
+CLASSIFIER=jar-with-dependencies
+VERSION=${project.version}
+JAR=$M2_REPO/$GROUPID/$ARTIFACTID/$VERSION/$ARTIFACTID-$VERSION-$CLASSIFIER.jar
+
+java -jar $JAR ${@}
