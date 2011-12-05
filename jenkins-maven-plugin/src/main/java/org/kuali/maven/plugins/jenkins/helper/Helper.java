887a6ef03aec8423b234ca13628c2ec6ec7ff084
jcaddel <jcaddel@kuali.org>
2011-12-05 13:12:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4479 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/helper/Helper.java
@@ -0,0 +1,20 @@
+package org.kuali.maven.plugins.jenkins.helper;
+
+import java.util.List;
+
+public class Helper {
+
+    public static final boolean isEmpty(String... args) {
+        return args == null || args.length == 0;
+    }
+
+    public static final void addArgs(List<String> list, String... args) {
+        if (isEmpty(args)) {
+            return;
+        }
+        for (String arg : args) {
+            list.add(arg);
+        }
+    }
+
+}
