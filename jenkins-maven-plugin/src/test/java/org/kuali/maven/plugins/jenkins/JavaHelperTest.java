4c34c5b50e58bdd8de02af0edb33c18bdd548a4e
jcaddel <jcaddel@kuali.org>
2011-12-05 11:58:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4471 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ jenkins-maven-plugin/src/test/java/org/kuali/maven/plugins/jenkins/JavaHelperTest.java
@@ -0,0 +1,19 @@
+package org.kuali.maven.plugins.jenkins;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JavaHelperTest {
+    private static final Logger logger = LoggerFactory.getLogger(JavaHelperTest.class);
+    JavaHelper helper = new JavaHelper();
+
+    @Test
+    public void testGetExecutable() {
+        String executable = helper.getExecutable();
+        logger.info(executable);
+        Assert.assertNotNull(executable);
+    }
+
+}
