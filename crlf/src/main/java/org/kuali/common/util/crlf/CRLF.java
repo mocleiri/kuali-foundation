c55769224ecd8093a8f76876b8e4e05b9161976d
jcaddel <jcaddel@kuali.org>
2011-12-04 23:19:38 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4449 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- crlf/src/main/java/org/kuali/common/util/crlf/CRLF.java
+++ crlf/src/main/java/org/kuali/common/util/crlf/CRLF.java
@@ -24,89 +24,90 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 
 public class CRLF {
-	public static final String FS = System.getProperty("file.separator");
-	public static final String LF = "\n";
-	public static final String CR = "\r";
-	public static final String CRLF = CR + LF;
-	public static final String DEBUG_KEY = "crlf.debug";
-	public static final boolean DEBUG = Boolean.getBoolean(DEBUG_KEY);
+    public static final String FS = System.getProperty("file.separator");
+    public static final String LF = "\n";
+    public static final String CR = "\r";
+    public static final String CRLF = CR + LF;
+    public static final String DEBUG_KEY = "crlf.debug";
+    public static final boolean DEBUG = Boolean.getBoolean(DEBUG_KEY);
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new CRLF().exec(args);
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new CRLF().exec(args);
+    }
 
-	public void exec(String[] args) {
-		if (isEmpty(args)) {
-			showUsage();
-			return;
-		}
-		try {
-			File dir = new File(".");
-			List<File> files = getFiles(dir, args);
-			replace(files);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void exec(String[] args) {
+        if (isEmpty(args)) {
+            showUsage();
+            return;
+        }
+        try {
+            File dir = new File(".");
+            List<File> files = getFiles(dir, args);
+            replace(files);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	protected boolean isEmpty(String[] args) {
-		return args == null || args.length == 0;
-	}
+    protected boolean isEmpty(String[] args) {
+        return args == null || args.length == 0;
+    }
 
-	protected void showUsage() {
-		System.out.println("usage: crlf [files]");
-	}
+    protected void showUsage() {
+        System.out.println("usage: crlf [files]");
+    }
 
-	protected void debug(String msg) {
-		if (!DEBUG) {
-			return;
-		} else {
-			System.out.println(msg);
-		}
-	}
+    protected void debug(String msg) {
+        if (!DEBUG) {
+            return;
+        } else {
+            System.out.println(msg);
+        }
+    }
 
-	protected void handleFile(File file, List<File> files) {
-		if (file.isDirectory()) {
-			debug("skipped " + file.getName() + " - dir");
-		} else if (!file.canRead()) {
-			System.out.println("skipped " + file.getName() + " - can't read");
-		} else if (!file.canWrite()) {
-			System.out.println("skipped " + file.getName() + " - can't write");
-		} else {
-			files.add(file);
-		}
-	}
+    protected void handleFile(File file, List<File> files) {
+        if (file.isDirectory()) {
+            debug("skipped " + file.getName() + " - dir");
+        } else if (!file.canRead()) {
+            System.out.println("skipped " + file.getName() + " - can't read");
+        } else if (!file.canWrite()) {
+            System.out.println("skipped " + file.getName() + " - can't write");
+        } else {
+            files.add(file);
+        }
+    }
 
-	protected List<File> getFiles(File dir, String[] args) throws IOException {
-		String path = dir.getCanonicalPath();
-		List<File> files = new ArrayList<File>();
-		for (String arg : args) {
-			String filename = path + FS + arg;
-			File file = new File(filename);
-			handleFile(file, files);
-		}
-		return files;
-	}
+    protected List<File> getFiles(File dir, String[] args) throws IOException {
+        String path = dir.getCanonicalPath();
+        List<File> files = new ArrayList<File>();
+        for (String arg : args) {
+            String filename = path + FS + arg;
+            File file = new File(filename);
+            handleFile(file, files);
+        }
+        return files;
+    }
 
-	protected void replace(List<File> files) throws IOException {
-		for (File file : files) {
-			String s = FileUtils.readFileToString(file);
-			if (s.indexOf(CR) == -1) {
-				debug("skipped " + file.getName() + " - no cr's");
-			} else {
-				s = replace(s);
-				FileUtils.write(file, s);
-				System.out.println("updated " + file.getName());
-			}
-		}
-	}
+    protected void replace(List<File> files) throws IOException {
+        for (File file : files) {
+            String s = FileUtils.readFileToString(file);
+            if (s.indexOf(CR) == -1) {
+                debug("skipped " + file.getName() + " - no cr's");
+            } else {
+                s = replace(s);
+                File newFile = new File(file.getAbsolutePath() + ".crlf");
+                FileUtils.write(newFile, s);
+                System.out.println("created " + newFile.getName());
+            }
+        }
+    }
 
-	protected String replace(String s) {
-		s = StringUtils.replace(s, CRLF, LF);
-		return StringUtils.replace(s, CR, LF);
-	}
+    protected String replace(String s) {
+        s = StringUtils.replace(s, CRLF, LF);
+        return StringUtils.replace(s, CR, LF);
+    }
 
 }
