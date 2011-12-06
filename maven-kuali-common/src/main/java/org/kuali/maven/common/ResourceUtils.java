0aacc52b64d6a2fe222a7f42972984ca507b7216
jcaddel <jcaddel@kuali.org>
2011-12-06 16:10:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4554 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-kuali-common/src/main/java/org/kuali/maven/common/ResourceUtils.java
+++ maven-kuali-common/src/main/java/org/kuali/maven/common/ResourceUtils.java
@@ -20,6 +20,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -28,80 +30,90 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 public class ResourceUtils {
-	ResourceLoader loader = new DefaultResourceLoader();
+    ResourceLoader loader = new DefaultResourceLoader();
 
-	/**
-	 * Given a location that can represent either a file on the file system or a Spring style resource, return an InputStream. The method checks the file
-	 * system first. If no file exists, it uses Spring resource loading to obtain an InputStream
-	 */
-	public InputStream getInputStream(String location) throws IOException {
-		File file = new File(location);
-		if (file.exists()) {
-			return new FileInputStream(file);
-		}
-		Resource resource = loader.getResource(location);
-		if (!resource.exists()) {
-			throw new IOException("Unable to locate " + location);
-		}
-		return resource.getInputStream();
-	}
+    /**
+     * Given a location that can represent either a file on the file system or a Spring style resource, return an
+     * InputStream. The method checks the file system first. If no file exists, it uses Spring resource loading to
+     * obtain an InputStream
+     */
+    public InputStream getInputStream(String location) throws IOException {
+        File file = new File(location);
+        if (file.exists()) {
+            return new FileInputStream(file);
+        }
+        Resource resource = loader.getResource(location);
+        if (!resource.exists()) {
+            throw new IOException("Unable to locate " + location);
+        }
+        return resource.getInputStream();
+    }
 
-	/**
-	 * Copy a URL location to the local file system
-	 */
-	public void copy(String location, String filename) throws IOException {
-		InputStream in = null;
-		OutputStream out = null;
-		try {
-			in = getInputStream(location);
-			out = FileUtils.openOutputStream(new File(filename));
-			IOUtils.copy(in, out);
-		} finally {
-			IOUtils.closeQuietly(in);
-			IOUtils.closeQuietly(out);
+    /**
+     * Copy a URL location to the local file system
+     */
+    public void copy(String location, String filename) throws IOException {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = getInputStream(location);
+            out = FileUtils.openOutputStream(new File(filename));
+            IOUtils.copy(in, out);
+        } finally {
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
 
-		}
-	}
+        }
+    }
 
-	/**
-	 * Write the string to the file system
-	 */
-	public void write(String filename, String contents) throws IOException {
-		OutputStream out = null;
-		try {
-			out = FileUtils.openOutputStream(new File(filename));
-			IOUtils.write(contents, out);
-		} finally {
-			IOUtils.closeQuietly(out);
-		}
+    /**
+     * Write the string to the file system
+     */
+    public void write(String filename, String contents) throws IOException {
+        OutputStream out = null;
+        try {
+            out = FileUtils.openOutputStream(new File(filename));
+            IOUtils.write(contents, out);
+        } finally {
+            IOUtils.closeQuietly(out);
+        }
 
-	}
+    }
 
-	/**
-	 * Read the contents of the URL location into a string
-	 */
-	public String read(String location) throws IOException {
-		InputStream in = null;
-		try {
-			in = getInputStream(location);
-			return IOUtils.toString(in);
-		} finally {
-			IOUtils.closeQuietly(in);
-		}
-	}
+    public List<String> read(List<String> locations) throws IOException {
+        List<String> contents = new ArrayList<String>();
+        for (String location : locations) {
+            String content = read(location);
+            contents.add(content);
+        }
+        return contents;
+    }
 
-	/**
-	 * Return a filename for this resource, i.e. typically the last part of the path: for example, "myfile.txt".
-	 */
-	public String getFilename(String location) throws IOException {
-		File file = new File(location);
-		if (file.exists()) {
-			return file.getName();
-		}
-		Resource resource = loader.getResource(location);
-		if (!resource.exists()) {
-			throw new IOException("Unable to locate " + location);
-		}
-		return resource.getFilename();
-	}
+    /**
+     * Read the contents of the URL location into a string
+     */
+    public String read(String location) throws IOException {
+        InputStream in = null;
+        try {
+            in = getInputStream(location);
+            return IOUtils.toString(in);
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
+    }
+
+    /**
+     * Return a filename for this resource, i.e. typically the last part of the path: for example, "myfile.txt".
+     */
+    public String getFilename(String location) throws IOException {
+        File file = new File(location);
+        if (file.exists()) {
+            return file.getName();
+        }
+        Resource resource = loader.getResource(location);
+        if (!resource.exists()) {
+            throw new IOException("Unable to locate " + location);
+        }
+        return resource.getFilename();
+    }
 }
