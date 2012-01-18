eb829155a89da3b70847103dca18e8f5a18b3787
jcaddel <jcaddel@kuali.org>
2012-01-18 12:41:18 -0500

    Change several log.info calls to log.debug
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@4957 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- license-maven-plugin/src/main/java/org/codehaus/mojo/license/AddThirdPartyMojo.java
+++ license-maven-plugin/src/main/java/org/codehaus/mojo/license/AddThirdPartyMojo.java
@@ -129,12 +129,12 @@ public class AddThirdPartyMojo extends AbstractAddThirdPartyMojo implements Mave
 
         SortedSet<MavenProject> unsafeDependencies = getUnsafeDependencies();
 
-        getLog().info("1.0");
+        getLog().debug("1.0");
         if (CollectionUtils.isNotEmpty(unsafeDependencies)) {
 
             // there is some unresolved license
 
-            getLog().info("2.0");
+            getLog().debug("2.0");
             if (isUseRepositoryMissingFiles()) {
 
                 // try to load missing third party files from dependencies
@@ -143,11 +143,11 @@ public class AddThirdPartyMojo extends AbstractAddThirdPartyMojo implements Mave
                 projects.remove(getProject());
                 projects.removeAll(unsafeDependencies);
 
-                getLog().info("2.1");
+                getLog().debug("2.1");
                 SortedProperties resolvedUnsafeMapping = new SortedProperties("UTF-8");// getThridPartyTool().loadThirdPartyDescriptorsForUnsafeMapping(
                 // getEncoding(), projects, unsafeDependencies, getLicenseMap(), localRepository,
                 // remoteRepositories);
-                getLog().info("2.2");
+                getLog().debug("2.2");
 
                 // push back resolved unsafe mappings
                 unsafeMappings.putAll(resolvedUnsafeMapping);
