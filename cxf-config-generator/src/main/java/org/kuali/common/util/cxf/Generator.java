8d65acab9d7be1564f60f65110decb8249b4aa08
jcaddel <jcaddel@kuali.org>
2012-01-19 11:54:58 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@5011 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- cxf-config-generator/src/main/java/org/kuali/common/util/cxf/Generator.java
+++ cxf-config-generator/src/main/java/org/kuali/common/util/cxf/Generator.java
@@ -13,31 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.common.threads.listener;
+package org.kuali.common.util.cxf;
 
-/**
- * Listen for events related to making progress towards an overall total.
- *
- * @param <T>
- */
-public interface ProgressListener<T> {
 
-    /**
-     * Progress has begun
-     */
-    void progressStarted();
+public class Generator {
+    public static void main(String[] args) {
 
-    /**
-     * Progress towards the total has occurred
-     *
-     * @param count
-     * @param total
-     * @param event
-     */
-    void progressOccurred(int count, int total, ProgressEvent<T> event);
-
-    /**
-     * Progress is now complete
-     */
-    void progressCompleted();
+    }
 }
