803b50b975f2907f1f098132423cf666a249f767
jcaddel <jcaddel@kuali.org>
2012-03-22 19:11:48 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@7523 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- deploy-artifact/deploy-tomcat-binaries.sh
+++ deploy-artifact/deploy-tomcat-binaries.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mvn process-resources -Ppublic -Ddp.groupId=org.apache.tomcat -Ddp.version=6.0.35 -Ddp.artifactId=apache-tomcat -Ddp.file=/home/ubuntu/.tomcat/apache-tomcat-6.0.35.zip
-mvn process-resources -Ppublic -Ddp.groupId=org.apache.tomcat -Ddp.version=7.0.22 -Ddp.artifactId=apache-tomcat -Ddp.file=/home/ubuntu/.tomcat/apache-tomcat-7.0.22.zip
+mvn process-resources -Ppublic -Ddp.groupId=org.apache.tomcat -Ddp.version=7.0.26 -Ddp.artifactId=apache-tomcat -Ddp.file=/home/ubuntu/.tomcat/apache-tomcat-7.0.26.zip
