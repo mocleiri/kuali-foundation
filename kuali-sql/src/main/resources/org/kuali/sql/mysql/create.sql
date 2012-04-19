22fd619375367a42bba80241e03546e3868090c1
jcaddel <jcaddel@kuali.org>
2012-04-18 21:41:48 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@7764 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-sql/src/main/resources/org/kuali/sql/mysql/create.sql
+++ kuali-sql/src/main/resources/org/kuali/sql/mysql/create.sql
@@ -1,11 +1,16 @@
 CREATE DATABASE IF NOT EXISTS ${sql.database}
-DEFAULT CHARACTER SET '${sql.mysql.character.set}'
-DEFAULT COLLATE '${sql.mysql.collate}'
+DEFAULT CHARACTER SET
+'${sql.mysql.character.set}'
+DEFAULT COLLATE
+'${sql.mysql.collate}'
 /
 
-CREATE USER '${sql.username}'@'%' IDENTIFIED BY '${sql.password}'
+CREATE USER '${sql.username}'@'%' IDENTIFIED
+BY '${sql.password}'
 /
-
+CREATE USER '${sql.username}'@'localhost' IDENTIFIED
+BY '${sql.password}'
+/
 GRANT ALL ON ${sql.database}.*
 TO '${sql.username}'@'%'
 WITH GRANT OPTION
--- kuali-sql/src/main/resources/org/kuali/sql/mysql/drop.sql
+++ kuali-sql/src/main/resources/org/kuali/sql/mysql/drop.sql
@@ -1,10 +1,15 @@
 DROP DATABASE IF EXISTS ${sql.database}
 /
--- ${sql.username} @ ${sql.password}
--- MySQL does not have DROP USER IF EXISTS, so we grant permissions on *.*
--- This creates a user if they don't exist (but doesn't error out if they already do)
--- Them immediately drop them
-GRANT USAGE ON *.* TO '${sql.username}'@'%' IDENTIFIED BY '${sql.password}'
+GRANT USAGE ON *.* TO '${sql.username}'@'%'
+IDENTIFIED BY '${sql.password}'
 /
-DROP USER ${sql.username}
+GRANT USAGE ON *.* TO '${sql.username}'@'localhost'
+IDENTIFIED BY
+'${sql.password}'
+/
+DROP
+USER '${sql.username}'@'%'
+/
+DROP
+USER '${sql.username}'@'localhost'
 /
