520cf61f1b329f32c3906d2ed5365ebc5ce62e43
jcaddel <jcaddel@kuali.org>
2012-04-22 14:10:03 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@7882 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ingester-maven-plugin/src/main/java/org/kuali/maven/plugins/ingester/IngestMojo.java
+++ ingester-maven-plugin/src/main/java/org/kuali/maven/plugins/ingester/IngestMojo.java
@@ -275,4 +275,36 @@ public class IngestMojo extends AbstractMojo {
         this.targetDatabase = targetDatabase;
     }
 
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getJdbcUsername() {
+        return jdbcUsername;
+    }
+
+    public void setJdbcUsername(String jdbcUsername) {
+        this.jdbcUsername = jdbcUsername;
+    }
+
+    public String getJdbcPassword() {
+        return jdbcPassword;
+    }
+
+    public void setJdbcPassword(String jdbcPassword) {
+        this.jdbcPassword = jdbcPassword;
+    }
+
+    public String getJdbcDriver() {
+        return jdbcDriver;
+    }
+
+    public void setJdbcDriver(String jdbcDriver) {
+        this.jdbcDriver = jdbcDriver;
+    }
+
 }
