9fa9dc60821cd702435a8a9e980e9ad0d36d3147
jcaddel <jcaddel@kuali.org>
2012-05-12 16:39:39 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@8876 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/pojo/Table.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/pojo/Table.java
@@ -5,15 +5,19 @@ import java.util.List;
 public class Table {
     List<Column> columns;
     List<Row> rows;
+
     public List<Column> getColumns() {
         return columns;
     }
+
     public void setColumns(List<Column> columns) {
         this.columns = columns;
     }
+
     public List<Row> getRows() {
         return rows;
     }
+
     public void setRows(List<Row> rows) {
         this.rows = rows;
     }
