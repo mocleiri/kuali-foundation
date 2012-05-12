88ccd33ef0daa6c5a3bac91102b8be4e12681c1e
jcaddel <jcaddel@kuali.org>
2012-05-12 16:39:16 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@8875 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/DescribeInstancesMojo.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/DescribeInstancesMojo.java
@@ -3,12 +3,14 @@ package org.kuali.maven.ec2;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.Tag;
 
 /**
  *
@@ -30,8 +32,32 @@ public class DescribeInstancesMojo extends AbstractEC2Mojo {
         DescribeInstancesResult result = client.describeInstances();
         List<Instance> instances = getInstances(result.getReservations());
         for (Instance i : instances) {
-            getLog().info(i.getInstanceId());
+            getLog().info(getInfo(i));
         }
+    }
+
+    protected String getHeader() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(StringUtils.rightPad("Name", 15, " "));
+        sb.append(StringUtils.rightPad("Instance ID", 15, " "));
+        return sb.toString();
+    }
+
+    protected String getInfo(Instance i) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(StringUtils.rightPad(getName(i), 15, " "));
+        sb.append(StringUtils.rightPad(i.getInstanceId(), 15, " "));
+        return sb.toString();
+    }
+
+    protected String getName(Instance i) {
+        List<Tag> tags = i.getTags();
+        for (Tag t : tags) {
+            if (t.getKey().equalsIgnoreCase("name")) {
+                return t.getValue();
+            }
+        }
+        return "";
     }
 
 }
---
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/pojo/Column.java
@@ -0,0 +1,23 @@
+package org.kuali.maven.ec2.pojo;
+
+public class Column {
+    String title;
+    int width;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+}
