5d9b023e28865f4333357cd73fab137b6e84fc46
jcaddel <jcaddel@kuali.org>
2012-05-12 16:26:06 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@8873 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/DescribeInstancesMojo.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/DescribeInstancesMojo.java
@@ -1,11 +1,13 @@
 package org.kuali.maven.ec2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.maven.plugin.MojoExecutionException;
 
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
 
 /**
@@ -14,13 +16,22 @@ import com.amazonaws.services.ec2.model.Reservation;
  */
 public class DescribeInstancesMojo extends AbstractEC2Mojo {
 
+    protected List<Instance> getInstances(List<Reservation> reservations) {
+        List<Instance> instances = new ArrayList<Instance>();
+        for (Reservation r : reservations) {
+            getLog().info(r.getReservationId());
+            instances.addAll(r.getInstances());
+        }
+        return instances;
+    }
+
     @Override
     public void execute() throws MojoExecutionException {
         AmazonEC2 client = getEC2Client();
         DescribeInstancesResult result = client.describeInstances();
-        List<Reservation> reservations = result.getReservations();
-        for (Reservation r : reservations) {
-            getLog().info(r.getReservationId());
+        List<Instance> instances = getInstances(result.getReservations());
+        for (Instance i : instances) {
+            getLog().info(i.getInstanceId());
         }
     }
 
