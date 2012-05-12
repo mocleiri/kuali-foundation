a7229d7771411882a48b70d97af61c827a355e0c
jcaddel <jcaddel@kuali.org>
2012-05-12 15:48:29 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@8861 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/RunInstanceMojo.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/RunInstanceMojo.java
@@ -3,6 +3,10 @@ package org.kuali.maven.ec2;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
+/**
+ *
+ * @goal runinstance
+ */
 public class RunInstanceMojo extends AbstractMojo {
 
     @Override
