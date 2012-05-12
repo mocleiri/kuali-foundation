aed6c9e5241dab0c9eb24281092b90a9755a6ffb
jcaddel <jcaddel@kuali.org>
2012-05-12 16:13:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@8870 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/DescribeInstancesMojo.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/DescribeInstancesMojo.java
@@ -2,14 +2,17 @@ package org.kuali.maven.ec2;
 
 import org.apache.maven.plugin.MojoExecutionException;
 
+import com.amazonaws.services.ec2.AmazonEC2;
+
 /**
  *
  * @goal describeinstances
  */
-public class DescribeInstancesMojo extends AbstractEc2Mojo {
+public class DescribeInstancesMojo extends AbstractEc1Mojo {
 
     @Override
     public void execute() throws MojoExecutionException {
+        AmazonEC2 client = getEC2Client();
         getLog().info("Hello world");
     }
 
--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/RunInstanceMojo.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/RunInstanceMojo.java
@@ -5,7 +5,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 /**
  * @goal runinstance
  */
-public class RunInstanceMojo extends AbstractEc2Mojo {
+public class RunInstanceMojo extends AbstractEc1Mojo {
 
     @Override
     public void execute() throws MojoExecutionException {
