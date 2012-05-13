2661ef16cd742bc5f0660c445019a30cad5cafdc
jcaddel <jcaddel@kuali.org>
2012-05-12 21:16:08 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@8893 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/RunInstanceMojo.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/RunInstanceMojo.java
@@ -2,14 +2,37 @@ package org.kuali.maven.ec2;
 
 import org.apache.maven.plugin.MojoExecutionException;
 
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+
 /**
  * @goal runinstance
  */
 public class RunInstanceMojo extends AbstractEC2Mojo {
 
+    /**
+     * The AMI to launch
+     *
+     * @parameter expression="${ec2.ami}"
+     * @required
+     */
+    private String ami;
+
+    /**
+     * The name of the key to use
+     *
+     * @parameter expression="${ec2.key}"
+     * @required
+     */
+    private String key;
+
     @Override
     public void execute() throws MojoExecutionException {
-        getLog().info("Hello world");
+        AmazonEC2 client = getEC2Client();
+        RunInstancesRequest request = new RunInstancesRequest();
+        request.setImageId(ami);
+        request.setKeyName(key);
+        client.runInstances(request);
     }
 
 }
