9db382a524af2eb798ac1b02b11238d3b3b357d9
jcaddel <jcaddel@kuali.org>
2012-05-12 21:39:19 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@8897 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-kuali-common/src/main/java/org/kuali/maven/common/PropertiesUtils.java
+++ maven-kuali-common/src/main/java/org/kuali/maven/common/PropertiesUtils.java
@@ -23,64 +23,77 @@ import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.maven.project.MavenProject;
 import org.springframework.util.PropertyPlaceholderHelper;
 
 public class PropertiesUtils {
-	PropertyPlaceholderHelper helper = new PropertyPlaceholderHelper("${", "}");
-	ResourceUtils resourceUtils = new ResourceUtils();
+    PropertyPlaceholderHelper helper = new PropertyPlaceholderHelper("${", "}");
+    ResourceUtils resourceUtils = new ResourceUtils();
 
-	/**
-	 * Split the string trimming as we go
-	 */
-	public static String[] splitAndTrim(String csv, String separator) {
-		String[] tokens = StringUtils.split(csv, separator);
-		for (String token : tokens) {
-			token = token.trim();
-		}
-		return tokens;
-	}
+    /**
+     * Split the string trimming as we go
+     */
+    public static String[] splitAndTrim(String csv, String separator) {
+        String[] tokens = StringUtils.split(csv, separator);
+        for (String token : tokens) {
+            token = token.trim();
+        }
+        return tokens;
+    }
 
-	public String getResolvedValue(String value, Properties properties) {
-		return helper.replacePlaceholders(value, properties);
-	}
+    public String getResolvedValue(String value, Properties properties) {
+        return helper.replacePlaceholders(value, properties);
+    }
 
-	public Properties getProperties(List<String> locations) throws IOException {
-		Properties properties = new Properties();
-		for (String location : locations) {
-			properties.putAll(getProperties(location));
-		}
-		return properties;
-	}
+    public Properties getProperties(List<String> locations) throws IOException {
+        Properties properties = new Properties();
+        for (String location : locations) {
+            properties.putAll(getProperties(location));
+        }
+        return properties;
+    }
 
-	public Properties getEnvironmentProperties() {
-		String prefix = "env.";
-		Map<String, String> env = System.getenv();
-		Properties properties = new Properties();
-		for (Map.Entry<String, String> pair : env.entrySet()) {
-			String key = prefix + pair.getKey();
-			String value = pair.getValue();
-			properties.setProperty(key, value);
-		}
-		return properties;
-	}
+    /**
+     * Return properties as viewed by Maven. ie, project properties get overridden by system properties and environment
+     * properties are prefixed with "env"
+     */
+    public Properties getMavenProperties(MavenProject project) {
+        Properties properties = new Properties();
+        properties.putAll(project.getProperties());
+        properties.putAll(getEnvironmentProperties());
+        properties.putAll(System.getProperties());
+        return properties;
+    }
 
-	public Properties getProperties(String location) throws IOException {
-		InputStream in = null;
-		try {
-			in = resourceUtils.getInputStream(location);
-			Properties properties = new Properties();
-			if (isXml(location)) {
-				properties.loadFromXML(in);
-			} else {
-				properties.load(in);
-			}
-			return properties;
-		} finally {
-			IOUtils.closeQuietly(in);
-		}
-	}
+    public Properties getEnvironmentProperties() {
+        String prefix = "env.";
+        Map<String, String> env = System.getenv();
+        Properties properties = new Properties();
+        for (Map.Entry<String, String> pair : env.entrySet()) {
+            String key = prefix + pair.getKey();
+            String value = pair.getValue();
+            properties.setProperty(key, value);
+        }
+        return properties;
+    }
 
-	public boolean isXml(String location) {
-		return location.toLowerCase().endsWith(".xml");
-	}
+    public Properties getProperties(String location) throws IOException {
+        InputStream in = null;
+        try {
+            in = resourceUtils.getInputStream(location);
+            Properties properties = new Properties();
+            if (isXml(location)) {
+                properties.loadFromXML(in);
+            } else {
+                properties.load(in);
+            }
+            return properties;
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
+    }
+
+    public boolean isXml(String location) {
+        return location.toLowerCase().endsWith(".xml");
+    }
 }
