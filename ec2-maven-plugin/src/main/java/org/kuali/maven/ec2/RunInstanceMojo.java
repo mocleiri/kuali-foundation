79a63ecd51a79d7edac671898203bbb583e650f3
jcaddel <jcaddel@kuali.org>
2012-05-12 21:29:15 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@8894 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/RunInstanceMojo.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/RunInstanceMojo.java
@@ -1,8 +1,14 @@
 package org.kuali.maven.ec2;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.InstanceType;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 
 /**
@@ -26,13 +32,65 @@ public class RunInstanceMojo extends AbstractEC2Mojo {
      */
     private String key;
 
+    /**
+     * The type of instance to launch
+     *
+     * @parameter expression="${ec2.type}" default-value="c1.medium";
+     * @required
+     */
+    private String type;
+
+    /**
+     * The security groups into which the instance will be launched
+     *
+     * @parameter
+     */
+    private List<String> securityGroups;
+
+    /**
+     * Optional user data for the instance
+     *
+     * @parameter expression="${ec2.userData}"
+     */
+    private String userData;
+
+    /**
+     * If supplied, the contents of the file are supplied to the instance as userData
+     *
+     * @parameter expression="${ec2.userDataFile}"
+     */
+    private File userDataFile;
+
+    /**
+     * The encoding of the userDataFile
+     *
+     * @parameter expression="${ec2.encoding}" default-value="${project.build.sourceEncoding}"
+     */
+    private String encoding;
+
     @Override
     public void execute() throws MojoExecutionException {
         AmazonEC2 client = getEC2Client();
         RunInstancesRequest request = new RunInstancesRequest();
         request.setImageId(ami);
         request.setKeyName(key);
+        request.setInstanceType(InstanceType.fromValue(type));
+        request.setSecurityGroups(securityGroups);
+        attachUserData(request);
         client.runInstances(request);
     }
 
+    protected void attachUserData(RunInstancesRequest request) throws MojoExecutionException {
+
+        if (userDataFile != null) {
+            try {
+                String userData = FileUtils.readFileToString(userDataFile, encoding);
+                request.setUserData(userData);
+            } catch (IOException e) {
+                throw new MojoExecutionException("Error reading user data file", e);
+            }
+        } else {
+            request.setUserData(userData);
+        }
+    }
 }
