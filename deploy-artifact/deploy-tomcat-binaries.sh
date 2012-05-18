b3ec8b6a03272ffcd303f2a30a8b5c5caa9d739f
jcaddel <jcaddel@kuali.org>
2012-05-18 17:58:05 -0400

    [maven-release-plugin]  copy for tag kuali-pom-1.2.17
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-pom-1.2.17@9171 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- deploy-artifact/deploy-tomcat-binaries.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mvn process-resources -Ppublic -Ddp.groupId=org.apache.tomcat -Ddp.version=6.0.35 -Ddp.artifactId=apache-tomcat -Ddp.file=/home/ubuntu/.tomcat/apache-tomcat-6.0.35.zip
-mvn process-resources -Ppublic -Ddp.groupId=org.apache.tomcat -Ddp.version=7.0.26 -Ddp.artifactId=apache-tomcat -Ddp.file=/home/ubuntu/.tomcat/apache-tomcat-7.0.26.zip
