ffbd1e61e376be6a7db731c6bcbacc6a90558186
jcaddel <jcaddel@kuali.org>
2012-05-24 18:13:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@9475 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/SyncWorkspaceMojo.java
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/SyncWorkspaceMojo.java
@@ -15,9 +15,22 @@
  */
 package org.kuali.maven.plugins.jenkins;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.cli.CommandLineException;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
+import org.codehaus.plexus.util.cli.Commandline;
+import org.codehaus.plexus.util.cli.DefaultConsumer;
+import org.codehaus.plexus.util.cli.StreamConsumer;
+import org.kuali.maven.plugins.jenkins.helper.DirectoryFileFilter;
+import org.kuali.maven.plugins.jenkins.helper.RsyncHelper;
 
 /**
  * @goal syncworkspace
@@ -25,6 +38,7 @@ import org.apache.maven.project.MavenProject;
  * @aggregator
  */
 public class SyncWorkspaceMojo extends AbstractMojo {
+    RsyncHelper helper = new RsyncHelper();
 
     /**
      * The Maven project object
@@ -39,9 +53,110 @@ public class SyncWorkspaceMojo extends AbstractMojo {
      */
     private boolean excludeTarget;
 
+    /**
+     * @parameter expression="${jenkins.failOnError}" default-value="true"
+     */
+    private boolean failOnError;
+
+    /**
+     * @parameter expression="${jenkins.excludesFile}" default-value="${project.build.directory}/jenkins/rsync-excludes"
+     */
+    private File excludesFile;
+
+    /**
+     * @parameter expression="${jenkins.workingDir}" default-value="${project.build.directory}/jenkins"
+     */
+    private File workingDir;
+
+    /**
+     * @parameter expression="${jenkins.source}" default-value="${project.basedir}"
+     * @required
+     */
+    private String source;
+
+    /**
+     * @parameter expression="${jenkins.destination}"
+     * @required
+     */
+    private String destination;
+
+    /**
+     * @parameter expression="${jenkins.executable}" default-value="rsync"
+     * @required
+     */
+    private String executable;
+
+    protected Commandline getCommandLine() {
+        Commandline cl = new Commandline();
+        cl.setExecutable(executable);
+        cl.setWorkingDirectory(project.getBasedir());
+        addArgs(cl, getArgs());
+        return cl;
+    }
+
+    protected void addArgs(Commandline cl, List<String> args) {
+        if (args == null || args.size() == 0) {
+            return;
+        }
+        for (String arg : args) {
+            cl.createArg().setValue(arg);
+        }
+    }
+
+    protected List<String> getArgs() {
+        List<String> args = new ArrayList<String>();
+        args.add("-azv");
+        args.add("--stats");
+        args.add("--delete");
+        args.add("--delete-excluded");
+        args.add("--exclude-from");
+        args.add(excludesFile.getAbsolutePath());
+        args.add(source);
+        args.add(destination);
+        return args;
+    }
+
+    protected int executeRsync() throws MojoExecutionException {
+        StreamConsumer stdout = new DefaultConsumer();
+        StreamConsumer stderr = new DefaultConsumer();
+        Commandline cl = getCommandLine();
+        getLog().info(cl.toString());
+        try {
+            return CommandLineUtils.executeCommandLine(cl, stdout, stderr);
+        } catch (CommandLineException e) {
+            throw new MojoExecutionException("Error executing " + executable, e);
+        }
+    }
+
     @Override
     public void execute() throws MojoExecutionException {
-        getLog().info("Hello world");
+        prepareFileSystem();
+        int exitValue = executeRsync();
+        validateExitValue(exitValue);
+    }
+
+    protected boolean isFail(int exitValue) {
+        return exitValue != 0 && failOnError;
+    }
+
+    protected void validateExitValue(int exitValue) throws MojoExecutionException {
+        if (isFail(exitValue)) {
+            throw new MojoExecutionException("Non-zero exit value");
+        }
+    }
+
+    protected void prepareFileSystem() throws MojoExecutionException {
+        try {
+            if (excludeTarget) {
+                DirectoryFileFilter dff = new DirectoryFileFilter();
+                List<File> excludeDirs = helper.getMatchingDirs(project.getBasedir(), "/target", dff);
+                List<String> excludes = helper.getExcludesList(project.getBasedir(), excludeDirs);
+                FileUtils.touch(excludesFile);
+                FileUtils.writeLines(excludesFile, excludes);
+            }
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error preparing file system", e);
+        }
     }
 
     public MavenProject getProject() {
@@ -54,6 +169,30 @@ public class SyncWorkspaceMojo extends AbstractMojo {
 
     public void setExcludeTarget(boolean excludeTarget) {
         this.excludeTarget = excludeTarget;
+    }
+
+    public File getWorkingDir() {
+        return workingDir;
+    }
+
+    public void setWorkingDir(File workingDir) {
+        this.workingDir = workingDir;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
     }
 
 }
