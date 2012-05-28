5152fb93abdbfdfef61a8e025e539d50e703889a
jcaddel <jcaddel@kuali.org>
2012-05-28 16:45:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@9630 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/resources/org/kuali/common/deploy/bin/setenv.sh
+++ kuali-deploy/src/main/resources/org/kuali/common/deploy/bin/setenv.sh
@@ -3,7 +3,23 @@
 #
 # This script is invoked during the Tomcat startup sequence BEFORE Tomcat starts
 #
-# This provides a hook for setting environment variables or doing whatever
-# else needs to be done to the environment before launching Tomcat
+
 #
+# All of the properties listed below default to the empty string, override them as necessary
 #
+
+# Environment setup that applies Kuali wide
+${setenv.kuali.content}
+
+# Environment setup specific to a Kuali project eg, Rice, Student, or OLE
+${setenv.group.content}
+
+# Environment setup specific to a Kuali application eg rice-sampleapp or ks-embedded
+${setenv.app.content}
+
+# Environment setup specific to a version of a Kuali application eg config for 1.0 might be very different from 2.0
+${setenv.version.content}
+
+# Environment setup specific to the environment the application is deployed to
+${setenv.env.content}
+
