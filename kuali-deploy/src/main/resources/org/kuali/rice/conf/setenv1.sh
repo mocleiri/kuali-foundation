b572882087859ab3409c62f120df3f4c0cdc1930
jcaddel <jcaddel@kuali.org>
2012-05-28 12:59:15 -0400

    [maven-release-plugin]  copy for tag ec2-maven-plugin-1.0.4
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/ec2-maven-plugin-1.0.4@9609 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/resources/org/kuali/rice/conf/setenv1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#
-# This script is invoked during the Tomcat startup sequence BEFORE Tomcat starts
-#
-# This provides a hook for setting environment variables or doing whatever
-# else needs to be done to the environment before launching Tomcat
-#
-#
