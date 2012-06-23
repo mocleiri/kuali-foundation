859d1fdf70ec8a2535d6e2f9cb2d3ef265adfd99
jcaddel <jcaddel@kuali.org>
2012-06-22 20:10:07 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@9956 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/SyncWorkspacesMojo.java
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/SyncWorkspacesMojo.java
@@ -86,7 +86,7 @@ public class SyncWorkspacesMojo extends AbstractMojo {
         List<Commandline> executions = new ArrayList<Commandline>();
         for (String name : names) {
             String src = basedir.getAbsolutePath() + "/" + name + "/workspace/";
-            String dst = destination + "/" + name;
+            String dst = "root@ec2-174-129-111-212.compute-1.amazonaws.com:" + destination + "/" + name;
             Commandline cl = getCommandLine();
             addArg(cl, "-av");
             addArg(cl, "--stats");
@@ -99,7 +99,7 @@ public class SyncWorkspacesMojo extends AbstractMojo {
         for (int i = 0; i < executions.size(); i++) {
             Commandline cl = executions.get(i);
             getLog().info(StringUtils.leftPad((i + 1) + "", 3) + " : " + cl.toString());
-            int exitValue = executeRsync(cl);
+            int exitValue = 0; // executeRsync(cl);
             validateExitValue(exitValue);
         }
         long elapsed = System.currentTimeMillis() - start;
