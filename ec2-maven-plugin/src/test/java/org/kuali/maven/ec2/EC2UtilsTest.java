bc6b804b1dafe428ba08683b143ff7728fe2ca2e
jcaddel <jcaddel@kuali.org>
2012-06-26 21:25:42 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@10060 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/test/java/org/kuali/maven/ec2/EC2UtilsTest.java
+++ ec2-maven-plugin/src/test/java/org/kuali/maven/ec2/EC2UtilsTest.java
@@ -7,9 +7,13 @@ import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.Test;
 
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.BlockDeviceMapping;
 import com.amazonaws.services.ec2.model.DescribeImagesRequest;
 import com.amazonaws.services.ec2.model.DescribeImagesResult;
+import com.amazonaws.services.ec2.model.EbsBlockDevice;
 import com.amazonaws.services.ec2.model.Image;
+import com.amazonaws.services.ec2.model.RegisterImageRequest;
+import com.amazonaws.services.ec2.model.RegisterImageResult;
 
 public class EC2UtilsTest {
 
@@ -31,18 +35,57 @@ public class EC2UtilsTest {
     }
 
     @Test
+    public void testRegisterImage() {
+        AmazonEC2Client client = getEC2Client();
+        try {
+            RegisterImageRequest request = new RegisterImageRequest();
+            request.setName("ci-slave-2012-02-22-test");
+            request.setDescription("ci.slave.test.image");
+            request.setArchitecture("x86_64");
+            request.setRootDeviceName("/dev/sda1");
+            request.setKernelId("aki-825ea7eb");
+            BlockDeviceMapping bdm = new BlockDeviceMapping();
+            bdm.setDeviceName("/dev/sda1");
+            EbsBlockDevice ebs = new EbsBlockDevice();
+            ebs.setDeleteOnTermination(true);
+            ebs.setSnapshotId("snap-a6dc42d8");
+            ebs.setVolumeSize(128);
+            bdm.setEbs(ebs);
+            request.setBlockDeviceMappings(Collections.singletonList(bdm));
+
+            RegisterImageResult result = client.registerImage(request);
+            String id = result.getImageId();
+            System.out.println(id);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void testDescribeImages() {
         AmazonEC2Client client = getEC2Client();
         DescribeImagesRequest request = new DescribeImagesRequest();
         request.setOwners(Collections.singletonList("self"));
         DescribeImagesResult result = client.describeImages(request);
         List<Image> images = result.getImages();
+        Collections.sort(images, new ImageComparator());
         System.out.println(images.size());
         System.out.println("------------ --------------");
         System.out.println("Image Id     Image Location");
         System.out.println("------------ --------------");
         for (Image image : images) {
-            System.out.println(image.getImageId() + " " + image.getImageLocation());
+            System.out.print(image.getImageId() + " " + image.getImageLocation());
+            System.out.print(" ");
+            List<BlockDeviceMapping> bdms = image.getBlockDeviceMappings();
+            for (BlockDeviceMapping bdm : bdms) {
+                System.out.print(bdm.getDeviceName());
+                EbsBlockDevice ebs = bdm.getEbs();
+                System.out.print("=" + ebs.getSnapshotId() + ":" + ebs.getVolumeSize() + ":"
+                        + ebs.getDeleteOnTermination());
+                System.out.print(" ");
+            }
+            System.out.println();
+
         }
     }
 }
