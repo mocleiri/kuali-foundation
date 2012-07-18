811e17778baf5b60b2671b8132261236551018c5
jcaddel <jcaddel@kuali.org>
2012-07-17 20:16:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@10286 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/CleanupImagesMojo.java
+++ ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/CleanupImagesMojo.java
@@ -10,21 +10,29 @@ import org.apache.maven.plugin.MojoExecutionException;
 public class CleanupImagesMojo extends AbstractEC2Mojo {
 
     /**
+     * This is the name of the tag to match.
+     * 
      * @parameter expression="${ec2.key}" default-value="Name"
      */
     private String key;
 
     /**
+     * If the specified tag starts with the value provided here, the image will be deregistered.
+     * 
      * @parameter expression="${ec2.prefix}" default-value="CI Slave"
      */
     private String prefix;
 
     /**
+     * The name of the device whose associated snapshot will be deleted when the image is deregistered.
+     * 
      * @parameter expression="${ec2.device}" default-value="/dev/sda1"
      */
     private String device;
 
     /**
+     * The minimum number of images to retain.
+     * 
      * @parameter expression="${ec2.minimumToRetain}" default-value="3"
      */
     private int minimumToRetain;
