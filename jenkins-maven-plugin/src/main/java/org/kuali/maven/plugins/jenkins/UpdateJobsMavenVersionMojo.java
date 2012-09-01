f5d9608c1c517e8bc435154f9dba5820cd29b056
jcaddel <jcaddel@kuali.org>
2012-09-01 01:28:52 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@10865 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/UpdateJobsMavenVersionMojo.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2011-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.maven.plugins.jenkins;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * @goal updatejobsmavenversion
+ */
+public class UpdateJobsMavenVersionMojo extends AbstractMojo {
+	@Override
+	public void execute() throws MojoExecutionException {
+		try {
+			File directory = new File("/var/lib/jenkins/jobs");
+			File[] files = directory.listFiles();
+			List<File> list = new ArrayList<File>();
+			for (File file : files) {
+				File config = new File(file.getAbsolutePath() + "/config.xml");
+				if (config.exists()) {
+					list.add(config);
+				}
+			}
+			getLog().info("Located " + list.size() + " files");
+		} catch (Exception e) {
+			throw new MojoExecutionException("Unexpected error", e);
+		}
+	}
+}
