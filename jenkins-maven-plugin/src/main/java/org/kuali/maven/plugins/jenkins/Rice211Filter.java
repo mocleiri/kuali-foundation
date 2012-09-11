388a8ab25ebf26b4a52c2ca5d8e052927d02f67d
jcaddel <jcaddel@kuali.org>
2012-09-11 09:08:07 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@10946 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/Rice211Filter.java
@@ -0,0 +1,24 @@
+package org.kuali.maven.plugins.jenkins;
+
+import java.io.File;
+
+import org.apache.commons.io.filefilter.IOFileFilter;
+
+public class Rice211Filter implements IOFileFilter {
+	String token = "2.1.1.";
+
+	@Override
+	public boolean accept(File file) {
+		return accept(file.getAbsolutePath());
+	}
+
+	@Override
+	public boolean accept(File dir, String name) {
+		return accept(name);
+	}
+
+	protected boolean accept(String name) {
+		return name.indexOf(token) != -1;
+	}
+
+}
