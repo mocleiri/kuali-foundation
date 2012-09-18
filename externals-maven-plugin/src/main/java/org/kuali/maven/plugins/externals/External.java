4fd8853e9c8103813dc4caee2e22c2f64e85d6d0
jcaddel <jcaddel@kuali.org>
2012-09-18 19:58:25 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11035 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ externals-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/External.java
@@ -0,0 +1,32 @@
+package org.kuali.maven.plugins.externals;
+
+public class External {
+	String url;
+	String module;
+	Integer revision;
+	String property;
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getModule() {
+		return module;
+	}
+	public void setModule(String module) {
+		this.module = module;
+	}
+	public Integer getRevision() {
+		return revision;
+	}
+	public void setRevision(Integer revision) {
+		this.revision = revision;
+	}
+	public String getProperty() {
+		return property;
+	}
+	public void setProperty(String property) {
+		this.property = property;
+	}
+}
