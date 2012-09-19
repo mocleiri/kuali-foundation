b55e0cc3d187c0f55845f05bb2cfe13aeeceb43e
jcaddel <jcaddel@kuali.org>
2012-09-19 15:02:09 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11053 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- deploy-artifact/jdk7.sh
+++ deploy-artifact/jdk7.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+VERSION=1.7.0-u07
 ARGS="$ARGS process-resources"
 ARGS="$ARGS -Ps3private"
 ARGS="$ARGS -Ddp.groupId=com.oracle"
-ARGS="$ARGS -Ddp.version=1.7.0-u07"
+ARGS="$ARGS -Ddp.version=$VERSION"
 ARGS="$ARGS -Ddp.artifactId=jdk7"
 ARGS="$ARGS -Ddp.classifier=linux-x64"
-ARGS="$ARGS -Ddp.file=/Users/jeffcaddel/Downloads/jdk1.7.0-u07.zip"
-mvn process-resources -Ps3private -Ddp.groupId=com.oracle -Ddp.version=1.7.0-u07 -Ddp.artifactId=jdk7 -Ddp.file=/Users/jeffcaddel/Downloads/jdk1.7.0-u07.zip
+ARGS="$ARGS -Ddp.file=/Users/jeffcaddel/Downloads/jdk7/jdk$VERSION.zip"
+mvn $ARGS
