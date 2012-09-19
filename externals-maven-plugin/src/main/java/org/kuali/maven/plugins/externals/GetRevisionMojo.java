9ff5aa80e41983bcc3c2477e7f2bcf3eb392a4d5
jcaddel <jcaddel@kuali.org>
2012-09-19 08:24:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11039 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ externals-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/GetRevisionMojo.java
@@ -0,0 +1,18 @@
+package org.kuali.maven.plugins.externals;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @goal getrevision
+ * 
+ * @author jeffcaddel
+ */
+public class GetRevisionMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+	}
+
+}
