77c3e6b0e6280614a0222224499dce4f89e09a2b
jcaddel <jcaddel@kuali.org>
2012-09-19 09:34:19 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11045 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- externals-maven-plugin/src/test/java/org/kuali/maven/plugins/externals/SVNRepositoryTest.java
+++ externals-maven-plugin/src/test/java/org/kuali/maven/plugins/externals/SVNRepositoryTest.java
@@ -1,24 +1,19 @@
 package org.kuali.maven.plugins.externals;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tmatesoft.svn.core.SVNDirEntry;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
-import org.tmatesoft.svn.core.auth.SVNAuthentication;
-import org.tmatesoft.svn.core.auth.SVNPasswordAuthentication;
 import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 public class SVNRepositoryTest {
 
-	private static final String UTF8 = "UTF-8";
 	private static final Logger log = LoggerFactory.getLogger(SVNRepositoryTest.class);
 
 	@Test
@@ -27,9 +22,12 @@ public class SVNRepositoryTest {
 			String url = "https://svn.kuali.org/repos/student/enrollment/aggregate/trunk";
 			String username = "jcaddel";
 			String password = System.getProperty("svn.password");
-			SVNRepository repository = getRepository(url, username, password);
-			log.info("Repository Root: " + repository.getRepositoryRoot(true));
-			log.info("Repository UUID: " + repository.getRepositoryUUID(true));
+			SVNRepository repo = getRepository(url, username, password);
+			SVNDirEntry entry = repo.info("", -1);
+			log.info("Repository Root: " + repo.getRepositoryRoot(true));
+			log.info("Repository UUID: " + repo.getRepositoryUUID(true));
+			log.info("Latest Repository Revision: " + repo.getLatestRevision());
+			log.info("Latest Path revision: " + entry.getRevision());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -37,25 +35,12 @@ public class SVNRepositoryTest {
 
 	protected SVNRepository getRepository(String url, String username, String password) throws SVNException {
 		DAVRepositoryFactory.setup();
-		SVNURL svnUrl = SVNURL.parseURIEncoded(url);
-		ISVNAuthenticationManager authManager = getAuthenticationManager(username, password);
+		@SuppressWarnings("deprecation")
+		SVNURL svnUrl = SVNURL.parseURIDecoded(url);
+		ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(username, password);
 		SVNRepository repository = SVNRepositoryFactory.create(svnUrl, null);
 		repository.setAuthenticationManager(authManager);
 		return repository;
-	}
-
-	protected String encodeUTF8(String s) {
-		try {
-			return URLEncoder.encode(s, UTF8);
-		} catch (UnsupportedEncodingException e) {
-			throw new IllegalStateException(UTF8 + " encoding is not supported.", e);
-		}
-	}
-
-	protected ISVNAuthenticationManager getAuthenticationManager(String username, String password) {
-		SVNAuthentication auth = new SVNPasswordAuthentication(username, password, false, null, false);
-		SVNAuthentication[] array = new SVNAuthentication[] { auth };
-		return new BasicAuthenticationManager(array);
 	}
 
 }
