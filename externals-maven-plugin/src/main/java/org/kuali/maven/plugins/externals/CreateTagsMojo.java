ae6e1ebc457e68bd3d479f16e20670a19be88e46
jcaddel <jcaddel@kuali.org>
2012-09-20 20:25:05 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11075 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- externals-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/CreateTagsMojo.java
+++ externals-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/CreateTagsMojo.java
@@ -4,11 +4,23 @@ import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
 
 /**
+ * 
  * @goal createtags
  */
 public class CreateTagsMojo extends AbstractMojo {
+
+	SVNUtils svnUtils = SVNUtils.getInstance();
+
+	/**
+	 * The Maven project object
+	 * 
+	 * @parameter expression="${project}"
+	 * @readonly
+	 */
+	private MavenProject project;
 
 	/**
 	 * 
@@ -25,6 +37,10 @@ public class CreateTagsMojo extends AbstractMojo {
 
 	public void setMappings(List<Mapping> mappings) {
 		this.mappings = mappings;
+	}
+
+	public MavenProject getProject() {
+		return project;
 	}
 
 }
