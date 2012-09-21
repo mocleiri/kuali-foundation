9b6b14ee522edc9c3b1e1b1f40f7065c7ab55d61
jcaddel <jcaddel@kuali.org>
2012-09-20 20:20:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11074 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- externals-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/CreateTagsMojo.java
+++ externals-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/CreateTagsMojo.java
@@ -1,16 +1,30 @@
 package org.kuali.maven.plugins.externals;
 
+import java.util.List;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 /**
- * 
  * @goal createtags
  */
 public class CreateTagsMojo extends AbstractMojo {
 
+	/**
+	 * 
+	 */
+	private List<Mapping> mappings;
+
 	@Override
 	public void execute() throws MojoExecutionException {
+	}
+
+	public List<Mapping> getMappings() {
+		return mappings;
+	}
+
+	public void setMappings(List<Mapping> mappings) {
+		this.mappings = mappings;
 	}
 
 }
---
+++ externals-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/Mapping.java
@@ -0,0 +1,22 @@
+package org.kuali.maven.plugins.externals;
+
+public class Mapping {
+	String path;
+	String property;
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+}
