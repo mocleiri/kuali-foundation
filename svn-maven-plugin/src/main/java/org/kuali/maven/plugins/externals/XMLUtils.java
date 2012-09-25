dceecc0d223ecd34f460c0994dabcb61270bd154
jcaddel <jcaddel@kuali.org>
2012-09-25 09:17:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11134 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- svn-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/XMLUtils.java
+++ svn-maven-plugin/src/main/java/org/kuali/maven/plugins/externals/XMLUtils.java
@@ -54,6 +54,7 @@ public class XMLUtils {
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document document = builder.parse(src);
 			NodeList children = document.getChildNodes();
+			getParentNode(document);
 			String s = getDisplayString(children, -1);
 			logger.info("XML Structure: \n" + s);
 			return null;
@@ -62,18 +63,45 @@ public class XMLUtils {
 		}
 	}
 
+	protected Node getParentNode(Document document) {
+		NodeList nodeList = document.getElementsByTagName("parent");
+		int childCount = nodeList.getLength();
+		for (int i = 0; i < childCount; i++) {
+			Node node = nodeList.item(i);
+		}
+		logger.info(nodeList + "");
+		return null;
+	}
+
 	protected String getDisplayString(NodeList nodeList, int level) {
 		StringBuilder sb = new StringBuilder();
 		int childCount = nodeList.getLength();
 		level++;
 		for (int i = 0; i < childCount; i++) {
 			Node node = nodeList.item(i);
-			String name = node.getNodeName();
-			int type = node.getNodeType();
-			String value = node.getNodeValue();
-			sb.append(level + ":" + StringUtils.repeat(" ", level) + "[" + name + "," + type + "," + flatten(value) + "]\n");
+			sb.append(getDisplayString(node, level) + "\n");
 			sb.append(getDisplayString(node.getChildNodes(), level));
 		}
+		return sb.toString();
+	}
+
+	protected String getDisplayString(Node node, int level) {
+		String name = node.getNodeName();
+		int type = node.getNodeType();
+		String value = node.getNodeValue();
+		StringBuilder sb = new StringBuilder();
+		if (level != -1) {
+			sb.append(level + ":");
+			sb.append(StringUtils.repeat(" ", level));
+		}
+		sb.append("[");
+		sb.append(name);
+		sb.append(",");
+		sb.append(type);
+		sb.append(",");
+		sb.append(flatten(value));
+		sb.append(",");
+		sb.append("]");
 		return sb.toString();
 	}
 
