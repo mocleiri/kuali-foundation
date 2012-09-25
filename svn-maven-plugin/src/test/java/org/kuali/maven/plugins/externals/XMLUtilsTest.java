6a0dac6e4ae71c21ee771a6dca1fb88e091f3948
jcaddel <jcaddel@kuali.org>
2012-09-25 07:46:46 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11131 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ svn-maven-plugin/src/test/java/org/kuali/maven/plugins/externals/XMLUtilsTest.java
@@ -0,0 +1,32 @@
+package org.kuali.maven.plugins.externals;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XMLUtilsTest {
+	private static final Logger logger = LoggerFactory.getLogger(XMLUtilsTest.class);
+
+	MojoHelper helper = MojoHelper.getInstance();
+	XMLUtils xmlUtils = new XMLUtils();
+	private static final String POM = "pom.xml";
+	private static final String IGNORE = "src,target,.svn,.git";
+	private static final File BASEDIR = new File("/Users/jeffcaddel/ws/aggregate");
+
+	@Test
+	public void testGetFiles() {
+		try {
+			List<File> poms = helper.getPoms(BASEDIR, POM, IGNORE);
+			File pom = poms.get(0);
+			String oldXml = FileUtils.readFileToString(pom);
+			String newXml = xmlUtils.format(oldXml);
+			logger.info(newXml);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
