f701676e81a2d343379cc01c160fc0f159996287
jcaddel <jcaddel@kuali.org>
2012-10-04 09:25:33 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11357 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- jenkins-maven-plugin/src/test/java/org/kuali/maven/plugins/jenkins/helper/JavaHelperTest.java
+++ jenkins-maven-plugin/src/test/java/org/kuali/maven/plugins/jenkins/helper/JavaHelperTest.java
@@ -18,32 +18,31 @@ package org.kuali.maven.plugins.jenkins.helper;
 import java.io.File;
 
 import org.junit.Assert;
-import org.junit.Test;
 import org.kuali.maven.plugins.jenkins.context.ProcessResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JavaHelperTest {
-    private static final String FS = System.getProperty("file.separator");
-    private static final Logger logger = LoggerFactory.getLogger(JavaHelperTest.class);
-    JavaHelper helper = new JavaHelper();
+	private static final String FS = System.getProperty("file.separator");
+	private static final Logger logger = LoggerFactory.getLogger(JavaHelperTest.class);
+	JavaHelper helper = new JavaHelper();
 
-    @Test
-    public void testGetExecutable() {
-        String executable = helper.getExecutable();
-        logger.info(executable);
-        Assert.assertNotNull(executable);
-    }
+	// @Test
+	public void testGetExecutable() {
+		String executable = helper.getExecutable();
+		logger.info(executable);
+		Assert.assertNotNull(executable);
+	}
 
-    @Test
-    public void testExecuteJar() {
-        String path = "." + FS + "src" + FS + "test" + FS + "resources" + FS + "jenkins-cli-1.440.jar";
-        File jar = new File(path);
-        ProcessResult result = helper.executeJar(jar);
-        logger.info("Result: " + result.getExitValue());
-        for (String line : result.getOutputLines()) {
-            logger.info(line);
-        }
-        // Assert.assertEquals(255, result.getExitValue());
-    }
+	// @Test
+	public void testExecuteJar() {
+		String path = "." + FS + "src" + FS + "test" + FS + "resources" + FS + "jenkins-cli-1.440.jar";
+		File jar = new File(path);
+		ProcessResult result = helper.executeJar(jar);
+		logger.info("Result: " + result.getExitValue());
+		for (String line : result.getOutputLines()) {
+			logger.info(line);
+		}
+		// Assert.assertEquals(255, result.getExitValue());
+	}
 }
