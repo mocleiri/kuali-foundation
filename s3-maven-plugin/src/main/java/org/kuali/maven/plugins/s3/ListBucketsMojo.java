b745076995cce8ed1ee20f5cc6a8d67a839c9f05
jcaddel <jcaddel@kuali.org>
2012-10-23 14:30:04 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11586 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ s3-maven-plugin/src/main/java/org/kuali/maven/plugins/s3/ListBucketsMojo.java
@@ -0,0 +1,22 @@
+package org.kuali.maven.plugins.s3;
+
+import java.util.List;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+
+/**
+ * @goal listbuckets
+ */
+public class ListBucketsMojo extends AbstractS3Mojo {
+
+	@Override
+	protected void execute(AmazonS3Client client) {
+		getLog().info("Listing buckets for Access Key: " + getAccessKey());
+		List<Bucket> buckets = client.listBuckets();
+		for (Bucket bucket : buckets) {
+			getLog().info(bucket.getName() + " " + bucket.getCreationDate() + " " + bucket.getOwner());
+		}
+	}
+
+}
