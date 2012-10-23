09230eebb979acf5887e6be6d362cded1d04a6aa
jcaddel <jcaddel@kuali.org>
2012-10-23 18:35:27 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11617 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- s3-maven-plugin/src/main/java/org/kuali/maven/plugins/s3/SummarizeBucketsMojo.java
+++ s3-maven-plugin/src/main/java/org/kuali/maven/plugins/s3/SummarizeBucketsMojo.java
@@ -1,5 +1,6 @@
 package org.kuali.maven.plugins.s3;
 
+import java.io.File;
 import java.util.List;
 
 import org.kuali.common.aws.s3.AccountSummary;
@@ -7,6 +8,8 @@ import org.kuali.common.aws.s3.AccountSummary;
 import com.amazonaws.services.s3.AmazonS3Client;
 
 /**
+ * Generate summary information for any S3 buckets "owned" by the provided <code>accessKey</code>
+ *
  * @goal summarizebuckets
  */
 public class SummarizeBucketsMojo extends AbstractS3Mojo {
@@ -25,6 +28,20 @@ public class SummarizeBucketsMojo extends AbstractS3Mojo {
 	 */
 	String exclude;
 
+	/**
+	 * If true, a CSV file is generated containing bucket summary statistics.
+	 *
+	 * @parameter expression="${s3.writeCsv}" default-value="true"
+	 */
+	boolean writeCsv;
+
+	/**
+	 * The file to where the CSV summary is written. If this file already exists, it is appended to.
+	 *
+	 * @parameter expression="${s3.csvFile}" default-value="${project.build.directory}/s3/summary.csv"
+	 */
+	File csvFile;
+
 	@Override
 	protected void execute(AmazonS3Client client) {
 		getLog().info(SEPARATOR);
@@ -34,6 +51,15 @@ public class SummarizeBucketsMojo extends AbstractS3Mojo {
 		List<String> excludes = s3Utils.toList(exclude);
 		AccountSummary summary = s3Utils.getAccountSummary(accessKey, secretKey, includes, excludes);
 		getLog().info("\n" + s3Utils.toString(summary));
+		if (writeCsv) {
+			if (csvFile.exists()) {
+				getLog().info("Appending to " + csvFile.getAbsolutePath());
+			} else {
+				getLog().info("Creating " + csvFile.getAbsolutePath());
+			}
+			boolean printColumnHeaders = !csvFile.exists();
+			s3Utils.write(csvFile, s3Utils.toCSV(summary, printColumnHeaders), true);
+		}
 	}
 
 }
