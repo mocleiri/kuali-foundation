ff310b99ff35487677298411a8cb85ec1a8a2a68
jcaddel <jcaddel@kuali.org>
2012-10-25 10:48:11 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11754 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- properties-maven-plugin/src/test/java/org/codehaus/mojo/properties/WriteProjectPropertiesTest.java
+++ properties-maven-plugin/src/test/java/org/codehaus/mojo/properties/WriteProjectPropertiesTest.java
@@ -34,7 +34,7 @@ public class WriteProjectPropertiesTest {
 			properties.setProperty("filename", "C:\\temp\\mvn.txt :   #   =");
 			properties.putAll(System.getenv());
 			properties.putAll(WriteProjectProperties.getEnvironmentVariables());
-			mojo.trim(properties, null, "filename");
+			mojo.trim(properties, "filename", null);
 			mojo.writeProperties(outputFile, properties, OutputStyle.ENVIRONMENT_VARIABLE, "nightlytag");
 			File props2 = new File("/Users/jeffcaddel/jvm.properties");
 			OutputStream out = FileUtils.openOutputStream(props2);
