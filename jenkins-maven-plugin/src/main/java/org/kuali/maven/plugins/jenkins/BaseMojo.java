d0c63af7afb2fd07c1bd13efe7fae5e73b8ad4ec
jcaddel <jcaddel@kuali.org>
2012-10-27 22:28:23 -0400

    Add template for freestyle software jobs and improve configuration options so any arbitrary number of named styles and templates can be supplied to the plugin
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11791 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/BaseMojo.java
+++ jenkins-maven-plugin/src/main/java/org/kuali/maven/plugins/jenkins/BaseMojo.java
@@ -27,220 +27,234 @@ import org.kuali.maven.plugins.jenkins.helper.JenkinsHelper;
  *
  */
 public abstract class BaseMojo extends AbstractMojo {
-    JenkinsHelper helper = new JenkinsHelper();
+	JenkinsHelper helper = new JenkinsHelper();
 
-    /**
-     * The Maven project object
-     *
-     * @parameter expression="${project}"
-     * @readonly
-     */
-    private MavenProject project;
+	/**
+	 * The Maven project object
+	 *
+	 * @parameter expression="${project}"
+	 * @readonly
+	 */
+	private MavenProject project;
 
-    /**
-     * The plugin dependencies.
-     *
-     * @parameter expression="${plugin.artifacts}"
-     * @required
-     * @readonly
-     */
-    private List<Artifact> pluginArtifacts;
+	/**
+	 * The plugin dependencies.
+	 *
+	 * @parameter expression="${plugin.artifacts}"
+	 * @required
+	 * @readonly
+	 */
+	private List<Artifact> pluginArtifacts;
 
-    /**
-     * The Jenkins instance to connect to.
-     *
-     * @parameter expression="${jenkins.url}" default-value="${project.ciManagement.url}"
-     * @required
-     */
-    private String url;
+	/**
+	 * The Jenkins instance to connect to.
+	 *
+	 * @parameter expression="${jenkins.url}" default-value="${project.ciManagement.url}"
+	 * @required
+	 */
+	private String url;
 
-    /**
-     * The format for timestamp displays
-     *
-     * @parameter expression="${jenkins.timestampFormat}" default-value="yyyy-MM-dd HH:mm:ss z"
-     * @required
-     */
-    private String timestampFormat;
+	/**
+	 * The format for timestamp displays
+	 *
+	 * @parameter expression="${jenkins.timestampFormat}" default-value="yyyy-MM-dd HH:mm:ss z"
+	 * @required
+	 */
+	private String timestampFormat;
 
-    /**
-     * The working directory for the plugin
-     *
-     * @parameter expression="${jenkins.workingDir}" default-value="${project.build.directory}/jenkins"
-     * @required
-     */
-    private File workingDir;
+	/**
+	 * The working directory for the plugin
+	 *
+	 * @parameter expression="${jenkins.workingDir}" default-value="${project.build.directory}/jenkins"
+	 * @required
+	 */
+	private File workingDir;
 
-    /**
-     * If set to true, the build will fail the first time Jenkins CLI encounters an issue. When false, mojo's that issue
-     * multiple CLI requests, will proceed through their list of requests and then fail at the end if an issue was
-     * encountered along the way.
-     *
-     * @parameter expression="${jenkins.stopOnError}" default-value="false"
-     * @required
-     */
-    private boolean stopOnError;
+	/**
+	 * If set to true, the build will fail the first time Jenkins CLI encounters an issue. When false, mojo's that issue multiple CLI
+	 * requests, will proceed through their list of requests and then fail at the end if an issue was encountered along the way.
+	 *
+	 * @parameter expression="${jenkins.stopOnError}" default-value="false"
+	 * @required
+	 */
+	private boolean stopOnError;
 
-    /**
-     * If set to true, the Maven build will fail if Jenkins CLI returns a non-zero exit value, otherwise the Maven build
-     * will continue
-     *
-     * @parameter expression="${jenkins.failOnError}" default-value="true"
-     * @required
-     */
-    private boolean failOnError;
+	/**
+	 * If set to true, the Maven build will fail if Jenkins CLI returns a non-zero exit value, otherwise the Maven build will continue
+	 *
+	 * @parameter expression="${jenkins.failOnError}" default-value="true"
+	 * @required
+	 */
+	private boolean failOnError;
 
-    /**
-     * The location of the jenkins job config template
-     *
-     * @parameter expression="${jenkins.template}" default-value="classpath:org/kuali/jenkins/jobs/template.xml"
-     * @required
-     */
-    private String template;
+	/**
+	 * A <code>List</code> of <code>Style</code> that map named styles to an xml template for each style
+	 *
+	 * @parameter expression="${jenkins.styles}"
+	 * @required
+	 */
+	private List<Style> styles;
 
-    /**
-     * Comma separated list of integers that represent a Jenkins CLI command completing successfully
-     *
-     * @parameter expression="${jenkins.successCodes}" default-value="0"
-     * @required
-     */
-    private String successCodes;
+	/**
+	 * The style of Jenkins job. Default styles are <code>maven</code> and <code>freestyle</code>
+	 *
+	 * @parameter expression="${jenkins.style}" default-value="freestyle"
+	 * @required
+	 */
+	private String style;
 
-    /**
-     * Comma separated list of known job types. When specifying a job name these can be used as shorthand for the fully
-     * qualified job name. eg "publish" gets expanded to "jenkins-maven-plugin-1.1-publish". To turn off expansion set
-     * 'jobTypes' to the empty string, null, or 'NONE'
-     *
-     * @parameter expression="${jenkins.jobTypes}" default-value="publish,unit,license,release"
-     * @required
-     */
-    private String jobTypes;
+	/**
+	 * Comma separated list of integers that represent a Jenkins CLI command completing successfully
+	 *
+	 * @parameter expression="${jenkins.successCodes}" default-value="0"
+	 * @required
+	 */
+	private String successCodes;
 
-    /**
-     * This is the type of SCM for the project eg "svn" or "git". If not provided, this value is filled in automatically
-     * based on information in the POM.
-     *
-     * @parameter expression="${jenkins.scmType}"
-     */
-    private String scmType;
+	/**
+	 * Comma separated list of known job types. When specifying a job name these can be used as shorthand for the fully qualified job name.
+	 * eg "publish" gets expanded to "jenkins-maven-plugin-1.1-publish". To turn off expansion set 'jobTypes' to the empty string, null, or
+	 * 'NONE'
+	 *
+	 * @parameter expression="${jenkins.jobTypes}" default-value="publish,unit,license,release"
+	 * @required
+	 */
+	private String jobTypes;
 
-    /**
-     * This is the SCM url for the project eg "http://svn.kuali.org/repos/foundation/trunk/jenkins-maven-plugin". If not
-     * provided, this value is filled in automatically based on information in the POM.
-     *
-     * @parameter expression="${jenkins.scmUrl}"
-     */
-    private String scmUrl;
+	/**
+	 * This is the type of SCM for the project eg "svn" or "git". If not provided, this value is filled in automatically based on
+	 * information in the POM.
+	 *
+	 * @parameter expression="${jenkins.scmType}"
+	 */
+	private String scmType;
 
-    /**
-     * This is the "major version" for the project. For a project with the version "1.1.0" the major version is "1.1".
-     * If not provided, this value is filled in automatically based on information from the POM.
-     *
-     * @parameter expression="${jenkins.majorVersion}"
-     */
-    private String majorVersion;
+	/**
+	 * This is the SCM url for the project eg "http://svn.kuali.org/repos/foundation/trunk/jenkins-maven-plugin". If not provided, this
+	 * value is filled in automatically based on information in the POM.
+	 *
+	 * @parameter expression="${jenkins.scmUrl}"
+	 */
+	private String scmUrl;
 
-    protected abstract void executeMojo();
+	/**
+	 * This is the "major version" for the project. For a project with the version "1.1.0" the major version is "1.1". If not provided, this
+	 * value is filled in automatically based on information from the POM.
+	 *
+	 * @parameter expression="${jenkins.majorVersion}"
+	 */
+	private String majorVersion;
 
-    @Override
-    public void execute() {
-        helper.updateMojo(this);
-        executeMojo();
-    }
+	protected abstract void executeMojo();
 
-    public String getTemplate() {
-        return template;
-    }
+	@Override
+	public void execute() {
+		helper.updateMojo(this);
+		executeMojo();
+	}
 
-    public void setTemplate(String template) {
-        this.template = template;
-    }
+	public MavenProject getProject() {
+		return project;
+	}
 
-    public MavenProject getProject() {
-        return project;
-    }
+	public List<Artifact> getPluginArtifacts() {
+		return pluginArtifacts;
+	}
 
-    public List<Artifact> getPluginArtifacts() {
-        return pluginArtifacts;
-    }
+	public String getUrl() {
+		return url;
+	}
 
-    public String getUrl() {
-        return url;
-    }
+	public void setUrl(String server) {
+		this.url = server;
+	}
 
-    public void setUrl(String server) {
-        this.url = server;
-    }
+	public String getTimestampFormat() {
+		return timestampFormat;
+	}
 
-    public String getTimestampFormat() {
-        return timestampFormat;
-    }
+	public void setTimestampFormat(String timestampFormat) {
+		this.timestampFormat = timestampFormat;
+	}
 
-    public void setTimestampFormat(String timestampFormat) {
-        this.timestampFormat = timestampFormat;
-    }
+	public File getWorkingDir() {
+		return workingDir;
+	}
 
-    public File getWorkingDir() {
-        return workingDir;
-    }
+	public void setWorkingDir(File workingDir) {
+		this.workingDir = workingDir;
+	}
 
-    public void setWorkingDir(File workingDir) {
-        this.workingDir = workingDir;
-    }
+	public boolean isStopOnError() {
+		return stopOnError;
+	}
 
-    public boolean isStopOnError() {
-        return stopOnError;
-    }
+	public void setStopOnError(boolean stopOnError) {
+		this.stopOnError = stopOnError;
+	}
 
-    public void setStopOnError(boolean stopOnError) {
-        this.stopOnError = stopOnError;
-    }
+	public boolean isFailOnError() {
+		return failOnError;
+	}
 
-    public boolean isFailOnError() {
-        return failOnError;
-    }
+	public void setFailOnError(boolean failOnError) {
+		this.failOnError = failOnError;
+	}
 
-    public void setFailOnError(boolean failOnError) {
-        this.failOnError = failOnError;
-    }
+	public String getSuccessCodes() {
+		return successCodes;
+	}
 
-    public String getSuccessCodes() {
-        return successCodes;
-    }
+	public void setSuccessCodes(String successCodes) {
+		this.successCodes = successCodes;
+	}
 
-    public void setSuccessCodes(String successCodes) {
-        this.successCodes = successCodes;
-    }
+	public String getJobTypes() {
+		return jobTypes;
+	}
 
-    public String getJobTypes() {
-        return jobTypes;
-    }
+	public void setJobTypes(String jobTypes) {
+		this.jobTypes = jobTypes;
+	}
 
-    public void setJobTypes(String jobTypes) {
-        this.jobTypes = jobTypes;
-    }
+	public String getScmType() {
+		return scmType;
+	}
 
-    public String getScmType() {
-        return scmType;
-    }
+	public void setScmType(String scmType) {
+		this.scmType = scmType;
+	}
 
-    public void setScmType(String scmType) {
-        this.scmType = scmType;
-    }
+	public String getScmUrl() {
+		return scmUrl;
+	}
 
-    public String getScmUrl() {
-        return scmUrl;
-    }
+	public void setScmUrl(String scmUrl) {
+		this.scmUrl = scmUrl;
+	}
 
-    public void setScmUrl(String scmUrl) {
-        this.scmUrl = scmUrl;
-    }
+	public String getMajorVersion() {
+		return majorVersion;
+	}
 
-    public String getMajorVersion() {
-        return majorVersion;
-    }
+	public void setMajorVersion(String majorVersion) {
+		this.majorVersion = majorVersion;
+	}
 
-    public void setMajorVersion(String majorVersion) {
-        this.majorVersion = majorVersion;
-    }
+	public List<Style> getStyles() {
+		return styles;
+	}
+
+	public void setStyles(List<Style> styles) {
+		this.styles = styles;
+	}
+
+	public String getStyle() {
+		return style;
+	}
+
+	public void setStyle(String style) {
+		this.style = style;
+	}
 
 }
