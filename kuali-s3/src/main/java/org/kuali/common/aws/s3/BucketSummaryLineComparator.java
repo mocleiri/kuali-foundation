8a3cbfab835b33859439704ec613949da478f692
jcaddel <jcaddel@kuali.org>
2012-10-29 10:14:04 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11854 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/BucketSummaryLineComparator.java
@@ -0,0 +1,15 @@
+package org.kuali.common.aws.s3;
+
+import java.util.Comparator;
+
+public class BucketSummaryLineComparator implements Comparator<BucketSummaryLine> {
+
+	@Override
+	public int compare(BucketSummaryLine one, BucketSummaryLine two) {
+		if (one.getBucket().equals(two.getBucket())) {
+			return one.getDate().compareTo(two.getDate());
+		} else {
+			return one.getBucket().compareTo(two.getBucket());
+		}
+	}
+}
