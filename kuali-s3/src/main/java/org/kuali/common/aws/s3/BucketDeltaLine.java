b600784d9847b572d709f01170ec3f65e1a15ea3
jcaddel <jcaddel@kuali.org>
2012-10-29 10:40:16 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11859 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/BucketDeltaLine.java
@@ -0,0 +1,52 @@
+package org.kuali.common.aws.s3;
+
+import java.util.Date;
+
+public class BucketDeltaLine {
+
+	String bucket;
+	long fileDelta;
+	long byteDelta;
+	Date startDate;
+	Date endDate;
+
+	public String getBucket() {
+		return bucket;
+	}
+
+	public void setBucket(String bucket) {
+		this.bucket = bucket;
+	}
+
+	public long getFileDelta() {
+		return fileDelta;
+	}
+
+	public void setFileDelta(long fileDelta) {
+		this.fileDelta = fileDelta;
+	}
+
+	public long getByteDelta() {
+		return byteDelta;
+	}
+
+	public void setByteDelta(long byteDelta) {
+		this.byteDelta = byteDelta;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+}
