5df747f18f185579307af2d5c7db7d34ff4a8355
mztaylor <mztaylor@kuali.org>
2012-11-05 16:21:37 -0500

    KULRICE-8106: Updating SpringContext to mirror calls from RiceTestCase
    git-svn-id: https://svn.kuali.org/repos/foundation/sandbox/ingester-maven-plugin-mz@11977 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/maven/plugins/ingester/SpringContext.java
+++ src/main/java/org/kuali/maven/plugins/ingester/SpringContext.java
@@ -15,41 +15,40 @@
  */
 package org.kuali.maven.plugins.ingester;
 
-import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
+import javax.xml.namespace.QName;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
-import org.directwebremoting.spring.SpringCreator;
 //import org.kuali.rice.core.api.config.ConfigContext;
-import org.kuali.rice.core.api.*;
 import org.kuali.rice.core.api.config.property.ConfigContext;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 //import org.kuali.rice.core.api.resourceloader.RiceResourceLoaderFactory;
-import org.kuali.rice.core.api.util.RiceConstants;
-import org.kuali.rice.core.framework.resourceloader.RiceResourceLoaderFactory;
 import org.kuali.rice.core.framework.resourceloader.SpringResourceLoader;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.aop.support.AopUtils;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.kuali.rice.core.impl.config.property.JAXBConfigImpl;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.kuali.rice.core.api.lifecycle.BaseLifecycle;
+import org.kuali.rice.core.api.lifecycle.Lifecycle;
+import org.kuali.rice.core.api.config.property.Config;
 
 //import uk.ltd.getahead.dwr.create.SpringCreator;
 
 public class SpringContext {
     protected static final Logger LOG = Logger.getLogger(SpringContext.class);
-    protected static final String APPLICATION_CONTEXT_DEFINITION = "spring-rice-startup.xml";
+    protected static final String BOOTSTRAP_CONTEXT_DEFINITION = "classpath:spring-beans.xml";
+    protected static final String APPLICATION_CONTEXT_DEFINITION = "classpath:spring-rice-configurer.xml";
+    protected static final String APPLICATION_CONFIG_LOCATION = "classpath:spring-rice-config.xml";
+    
+    protected static SpringResourceLoader springResourceLoader;
+    
     protected static ConfigurableApplicationContext applicationContext;
-    protected static Set<Class<? extends Object>> SINGLETON_TYPES = new HashSet<Class<? extends Object>>();
-    protected static Map<Class<? extends Object>, Object> SINGLETON_BEANS_BY_TYPE_CACHE = new HashMap<Class<? extends Object>, Object>();
-    protected static Map<String, Object> SINGLETON_BEANS_BY_NAME_CACHE = new HashMap<String, Object>();
-    @SuppressWarnings("rawtypes")
-    protected static Map<Class<? extends Object>, Map> SINGLETON_BEANS_OF_TYPE_CACHE = new HashMap<Class<? extends Object>, Map>();
 
     /**
      * Use this method to retrieve a service which may or may not be implemented locally. (That is, defined in the main
@@ -58,165 +57,12 @@ public class SpringContext {
     public static Object getService(String serviceName) {
         return GlobalResourceLoader.getService(serviceName);
     }
-
-    /**
-     * Use this method to retrieve a spring bean when one of the following is the case. Pass in the type of the service
-     * interface, NOT the service implementation.<br>
-     * 1. there is only one bean of the specified type in our spring context<br>
-     * 2. there is only one bean of the specified type in our spring context, but you want the one whose bean id is the
-     * same as type.getSimpleName() with the exception of the first letter being lower case in the former and upper case
-     * in the latter. <br>
-     * For example, there are two beans of type DateTimeService in our context dateTimeService and testDateTimeService.
-     * To retrieve the former, you should specific DateTimeService.class as the type. To retrieve the latter, you should
-     * specify ConfigurableDateService.class as the type. Unless you are writing a unit test and need to down cast to an
-     * implementation, you do not need to cast the result of this method.
-     *
-     * @param <T>
-     * @param type
-     * @return an object that has been defined as a bean in our spring context and is of the specified type
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T getBean(Class<T> type) {
-        verifyProperInitialization();
-        T bean = null;
-        if (SINGLETON_BEANS_BY_TYPE_CACHE.containsKey(type)) {
-            bean = (T) SINGLETON_BEANS_BY_TYPE_CACHE.get(type);
-        } else {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Bean not already in cache: " + type + " - calling getBeansOfType() ");
-            }
-            Collection<T> beansOfType = getBeansOfType(type).values();
-            if (!beansOfType.isEmpty()) {
-                if (beansOfType.size() > 1) {
-                    bean = getBean(type, StringUtils.uncapitalize(type.getSimpleName()));
-                } else {
-                    bean = beansOfType.iterator().next();
-                }
-            } else {
-                try {
-                    bean = getBean(type, StringUtils.uncapitalize(type.getSimpleName()));
-                } catch (Exception ex) {
-                    // do nothing, let fall through
-                }
-                if (bean == null) {
-                    // unable to find bean - check GRL
-                    // this is needed in case no beans of the given type exist locally
-                    if (LOG.isDebugEnabled()) {
-                        LOG.debug("Bean not found in local context: " + type.getName() + " - calling GRL");
-                    }
-                    Object remoteServiceBean = getService(StringUtils.uncapitalize(type.getSimpleName()));
-                    if (remoteServiceBean != null) {
-                        if (type.isAssignableFrom(remoteServiceBean.getClass())) {
-                            bean = (T) remoteServiceBean;
-                        }
-                    }
-                }
-            }
-            if (bean != null) {
-                synchronized (SINGLETON_TYPES) {
-                    if (SINGLETON_TYPES.contains(type) || hasSingletonSuperType(type, SINGLETON_TYPES)) {
-                        SINGLETON_TYPES.add(type);
-                        SINGLETON_BEANS_BY_TYPE_CACHE.put(type, bean);
-                    }
-                }
-            } else {
-                throw new RuntimeException(
-                        "Request for non-existent bean.  Unable to find in local context or on the GRL: "
-                                + type.getName());
-            }
+  
+    public static SpringResourceLoader getPluginSpringResourceLoader() {
+        if (springResourceLoader == null) {
+            springResourceLoader = new SpringResourceLoader(new QName("PluginContext"), Collections.singletonList( BOOTSTRAP_CONTEXT_DEFINITION ), null);
         }
-        return bean;
-    }
-
-    /**
-     * Use this method to retrieve all beans of a give type in our spring context. Pass in the type of the service
-     * interface, NOT the service implementation.
-     *
-     * @param <T>
-     * @param type
-     * @return a map of the spring bean ids / beans that are of the specified type
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> Map<String, T> getBeansOfType(Class<T> type) {
-        verifyProperInitialization();
-        Map<String, T> beansOfType = null;
-        if (SINGLETON_BEANS_OF_TYPE_CACHE.containsKey(type)) {
-            beansOfType = SINGLETON_BEANS_OF_TYPE_CACHE.get(type);
-        } else {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Bean not already in \"OF_TYPE\" cache: " + type
-                        + " - calling getBeansOfType() on Spring context");
-            }
-            boolean allOfTypeAreSingleton = true;
-            beansOfType = applicationContext.getBeansOfType(type);
-            for (String key : beansOfType.keySet()) {
-                if (!applicationContext.isSingleton(key)) {
-                    allOfTypeAreSingleton = false;
-                }
-            }
-            if (allOfTypeAreSingleton) {
-                synchronized (SINGLETON_TYPES) {
-                    SINGLETON_TYPES.add(type);
-                    SINGLETON_BEANS_OF_TYPE_CACHE.put(type, beansOfType);
-                }
-            }
-        }
-        return beansOfType;
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T> T getBean(Class<T> type, String name) {
-        T bean = null;
-        if (SINGLETON_BEANS_BY_NAME_CACHE.containsKey(name)) {
-            bean = (T) SINGLETON_BEANS_BY_NAME_CACHE.get(name);
-        } else {
-            try {
-                bean = (T) applicationContext.getBean(name);
-                if (applicationContext.isSingleton(name)) {
-                    synchronized (SINGLETON_BEANS_BY_NAME_CACHE) {
-                        SINGLETON_BEANS_BY_NAME_CACHE.put(name, bean);
-                    }
-                }
-            } catch (NoSuchBeanDefinitionException nsbde) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("Bean with name and type not found in local context: " + name + "/" + type.getName()
-                            + " - calling GRL");
-                }
-                Object remoteServiceBean = getService(name);
-                if (remoteServiceBean != null) {
-                    if (type.isAssignableFrom(AopUtils.getTargetClass(remoteServiceBean))) {
-                        bean = (T) remoteServiceBean;
-                        // assume remote beans are services and thus singletons
-                        synchronized (SINGLETON_BEANS_BY_NAME_CACHE) {
-                            SINGLETON_BEANS_BY_NAME_CACHE.put(name, bean);
-                        }
-                    }
-                }
-                throw new RuntimeException(
-                        "No bean of this type and name exist in the application context or from the GRL: "
-                                + type.getName() + ", " + name);
-            }
-        }
-        return bean;
-    }
-
-    private static boolean hasSingletonSuperType(Class<? extends Object> type,
-            Set<Class<? extends Object>> knownSingletonTypes) {
-        for (Class<? extends Object> singletonType : knownSingletonTypes) {
-            if (singletonType.isAssignableFrom(type)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    protected static Object getBean(String beanName) {
-        return getBean(Object.class, beanName);
-    }
-
-    protected static String[] getBeanNames() {
-        verifyProperInitialization();
-        return applicationContext.getBeanDefinitionNames();
+        return springResourceLoader;
     }
 
     protected static void initializeApplicationContext() {
@@ -233,21 +79,7 @@ public class SpringContext {
 
     protected static void close() throws Exception {
         if (applicationContext == null) {
-            //applicationContext = RiceResourceLoaderFactory.getSpringResourceLoaders().iterator()APPLICATION_CONTEXT_DEFINITION.
-        }
-        DisposableBean riceConfigurer = null;
-        try {
-            riceConfigurer = (DisposableBean) applicationContext.getBean("rice");
-        } catch (Exception ex) {
-            LOG.debug("Unable to get 'rice' bean - attempting to get from the Rice ConfigContext", ex);
-          //  riceConfigurer = (DisposableBean) ConfigContext
-          //          .getObjectFromConfigHierarchy(RiceConstants.RICE_CONFIGURER_CONFIG_NAME);
-        }
-        applicationContext = null;
-        if (riceConfigurer != null) {
-            riceConfigurer.destroy();
-        } else {
-            LOG.error("Unable to close SpringContext - unable to get a handle to a RiceConfigurer object.");
+        	applicationContext = getPluginSpringResourceLoader().getContext();
         }
         ConfigContext.destroy();
     }
@@ -276,18 +108,99 @@ public class SpringContext {
         }
     }
 
-    private static void initializeApplicationContext(String riceInitializationSpringFile, boolean initializeSchedule) {
-        LOG.info("Starting Spring context initialization");
+    private static void initializeApplicationContext(String riceInitializationSpringFile, boolean initializeSchedule)  {
+    	List<Lifecycle> suiteLifeCycles = null;
+    	getBaseDir("kew");
+    	LOG.info("Starting Spring context initialization");
         // use the base config file to bootstrap the real application context started by Rice
-        new ClassPathXmlApplicationContext(riceInitializationSpringFile);
-        // pull the Rice application context into here for further use and efficiency
-        Iterator<SpringResourceLoader> i = RiceResourceLoaderFactory.getSpringResourceLoaders().iterator();
-        while(i.hasNext()) {
-        	SpringResourceLoader loader = i.next();
-        	applicationContext = loader.getContext();
+        applicationContext = getPluginSpringResourceLoader().getContext();
+        suiteLifeCycles = getPluginLifecycles();
+    	 try {
+    		 startLifecycles(suiteLifeCycles);
+    	 } catch (Throwable e) {
+     		e.printStackTrace();
+            try {
+				stopLifecycles(suiteLifeCycles);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+            throw new RuntimeException(e);
         }
-        LOG.info("Completed Spring context initialization");
-
-        SpringCreator.setOverrideBeanFactory(applicationContext.getBeanFactory());
     }
+
+
+	private static void getBaseDir(String moduleName) {
+		if (System.getProperty("basedir") == null) {
+            System.setProperty("basedir", FileUtils.getUserDirectory().getAbsolutePath());
+        }
+	}
+    
+    protected static void startLifecycles(List<Lifecycle> lifecycles) throws Exception {
+        for (Lifecycle lifecycle : lifecycles) {
+                lifecycle.start();
+        }
+    }
+    
+    protected static void stopLifecycles(List<Lifecycle> lifecycles) throws Exception {
+        int lifecyclesSize = lifecycles.size() - 1;
+        for (int i = lifecyclesSize; i >= 0; i--) {
+            try {
+            	if (lifecycles.get(i) == null) {
+            		LOG.warn("Attempted to stop a null lifecycle");
+            	} else {
+            		if (lifecycles.get(i).isStarted()) {
+                        LOG.warn("Attempting to stop a lifecycle " + lifecycles.get(i).getClass());
+            			lifecycles.get(i).stop();
+            		}
+            	}
+            } catch (Exception e) {
+                LOG.error("Failed to shutdown one of the lifecycles!", e);
+            }
+        }
+    }
+       
+    protected static List<Lifecycle>  getPluginLifecycles() {
+        List<Lifecycle> lifecycles = new LinkedList<Lifecycle>();
+        
+        /**
+         * Initializes Rice configuration from base plugin configuration
+         */
+        lifecycles.add(new BaseLifecycle() {
+            @Override
+			public void start() throws Exception {
+                Config config = getPluginBaseConfiguration();
+                ConfigContext.init(config);
+                super.start();
+            }
+        });
+        
+        /**
+         * Loads the Rice Configuration for datasources, etc...
+         */
+        lifecycles.add(getPluginSpringResourceLoader());
+        
+        /**
+         * Establishes the specific configurations to this lifecycle (ksb, kew, etc..)
+         */
+        Lifecycle loadApplicationLifecycle = getLoadPluginGoalLifecycle();
+        if (loadApplicationLifecycle != null) {
+        	lifecycles.add(loadApplicationLifecycle);
+        }
+        return lifecycles;
+    }
+ 
+    // This should be made into a generic call/utility
+    protected static Lifecycle getLoadPluginGoalLifecycle() {
+        SpringResourceLoader springResourceLoader = new SpringResourceLoader(new QName("PluginResourceLoader"), APPLICATION_CONTEXT_DEFINITION, null);
+    	springResourceLoader.setParentSpringResourceLoader(getPluginSpringResourceLoader());
+    	return springResourceLoader;
+    }
+    
+    protected static Config getPluginBaseConfiguration() throws Exception {
+        Config config = new JAXBConfigImpl(APPLICATION_CONFIG_LOCATION, System.getProperties());
+        config.parseConfig();
+        return config;
+    }
+    
 }
