ed795f39348e375b47e4a2f7785fc0fdce5e1d86
jcaddel <jcaddel@kuali.org>
2012-11-06 08:32:16 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11989 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/HelloWorldTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/HelloWorldTest.java
@@ -1,14 +1,20 @@
 package org.kuali.common.jdbc;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class HelloWorldTest {
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldTest.class);
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		logger.info("Hello World");
 	}
 
 }
