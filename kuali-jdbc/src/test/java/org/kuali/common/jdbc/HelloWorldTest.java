b44cdc6691390f3cc221b41c72df397fd228b1d2
jcaddel <jcaddel@kuali.org>
2012-11-06 08:17:57 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11985 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package org.kuali.common.jdbc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
