602c993c85dea4be9c755153e45692a00f0efe64
jcaddel <jcaddel@kuali.org>
2012-11-06 08:19:57 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11986 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.kuali.common.jdbc;
+
+public class HelloWorld {
+	String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
