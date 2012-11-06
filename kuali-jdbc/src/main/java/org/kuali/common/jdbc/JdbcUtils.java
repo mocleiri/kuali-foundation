b931b5a4f9574b2d45431f998f1cd834c9309610
jcaddel <jcaddel@kuali.org>
2012-11-06 09:18:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@11997 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/JdbcUtils.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/JdbcUtils.java
@@ -1,6 +1,12 @@
 package org.kuali.common.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 public class JdbcUtils {
 
@@ -12,6 +18,41 @@ public class JdbcUtils {
 
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
+	}
+
+	public boolean execute(String sql) {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try {
+			conn = DataSourceUtils.doGetConnection(dataSource);
+			ps = conn.prepareStatement(sql);
+			return ps.execute();
+		} catch (SQLException e) {
+			throw new JdbcException(e);
+		} finally {
+			closeQuietly(conn);
+		}
+	}
+
+	protected void closeQuietly(Connection conn, PreparedStatement ps) {
+		closeQuietly(ps);
+		closeQuietly(conn);
+	}
+
+	protected void closeQuietly(PreparedStatement ps) {
+		try {
+			ps.close();
+		} catch (SQLException e) {
+			throw new JdbcException(e);
+		}
+	}
+
+	public void closeQuietly(Connection conn) {
+		try {
+			DataSourceUtils.doReleaseConnection(conn, dataSource);
+		} catch (SQLException e) {
+			throw new JdbcException(e);
+		}
 	}
 
 }
