d5e3548810071904b64ababf70c81a63c94c33d4
jcaddel <jcaddel@kuali.org>
2012-11-06 17:02:17 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12040 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/JdbcUtilsTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/JdbcUtilsTest.java
@@ -26,8 +26,12 @@ public class JdbcUtilsTest {
 			Assert.assertNotNull("dbaJdbcUtils is null.", dbaJdbcUtils);
 			Assert.assertNotNull("dataSource is null.", dbaJdbcUtils.getDataSource());
 			Properties props = ResourceUtils.getProperties("classpath:mysql.xml");
-			String sql = props.getProperty("sql.mysql.validate");
-			dbaJdbcUtils.execute(sql);
+			String validate = props.getProperty("sql.mysql.validate");
+			String drop = props.getProperty("sql.mysql.drop");
+			String create = props.getProperty("sql.mysql.create");
+			dbaJdbcUtils.execute(validate);
+			dbaJdbcUtils.execute(drop);
+			dbaJdbcUtils.execute(create);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
