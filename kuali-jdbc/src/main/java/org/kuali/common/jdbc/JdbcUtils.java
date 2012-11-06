3fcaedfbf8063e4fe4e3e85bc3b5c7a84a601332
jcaddel <jcaddel@kuali.org>
2012-11-06 10:42:11 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12001 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/JdbcUtils.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/JdbcUtils.java
@@ -1,6 +1,5 @@
 package org.kuali.common.jdbc;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,17 +8,16 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 
 public class JdbcUtils {
-
-	private static final String DEFAULT_DELIMITER = "/";
-	private static final String DEFAULT_LINE_SEPARATOR = System.getProperty("line.separator");
+	final Logger logger = LoggerFactory.getLogger(JdbcUtils.class);
 
 	String encoding;
 	DataSource dataSource;
-	String delimiter = DEFAULT_DELIMITER;
-	String lineSeparator = DEFAULT_LINE_SEPARATOR;
+	SqlGenerator generator;
 
 	public DataSource getDataSource() {
 		return dataSource;
@@ -29,25 +27,9 @@ public class JdbcUtils {
 		this.dataSource = dataSource;
 	}
 
-	protected List<String> getSql(String location, String encoding, String delimiter, String lineSeparator) {
-		try {
-			List<String> lines = ResourceUtils.getLines(location, encoding);
-			List<String> sql = new ArrayList<String>();
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < lines.size(); i++) {
-				String line = lines.get(i);
-				String trimmed = line.trim();
-				if (delimiter.equals(trimmed)) {
-					sql.add(sb.toString());
-					sb = new StringBuilder();
-				} else {
-					sb.append(line + lineSeparator);
-				}
-			}
-			return sql;
-		} catch (IOException e) {
-			throw new JdbcException(e);
-		}
+	public List<Boolean> readAndExecute(String location) {
+		List<String> sql = generator.generateSQL(location);
+		return execute(sql);
 	}
 
 	public boolean execute(String sql) {
@@ -103,14 +85,6 @@ public class JdbcUtils {
 		} catch (SQLException e) {
 			throw new JdbcException(e);
 		}
-	}
-
-	public String getDelimiter() {
-		return delimiter;
-	}
-
-	public void setDelimiter(String delimiter) {
-		this.delimiter = delimiter;
 	}
 
 	public String getEncoding() {
---
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/SqlGenerator.java
@@ -0,0 +1,57 @@
+package org.kuali.common.jdbc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SqlGenerator {
+	final Logger logger = LoggerFactory.getLogger(SqlGenerator.class);
+
+	private static final String DEFAULT_DELIMITER = "/";
+	private static final String DEFAULT_LINE_SEPARATOR = System.getProperty("line.separator");
+
+	String encoding;
+	String delimiter = DEFAULT_DELIMITER;
+	String lineSeparator = DEFAULT_LINE_SEPARATOR;
+
+	public List<String> generateSQL(String location) {
+		try {
+			List<String> lines = ResourceUtils.getLines(location, encoding);
+			List<String> sql = new ArrayList<String>();
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < lines.size(); i++) {
+				String line = lines.get(i);
+				String trimmed = line.trim();
+				if (delimiter.equals(trimmed)) {
+					sql.add(sb.toString());
+					sb = new StringBuilder();
+				} else {
+					sb.append(line + lineSeparator);
+				}
+			}
+			return sql;
+		} catch (IOException e) {
+			throw new JdbcException(e);
+		}
+	}
+
+	public String getDelimiter() {
+		return delimiter;
+	}
+
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+}
