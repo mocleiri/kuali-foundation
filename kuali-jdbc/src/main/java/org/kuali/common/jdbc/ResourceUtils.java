9b4b1122afc6abf6a64b3493f4e3b8c22ff9047e
jcaddel <jcaddel@kuali.org>
2012-11-07 09:03:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12081 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/ResourceUtils.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/ResourceUtils.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
@@ -19,20 +20,29 @@ public class ResourceUtils {
 
 	private static final String XML_EXTENSION = ".xml";
 
-	public static final Properties getProperties(String location) throws IOException {
+	public static final Properties getProperties(String location) {
+		return getProperties(location, null);
+	}
+
+	public static final Properties getProperties(String location, String encoding) {
 		InputStream in = null;
+		Reader reader = null;
 		try {
-			in = getInputStream(location);
 			Properties properties = new Properties();
 			boolean xml = location.toLowerCase().endsWith(XML_EXTENSION);
 			if (xml) {
+				in = getInputStream(location);
 				properties.loadFromXML(in);
 			} else {
-				properties.load(in);
+				reader = getBufferedReader(location, encoding);
+				properties.load(reader);
 			}
 			return properties;
+		} catch (IOException e) {
+			throw new IllegalStateException("Unexpected IO error", e);
 		} finally {
 			IOUtils.closeQuietly(in);
+			IOUtils.closeQuietly(reader);
 		}
 
 	}
