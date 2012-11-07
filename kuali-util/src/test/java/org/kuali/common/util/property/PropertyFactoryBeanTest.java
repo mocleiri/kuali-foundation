e10a525d08dd070d39530baf8f86c5b35c600ef3
jcaddel <jcaddel@kuali.org>
2012-11-07 17:09:31 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12145 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/property/PropertyFactoryBeanTest.java
@@ -0,0 +1,33 @@
+package org.kuali.common.util.property;
+
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class PropertyFactoryBeanTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(PropertyFactoryBeanTest.class);
+
+	@Autowired
+	private Properties properties = null;
+
+	@Test
+	public void test() {
+		try {
+			logger.info("Hello World");
+			Assert.assertNotNull("properties is null.", properties);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
