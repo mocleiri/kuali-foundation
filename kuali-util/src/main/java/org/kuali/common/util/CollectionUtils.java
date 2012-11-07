c8b27f597d5c050d994b7479bdb01830cd21e088
jcaddel <jcaddel@kuali.org>
2012-11-07 18:08:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12158 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/CollectionUtils.java
@@ -0,0 +1,27 @@
+package org.kuali.common.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+public class CollectionUtils {
+
+	public static final boolean isEmpty(Collection<?> c) {
+		return c == null || c.size() == 0;
+	}
+
+	public static final List<String> getList(List<String> list, String csv) {
+		List<String> newList = new ArrayList<String>();
+		if (!isEmpty(list)) {
+			newList.addAll(list);
+		}
+		if (!StringUtils.isBlank(csv)) {
+			newList.addAll(Arrays.asList(Str.splitAndTrimCSV(csv)));
+		}
+		return newList;
+	}
+
+}
--- kuali-util/src/main/java/org/kuali/common/util/property/PropertyFactoryBean.java
+++ kuali-util/src/main/java/org/kuali/common/util/property/PropertyFactoryBean.java
@@ -3,17 +3,20 @@ package org.kuali.common.util.property;
 import java.util.List;
 import java.util.Properties;
 
+import org.kuali.common.util.CollectionUtils;
 import org.kuali.common.util.PropertyUtils;
 import org.springframework.beans.factory.FactoryBean;
 
 public class PropertyFactoryBean implements FactoryBean<Properties> {
 
+	String locs;
 	List<String> locations;
 	String encoding;
 
 	@Override
 	public Properties getObject() throws Exception {
-		return PropertyUtils.getProperties(locations, encoding);
+		List<String> list = CollectionUtils.getList(locations, locs);
+		return PropertyUtils.getProperties(list, encoding);
 	}
 
 	@Override
@@ -40,6 +43,14 @@ public class PropertyFactoryBean implements FactoryBean<Properties> {
 
 	public void setEncoding(String encoding) {
 		this.encoding = encoding;
+	}
+
+	public String getLocs() {
+		return locs;
+	}
+
+	public void setLocs(String locs) {
+		this.locs = locs;
 	}
 
 }
