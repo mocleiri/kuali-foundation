8e6233fe2eca05c0830b155ea761ea5fe67457e1
jcaddel <jcaddel@kuali.org>
2012-11-08 07:11:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12222 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/PropertyFactoryBean.java
+++ kuali-util/src/main/java/org/kuali/common/util/PropertyFactoryBean.java
@@ -1,26 +1,11 @@
 package org.kuali.common.util;
 
-import java.util.List;
 import java.util.Properties;
 
-import org.kuali.common.util.property.PropertyLoadingContext;
-import org.kuali.common.util.property.PropertyStyle;
+import org.kuali.common.util.property.DefaultPropertyLoadingContext;
 import org.springframework.beans.factory.FactoryBean;
 
-public class PropertyFactoryBean implements FactoryBean<Properties>, PropertyLoadingContext {
-
-	List<String> locations;
-	String encoding;
-	String include;
-	String exclude;
-	boolean includeEnvironmentVariables;
-	boolean includeSystemProperties;
-	boolean resolvePlaceholders;
-	boolean ignoreMissingLocations;
-	String placeHolderPrefix = PropertyUtils.DEFAULT_PLACEHOLDER_PREFIX;
-	String placeHolderSuffix = PropertyUtils.DEFAULT_PLACEHOLDER_SUFFIX;
-	String prefix;
-	PropertyStyle style = PropertyStyle.NORMAL;
+public class PropertyFactoryBean extends DefaultPropertyLoadingContext implements FactoryBean<Properties> {
 
 	@Override
 	public Properties getObject() throws Exception {
@@ -36,113 +21,4 @@ public class PropertyFactoryBean implements FactoryBean<Properties>, PropertyLoa
 	public boolean isSingleton() {
 		return true;
 	}
-
-	@Override
-	public List<String> getLocations() {
-		return locations;
-	}
-
-	public void setLocations(List<String> locations) {
-		this.locations = locations;
-	}
-
-	@Override
-	public String getEncoding() {
-		return encoding;
-	}
-
-	public void setEncoding(String encoding) {
-		this.encoding = encoding;
-	}
-
-	@Override
-	public String getInclude() {
-		return include;
-	}
-
-	public void setInclude(String include) {
-		this.include = include;
-	}
-
-	@Override
-	public String getExclude() {
-		return exclude;
-	}
-
-	public void setExclude(String exclude) {
-		this.exclude = exclude;
-	}
-
-	@Override
-	public boolean isIncludeEnvironmentVariables() {
-		return includeEnvironmentVariables;
-	}
-
-	public void setIncludeEnvironmentVariables(boolean includeEnvironmentVariables) {
-		this.includeEnvironmentVariables = includeEnvironmentVariables;
-	}
-
-	@Override
-	public boolean isIncludeSystemProperties() {
-		return includeSystemProperties;
-	}
-
-	public void setIncludeSystemProperties(boolean includeSystemProperties) {
-		this.includeSystemProperties = includeSystemProperties;
-	}
-
-	@Override
-	public boolean isResolvePlaceholders() {
-		return resolvePlaceholders;
-	}
-
-	public void setResolvePlaceholders(boolean resolvePlaceholders) {
-		this.resolvePlaceholders = resolvePlaceholders;
-	}
-
-	@Override
-	public boolean isIgnoreMissingLocations() {
-		return ignoreMissingLocations;
-	}
-
-	public void setIgnoreMissingLocations(boolean ignoreMissingLocations) {
-		this.ignoreMissingLocations = ignoreMissingLocations;
-	}
-
-	@Override
-	public String getPlaceHolderPrefix() {
-		return placeHolderPrefix;
-	}
-
-	public void setPlaceHolderPrefix(String placeHolderPrefix) {
-		this.placeHolderPrefix = placeHolderPrefix;
-	}
-
-	@Override
-	public String getPlaceHolderSuffix() {
-		return placeHolderSuffix;
-	}
-
-	public void setPlaceHolderSuffix(String placeHolderSuffix) {
-		this.placeHolderSuffix = placeHolderSuffix;
-	}
-
-	@Override
-	public String getPrefix() {
-		return prefix;
-	}
-
-	public void setPrefix(String prefix) {
-		this.prefix = prefix;
-	}
-
-	@Override
-	public PropertyStyle getStyle() {
-		return style;
-	}
-
-	public void setStyle(PropertyStyle style) {
-		this.style = style;
-	}
-
 }
