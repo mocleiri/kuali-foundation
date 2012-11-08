83829d8bf6fa32eff68ab3bdd1ed4835c4ecc8b3
jcaddel <jcaddel@kuali.org>
2012-11-08 10:12:13 -0500

    Add mojo for creating a plain text file holding a list of the table names that contained data at the time of the export
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/maven-impex-plugin-1.0@12248 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/apache/torque/mojo/GenerateTableListingMojo.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright 2004-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.torque.mojo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @goal generatetablelisting
+ */
+public class GenerateTableListingMojo extends BaseMojo {
+
+	/**
+	 * @parameter expression="${impex.extension}" default-value="sql"
+	 * @required
+	 */
+	private String extension;
+
+	/**
+	 * @parameter expression="${impex.targetDatabase}"
+	 * @required
+	 */
+	private String targetDatabase;
+
+	/**
+	 * @parameter expression="${impex.baseSqlDir}" default-value="${project.build.directory}/impex/sql"
+	 * @required
+	 */
+	private File baseSqlDir;
+
+	/**
+	 * @parameter expression="${impex.artifactId}" default-value="${project.artifactId}"
+	 * @required
+	 */
+	private String artifactId;
+
+	@Override
+	protected void executeMojo() throws MojoExecutionException, MojoFailureException {
+		File databaseSQLDir = new File(baseSqlDir, targetDatabase);
+		if (!databaseSQLDir.exists()) {
+			throw new MojoExecutionException(databaseSQLDir + " does not exist");
+		}
+		getLog().info("Examining " + databaseSQLDir);
+		List<File> files = getFileListing(databaseSQLDir);
+		getLog().info("Located " + files.size() + " " + extension + " files");
+		List<String> tableNames = getTableNames(files);
+		getLog().info("Located " + tableNames.size() + " tables");
+		File outputFile = new File(databaseSQLDir, artifactId + "-tables.txt");
+		try {
+			getLog().info("Generating " + outputFile);
+			FileUtils.writeLines(outputFile, tableNames);
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unexpected IO error", e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected List<File> getFileListing(File dir) {
+		String[] extensions = new String[] { extension };
+		Collection<File> files = FileUtils.listFiles(dir, extensions, false);
+		List<File> fileList = new ArrayList<File>();
+		if (files != null) {
+			fileList.addAll(files);
+		}
+		return fileList;
+	}
+
+	protected List<String> getTableNames(List<File> files) {
+		String[] skipTokens = new String[] { artifactId + "." + extension, artifactId + "-constraints." + extension };
+		List<String> tableNames = new ArrayList<String>();
+		for (File file : files) {
+			if (isSkip(file, skipTokens)) {
+				continue;
+			}
+			String fragment = file.getName();
+			int beginIndex = 0;
+			int endIndex = fragment.length() - (extension.length() + 1);
+			String fragmentMinusExtension = fragment.substring(beginIndex, endIndex);
+			tableNames.add(fragmentMinusExtension);
+		}
+		Collections.sort(tableNames);
+		return tableNames;
+	}
+
+	protected boolean isSkip(File file, String[] skipTokens) {
+		String path = file.getAbsolutePath();
+		for (String skipToken : skipTokens) {
+			if (path.endsWith(skipToken)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	public String getTargetDatabase() {
+		return targetDatabase;
+	}
+
+	public void setTargetDatabase(String targetDatabase) {
+		this.targetDatabase = targetDatabase;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+
+	public void setArtifactId(String artifactId) {
+		this.artifactId = artifactId;
+	}
+
+	public File getBaseSqlDir() {
+		return baseSqlDir;
+	}
+
+	public void setBaseSqlDir(File sqlDir) {
+		this.baseSqlDir = sqlDir;
+	}
+
+}
