9e06a2de4bdc40bf1f6ecd8e9dd2fac8e84473b5
jcaddel <jcaddel@kuali.org>
2012-11-08 13:58:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12274 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/SqlExecutorTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/SqlExecutorTest.java
@@ -1,10 +1,13 @@
 package org.kuali.common.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kuali.common.util.ResourceUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +29,16 @@ public class SqlExecutorTest {
 	@Autowired
 	private Properties properties = null;
 
+	@Autowired
+	private String tables = null;
+
 	@Test
 	public void test() {
 		try {
 			long start = System.currentTimeMillis();
 			logger.info("Jdbc Utils Test");
+			logger.info("tables.length()=" + tables.length());
+			String base = properties.getProperty("sql.location.base");
 			Assert.assertNotNull("sqlExecutor is null.", sqlExecutor);
 			Assert.assertNotNull("dbaSqlExecutor is null.", dbaSqlExecutor);
 			String dbaUser = properties.getProperty("jdbc.dba.username");
@@ -47,16 +55,29 @@ public class SqlExecutorTest {
 			logger.info("Validating database credentials for user '{}' on [{}]", user, url);
 			logger.info("Executed " + sqlExecutor.executeString(properties.getProperty("sql.validate")) + " SQL statements");
 			String schema = properties.getProperty("sql.source.schema");
-			// String schemaLocation = properties.getProperty("sql.schema");
-			// String schemaConstraintsLocation = properties.getProperty("sql.schema.constraints");
+			String schemaLocation = properties.getProperty("sql.schema");
+			String schemaConstraintsLocation = properties.getProperty("sql.schema.constraints");
+			List<String> locations = getTableLocations(base, tables);
 			logger.info("Executing SQL for [{}] on database [{}] at [" + url + "]", schema, db);
-			// logger.info("Executed " + sqlExecutor.executeSQL(schemaLocation) + " SQL statements");
+			logger.info("Executed " + sqlExecutor.executeSQL(schemaLocation) + " SQL statements");
+			for (String location : locations) {
+				logger.info("Executed " + sqlExecutor.executeSQL(location) + " SQL statements");
+			}
 			logger.info("Adding schema constraints for [{}] on database [{}] at [" + url + "]", schema, db);
-			// logger.info("Executed " + sqlExecutor.executeSQL(schemaConstraintsLocation) + " SQL statements");
+			logger.info("Executed " + sqlExecutor.executeSQL(schemaConstraintsLocation) + " SQL statements");
 			logger.info("Elapsed: " + (System.currentTimeMillis() - start));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	protected List<String> getTableLocations(String base, String tables) {
+		List<String> lines = ResourceUtils.readLinesFromString(tables);
+		List<String> tableLocations = new ArrayList<String>();
+		for (String line : lines) {
+			String tableLocation = base + "/" + line + ".sql";
+			tableLocations.add(tableLocation);
+		}
+		return tableLocations;
+	}
 }
