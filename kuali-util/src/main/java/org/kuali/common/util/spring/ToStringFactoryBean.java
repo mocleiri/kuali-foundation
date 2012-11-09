080d5f64b6ddc31690c2de47a2311199b30b9514
jcaddel <jcaddel@kuali.org>
2012-11-09 10:08:01 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12308 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/ToStringFactoryBean.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/ToStringFactoryBean.java
@@ -2,18 +2,12 @@ package org.kuali.common.util.spring;
 
 import org.kuali.common.util.ResourceUtils;
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.util.Assert;
 
 public class ToStringFactoryBean extends ToStringContext implements FactoryBean<String> {
 
 	@Override
 	public String getObject() throws Exception {
-		Assert.notNull("location is null", location);
-		String s = ResourceUtils.toString(location, encoding);
-		if (delete) {
-			ResourceUtils.delete(location, deleteQuietly);
-		}
-		return (trim) ? s.trim() : s;
+		return ResourceUtils.toString(this);
 	}
 
 	@Override
