5095763af5ee81289cb59c41b1dd80c333479c38
jcaddel <jcaddel@kuali.org>
2012-11-09 22:14:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/maven-impex-plugin-1.0@12352 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/apache/torque/mojo/ConvertCloverETLMojo.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright 2004-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.torque.mojo;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.torque.util.CloverETLTable;
+
+/**
+ * @goal convertcloveretl
+ */
+public class ConvertCloverETLMojo extends BaseMojo {
+
+	/**
+	 * @parameter expression="${impex.sourceDirectory}" default-value="${project.basedir}/src/main/resources/data"
+	 * @required
+	 */
+	File sourceDir;
+
+	/**
+	 * @parameter expression="${impex.outputDir}" default-value="${project.build.directory}/impex"
+	 * @required
+	 */
+	File outputDir;
+
+	/**
+	 * @parameter expression="${impex.cloverETLDelimiter}" default-value="|"
+	 * @required
+	 */
+	String cloverETLDelimiter;
+
+	@Override
+	protected void executeMojo() throws MojoExecutionException, MojoFailureException {
+		File[] files = sourceDir.listFiles();
+		Arrays.sort(files);
+		for (File file : files) {
+			convertFile(file);
+		}
+	}
+
+	protected void convertFile(File file) {
+		try {
+			CloverETLTable table = getTable(file);
+			String xml = getXml(table);
+			File outputFile = new File(outputDir + "/" + table.getName() + ".xml");
+			FileUtils.writeStringToFile(outputFile, xml);
+		} catch (IOException e) {
+			throw new IllegalStateException("Unexpected IO error", e);
+		}
+	}
+
+	protected CloverETLTable getTable(File file) {
+		try {
+			String filename = file.getName().toUpperCase();
+			int pos = filename.indexOf(".");
+			String tablename = filename.substring(0, pos);
+
+			String content = FileUtils.readFileToString(file);
+			String headerLine = content.substring(0, content.indexOf("\n"));
+			String[] columns = StringUtils.splitByWholeSeparatorPreserveAllTokens(headerLine, cloverETLDelimiter);
+
+			List<String[]> rows = getRows(content, columns, file);
+
+			CloverETLTable table = new CloverETLTable();
+			table.setName(tablename);
+			table.setColumns(Arrays.asList(columns));
+			table.setRows(rows);
+			return table;
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected List<String> readLines(String s) {
+		try {
+			InputStream in = new ByteArrayInputStream(s.getBytes());
+			return IOUtils.readLines(in);
+		} catch (IOException e) {
+			throw new IllegalStateException("Unexpected IO error", e);
+		}
+
+	}
+
+	protected List<String[]> getRows(String content, String[] columns, File file) {
+		List<String> lines = readLines(content);
+		List<String[]> rows = new ArrayList<String[]>();
+		for (int i = 1; i < lines.size(); i++) {
+			String line = lines.get(i);
+			while (!line.endsWith("|")) {
+				i = i + 1;
+				line = line + "\n" + lines.get(i);
+			}
+			String[] row = StringUtils.splitByWholeSeparatorPreserveAllTokens(line, "|");
+			if (row.length != columns.length) {
+				throw new IllegalStateException("Column count doesn't match. [" + file.getAbsolutePath() + ",row " + i + "] columns=" + columns.length + " row=" + row.length);
+			}
+			for (int j = 0; j < row.length; j++) {
+				row[j] = escape(row[j]);
+			}
+			rows.add(row);
+		}
+		return rows;
+	}
+
+	protected String escape(String s) {
+		return s.replace("<", "&lt;").replace("\"", "&quot;").replace("\n", "&#xa;").replace("\r", "&#xd;");
+	}
+
+	protected String getXml(CloverETLTable table) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+		sb.append("<!DOCTYPE dataset SYSTEM \"data.dtd\">\n");
+		sb.append("<dataset>\n");
+		List<String[]> rows = table.getRows();
+		List<String> columns = table.getColumns();
+		for (int i = 0; i < rows.size(); i++) {
+			sb.append("    <" + table.getName());
+			String[] row = rows.get(i);
+			for (int j = 0; j < columns.size(); j++) {
+				String column = columns.get(j);
+				String value = row[j];
+				if (!StringUtils.isBlank(value)) {
+					sb.append(" " + column.toUpperCase() + "=" + '"' + value + '"');
+				}
+			}
+			sb.append(" />\n");
+		}
+		sb.append("</dataset>\n");
+		return sb.toString();
+	}
+}
