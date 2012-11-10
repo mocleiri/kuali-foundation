328990f7b3dbb0b6d31852f7eec473c976ee689a
jcaddel <jcaddel@kuali.org>
2012-11-09 19:04:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12347 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-morph/src/test/java/org/kuali/common/morph/MorpherTest.java
+++ kuali-morph/src/test/java/org/kuali/common/morph/MorpherTest.java
@@ -2,9 +2,12 @@ package org.kuali.common.morph;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,13 +28,18 @@ public class MorpherTest {
 	@Test
 	public void test() {
 		try {
+			long start = System.currentTimeMillis();
 			Assert.assertNotNull("properties is null.", properties);
 			File dir = new File(properties.getProperty("clover.data.dir"));
 			logger.info(dir.exists() + "");
 			File[] files = dir.listFiles();
-			for (File file : files) {
-				logger.info(file.getCanonicalPath());
+			Arrays.sort(files);
+			List<Table> tables = getTables(Arrays.asList(files));
+			for (Table table : tables) {
+				logger.info(table.getName());
 			}
+			long elapsed = System.currentTimeMillis() - start;
+			logger.info("Elapsed: " + elapsed);
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
@@ -39,7 +47,40 @@ public class MorpherTest {
 
 	protected List<Table> getTables(List<File> files) {
 		List<Table> tables = new ArrayList<Table>();
+		for (File file : files) {
+			Table table = getTable(file);
+			tables.add(table);
+		}
 		return tables;
 	}
 
+	protected Table getTable(File file) {
+		try {
+			String filename = file.getName().toUpperCase();
+			int pos = filename.indexOf(".");
+			String tablename = filename.substring(0, pos);
+
+			List<String> lines = FileUtils.readLines(file);
+			String line1 = lines.get(0);
+			String[] columns = StringUtils.splitByWholeSeparatorPreserveAllTokens(line1, "|");
+
+			List<String[]> rows = new ArrayList<String[]>();
+			for (int i = 1; i < lines.size(); i++) {
+				String line = lines.get(i);
+				String[] row = StringUtils.splitByWholeSeparatorPreserveAllTokens(line, "|");
+				if (row.length > columns.length) {
+					throw new IllegalStateException("Column count doesn't match. [" + file.getAbsolutePath() + ",row " + i + "] columns=" + columns.length + " row=" + row.length);
+				}
+				rows.add(row);
+			}
+
+			Table table = new Table();
+			table.setName(tablename);
+			table.setColumns(Arrays.asList(columns));
+			table.setRows(rows);
+			return table;
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+	}
 }
