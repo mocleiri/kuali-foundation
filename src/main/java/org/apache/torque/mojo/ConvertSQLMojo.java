aefd68f99d5a8086a475a037bde57d73a2201bf9
jcaddel <jcaddel@kuali.org>
2012-11-19 13:29:21 -0500

    Convert sql mojo
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/maven-impex-plugin-1.0@12680 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/apache/torque/mojo/ConvertSQLMojo.java
@@ -0,0 +1,188 @@
+/**
+ * Copyright 2006-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.torque.mojo;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.torque.util.SimpleScanner;
+
+/**
+ * Examine SQL files and convert the delimiter style from using a <code>;</code> at the end of the line to a <code>/</code> on it's own line
+ *
+ * @goal convertsql
+ */
+public class ConvertSQLMojo extends AbstractMojo {
+	private static final String FS = File.separator;
+
+	/**
+	 * @parameter expression="${sql.encoding}" default-value="${project.build.sourceEncoding}"
+	 */
+	private String encoding;
+
+	/**
+	 * @parameter expression="${sql.oldDelimiter}" default-value=";"
+	 * @required
+	 */
+	private String oldDelimiter;
+
+	/**
+	 * @parameter expression="${sql.newDelimiter}" default-value="/"
+	 * @required
+	 */
+	private String newDelimiter;
+
+	/**
+	 * @parameter expression="${sql.sourceDir}" default-value="${project.build.directory}/sql/source"
+	 * @required
+	 */
+	private File sourceDir;
+
+	/**
+	 * @parameter expression="${sql.outputDir}" default-value="${project.build.directory}/sql/output"
+	 * @required
+	 */
+	private File outputDir;
+
+	/**
+	 * @parameter expression="${sql.includes}" default-value="*.sql"
+	 */
+	private String includes;
+
+	/**
+	 * @parameter expression="${sql.excludes}"
+	 */
+	private String excludes;
+
+	@Override
+	public void execute() throws MojoExecutionException {
+		try {
+			List<File> files = getFiles();
+			if (files == null || files.size() == 0) {
+				getLog().info("No files found");
+				return;
+			}
+			convert(files);
+		} catch (Exception e) {
+			throw new MojoExecutionException("Unexpected error", e);
+		}
+	}
+
+	protected void convert(List<File> files) throws IOException {
+		for (File file : files) {
+			convert(file);
+		}
+	}
+
+	protected void convert(File file) throws IOException {
+		String outputFilename = outputDir + FS + file.getName();
+		File outputFile = new File(outputFilename);
+		@SuppressWarnings("unchecked")
+		List<String> lines = FileUtils.readLines(file, encoding);
+		getLog().info("Creating " + outputFile);
+		OutputStream out = null;
+		try {
+			out = FileUtils.openOutputStream(outputFile);
+			for (int i = 0; i < lines.size(); i++) {
+				String line = lines.get(i);
+				String convertedLine = getConvertedLine(line);
+				out.write(convertedLine.getBytes(encoding));
+			}
+		} finally {
+			IOUtils.closeQuietly(out);
+		}
+	}
+
+	protected String getConvertedLine(String line) {
+		String trimmed = StringUtils.trim(line);
+		if (trimmed.endsWith(oldDelimiter)) {
+			int pos = line.lastIndexOf(oldDelimiter);
+			return line.substring(0, pos) + IOUtils.LINE_SEPARATOR_UNIX + newDelimiter + IOUtils.LINE_SEPARATOR_UNIX;
+		} else {
+			return line + IOUtils.LINE_SEPARATOR_UNIX;
+		}
+	}
+
+	protected List<File> getFiles() throws IOException {
+		FileUtils.forceMkdir(sourceDir);
+		SimpleScanner scanner = new SimpleScanner(sourceDir, includes, excludes);
+		return scanner.getFiles();
+	}
+
+	public File getSourceDir() {
+		return sourceDir;
+	}
+
+	public void setSourceDir(File inputDir) {
+		this.sourceDir = inputDir;
+	}
+
+	public File getOutputDir() {
+		return outputDir;
+	}
+
+	public void setOutputDir(File outputDir) {
+		this.outputDir = outputDir;
+	}
+
+	public String getIncludes() {
+		return includes;
+	}
+
+	public void setIncludes(String includes) {
+		this.includes = includes;
+	}
+
+	public String getExcludes() {
+		return excludes;
+	}
+
+	public void setExcludes(String excludes) {
+		this.excludes = excludes;
+	}
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public String getOldDelimiter() {
+		return oldDelimiter;
+	}
+
+	public void setOldDelimiter(String oldDelimiter) {
+		this.oldDelimiter = oldDelimiter;
+	}
+
+	public String getNewDelimiter() {
+		return newDelimiter;
+	}
+
+	public void setNewDelimiter(String newDelimiter) {
+		this.newDelimiter = newDelimiter;
+	}
+
+}
