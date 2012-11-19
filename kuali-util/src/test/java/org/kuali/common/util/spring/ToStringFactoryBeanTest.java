809fed1a500f4baf0e387ae816cf7b80be0fcd73
jcaddel <jcaddel@kuali.org>
2012-11-18 19:03:19 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12651 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/ToStringFactoryBeanTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/ToStringFactoryBeanTest.java
@@ -3,9 +3,6 @@ package org.kuali.common.util.spring;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.kuali.common.util.Str;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,19 +11,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration
 public class ToStringFactoryBeanTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(ToStringFactoryBeanTest.class);
-
 	@Autowired
 	private String password;
 
 	@Test
 	public void test() {
-		try {
-			Assert.assertNotNull("password is null.", password);
-			logger.info(Str.flatten(password, "CR", "LF"));
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+		Assert.assertNotNull("password is null.", password);
+		Assert.assertEquals(password, "foo");
 	}
 
 }
