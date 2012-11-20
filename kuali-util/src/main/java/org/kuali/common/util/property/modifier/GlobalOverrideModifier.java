9d8743d951bd222ecf63a1fca9554be2119b056d
jcaddel <jcaddel@kuali.org>
2012-11-20 15:41:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12747 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/property/modifier/GlobalOverrideModifier.java
+++ kuali-util/src/main/java/org/kuali/common/util/property/modifier/GlobalOverrideModifier.java
@@ -3,13 +3,12 @@ package org.kuali.common.util.property.modifier;
 import java.util.List;
 import java.util.Properties;
 
-import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.util.Mode;
 import org.kuali.common.util.PropertyUtils;
 import org.kuali.common.util.property.Constants;
 import org.kuali.common.util.property.GlobalPropertiesMode;
 
-public class OverrideModifier implements PropertyModifier {
+public class GlobalOverrideModifier implements PropertyModifier {
 
 	GlobalPropertiesMode globalPropertiesMode = GlobalPropertiesMode.BOTH;
 	Mode propertyOverwriteMode = Constants.DEFAULT_PROPERTY_OVERWRITE_MODE;
@@ -22,14 +21,6 @@ public class OverrideModifier implements PropertyModifier {
 			String newValue = global.getProperty(key);
 			PropertyUtils.setProperty(properties, key, newValue, propertyOverwriteMode);
 		}
-	}
-
-	protected boolean isOverride(String globalValue, Properties properties, String key) {
-		if (StringUtils.isBlank(globalValue)) {
-			return false;
-		}
-		String originalValue = properties.getProperty(key);
-		return !StringUtils.equals(globalValue, originalValue);
 	}
 
 	public GlobalPropertiesMode getGlobalPropertiesMode() {
