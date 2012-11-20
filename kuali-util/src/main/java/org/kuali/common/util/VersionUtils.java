fcd3948840844610f091bde222965826320e1b90
jcaddel <jcaddel@kuali.org>
2012-11-20 10:03:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12706 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/VersionUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/VersionUtils.java
@@ -4,17 +4,24 @@ import org.apache.commons.lang3.StringUtils;
 
 public class VersionUtils {
 
-	public static final String MAVEN_SNAPSHOT_TOKEN = "-SNAPSHOT";
+	public static final String MAVEN_SNAPSHOT_TOKEN = "SNAPSHOT";
+	private static final String[] DELIMITERS = new String[] { ".", "-" };
 
 	/**
-	 * Return true if <code>version</code> ends with <code>-SNAPSHOT</code> (case insensitive).
+	 * Return true if <code>version</code> ends with <code>-SNAPSHOT</code> or <code>.SNAPSHOT</code> (case insensitive).
 	 */
 	public static final boolean isSnapshot(String version) {
-		return StringUtils.endsWithIgnoreCase(version, MAVEN_SNAPSHOT_TOKEN);
+		for (String delimiter : DELIMITERS) {
+			String suffix = delimiter + MAVEN_SNAPSHOT_TOKEN;
+			if (StringUtils.endsWithIgnoreCase(version, suffix)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
-	 * Return <code>version</code> with <code>-SNAPSHOT</code> removed from the end (if it is present)
+	 * Return <code>version</code> with <code>SNAPSHOT</code> removed from the end (if it is present)
 	 *
 	 * <pre>
 	 * 1.0.0-SNAPSHOT returns 1.0.0
@@ -23,26 +30,35 @@ public class VersionUtils {
 	 */
 	public static final String trimSnapshot(String version) {
 		if (isSnapshot(version)) {
-			int length = MAVEN_SNAPSHOT_TOKEN.length();
+			int length = MAVEN_SNAPSHOT_TOKEN.length() + 1;
 			return StringUtils.left(version, version.length() - length);
 		} else {
 			return version;
 		}
 	}
 
+	protected static final String getSeparatorChars() {
+		StringBuilder sb = new StringBuilder();
+		for (String delimiter : DELIMITERS) {
+			sb.append(delimiter);
+		}
+		return sb.toString();
+	}
+
 	/**
 	 * Parse a <code>Version</code> object from the <code>version</code> string. The logic here is crudely simple. First
-	 * <code>-SNAPSHOT</code> is trimmed off the end of the string (if it exists). Whatever remains is then split into tokens using
+	 * <code>SNAPSHOT</code> is trimmed off the end of the string (if it exists). Whatever remains is then split into tokens using
 	 * <code>.</code> and <code>-</code> as delimiters. The first 3 tokens encountered are <code>major</code>, <code>minor</code>, and
 	 * <code>incremental</code>, respectively. Anything after that is the <code>qualifier</code>
 	 */
 	public static final Version getVersion(String version) {
+		String separatorChars = getSeparatorChars();
 		boolean snapshot = isSnapshot(version);
 		String trimmed = trimSnapshot(version);
 		Version v = new Version();
 		v.setTrimmed(trimmed);
 		v.setSnapshot(snapshot);
-		String[] tokens = StringUtils.split(trimmed, ".-");
+		String[] tokens = StringUtils.split(trimmed, separatorChars);
 		if (tokens.length > 0) {
 			v.setMajor(tokens[0]);
 		}
