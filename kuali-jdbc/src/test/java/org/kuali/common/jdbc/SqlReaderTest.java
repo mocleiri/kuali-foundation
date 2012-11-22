15bd7151d04dfc9fffe331322f3768bf073f09e4
jcaddel <jcaddel@kuali.org>
2012-11-21 20:08:19 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12808 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/SqlReaderTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/SqlReaderTest.java
@@ -1,6 +1,7 @@
 package org.kuali.common.jdbc;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
@@ -14,18 +15,25 @@ public class SqlReaderTest {
 	final Logger logger = LoggerFactory.getLogger(SqlReaderTest.class);
 
 	@Test
-	public void simpleTest() {
-		try {
-			SqlReader sqlReader = new DefaultSqlReader();
-			String sql = "SELECT 1\r/\nSELECT 1\n/\nSELECT 1\r\n/";
-			BufferedReader reader = LocationUtils.getBufferedReaderFromString(sql);
-			String s = sqlReader.getSqlStatement(reader);
-			while (s != null) {
-				logger.info("[" + Str.flatten(s) + "]");
-				s = sqlReader.getSqlStatement(reader);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+	public void simpleCommentTest() throws IOException {
+		SqlReader sqlReader = new DefaultSqlReader();
+		BufferedReader reader = LocationUtils.getBufferedReaderFromString(getSqlWithComment1());
+		Assert.assertEquals("SELECT 1", sqlReader.getSqlStatement(reader));
+		reader = LocationUtils.getBufferedReaderFromString(getSqlWithComment2());
+		Assert.assertEquals("SELECT 1", sqlReader.getSqlStatement(reader));
+		reader = LocationUtils.getBufferedReaderFromString(getSqlWithComment3());
+		Assert.assertEquals("SELECT '\n-- Howdy'", sqlReader.getSqlStatement(reader));
+	}
+
+	@Test
+	public void simpleTest() throws IOException {
+		SqlReader sqlReader = new DefaultSqlReader();
+		String sql = "SELECT 1\r/\nSELECT 1\n/\nSELECT 1\r\n/";
+		BufferedReader reader = LocationUtils.getBufferedReaderFromString(sql);
+		String s = sqlReader.getSqlStatement(reader);
+		while (s != null) {
+			logger.info("[" + Str.flatten(s) + "]");
+			s = sqlReader.getSqlStatement(reader);
 		}
 	}
 
@@ -36,6 +44,27 @@ public class SqlReaderTest {
 		for (LineSeparator ls : LineSeparator.values()) {
 			Assert.assertTrue(StringUtils.isBlank(ls.getValue()));
 		}
+	}
+
+	protected String getSqlWithComment1() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("-- Howdy\n");
+		sb.append("SELECT 1");
+		return sb.toString();
+	}
+
+	protected String getSqlWithComment2() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("# Howdy\n");
+		sb.append("SELECT 1");
+		return sb.toString();
+	}
+
+	protected String getSqlWithComment3() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("-- Howdy\n");
+		sb.append("SELECT '\n-- Howdy'");
+		return sb.toString();
 	}
 
 }
