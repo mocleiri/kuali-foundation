0d25e15808d04a5b57dd68fda21907a8a9ec3d3e
jcaddel <jcaddel@kuali.org>
2012-11-21 23:59:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12831 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/DatabaseContext.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/DatabaseContext.java
@@ -6,27 +6,35 @@ public class DatabaseContext {
 	String driver;
 	String username;
 	String password;
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public String getDriver() {
 		return driver;
 	}
+
 	public void setDriver(String driver) {
 		this.driver = driver;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
