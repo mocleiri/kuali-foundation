d03461d9b9a3e8249667901cd66244ae44754b70
jcaddel <jcaddel@kuali.org>
2012-11-27 02:33:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@12999 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/DefaultJdbcService.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/DefaultJdbcService.java
@@ -64,6 +64,7 @@ public class DefaultJdbcService implements JdbcService {
 			conn.setAutoCommit(false);
 			statement = conn.createStatement();
 			SqlMetaDataList smdl = new SqlMetaDataList();
+			logger.info("Execution SQL from {} sources", sources.size());
 			for (SqlSource source : sources) {
 				SqlSourceExecutionContext sec = getSourceSqlExecutionContext(context, conn, statement, source, count);
 				SqlMetaData smd = executeSqlFromSource(sec);
