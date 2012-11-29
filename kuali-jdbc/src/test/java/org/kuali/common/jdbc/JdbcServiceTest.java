571efaa56ea8a79dcc622e78d9611815d430a901
jcaddel <jcaddel@kuali.org>
2012-11-29 09:31:31 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13069 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/JdbcServiceTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/JdbcServiceTest.java
@@ -1,8 +1,13 @@
 package org.kuali.common.jdbc;
 
+import java.util.List;
+import java.util.Properties;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kuali.common.jdbc.context.DatabaseInitializeContext;
+import org.kuali.common.util.PropertyUtils;
+import org.kuali.common.util.Str;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +28,11 @@ public class JdbcServiceTest {
 
 	@Test
 	public void testOLEDatabaseProcess() {
-		logger.info("Hello world");
-		service.initialize(context);
+		Properties properties = context.getProperties();
+		List<String> keys = PropertyUtils.getSortedKeys(properties);
+		for (String key : keys) {
+			logger.info(key + "=" + Str.flatten(properties.getProperty(key)));
+		}
+		// service.initialize(context);
 	}
 }
