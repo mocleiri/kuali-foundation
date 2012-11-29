96ebcfea54d653abb8b730d4a28779c578683453
jcaddel <jcaddel@kuali.org>
2012-11-29 17:45:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13098 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/DatabaseServiceTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/DatabaseServiceTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class DatabaseInitializerTest {
+public class DatabaseServiceTest {
 
 	@Autowired
 	DatabaseResetContext context = null;
