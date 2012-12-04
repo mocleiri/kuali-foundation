0b335b36356dcf559cdb448e3da67c4aac91e728
jcaddel <jcaddel@kuali.org>
2012-12-04 12:35:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13264 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/ContextMojo.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/ContextMojo.java
@@ -5,7 +5,7 @@ import java.io.File;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
-import org.kuali.common.util.Executable;
+import org.kuali.common.util.execute.Executable;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
