e230845046c28f8af16063e22337f5640ac82e98
jcaddel <jcaddel@kuali.org>
2012-12-04 03:35:48 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13238 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/property/processor/HomeProcessor.java
+++ kuali-util/src/main/java/org/kuali/common/util/property/processor/HomeProcessor.java
@@ -25,7 +25,7 @@ public class HomeProcessor implements PropertyProcessor {
 	String orgGroupCodeKey;
 	String projectGroupCodeKey;
 	String orgHomeKey;
-	String groupHomeKey = Constants.DEFAULT_GROUP_HOME_PREFIX + "." + Constants.DEFAULT_HOME_SUFFIX;
+	String groupHomeKey;
 
 	public HomeProcessor() {
 		this(null, null, GlobalPropertiesMode.BOTH);
