25e072d10458904d7fdcd9a06756a0daceedd6c6
jcaddel <jcaddel@kuali.org>
2012-12-04 12:09:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13260 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/ContextMojo.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/ContextMojo.java
@@ -2,6 +2,9 @@ package org.kuali.maven.plugins.spring;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -9,9 +12,35 @@ import org.apache.maven.plugin.MojoExecutionException;
  */
 public class ContextMojo extends AbstractMojo {
 
+	/**
+	 * @parameter default-value="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
+
+	/**
+	 * @parameter expression="${spring.contextLocation}" default-value="classpath:${project.artifactId}-context.xml"
+	 * @required
+	 */
+	private String contextLocation;
+
 	@Override
 	public void execute() throws MojoExecutionException {
 		getLog().info("Hello world");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(contextLocation);
+	}
+
+	public String getContextLocation() {
+		return contextLocation;
+	}
+
+	public void setContextLocation(String contextLocation) {
+		this.contextLocation = contextLocation;
+	}
+
+	public MavenProject getProject() {
+		return project;
 	}
 
 }
