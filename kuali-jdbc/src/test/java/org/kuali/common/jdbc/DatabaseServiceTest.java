c8a4dc294bba4dae30b06c4bb842e63545e5b107
jcaddel <jcaddel@kuali.org>
2012-12-04 03:30:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13237 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/DatabaseServiceTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/DatabaseServiceTest.java
@@ -1,8 +1,15 @@
 package org.kuali.common.jdbc;
 
+import java.util.List;
+import java.util.Properties;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kuali.common.jdbc.context.DatabaseResetContext;
+import org.kuali.common.util.PropertyUtils;
+import org.kuali.common.util.Str;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -10,6 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class DatabaseServiceTest {
+	private static final Logger logger = LoggerFactory.getLogger(DatabaseServiceTest.class);
 
 	@Autowired
 	DatabaseResetContext context = null;
@@ -17,8 +25,16 @@ public class DatabaseServiceTest {
 	@Autowired
 	DatabaseService service = null;
 
+	@Autowired
+	Properties properties = null;
+
 	@Test
 	public void resetDatabaseTest() {
+		List<String> keys = PropertyUtils.getSortedKeys(properties);
+		for (String key : keys) {
+			String value = properties.getProperty(key);
+			logger.info(key + "=" + Str.flatten(value));
+		}
 		service.reset(context);
 	}
 }
