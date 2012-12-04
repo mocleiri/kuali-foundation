d4c89bc76dccf777293e1ad7e3391318332e8d11
jcaddel <jcaddel@kuali.org>
2012-12-04 17:57:57 -0500

    Commented out unit tests
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13287 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/DatabaseServiceTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/DatabaseServiceTest.java
@@ -3,32 +3,28 @@ package org.kuali.common.jdbc;
 import java.util.List;
 import java.util.Properties;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.kuali.common.jdbc.context.DatabaseResetContext;
 import org.kuali.common.util.PropertyUtils;
 import org.kuali.common.util.Str;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration
 public class DatabaseServiceTest {
+
 	private static final Logger logger = LoggerFactory.getLogger(DatabaseServiceTest.class);
 
-	@Autowired
+	// @Autowired
 	DatabaseResetContext context = null;
 
-	@Autowired
+	// @Autowired
 	DatabaseService service = null;
 
-	@Autowired
+	// @Autowired
 	Properties properties = null;
 
-	@Test
+	// @Test
 	public void resetDatabaseTest() {
 		List<String> keys = PropertyUtils.getSortedKeys(properties);
 		for (String key : keys) {
--- kuali-jdbc/src/test/java/org/kuali/common/jdbc/PropertiesTest.java
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/PropertiesTest.java
@@ -2,20 +2,14 @@ package org.kuali.common.jdbc;
 
 import java.util.Properties;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration
 public class PropertiesTest {
 
-	@Autowired
+	// @Autowired
 	Properties properties = null;
 
-	@Test
+	// @Test
 	public void resetDatabaseTest() {
 		System.out.println(properties.size());
 	}
