fb171e32be40abee3415a216d98ff46ceec6cdbc
jcaddel <jcaddel@kuali.org>
2012-12-05 00:27:12 -0500

    [maven-release-plugin]  copy for tag kuali-deploy-1.2.22
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-deploy-1.2.22@13310 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/Mode.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common.util;
-
-public enum Mode {
-	IGNORE, // Proceed silently, emit no logging messages
-	DEBUG, // Log a debug message
-	INFORM, // Log an info level message
-	WARN, // Log a warn level message
-	ERROR; // Throw an exception
-}
