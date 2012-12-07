9e66db9b64c30b617748b3f7aa783f9c2e096d7a
jcaddel <jcaddel@kuali.org>
2012-12-07 15:45:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13464 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/service/DefaultSpringService.java
+++ kuali-util/src/main/java/org/kuali/common/util/service/DefaultSpringService.java
@@ -70,7 +70,8 @@ public class DefaultSpringService implements SpringService {
 	}
 
 	protected String getFilteredContent(LoadContext context) {
-		Properties duplicate = PropertyUtils.getProperties(context.getProperties(), context.getGlobalPropertiesMode());
+		Properties original = context.getProperties() == null ? new Properties() : context.getProperties();
+		Properties duplicate = PropertyUtils.getProperties(original, context.getGlobalPropertiesMode());
 		PropertyUtils.trim(duplicate, context.getFilterIncludes(), context.getFilterExcludes());
 		String content = LocationUtils.toString(context.getContextLocation(), context.getEncoding());
 		logger.info("Filtering [" + context.getContextLocation() + "] using " + duplicate.size() + " properties");
