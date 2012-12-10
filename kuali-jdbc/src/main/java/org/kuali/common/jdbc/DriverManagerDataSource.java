52dcc33620dbb0058cc214e24e38da2f03f0bf4c
jcaddel <jcaddel@kuali.org>
2012-12-10 12:31:58 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13656 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/DriverManagerDataSource.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/DriverManagerDataSource.java
@@ -2,6 +2,7 @@ package org.kuali.common.jdbc;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 import org.kuali.common.util.CollectionUtils;
 import org.kuali.common.util.property.Constants;
@@ -9,9 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Very simple extension providing a token based method for explicitly setting the database password to <code>null</code>. If
- * <code>password</code> equals any of the strings in <code>nullPasswordTokens</code>, the <code>init()</code> method sets
- * <code>password<code> to <code>null</code>.
+ * Very simple extension providing a token based method for explicitly setting the database password to <code>null</code>.
  */
 public class DriverManagerDataSource extends org.springframework.jdbc.datasource.DriverManagerDataSource {
 
@@ -19,7 +18,28 @@ public class DriverManagerDataSource extends org.springframework.jdbc.datasource
 
 	List<String> nullPasswordTokens = Arrays.asList(new String[] { Constants.NULL, Constants.NONE });
 
-	public void init() {
+	public DriverManagerDataSource() {
+		super();
+	}
+
+	public DriverManagerDataSource(String url, Properties conProps) {
+		super(url, conProps);
+	}
+
+	public DriverManagerDataSource(String url, String username, String password) {
+		super(url, username, password);
+		nullifyPassword();
+	}
+
+	public DriverManagerDataSource(String url) {
+		super(url);
+	}
+
+	/**
+	 * If <code>super.getPassword()</code> equals any of the strings in <code>nullPasswordTokens</code>, invoke
+	 * <code>super.setPassword(null)</code>
+	 */
+	public void nullifyPassword() {
 		String pw = super.getPassword();
 		List<String> tokens = CollectionUtils.toEmpty(nullPasswordTokens);
 		// Null it out if it matches one of the tokens
@@ -30,4 +50,11 @@ public class DriverManagerDataSource extends org.springframework.jdbc.datasource
 		}
 	}
 
+	public List<String> getNullPasswordTokens() {
+		return nullPasswordTokens;
+	}
+
+	public void setNullPasswordTokens(List<String> nullPasswordTokens) {
+		this.nullPasswordTokens = nullPasswordTokens;
+	}
 }
