713c452c26798715835f70a303c39776de125cd3
jcaddel <jcaddel@kuali.org>
2012-12-10 10:30:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13632 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/test/java/org/springframework/jdbc/datasource/DriverManagerDataSourceTest.java
+++ kuali-jdbc/src/test/java/org/springframework/jdbc/datasource/DriverManagerDataSourceTest.java
@@ -1,17 +1,34 @@
 package org.springframework.jdbc.datasource;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
+import org.kuali.common.jdbc.JdbcUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DriverManagerDataSourceTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(DriverManagerDataSourceTest.class);
+
+	protected DataSource getMySQLDataSource(String url, String username, String password) {
+		DriverManagerDataSource dmsd = new DriverManagerDataSource();
+		dmsd.setDriverClassName("com.mysql.jdbc.Driver");
+		dmsd.setUrl(url);
+		dmsd.setUsername(username);
+		dmsd.setPassword(password);
+		return dmsd;
+	}
 
 	@Test
 	public void test() {
 		try {
-			DriverManagerDataSource dmsd = new DriverManagerDataSource();
-			dmsd.setDriverClassName("com.mysql.jdbc.Driver");
-			dmsd.setUrl("jdbc:mysql://localhost");
-			dmsd.setUsername("root");
-			dmsd.setPassword(null);
+			DataSource dataSource = getMySQLDataSource("jdbc:mysql://localhost", "root", null);
+			Connection conn = DataSourceUtils.doGetConnection(dataSource);
+			logger.info(conn + "");
+			JdbcUtils.closeQuietly(dataSource, conn);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
