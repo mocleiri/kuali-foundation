1765c76aeadd3ffeb2330a8d9b094df1cc18e5d6
jcaddel <jcaddel@kuali.org>
2012-12-13 13:47:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@13992 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/property/processor/CopyStringProcessor.java
+++ kuali-util/src/main/java/org/kuali/common/util/property/processor/CopyStringProcessor.java
@@ -20,6 +20,9 @@ import java.util.Properties;
 
 import org.apache.commons.beanutils.BeanUtils;
 
+/**
+ * Copy a String property value into a bean
+ */
 public class CopyStringProcessor implements PropertyProcessor {
 
 	Object bean;
