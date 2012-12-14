690e2ed22f484d9cf871fd3c03d6f05cd7d9f366
jcaddel <jcaddel@kuali.org>
2012-12-13 19:02:14 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14011 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/property/DefaultPropertyLoadContext.java
+++ kuali-util/src/main/java/org/kuali/common/util/property/DefaultPropertyLoadContext.java
@@ -25,6 +25,7 @@ import org.kuali.common.util.ModeUtils;
 import org.kuali.common.util.PropertyUtils;
 import org.kuali.common.util.property.processor.GlobalOverrideProcessor;
 import org.kuali.common.util.property.processor.GroupCodeProcessor;
+import org.kuali.common.util.property.processor.HomeProcessor;
 import org.kuali.common.util.property.processor.PathProcessor;
 import org.kuali.common.util.property.processor.PropertyProcessor;
 import org.kuali.common.util.property.processor.ResolvePlaceholdersProcessor;
@@ -156,6 +157,7 @@ public class DefaultPropertyLoadContext extends DefaultPropertyContext implement
 		String groupId = getGroupId();
 		if (organizationGroupId != null && groupId != null) {
 			processors.add(new GroupCodeProcessor(organizationGroupId, groupId));
+			processors.add(new HomeProcessor(organizationGroupId, groupId));
 		}
 
 		if (groupIdProperty != null) {
