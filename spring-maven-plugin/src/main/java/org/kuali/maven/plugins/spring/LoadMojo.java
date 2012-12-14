17323749248505c55f2230d3f0aed90f1aac13ca
jcaddel <jcaddel@kuali.org>
2012-12-14 17:31:17 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14126 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/LoadMojo.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/LoadMojo.java
@@ -57,8 +57,8 @@ import org.springframework.util.PropertyPlaceholderHelper;
  * &lt;context:property-placeholder location="${maven.spring.properties}" /&gt;
  * </pre>
  *
- * Executing {@code mvn spring:load} will then filter {@code my-context.xml} replacing <code>${maven.spring.properties>}</code> with the
- * file Maven properties were exported to. When the Spring context is loaded, Maven properties will be used to perform placeholder
+ * Executing {@code mvn spring:load} will then filter {@code my-context.xml} replacing <code>${maven.spring.properties}</code> with the
+ * location Maven properties were exported to. When the Spring context is loaded, Maven properties will be used to perform placeholder
  * substitution. This makes it possible to easily take advantage of Maven configuration like {@code project.artifactId} in the Spring
  * context.<br>
  * <br>
