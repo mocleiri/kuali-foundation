7272f67540e91d14d87f5adb5a9b6b66c2c31241
jcaddel <jcaddel@kuali.org>
2012-12-17 09:18:35 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14209 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/LoggerUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/LoggerUtils.java
@@ -19,7 +19,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.util.obscure.DefaultObscurer;
 import org.kuali.common.util.obscure.Obscurer;
 import org.kuali.common.util.property.Constants;
-import org.slf4j.Logger;
 
 public class LoggerUtils {
 
@@ -52,25 +51,6 @@ public class LoggerUtils {
 			// Otherwise obscure it
 			return obscurer.obscure(password);
 		}
-	}
-
-	public static final void logUsername(Logger logger, String prefix, String username) {
-		logger.info("{} - {}", prefix, getUsername(username));
-	}
-
-	public static final void logPassword(Logger logger, String prefix, String username, String password) {
-		logPassword(logger, prefix, username, password, DEFAULT_OBSCURER);
-	}
-
-	public static final void logPassword(Logger logger, String prefix, String username, String password, Obscurer obscurer) {
-		logger.info("{} - {}", prefix, getPassword(username, password, obscurer));
-	}
-
-	/**
-	 * Log <code>null</code> as <code>NONE</code>
-	 */
-	public static final void logNullAsNone(Logger logger, String prefix) {
-		logger.info("{} - {}", prefix, Constants.NONE);
 	}
 
 }
