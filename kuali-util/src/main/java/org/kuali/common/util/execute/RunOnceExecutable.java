e413e551e40612aa55c87b57eba537ea53bc1c6d
jcaddel <jcaddel@kuali.org>
2012-12-18 21:50:30 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14319 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/execute/RunOnceExecutable.java
+++ kuali-util/src/main/java/org/kuali/common/util/execute/RunOnceExecutable.java
@@ -13,34 +13,37 @@ public class RunOnceExecutable implements Executable {
 	private static final Logger logger = LoggerFactory.getLogger(RunOnceExecutable.class);
 
 	Executable executable;
+	String groupHome;
+	Properties properties;
 	File propertiesFile;
 	String property;
 	String encoding;
 
 	@Override
 	public void execute() {
-		Properties properties = PropertyUtils.load(propertiesFile);
-		String value = properties.getProperty(property);
-		logger.info("[{}={}]", property, value);
-		boolean runonce = StringUtils.equals(ExecutionMode.RUNONCE.name(), value);
+		String runOnceValue = properties.getProperty(property);
+		boolean runonce = StringUtils.equals(ExecutionMode.RUNONCE.name(), runOnceValue);
 		if (runonce) {
+			logger.info("Running once - [{}={}]", property, runOnceValue);
+			Properties loaded = PropertyUtils.load(propertiesFile, encoding);
 			// Make sure we have the ability to successfully store updated properties back to the file
-			logger.info("Updating state to {}", ExecutionMode.INPROGRESS);
-			setState(properties, property, ExecutionMode.INPROGRESS);
+			setState(loaded, property, ExecutionMode.INPROGRESS);
 			try {
 				// Invoke execute now that we have successfully transitioned things to INPROGRESS
 				logger.info("Executing task");
 				executable.execute();
-				logger.info("Updating state to {}", ExecutionMode.COMPLETED);
-				setState(properties, property, ExecutionMode.COMPLETED);
+				setState(loaded, property, ExecutionMode.COMPLETED);
 			} catch (Exception e) {
-				setState(properties, property, ExecutionMode.FAILED);
+				setState(loaded, property, ExecutionMode.FAILED);
 				throw new IllegalStateException("Unexpected execution error", e);
 			}
+		} else {
+			logger.info("Skipping execution - [{}={}]", property, runOnceValue);
 		}
 	}
 
 	protected void setState(Properties properties, String key, ExecutionMode mode) {
+		logger.info("Updating state to {}", ExecutionMode.INPROGRESS);
 		properties.setProperty(property, mode.name());
 		PropertyUtils.store(properties, propertiesFile, encoding);
 	}
