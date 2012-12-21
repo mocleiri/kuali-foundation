5dbb2109a0bc0ace00702204eebb955378d9b86d
jcaddel <jcaddel@kuali.org>
2012-12-21 16:47:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14459 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/execute/Constants.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/execute/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
 	public static final String TOMCAT_LOGS = TOMCAT_HOME + "/logs";
 	public static final String TOMCAT_CONF = TOMCAT_HOME + "/conf";
 	public static final String TOMCAT_WEBAPPS = TOMCAT_HOME + "/webapps";
+	public static final String TOMCAT_ROOT_WAR = TOMCAT_WEBAPPS + "/ROOT.war";
 	public static final String TOMCAT_SETENV = TOMCAT_BIN + "/setenv.sh";
 	public static final String TOMCAT_SHUTDOWN = TOMCAT_BIN + "/forced-shutdown.sh";
 	public static final String TOMCAT_STARTUP = TOMCAT_BIN + "/startup.sh";
---
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/execute/TomcatCopyWar.java
@@ -0,0 +1,17 @@
+package org.kuali.common.deploy.execute;
+
+import java.io.File;
+
+public class TomcatCopyWar extends CopyToRemote {
+
+	public TomcatCopyWar() {
+		this(null, null);
+	}
+
+	public TomcatCopyWar(String hostname, File war) {
+		super();
+		this.hostname = hostname;
+		this.localFile = war;
+		this.remoteFile = Constants.TOMCAT_ROOT_WAR;
+	}
+}
