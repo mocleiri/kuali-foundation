9bc663005bf71ddd91b7233c5669a47daf55fea5
jcaddel <jcaddel@kuali.org>
2012-12-21 18:35:35 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14476 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/test/java/org/kuali/common/deploy/execute/TomcatController.java
+++ kuali-deploy/src/test/java/org/kuali/common/deploy/execute/TomcatController.java
@@ -20,6 +20,8 @@ public class TomcatController {
 			File jdbcDriver = new File("/Users/jeffcaddel/.m2/repository/com/oracle/ojdbc6_g/11.2.0.3/ojdbc6_g-11.2.0.3.jar");
 			// File war = new File("/Users/jeffcaddel/ws/ole-kfs-5.0/ole-app/ole-fs/target/ole-fs-0.8.0-q-SNAPSHOT-webapp.war");
 			File war = new File("/Users/jeffcaddel/Downloads/tomcatPluginV33.zip");
+			File setenv = war;
+			File appDynamicsController = war;
 			String hostname = "env7.ole.kuali.org";
 			List<Executable> executables = new ArrayList<Executable>();
 			executables.add(new TomcatShutdown(hostname));
@@ -27,7 +29,8 @@ public class TomcatController {
 			executables.add(new TomcatRemoveRootWebapp(hostname));
 			executables.add(new TomcatCopyJdbcDriver(hostname, jdbcDriver));
 			executables.add(new TomcatCopyWar(hostname, war));
-			executables.add(new TomcatCopySetEnv(hostname, war));
+			executables.add(new TomcatCopySetEnv(hostname, setenv));
+			executables.add(new AppDynamicsSetup(hostname, appDynamicsController));
 			executables.add(new TomcatCleanup(hostname));
 			executables.add(new TomcatStartup(hostname));
 			for (Executable executable : executables) {
