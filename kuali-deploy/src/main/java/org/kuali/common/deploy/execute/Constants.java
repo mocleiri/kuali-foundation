fd9641c6a7c60ae2bfabcb34cbf24994f18cce17
jcaddel <jcaddel@kuali.org>
2012-12-21 18:28:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14474 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/execute/Constants.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/execute/Constants.java
@@ -19,6 +19,9 @@ public class Constants {
 	public static final String TOMCAT_SHUTDOWN = TOMCAT_BIN + "/forced-shutdown.sh";
 	public static final String TOMCAT_STARTUP = TOMCAT_BIN + "/startup.sh";
 	public static final String TOMCAT_CLEANUP = TOMCAT_BIN + "/cleanup.sh";
+	public static final String APP_DYNAMICS_DIR = "/usr/local/server-agent/conf";
+	public static final String APP_DYNAMICS_CONTROLLER = APP_DYNAMICS_DIR + "/controller-info.xml";
+
 	// JAR files matching these patterns are removed prior to a fresh deploy
 	public static final List<String> JDBC_DRIVER_PATTERNS = Arrays.asList(TOMCAT_LIB + "/mysql*.jar", TOMCAT_LIB + "/ojdbc*.jar", TOMCAT_LIB + "/oracle*.jar");
 }
