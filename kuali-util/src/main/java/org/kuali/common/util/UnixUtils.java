d16eff711f261d47c33b077aa7148fcd4609f27d
jcaddel <jcaddel@kuali.org>
2012-12-21 02:47:22 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14419 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/UnixUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/UnixUtils.java
@@ -29,6 +29,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
+/**
+ * Execute Unix utilities using java.
+ */
 public class UnixUtils {
 	private static final Logger logger = LoggerFactory.getLogger(UnixUtils.class);
 	private static final String SCP = "scp";
