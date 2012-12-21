a90a88a07798475b378d539af6ea07ecbaae4109
jcaddel <jcaddel@kuali.org>
2012-12-21 14:43:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14446 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/test/java/org/kuali/common/deploy/execute/TomcatController.java
+++ kuali-deploy/src/test/java/org/kuali/common/deploy/execute/TomcatController.java
@@ -1,6 +1,10 @@
 package org.kuali.common.deploy.execute;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+import org.kuali.common.util.execute.Executable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +16,14 @@ public class TomcatController {
 	public void test() {
 		try {
 			logger.info("Hello world");
+			String hostname = "env7.ole.kuali.org";
+			List<Executable> executables = new ArrayList<Executable>();
+			executables.add(new TomcatStop(hostname));
+			executables.add(new TomcatCleanup(hostname));
+			executables.add(new TomcatStart(hostname));
+			for (Executable executable : executables) {
+				executable.execute();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
