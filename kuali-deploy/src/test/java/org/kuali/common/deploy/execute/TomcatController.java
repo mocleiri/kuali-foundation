40a9fffb2b01905d2c067836259f1fbcd557dc04
jcaddel <jcaddel@kuali.org>
2012-12-21 17:23:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14465 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/test/java/org/kuali/common/deploy/execute/TomcatController.java
+++ kuali-deploy/src/test/java/org/kuali/common/deploy/execute/TomcatController.java
@@ -1,5 +1,6 @@
 package org.kuali.common.deploy.execute;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,14 +17,14 @@ public class TomcatController {
 	public void test() {
 		try {
 			logger.info("Testing Tomcat Basics");
+			File jdbcDriver = new File("/Users/jeffcaddel/.m2/repository/com/oracle/ojdbc6_g/11.2.0.3/ojdbc6_g-11.2.0.3.jar");
 			String hostname = "env7.ole.kuali.org";
 			List<Executable> executables = new ArrayList<Executable>();
-			executables.add(new TomcatRemoveJdbcDrivers(hostname));
 			executables.add(new TomcatShutdown(hostname));
+			executables.add(new TomcatRemoveJdbcDrivers(hostname));
+			executables.add(new TomcatCopyJdbcDriver(hostname, jdbcDriver));
 			executables.add(new TomcatCleanup(hostname));
 			executables.add(new TomcatStartup(hostname));
-			executables.add(new TomcatShutdown(hostname));
-			executables.add(new TomcatShutdown(hostname));
 			for (Executable executable : executables) {
 				executable.execute();
 			}
