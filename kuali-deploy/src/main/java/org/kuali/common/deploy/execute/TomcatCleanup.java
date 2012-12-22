325ea2b6f70f5de2205f91a64aa6db2f6875eed3
jcaddel <jcaddel@kuali.org>
2012-12-21 19:17:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14482 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/execute/TomcatCleanup.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/execute/TomcatCleanup.java
@@ -15,7 +15,6 @@
  */
 package org.kuali.common.deploy.execute;
 
-import org.kuali.common.util.Mode;
 
 public class TomcatCleanup extends RemoteShellScript {
 
@@ -28,6 +27,5 @@ public class TomcatCleanup extends RemoteShellScript {
 		this.hostname = hostname;
 		this.login = Constants.TOMCAT_USER;
 		this.script = Constants.TOMCAT_CLEANUP;
-		this.nonZeroExitValueMode = Mode.WARN;
 	}
 }
