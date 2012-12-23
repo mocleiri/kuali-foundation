2a1cb287af4d8c0f6be7c1b2e21206da4323ae20
jcaddel <jcaddel@kuali.org>
2012-12-22 23:05:49 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14488 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/context/DeployContext.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/context/DeployContext.java
@@ -2,4 +2,41 @@ package org.kuali.common.deploy.context;
 
 public class DeployContext {
 
+	SecureContext security;
+	ApplicationContext application;
+	AppDynamicsContext appDynamics;
+	TomcatContext tomcat;
+
+	public SecureContext getSecurity() {
+		return security;
+	}
+
+	public void setSecurity(SecureContext security) {
+		this.security = security;
+	}
+
+	public ApplicationContext getApplication() {
+		return application;
+	}
+
+	public void setApplication(ApplicationContext application) {
+		this.application = application;
+	}
+
+	public AppDynamicsContext getAppDynamics() {
+		return appDynamics;
+	}
+
+	public void setAppDynamics(AppDynamicsContext appDynamics) {
+		this.appDynamics = appDynamics;
+	}
+
+	public TomcatContext getTomcat() {
+		return tomcat;
+	}
+
+	public void setTomcat(TomcatContext tomcat) {
+		this.tomcat = tomcat;
+	}
+
 }
