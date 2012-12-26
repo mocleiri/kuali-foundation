c24134fef83c4bb62750ecb7d75e9610d10bf018
jcaddel <jcaddel@kuali.org>
2012-12-26 07:38:13 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14549 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/secure/Group.java
@@ -0,0 +1,24 @@
+package org.kuali.common.util.secure;
+
+public class Group {
+
+	int groupId;
+	String name;
+
+	public int getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ kuali-util/src/main/java/org/kuali/common/util/secure/Owner.java
@@ -0,0 +1,20 @@
+package org.kuali.common.util.secure;
+
+public class Owner {
+
+	String name;
+	int userId;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+}
