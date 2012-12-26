483c3285c3adedb3be5fa2961b79574e2b3bcff0
jcaddel <jcaddel@kuali.org>
2012-12-26 08:19:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14556 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/secure/DefaultSecureService.java
+++ kuali-util/src/main/java/org/kuali/common/util/secure/DefaultSecureService.java
@@ -1,17 +1,38 @@
 package org.kuali.common.util.secure;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
+
+import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 public class DefaultSecureService implements SecureService {
 
+	private static final String SFTP = "sftp";
+
 	@Override
 	public void copyFile(File source, RemoteFile destination) {
+		InputStream in = null;
+		Session session = null;
+		ChannelSftp channel = null;
 		try {
 			JSch jsch = JSchUtils.getDefaultJSch();
+			session = jsch.getSession(destination.getUsername(), destination.getHostname(), 22);
+			session.setConfig(SSHUtils.getDefaultOptions());
+			session.connect();
+			channel = (ChannelSftp) session.openChannel(SFTP);
+			in = new FileInputStream(source);
+			channel.put(in, destination.getFilename());
 		} catch (Exception e) {
 			throw new IllegalStateException("Unexpected error", e);
+		} finally {
+			IOUtils.closeQuietly(in);
+			JSchUtils.disconnectQuietly(channel);
+			JSchUtils.disconnectQuietly(session);
 		}
 	}
 
