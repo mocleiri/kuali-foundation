b954adf8ac6840309414be87581c65af8d83a843
jcaddel <jcaddel@kuali.org>
2012-12-27 17:06:32 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14637 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/secure/SecureChannel.java
+++ kuali-util/src/main/java/org/kuali/common/util/secure/SecureChannel.java
@@ -19,11 +19,11 @@ public interface SecureChannel {
 
 	void copyFile(RemoteFile source, File destination);
 
-	void copyFileToDirectory(RemoteFile source, File destination);
+	void copyLocationToDirectory(String location, RemoteFile destination);
 
 	void copyFileToDirectory(File source, RemoteFile destination);
 
-	void copyLocationToDirectory(String location, RemoteFile destination);
+	void copyFileToDirectory(RemoteFile source, File destination);
 
 	RemoteFile getMetaData(String absolutePath);
 
