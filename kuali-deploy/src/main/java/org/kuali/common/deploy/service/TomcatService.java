1dbecea6c5e773ebf06299010eee3fe43fd12ae2
jcaddel <jcaddel@kuali.org>
2013-01-03 16:08:48 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14796 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/service/TomcatService.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/service/TomcatService.java
@@ -15,15 +15,10 @@
  */
 package org.kuali.common.deploy.service;
 
-import org.kuali.common.deploy.context.SecureContext;
-import org.kuali.common.deploy.context.TomcatContext;
-
 public interface TomcatService {
 
-	void shutdown(SecureContext security, TomcatContext tomcat);
+	void shutdown();
 
-	void cleanup(SecureContext security, TomcatContext tomcat);
-
-	void startup(SecureContext security, TomcatContext tomcat);
+	void startup();
 
 }
