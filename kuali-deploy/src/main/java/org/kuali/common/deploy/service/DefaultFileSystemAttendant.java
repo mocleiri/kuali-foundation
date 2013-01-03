08d90a798baf3ad3fce8f9bdeda6bc21234d161c
jcaddel <jcaddel@kuali.org>
2013-01-03 18:03:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14821 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/service/DefaultFileSystemAttendant.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/service/DefaultFileSystemAttendant.java
@@ -1,10 +1,16 @@
 package org.kuali.common.deploy.service;
 
+import java.util.List;
+
+import org.kuali.common.util.UnixCmds;
 import org.kuali.common.util.secure.SecureChannel;
 
 public class DefaultFileSystemAttendant implements FileSystemAttendant {
 
+	UnixCmds cmds = new UnixCmds();
 	SecureChannel channel;
+	List<String> filesToDelete;
+	List<String> directoriesToDelete;
 
 	@Override
 	public void clean() {
@@ -14,6 +20,38 @@ public class DefaultFileSystemAttendant implements FileSystemAttendant {
 	@Override
 	public void prepare() {
 
+	}
+
+	public UnixCmds getCmds() {
+		return cmds;
+	}
+
+	public void setCmds(UnixCmds cmds) {
+		this.cmds = cmds;
+	}
+
+	public SecureChannel getChannel() {
+		return channel;
+	}
+
+	public void setChannel(SecureChannel channel) {
+		this.channel = channel;
+	}
+
+	public List<String> getFilesToDelete() {
+		return filesToDelete;
+	}
+
+	public void setFilesToDelete(List<String> filesToDelete) {
+		this.filesToDelete = filesToDelete;
+	}
+
+	public List<String> getDirectoriesToDelete() {
+		return directoriesToDelete;
+	}
+
+	public void setDirectoriesToDelete(List<String> directoriesToDelete) {
+		this.directoriesToDelete = directoriesToDelete;
 	}
 
 }
