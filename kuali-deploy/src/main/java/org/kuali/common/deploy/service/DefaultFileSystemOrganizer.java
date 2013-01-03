98358ab6bad89b785fa6dafc1b7df1d967452f90
jcaddel <jcaddel@kuali.org>
2013-01-03 17:49:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14816 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/service/DefaultFileSystemOrganizer.java
@@ -0,0 +1,15 @@
+package org.kuali.common.deploy.service;
+
+public class DefaultFileSystemOrganizer implements FileSystemOrganizer {
+
+	@Override
+    public void clean() {
+
+	}
+
+	@Override
+    public void prepare() {
+
+	}
+
+}
