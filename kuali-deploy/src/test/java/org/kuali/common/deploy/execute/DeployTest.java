80466c64032a46d6e9c78a65ec80e1c6a668483e
jcaddel <jcaddel@kuali.org>
2013-01-03 16:31:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14799 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-deploy/src/test/java/org/kuali/common/deploy/execute/DeployTest.java
@@ -0,0 +1,16 @@
+package org.kuali.common.deploy.execute;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeployTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(DeployTest.class);
+
+	@Test
+	public void test() {
+		logger.info("Hello world");
+	}
+
+}
