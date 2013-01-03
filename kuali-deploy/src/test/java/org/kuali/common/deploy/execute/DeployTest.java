16455b10fa2b0f86cf3aa313abef8bf2e0f8c1e5
jcaddel <jcaddel@kuali.org>
2013-01-03 16:44:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14800 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/test/java/org/kuali/common/deploy/execute/DeployTest.java
+++ kuali-deploy/src/test/java/org/kuali/common/deploy/execute/DeployTest.java
@@ -1,16 +1,23 @@
 package org.kuali.common.deploy.execute;
 
 import org.junit.Test;
+import org.kuali.common.util.service.DefaultSpringService;
+import org.kuali.common.util.service.SpringService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DeployTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(DeployTest.class);
+	SpringService ss = new DefaultSpringService();
 
 	@Test
 	public void test() {
-		logger.info("Hello world");
+		try {
+			logger.info("Hello world");
+			ss.load("classpath:org/kuali/common/deploy/spring/deploy-context.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
