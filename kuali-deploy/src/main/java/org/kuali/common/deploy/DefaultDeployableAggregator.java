a8c6ec198074542731ce35d05f9c8bcedd136532
jcaddel <jcaddel@kuali.org>
2013-01-04 17:20:55 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14876 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/DefaultDeployableAggregator.java
@@ -0,0 +1,77 @@
+package org.kuali.common.deploy;
+
+import java.util.List;
+
+public class DefaultDeployableAggregator implements DeployableAggregator {
+
+	List<Deployable> deployables;
+
+	List<String> jsps;
+	String jspDir;
+	String setenv;
+	String jdbcDriver;
+	String jdbcDriverDir;
+	String war;
+
+	@Override
+	public List<Deployable> aggregate() {
+		return null;
+	}
+
+	public List<Deployable> getDeployables() {
+		return deployables;
+	}
+
+	public void setDeployables(List<Deployable> deployables) {
+		this.deployables = deployables;
+	}
+
+	public List<String> getJsps() {
+		return jsps;
+	}
+
+	public void setJsps(List<String> jsps) {
+		this.jsps = jsps;
+	}
+
+	public String getJspDir() {
+		return jspDir;
+	}
+
+	public void setJspDir(String jspDir) {
+		this.jspDir = jspDir;
+	}
+
+	public String getSetenv() {
+		return setenv;
+	}
+
+	public void setSetenv(String setenv) {
+		this.setenv = setenv;
+	}
+
+	public String getJdbcDriver() {
+		return jdbcDriver;
+	}
+
+	public void setJdbcDriver(String jdbcDriver) {
+		this.jdbcDriver = jdbcDriver;
+	}
+
+	public String getJdbcDriverDir() {
+		return jdbcDriverDir;
+	}
+
+	public void setJdbcDriverDir(String jdbcDriverDir) {
+		this.jdbcDriverDir = jdbcDriverDir;
+	}
+
+	public String getWar() {
+		return war;
+	}
+
+	public void setWar(String war) {
+		this.war = war;
+	}
+
+}
--- kuali-deploy/src/main/java/org/kuali/common/deploy/DeployableAggregator.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/DeployableAggregator.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public interface DeployableAggregator {
 
-	List<Deployable> getDeployables();
+	List<Deployable> aggregate();
 
 }
