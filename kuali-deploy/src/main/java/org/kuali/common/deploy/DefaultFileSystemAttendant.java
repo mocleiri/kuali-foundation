317c55aea5ab3074c7c4160cfa2e3b93a95831fb
jcaddel <jcaddel@kuali.org>
2013-01-04 00:05:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14854 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/DefaultFileSystemAttendant.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/DefaultFileSystemAttendant.java
@@ -61,8 +61,8 @@ public class DefaultFileSystemAttendant implements FileSystemAttendant {
 			if (deployable.isFilter()) {
 				String originalContent = LocationUtils.toString(location);
 				String resolvedContent = helper.replacePlaceholders(originalContent, properties);
-				logger.info("Filtering [{}] with {} properties", location, properties.size());
-				logger.info("Pushing filtered content to [{}]", destination.getAbsolutePath());
+				Object[] args = { properties.size(), location, destination.getAbsolutePath() };
+				logger.info("Using {} properties to filter [{}] -> [{}]", args);
 				channel.copyStringToFile(resolvedContent, destination);
 			} else {
 				logger.info("[{}] -> [{}]", location, destination.getAbsolutePath());
