3367cb44a6554ce7235c964b6617b289563f2fcb
jcaddel <jcaddel@kuali.org>
2013-01-07 17:58:26 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14973 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/DefaultDeployService.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/DefaultDeployService.java
@@ -29,7 +29,7 @@ public class DefaultDeployService implements DeployService {
 			if (context.getJdbcDriver() != null) {
 				logger.info("Jdbc Driver - {}", RepositoryUtils.toString(context.getJdbcDriver()));
 			}
-			logger.info("Config - [{}] -> [{}]", context.getConfig().getLocal(), context.getConfig().getRemote());
+			logger.info("Config - {}", context.getConfig().getLocal());
 			logger.info("----------------------------------------------------");
 			channel.open();
 			controller.stop();
