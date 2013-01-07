669eddb6b38563c2af0f7404ab1b6ce053d88f36
jcaddel <jcaddel@kuali.org>
2013-01-07 16:07:31 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@14958 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/DeployContext.java
@@ -0,0 +1,35 @@
+package org.kuali.common.deploy;
+
+import org.kuali.common.util.Artifact;
+
+public class DeployContext {
+
+	String username;
+	String hostname;
+	Artifact artifact;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public Artifact getArtifact() {
+		return artifact;
+	}
+
+	public void setArtifact(Artifact artifact) {
+		this.artifact = artifact;
+	}
+
+}
