a11dd9183094c25632926aa9eb8dca9f82843561
jcaddel <jcaddel@kuali.org>
2013-01-13 11:49:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15103 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/manifests/simple/packages.pp
+++ kuali-puppet/src/main/resources/manifests/simple/packages.pp
@@ -1,7 +1,13 @@
 package { 
-  "man": ensure => present;
-  "zip": ensure => present;
-  "unzip": ensure => present;
-  "java-1.7.0-openjdk": ensure => present;
-  "java-1.6.0-openjdk": ensure => present;
+  "man": 
+    ensure => present;
+  "zip": 
+    ensure => present;
+  "unzip": 
+    ensure => present;
+  "java-1.7.0-openjdk": 
+    ensure => present,
+    before => Package["java-1.6.0-openjdk"];
+  "java-1.6.0-openjdk": 
+    ensure => present;
 }
