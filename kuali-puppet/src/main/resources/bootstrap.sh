95f5de397b8eede8a35d6b7d93721088a326fbe4
jcaddel <jcaddel@kuali.org>
2013-01-13 12:06:50 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15106 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/bootstrap.sh
+++ kuali-puppet/src/main/resources/bootstrap.sh
@@ -10,9 +10,7 @@ PUPPET_VERSION=2.7.18
 RUBY_PACKAGE=$RUBY-$RUBY_VERSION
 PUPPET_PACKAGE=$PUPPET-$PUPPET_VERSION
 
-yum -y erase $PUPPET
-yum -y erase $RUBY
-yum -y install $RUBY_PACKAGE
-yum -y install $PUPPET_PACKAGE
+yum -y erase $PUPPET $RUBY
+yum -y install $RUBY_PACKAGE $PUPPET_PACKAGE
 
 echo $(date)
