59e80fb10eaa19991c5ce1f49f73eadc1fac5e9e
jcaddel <jcaddel@kuali.org>
2013-01-12 22:11:22 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15093 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/manifests/simple/hosts.pp
+++ kuali-puppet/src/main/resources/manifests/simple/hosts.pp
@@ -9,5 +9,5 @@ file {'/tmp/info.txt':
   ensure  => file,
   path    => '/tmp/info.txt',
   mode    => 0644,
-  content => "Welcome to ${hostname},\na ${operatingsystem} island in the sea of ${domain}.\n",
+  content => "AMI: $ec2_ami_id\nType:$ec2_instance_type\nPublic Hostname:$ec2_public_hostname",
 }
