713703b06a3a8766635882aaaf4c99b4fa84d60d
jcaddel <jcaddel@kuali.org>
2013-01-13 11:55:38 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15104 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/manifests/simple/packages.pp
+++ kuali-puppet/src/main/resources/manifests/simple/packages.pp
@@ -3,11 +3,17 @@ package {
     ensure => present;
   "zip": 
     ensure => present;
+  "wget": 
+    ensure => present;
+  "curl": 
+    ensure => present;
   "unzip": 
     ensure => present;
+  "rsync": 
+    ensure => present;
   "java-1.7.0-openjdk": 
-    ensure => present,
-    before => Package["java-1.6.0-openjdk"];
-  "java-1.6.0-openjdk": 
     ensure => present;
 }
+
+
+ Of course './jdk-6u21-linux-x64-rpm.bin -x ' will extract the RPM ... 
