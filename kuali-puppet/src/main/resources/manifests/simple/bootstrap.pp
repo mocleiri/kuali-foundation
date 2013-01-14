dfab899afc12f829b0a911af337cd4820dff2ce9
jcaddel <jcaddel@kuali.org>
2013-01-14 17:25:03 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15145 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/manifests/simple/bootstrap.pp
+++ kuali-puppet/src/main/resources/manifests/simple/bootstrap.pp
@@ -1,15 +1,25 @@
+$repo = "http://maven.kuali.org/external"
 $groupid = "com/oracle"
 $artifactid = "ojdbc14"
 $version = "10.2.0.3.0"
 $packaging = "pom"
-$repo = "https://maven.kuali.org/external"
-$repopath = "${groupid}/${artifactid}/${version}/${artifactid}-${version}";
+$repopath = "${groupid}/${artifactid}/${version}/"
+$filename = "${artifactid}-${version}.${packaging}"
 $userhome = "/root"
-$filename = "${userhome}/.m2/repository/${repopath}"
-$url = "${repository}/${repopath}"
+$localrepo = "${userhome}/.m2/repository"
+$localdir = "${localrepo}/${repopath}"
+$localfile = "${localdir}/${filename}"
+$url = "${repo}/${repopath}/${filename}"
 
+
+file { $localdir:
+  ensure  => directory,
+  recurse => true,
+  force   => true,
+}
 
 exec { "fetch ${filename}":
   path => ["/bin", "/usr/bin", "sbin", "/usr/sbin"],
   command => "curl -L -o ${filename} ${url}",
+  require => File[$localdir],
 }
