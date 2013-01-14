73a0b0d0c74158d37521acaccfe0f1fdf6078332
jcaddel <jcaddel@kuali.org>
2013-01-13 21:57:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15125 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/manifests/simple/jdk6.pp
+++ kuali-puppet/src/main/resources/manifests/simple/jdk6.pp
@@ -1,8 +1,8 @@
 $jdkpackage = "jdk"
 $installdir = "/usr/java"
-$versionsuffix = "-fcs"
 $jdk6version = "1.6.0_38"
 $jdk7version = "1.7.0_11"
+$versionsuffix = "-fcs"
 
 $version = "${jdk6version}-${versionsuffix}"
 $removedir = "${installdir}/${jdkpackage}-${jdk7version}"
