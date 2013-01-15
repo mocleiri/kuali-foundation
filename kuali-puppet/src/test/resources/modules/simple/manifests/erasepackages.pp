54f525257689bc5c37fc7e2e674f2871a7bd4f57
jcaddel <jcaddel@kuali.org>
2013-01-15 18:57:45 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15230 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/test/resources/modules/simple/manifests/erasepackages.pp
+++ kuali-puppet/src/test/resources/modules/simple/manifests/erasepackages.pp
@@ -39,41 +39,33 @@ package { 'rubygems':
   require => Package['fog', 'ruby-nokogiri', 'ruby-devel'], 
 }
 
-package { 'rsync': 
-  ensure => absent,
-}
-
-package { 'openssh-clients': 
-  ensure => absent,
-}
-
-package { 'perl-Git':
-  ensure => absent,
-  before => Package['rsync','openssh-clients'], 
-}
-
-package { 'git':
-  ensure => absent,
-  require => Package['perl-Git'], 
-}
-
 #    
 # rsync and openssh-clients are both required by git
 #
-#  'rsync': 
-#    ensure => absent;
-#  'openssh-clients': 
-#    ensure => absent;
+#package { 'rsync': 
+#  ensure => absent,
+#}
 #
-# The Amazon git rpm's don't play well with puppet
-# Attempting to uninstall git results in an error related
-# to not being able to uninstall it since perl-Git requires
-# it.  But you can't uninstall perl-Git first, because it
-# depends on git.  Seems like a catch-22.
+#package { 'openssh-clients': 
+#  ensure => absent,
+#}
 #
-#  'git': 
-#    ensure => absent,
-#    before => Package['rsync','openssh-clients'];
-#  'perl-Git': 
-#    ensure => absent,
-#    before => Package['git'];
+#package { 'perl-Git':
+#  ensure => absent,
+#  before => Package['rsync','openssh-clients'], 
+#}
+#
+#package { 'git':
+#  ensure => absent,
+#  require => Package['perl-Git'], 
+#}
+#
+# The Amazon rpm's (version 1.7.4.5-1.21.amzn1) for git and perl-Git are kinda jacked.
+#
+# They both list the other as a dependency.
+#
+# If you yum install perl-Git, both are installed
+# If you yum install git both are installed
+#
+# Same thing with removing
+# 
