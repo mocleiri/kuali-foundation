7d755be55493e04fdd943475702397ff26d9d490
jcaddel <jcaddel@kuali.org>
2013-01-15 18:20:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15215 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/test/resources/modules/simple/manifests/packages.pp
+++ kuali-puppet/src/test/resources/modules/simple/manifests/packages.pp
@@ -18,16 +18,16 @@ package {
 }
 
 package { 'rubygems':
-  ensure => '1.8.11',
+  ensure => '1.8.11-3.1.amzn1',
 }
 
 package { 'ruby-devel': 
-  ensure => '1.8.7.371',
+  ensure => '1.8.7.371-1.20.amzn1',
   require => Package['rubygems'],
 }
 
 package { 'ruby-nokogiri': 
-  ensure => '1.5.2',
+  ensure => '1.5.2-1.6.amzn1',
   require => Package['ruby-devel'],
 }
 
