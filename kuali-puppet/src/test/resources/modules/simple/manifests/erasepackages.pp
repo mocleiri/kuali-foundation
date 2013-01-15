a16cb4b845d458a6652456b6f19e189c23e99ab4
jcaddel <jcaddel@kuali.org>
2013-01-15 13:43:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15181 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-puppet/src/test/resources/modules/simple/manifests/erasepackages.pp
@@ -0,0 +1,20 @@
+package { 
+  "man": 
+    ensure => absent;
+  "zip": 
+    ensure => absent;
+  "unzip": 
+    ensure => absent;
+  "wget": 
+    ensure => absent;
+  "rsync": 
+    ensure => absent;
+  "openssh-clients": 
+    ensure => absent;
+  "rubygems": 
+    ensure => absent;
+  "subversion": 
+    ensure => absent;
+  "git": 
+    ensure => absent;
+}
