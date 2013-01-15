5d0b8abced8a7e31bf4b4b226139d79b5994a708
jcaddel <jcaddel@kuali.org>
2013-01-15 14:02:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15188 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/test/resources/modules/simple/manifests/erasepackages.pp
+++ kuali-puppet/src/test/resources/modules/simple/manifests/erasepackages.pp
@@ -15,10 +15,17 @@ package {
     ensure => absent;
   "subversion": 
     ensure => absent;
-  "git": 
-    ensure => absent,
-    before => Package["rsync","openssh-clients"];
-  "perl-Git": 
-    ensure => absent,
-    before => Package["git"];
+#
+# The Amazon git rpm's don't play well with puppet
+# Attempting to uninstall git results in an error related
+# to not being able to uninstall it since perl-Git requires
+# it.  But you can't uninstall perl-Git first, because it
+# depends on git.  Seems like a catch-22.
+#
+#  "git": 
+#    ensure => absent,
+#    before => Package["rsync","openssh-clients"];
+#  "perl-Git": 
+#    ensure => absent,
+#    before => Package["git"];
 }
