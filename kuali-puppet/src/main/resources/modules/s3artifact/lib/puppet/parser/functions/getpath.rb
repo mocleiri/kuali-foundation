4aa3f0277171bfa49de850203db59fa3d7c513c8
jcaddel <jcaddel@kuali.org>
2013-01-16 18:31:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15407 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-puppet/src/main/resources/modules/s3artifact/lib/puppet/parser/functions/getpath.rb
@@ -0,0 +1,9 @@
+module Puppet::Parser::Functions
+  newfunction(:getpath, :type => :rvalue, :doc => <<-EOS
+    Replace any dots in the string passed in with underscores
+    EOS
+             ) do |args|
+    mystring = args[0] # the string to convert
+    return mystring
+  end
+end
