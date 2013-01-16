783d4d7bbab6bae90699a01295827351b059f246
jcaddel <jcaddel@kuali.org>
2013-01-16 02:24:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15283 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/lib/puppet/parser/functions/s3curlcmd.rb
+++ kuali-puppet/src/main/resources/modules/maven/lib/puppet/parser/functions/s3curlcmd.rb
@@ -4,21 +4,18 @@ require 'time'
 
 module Puppet::Parser::Functions
   newfunction(:s3curlcmd, :type => :rvalue, :doc => <<-EOS
-    Generates a curl command including a presigned url for the s3 object specified
-    by the bucket and key and resulting filename.
+    Generates a curl command containing a presigned url for the s3 object specified
+    by the bucket and key.  The contents of the url are downloaded to filename.
     EOS
              ) do |args|
-    bucket   = args[0]
-    key      = args[1]
-    filename = args[2]
-    expires  = args[3] # in seconds from Time.now.to_i
-    #headers = { }
+    bucket   = args[0] # S3 bucket name
+    key      = args[1] # key into that bucket
+    filename = args[2] # local file where the file will be downloaded
+    expires  = args[3] # in seconds from the current time
     Fog.credentials_path = '/etc/puppet/fog_cred'
     s3 = Fog::Storage.new(:provider => 'AWS')
     s3bucket = s3.directories.get(bucket)
     url = s3bucket.files.get_https_url(key, Time.parse(DateTime.now.to_s).to_i + expires.to_i)
-    #heads = headers.map{|k,v| "-H '#{k}: #{v}'"}.join(' ')
-    #cmd = "curl #{heads} --create-dirs --retry 3 --progress-bar --fail -output #{filename} '#{url}'"
     cmd = "curl --create-dirs --retry 3 --silent --fail -output #{filename} '#{url}'"
     return cmd
   end
