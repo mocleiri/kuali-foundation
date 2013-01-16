f92dfbe1534b407d4a8ca3da8ef9ce1d97c9390b
jcaddel <jcaddel@kuali.org>
2013-01-16 05:27:22 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15304 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
@@ -13,11 +13,12 @@ class maven {
   $md5file = "${filename}.md5"
   $md5md5 = s3md5($bucket,$md5key)
   $md5unless = "echo \"${md5md5}  ${md5file}\" | md5sum -c --status"
+  $md5exec = "s3curl(${bucket}, ${md5key}, ${md5file}, ${expires})"
   
   notify {$md5unless:}
   
-  $md5exec = "s3curl(${bucket}, ${md5key}, ${md5file}, ${expires})"
   $objectexec = "s3curl(${bucket}, ${key}, ${filename}, ${expires})"
+  $objectunless = "grep `md5sum ${filename} | cut -c1-32` ${md5file}"
   
   exec { $md5exec:
     command => s3curl($bucket, $md5key, $md5file, $expires),
@@ -26,6 +27,7 @@ class maven {
   
   exec { $objectexec:
     command => s3curl($bucket, $key, $filename, $expires),
+    unless => $objectunless
     subscribe => Exec[$md5exec],
   }
 
