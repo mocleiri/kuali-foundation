fdc3fe59693434eade3e34db711cba09d4af7b06
jcaddel <jcaddel@kuali.org>
2013-01-16 05:43:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15308 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
@@ -15,8 +15,6 @@ class maven {
   $md5unless = "echo \"${md5md5}  ${md5file}\" | md5sum -c --status"
   $md5exec = "s3curl(${bucket}, ${md5key}, ${md5file}, ${expires})"
   
-#  notify {$md5unless:}
-  
   $objectexec = "s3curl(${bucket}, ${key}, ${filename}, ${expires})"
   $objectunless = "grep `md5sum ${filename} | cut -c1-32` ${md5file}"
   
