be54755568d1f71bfb28c7401ceec0d81c63c954
jcaddel <jcaddel@kuali.org>
2013-01-16 01:05:30 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15258 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
@@ -4,7 +4,7 @@ Exec { path => $exec_path }
 class maven {
   
   $bucket = "maven.kuali.org"
-  $key = "/private/com/oracle/jdk7/1.7.0-u07/jdk7-1.7.0-u07-linux-x64.zip"
+  $key = "private/com/oracle/jdk7/1.7.0-u07/jdk7-1.7.0-u07-linux-x64.zip"
   $filename = "/tmp/jdk7-1.7.0-u07-linux-x64.zip"
   $expires = 30
   
