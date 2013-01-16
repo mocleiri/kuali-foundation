b70937222d2e2d483d357e6bf1ff92c63b92a36a
jcaddel <jcaddel@kuali.org>
2013-01-16 15:35:42 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15357 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/s3artifact/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/s3artifact/manifests/init.pp
@@ -1,14 +1,2 @@
-class mvn {
-
-  class {'mvn::helloworld':}
-
-#  class { 'mvn::s3artifact' :
-#    bucket      => 'maven.kuali.org,
-#    prefix      => 'private',
-#    group_id    => 'com.oracle',
-#    artifact_id => 'ojdbc14',
-#    version     => '11.0.2.0',
-#  }
+define s3artifact (
 }
-
-class {'mvn':}
