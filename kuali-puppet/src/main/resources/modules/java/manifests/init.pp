7b67dd70a8e5ca947c8f6d568cefcf80e8a3aff1
jcaddel <jcaddel@kuali.org>
2013-01-16 18:12:26 -0500

    tweaking
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15399 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/java/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/java/manifests/init.pp
@@ -8,11 +8,11 @@ $classifier = "linux-x64"
 
 $jdk7artifactid = "jdk7"
 $jdk7version = "1.7.0-u07"
-$jdk7 = "${group_id}:${jdk7artifactid}:${jdk7version}:${packaging}:${classifier}"
+$jdk7 = "${groupid}:${jdk7artifactid}:${jdk7version}:${packaging}:${classifier}"
 
 $jdk6artifactid = "jdk6"
-$jdk6version = "1.6.0-u07"
-$jdk6 = "${group_id}:${jdk6artifactid}:${jdk6version}:${packaging}:${classifier}"
+$jdk6version = "1.6.0-u34"
+$jdk6 = "${groupid}:${jdk6artifactid}:${jdk6version}:${packaging}:${classifier}"
 
 s3artifact { $jdk6:
   localrepo   => $localrepo,
@@ -20,9 +20,9 @@ s3artifact { $jdk6:
   prefix      => $prefix,
   groupid     => $groupid,
   artifactid  => $jdk6artifactid,
-  packaging   => $packaging,
   version     => $jdk6version,
   classifier  => $classifier,
+  packaging   => $packaging,
 }
 
 s3artifact { $jdk7:
@@ -31,7 +31,7 @@ s3artifact { $jdk7:
   prefix      => $prefix,
   groupid     => $groupid,
   artifactid  => $jdk7artifactid,
-  packaging   => $packaging,
   version     => $jdk7version,
   classifier  => $classifier,
+  packaging   => $packaging,
 }
