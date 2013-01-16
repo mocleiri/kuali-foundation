54850cf6c7bec419806f838706bcc2932cca53d8
jcaddel <jcaddel@kuali.org>
2013-01-16 14:38:58 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15343 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/s3artifact.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/s3artifact.pp
@@ -1,6 +1,6 @@
 class maven::s3artifact ($group_id, $artifact_id, $version, $packaging = 'jar', $classifier = undef) {
 
   $gav = "${group_id}:${artifact_id}:${version}:${packaging}"
-  notify {$gav}
+  notify {$gav:}
   
 }
