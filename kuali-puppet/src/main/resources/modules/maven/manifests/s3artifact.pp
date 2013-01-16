ab87a97c44f65dbbea5814dcf716078d14a4b966
jcaddel <jcaddel@kuali.org>
2013-01-16 13:19:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15325 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/s3artifact.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/s3artifact.pp
@@ -1,6 +1,3 @@
-$exec_path = ["/bin", "/usr/bin", "/sbin", "/usr/sbin", "/usr/local/sbin", "/usr/local/bin"]
-Exec { path => $exec_path }
-
 class maven::s3artifact($bucket, $directory, $group_id, $artifact_id, $version, $packaging => 'jar', $classifier) {
 
   $gav = "${group_id}:${artifact_id}:${version}:${packaging}:${classifier}"
