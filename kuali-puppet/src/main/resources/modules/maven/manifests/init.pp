f3d6be1558f6c56e01efd49eee68e288efe331f6
jcaddel <jcaddel@kuali.org>
2013-01-16 14:50:37 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15348 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
@@ -1,8 +1,10 @@
 class maven {
   class { 'maven::s3artifact' :
-    group_id => 'com.oracle',
+    bucket      => 'maven.kuali.org,
+    prefix      => 'private',
+    group_id    => 'com.oracle',
     artifact_id => 'ojdbc14',
-    version => '11.0.2.0',
+    version     => '11.0.2.0',
   }
 }
 
