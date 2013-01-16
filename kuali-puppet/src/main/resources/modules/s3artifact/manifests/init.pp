2e48af5ec12ea62f61ebd18ad0c4768d4abfb437
jcaddel <jcaddel@kuali.org>
2013-01-16 17:13:58 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15378 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/s3artifact/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/s3artifact/manifests/init.pp
@@ -12,12 +12,12 @@ define s3artifact ($localrepo
   $id_fragment = "${group_id}:${artifact_id}:${version}:${packaging}"
   $filename_fragment = "${artifact_id}-${version}"
   
-  if ($classifier != undef) {
-    $id = "${id_fragment}:${classifier}"
-    $filename = "${filename_fragment}-${classifier}.${packaging}"
-  } else {
+  if ($classifier == undef) {
     $id = $id_fragment
     $filename = "${filename_fragment}.${packaging}"
+  } else {
+    $id = "${id_fragment}:${classifier}"
+    $filename = "${filename_fragment}-${classifier}.${packaging}"
   }
   
   $repo_path = "${group_id}/${artifact_id}/${version}"
