f3c1643d6cb52d1d30d192f729150c4378d25c5f
jcaddel <jcaddel@kuali.org>
2013-01-15 23:49:01 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15248 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
@@ -1,7 +1,15 @@
 class maven {
 
   notify {'Hello world':}
+  
+  $bucket = "maven.kuali.org"
+  $title = "foo"
+  $name = "bar"
+  $expires = 30
+  
+  $curl = s3getcurl($bucket, $title, $name, $expires),
 
+  notify {$curl:}
 }
 
 class {'maven': }
