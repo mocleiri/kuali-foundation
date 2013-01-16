daacc51b0505b34eef8a10789ce724462440554a
jcaddel <jcaddel@kuali.org>
2013-01-16 13:21:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15327 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
@@ -1,6 +1,6 @@
 class maven {
 
-  maven::s3artifact { "jdbcdriver":
+  class maven::s3artifact { "jdbcdriver":
     group_id => "com.oracle",
     artifact_id => "ojdbc14",
     version => "11.0.2.0",
