19bbdd10da7e50e6f27a7000433a6c90b0a8a753
jcaddel <jcaddel@kuali.org>
2013-01-16 18:21:15 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15404 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/java/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/java/manifests/init.pp
@@ -5,11 +5,16 @@ $bucket = "maven.kuali.org"
 $prefix = "private"
 $packaging = "zip"
 $classifier = "linux-x64"
-
+$jdk7artifactid = "jdk7"
 $jdk6artifactid = "jdk6"
+
 $jdk6version = "1.6.0-u34"
+$jdk7version = "1.7.0-u07"
+
 $jdk6packaging = "bin"
+
 $jdk6 = "${groupid}:${jdk6artifactid}:${jdk6version}:${jdk6packaging}:${classifier}"
+$jdk7 = "${groupid}:${jdk7artifactid}:${jdk7version}:${packaging}:${classifier}"
 
 s3artifact { $jdk6:
   localrepo   => $localrepo,
@@ -22,9 +27,6 @@ s3artifact { $jdk6:
   packaging   => $jdk6packaging,
 }
 
-$jdk7artifactid = "jdk7"
-$jdk7version = "1.7.0-u07"
-$jdk7 = "${groupid}:${jdk7artifactid}:${jdk7version}:${packaging}:${classifier}"
 
 s3artifact { $jdk7:
   localrepo   => $localrepo,
