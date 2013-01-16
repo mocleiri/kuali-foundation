cc0804e487f68b1f4181c32bf4d99b6a1f76f394
jcaddel <jcaddel@kuali.org>
2013-01-16 02:54:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15292 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
@@ -8,13 +8,18 @@ class maven {
   $filename = "/tmp/jdk7-1.7.0-u07-linux-x64.zip"
   $expires = 30
   
+  $s3curlmd5 = s3curlcmd($bucket, "${key}.md5", "${filename}.md5", $expires)
   $s3curlcmd = s3curlcmd($bucket, $key, $filename, $expires)
-
-  exec { $s3curlcmd:
-    command => $s3curlcmd,
-    logoutput => true,
+  
+  exec { $s3curlmd5:
+    command => $s3curlmd5,
   }
   
+  exec { $s3curlcmd:
+    command => $s3curlcmd,
+    require => Exec[$s3curlmd5],
+  }
+
 }
 
 class {'maven': }
