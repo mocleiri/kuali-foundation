daede96840a484f8e41b9ed5da5228f645905185
jcaddel <jcaddel@kuali.org>
2013-01-16 16:02:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15374 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/java/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/java/manifests/init.pp
@@ -3,7 +3,7 @@ s3artifact { "java":
   localrepo   => '/root/.m2/repository',
   bucket      => 'maven.kuali.org',
   prefix      => 'private',
-  group_id    => 'com.oracle',
+  group_id    => 'com/oracle',
   artifact_id => 'jdk7',
   version     => '1.7.0-u07',
   classifier  => 'linux-x64',
