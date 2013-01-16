b379da469360e2c37297ad13c1a114a56d93696d
jcaddel <jcaddel@kuali.org>
2013-01-16 15:59:58 -0500

    tweaking
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15373 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/s3artifact/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/s3artifact/manifests/init.pp
@@ -1,4 +1,14 @@
-define s3artifact ($localrepo, $bucket, $prefix = undef, $group_id, $artifact_id, $version, $packaging = 'jar', $classifier = undef) {
+define s3artifact ($localrepo
+  , $bucket
+  , $prefix = undef
+  , $group_id
+  , $artifact_id
+  , $version
+  , $packaging = 'jar'
+  , $classifier = undef
+  , $ensure = 'present'
+) {
+
   notify {$bucket:}
   if ($prefix != undef) {
     notify {$prefix:}
