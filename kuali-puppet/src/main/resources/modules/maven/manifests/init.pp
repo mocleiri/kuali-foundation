a7e46593f2b927a916e9f1315bbd04ce875bc1e0
jcaddel <jcaddel@kuali.org>
2013-01-16 01:45:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15270 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/maven/manifests/init.pp
@@ -9,10 +9,15 @@ class maven {
   $expires = 30
   
   $s3curlcmd = s3curlcmd($bucket, $key, $filename, $expires)
+  $s3md5sum = s3md5sum($bucket, $key)
 
   exec { $curl:
     command => $s3curlcmd,
     logoutput => true,
+  }
+  
+  file { "${filename}.md5":
+    
   }
 }
 
