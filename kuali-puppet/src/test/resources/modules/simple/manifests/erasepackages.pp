5c4ae0a8049d881738320a1a370c041a4b1c997a
jcaddel <jcaddel@kuali.org>
2013-01-15 19:07:08 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15232 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/test/resources/modules/simple/manifests/erasepackages.pp
+++ kuali-puppet/src/test/resources/modules/simple/manifests/erasepackages.pp
@@ -1,14 +1,14 @@
 package { 'man': 
-  ensure => purged,
+  ensure => absent,
 }
 package { 'zip': 
-  ensure => purged,
+  ensure => absent,
 }
 package { 'unzip': 
-  ensure => purged,
+  ensure => absent,
 }
 package { 'wget': 
-  ensure => purged,
+  ensure => absent,
 }
 
 # Leave svn alone so we don't have to keep re-installing it
@@ -17,55 +17,23 @@ package { 'wget':
 #}
 
 package { 'fog':
-  ensure => purged,
+  ensure => absent,
   provider => 'gem',
 }
 
 package { 'ruby-devel': 
-  ensure => purged,
-}
-
-package { 'rubygem-nokogiri': 
   ensure => absent,
 }
 
 package { 'ruby-nokogiri': 
-  ensure => absent,
-  before => Package['rubygem-nokogiri'],
+  ensure => purged,
 }
 
 package { 'rubygems': 
   ensure => purged,
-  require => Package['fog', 'ruby-nokogiri', 'ruby-devel'], 
+  require => Package['fog'], 
 }
 
-#    
-# rsync and openssh-clients are both required by git
-#
-#package { 'rsync': 
-#  ensure => absent,
-#}
-#
-#package { 'openssh-clients': 
-#  ensure => absent,
-#}
-#
-#package { 'perl-Git':
-#  ensure => absent,
-#  before => Package['rsync','openssh-clients'], 
-#}
-#
-#package { 'git':
-#  ensure => absent,
-#  require => Package['perl-Git'], 
-#}
-#
-# The Amazon rpm's (version 1.7.4.5-1.21.amzn1) for git and perl-Git are kinda jacked.
-#
-# They both list the other as a dependency.
-#
-# If you yum install perl-Git, both are installed
-# If you yum install git both are installed
-#
-# Same thing with removing
-# 
+package { 'git':
+  ensure => purged,
+}
