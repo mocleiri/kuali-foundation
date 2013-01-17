1b2f6bcf31ca1244cbf1ebcd8ab0dabea0a3193d
jcaddel <jcaddel@kuali.org>
2013-01-17 12:01:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15475 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
@@ -2,14 +2,17 @@ class kuali {
 
   notify {"Hello Kuali World":}
   
+  $jdk6version = "1.6.0-u38"
+  $jdk7version = "1.7.0-u11"
+  
   jdk { 'jdk6':
     level   => '6',
-    version => 'foo',
+    version => $jdk6version,
   }
 
   jdk { 'jdk7':
     level   => '7',
-    version => 'foo',
+    version => $jdk7version,
   }
 
 }
