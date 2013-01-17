726f60f2cc8ed46fa74992130fae1238290221e2
jcaddel <jcaddel@kuali.org>
2013-01-17 14:31:55 -0500

    tweaking
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15495 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/jdk/manifests/install.pp
+++ kuali-puppet/src/main/resources/modules/jdk/manifests/install.pp
@@ -1,8 +1,30 @@
 define jdk::install ($localrepo
-  , $level
+  , $groupid
+  , $artifactid
   , $version
+  , $packaging
+  , $classifier
+  , $level
 ) {
 
-  notify {"Install jdk-${version}":}
+    # The name of the file eg "commons-io-1.3.2.jar"
+  if ($classifier == undef) {
+    $filename = "${artifactid}-${version}.${packaging}"
+  } else {
+    $filename = "${artifactid}-${version}-${classifier}.${packaging}"
+  }
   
+  # Convert "org.apache.commons" into "org/apache/commons"
+  $groupidpath = getpath($groupid)
+  
+  # The relative path to the directory containing the file in a Maven repository
+  # This value is identical for both the S3 Maven repository and the Maven repository on the local file system
+  # eg "org/apache/commons/commons-io/1.3.2"
+  $path = "${groupidpath}/${artifactid}/${version}"
+
+  # Fully qualified filename that the S3 object will be downloaded to
+  # Any non-existing parent directories are automatically created by cURL as needed
+  $file = "${localrepo}/${path}/${filename}"
+	
+  notify {$file:}  
 }
