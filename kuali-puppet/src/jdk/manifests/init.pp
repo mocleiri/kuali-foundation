969713fcccecaa3c036d691e2ece3998bff9cc50
jcaddel <jcaddel@kuali.org>
2013-01-17 17:24:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15525 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-puppet/src/jdk/manifests/init.pp
@@ -0,0 +1,36 @@
+define jdk ($localrepo = "/root/.m2/repository"
+  , $bucket = "maven.kuali.org"
+  , $prefix = "private"
+  , $groupid = "com.oracle"
+  , $artifactid = "jdk"
+  , $version
+  , $packaging = "zip"
+  , $classifier = "linux-x64"
+  , $level
+) {
+
+  $artifact = "jdk-${version}"
+  
+  s3artifact { $artifact:
+    localrepo  => $localrepo,
+    bucket     => $bucket,
+    prefix     => $prefix,
+    groupid    => $groupid,
+    artifactid => $artifactid,
+    version    => $version,
+    packaging  => $packaging,
+    classifier => $classifier,
+  }
+
+  jdk::install { $artifact:
+    localrepo   => $localrepo,
+    groupid     => $groupid,
+    artifactid  => $artifactid,
+    version     => $version,
+    packaging   => $packaging,
+    classifier  => $classifier,
+    level       => $level,
+    require     => S3artifact[$artifact],
+  }
+  
+}
