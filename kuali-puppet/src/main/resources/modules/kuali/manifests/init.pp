a31511283c7269d4883f9834f5cb63bf0aec88a2
jcaddel <jcaddel@kuali.org>
2013-01-17 02:04:13 -0500

    tweaking
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15465 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
@@ -3,7 +3,12 @@ class kuali {
   notify {"Hello Kuali World":}
   
   class { 'jdk':
-    level   => '8',
+    level   => '6',
+    version => 'foo',
+  }
+
+  class { 'jdk':
+    level   => '7',
     version => 'foo',
   }
 
