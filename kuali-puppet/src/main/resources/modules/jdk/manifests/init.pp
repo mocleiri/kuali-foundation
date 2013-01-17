54fdb35348405af659d0f4ae4ded684b599f0933
jcaddel <jcaddel@kuali.org>
2013-01-17 13:24:08 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15479 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/jdk/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/jdk/manifests/init.pp
@@ -1,13 +1,16 @@
 define jdk ($level, $version) {
-  
-) {
 
   $artifact = "jdk-${version}"
+  notify {$artifact:}
   
   jdk::download {$artifact:
+    level   => $level,
+    version => $version,
   }
 
   jdk::install {$artifact:
+    level   => $level,
+    version => $version,
     require => Jdk::Download[$artifact]
   }
 }
