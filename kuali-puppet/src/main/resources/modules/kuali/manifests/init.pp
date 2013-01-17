f13100b6bbcda6cc7cf73f1a28b8a34678be64f9
jcaddel <jcaddel@kuali.org>
2013-01-17 01:32:50 -0500

    tweaking
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15444 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
@@ -1,26 +1,7 @@
-class java(
-  $distribution = 'jdk',
-  $version      = 'present'
-) {
+class kuali {
 
-  anchor { 'java::begin': }
-  anchor { 'java::end': }
+  notify {"Hello World":}
 
-  case $::osfamily {
-    'Linux': {
-      if $::hardwaremodel == 'x86_64' {
-        class { 'java::package_redhat':
-          version      => $version,
-          distribution => $distribution,
-          require      => Anchor['java::begin'],
-          before       => Anchor['java::end'],
-        }
-      } else {
-        fail("osfamily ${::osfamily} with hardwaremodel ${::hardwaremodel} is not supported")
-      }
-    }
-    default: {
-      fail("osfamily ${::osfamily} is not supported")
-    }
-  }
 }
+
+class {'kuali'}
