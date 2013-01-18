f582e8d3799af3692b8bb18d79d189f4887d2b26
jcaddel <jcaddel@kuali.org>
2013-01-18 03:05:21 -0500

    tweaking
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15571 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
@@ -1,15 +1,17 @@
 class kuali {
 
+  include stdlib
+
   # File system paths to check for exec calls
   # Do this once so each exec command inherits the paths
   $exec_path = ["/bin", "/usr/bin", "/sbin", "/usr/sbin", "/usr/local/sbin", "/usr/local/bin"]
   Exec { path => $exec_path }
   
+  class { 'kuali::packages': stage => "setup" }
+
   $local_repo = "/root/.m2/repository"
   $bucket = "maven.kuali.org"
   
-  class { 'kuali::packages': }
-
   class { 'java':
     jdk6_version => "1.6.0-u38",
     jdk7_version => "1.7.0-u11",
@@ -20,6 +22,7 @@ class kuali {
     artifact_id  => "jdk",
     packaging    => "zip",
     classifier   => "linux-x64",
+    stage        => "runtime",
   }
   
 }
