5989142773284c225c79dd8238cc4aec6e72d521
jcaddel <jcaddel@kuali.org>
2013-01-18 00:58:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15547 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/java/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/java/manifests/init.pp
@@ -8,29 +8,14 @@ class java ($jdk6_version = undef
   , $classifier
 ) {
 
-  $jdk6_artifact = "jdk-${jdk6_version}"
-  $jdk7_artifact = "jdk-${jdk7_version}"
-  
-  s3artifact { $jdk6_artifact:
-    local_repo  => $local_repo,
-    bucket      => $bucket,
-    prefix      => $prefix,
-    group_id    => $group_id,
-    artifact_id => $artifact_id,
-    version     => $jdk6_version,
-    packaging   => $packaging,
-    classifier  => $classifier,
+  java::jdk { 'jdk7':
+    level   => '7',
+    version => $jdk7_version,
   }
 
-  jdk::install { $jdk6_artifact:
-    local_repo   => $local_repo,
-    group_id     => $group_id,
-    artifact_id  => $artifact_id,
-    version      => $jdk6_version,
-    packaging    => $packaging,
-    classifier   => $classifier,
-    level        => $level,
-    require      => S3artifact[$artifact],
+  java::jdk { 'jdk6':
+    level   => '6',
+    version => $jdk6_version,
   }
-  
+
 }
