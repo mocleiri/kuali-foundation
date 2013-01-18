cf60c89e921937a177512e32955206bc0603b879
jcaddel <jcaddel@kuali.org>
2013-01-18 03:31:56 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15573 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/bootstrap.sh
+++ kuali-puppet/src/main/resources/bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
-PACKAGE=puppet
-VERSION=2.7.18
+#PACKAGE=puppet
+#VERSION=2.7.18
 
-yum erase $PACKAGE -y
-yum install $PACKAGE-$VERSION -y
-yum install rubygems
-yum install ruby-nokogiri
+#yum erase $PACKAGE -y
+#yum install $PACKAGE-$VERSION -y
+
+yum install puppet -y
+yum install rubygems -y
+yum install ruby-nokogiri -y
+yum install subversion -y
 
--- kuali-puppet/src/main/resources/modules/kuali/manifests/packages.pp
+++ kuali-puppet/src/main/resources/modules/kuali/manifests/packages.pp
@@ -15,15 +15,6 @@ class kuali::packages {
     ensure => '1.8.7.371-1.20.amzn1',
   }
 
-  package { 'rubygems':
-    ensure => '1.8.11-3.1.amzn1',
-  }
-
-  package { 'ruby-nokogiri': 
-    ensure  => '1.5.2-1.6.amzn1',
-    require => Package['rubygems'],
-  }
-
   package { 'fog': 
     ensure   => '1.8.0',
     provider => 'gem',
