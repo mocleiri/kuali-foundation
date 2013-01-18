1653e9054c59fd3637bbdffdb963cd38a99c9003
jcaddel <jcaddel@kuali.org>
2013-01-18 01:13:56 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15551 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
@@ -1,15 +1,3 @@
-class java ($jdk6_version = undef
-  , $jdk7_version = undef
-  , $local_repo = "/root/.m2/repository"
-  , $bucket = "maven.kuali.org"
-  , $prefix = "private"
-  , $group_id = "com.oracle"
-  , $artifact_id = "jdk"
-  , $packaging = "zip"
-  , $classifier = "linux-x64"
-) {
-
-
 class kuali {
 
   # File system paths to check for cURL and md5sum
@@ -18,9 +6,23 @@ class kuali {
   # Do this once so each exec command can inherit the paths
   Exec { path => $exec_path }
   
+  $local_repo = "/root/.m2/repository"
+  $bucket = "maven.kuali.org"
   $jdk6_version = "1.6.0-u38"
   $jdk7_version = "1.7.0-u11"
   $tomcat_version = "6.0.35"
+  
+  class java { 'java':
+    jdk6_version => "1.6.0-u38",
+    jdk7_version => "1.7.0-u11",
+    local_repo   => $local_repo,
+    bucket       => $bucket,
+    prefix       => "private",
+    group_id     => "com.oracle",
+    artifact_id  => "jdk",
+    packaging    => "zip",
+    classifier   => "linux-x64",
+  }
   
   jdk { 'jdk6':
     level      => '6',
