78b3c857e51c7d3420b4a0aaa3b154e8b079b024
jcaddel <jcaddel@kuali.org>
2013-01-18 00:54:11 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15544 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/java/manifests/install.pp
+++ kuali-puppet/src/main/resources/modules/java/manifests/install.pp
@@ -3,29 +3,29 @@ define jdk::install ($local_repo
   , $artifact_id
   , $version
   , $packaging
-  , $classifier
+  , $classifier = undef
   , $level
   , $basedir = '/usr/java'
 ) {
 
-  # The name of the file eg "commons-io-1.3.2.jar"
+  # Convert "com.oracle" into "com/oracle"
+  $group_id_path = getpath($group_id)
+  
+  # The name of the file eg "jdk-1.7.0-u11-linux-x64.jar"
   if ($classifier == undef) {
     $filename = "${artifact_id}-${version}.${packaging}"
   } else {
     $filename = "${artifact_id}-${version}-${classifier}.${packaging}"
   }
   
-  # Convert "org.apache.commons" into "org/apache/commons"
-  $group_id_path = getpath($group_id)
-  
   # The relative path to the directory containing the file in a Maven repository
   # This value is identical for both the S3 Maven repository and the Maven repository on the local file system
-  # eg "org/apache/commons/commons-io/1.3.2"
-  $repopath = "${group_id_path}/${artifact_id}/${version}"
+  # eg "com/oracle/jdk/1.7.0-u11"
+  $repo_path = "${group_id_path}/${artifact_id}/${version}"
 
   # Fully qualified filename that the S3 object will be downloaded to
   # Any non-existing parent directories are automatically created by cURL as needed
-  $file = "${local_repo}/${repopath}/${filename}"
+  $file = "${local_repo}/${repo_path}/${filename}"
   
   $jdk_dir = "${basedir}/jdk-${version}"
   $jdk_symlink = "${basedir}/jdk${level}"
