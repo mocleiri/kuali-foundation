9262b045242e926ed801e3110aa47269fe8ac415
jcaddel <jcaddel@kuali.org>
2013-01-18 01:28:31 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15554 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
@@ -1,12 +1,9 @@
-include 'kuali::setup'
-
 class kuali {
+
+  include kuali::setup
 
   $local_repo = "/root/.m2/repository"
   $bucket = "maven.kuali.org"
-  $jdk6_version = "1.6.0-u38"
-  $jdk7_version = "1.7.0-u11"
-  $tomcat_version = "6.0.35"
   
   class java { 'java':
     jdk6_version => "1.6.0-u38",
@@ -20,20 +17,6 @@ class kuali {
     classifier   => "linux-x64",
   }
   
-  jdk { 'jdk6':
-    level      => '6',
-    version    => $jdk6_version,
-  }
-
-  jdk { 'jdk7':
-    level      => '7',
-    version    => $jdk7_version,
-  }
-
-  tomcat { 'tomcat':
-    version    => $tomcat_version,
-  }
-
 }
 
 class {'kuali':}
