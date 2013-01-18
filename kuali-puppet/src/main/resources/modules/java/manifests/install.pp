5dacef782daf23b5140e66542d7e8eeae637e042
jcaddel <jcaddel@kuali.org>
2013-01-18 00:51:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15543 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/java/manifests/install.pp
+++ kuali-puppet/src/main/resources/modules/java/manifests/install.pp
@@ -1,6 +1,6 @@
-define jdk::install ($localrepo
-  , $groupid
-  , $artifactid
+define jdk::install ($local_repo
+  , $group_id
+  , $artifact_id
   , $version
   , $packaging
   , $classifier
@@ -10,35 +10,35 @@ define jdk::install ($localrepo
 
   # The name of the file eg "commons-io-1.3.2.jar"
   if ($classifier == undef) {
-    $filename = "${artifactid}-${version}.${packaging}"
+    $filename = "${artifact_id}-${version}.${packaging}"
   } else {
-    $filename = "${artifactid}-${version}-${classifier}.${packaging}"
+    $filename = "${artifact_id}-${version}-${classifier}.${packaging}"
   }
   
   # Convert "org.apache.commons" into "org/apache/commons"
-  $groupidpath = getpath($groupid)
+  $group_id_path = getpath($group_id)
   
   # The relative path to the directory containing the file in a Maven repository
   # This value is identical for both the S3 Maven repository and the Maven repository on the local file system
   # eg "org/apache/commons/commons-io/1.3.2"
-  $repopath = "${groupidpath}/${artifactid}/${version}"
+  $repopath = "${group_id_path}/${artifact_id}/${version}"
 
   # Fully qualified filename that the S3 object will be downloaded to
   # Any non-existing parent directories are automatically created by cURL as needed
-  $file = "${localrepo}/${repopath}/${filename}"
+  $file = "${local_repo}/${repopath}/${filename}"
   
-  $jdkdir = "${basedir}/jdk-${version}"
-  $jdksymlink = "${basedir}/jdk${level}"
+  $jdk_dir = "${basedir}/jdk-${version}"
+  $jdk_symlink = "${basedir}/jdk${level}"
   $unzip = "unzip -d ${basedir} ${file}"
   
   exec { $unzip:
     command => $unzip,
-    creates => $jdkdir,
+    creates => $jdk_dir,
   }
   
-  file { $jdksymlink:
+  file { $jdk_symlink:
     ensure  => 'link',
-    target  => $jdkdir,
+    target  => $jdk_dir,
     require => Exec[$unzip],
   }
   
