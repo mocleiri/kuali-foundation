2ebe58b42e64863986af3ef91d6d2c91dc511889
jcaddel <jcaddel@kuali.org>
2013-01-18 01:31:32 -0500

    tweaking
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15556 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
+++ kuali-puppet/src/main/resources/modules/kuali/manifests/init.pp
@@ -5,17 +5,17 @@ class kuali {
   $local_repo = "/root/.m2/repository"
   $bucket = "maven.kuali.org"
   
-  class java { 'java':
-    jdk6_version => "1.6.0-u38",
-    jdk7_version => "1.7.0-u11",
-    local_repo   => $local_repo,
-    bucket       => $bucket,
-    prefix       => "private",
-    group_id     => "com.oracle",
-    artifact_id  => "jdk",
-    packaging    => "zip",
-    classifier   => "linux-x64",
-  }
+#  class java { 'java':
+#    jdk6_version => "1.6.0-u38",
+#    jdk7_version => "1.7.0-u11",
+#    local_repo   => $local_repo,
+#    bucket       => $bucket,
+#    prefix       => "private",
+#    group_id     => "com.oracle",
+#    artifact_id  => "jdk",
+#    packaging    => "zip",
+#    classifier   => "linux-x64",
+#  }
   
 }
 
