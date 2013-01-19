9b2c7e201664d20ba99b314357a0219f4c95a7ef
jcaddel <jcaddel@kuali.org>
2013-01-19 17:13:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc@15615 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/SqlSource.java
+++ src/main/java/org/kuali/common/jdbc/SqlSource.java
@@ -3,6 +3,7 @@ package org.kuali.common.jdbc;
 public class SqlSource {
 
 	String location;
+	String sql;
 	SqlMetaData metaData;
 
 	public String getLocation() {
@@ -19,6 +20,14 @@ public class SqlSource {
 
 	public void setMetaData(SqlMetaData metaData) {
 		this.metaData = metaData;
+	}
+
+	public String getSql() {
+		return sql;
+	}
+
+	public void setSql(String sql) {
+		this.sql = sql;
 	}
 
 }
