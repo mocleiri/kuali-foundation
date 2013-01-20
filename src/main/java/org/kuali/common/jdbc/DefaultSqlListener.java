cf0170ce5f13bad56b4bab6b632e5744f6b8060d
jcaddel <jcaddel@kuali.org>
2013-01-20 15:03:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc@15633 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/kuali/common/jdbc/DefaultSqlListener.java
@@ -0,0 +1,51 @@
+package org.kuali.common.jdbc;
+import java.util.List;
+
+import org.kuali.common.jdbc.context.ExecutionContext;
+
+public class DefaultSqlListener implements SqlListener {
+
+	ExecutionMetaData start;
+	ExecutionMetaData finish;
+	int count = 0;
+
+	@Override
+	public synchronized void beforeMetaData(ExecutionContext context) {
+	}
+
+	@Override
+	public synchronized void beforeExecution(SqlExecutionEvent event) {
+		this.start = getStartMeta(event.getSources());
+	}
+
+	@Override
+	public synchronized void beforeExecuteSql(String sql) {
+	}
+
+	@Override
+	public synchronized void afterExecuteSql(String sql) {
+		count++;
+		if (count % 100 == 0) {
+			System.out.print(".");
+		}
+	}
+
+	@Override
+	public synchronized void afterExecution(SqlExecutionEvent event) {
+	}
+
+	protected ExecutionMetaData getStartMeta(List<SqlSource> sources) {
+		long count = 0;
+		long size = 0;
+		for (SqlSource source : sources) {
+			SqlMetaData smd = source.getMetaData();
+			count += smd.getCount();
+			count += smd.getSize();
+		}
+		ExecutionMetaData meta = new ExecutionMetaData();
+		meta.setCount(count);
+		meta.setSize(size);
+		return meta;
+	}
+
+}
