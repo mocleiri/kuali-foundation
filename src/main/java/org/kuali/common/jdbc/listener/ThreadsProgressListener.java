b4579e3359fd6747786c2fe76520d7417ce4fe4e
jcaddel <jcaddel@kuali.org>
2013-01-20 22:09:50 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc@15669 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/listener/ThreadsProgressListener.java
+++ src/main/java/org/kuali/common/jdbc/listener/ThreadsProgressListener.java
@@ -1,25 +1,16 @@
 package org.kuali.common.jdbc.listener;
 
-import java.util.List;
-
 import org.kuali.common.jdbc.SqlExecutionEvent;
-import org.kuali.common.jdbc.SqlSource;
 
 public class ThreadsProgressListener extends ProgressListener {
 
-	List<SqlSource> sources;
+	public void setTotal(long total) {
+		this.total = total;
+	}
 
 	@Override
 	public void beforeExecution(SqlExecutionEvent event) {
 		// intentionally do nothing to override the super class behavior
-	}
-
-	public List<SqlSource> getSources() {
-		return sources;
-	}
-
-	public void setSources(List<SqlSource> sources) {
-		this.sources = sources;
 	}
 
 }
