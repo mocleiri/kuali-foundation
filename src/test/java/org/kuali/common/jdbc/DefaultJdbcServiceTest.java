7fe4b9ed7bf4ea9fc0daaececa3c14c259af2c7f
jcaddel <jcaddel@kuali.org>
2013-01-21 19:42:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc@15707 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/test/java/org/kuali/common/jdbc/DefaultJdbcServiceTest.java
+++ src/test/java/org/kuali/common/jdbc/DefaultJdbcServiceTest.java
@@ -164,7 +164,7 @@ public class DefaultJdbcServiceTest {
 			dba.setExecute(execute);
 			schemas.setExecute(execute);
 			data.setExecute(execute);
-			data.setThreads(2);
+			data.setThreads(10);
 			constraints.setExecute(false);
 
 			long start = System.currentTimeMillis();
