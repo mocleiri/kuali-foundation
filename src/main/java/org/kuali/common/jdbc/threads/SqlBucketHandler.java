fd7ed82eb9f2928ffc508d8aae03a89584c68661
jcaddel <jcaddel@kuali.org>
2013-01-22 21:54:35 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc@15782 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/threads/SqlBucketHandler.java
+++ src/main/java/org/kuali/common/jdbc/threads/SqlBucketHandler.java
@@ -8,12 +8,8 @@ public class SqlBucketHandler implements ElementHandler<SqlBucketContext> {
 
 	@Override
 	public void handleElement(ListIteratorContext<SqlBucketContext> context, int index, SqlBucketContext element) {
-		try {
-			JdbcService service = element.getService();
-			service.executeSql(element.getContext());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		JdbcService service = element.getService();
+		service.executeSql(element.getContext());
 	}
 
 }
