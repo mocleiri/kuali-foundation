199af33a4d70b092f7efa498c4422b2328697487
jcaddel <jcaddel@kuali.org>
2013-01-26 11:39:47 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-2.0@15868 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/test/java/org/kuali/common/jdbc/DefaultDatabaseServiceTest.java
+++ src/test/java/org/kuali/common/jdbc/DefaultDatabaseServiceTest.java
@@ -38,6 +38,7 @@ public class DefaultDatabaseServiceTest {
 		System.setProperty("sql.execute", Boolean.TRUE.toString());
 		System.setProperty("kuali.db", "ole-fs");
 		System.setProperty("kuali.db", "ks-app-db");
+		System.setProperty("kuali.db", "ks-with-rice-bundled");
 		System.setProperty("db.vendor", "mysql");
 	}
 
