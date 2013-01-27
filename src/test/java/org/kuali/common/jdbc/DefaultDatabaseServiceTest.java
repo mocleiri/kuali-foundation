df89322ebcf8e8c02d85cf1929856d66a87b398e
jcaddel <jcaddel@kuali.org>
2013-01-27 17:40:42 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-2.0@15904 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/test/java/org/kuali/common/jdbc/DefaultDatabaseServiceTest.java
+++ src/test/java/org/kuali/common/jdbc/DefaultDatabaseServiceTest.java
@@ -38,9 +38,9 @@ public class DefaultDatabaseServiceTest {
 	private static final Logger logger = LoggerFactory.getLogger(DefaultDatabaseServiceTest.class);
 	static {
 		System.setProperty("sql.execute", Boolean.TRUE.toString());
+		System.setProperty("kuali.db", "ks-app-db");
 		System.setProperty("kuali.db", "ole-fs");
 		System.setProperty("kuali.db", "ks-with-rice-bundled");
-		System.setProperty("kuali.db", "ks-app-db");
 		System.setProperty("db.vendor", "mysql");
 	}
 
