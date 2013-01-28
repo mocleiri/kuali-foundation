6b713bcd9ca67789487eb571bb4837430432b3f6
jcaddel <jcaddel@kuali.org>
2013-01-28 08:52:11 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15931 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/service/DefaultMySqlDumpService.java
+++ kuali-util/src/main/java/org/kuali/common/util/service/DefaultMySqlDumpService.java
@@ -9,10 +9,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.util.Assert;
 import org.kuali.common.util.LocationUtils;
 import org.kuali.common.util.PrintlnStreamConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultMySqlDumpService extends DefaultExecService implements MySqlDumpService {
 
 	public static final String DEFAULT_EXECUTABLE = "mysqldump";
+	private static final Logger logger = LoggerFactory.getLogger(DefaultMySqlDumpService.class);
 
 	@Override
 	public void dump(MySqlDumpContext context) {
@@ -20,7 +23,17 @@ public class DefaultMySqlDumpService extends DefaultExecService implements MySql
 		Assert.notNull(context.getOutputFile(), "output file is null");
 		List<String> args = getMySqlDumpArgs(context);
 		ExecContext ec = getExecContext(context.getExecutable(), args, context.getOutputFile());
+		logDump(context);
 		dump(ec);
+	}
+
+	protected void logDump(MySqlDumpContext context) {
+		String username = context.getUsername();
+		String hostname = context.getHostname();
+		String database = context.getDatabase();
+		String path = LocationUtils.getCanonicalPath(context.getOutputFile());
+		Object[] args = { username, hostname, database, path };
+		logger.info("Dumping {}@{}:{} to {}", args);
 	}
 
 	@Override
