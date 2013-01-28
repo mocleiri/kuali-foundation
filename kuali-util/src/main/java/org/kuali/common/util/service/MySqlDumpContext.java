b66d50bafa1e67c3ef3e1f8f405c246d42109ca9
jcaddel <jcaddel@kuali.org>
2013-01-28 00:09:58 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@15916 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/service/MySqlDumpContext.java
@@ -0,0 +1,53 @@
+package org.kuali.common.util.service;
+
+import java.io.File;
+
+public class MySqlDumpContext {
+
+	String username;
+	String password;
+	String hostname;
+	String database;
+	File outputFile;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public String getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+
+	public File getOutputFile() {
+		return outputFile;
+	}
+
+	public void setOutputFile(File outputFile) {
+		this.outputFile = outputFile;
+	}
+
+}
---
+++ kuali-util/src/main/java/org/kuali/common/util/service/MySqlService.java
@@ -0,0 +1,7 @@
+package org.kuali.common.util.service;
+
+public interface MySqlService {
+
+	void dump(MySqlDumpContext context);
+
+}
