64dff37060ea91067b7fd814a71b0067a5f67acf
jcaddel <jcaddel@kuali.org>
2013-01-30 18:52:42 -0500

    Add Ignorer's
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@16088 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/ignore/PrefixSuffixIgnorer.java
@@ -0,0 +1,31 @@
+package org.kuali.common.util.ignore;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class PrefixSuffixIgnorer implements Ignore {
+
+	String prefix;
+	String suffix;
+
+	@Override
+	public boolean ignore(String line) {
+		return StringUtils.startsWith(line, prefix) && StringUtils.endsWith(line, suffix);
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+}
---
+++ kuali-util/src/main/java/org/kuali/common/util/ignore/StartsWithIgnorer.java
@@ -0,0 +1,22 @@
+package org.kuali.common.util.ignore;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StartsWithIgnorer implements Ignore {
+
+	String prefix;
+
+	@Override
+	public boolean ignore(String line) {
+		return StringUtils.startsWith(line, prefix);
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+}
