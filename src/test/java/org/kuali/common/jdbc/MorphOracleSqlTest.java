f3016bb633ed474a21c56cdbb3fbe28c5a3aa79e
jcaddel <jcaddel@kuali.org>
2013-01-29 21:57:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-2.0@16057 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/test/java/org/kuali/common/jdbc/MorphOracleSqlTest.java
+++ src/test/java/org/kuali/common/jdbc/MorphOracleSqlTest.java
@@ -34,7 +34,7 @@ import org.slf4j.LoggerFactory;
 public class MorphOracleSqlTest {
 	private static final Logger logger = LoggerFactory.getLogger(MorphOracleSqlTest.class);
 	public static final int MAX_LENGTH = new Integer(getProperty("max.length", 1024 * 50 + ""));
-	public static final int MAX_COUNT = new Integer(getProperty("max.count", 10 + ""));;
+	public static final int MAX_COUNT = new Integer(getProperty("max.count", 100000 + ""));;
 	public static final String INSERT = "INSERT";
 	public static final String DELIMITER = "/";
 	public static final String LF = "\n";
@@ -55,10 +55,7 @@ public class MorphOracleSqlTest {
 	public void parseSql() {
 		try {
 			logger.info("Parsing Old School SQL");
-			convert("classpath:KSEN_ATP.sql", new File("/Users/jeffcaddel/ws/kuali-jdbc-2.0/src/test/resources/KSEN_ATP-smart.sql"));
-			if (true) {
-				return;
-			}
+			// convert("classpath:KSEN_ATP.sql", new File("/Users/jeffcaddel/ws/kuali-jdbc-2.0/src/test/resources/KSEN_ATP-smart.sql"));
 			long start = System.currentTimeMillis();
 			convert("classpath:META-INF/sql/oracle/ks-core-sql-data.resources", ws + "/ks-core/ks-core-sql/src/main/resources");
 			convert("classpath:META-INF/sql/oracle/ks-lum-sql-data.resources", ws + "/ks-lum/ks-lum-sql/src/main/resources");
