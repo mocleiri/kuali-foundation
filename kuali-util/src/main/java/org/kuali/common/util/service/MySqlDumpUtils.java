80c7edf217bc351726656e54acbac0f57a3d7c56
jcaddel <jcaddel@kuali.org>
2013-01-30 19:34:22 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@16091 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/service/MySqlDumpUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/service/MySqlDumpUtils.java
@@ -1,7 +1,7 @@
 package org.kuali.common.util.service;
 
 import static org.kuali.common.util.service.MySqlDumpService.PREFIX_50013_DEFINER;
-import static org.kuali.common.util.service.MySqlDumpService.PREFIX_DUMP_COMPLETED;
+import static org.kuali.common.util.service.MySqlDumpService.SUFFIX_SQL_SECURITY_DEFINER;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +14,7 @@ public class MySqlDumpUtils {
 
 	public static List<Ignore> getDefaultIgnorers() {
 		List<Ignore> ignorers = new ArrayList<Ignore>();
-		ignorers.add(new PrefixSuffixIgnorer(PREFIX_50013_DEFINER, PREFIX_DUMP_COMPLETED));
+		ignorers.add(new PrefixSuffixIgnorer(PREFIX_50013_DEFINER, SUFFIX_SQL_SECURITY_DEFINER));
 		ignorers.add(new StartsWithIgnorer(MySqlDumpService.PREFIX_DUMP_COMPLETED));
 		ignorers.add(new StartsWithIgnorer(MySqlDumpService.PREFIX_MYSQL_DUMP));
 		ignorers.add(new StartsWithIgnorer(MySqlDumpService.PREFIX_MYSQL_HOST));
