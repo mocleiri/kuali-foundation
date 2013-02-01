6d72ea685beb9dc956e9ddc9f59f92805ef03ede
clrux <clrux@kuali.org>
2013-02-01 16:17:53 -0500

    New header and various other tweaks and additions
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@16152 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/prototypes-dev/assets/js/custom.js
+++ mocks/KCUIWG/prototypes-dev/assets/js/custom.js
@@ -1,31 +1,43 @@
 /*
 	Custom scripting for additional functionality
-	Author: Chris Rodriguez, clrux@bu.edu
-	Created: 29 Jan 2012, 12:09 PM
-	Version: 0.1
+	Author: 	Chris Rodriguez, clrux@bu.edu
+	Created: 	29 Jan 2013, 12:09
+	Last Mod: 	1 Feb 2013, 11:05
+	Version: 	0.2
 */
+
+
+
+/*
+ScrollTop and ScrollBottom functions
+---------------------------------- */
+(function($){$.fn.jScroll=function(e){var f=$.extend({},$.fn.jScroll.defaults,e);return this.each(function(){var a=$(this);var b=$(window);var c=new location(a);b.scroll(function(){a.stop().animate(c.getMargin(b),f.speed)})});function location(d){this.min=d.offset().top;this.originalMargin=parseInt(d.css("margin-top"),10)||0;this.getMargin=function(a){var b=d.parent().height()-d.outerHeight();var c=this.originalMargin;if(a.scrollTop()>=this.min)c=c+f.top+a.scrollTop()-this.min;if(c>b)c=b;return({"marginTop":c+'px'})}}};$.fn.jScroll.defaults={speed:"slow",top:10}})(jQuery);
+$.fn.scrollBottom=function(){return $(document).height()-this.scrollTop()-this.height();};
+
+
+
+/*
+Global variables
+---------------------------------- */
+var $header 	= $('#header'),
+	$document 	= $('#document'),
+	$toc 		= $('#ToC'),
+	$window 	= $(window),
+	$dOffset	= $document.offset(),
+	$tOffset 	= $toc.offset();
 
 
 
 /*
 Sticky table of contents
 ---------------------------------- */
-(function($){$.fn.jScroll=function(e){var f=$.extend({},$.fn.jScroll.defaults,e);return this.each(function(){var a=$(this);var b=$(window);var c=new location(a);b.scroll(function(){a.stop().animate(c.getMargin(b),f.speed)})});function location(d){this.min=d.offset().top;this.originalMargin=parseInt(d.css("margin-top"),10)||0;this.getMargin=function(a){var b=d.parent().height()-d.outerHeight();var c=this.originalMargin;if(a.scrollTop()>=this.min)c=c+f.top+a.scrollTop()-this.min;if(c>b)c=b;return({"marginTop":c+'px'})}}};$.fn.jScroll.defaults={speed:"slow",top:10}})(jQuery);
-
-$.fn.scrollBottom=function(){return $(document).height()-this.scrollTop()-this.height();};
-
-var $toc = $('#ToC');
-if ($toc.length) {
-
-	var $window = $(window),
-		$brand = $('#brand'),
-		$header = $('#header'),
-		tocOffset = $toc.offset();
-
-	function init_sticky_toc() {
-		if( $window.scrollTop() > tocOffset.top ) {
+function init_sticky_elems() {
+	if ($toc.length && $document.length) {
+		if ($window.scrollTop() > $dOffset.top) {
+			$document.addClass('fixed');
 			$toc.addClass('fixed');
 		} else {
+			$document.removeClass('fixed');
 			$toc.removeClass('fixed');
 		}
 	}
@@ -38,10 +50,10 @@ Document ready
 ---------------------------------- */
 $(document).ready(function() {
 	$window.bind("resize", function() {
-		init_sticky_toc();
+		init_sticky_elems();
 	});
 	
 	$window.bind("scroll", function() {
-		init_sticky_toc();
+		init_sticky_elems();
 	});
 });
