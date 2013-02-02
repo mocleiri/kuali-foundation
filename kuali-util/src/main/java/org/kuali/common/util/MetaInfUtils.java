346323e406a51db1951ec7c9ca2d948bccc376a6
jcaddel <jcaddel@kuali.org>
2013-02-02 10:07:01 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@16159 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/MetaInfUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/MetaInfUtils.java
@@ -3,6 +3,7 @@ package org.kuali.common.util;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,21 +16,27 @@ public class MetaInfUtils {
 
 	private static final Logger logger = LoggerFactory.getLogger(MetaInfUtils.class);
 
-	public static void scanAndCreateFile(MetaInfContext context) throws IOException {
-		List<String> includes = context.getIncludes();
-		List<String> excludes = context.getExcludes();
-		logger.info("Examining - " + context.getBaseDir().getCanonicalPath());
-		logger.info("Include - " + CollectionUtils.getSpaceSeparatedString(includes));
-		logger.info("Exclude - " + CollectionUtils.getSpaceSeparatedString(excludes));
-		SimpleScanner scanner = new SimpleScanner(context.getBaseDir(), includes, excludes);
-		List<File> files = scanner.getFiles();
-		logger.info("Located " + files.size() + " files");
-		List<String> locations = getLocations(context.getBaseDir(), files, context.getPrefix());
-		if (context.isSort()) {
-			Collections.sort(locations);
+	public static void scanAndCreateFiles(List<MetaInfContext> contexts) throws IOException {
+		for (MetaInfContext context : contexts) {
+			List<String> includes = context.getIncludes();
+			List<String> excludes = context.getExcludes();
+			logger.info("Examining - " + context.getBaseDir().getCanonicalPath());
+			logger.info("Include - " + CollectionUtils.getSpaceSeparatedString(includes));
+			logger.info("Exclude - " + CollectionUtils.getSpaceSeparatedString(excludes));
+			SimpleScanner scanner = new SimpleScanner(context.getBaseDir(), includes, excludes);
+			List<File> files = scanner.getFiles();
+			logger.info("Located " + files.size() + " files");
+			List<String> locations = getLocations(context.getBaseDir(), files, context.getPrefix());
+			if (context.isSort()) {
+				Collections.sort(locations);
+			}
+			logger.info("Creating " + context.getOutputFile().getCanonicalPath());
+			FileUtils.writeLines(context.getOutputFile(), locations);
 		}
-		logger.info("Creating " + context.getOutputFile().getCanonicalPath());
-		FileUtils.writeLines(context.getOutputFile(), locations);
+	}
+
+	public static void scanAndCreateFile(MetaInfContext context) throws IOException {
+		scanAndCreateFiles(Arrays.asList(context));
 	}
 
 	public static List<String> getLocations(File baseDir, List<File> files, String prefix) throws IOException {
