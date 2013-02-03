a4a5c5bfbf2eeca398ade00042dadc5da0678f5a
jcaddel <jcaddel@kuali.org>
2013-02-03 14:20:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-2.0@16251 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/test/java/org/kuali/common/jdbc/MySqlDirectoryConverterTest.java
@@ -0,0 +1,32 @@
+package org.kuali.common.jdbc;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.kuali.common.jdbc.convert.DirectoryContext;
+import org.kuali.common.jdbc.convert.MySqlConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MySqlDirectoryConverterTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(MySqlDirectoryConverterTest.class);
+
+	@Test
+	public void testConvert() {
+		try {
+			logger.info("");
+			String basedir = "/Users/jeffcaddel/ws/spring-db-jc/ks-deployments/ks-cfg-dbs/ks-rice-db";
+			String resourceDir = "src/main/resources/impex/sql/mysql";
+			String filename = basedir + "/" + resourceDir;
+			DirectoryContext context = new DirectoryContext();
+			context.setArtifactId("ks-rice-db");
+			context.setConverter(new MySqlConverter());
+			context.setDatabase("mysql");
+			context.setDirectory(new File(filename));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
