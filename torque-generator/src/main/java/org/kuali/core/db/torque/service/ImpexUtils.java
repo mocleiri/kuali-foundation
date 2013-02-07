09f379b3193a42d225ee96d274a9102b1981cdd4
jcaddel <jcaddel@kuali.org>
2013-02-07 02:11:24 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16396 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/main/java/org/kuali/core/db/torque/service/ImpexUtils.java
+++ torque-generator/src/main/java/org/kuali/core/db/torque/service/ImpexUtils.java
@@ -1,13 +1,20 @@
 package org.kuali.core.db.torque.service;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.LocationUtils;
 import org.kuali.common.util.PropertyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 
 public class ImpexUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(ImpexUtils.class);
 
 	private static final String FS = File.separator;
 
@@ -17,7 +24,7 @@ public class ImpexUtils {
 		return target;
 	}
 
-	public static ImpexContext cloneAndInitialize(ImpexContext source, String include, String artifactId) {
+	public static ImpexContext cloneAndInitialize(ImpexContext source, String include, String artifactId) throws IOException {
 		ImpexContext clone = clone(source);
 		clone.setTableIncludes(CollectionUtils.getTrimmedListFromCSV(include));
 		clone.setViewIncludes(CollectionUtils.getTrimmedListFromCSV(include));
@@ -32,13 +39,28 @@ public class ImpexUtils {
 	/**
 	 * Working dir must be set before invoking this method
 	 */
-	public static void initialize(ImpexContext context) {
-		context.setContextProperties(new File(context.getWorkingDir(), "/reports/context.datadtd.properties"));
-		context.setReportFile("../reports/report." + context.getArtifactId() + ".datadtd.generation");
-		prepareFileSystem(context);
+	public static void initialize(ImpexContext context) throws IOException {
+		initReportFile(context);
+		initContextPropertiesFile(context);
 	}
 
-	protected static void prepareFileSystem(ImpexContext context) {
+	protected static void initReportFile(ImpexContext context) throws IOException {
+		String relativePath = "../reports/" + context.getArtifactId() + "/context.datadtd.generation";
+		String absolutePath = context.getWorkingDir() + "/" + relativePath;
+		File file = new File(absolutePath);
+		String canonicalPath = LocationUtils.getCanonicalPath(file);
+		File canonicalFile = new File(canonicalPath);
+		logger.debug("Touching report file [{}]", canonicalPath);
+		FileUtils.touch(canonicalFile);
+		context.setReportFile(relativePath);
+	}
+
+	protected static void initContextPropertiesFile(ImpexContext context) {
+		String path = context.getWorkingDir() + "/../reports/" + context.getArtifactId() + "/context.datadtd.properties";
+		String canonicalPath = LocationUtils.getCanonicalPath(new File(path));
+		File file = new File(canonicalPath);
+		context.setContextProperties(file);
+
 		Properties properties = new Properties();
 		properties.setProperty("project", "impex");
 		properties.setProperty("version", "2.0");
