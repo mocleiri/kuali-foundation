7fd562a09061ec8964def2184e6a376b7aa6f1eb
jcaddel <jcaddel@kuali.org>
2013-02-08 13:53:50 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16455 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/main/java/org/kuali/core/db/torque/service/DefaultDataHandler.java
+++ torque-generator/src/main/java/org/kuali/core/db/torque/service/DefaultDataHandler.java
@@ -1,15 +1,31 @@
 package org.kuali.core.db.torque.service;
 
+import org.kuali.common.util.FormatUtils;
 import org.kuali.core.db.torque.pojo.DumpTableContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultDataHandler implements DataHandler {
 
+	private static final Logger logger = LoggerFactory.getLogger(DefaultDataHandler.class);
+
 	@Override
 	public void handleData(DumpTableContext context) {
+		handle(context);
 	}
 
 	@Override
 	public void finalizeData(DumpTableContext context) {
+		handle(context);
+	}
+
+	protected void handle(DumpTableContext context) {
+		String crc = FormatUtils.getCount(context.getCurrentRowCount());
+		String trc = FormatUtils.getCount(context.getTotalRowCount());
+		String cds = FormatUtils.getSize(context.getCurrentDataSize());
+		String tds = FormatUtils.getSize(context.getTotalDataSize());
+		Object[] args = { crc, trc, cds, tds };
+		logger.info("Current Rows: {} Total Rows: {} Current Size: {} Total Size: {}", args);
 	}
 
 }
