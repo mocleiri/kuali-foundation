c2b7dc73b1bb7bf2d0d0c8d1b7d3598875d6778b
jcaddel <jcaddel@kuali.org>
2013-02-10 16:48:13 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16513 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/test/java/org/kuali/core/db/torque/service/ImpexServiceTest.java
+++ torque-generator/src/test/java/org/kuali/core/db/torque/service/ImpexServiceTest.java
@@ -48,7 +48,7 @@ public class ImpexServiceTest {
 	public void test() {
 		try {
 			long start = System.currentTimeMillis();
-			Properties p = getProperties("KR.*,KS.*", 5, 15);
+			Properties p = getProperties("KR.*,KS.*", 10, 20);
 			// Properties p = getProperties("KRIM.*", 5, 15);
 			// Properties p = getProperties("KSEN_MSTONE.*", 5, 15);
 			ImpexContext sourceContext = getImpexContext(p);
