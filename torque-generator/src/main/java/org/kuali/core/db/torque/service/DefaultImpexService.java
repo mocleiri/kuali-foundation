0c0996165c922477b4616ad9df88e18d1fb9e620
jcaddel <jcaddel@kuali.org>
2013-02-10 17:20:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16519 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/main/java/org/kuali/core/db/torque/service/DefaultImpexService.java
+++ torque-generator/src/main/java/org/kuali/core/db/torque/service/DefaultImpexService.java
@@ -607,6 +607,8 @@ public class DefaultImpexService implements ImpexService {
 			bucket.setContext(context);
 			bucket.setService(this);
 			bucket.setResults(results);
+			// Fewest rows to most rows
+			Collections.sort(bucket.getTables());
 		}
 		return buckets;
 	}
