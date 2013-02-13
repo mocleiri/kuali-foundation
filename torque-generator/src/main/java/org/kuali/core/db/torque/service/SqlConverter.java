95593b5e43ba87f0d7f8dd3c45ee87736c4b088f
jcaddel <jcaddel@kuali.org>
2013-02-13 12:51:54 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16581 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/main/java/org/kuali/core/db/torque/service/SqlConverter.java
+++ torque-generator/src/main/java/org/kuali/core/db/torque/service/SqlConverter.java
@@ -4,6 +4,9 @@ import org.apache.torque.engine.database.model.Column;
 
 public interface SqlConverter {
 
+	/**
+	 * Return a value usable in an SQL insert statement
+	 */
 	String getSqlValue(Column column, String token);
 
 }
