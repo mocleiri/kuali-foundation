b6b7283ad32cfd662b97fbbcb0e3ac1634884d7e
jcaddel <jcaddel@kuali.org>
2013-02-13 12:16:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16576 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ torque-generator/src/main/java/org/kuali/core/db/torque/service/MySQLConverter.java
@@ -0,0 +1,12 @@
+package org.kuali.core.db.torque.service;
+
+import org.apache.torque.engine.database.model.Column;
+
+public class MySQLConverter implements SqlConverter {
+
+	@Override
+	public String getSqlValue(Column column, String token) {
+		return null;
+	}
+
+}
