1003003d7838e1a4ae0bcc09af5ddc8fd00f5c4f
jcaddel <jcaddel@kuali.org>
2013-02-17 15:46:14 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16674 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ torque-generator/src/main/java/org/kuali/common/impex/ImpexContextFactoryBean.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2010-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.impex;
+
+import org.kuali.common.impex.service.ImpexContext;
+import org.springframework.beans.factory.FactoryBean;
+
+public class ImpexContextFactoryBean implements FactoryBean<ImpexContext> {
+
+	@Override
+	public ImpexContext getObject() throws Exception {
+		return null;
+	}
+
+	@Override
+	public Class<ImpexContext> getObjectType() {
+		return ImpexContext.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return false;
+	}
+}
