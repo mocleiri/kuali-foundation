7df55d6d028362980dd85f94814d43f1a8885130
jcaddel <jcaddel@kuali.org>
2013-02-17 21:15:03 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16678 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ torque-generator/src/main/java/org/kuali/common/impex/DumpExecutable.java
@@ -0,0 +1,11 @@
+package org.kuali.common.impex;
+
+import org.kuali.common.util.execute.Executable;
+
+public class DumpExecutable implements Executable {
+
+	@Override
+	public void execute() {
+	}
+
+}
