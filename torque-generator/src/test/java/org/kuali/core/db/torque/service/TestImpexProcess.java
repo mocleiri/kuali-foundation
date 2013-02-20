7d9e624d284c85b9956352e02cbc143e7a30bcba
andrewlubbers <andrewlubbers@kuali.org>
2013-02-19 19:49:53 -0500

    KSENROLL-4573 Updated to build ImpexContext from spring context
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16699 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/test/java/org/kuali/core/db/torque/service/TestImpexProcess.java
+++ torque-generator/src/test/java/org/kuali/core/db/torque/service/TestImpexProcess.java
@@ -17,8 +17,15 @@ package org.kuali.core.db.torque.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kuali.common.impex.DatabaseContext;
+import org.kuali.common.impex.DumpTableResult;
+import org.kuali.common.impex.ImpexContextFactoryBean;
+import org.kuali.common.impex.service.DefaultImpexService;
 import org.kuali.common.impex.service.ImpexContext;
+import org.kuali.common.impex.service.ImpexService;
+import org.kuali.common.impex.service.ImpexUtils;
 import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.FormatUtils;
 import org.kuali.common.util.LocationUtils;
 import org.kuali.common.util.LoggerUtils;
 import org.kuali.common.util.PropertyUtils;
@@ -33,6 +40,9 @@ import org.springframework.util.Assert;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -52,14 +62,28 @@ public class TestImpexProcess {
     @Resource(name = "deploy.properties")
     private Properties dbProperties;
 
+    @Resource
+    private ImpexContext impexContext;
+
     @Test
     public void test() throws Exception {
-        logger.info("Here's a test!");
-        System.out.println("Here's a test SYSOUT!");
+        logger.info("Starting database dump");
 
-        Properties p = getImpexProperties("*", 5, 15);
-        ImpexContext sourceContext = getImpexContext(p);
-        log(sourceContext);
+        log(impexContext);
+
+        List<ImpexContext> contexts = Collections.singletonList(impexContext);
+
+        ImpexService service = new DefaultImpexService();
+        DatabaseContext database = service.getDatabaseObjectLists(impexContext);
+        service.fillInMetaData(impexContext, database);
+        service.serializeSchemas(contexts, database);
+        // service.generateDataDtds(contexts);
+        service.generateSchemaSql(contexts, Arrays.asList("oracle", "mysql"));
+        List<DumpTableResult> results = service.dumpTables(impexContext, database);
+
+        ImpexUtils.doStats(results);
+
+
     }
 
     protected void log(ImpexContext context) {
@@ -79,101 +103,6 @@ public class TestImpexProcess {
         logger.info("Sequence Includes - {}", CollectionUtils.getSpaceSeparatedString(context.getSequenceIncludes()));
         logger.info("Sequence Excludes - {}", CollectionUtils.getSpaceSeparatedString(context.getSequenceExcludes()));
         logger.info("---------------------------------------------------------------");
-    }
-
-    protected DataSource getDataSource(Properties p) {
-        DriverManagerDataSource dmds = new DriverManagerDataSource();
-        dmds.setDriverClassName(p.getProperty("impex.driver"));
-        dmds.setPassword(p.getProperty("impex.password"));
-        dmds.setUsername(p.getProperty("impex.username"));
-        dmds.setUrl(p.getProperty("impex.url"));
-        return dmds;
-    }
-
-    protected Properties getImpexProperties(String includes, int metaDataThreads, int dataThreads) throws Exception {
-        String tableIncludes = includes;
-        String viewIncludes = includes;
-        String sequenceIncludes = includes;
-
-        Properties p = new Properties();
-        p.setProperty("project.basedir", mavenProperties.getProperty("project.basedir"));
-        p.setProperty("project.build.directory", p.getProperty("project.basedir") + "/target");
-        p.setProperty("project.artifactId", mavenProperties.getProperty("project.artifactId"));
-        p.setProperty("impex.antCompatibilityMode", "false");
-        p.setProperty("impex.table.includes", tableIncludes);
-        p.setProperty("impex.view.includes", viewIncludes);
-        p.setProperty("impex.sequence.includes", sequenceIncludes);
-        p.setProperty("impex.url", dbProperties.getProperty("jdbc.url"));
-        p.setProperty("impex.driver", dbProperties.getProperty("jdbc.driver"));
-        p.setProperty("impex.username", dbProperties.getProperty("jdbc.username"));
-        p.setProperty("impex.password", p.getProperty("impex.username"));
-        p.setProperty("impex.schema", p.getProperty("impex.username"));
-        p.setProperty("impex.databaseVendor", mavenProperties.getProperty("db.vendor"));
-        p.setProperty("impex.metadata.threads", metaDataThreads + "");
-        p.setProperty("impex.data.threads", dataThreads + "");
-        p.setProperty("impex.workingDir", p.getProperty("project.build.directory") + "/impex");
-        p.setProperty("impex.databaseTablePropertiesFile", p.getProperty("project.basedir") + "/src/main/resources/" + p.getProperty("project.artifactId") + ".properties");
-        return p;
-    }
-
-    protected ImpexContext getImpexContext(Properties p) {
-        ImpexContext context = new ImpexContext();
-        // simple property copying
-        context.setArtifactId(p.getProperty("project.artifactId"));
-        context.setSchema(p.getProperty("impex.schema"));
-        context.setDriver(p.getProperty("impex.driver"));
-        context.setUrl(p.getProperty("impex.url"));
-        context.setUsername(p.getProperty("impex.username"));
-        context.setPassword(p.getProperty("impex.password"));
-        context.setDatabaseVendor(p.getProperty("impex.databaseVendor"));
-        context.setWorkingDir(new File(p.getProperty("impex.workingDir")));
-        context.setBaseDir(new File(p.getProperty("project.basedir")));
-        context.setBuildDir(new File(p.getProperty("project.build.directory")));
-        context.setDatabaseTablePropertiesLocation(p.getProperty("impex.databaseTablePropertiesFile"));
-
-        // Default to [artifactId].xml
-        context.setSchemaXmlFile(new File(context.getWorkingDir(), context.getArtifactId() + ".xml"));
-
-        // Setup the datasource
-        context.setDataSource(getDataSource(p));
-
-        // Properties that already have default values, don't override unless the corresponding property is explicitly set
-        if (p.getProperty("impex.dateFormat") != null) {
-            context.setDateFormat(p.getProperty("impex.dateFormat"));
-        }
-        if (p.getProperty("impex.comment") != null) {
-            context.setComment(p.getProperty("impex.comment"));
-        }
-        if (p.getProperty("impex.schemaXMLFile") != null) {
-            context.setSchemaXmlFile(new File(p.getProperty("impex.schemaXMLFile")));
-        }
-        if (p.getProperty("impex.metadata.threads") != null) {
-            context.setMetaDataThreads(new Integer(p.getProperty("impex.metadata.threads")));
-        }
-        if (p.getProperty("impex.data.threads") != null) {
-            context.setDataThreads(new Integer(p.getProperty("impex.data.threads")));
-        }
-        if (p.getProperty("impex.antCompatibilityMode") != null) {
-            context.setAntCompatibilityMode(new Boolean(p.getProperty("impex.antCompatibilityMode")));
-        }
-        if (p.getProperty("impex.encoding") != null) {
-            context.setEncoding(p.getProperty("impex.encoding"));
-        }
-
-        // Properties that need processing in some way
-        Assert.notNull(context.getDatabaseTablePropertiesLocation());
-        if (LocationUtils.exists(context.getDatabaseTablePropertiesLocation())) {
-            context.setDatabaseTableProperties(PropertyUtils.load(context.getDatabaseTablePropertiesLocation()));
-        } else {
-            context.setDatabaseTableProperties(new Properties());
-        }
-        context.setTableIncludes(CollectionUtils.getTrimmedListFromCSV(p.getProperty("impex.table.includes")));
-        context.setTableExcludes(CollectionUtils.getTrimmedListFromCSV(p.getProperty("impex.table.excludes")));
-        context.setSequenceIncludes(CollectionUtils.getTrimmedListFromCSV(p.getProperty("impex.sequence.includes")));
-        context.setSequenceExcludes(CollectionUtils.getTrimmedListFromCSV(p.getProperty("impex.sequence.excludes")));
-        context.setViewIncludes(CollectionUtils.getTrimmedListFromCSV(p.getProperty("impex.view.includes")));
-        context.setViewExcludes(CollectionUtils.getTrimmedListFromCSV(p.getProperty("impex.view.excludes")));
-        return context;
     }
 
 }
