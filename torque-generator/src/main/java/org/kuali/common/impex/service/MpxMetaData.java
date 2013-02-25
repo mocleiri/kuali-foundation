70f72dbde7665fe3e18f64e8d8717bb4e1ccc437
andrewlubbers <andrewlubbers@kuali.org>
2013-02-25 18:08:54 -0500

    KSENROLL-4573
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16815 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ torque-generator/src/main/java/org/kuali/common/impex/service/MpxMetaData.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2011 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.kuali.common.impex.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andy
+ * Date: 2/25/13
+ * Time: 11:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MpxMetaData implements Comparable<MpxMetaData> {
+
+    Integer rowCount;
+    String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(Integer rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    @Override
+    public int compareTo(MpxMetaData o) {
+        return rowCount.compareTo(o.getRowCount());
+    }
+}
