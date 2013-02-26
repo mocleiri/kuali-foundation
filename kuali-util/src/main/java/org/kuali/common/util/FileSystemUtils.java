8f2350f341b6f5295defc93d014f3ac358cd1852
jcaddel <jcaddel@kuali.org>
2013-02-26 11:49:57 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@16839 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/FileSystemUtils.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2010-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.util;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileSystemUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(FileSystemUtils.class);
+
+	public static void syncFiles(File srcDir, File dstDir) {
+		logger.info("sync files");
+	}
+
+}
