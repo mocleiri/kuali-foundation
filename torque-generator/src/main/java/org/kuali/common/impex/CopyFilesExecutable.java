cc7db1a7c04e251f8e3ff7141b0a06ff24889bd7
jcaddel <jcaddel@kuali.org>
2013-02-26 11:39:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16836 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/main/java/org/kuali/common/impex/CopyFilesExecutable.java
+++ torque-generator/src/main/java/org/kuali/common/impex/CopyFilesExecutable.java
@@ -3,6 +3,7 @@ package org.kuali.common.impex;
 import java.util.List;
 
 import org.kuali.common.impex.service.ImpexContext;
+import org.kuali.common.impex.service.ImpexUtils;
 import org.kuali.common.util.execute.Executable;
 
 public class CopyFilesExecutable implements Executable {
@@ -11,6 +12,9 @@ public class CopyFilesExecutable implements Executable {
 
 	@Override
 	public void execute() {
+		for (ImpexContext context : contexts) {
+			ImpexUtils.copyFiles(context);
+		}
 	}
 
 	public List<ImpexContext> getContexts() {
