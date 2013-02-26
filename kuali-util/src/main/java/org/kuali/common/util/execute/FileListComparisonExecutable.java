24c4a21d1d3cf27371173268b1d20da9c3b7e9eb
jcaddel <jcaddel@kuali.org>
2013-02-25 23:35:27 -0500

    Logging tweaks
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@16825 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/execute/FileListComparisonExecutable.java
+++ kuali-util/src/main/java/org/kuali/common/util/execute/FileListComparisonExecutable.java
@@ -15,6 +15,9 @@
 
 package org.kuali.common.util.execute;
 
+import java.util.List;
+import java.util.Properties;
+
 import org.kuali.common.util.CollectionUtils;
 import org.kuali.common.util.ComparisonResults;
 import org.kuali.common.util.LocationUtils;
@@ -24,103 +27,99 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
-import java.util.List;
-import java.util.Properties;
-
 /**
- * This executable compares the files found by the given pattern in two different locations and populates a properties object with
- * information about the comparison
- *
+ * This executable compares the files found by the given pattern in two different locations and populates a properties
+ * object with information about the comparison
+ * 
  * @author andrewlubbers
  */
 public class FileListComparisonExecutable implements Executable {
 
-    private static final Logger logger = LoggerFactory.getLogger(FileListComparisonExecutable.class);
+	private static final Logger logger = LoggerFactory.getLogger(FileListComparisonExecutable.class);
 
-    private String newFilesBaseDir;
-    private String filePattern;
-    private String originalFilesBaseDir;
-    private List<String> propertyNames;
-    private Properties mavenProperties;
+	private String newFilesBaseDir;
+	private String filePattern;
+	private String originalFilesBaseDir;
+	private List<String> propertyNames;
+	private Properties mavenProperties;
 
-    @Override
-    public void execute() {
-        Assert.notNull(mavenProperties);
-        Assert.notNull(propertyNames);
-        Assert.notNull(newFilesBaseDir);
-        Assert.notNull(originalFilesBaseDir);
-        Assert.notNull(filePattern);
+	@Override
+	public void execute() {
+		Assert.notNull(mavenProperties);
+		Assert.notNull(propertyNames);
+		Assert.notNull(newFilesBaseDir);
+		Assert.notNull(originalFilesBaseDir);
+		Assert.notNull(filePattern);
 
-        logger.info("Starting File List Comparison");
-        logger.info("Original files dir: " + originalFilesBaseDir);
-        logger.info("New files dir: " + newFilesBaseDir);
-        logger.info("File pattern: " + filePattern);
-        logger.info("Property names: " + propertyNames);
+		logger.info("Starting File List Comparison");
+		logger.info("Original files dir: " + originalFilesBaseDir);
+		logger.info("New files dir: " + newFilesBaseDir);
+		logger.info("File pattern: " + filePattern);
+		logger.info("Property names: " + propertyNames);
 
-        List<String> filePatterns = CollectionUtils.getTrimmedListFromCSV(filePattern);
-        String[] includesArray = filePatterns.toArray(new String[filePatterns.size()]);
+		List<String> filePatterns = CollectionUtils.getTrimmedListFromCSV(filePattern);
+		String[] includesArray = filePatterns.toArray(new String[filePatterns.size()]);
 
-        SimpleScanner scanner = new SimpleScanner();
-        scanner.setBasedir(newFilesBaseDir);
-        scanner.setIncludes(includesArray);
+		SimpleScanner scanner = new SimpleScanner();
+		scanner.setBasedir(newFilesBaseDir);
+		scanner.setIncludes(includesArray);
 
-        List<String> newLocations = LocationUtils.getAbsolutePaths(scanner.getFiles());
-        List<String> newFileNames = LocationUtils.getFilenames(newLocations);
+		List<String> newLocations = LocationUtils.getAbsolutePaths(scanner.getFiles());
+		List<String> newFileNames = LocationUtils.getFilenames(newLocations);
 
-        scanner = new SimpleScanner();
-        scanner.setBasedir(originalFilesBaseDir);
-        scanner.setIncludes(includesArray);
+		scanner = new SimpleScanner();
+		scanner.setBasedir(originalFilesBaseDir);
+		scanner.setIncludes(includesArray);
 
-        List<String> originalLocations = LocationUtils.getAbsolutePaths(scanner.getFiles());
-        List<String> originalFileNames = LocationUtils.getFilenames(originalLocations);
+		List<String> originalLocations = LocationUtils.getAbsolutePaths(scanner.getFiles());
+		List<String> originalFileNames = LocationUtils.getFilenames(originalLocations);
 
-        ComparisonResults comparison = LocationUtils.getLocationListComparison(newFileNames, originalFileNames);
+		ComparisonResults comparison = LocationUtils.getLocationListComparison(newFileNames, originalFileNames);
 
-        logger.info("Comparison finished.  Results:");
-        logger.info("Added: " + comparison.getAdded());
-        logger.info("Same " + comparison.getSame());
-        logger.info("Deleted " + comparison.getDeleted());
+		logger.info("Added {} files.", comparison.getAdded().size());
+		logger.info("Updated {} existing files.", comparison.getSame().size());
+		logger.info("Deleted {} files.", comparison.getDeleted().size());
 
-        PropertyUtils.addListComparisonProperties(mavenProperties, comparison, propertyNames);
-    }
+		PropertyUtils.addListComparisonProperties(mavenProperties, comparison, propertyNames);
+	}
 
-    public String getFilePattern() {
-        return filePattern;
-    }
+	public String getFilePattern() {
+		return filePattern;
+	}
 
-    public void setFilePattern(String filePattern) {
-        this.filePattern = filePattern;
-    }
+	public void setFilePattern(String filePattern) {
+		this.filePattern = filePattern;
+	}
 
-    public Properties getMavenProperties() {
-        return mavenProperties;
-    }
+	public Properties getMavenProperties() {
+		return mavenProperties;
+	}
 
-    public void setMavenProperties(Properties mavenProperties) {
-        this.mavenProperties = mavenProperties;
-    }
+	public void setMavenProperties(Properties mavenProperties) {
+		this.mavenProperties = mavenProperties;
+	}
 
-    public String getNewFilesBaseDir() {
-        return newFilesBaseDir;
-    }
+	public String getNewFilesBaseDir() {
+		return newFilesBaseDir;
+	}
 
-    public void setNewFilesBaseDir(String newFilesBaseDir) {
-        this.newFilesBaseDir = newFilesBaseDir;
-    }
+	public void setNewFilesBaseDir(String newFilesBaseDir) {
+		this.newFilesBaseDir = newFilesBaseDir;
+	}
 
-    public String getOriginalFilesBaseDir() {
-        return originalFilesBaseDir;
-    }
+	public String getOriginalFilesBaseDir() {
+		return originalFilesBaseDir;
+	}
 
-    public void setOriginalFilesBaseDir(String originalFilesBaseDir) {
-        this.originalFilesBaseDir = originalFilesBaseDir;
-    }
+	public void setOriginalFilesBaseDir(String originalFilesBaseDir) {
+		this.originalFilesBaseDir = originalFilesBaseDir;
+	}
 
-    public List<String> getPropertyNames() {
-        return propertyNames;
-    }
+	public List<String> getPropertyNames() {
+		return propertyNames;
+	}
 
-    public void setPropertyNames(List<String> propertyNames) {
-        this.propertyNames = propertyNames;
-    }
+	public void setPropertyNames(List<String> propertyNames) {
+		this.propertyNames = propertyNames;
+	}
 }
