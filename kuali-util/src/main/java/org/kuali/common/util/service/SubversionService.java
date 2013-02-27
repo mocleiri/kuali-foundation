08c4560ebdec6d8b6c4bd870b227957fbbde36d7
jcaddel <jcaddel@kuali.org>
2013-02-26 21:54:06 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@16886 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/service/SubversionService.java
+++ kuali-util/src/main/java/org/kuali/common/util/service/SubversionService.java
@@ -1,12 +1,36 @@
 package org.kuali.common.util.service;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class SubversionService implements ScmService {
+import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.LocationUtils;
+
+public class SubversionService extends DefaultExecService implements ScmService {
+
+	private static final String SVN = "svn";
 
 	@Override
 	public void add(List<File> files) {
+		if (CollectionUtils.isEmpty(files)) {
+			// Nothing to do
+			return;
+		}
+		String command = "add";
+		List<String> paths = LocationUtils.getCanonicalPaths(files);
+		List<String> options = Arrays.asList("--force", "--parents");
+
+		List<String> arguments = new ArrayList<String>();
+		arguments.add(command);
+		arguments.addAll(paths);
+		arguments.addAll(options);
+
+		int exitValue = execute(SVN, arguments);
+		if (exitValue != 0) {
+			throw new IllegalStateException("Non-zero exit value - " + exitValue);
+		}
 	}
 
 	@Override
