65a125656aadd5f5a0db423368c224a778f45e8f
jcaddel <jcaddel@kuali.org>
2013-02-26 22:07:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@16890 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/service/ScmService.java
+++ kuali-util/src/main/java/org/kuali/common/util/service/ScmService.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public interface ScmService {
 
-	void add(List<File> files);
+	void add(List<File> paths);
 
-	void delete(List<File> files);
+	void delete(List<File> paths);
 
 	void commit(List<File> paths, String message);
 
