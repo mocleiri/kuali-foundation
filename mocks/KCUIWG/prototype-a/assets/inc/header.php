f7370a5b44df3bf920224cc0126af896d4c2e770
clrux <clrux@kuali.org>
2013-02-28 15:00:33 -0500

    Adding files that weren't previously committed
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@16973 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/prototype-a/assets/inc/header.php
@@ -0,0 +1,142 @@
+<!-- Header -->
+<div id="header" class="header" role="banner">
+  <header> 
+    
+    <!-- Global items -->
+    <div id="global" class="clearfix">
+      <div class="brand">
+        <ul class="nav nav-tabs">
+          <li class="active"><a href="#">Researcher</a></li>
+          <li><a href="#">Unit</a></li>
+          <li><a href="#">Central Admin</a></li>
+          <li><a href="#">Maintenance</a></li>
+          <li><a href="#">System Admin</a></li>
+        </ul>
+      </div>
+      <div class="toolset">
+        <ul>
+          <li><a href="#" class="my-account">Logout</a></li>
+          <li><a href="#" class="action-list">Action List</a></li>
+        </ul>
+      </div> 
+    </div>
+    <!-- // --> 
+    
+    <!-- Document items -->
+    <div id="document" class="clearfix">
+      <div class="title">
+        <div class="title-name">
+          <a href="#"><i class="icon-white icon-home"></i>Portal</a> &raquo; Doc #3289 Testing a very long name that should automatically get cutoff at some point
+        </div>
+        <div class="title-author">
+          by quickstart
+        </div>
+      </div>
+    </div>
+
+    <!-- Secondary -->
+
+    <!-- //-->
+    <div id="secondary" class="clearfix">
+      
+      <div class="meta">
+        <ul>
+          <li><span>Created:</span>2 Feb 2013</li>
+          <li><span>Updated:</span>28 Feb 2013 by Geoff McGregor</li>
+          <li>
+            <a href="#" class="show-popover" rel="popover" data-placement="bottom" data-original-title="Document meta information" data-content="
+            <ul>
+              <li><p><span>Proposal Number:</span>3341</p></li>
+              <li><p><span>Initiator:</span>Megatron</p></li>
+              <li><p><span>PI:</span>Sam Witwicky</p></li>
+              <li><p><span>Sponsor:</span>Optimus Prime</p></li>
+            </ul>
+            ">See more</a>
+          </li>
+        </ul>
+      </div>
+
+      <!-- Tools --> 
+      <div class="btn-group">
+        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">Actions<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#"><i class="icon-ok"></i>Data validation</a></li>
+          <li><a href="#"><i class="icon-list-alt"></i>Proposal hierarchy</a></li>
+          <li><a href="#"><i class="icon-print"></i>Print</a></li>
+          <li><a href="#"><i class="icon-file"></i>Copy document</a></li>
+          <li class="divider"></li>
+          <li><a href="#"><i class="icon-share"></i>Routing</a></li>
+          <li><a href="#"><i class="icon-download-alt"></i>Ad hoc recipients</a></li>
+        </ul>
+      </div>
+
+      <div class="proposal-stats">
+        <span class="show-popover" rel="popover" data-placement="bottom" data-original-title="Proposal completion" data-content="
+          <ul>
+            <li>
+              <strong>Grants.gov</strong>
+              <div class='progress'>
+                <div class='bar' style='width: 40%;'></div>
+              </div>
+            </li>
+            <li>
+              <strong>Key Personnel</strong>
+              <div class='progress'>
+                <div class='bar complete' style='width: 100%;'></div>
+              </div>
+            </li>
+            <li>
+              <strong>Special Review</strong>
+              <div class='progress'>
+                <div class='bar' style='width: 50%;'></div>
+              </div>
+            </li>
+            <li>
+              <strong>Custom Data</strong>
+              <div class='progress'>
+                <div class='bar' style='width: 40%;'></div>
+              </div>
+            </li>
+            <li>
+              <strong>Abstracts &amp; Attachments</strong>
+              <div class='progress'>
+                <div class='bar complete' style='width: 100%;'></div>
+              </div>
+            </li>
+            <li>
+              <strong>Questions</strong>
+              <div class='progress'>
+                <div class='bar' style='width: 80%;'></div>
+              </div>
+            </li>
+            <li>
+              <strong>Budgets</strong>
+              <div class='progress'>
+                <div class='bar' style='width: 10%;'></div>
+              </div>
+            </li>
+            <li>
+              <strong>Permissions</strong>
+              <div class='progress'>
+                <div class='bar' style='width: 40%;'></div>
+              </div>
+            </li>
+            <li>
+              <strong>Medusa</strong>
+              <div class='progress'>
+                <div class='bar' style='width: 40%;'></div>
+              </div>
+            </li>
+          </ul>
+        ">Completion:</span>
+        <div class="progress clearfix">
+          <div class="bar" style="width: 60%;"></div>
+        </div>
+        <span>60%</span>
+      </div>
+      <!-- // --> 
+      
+    </div>
+    <!-- // --> 
+  </header>
+</div>
