98a7047f134ca887b3f7869e615d08e0107bde9e
jcaddel <jcaddel@kuali.org>
2013-03-01 10:55:33 -0500

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@16985 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-executor/src/main/java/org/kuali/common/impex/service/ImpexExecutorService.java
+++ torque-executor/src/main/java/org/kuali/common/impex/service/ImpexExecutorService.java
@@ -1,24 +1,25 @@
 package org.kuali.common.impex.service;
 
-import org.kuali.common.jdbc.context.ExecutionContext;
-
 import java.io.IOException;
 import java.util.List;
 
+import org.kuali.common.jdbc.context.ExecutionContext;
+
 /**
  * This interface provides an API to execute sql from artifacts created by torque-generator
- *
+ * 
  * @author andrewlubbers
  */
 public interface ImpexExecutorService {
 
-    /**
-     * Import data from MPX into database defined in given context
-     *
-     * @param context contains the database connection context and other meta information
-     */
-    List<MpxImportResult> importData(ImportContext context, ExecutionContext sqlExectuionContext) throws IOException;
+	/**
+	 * Import data from MPX into database defined in given context
+	 * 
+	 * @param context
+	 *            contains the database connection context and other meta information
+	 */
+	List<MpxImportResult> importData(ImportContext context, ExecutionContext executionContext) throws IOException;
 
-    MpxImportResult importDataLocation(MpxMetaData metaData, ImportContext context, ExecutionContext sqlExecutionContext);
+	MpxImportResult importDataLocation(MpxMetaData metaData, ImportContext context, ExecutionContext executionContext);
 
 }
