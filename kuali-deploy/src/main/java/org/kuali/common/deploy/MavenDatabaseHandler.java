6e7061a1f66158424ed190ce7098b3045d32535b
jcaddel <jcaddel@kuali.org>
2013-03-01 13:20:28 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17009 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/MavenDatabaseHandler.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/MavenDatabaseHandler.java
@@ -1,9 +1,17 @@
 package org.kuali.common.deploy;
 
+import org.kuali.common.util.service.DefaultMavenService;
+import org.kuali.common.util.service.MavenContext;
+import org.kuali.common.util.service.MavenService;
+
 public class MavenDatabaseHandler implements DatabaseHandler {
+
+	MavenService service = new DefaultMavenService();
+	MavenContext context;
 
 	@Override
 	public void reset() {
+		service.execute(context);
 	}
 
 }
