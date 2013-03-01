78c4285b5c1c00fd1606e354c483a8f428c01b71
jcaddel <jcaddel@kuali.org>
2013-03-01 13:20:51 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17010 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/MavenDatabaseHandler.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/MavenDatabaseHandler.java
@@ -3,6 +3,7 @@ package org.kuali.common.deploy;
 import org.kuali.common.util.service.DefaultMavenService;
 import org.kuali.common.util.service.MavenContext;
 import org.kuali.common.util.service.MavenService;
+import org.springframework.util.Assert;
 
 public class MavenDatabaseHandler implements DatabaseHandler {
 
@@ -11,6 +12,8 @@ public class MavenDatabaseHandler implements DatabaseHandler {
 
 	@Override
 	public void reset() {
+		Assert.notNull(service);
+		Assert.notNull(context);
 		service.execute(context);
 	}
 
