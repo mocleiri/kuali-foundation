ea506fd484af3aecec630d190edc2c576085ab2f
jcaddel <jcaddel@kuali.org>
2013-03-04 09:11:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17160 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-http/src/test/java/org/kuali/common/http/Env16Test.java
@@ -0,0 +1,19 @@
+package org.kuali.common.http;
+
+import org.junit.Test;
+import org.kuali.common.util.service.DefaultSpringService;
+import org.kuali.common.util.service.SpringService;
+
+public class Env16Test {
+
+	@Test
+	public void test() {
+		try {
+			SpringService ss = new DefaultSpringService();
+			ss.load("classpath:org/kuali/common/http/env16-wait-context.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
