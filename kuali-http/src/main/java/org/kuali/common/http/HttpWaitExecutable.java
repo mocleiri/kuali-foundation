aa89ab5ba4a6283476c5c3d928eedbe3b8479235
jcaddel <jcaddel@kuali.org>
2013-03-04 08:30:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17146 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-http/src/main/java/org/kuali/common/http/HttpWaitExecutable.java
+++ kuali-http/src/main/java/org/kuali/common/http/HttpWaitExecutable.java
@@ -1,16 +1,19 @@
 package org.kuali.common.http;
 
+import org.kuali.common.util.Assert;
 import org.kuali.common.util.execute.Executable;
 
 public class HttpWaitExecutable implements Executable {
 
 	HttpContext context;
 	HttpService service = new DefaultHttpService();
-	
+	HttpStatus expected = HttpStatus.SUCCESS;
 
 	@Override
 	public void execute() {
 		HttpWaitResult result = service.wait(context);
+		HttpStatus actual = result.getStatus();
+		Assert.isTrue(expected.equals(actual), "Expected status is [" + expected + "] but actual status is [" + actual + "]");
 	}
 
 	public HttpContext getContext() {
