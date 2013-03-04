73d16273b7bc2db7116202d7900b9676e945ed8d
jcaddel <jcaddel@kuali.org>
2013-03-03 20:11:08 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17097 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-http/src/main/java/org/kuali/common/http/HttpContext.java
+++ kuali-http/src/main/java/org/kuali/common/http/HttpContext.java
@@ -1,5 +1,26 @@
 package org.kuali.common.http;
 
+import java.util.List;
+
 public class HttpContext {
+
+	String url;
+	List<Integer> successCodes;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public List<Integer> getSuccessCodes() {
+		return successCodes;
+	}
+
+	public void setSuccessCodes(List<Integer> successCodes) {
+		this.successCodes = successCodes;
+	}
 
 }
