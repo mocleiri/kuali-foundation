836e91a810349a930dc9772693eb5836be47987d
jcaddel <jcaddel@kuali.org>
2013-03-04 07:55:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17138 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-http/src/main/java/org/kuali/common/http/HttpWaitExecutable.java
+++ kuali-http/src/main/java/org/kuali/common/http/HttpWaitExecutable.java
@@ -2,7 +2,7 @@ package org.kuali.common.http;
 
 import org.kuali.common.util.execute.Executable;
 
-public class WaitExecutable implements Executable {
+public class HttpWaitExecutable implements Executable {
 
 	HttpContext context;
 	HttpService service = new DefaultHttpService();
