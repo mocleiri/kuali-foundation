1b4fcb8b14c837be83d16f0589e68d8530dbc0cd
jcaddel <jcaddel@kuali.org>
2013-03-05 11:27:03 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-2.0@17256 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/supplier/SqlSupplier.java
+++ src/main/java/org/kuali/common/jdbc/supplier/SqlSupplier.java
@@ -2,6 +2,9 @@ package org.kuali.common.jdbc.supplier;
 
 public interface SqlSupplier {
 
+	/**
+	 * Return a single SQL statement to execute. Return <code>null</code> when the SQL supply has been exhausted.
+	 */
 	String getSql();
 
 }
