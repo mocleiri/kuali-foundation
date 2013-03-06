972e460e6acd15f3d6bd6a753edc61ef6ff609a4
jcaddel <jcaddel@kuali.org>
2013-03-06 14:48:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@17318 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/supplier/LocationExtensionMapping.java
+++ src/main/java/org/kuali/common/jdbc/supplier/LocationExtensionMapping.java
@@ -4,6 +4,7 @@ public class LocationExtensionMapping {
 
 	String extension;
 	Class<? extends SqlSupplier> supplierClass;
+	Object sourceBean;
 
 	public String getExtension() {
 		return extension;
@@ -19,6 +20,14 @@ public class LocationExtensionMapping {
 
 	public void setSupplierClass(Class<? extends SqlSupplier> supplierClass) {
 		this.supplierClass = supplierClass;
+	}
+
+	public Object getSourceBean() {
+		return sourceBean;
+	}
+
+	public void setSourceBean(Object sourceBean) {
+		this.sourceBean = sourceBean;
 	}
 
 }
--- src/main/java/org/kuali/common/jdbc/supplier/SqlSupplierFactoryBean.java
+++ src/main/java/org/kuali/common/jdbc/supplier/SqlSupplierFactoryBean.java
@@ -1,31 +1,34 @@
 package org.kuali.common.jdbc.supplier;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.kuali.common.jdbc.SqlReader;
 import org.kuali.common.util.Assert;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.FactoryBean;
 
-public class SqlSupplierFactoryBean implements FactoryBean<SqlSupplier> {
+public class SqlSupplierFactoryBean implements FactoryBean<List<SqlSupplier>> {
 
-	String location;
-	SqlReader reader;
-	String encoding;
+	List<String> locations;
 	List<LocationExtensionMapping> mappings;
 
 	@Override
-	public SqlSupplier getObject() throws Exception {
-		Assert.notBlank(location, "location is null");
+	public List<SqlSupplier> getObject() throws Exception {
+		Assert.notNull(locations, "locations is null");
 		Assert.notNull(mappings, "mappings is null");
-		String extension = FilenameUtils.getExtension(location);
-		LocationExtensionMapping mapping = getMapping(extension, mappings);
-		Class<? extends SqlSupplier> supplierClass = mapping.getSupplierClass();
-		SqlSupplier instance = supplierClass.newInstance();
-		BeanUtils.copyProperties(this, instance);
-		return instance;
+		List<SqlSupplier> suppliers = new ArrayList<SqlSupplier>();
+		for (String location : locations) {
+			String extension = FilenameUtils.getExtension(location);
+			LocationExtensionMapping mapping = getMapping(extension, mappings);
+			Class<? extends SqlSupplier> supplierClass = mapping.getSupplierClass();
+			SqlSupplier instance = supplierClass.newInstance();
+			if (mapping.getSourceBean() != null) {
+				BeanUtils.copyProperties(mapping.getSourceBean(), instance);
+			}
+		}
+		return suppliers;
 	}
 
 	protected LocationExtensionMapping getMapping(String extension, List<LocationExtensionMapping> mappings) {
@@ -38,45 +41,13 @@ public class SqlSupplierFactoryBean implements FactoryBean<SqlSupplier> {
 	}
 
 	@Override
-	public Class<? extends SqlSupplier> getObjectType() {
-		return SqlSupplier.class;
+	public Class<?> getObjectType() {
+		return List.class;
 	}
 
 	@Override
 	public boolean isSingleton() {
 		return false;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public SqlReader getReader() {
-		return reader;
-	}
-
-	public void setReader(SqlReader reader) {
-		this.reader = reader;
-	}
-
-	public String getEncoding() {
-		return encoding;
-	}
-
-	public void setEncoding(String encoding) {
-		this.encoding = encoding;
-	}
-
-	public List<LocationExtensionMapping> getMappings() {
-		return mappings;
-	}
-
-	public void setMappings(List<LocationExtensionMapping> mappings) {
-		this.mappings = mappings;
 	}
 
 }
