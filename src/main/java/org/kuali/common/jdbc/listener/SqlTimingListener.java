aef88858cd2febcbd30ea559243be4649382b53b
jcaddel <jcaddel@kuali.org>
2013-03-07 08:20:17 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@17401 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/kuali/common/jdbc/listener/SqlTimingListener.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.jdbc.listener;
+
+import org.kuali.common.jdbc.context.JdbcContext;
+
+/**
+ * Print a dot to the console each time 1% of the SQL finishes executing
+ */
+public class SqlTimingListener implements SqlListener {
+
+	long elapsed;
+
+	@Override
+	public void beforeMetaData(JdbcContext context) {
+	}
+
+	@Override
+	public void beforeExecution(SqlExecutionEvent event) {
+	}
+
+	@Override
+	public void bucketsCreated(BucketEvent event) {
+	}
+
+	@Override
+	public void beforeExecuteSql(SqlEvent event) {
+	}
+
+	@Override
+	public synchronized void afterExecuteSql(SqlEvent event) {
+		this.elapsed += event.getStopTimeMillis() - event.getStartTimeMillis();
+	}
+
+	@Override
+	public void afterExecution(SqlExecutionEvent event) {
+	}
+
+}
