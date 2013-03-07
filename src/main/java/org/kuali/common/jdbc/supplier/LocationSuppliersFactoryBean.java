259b6b416154d0b6d17cce6d6c0c993f138f68db
jcaddel <jcaddel@kuali.org>
2013-03-07 09:56:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@17412 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/supplier/LocationSuppliersFactoryBean.java
+++ src/main/java/org/kuali/common/jdbc/supplier/LocationSuppliersFactoryBean.java
@@ -3,24 +3,41 @@ package org.kuali.common.jdbc.supplier;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.LocationUtils;
+import org.kuali.common.util.PropertyUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.FactoryBean;
 
 public class LocationSuppliersFactoryBean implements FactoryBean<List<LocationSupplier>> {
 
-	List<String> locations;
-	Map<String, LocationSupplierSourceBean> mappings;
+	public static final String DEFAULT_LIST_SUFFIX = ".list";
+
+	String listSuffix = DEFAULT_LIST_SUFFIX;
+	Properties properties;
+	String prefix;
+	Map<String, LocationSupplierSourceBean> extensionMappings;
 
 	@Override
 	public List<LocationSupplier> getObject() throws Exception {
 
 		// Make sure we are configured correctly
-		Assert.notNull(locations, "locations is null");
-		Assert.notNull(mappings, "mappings is null");
+		Assert.notNull(properties, "properties is null");
+		Assert.notNull(prefix, "prefix is null");
+		Assert.notNull(extensionMappings, "mappings is null");
 
+		// Get a list of locations using properties, prefix, and listSuffix
+		List<String> locations = getLocations(properties, prefix, listSuffix);
+
+		// Convert the locations into LocationSupplier's based on extension
+		return getSuppliers(locations, extensionMappings);
+	}
+
+	protected List<LocationSupplier> getSuppliers(List<String> locations, Map<String, LocationSupplierSourceBean> mappings) {
 		// Cycle through the list of locations, creating one supplier per location
 		List<LocationSupplier> suppliers = new ArrayList<LocationSupplier>();
 		for (String location : locations) {
@@ -38,7 +55,7 @@ public class LocationSuppliersFactoryBean implements FactoryBean<List<LocationSu
 
 			// Create a new instance of the impl class
 			Class<? extends LocationSupplier> supplierClass = sourceBean.getSupplierClass();
-			LocationSupplier implementation = supplierClass.newInstance();
+			LocationSupplier implementation = getNewInstance(supplierClass);
 
 			// Configure the impl with default properties from the source bean
 			if (sourceBean.getSupplierInstance() != null) {
@@ -56,6 +73,30 @@ public class LocationSuppliersFactoryBean implements FactoryBean<List<LocationSu
 		return suppliers;
 	}
 
+	protected <T> T getNewInstance(Class<T> instanceClass) {
+		try {
+			return instanceClass.newInstance();
+		} catch (IllegalAccessException e) {
+			throw new IllegalArgumentException(e);
+		} catch (InstantiationException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
+	protected List<String> getLocations(Properties properties, String prefix, String listSuffix) {
+		List<String> keys = PropertyUtils.getStartsWithKeys(properties, prefix);
+		List<String> locations = new ArrayList<String>();
+		for (String key : keys) {
+			String value = properties.getProperty(key);
+			if (StringUtils.endsWithIgnoreCase(key, listSuffix)) {
+				locations.addAll(LocationUtils.getLocations(value));
+			} else {
+				locations.add(value);
+			}
+		}
+		return locations;
+	}
+
 	@Override
 	public Class<?> getObjectType() {
 		return List.class;
@@ -66,20 +107,36 @@ public class LocationSuppliersFactoryBean implements FactoryBean<List<LocationSu
 		return false;
 	}
 
-	public List<String> getLocations() {
-		return locations;
+	public String getListSuffix() {
+		return listSuffix;
 	}
 
-	public void setLocations(List<String> locations) {
-		this.locations = locations;
+	public void setListSuffix(String listSuffix) {
+		this.listSuffix = listSuffix;
 	}
 
-	public Map<String, LocationSupplierSourceBean> getMappings() {
-		return mappings;
+	public Properties getProperties() {
+		return properties;
 	}
 
-	public void setMappings(Map<String, LocationSupplierSourceBean> mappings) {
-		this.mappings = mappings;
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public Map<String, LocationSupplierSourceBean> getExtensionMappings() {
+		return extensionMappings;
+	}
+
+	public void setExtensionMappings(Map<String, LocationSupplierSourceBean> extensionMappings) {
+		this.extensionMappings = extensionMappings;
 	}
 
 }
