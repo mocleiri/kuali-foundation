a6378148f576cf88fb9d5eab633f817a50d25005
jcaddel <jcaddel@kuali.org>
2013-03-07 07:43:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@17391 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/kuali/common/jdbc/ShowConfigExecutable.java
@@ -0,0 +1,84 @@
+package org.kuali.common.jdbc;
+
+import javax.sql.DataSource;
+
+import org.kuali.common.jdbc.context.DatabaseProcessContext;
+import org.kuali.common.util.LoggerUtils;
+import org.kuali.common.util.execute.Executable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+
+public class ShowConfigExecutable implements Executable {
+
+	private static final Logger logger = LoggerFactory.getLogger(ShowConfigExecutable.class);
+
+	DatabaseProcessContext context;
+	DataSource dataSource;
+	JdbcService service;
+	boolean skip;
+
+	@Override
+	public void execute() {
+		if (skip) {
+			logger.info("Skipping execution");
+			return;
+		}
+
+		Assert.notNull(service, "service is null");
+		Assert.notNull(context, "context is null");
+		Assert.notNull(dataSource, "dataSource is null");
+
+		logger.info("------------------------------------------------------------------------");
+		logger.info("JDBC Configuration - {}", context.getUrl());
+		logger.info("------------------------------------------------------------------------");
+		logger.info("Vendor - {}", context.getVendor());
+		logger.info("URL - {}", context.getUrl());
+		logger.info("User - {}", LoggerUtils.getUsername(context.getUsername()));
+		logger.info("Password - {}", LoggerUtils.getPassword(context.getUsername(), context.getPassword()));
+		logger.info("DBA URL - {}", context.getDbaUrl());
+		logger.info("DBA User - {}", LoggerUtils.getUsername(context.getDbaUsername()));
+		logger.info("DBA Password - {}", LoggerUtils.getPassword(context.getDbaUsername(), context.getDbaPassword()));
+		JdbcMetaData metadata = service.getJdbcMetaData(dataSource);
+		logger.info("Product Name - {}", metadata.getDatabaseProductName());
+		logger.info("Product Version - {}", metadata.getDatabaseProductVersion());
+		logger.info("Driver - {}", context.getDriver());
+		logger.info("Driver Name - {}", metadata.getDriverName());
+		logger.info("Driver Version - {}", metadata.getDriverVersion());
+		logger.info("SQL Encoding - {}", context.getEncoding());
+		logger.info("------------------------------------------------------------------------");
+	}
+
+	public DatabaseProcessContext getContext() {
+		return context;
+	}
+
+	public void setContext(DatabaseProcessContext context) {
+		this.context = context;
+	}
+
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public JdbcService getService() {
+		return service;
+	}
+
+	public void setService(JdbcService service) {
+		this.service = service;
+	}
+
+	public boolean isSkip() {
+		return skip;
+	}
+
+	public void setSkip(boolean skip) {
+		this.skip = skip;
+	}
+
+}
