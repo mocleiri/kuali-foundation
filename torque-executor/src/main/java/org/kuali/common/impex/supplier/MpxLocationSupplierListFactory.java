b6edc147d273087671f1dfcde60b7b2909c84333
jcaddel <jcaddel@kuali.org>
2013-03-07 16:34:43 -0500

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@17452 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-executor/src/main/java/org/kuali/common/impex/supplier/MpxLocationSupplierListFactory.java
+++ torque-executor/src/main/java/org/kuali/common/impex/supplier/MpxLocationSupplierListFactory.java
@@ -36,91 +36,90 @@ import org.springframework.beans.factory.FactoryBean;
 
 /**
  * This class builds a list of MpxLocationSupplier objects from an ImportContext
- *
+ * 
  * @author andrewlubbers
  */
 public class MpxLocationSupplierListFactory implements FactoryBean<List<MpxLocationSupplier>> {
 
-    private static final Logger logger = LoggerFactory.getLogger(MpxLocationSupplierListFactory.class);
+	private static final Logger logger = LoggerFactory.getLogger(MpxLocationSupplierListFactory.class);
 
-    final ImportContext context;
+	final ImportContext context;
 
-    List<MpxLocationSupplier> mpxLocationSuppliers;
+	List<MpxLocationSupplier> mpxLocationSuppliers;
 
-    public MpxLocationSupplierListFactory(ImportContext contextArg) throws IOException {
-        context = contextArg;
+	public MpxLocationSupplierListFactory(ImportContext contextArg) throws IOException {
+		context = contextArg;
 
-        initialize();
-    }
+		initialize();
+	}
 
-    protected void initialize() throws IOException {
-        Platform platform = PlatformFactory.getPlatformFor(context.getDatabaseVendor());
+	protected void initialize() throws IOException {
+		Platform platform = PlatformFactory.getPlatformFor(context.getDatabaseVendor());
 
-        // extract all mpx locations from the list of location resources
-        List<String> mpxLocations = LocationUtils.getLocations(context.getDataLocations());
+		// extract all mpx locations from the list of location resources
+		List<String> mpxLocations = LocationUtils.getLocations(context.getDataLocations());
 
-        Map<String, Table> locationTableMap = getLocationTableMap(context.getDatabaseVendor(), context.getSchemaXmlLocation(), mpxLocations);
-        mpxLocationSuppliers = new ArrayList<MpxLocationSupplier>(mpxLocations.size());
+		Map<String, Table> locationTableMap = getLocationTableMap(context.getDatabaseVendor(), context.getSchemaXmlLocation(), mpxLocations);
+		mpxLocationSuppliers = new ArrayList<MpxLocationSupplier>(mpxLocations.size());
 
-        // build a map of locations to tables
-        for(String location : mpxLocations) {
-            Table table = locationTableMap.get(location);
+		// build a map of locations to tables
+		for (String location : mpxLocations) {
+			Table table = locationTableMap.get(location);
 
-            SqlProducer sqlProducer = platform.getSqlProducer();
-            sqlProducer.setBatchDataSizeLimit(context.getBatchDataSize());
-            sqlProducer.setBatchRowCountLimit(context.getBatchRowCount());
+			SqlProducer sqlProducer = platform.getSqlProducer();
+			sqlProducer.setBatchDataSizeLimit(context.getBatchDataSize());
+			sqlProducer.setBatchRowCountLimit(context.getBatchRowCount());
 
-            MpxLocationSupplier supplier = new MpxLocationSupplier();
-            supplier.setEncoding(context.getEncoding());
-            supplier.setLocation(location);
-            supplier.setSqlProducer(sqlProducer);
-            supplier.setTable(table);
+			MpxLocationSupplier supplier = new MpxLocationSupplier();
+			supplier.setEncoding(context.getEncoding());
+			supplier.setLocation(location);
+			supplier.setSqlProducer(sqlProducer);
+			supplier.setTable(table);
 
-            mpxLocationSuppliers.add(supplier);
-        }
+			mpxLocationSuppliers.add(supplier);
+		}
 
-    }
+	}
 
-    @SuppressWarnings("unchecked")
-    protected Map<String, Table> getLocationTableMap(String databaseVendor, String tablesXmlLocation, List<String> locations) {
+	protected Map<String, Table> getLocationTableMap(String databaseVendor, String tablesXmlLocation, List<String> locations) {
 
-        Map<String, Table> results = new HashMap<String, Table>(locations.size());
+		Map<String, Table> results = new HashMap<String, Table>(locations.size());
 
-        Database database;
+		Database database;
 
-        try {
-            // Get an xml parser for schema.xml
-            KualiXmlToAppData xmlParser = new KualiXmlToAppData(databaseVendor, "");
+		try {
+			// Get an xml parser for schema.xml
+			KualiXmlToAppData xmlParser = new KualiXmlToAppData(databaseVendor, "");
 
-            // Parse schema.xml into a database object
-            database = xmlParser.parseResource(tablesXmlLocation);
-        } catch (Exception e) {
-            logger.info("Execption thrown when processing xml location: " + tablesXmlLocation);
-            throw new IllegalStateException(e);
-        }
+			// Parse schema.xml into a database object
+			database = xmlParser.parseResource(tablesXmlLocation);
+		} catch (Exception e) {
+			logger.info("Execption thrown when processing xml location: " + tablesXmlLocation);
+			throw new IllegalStateException(e);
+		}
 
-        // Populate map with locations as keys and database table objects as values
-        for(String location : locations) {
-            String filename = LocationUtils.getFilename(location);
-            String tableName = StringUtils.substring(filename, 0, StringUtils.indexOf(filename, "."));
-            results.put(location, database.getTable(tableName));
-        }
+		// Populate map with locations as keys and database table objects as values
+		for (String location : locations) {
+			String filename = LocationUtils.getFilename(location);
+			String tableName = StringUtils.substring(filename, 0, StringUtils.indexOf(filename, "."));
+			results.put(location, database.getTable(tableName));
+		}
 
-        return results;
-    }
+		return results;
+	}
 
-    @Override
-    public List<MpxLocationSupplier> getObject() throws Exception {
-        return mpxLocationSuppliers;
-    }
+	@Override
+	public List<MpxLocationSupplier> getObject() throws Exception {
+		return mpxLocationSuppliers;
+	}
 
-    @Override
-    public Class<?> getObjectType() {
-        return List.class;
-    }
+	@Override
+	public Class<?> getObjectType() {
+		return List.class;
+	}
 
-    @Override
-    public boolean isSingleton() {
-        return true;
-    }
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
 }
