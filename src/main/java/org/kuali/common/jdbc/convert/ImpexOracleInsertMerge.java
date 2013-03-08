a6ca286da1e12af3a0c7df980a8249125eedaaec
jcaddel <jcaddel@kuali.org>
2013-03-08 15:24:14 -0500

    Blankify the 2 converters so things compile.  Need to re-work this after the switch to List<String> getSql() in the underlying interfaces
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@17535 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/convert/ImpexOracleInsertMerge.java
+++ src/main/java/org/kuali/common/jdbc/convert/ImpexOracleInsertMerge.java
@@ -15,67 +15,11 @@
  */
 package org.kuali.common.jdbc.convert;
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.io.IOException;
 
 /**
- * This class provides functionality to merge insert statements into batch insert statements from oracle impex sql files
- * within the SqlConverter framework
- *
+ * This class provides functionality to merge insert statements into batch insert statements from oracle impex sql files within the SqlConverter framework
+ * 
  * @author andrewlubbers
  */
-public class ImpexOracleInsertMerge extends AbstractInsertMergeSqlConverter {
-
-    private static final String INSERT = "INSERT";
-    private static final String DELIMITER = "/";
-    private static final String LF = "\n";
-
-    private static final String OPEN = "INSERT ALL" + LF;
-    private static final String CLOSE = "SELECT * FROM DUAL" + LF + DELIMITER + LF;
-
-    @Override
-    protected String combineInserts(ConversionContext conversionContext, SqlInsertContext sqlInsertContext) throws IOException {
-        String sql = sqlInsertContext.getSql();
-        StringBuilder sb = new StringBuilder();
-        sb.append(OPEN);
-        String trimmed = StringUtils.trimToNull(sql);
-        int length = sb.length();
-        int count = 1;
-        boolean proceed = continueBatch(trimmed, count, length, conversionContext);
-        while (proceed) {
-            String into = getIntoStatement(trimmed);
-            count++;
-            length += into.length();
-            sb.append(into);
-            sql = sqlInsertContext.getReader().getSqlStatement(sqlInsertContext.getInput());
-            trimmed = StringUtils.trimToNull(sql);
-            proceed = continueBatch(trimmed, count, length, conversionContext);
-        }
-        // The last SQL statement we read was an insert
-        if (isInsert(trimmed)) {
-            String into = getIntoStatement(trimmed);
-            sb.append(into);
-            count++;
-        }
-        sb.append(CLOSE);
-
-        // There is a trailing SQL statement that is not an INSERT
-        if (trimmed != null && !isInsert(trimmed)) {
-            sb.append(sql).append(LF).append(DELIMITER).append(LF);
-            count++;
-        }
-
-        return sb.toString();
-    }
-
-    @Override
-    public String getInsertPrefix() {
-        return INSERT;
-    }
-
-    protected String getIntoStatement(String trimmed) {
-        return "  " + StringUtils.trim(StringUtils.substring(trimmed, INSERT.length())) + LF;
-    }
-
+public class ImpexOracleInsertMerge {
 }
