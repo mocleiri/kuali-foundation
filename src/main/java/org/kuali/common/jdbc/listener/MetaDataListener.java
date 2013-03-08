26ae6a3c551c79abfb61e4214e9923e8925fff38
jcaddel <jcaddel@kuali.org>
2013-03-08 10:13:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@17504 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/kuali/common/jdbc/listener/MetaDataListener.java
@@ -0,0 +1,24 @@
+package org.kuali.common.jdbc.listener;
+
+import java.util.List;
+
+import org.kuali.common.jdbc.context.JdbcContext;
+import org.kuali.common.jdbc.supplier.SqlSupplier;
+import org.kuali.common.util.FormatUtils;
+
+public class MetaDataListener extends NoOpSqlListener {
+
+	@Override
+	public void beforeMetaData(SqlMetaDataEvent event) {
+		JdbcContext context = event.getContext();
+		List<SqlSupplier> suppliers = context.getSuppliers();
+		System.out.print("[INFO] Accumulating SQL metadata for " + suppliers.size() + " suppliers...");
+	}
+
+	@Override
+	public void afterMetaData(SqlMetaDataEvent event) {
+		String elapsed = FormatUtils.getTime(event.getStopTimeMillis() - event.getStartTimeMillis());
+		System.out.println("[Total time: " + elapsed + "]");
+	}
+
+}
