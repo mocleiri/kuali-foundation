30b767b21ed5a21564cc32a301f57c360d26bab6
jcaddel <jcaddel@kuali.org>
2013-03-08 11:42:23 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@17520 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/DefaultJdbcService.java
+++ src/main/java/org/kuali/common/jdbc/DefaultJdbcService.java
@@ -164,7 +164,7 @@ public class DefaultJdbcService implements JdbcService {
 		String wTime = FormatUtils.getTime(wallTime);
 		String diff = FormatUtils.getTime(aggregateTime - wallTime);
 		Object[] args = { buckets.size(), aTime, wTime, diff };
-		logger.info("Threads: {}  Aggregate: {}  Wall: {}  Diff: {}", args);
+		logger.info("Threads Summary - [t:{} agg:{} wall:{} diff:{}]", args);
 	}
 
 	@Override
