19328a025bbc021a53a5899ab12a9aafd6dd8ae6
jcaddel <jcaddel@kuali.org>
2013-03-09 12:46:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@17597 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/main/java/org/kuali/common/impex/SchemaRequestHandler.java
+++ torque-generator/src/main/java/org/kuali/common/impex/SchemaRequestHandler.java
@@ -41,7 +41,7 @@ public class SchemaRequestHandler implements ElementHandler<SchemaRequestBucket>
 					Long nextVal = platform.getSequenceNextVal(metaData.getConnection(), schema, sequence.getName());
 					sequence.setNextVal(nextVal.toString());
 				}
-				element.getProgressTracker().progressOccurred();
+				element.getProgressTracker().update(1);
 			}
 		} catch (Exception e) {
 			throw new IllegalStateException(e);
