6289590d9f2225e8e6b0eef86751059f8b44b2b1
jcaddel <jcaddel@kuali.org>
2013-03-09 17:52:54 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@17601 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/listener/ProgressListener.java
+++ src/main/java/org/kuali/common/jdbc/listener/ProgressListener.java
@@ -15,112 +15,44 @@
  */
 package org.kuali.common.jdbc.listener;
 
-import java.io.PrintStream;
-
 import org.kuali.common.jdbc.JdbcUtils;
+import org.kuali.common.util.PercentCompleteInformer;
 
 /**
  * Print a dot to the console each time 1% of the SQL finishes executing
  */
 public class ProgressListener extends NoOpSqlListener {
 
-	PrintStream out = System.out;
-	long count = 0;
-	long total = 0;
-	int percentageIncrement = 1;
-	int percentCompletePrevious;
-	String startToken = "[INFO] Progress: ";
-	String progressToken = ".";
-	String completeToken = "\n";
-
-	public void setTotal(long total) {
-		this.total = total;
-	}
+	PercentCompleteInformer informer = new PercentCompleteInformer();
 
 	@Override
 	public synchronized void beforeExecution(SqlExecutionEvent event) {
 		// The total number of SQL statements being executed
-		this.total = JdbcUtils.getSqlCount(event.getContext().getSuppliers());
+		informer.setTotal(JdbcUtils.getSqlCount(event.getContext().getSuppliers()));
 	}
 
 	@Override
 	public synchronized void afterExecuteSql(SqlEvent event) {
+		informer.incrementProgress();
 		// The first SQL statement was just executed
-		if (count == 0) {
-			out.print(startToken);
+		if (informer.getProgress() == 0) {
+			informer.begin();
 		}
 
 		// Increment the counter
-		this.count++;
-
-		// Print a dot anytime we make 1% progress
-		int percentComplete = (int) ((count * 100) / total);
-		if (enoughProgress(percentComplete)) {
-			this.percentCompletePrevious = percentComplete;
-			out.print(progressToken);
-		}
+		informer.incrementProgress();
 
 		// The last SQL statement was just executed
-		if (count == total) {
-			out.print(completeToken);
+		if (informer.getProgress() == informer.getTotal()) {
+			informer.end();
 		}
 	}
 
-	protected boolean enoughProgress(int percentComplete) {
-		int needed = percentCompletePrevious + percentageIncrement;
-		return percentComplete >= needed;
+	public PercentCompleteInformer getInformer() {
+		return informer;
 	}
 
-	public PrintStream getOut() {
-		return out;
+	public void setInformer(PercentCompleteInformer informer) {
+		this.informer = informer;
 	}
-
-	public void setOut(PrintStream out) {
-		this.out = out;
-	}
-
-	public long getCount() {
-		return count;
-	}
-
-	public void setCount(long count) {
-		this.count = count;
-	}
-
-	public int getPercentageIncrement() {
-		return percentageIncrement;
-	}
-
-	public void setPercentageIncrement(int percentageIncrement) {
-		this.percentageIncrement = percentageIncrement;
-	}
-
-	public String getStartToken() {
-		return startToken;
-	}
-
-	public void setStartToken(String startToken) {
-		this.startToken = startToken;
-	}
-
-	public String getCompleteToken() {
-		return completeToken;
-	}
-
-	public void setCompleteToken(String completeToken) {
-		this.completeToken = completeToken;
-	}
-
-	public String getProgressToken() {
-		return progressToken;
-	}
-
-	public void setProgressToken(String progressToken) {
-		this.progressToken = progressToken;
-	}
-
-	public long getTotal() {
-		return total;
-	}
-
 }
