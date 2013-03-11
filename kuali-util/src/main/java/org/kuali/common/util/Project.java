66403de64b2d490e33877230ef8f28d8ac8db971
jcaddel <jcaddel@kuali.org>
2013-03-11 17:03:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17760 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/Project.java
@@ -0,0 +1,42 @@
+package org.kuali.common.util;
+
+public class Project {
+
+	String organizationId;
+	String groupId;
+	String artifactId;
+	String version;
+
+	public String getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	}
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+
+	public void setArtifactId(String artifactId) {
+		this.artifactId = artifactId;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+}
