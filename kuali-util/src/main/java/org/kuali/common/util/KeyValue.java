49b294353852e7e1c9dbf6868fecfc934d7887bc
jcaddel <jcaddel@kuali.org>
2013-03-11 16:50:03 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17758 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/KeyValue.java
@@ -0,0 +1,24 @@
+package org.kuali.common.util;
+
+public class KeyValue {
+
+	String key;
+	String value;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
