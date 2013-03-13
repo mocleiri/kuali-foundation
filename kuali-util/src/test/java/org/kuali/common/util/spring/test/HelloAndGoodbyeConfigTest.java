0e1ce814f56a14702ae98e42e6e2acbc964327b5
jcaddel <jcaddel@kuali.org>
2013-03-13 11:22:25 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17869 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/test/HelloAndGoodbyeConfigTest.java
@@ -0,0 +1,24 @@
+package org.kuali.common.util.spring.test;
+
+import org.junit.Test;
+import org.kuali.common.util.spring.beans.Message;
+import org.kuali.common.util.spring.config.HelloWorldConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class HelloAndGoodbyeConfigTest {
+
+	@Test
+	public void test() {
+		try {
+			AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+			Message helloWorld = ctx.getBean("helloWorld", Message.class);
+			Message goodbye = ctx.getBean("goodbye", Message.class);
+			System.out.println(helloWorld.getMessage());
+			System.out.println(goodbye.getMessage());
+			ctx.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
