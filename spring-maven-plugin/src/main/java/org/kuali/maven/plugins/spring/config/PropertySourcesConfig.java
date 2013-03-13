b636fe0274720ba7c549e0c5422ea97d2df5242d
jcaddel <jcaddel@kuali.org>
2013-03-13 18:58:37 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17934 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/PropertySourcesConfig.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/PropertySourcesConfig.java
@@ -1,17 +1,17 @@
 package org.kuali.maven.plugins.spring.config;
 
-import org.kuali.maven.plugins.spring.AbstractSpringMojo;
+import org.kuali.maven.plugins.spring.LoadMojo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.PropertiesPropertySource;
 
 @Configuration
 public class PropertySourcesConfig {
 
 	@Bean
-	public PropertiesPropertySource propertySource() {
-		PropertiesPropertySource pps = new PropertiesPropertySource(AbstractSpringMojo.DEFAULT_MAVEN_PROPERTIES_BEAN_NAME, null);
-		return pps;
+	@Autowired(required = true)
+	public LoadMojo mojo() {
+		return null;
 	}
 
 }
