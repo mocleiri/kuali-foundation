70ff5e396265784000c6d7bbe21d9b4ece0bf770
jcaddel <jcaddel@kuali.org>
2013-03-13 19:15:59 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17938 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/ReflectionUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/ReflectionUtils.java
@@ -2,6 +2,15 @@ package org.kuali.common.util;
 
 public class ReflectionUtils extends org.springframework.util.ReflectionUtils {
 
+	public static Object newInstance(String className) {
+		try {
+			Class<?> clazz = Class.forName(className);
+			return newInstance(clazz);
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
 	public static <T> T newInstance(Class<T> instanceClass) {
 		try {
 			return (T) instanceClass.newInstance();
