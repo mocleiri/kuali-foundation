d12d857fc901363514ed7e1fed40030b7a56c280
jcaddel <jcaddel@kuali.org>
2013-03-13 19:57:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17957 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/XmlLoadMojoConfig.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/XmlLoadMojoConfig.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(BaseConfig.class)
 public class XmlLoadMojoConfig {
 
 	@Autowired
