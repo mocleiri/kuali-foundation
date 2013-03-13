e83e8fcb4c3f280525a853fa819664a1816e7e33
jcaddel <jcaddel@kuali.org>
2013-03-13 18:17:38 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17919 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/ReflectionUtils.java
@@ -0,0 +1,15 @@
+package org.kuali.common.util;
+
+public class ReflectionUtils extends org.springframework.util.ReflectionUtils {
+
+	public static <T> T newInstance(Class<T> instanceClass) {
+		try {
+			return (T) instanceClass.newInstance();
+		} catch (IllegalAccessException e) {
+			throw new IllegalStateException("Unexpected error", e);
+		} catch (InstantiationException e) {
+			throw new IllegalStateException("Unexpected error", e);
+		}
+	}
+
+}
