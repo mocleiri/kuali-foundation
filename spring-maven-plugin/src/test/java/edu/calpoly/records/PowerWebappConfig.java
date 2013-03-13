8c804d0393426a839541704a52d1955c0a183ce8
jcaddel <jcaddel@kuali.org>
2013-03-13 19:34:38 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17947 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/edu/calpoly/records/PowerWebappConfig.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/PowerWebappConfig.java
@@ -1,8 +1,16 @@
 package edu.calpoly.records;
 
+import org.kuali.common.util.execute.Executable;
+import org.kuali.common.util.execute.HelloWorldExecutable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PowerWebappConfig {
+
+	@Bean(initMethod = "execute")
+	public Executable helloWorldExecutable() {
+		return new HelloWorldExecutable();
+	}
 
 }
