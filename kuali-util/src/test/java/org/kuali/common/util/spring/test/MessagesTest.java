add127913c2fb84c7ab93fcfb3730280c83a3e02
jcaddel <jcaddel@kuali.org>
2013-03-13 11:29:36 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17873 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/test/MessagesTest.java
@@ -0,0 +1,28 @@
+package org.kuali.common.util.spring.test;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.kuali.common.util.spring.beans.Message;
+import org.kuali.common.util.spring.config.HelloAndGoodbyeConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MessagesTest {
+
+	@Test
+	public void test() {
+		try {
+			AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(HelloAndGoodbyeConfig.class);
+			Map<String, Message> messages = ctx.getBeansOfType(Message.class);
+			for (Map.Entry<String, Message> pair : messages.entrySet()) {
+				String key = pair.getKey();
+				Message message = pair.getValue();
+				System.out.println(key + "=" + message.getMessage());
+			}
+			ctx.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
