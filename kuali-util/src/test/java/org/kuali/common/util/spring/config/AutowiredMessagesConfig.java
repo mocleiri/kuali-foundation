41b143a01735875fa2c213fa34eec32107264209
jcaddel <jcaddel@kuali.org>
2013-03-13 11:59:15 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17879 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/config/AutowiredMessagesConfig.java
@@ -0,0 +1,56 @@
+package org.kuali.common.util.spring.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.common.util.execute.Executable;
+import org.kuali.common.util.spring.beans.DefaultMessage;
+import org.kuali.common.util.spring.beans.Message;
+import org.kuali.common.util.spring.beans.PrintMessagesExecutable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AutowiredMessagesConfig {
+
+	@Bean
+	public String helloWorldString() {
+		return "Hello World";
+	}
+
+	@Bean
+	public String goodbyeString() {
+		return "Good bye";
+	}
+
+	@Bean
+	public Message helloWorldMessage() {
+		DefaultMessage message = new DefaultMessage();
+		message.setMessage(helloWorldString());
+		return message;
+	}
+
+	@Bean
+	public Message goodbyeMessage() {
+		DefaultMessage message = new DefaultMessage();
+		message.setMessage(goodbyeString());
+		return message;
+	}
+
+	@Bean
+	@Autowired
+	public List<Message> messages() {
+		return null;
+	}
+
+	@Bean(initMethod = "execute")
+	public Executable printMessagesExecutable() {
+		PrintMessagesExecutable pme = new PrintMessagesExecutable();
+		List<Message> messages = new ArrayList<Message>();
+		messages.add(helloWorldMessage());
+		messages.add(goodbyeMessage());
+		pme.setMessages(messages);
+		return pme;
+	}
+}
