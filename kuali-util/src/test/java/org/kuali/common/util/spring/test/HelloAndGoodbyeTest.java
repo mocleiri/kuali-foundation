c0e94b95abc11f640a2bab2bd08ccfabf7f25e85
jcaddel <jcaddel@kuali.org>
2013-03-13 11:24:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17872 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/test/HelloAndGoodbyeTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/test/HelloAndGoodbyeTest.java
@@ -2,7 +2,7 @@ package org.kuali.common.util.spring.test;
 
 import org.junit.Test;
 import org.kuali.common.util.spring.beans.Message;
-import org.kuali.common.util.spring.config.HelloWorldConfig;
+import org.kuali.common.util.spring.config.HelloAndGoodbyeConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class HelloAndGoodbyeTest {
@@ -10,7 +10,7 @@ public class HelloAndGoodbyeTest {
 	@Test
 	public void test() {
 		try {
-			AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+			AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(HelloAndGoodbyeConfig.class);
 			Message helloWorld = ctx.getBean("helloWorldMessage", Message.class);
 			Message goodbye = ctx.getBean("goodbyeMessage", Message.class);
 			System.out.println(helloWorld.getMessage());
