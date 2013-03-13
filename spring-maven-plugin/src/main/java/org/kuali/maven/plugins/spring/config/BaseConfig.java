b5b2be5e4c88f31cd0cef5125eaf0f0bac15a878
jcaddel <jcaddel@kuali.org>
2013-03-13 19:58:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17958 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/BaseConfig.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/BaseConfig.java
@@ -3,10 +3,8 @@ package org.kuali.maven.plugins.spring.config;
 import org.kuali.maven.plugins.spring.SpringMojoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(BaseConfig.class)
 public class BaseConfig {
 
 	@Bean
