495dcfc9afd58ed50e4e53e41f75e109294ba412
jcaddel <jcaddel@kuali.org>
2013-03-13 09:15:08 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17836 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/service/PropertySourceTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/service/PropertySourceTest.java
@@ -23,9 +23,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.PropertySource;
 
-public class SpringServiceTest {
+public class PropertySourceTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(SpringServiceTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(PropertySourceTest.class);
 
 	@Test
 	public void test() {
