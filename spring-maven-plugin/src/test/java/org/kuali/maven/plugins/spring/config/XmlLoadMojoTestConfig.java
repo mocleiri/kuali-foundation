1f0d183aa37a7974ab80e098b839e50aeaf5c80c
jcaddel <jcaddel@kuali.org>
2013-03-13 18:33:06 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17927 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/org/kuali/maven/plugins/spring/config/XmlLoadMojoTestConfig.java
+++ spring-maven-plugin/src/test/java/org/kuali/maven/plugins/spring/config/XmlLoadMojoTestConfig.java
@@ -1,21 +1,13 @@
 package org.kuali.maven.plugins.spring.config;
 
-import java.io.File;
-import java.util.Properties;
-
-import org.apache.maven.model.Build;
-import org.apache.maven.model.CiManagement;
-import org.apache.maven.model.IssueManagement;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.project.MavenProject;
-import org.kuali.common.util.LocationUtils;
 import org.kuali.maven.plugins.spring.XmlLoadMojo;
-import org.kuali.maven.plugins.spring.TestableMavenProject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class XmlLoadMojoTestConfig {
+public class XmlLoadMojoTestConfig extends BaseMojoTestConfig {
 
 	@Bean(initMethod = "execute")
 	public AbstractMojo mojo() {
@@ -26,65 +18,4 @@ public class XmlLoadMojoTestConfig {
 		mojo.setLocation("classpath:" + project.getArtifactId() + "-context.xml");
 		return mojo;
 	}
-
-	@Bean
-	public CiManagement ci() {
-		CiManagement ci = new CiManagement();
-		ci.setUrl("http://ci.rice.kuali.org");
-		ci.setSystem("jenkins");
-		return ci;
-	}
-
-	@Bean
-	public File basedir() {
-		return new File(".");
-	}
-
-	@Bean
-	public Build build() {
-		Build build = new Build();
-		build.setSourceDirectory(LocationUtils.getCanonicalPath(new File(basedir(), "src/main")));
-		build.setDirectory(LocationUtils.getCanonicalPath(new File(basedir(), "target")));
-		build.setOutputDirectory(LocationUtils.getCanonicalPath(new File(basedir(), "target/classes")));
-		build.setTestOutputDirectory(LocationUtils.getCanonicalPath(new File(basedir(), "target/test-classes")));
-		build.setScriptSourceDirectory(LocationUtils.getCanonicalPath(new File(basedir(), "src/scripts")));
-		build.setTestSourceDirectory(LocationUtils.getCanonicalPath(new File(basedir(), "src/test")));
-		return build;
-	}
-
-	@Bean
-	public IssueManagement im() {
-		IssueManagement im = new IssueManagement();
-		im.setSystem("jira");
-		im.setUrl("http://jira.kuali.org");
-		return im;
-	}
-
-	@Bean
-	public String artifactId() {
-		return "spring-maven-plugin";
-	}
-
-	@Bean
-	public Properties properties() {
-		Properties properties = new Properties();
-		properties.setProperty("spring.message", "Hello");
-		return properties;
-	}
-
-	@Bean
-	public MavenProject mavenProject() {
-		TestableMavenProject project = new TestableMavenProject();
-		project.setArtifactId(artifactId());
-		project.setProperties(properties());
-		project.setPackaging("jar");
-		project.setDescription("description");
-		project.setInceptionYear("2013");
-		project.setCiManagement(ci());
-		project.setIssueManagement(im());
-		project.setBasedir(basedir());
-		project.setBuild(build());
-		return project;
-	}
-
 }
