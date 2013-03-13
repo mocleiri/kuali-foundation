7f505ffdf6105d2d10853dad3eced41bd581c266
jcaddel <jcaddel@kuali.org>
2013-03-13 15:15:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17906 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/LoadMojoConfig.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/LoadMojoConfig.java
@@ -1,10 +1,8 @@
 package org.kuali.maven.plugins.spring.config;
 
 import org.kuali.common.util.execute.Executable;
-import org.kuali.maven.plugins.spring.LoadMojo;
 import org.kuali.maven.plugins.spring.LoadMojoExecutable;
 import org.kuali.maven.plugins.spring.LoadMojoService;
-import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +12,6 @@ public class LoadMojoConfig {
 	@Bean
 	public LoadMojoService service() {
 		return new LoadMojoService();
-	}
-
-	@Bean(autowire = Autowire.BY_NAME)
-	public LoadMojo mojo() {
-		return null;
 	}
 
 	@Bean(initMethod = "execute")
