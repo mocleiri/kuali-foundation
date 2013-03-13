5efb61224c848a4bec2bcfbcc65a68b8f41e2a59
jcaddel <jcaddel@kuali.org>
2013-03-13 14:32:35 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17902 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/org/kuali/maven/plugins/spring/LoadMojoServiceTest.java
+++ spring-maven-plugin/src/test/java/org/kuali/maven/plugins/spring/LoadMojoServiceTest.java
@@ -1,62 +1,17 @@
 package org.kuali.maven.plugins.spring;
 
-import java.io.File;
-import java.util.Properties;
-
-import org.apache.maven.model.Build;
-import org.apache.maven.model.CiManagement;
-import org.apache.maven.model.IssueManagement;
-import org.apache.maven.project.MavenProject;
 import org.junit.Test;
-import org.kuali.common.util.LocationUtils;
+import org.kuali.common.util.service.DefaultSpringService;
+import org.kuali.common.util.service.SpringService;
+import org.kuali.maven.plugins.spring.config.LoadMojoServiceTestConfig;
 
 public class LoadMojoServiceTest {
-
-	protected MavenProject getMavenProject() {
-		Properties properties = new Properties();
-		properties.setProperty("spring.message", "Hello");
-
-		String artifactId = "spring-maven-plugin";
-
-		CiManagement ci = new CiManagement();
-		ci.setUrl("http://ci.rice.kuali.org");
-		ci.setSystem("jenkins");
-
-		IssueManagement im = new IssueManagement();
-		im.setSystem("jira");
-		im.setUrl("http://jira.kuali.org");
-
-		File basedir = new File(".");
-
-		Build build = new Build();
-		build.setSourceDirectory(LocationUtils.getCanonicalPath(new File(basedir, "src/main")));
-		build.setDirectory(LocationUtils.getCanonicalPath(new File(basedir, "target")));
-		build.setOutputDirectory(LocationUtils.getCanonicalPath(new File(basedir, "target/classes")));
-		build.setTestOutputDirectory(LocationUtils.getCanonicalPath(new File(basedir, "target/test-classes")));
-		build.setScriptSourceDirectory(LocationUtils.getCanonicalPath(new File(basedir, "src/scripts")));
-		build.setTestSourceDirectory(LocationUtils.getCanonicalPath(new File(basedir, "src/test")));
-
-		TestableMavenProject project = new TestableMavenProject();
-		project.setArtifactId(artifactId);
-		project.setProperties(properties);
-		project.setPackaging("jar");
-		project.setDescription("description");
-		project.setInceptionYear("2013");
-		project.setCiManagement(ci);
-		project.setIssueManagement(im);
-		project.setBasedir(basedir);
-		project.setBuild(build);
-		return project;
-	}
 
 	@Test
 	public void test() {
 		try {
-			MavenProject project = getMavenProject();
-			LoadMojo mojo = new LoadMojo();
-			mojo.setProject(project);
-			mojo.setLocation("classpath:" + project.getArtifactId() + "-context.xml");
-			mojo.execute();
+			SpringService ss = new DefaultSpringService();
+			ss.load(LoadMojoServiceTestConfig.class);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
