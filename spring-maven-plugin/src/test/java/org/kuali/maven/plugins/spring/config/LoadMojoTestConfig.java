7ea334ec3fdc91305f0ce0ce80da94c59ebe577c
jcaddel <jcaddel@kuali.org>
2013-03-14 12:46:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18054 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/org/kuali/maven/plugins/spring/config/LoadMojoTestConfig.java
+++ spring-maven-plugin/src/test/java/org/kuali/maven/plugins/spring/config/LoadMojoTestConfig.java
@@ -3,15 +3,21 @@ package org.kuali.maven.plugins.spring.config;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.project.MavenProject;
 import org.kuali.maven.plugins.spring.LoadMojo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-public class LoadMojoTestConfig extends BaseMojoTestConfig {
+@Import(BaseMojoTestConfig.class)
+public class LoadMojoTestConfig {
+
+	@Autowired
+	BaseMojoTestConfig baseConfig;
 
 	@Bean(initMethod = "execute")
 	public AbstractMojo mojo() {
-		MavenProject project = mavenProject();
+		MavenProject project = baseConfig.mavenProject();
 
 		LoadMojo mojo = new LoadMojo();
 		mojo.setProject(project);
