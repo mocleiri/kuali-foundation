9250fd696b0d42da002b822a14fc09f56b87fc2c
jcaddel <jcaddel@kuali.org>
2013-03-13 22:48:02 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@17980 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/AbstractSpringMojo.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/AbstractSpringMojo.java
@@ -28,36 +28,69 @@ public abstract class AbstractSpringMojo extends AbstractMojo {
 	@Component
 	MavenProject project;
 
+	/**
+	 * If true, any PropertySource's supplied to the mojo are added to the configured Spring environment.
+	 */
 	@Parameter(property = "spring.addPropertySources", defaultValue = MavenConstants.DEFAULT_ADD_PROPERTY_SOURCES)
 	boolean addPropertySources = new Boolean(MavenConstants.DEFAULT_ADD_PROPERTY_SOURCES);
 
+	/**
+	 * If true, Maven properties are injected into the context as a java.util.Properties bean
+	 */
 	@Parameter(property = "spring.injectMavenProperties", defaultValue = MavenConstants.DEFAULT_INJECT_MAVEN_PROPERTIES)
 	boolean injectMavenProperties = new Boolean(MavenConstants.DEFAULT_INJECT_MAVEN_PROPERTIES);
 
+	/**
+	 * If true, the Maven project object is injected into the context
+	 */
 	@Parameter(property = "spring.injectMavenProject", defaultValue = MavenConstants.DEFAULT_INJECT_MAVEN_PROJECT)
 	boolean injectMavenProject = new Boolean(MavenConstants.DEFAULT_INJECT_MAVEN_PROJECT);
 
+	/**
+	 * If true, the Maven project object is injected into the context
+	 */
 	@Parameter(property = "spring.injectMavenMojo", defaultValue = MavenConstants.DEFAULT_INJECT_MAVEN_MOJO)
 	boolean injectMavenMojo = new Boolean(MavenConstants.DEFAULT_INJECT_MAVEN_MOJO);
 
+	/**
+	 * The name under which the Maven properties object is injected
+	 */
 	@Parameter(property = "spring.mavenPropertiesBeanName", defaultValue = MavenConstants.DEFAULT_MAVEN_PROPERTIES_BEAN_NAME)
 	String mavenPropertiesBeanName = MavenConstants.DEFAULT_MAVEN_PROPERTIES_BEAN_NAME;
 
+	/**
+	 * The name under which the Maven project object is injected
+	 */
 	@Parameter(property = "spring.mavenProjectBeanName", defaultValue = MavenConstants.DEFAULT_MAVEN_PROJECT_BEAN_NAME)
 	String mavenProjectBeanName = MavenConstants.DEFAULT_MAVEN_PROJECT_BEAN_NAME;
 
+	/**
+	 * The name under which this mojo is injected
+	 */
 	@Parameter(property = "spring.mavenMojoBeanName", defaultValue = MavenConstants.DEFAULT_MAVEN_MOJO_BEAN_NAME)
 	String mavenMojoBeanName = MavenConstants.DEFAULT_MAVEN_MOJO_BEAN_NAME;
 
+	/**
+	 * The implementation of SpringService to use. If not supplied this defaults to <code>org.kuali.common.util.service.DefaultSpringService</code>
+	 */
 	@Parameter(property = "spring.serviceClass")
 	Class<? extends SpringService> serviceClass = MavenConstants.DEFAULT_SERVICE_CLASS;
 
+	/**
+	 * Extra properties supplied directly to the mojo
+	 */
 	@Parameter(property = "spring.properties")
 	Properties properties;
 
+	/**
+	 * If true, the mojo will always execute
+	 */
 	@Parameter(property = "spring.forceMojoExecution", defaultValue = MavenConstants.DEFAULT_FORCE_MOJO_EXECUTION)
 	boolean forceMojoExecution = new Boolean(MavenConstants.DEFAULT_FORCE_MOJO_EXECUTION);
 
+	/**
+	 * If true, the mojo execution will always be skipped (unless forceMojoExecution is true)
+	 */
 	@Parameter(property = "spring.skip", defaultValue = MavenConstants.DEFAULT_SKIP)
 	boolean skip = new Boolean(MavenConstants.DEFAULT_SKIP);
 
