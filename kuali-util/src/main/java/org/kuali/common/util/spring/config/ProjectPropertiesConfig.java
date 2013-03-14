52626e3b8fbae66fb80a7189cd8f7f6e2fcb6d27
jcaddel <jcaddel@kuali.org>
2013-03-14 14:46:40 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18095 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/config/ProjectPropertiesConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/config/ProjectPropertiesConfig.java
@@ -1,8 +1,22 @@
 package org.kuali.common.util.spring.config;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProjectPropertiesConfig {
+
+	@Autowired
+	@Qualifier("mavenProperties")
+	Properties mavenProperties;
+
+	@Bean
+	public Properties augmentedProjectProperties() {
+		return mavenProperties;
+	}
 
 }
