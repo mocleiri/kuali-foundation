ef571a5d51ded7290998d59b9ead6d9d123bd123
jcaddel <jcaddel@kuali.org>
2013-03-14 14:47:48 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18096 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/config/ProjectPropertiesConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/config/ProjectPropertiesConfig.java
@@ -2,6 +2,7 @@ package org.kuali.common.util.spring.config;
 
 import java.util.Properties;
 
+import org.kuali.common.util.Str;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,9 @@ public class ProjectPropertiesConfig {
 
 	@Bean
 	public Properties augmentedProjectProperties() {
+		String groupId = mavenProperties.getProperty("project.groupId");
+		String groupIdPath = Str.getPath(groupId);
+		mavenProperties.setProperty("project.groupId.path", groupIdPath);
 		return mavenProperties;
 	}
 
