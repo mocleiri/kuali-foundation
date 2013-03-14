4bbdf2d0664f27a2ea1e738b2ec9a63d13eb1fa9
jcaddel <jcaddel@kuali.org>
2013-03-14 13:23:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18071 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PowerWebappConfig.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PowerWebappConfig.java
@@ -1,17 +1,22 @@
 package edu.calpoly.records.spring;
 
 import org.kuali.common.util.execute.Executable;
-import org.kuali.common.util.execute.PrintMessageExecutable;
+import org.kuali.common.util.spring.config.PrintMessageConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(PrintMessageConfig.class)
 public class PowerWebappConfig {
+
+	@Autowired
+	PrintMessageConfig pmc;
 
 	@Bean(initMethod = "execute")
 	public Executable printMessageExecutable() {
-		PrintMessageExecutable pme = new PrintMessageExecutable();
-		return pme;
+		return pmc.printMessageExecutable();
 	}
 
 }
