87da827d039005a8f6ead89ca156636154bc2801
jcaddel <jcaddel@kuali.org>
2013-03-14 15:19:05 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18103 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PowerWebappConfig.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PowerWebappConfig.java
@@ -1,8 +1,14 @@
 package edu.calpoly.records.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 import org.kuali.common.util.execute.Executable;
-import org.kuali.common.util.execute.ExecutableExecutable;
+import org.kuali.common.util.execute.ExecutablesExecutable;
+import org.kuali.maven.plugins.spring.MavenConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -15,16 +21,30 @@ public class PowerWebappConfig {
 	@Autowired
 	PrintMessageConfig pmc;
 
+	@Autowired
+	@Qualifier(MavenConstants.DEFAULT_MAVEN_PROPERTIES_BEAN_NAME)
+	Properties mavenProperties;
+
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer pspc() {
+	public PropertySourcesPlaceholderConfigurer pspc() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
 
-	@Bean(initMethod = "execute")
-	public Executable executableExecutable() {
-		ExecutableExecutable ee = new ExecutableExecutable();
-		ee.setExecutable(pmc.printMessageExecutable());
-		return ee;
+	@Bean
+	public Executable showProperties() {
+		ShowPropertiesExecutable spe = new ShowPropertiesExecutable();
+		spe.setProperties(mavenProperties);
+		return spe;
 	}
 
+	@Bean(initMethod = "execute")
+	public Executable executablesExecutable() {
+		List<Executable> executables = new ArrayList<Executable>();
+		executables.add(pmc.printMessageExecutable());
+		executables.add(showProperties());
+
+		ExecutablesExecutable ee = new ExecutablesExecutable();
+		ee.setExecutables(executables);
+		return ee;
+	}
 }
---
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/ShowPropertiesExecutable.java
@@ -0,0 +1,25 @@
+package edu.calpoly.records.spring;
+
+import java.util.Properties;
+
+import org.kuali.common.util.PropertyUtils;
+import org.kuali.common.util.execute.Executable;
+
+public class ShowPropertiesExecutable implements Executable {
+
+	Properties properties;
+
+	@Override
+	public void execute() {
+		PropertyUtils.info(properties);
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+}
