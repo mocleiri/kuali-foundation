3e1ed374c90f2dc4c29896655b2b84be5814cf39
jcaddel <jcaddel@kuali.org>
2013-03-14 14:09:38 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18086 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/config/PrintMessageConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/config/PrintMessageConfig.java
@@ -2,25 +2,20 @@ package org.kuali.common.util.spring.config;
 
 import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.execute.PrintMessageExecutable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Configuration
 public class PrintMessageConfig {
 
-	@Autowired
-	Environment env;
-
-	@Value("${print.message}")
-	String printMessage;
+	@Value("${print.message:NONE}")
+	String message;
 
 	@Bean
 	public Executable printMessageExecutable() {
 		PrintMessageExecutable pme = new PrintMessageExecutable();
-		pme.setMessage(printMessage);
+		pme.setMessage(message);
 		return pme;
 	}
 
