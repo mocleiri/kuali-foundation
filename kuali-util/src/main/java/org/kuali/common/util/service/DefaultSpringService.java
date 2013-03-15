74232e6ae4d999dc608776364cab6ae5abfb8ac8
jcaddel <jcaddel@kuali.org>
2013-03-14 21:41:12 -0400

    Tone down logging
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18176 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/service/DefaultSpringService.java
+++ kuali-util/src/main/java/org/kuali/common/util/service/DefaultSpringService.java
@@ -240,7 +240,7 @@ public class DefaultSpringService implements SpringService {
 		for (int i = 0; i < beanNames.size(); i++) {
 			String beanName = beanNames.get(i);
 			Object bean = beans.get(i);
-			logger.info("Registering bean - [{}] -> [{}]", beanName, bean.getClass().getName());
+			logger.debug("Registering bean - [{}] -> [{}]", beanName, bean.getClass().getName());
 			factory.registerSingleton(beanName, bean);
 		}
 		return appContext;
@@ -262,7 +262,7 @@ public class DefaultSpringService implements SpringService {
 			Collections.reverse(propertySources);
 		}
 		for (PropertySource<?> propertySource : propertySources) {
-			logger.info("Adding property source - [{}] -> [{}]", propertySource.getName(), propertySource.getClass().getName());
+			logger.debug("Adding property source - [{}] -> [{}]", propertySource.getName(), propertySource.getClass().getName());
 			sources.addLast(propertySource);
 		}
 	}
