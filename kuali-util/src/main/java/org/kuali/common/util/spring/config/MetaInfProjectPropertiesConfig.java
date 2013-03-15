0157e38d388a753a0cf3bf99589b10800fd5b5f9
jcaddel <jcaddel@kuali.org>
2013-03-14 21:11:19 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18170 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/config/MetaInfProjectPropertiesConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/config/MetaInfProjectPropertiesConfig.java
@@ -1,15 +1,23 @@
 package org.kuali.common.util.spring.config;
 
 import java.io.File;
+import java.util.Properties;
 
 import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.execute.StorePropertiesExecutable;
+import org.kuali.common.util.property.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetaInfProjectPropertiesConfig {
+
+	@Autowired
+	@Qualifier(Constants.DEFAULT_MAVEN_PROPERTIES_BEAN_NAME)
+	Properties mavenProperties;
 
 	@Value(value = "${project.build.outputDirectory}/META-INF/${project.orgId.path}/${project.artifactId}.properties")
 	File outputFile;
@@ -22,6 +30,7 @@ public class MetaInfProjectPropertiesConfig {
 		StorePropertiesExecutable spe = new StorePropertiesExecutable();
 		spe.setEncoding(encoding);
 		spe.setOutputFile(outputFile);
+		spe.setProperties(mavenProperties);
 		return spe;
 	}
 }
