aaefe53aea6dbf4d15b41f5251f6717337210281
jcaddel <jcaddel@kuali.org>
2013-03-15 15:14:25 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18212 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/SpringUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/SpringUtils.java
@@ -21,7 +21,7 @@ public class SpringUtils {
 
 	/**
 	 * Get a fully resolved property value from the environment. If the property is not found or contains unresolvable placeholders an exception is thrown. This calls
-	 * <code>getRequiredProperty</code> and <code>resolveRequiredPlaceholders</code> on <code>env</code>.
+	 * <code>env.getRequiredProperty</code> and <code>env.resolveRequiredPlaceholders</code>
 	 */
 	public static String getProperty(Environment env, String key) {
 		String value = env.getRequiredProperty(key);
