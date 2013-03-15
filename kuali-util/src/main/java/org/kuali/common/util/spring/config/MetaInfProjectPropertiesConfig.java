5913025a82aa3355a000269b9dfabbacbfcce773
jcaddel <jcaddel@kuali.org>
2013-03-14 21:19:13 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18171 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/config/MetaInfProjectPropertiesConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/config/MetaInfProjectPropertiesConfig.java
@@ -1,6 +1,8 @@
 package org.kuali.common.util.spring.config;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import org.kuali.common.util.execute.Executable;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 public class MetaInfProjectPropertiesConfig {
@@ -18,6 +21,11 @@ public class MetaInfProjectPropertiesConfig {
 	@Autowired
 	@Qualifier(Constants.DEFAULT_MAVEN_PROPERTIES_BEAN_NAME)
 	Properties mavenProperties;
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer pspc() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 
 	@Value(value = "${project.build.outputDirectory}/META-INF/${project.orgId.path}/${project.artifactId}.properties")
 	File outputFile;
@@ -27,10 +35,15 @@ public class MetaInfProjectPropertiesConfig {
 
 	@Bean(initMethod = "execute")
 	public Executable storePropertiesExecutable() {
+		List<String> includes = Arrays.asList("project.*");
+		List<String> excludes = Arrays.asList("project.basedir", "project.build.*", "project.issueManagement.*", "project.ciManagement.*");
+
 		StorePropertiesExecutable spe = new StorePropertiesExecutable();
 		spe.setEncoding(encoding);
 		spe.setOutputFile(outputFile);
 		spe.setProperties(mavenProperties);
+		spe.setIncludes(includes);
+		spe.setExcludes(excludes);
 		return spe;
 	}
 }
