7f7499bfc4b3c8c50093ba0a8c7a11dcfcfeb60e
jcaddel <jcaddel@kuali.org>
2013-03-15 15:29:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18217 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/config/MetaInfProjectPropertiesConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/config/MetaInfProjectPropertiesConfig.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.util.Assert;
 
@@ -20,6 +21,11 @@ public class MetaInfProjectPropertiesConfig {
 
 	@Autowired
 	ConfigurableEnvironment env;
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer pspc() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 
 	@Value("${project.build.directory}/META-INF/${project.groupId.path}/${project.artifactId}.properties")
 	File outputFile;
