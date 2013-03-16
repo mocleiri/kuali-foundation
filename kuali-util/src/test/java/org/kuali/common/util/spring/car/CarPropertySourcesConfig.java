1a049394f9c325d0e8c66f40754f8be2007de6eb
jcaddel <jcaddel@kuali.org>
2013-03-16 19:42:25 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18365 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/car/CarPropertySourcesConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/car/CarPropertySourcesConfig.java
@@ -1,31 +1,43 @@
 package org.kuali.common.util.spring.car;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
+import org.kuali.common.util.LocationUtils;
 import org.kuali.common.util.PropertyUtils;
+import org.kuali.common.util.property.Constants;
+import org.kuali.common.util.spring.SpringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.util.PropertyPlaceholderHelper;
 
 @Configuration
 public class CarPropertySourcesConfig {
-
-	@Bean
-	public Properties base() {
-		return PropertyUtils.load("classpath:car.properties");
-	}
-
-	@Bean
-	public Properties make() {
-		Properties base = base();
-		String make = base.getProperty("car.make");
-		return PropertyUtils.load("classpath:" + make + ".properties");
-	}
-
+	
+	@Autowired
+	Environment env;
+	
 	@Bean()
 	public PropertiesPropertySource pps() {
+		String encoding = SpringUtils.getProperty(env, "project.encoding");
+		PropertyPlaceholderHelper pph = Constants.DEFAULT_PROPERTY_PLACEHOLDER_HELPER;
+		Properties global = PropertyUtils.getGlobalProperties();
+		Properties source = new Properties();
+		source.putAll(global);
+		List<String> locations = Arrays.asList("classpath:car.properties", "classpath:${car.make}.properties");
+		for (String location : locations) {
+			String resolvedLocation = pph.replacePlaceholders(location, source);
+			boolean exists = LocationUtils.exists(resolvedLocation);
+			if (exists) {
+				source.putAll(PropertyUtils.load(resolvedLocation,encoding));
+				source.putAll(global);
+			}
+		}
 		String name = "springProperties";
-		Properties source = PropertyUtils.combine(base(), make(), PropertyUtils.getEnvAsProperties());
 		return new PropertiesPropertySource(name, source);
 	}
 
