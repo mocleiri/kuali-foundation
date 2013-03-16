d36ab8526000fb3f612928db12f75dde6703a310
jcaddel <jcaddel@kuali.org>
2013-03-16 15:06:14 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18339 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/config/PropertySourcesPlaceholderConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/config/PropertySourcesPlaceholderConfig.java
@@ -1,47 +1,26 @@
 package org.kuali.common.util.spring.config;
 
-import org.kuali.common.util.LocationUtils;
-import org.kuali.common.util.spring.SpringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.io.Resource;
 
 @Configuration
-@PropertySource(name = "carProperties", value = "classpath:car.properties")
 public class PropertySourcesPlaceholderConfig {
 
-	@Autowired
-	ConfigurableEnvironment env;
-
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer pspc1() {
-		int order = 2;
-		Resource resource = LocationUtils.getResource("classpath:car.properties");
-
-		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-		pspc.setLocations(new Resource[] { resource });
-		pspc.setOrder(order);
-		return pspc;
+	public Object b() {
+		System.out.println("b");
+		return null;
 	}
 
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer pspc2() {
-		int order = 1;
-		Resource resource = LocationUtils.getResource("classpath:chevy.properties");
-
-		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-		pspc.setLocations(new Resource[] { resource });
-		pspc.setOrder(order);
-		return pspc;
+	public Object d() {
+		System.out.println("d");
+		return null;
 	}
 
 	@Bean
-	public Object whatever() {
-		SpringUtils.showPropertySources(env);
+	public Object c() {
+		System.out.println("c");
 		return null;
 	}
 
