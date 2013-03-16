fb21ea89d11008da96956b430cc107716e13f8dc
jcaddel <jcaddel@kuali.org>
2013-03-16 16:32:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18357 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/car/CompositeConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/car/CompositeConfig.java
@@ -4,11 +4,9 @@ import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @Configuration
-@Import({ MakeConfig.class, CarConfig.class })
 public class CompositeConfig {
 
 	@Autowired
