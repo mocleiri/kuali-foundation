ac957619bc8cec7c7ca6c28ad1e0a495ad0e6d95
jcaddel <jcaddel@kuali.org>
2013-03-15 22:23:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18260 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/config/CarConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/config/CarConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @Configuration
-@PropertySource(name = "carProperties", value = "classpath:car.properties")
+@PropertySource(name = "carProperties", value = { "classpath:car.properties", "${car.make}.properties" })
 public class CarConfig {
 
 	@Autowired
