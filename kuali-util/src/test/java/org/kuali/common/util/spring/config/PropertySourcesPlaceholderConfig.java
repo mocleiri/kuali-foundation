2be1eecea80ebc865ec35b3a3a7515be279bdf76
jcaddel <jcaddel@kuali.org>
2013-03-16 14:22:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18336 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/config/PropertySourcesPlaceholderConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/config/PropertySourcesPlaceholderConfig.java
@@ -1,34 +1,48 @@
 package org.kuali.common.util.spring.config;
 
 import org.kuali.common.util.LocationUtils;
+import org.kuali.common.util.spring.SpringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.io.Resource;
 
 @Configuration
+@PropertySource(name = "carProperties", value = "classpath:car.properties")
 public class PropertySourcesPlaceholderConfig {
+
+	@Autowired
+	ConfigurableEnvironment env;
 
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer pspc1() {
-		int order = 1;
+		int order = 2;
 		Resource resource = LocationUtils.getResource("classpath:car.properties");
 
 		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-		pspc.setLocation(resource);
+		pspc.setLocations(new Resource[] { resource });
 		pspc.setOrder(order);
 		return pspc;
 	}
 
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer pspc2() {
-		int order = 2;
+		int order = 1;
 		Resource resource = LocationUtils.getResource("classpath:chevy.properties");
 
 		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-		pspc.setLocation(resource);
+		pspc.setLocations(new Resource[] { resource });
 		pspc.setOrder(order);
 		return pspc;
+	}
+
+	@Bean
+	public Object whatever() {
+		SpringUtils.showPropertySources(env);
+		return null;
 	}
 
 }
