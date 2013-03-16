4534e89ae3cd288deacaf1de118ef5297771d340
jcaddel <jcaddel@kuali.org>
2013-03-16 16:28:27 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18356 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/car/CarConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/car/CarConfig.java
@@ -1,10 +1,8 @@
 package org.kuali.common.util.spring.car;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = "classpath:car.properties")
 public class CarConfig {
 
 }
--- kuali-util/src/test/java/org/kuali/common/util/spring/car/MakeConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/car/MakeConfig.java
@@ -1,10 +1,8 @@
 package org.kuali.common.util.spring.car;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:chevy.properties")
 public class MakeConfig {
 
 }
