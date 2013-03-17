4a0f9be482a3a9b32f0c0137fd13c234bf69da83
jcaddel <jcaddel@kuali.org>
2013-03-17 09:03:59 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18375 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/service/PropertySourceContext.java
+++ kuali-util/src/main/java/org/kuali/common/util/service/PropertySourceContext.java
@@ -1,26 +1,43 @@
 package org.kuali.common.util.service;
 
+import java.util.List;
+
 import org.springframework.core.env.PropertySource;
 
 public class PropertySourceContext {
 
-	PropertySourcePriority priority;
-	PropertySource<?> source;
+	// If true, any existing property sources are removed and replaced by the list from this context
+	boolean removeExistingSources = true;
 
-	public PropertySourcePriority getPriority() {
-		return priority;
+	// If true, the last PropertySource in the list has the highest priority
+	// That is to say, Spring will search for property values starting at the bottom of the list and work its way upwards
+	boolean lastOneInWins = true;
+
+	// The list of property source objects to add to the environment
+	List<PropertySource<?>> sources;
+
+	public boolean isRemoveExistingSources() {
+		return removeExistingSources;
 	}
 
-	public void setPriority(PropertySourcePriority priority) {
-		this.priority = priority;
+	public void setRemoveExistingSources(boolean removeExistingSources) {
+		this.removeExistingSources = removeExistingSources;
 	}
 
-	public PropertySource<?> getSource() {
-		return source;
+	public boolean isLastOneInWins() {
+		return lastOneInWins;
 	}
 
-	public void setSource(PropertySource<?> source) {
-		this.source = source;
+	public void setLastOneInWins(boolean lastOneInWins) {
+		this.lastOneInWins = lastOneInWins;
+	}
+
+	public List<PropertySource<?>> getSources() {
+		return sources;
+	}
+
+	public void setSources(List<PropertySource<?>> sources) {
+		this.sources = sources;
 	}
 
 }
