3c6ef3e027ff5d6ed44e802d9210deed7d107980
jcaddel <jcaddel@kuali.org>
2013-03-17 16:28:07 -0400

    [maven-release-plugin]  copy for tag kuali-pom-2.3.2
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-pom-2.3.2@18451 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- ec2-maven-plugin/src/main/java/org/kuali/maven/ec2/Constants.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.maven.ec2;
-
-public class Constants {
-    public static final String NONE = "NONE";
-    public static final int DEFAULT_SLEEP_MILLIS = 5000;
-    public static final int DEFAULT_INITIAL_PAUSE_MILLIS = 1500;
-
-}
