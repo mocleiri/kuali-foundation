8835cc7a6b32d923aca2253f00f7ff8cc184069e
jcaddel <jcaddel@kuali.org>
2013-03-17 20:09:42 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18478 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/ProjectPropertiesContext.java
@@ -0,0 +1,26 @@
+package org.kuali.common.util;
+
+import java.util.Properties;
+
+public class ProjectPropertiesContext {
+
+	Project project;
+	Properties properties;
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+}
