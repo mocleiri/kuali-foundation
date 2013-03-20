ce59fa2a452a9430f35558a5e416c3ef8cb6f0bc
jcaddel <jcaddel@kuali.org>
2013-03-20 19:43:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18614 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/PropertyLocation.java
@@ -0,0 +1,20 @@
+package org.kuali.common.util;
+
+public class PropertyLocation {
+
+	boolean gav;
+	String location;
+	public boolean isGav() {
+		return gav;
+	}
+	public void setGav(boolean gav) {
+		this.gav = gav;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
