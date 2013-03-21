016abea31d8f7b25c733e0f33e50af5fe88f2ba5
jcaddel <jcaddel@kuali.org>
2013-03-21 15:02:25 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18699 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/ResetCommon.java
+++ src/main/java/org/kuali/common/jdbc/spring/ResetCommon.java
@@ -1,6 +1,9 @@
 package org.kuali.common.jdbc.spring;
 
+import org.kuali.common.jdbc.DefaultSqlReader;
+import org.kuali.common.jdbc.SqlReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
@@ -9,5 +12,10 @@ public class ResetCommon {
 
 	@Autowired
 	Environment env;
+
+	@Bean
+	public SqlReader sqlReader() {
+		return new DefaultSqlReader();
+	}
 
 }
