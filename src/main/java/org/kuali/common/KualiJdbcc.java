5bd3f0e460ae411fa7c63b30580575cb92993000
jcaddel <jcaddel@kuali.org>
2013-03-21 17:23:45 -0400

    Automatically generated java source code - Maven GAV
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18713 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/kuali/common/KualiJdbcc.java
@@ -0,0 +1,9 @@
+package org.kuali.common;
+
+public abstract class KualiJdbcc {
+
+	public static final String GROUP_ID = "org.kuali.common";
+	public static final String ARTIFACT_ID = "kuali-jdbcc";
+	public static final String VERSION = "3.0.0-SNAPSHOT";
+
+}
