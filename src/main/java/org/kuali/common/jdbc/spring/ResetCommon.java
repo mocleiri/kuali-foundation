57377cd225db37eedcc2890070b6118440c63686
jcaddel <jcaddel@kuali.org>
2013-03-21 15:15:47 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18701 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/ResetCommon.java
+++ src/main/java/org/kuali/common/jdbc/spring/ResetCommon.java
@@ -1,9 +1,16 @@
 package org.kuali.common.jdbc.spring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.kuali.common.jdbc.DefaultJdbcService;
 import org.kuali.common.jdbc.DefaultSqlReader;
 import org.kuali.common.jdbc.JdbcService;
 import org.kuali.common.jdbc.SqlReader;
+import org.kuali.common.jdbc.supplier.LocationSupplierSourceBean;
+import org.kuali.common.jdbc.supplier.SqlLocationSupplier;
+import org.kuali.common.util.Project;
+import org.kuali.common.util.ProjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +23,11 @@ public class ResetCommon {
 	Environment env;
 
 	@Bean
+	public Project project() {
+		return ProjectUtils.getProject("org.kuali.common:kuali-jdbc");
+	}
+
+	@Bean
 	public SqlReader sqlReader() {
 		return new DefaultSqlReader();
 	}
@@ -23,6 +35,23 @@ public class ResetCommon {
 	@Bean
 	public JdbcService jdbcService() {
 		return new DefaultJdbcService();
+	}
+
+	@Bean
+	public Map<String, LocationSupplierSourceBean> defaultExtensionMappings() {
+		Project project = project();
+
+		SqlLocationSupplier sls = new SqlLocationSupplier();
+		sls.setReader(sqlReader());
+		sls.setEncoding(project.getEncoding());
+
+		LocationSupplierSourceBean lssb = new LocationSupplierSourceBean();
+		lssb.setSupplierClass(SqlLocationSupplier.class);
+		lssb.setSupplierInstance(sls);
+
+		Map<String, LocationSupplierSourceBean> map = new HashMap<String, LocationSupplierSourceBean>();
+		map.put("sql", lssb);
+		return map;
 	}
 
 }
