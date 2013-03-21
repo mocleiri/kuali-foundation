0eda17b6f0192ad85b05e9c4f38e87d99cbfc3b8
jcaddel <jcaddel@kuali.org>
2013-03-21 16:34:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18704 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/GenerateProjectSourceFileConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/GenerateProjectSourceFileConfig.java
@@ -1,8 +1,13 @@
 package org.kuali.common.util.spring;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.codehaus.plexus.util.StringUtils;
 import org.kuali.common.util.ProjectUtils;
+import org.kuali.common.util.Str;
 import org.kuali.common.util.property.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,16 +18,51 @@ import org.springframework.util.PropertyPlaceholderHelper;
 @Configuration
 public class GenerateProjectSourceFileConfig {
 
+	private static final String FS = File.separator;
+
 	@Autowired
 	@Qualifier(Constants.DEFAULT_MAVEN_PROPERTIES_BEAN_NAME)
 	Properties mavenProperties;
 
 	@Bean
-	public Object foo() {
+	public Object doFile() {
 		String template = ProjectUtils.getJavaSourceFileTemplate();
 		PropertyPlaceholderHelper pph = Constants.DEFAULT_PROPERTY_PLACEHOLDER_HELPER;
 		String source = pph.replacePlaceholders(template, mavenProperties);
+		String filename = getFilename(mavenProperties);
+		File outputFile = new File(filename);
+		try {
+			FileUtils.write(outputFile, source);
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
 		return null;
+	}
+
+	protected String getFilename(Properties p) {
+		String srcDir = p.getProperty("project.build.sourceDirectory");
+		String groupId = p.getProperty("project.groupId");
+		String artifactId = p.getProperty("project.artifactId");
+		String classname = getJavaClassName(artifactId);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(srcDir);
+		sb.append(FS);
+		sb.append(Str.getPath(groupId));
+		sb.append(FS);
+		sb.append(classname);
+		sb.append(".java");
+		return sb.toString();
+	}
+
+	protected String getJavaClassName(String artifactId) {
+		String[] tokens = StringUtils.split(artifactId, "-");
+		StringBuilder sb = new StringBuilder();
+		for (String token : tokens) {
+			String capitalized = StringUtils.capitalizeFirstLetter(token);
+			sb.append(capitalized);
+		}
+		return sb.toString();
 	}
 
 }
