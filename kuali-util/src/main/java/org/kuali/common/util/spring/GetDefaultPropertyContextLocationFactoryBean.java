9565f814f900ca3ac4a5da674464b723dc6f82fd
jcaddel <jcaddel@kuali.org>
2013-03-21 11:32:42 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18662 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/GetDefaultPropertyContextLocationFactoryBean.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/GetDefaultPropertyContextLocationFactoryBean.java
@@ -17,7 +17,7 @@ package org.kuali.common.util.spring;
 
 import org.springframework.beans.factory.FactoryBean;
 
-public class PropertyContextLocationFactoryBean implements FactoryBean<String> {
+public class GetDefaultPropertyContextLocationFactoryBean implements FactoryBean<String> {
 
 	String gav;
 	boolean singleton = true;
