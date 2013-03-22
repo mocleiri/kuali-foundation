9fc79039b3067aed0cc50333620190f9c1e1928b
jcaddel <jcaddel@kuali.org>
2013-03-22 06:26:58 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18842 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/kuali/common/jdbc/spring/ResetDba.java
@@ -0,0 +1,64 @@
+package org.kuali.common.jdbc.spring;
+
+import java.util.Arrays;
+
+import org.kuali.common.jdbc.JdbcExecutable;
+import org.kuali.common.jdbc.context.JdbcContext;
+import org.kuali.common.jdbc.listener.LogSqlListener;
+import org.kuali.common.jdbc.supplier.ComplexStringSupplier;
+import org.kuali.common.jdbc.supplier.SqlSupplier;
+import org.kuali.common.util.execute.Executable;
+import org.kuali.common.util.spring.SpringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@Import({ ResetCommon.class, ResetDataSource.class })
+public class ResetDba {
+
+	@Autowired
+	Environment env;
+
+	@Autowired
+	ResetCommon resetCommon;
+
+	@Autowired
+	ResetDataSource resetDataSource;
+
+	@Bean
+	public Executable jdbcDbaExecutable() {
+		String skip = SpringUtils.getProperty(env, "jdbc.dba.skip", "false");
+
+		JdbcExecutable exec = new JdbcExecutable();
+		exec.setSkip(new Boolean(skip));
+		exec.setService(resetCommon.jdbcService());
+		exec.setContext(getJdbcContext());
+		return exec;
+	}
+
+	protected JdbcContext getJdbcContext() {
+		String skip = SpringUtils.getProperty(env, "sql.dba.skip", "false");
+		JdbcContext ctx = new JdbcContext();
+		ctx.setMessage(SpringUtils.getProperty(env, "sql.dba.message"));
+		ctx.setSkip(new Boolean(skip));
+		ctx.setDataSource(resetDataSource.jdbcDbaDataSource());
+		ctx.setSuppliers(Arrays.asList(getSqlSupplier()));
+		ctx.setListener(new LogSqlListener());
+		return ctx;
+	}
+
+	protected SqlSupplier getSqlSupplier() {
+		String validate = SpringUtils.getProperty(env, "sql.validate");
+		String drop = SpringUtils.getProperty(env, "sql.drop");
+		String create = SpringUtils.getProperty(env, "sql.create");
+
+		ComplexStringSupplier css = new ComplexStringSupplier();
+		css.setReader(resetCommon.jdbcSqlReader());
+		css.setStrings(Arrays.asList(validate, drop, create));
+		return css;
+	}
+
+}
