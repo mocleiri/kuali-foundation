876c47caa9bceec2d524072b0e3332e5c1425948
jcaddel <jcaddel@kuali.org>
2013-03-22 09:23:03 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18857 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/ResetDataConfig.java
+++ src/main/java/org/kuali/common/jdbc/spring/ResetDataConfig.java
@@ -3,12 +3,15 @@ package org.kuali.common.jdbc.spring;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.kuali.common.jdbc.JdbcExecutable;
 import org.kuali.common.jdbc.context.JdbcContext;
 import org.kuali.common.jdbc.listener.DataSummaryListener;
 import org.kuali.common.jdbc.listener.MetaDataListener;
 import org.kuali.common.jdbc.listener.NotifyingListener;
 import org.kuali.common.jdbc.listener.SqlListener;
+import org.kuali.common.jdbc.supplier.SqlSupplier;
 import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +48,8 @@ public class ResetDataConfig {
 		String skip = SpringUtils.getProperty(env, "sql.data.skip", "false");
 		String threads = SpringUtils.getProperty(env, "sql.threads");
 		String message = SpringUtils.getProperty(env, "sql.data.concurrent.message");
+		List<SqlSupplier> suppliers = commonConfig.getSqlSuppliers("sql.data.concurrent");
+		DataSource dataSource = dbaConfig.jdbcDataSource();
 
 		JdbcContext ctx = new JdbcContext();
 		ctx.setTrackProgressByUpdateCount(true);
@@ -52,8 +57,8 @@ public class ResetDataConfig {
 		ctx.setThreads(new Integer(threads));
 		ctx.setMessage(message);
 		ctx.setSkip(new Boolean(skip));
-		ctx.setDataSource(dbaConfig.jdbcDataSource());
-		ctx.setSuppliers(commonConfig.getSqlSuppliers("sql.data.concurrent"));
+		ctx.setDataSource(dataSource);
+		ctx.setSuppliers(suppliers);
 		ctx.setListener(getListener());
 		return ctx;
 	}
