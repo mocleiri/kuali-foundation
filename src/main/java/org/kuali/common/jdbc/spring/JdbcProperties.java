f80e08ada2c095e17bc54c34b57abfcf04e94cd7
jcaddel <jcaddel@kuali.org>
2013-03-22 05:15:02 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18831 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/JdbcProperties.java
+++ src/main/java/org/kuali/common/jdbc/spring/JdbcProperties.java
@@ -1,9 +1,14 @@
 package org.kuali.common.jdbc.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(JdbcProject.class)
 public class JdbcProperties {
 
-	
+	@Autowired
+	JdbcProject jdbcProject;
+
 }
