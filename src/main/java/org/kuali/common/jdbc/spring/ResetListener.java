e4d4d1546d6283a9534ff1a5151c4d226768d176
jcaddel <jcaddel@kuali.org>
2013-03-22 05:06:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18827 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/ResetListener.java
+++ src/main/java/org/kuali/common/jdbc/spring/ResetListener.java
@@ -1,5 +1,11 @@
 package org.kuali.common.jdbc.spring;
 
+import java.util.Arrays;
+
+import org.kuali.common.jdbc.listener.DataSummaryListener;
+import org.kuali.common.jdbc.listener.MetaDataListener;
+import org.kuali.common.jdbc.listener.NotifyingListener;
+import org.kuali.common.jdbc.listener.ProgressListener;
 import org.kuali.common.jdbc.listener.SqlListener;
 import org.kuali.common.jdbc.listener.SummaryListener;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +17,25 @@ public class ResetListener {
 	@Bean
 	public SqlListener jdbcSummaryListener() {
 		return new SummaryListener();
+	}
+
+	@Bean
+	public SqlListener jdbcDataSummaryListener() {
+		return new DataSummaryListener();
+	}
+
+	@Bean
+	public SqlListener jdbcProgressListener() {
+		return new ProgressListener();
+	}
+
+	@Bean
+	public SqlListener jdbcMetaDataSummaryListener() {
+		return new MetaDataListener();
+	}
+
+	@Bean
+	public SqlListener jdbcSummaryAndProgressListener() {
+		return new NotifyingListener(Arrays.asList(jdbcSummaryListener(), jdbcProgressListener()));
 	}
 }
