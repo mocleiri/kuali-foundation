2561797f248c3754fa651c055170cbfcb5268db0
jcaddel <jcaddel@kuali.org>
2013-03-21 20:43:42 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18785 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/KualiUtilGAV.java
+++ kuali-util/src/main/java/org/kuali/common/KualiUtilGAV.java
@@ -3,7 +3,7 @@ package org.kuali.common;
 public abstract class KualiUtilGAV {
 
 	public static final String GROUP_ID = "org.kuali.common";
-	public static final String ARTIFACT_ID = "kuali-util";
+	public static final String ARTIFACT_ID = "kualddi-util";
 	public static final String VERSION = "4.1.6-SNAPSHOT";
 
 }
