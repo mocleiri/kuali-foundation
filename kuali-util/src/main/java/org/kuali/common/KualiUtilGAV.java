01b5d69ce0422cc10a0d97d78385771ff187c657
jcaddel <jcaddel@kuali.org>
2013-03-21 21:27:07 -0400

    [maven-release-plugin]  copy for tag kuali-pom-2.3.7
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-pom-2.3.7@18818 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/KualiUtilGAV.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common;
-
-public abstract class KualiUtilGAV {
-
-	public static final String GROUP_ID = "org.kuali.common";
-	public static final String ARTIFACT_ID = "kuali-util";
-	public static final String VERSION = "4.1.9-SNAPSHOT";
-
-}
