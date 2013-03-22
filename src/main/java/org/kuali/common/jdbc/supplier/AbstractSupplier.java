a350337e5cfa4dc19d035748d48fe86c8e7633b3
jcaddel <jcaddel@kuali.org>
2013-03-22 08:12:35 -0400

    Simplify the comparison logic.  Prevent compareTo from creating unnecessary objects
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18847 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/supplier/AbstractSupplier.java
+++ src/main/java/org/kuali/common/jdbc/supplier/AbstractSupplier.java
@@ -13,9 +13,7 @@ public abstract class AbstractSupplier implements SqlSupplier {
 
 	@Override
 	public int compareTo(SqlSupplier other) {
-		Long one = metaData.getSize();
-		Long two = other.getMetaData().getSize();
-		return one.compareTo(two);
+		return Double.compare(metaData.getSize(), other.getMetaData().getSize());
 	}
 
 }
