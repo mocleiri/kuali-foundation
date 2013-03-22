bf563e2dfdbce3334830c210a473401a9ee6e454
jcaddel <jcaddel@kuali.org>
2013-03-21 20:59:49 -0400

    Automatically generated java source code - Maven GAV
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18800 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/KualiUtilGAV.java
+++ kuali-util/src/main/java/org/kuali/common/KualiUtilGAV.java
@@ -1,13 +1,5 @@
 package org.kuali.common;
 
-/**
- * ############################# WARNING ##############################
- * 
- * This is a generated file. Do NOT edit.
- * 
- * Automated process keeps this source file in sync with the Maven POM.
- * 
- */
 public abstract class KualiUtilGAV {
 
 	public static final String GROUP_ID = "org.kuali.common";
