aa930108b6ffa05f7dcdf24283e05865bab091ac
jcaddel <jcaddel@kuali.org>
2013-03-22 05:21:10 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18832 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/JdbcProperties.java
+++ src/main/java/org/kuali/common/jdbc/spring/JdbcProperties.java
@@ -1,6 +1,14 @@
 package org.kuali.common.jdbc.spring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.kuali.common.util.Project;
+import org.kuali.common.util.property.ProjectProperties;
+import org.kuali.common.util.property.PropertiesLoaderContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -10,5 +18,28 @@ public class JdbcProperties {
 
 	@Autowired
 	JdbcProject jdbcProject;
+
+	@Bean
+	public ProjectProperties jdbcProjectProperties() {
+		Project project = jdbcProject.jdbcProject();
+
+		List<String> locations = new ArrayList<String>();
+		locations.add("classpath:org/kuali/common/jdbc/jdbc.properties");
+		locations.add("classpath:org/kuali/common/jdbc/service.properties");
+		locations.add("classpath:org/kuali/common/sql/sql.xml");
+		locations.add("classpath:org/kuali/common/sql/mysql.xml");
+		locations.add("classpath:org/kuali/common/sql/oracle.xml");
+		locations.add("classpath:org/kuali/common/sql/h2.xml");
+		locations.add("classpath:org/kuali/common/sql/derby.xml");
+
+		PropertiesLoaderContext plc = new PropertiesLoaderContext();
+		plc.setEncoding(project.getEncoding());
+		plc.setLocations(locations);
+
+		ProjectProperties pp = new ProjectProperties();
+		pp.setProject(project);
+		pp.setLoaderContexts(Arrays.asList(plc));
+		return pp;
+	}
 
 }
