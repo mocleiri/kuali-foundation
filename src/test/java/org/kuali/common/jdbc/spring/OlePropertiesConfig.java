ec920761d6def3c0ab476699957c0955981f6b38
jcaddel <jcaddel@kuali.org>
2013-03-23 21:27:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18869 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/test/java/org/kuali/common/jdbc/spring/OlePropertiesConfig.java
@@ -0,0 +1,37 @@
+package org.kuali.common.jdbc.spring;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.kuali.common.util.Project;
+import org.kuali.common.util.property.ProjectProperties;
+import org.kuali.common.util.property.PropertiesLoaderContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OlePropertiesConfig {
+
+	@Bean
+	public ProjectProperties oleProjectProperties() {
+		Project project = new Project();
+		project.setArtifactId("ole-fs");
+		project.setGroupId("org.kuali.ole");
+		project.setVersion("0.8.1-s-r11074");
+		project.setEncoding("UTF-8");
+
+		List<String> locations = new ArrayList<String>();
+		locations.add("classpath:ole-fs.properties");
+
+		PropertiesLoaderContext plc = new PropertiesLoaderContext();
+		plc.setEncoding(project.getEncoding());
+		plc.setLocations(locations);
+
+		ProjectProperties pp = new ProjectProperties();
+		pp.setProject(project);
+		pp.setLoaderContexts(Arrays.asList(plc));
+		return pp;
+	}
+
+}
