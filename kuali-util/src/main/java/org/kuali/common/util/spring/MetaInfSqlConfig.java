e58b7b0189f04b76742b617239e0b995ae4b8cba
jcaddel <jcaddel@kuali.org>
2013-03-25 13:19:40 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18952 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/spring/MetaInfSqlConfig.java
@@ -0,0 +1,42 @@
+package org.kuali.common.util.spring;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.MetaInfContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class MetaInfSqlConfig {
+
+	@Autowired
+	Environment env;
+
+	@Bean
+	public Object invokeMethod() {
+		List<MetaInfContext> contexts = new ArrayList<MetaInfContext>();
+		contexts.add(getMetaInfContext("metainf.output.schema", "metainf.includes.schema"));
+		contexts.add(getMetaInfContext("metainf.output.data", "metainf.includes.data"));
+		contexts.add(getMetaInfContext("metainf.output.constraints", "metainf.includes.constraints"));
+		contexts.add(getMetaInfContext("metainf.output.other", "metainf.includes.other"));
+		return null;
+	}
+
+	protected MetaInfContext getMetaInfContext(String outputFileKey, String includesKey) {
+		String csv = SpringUtils.getProperty(env, includesKey);
+		List<String> includes = CollectionUtils.getTrimmedListFromCSV(csv);
+		File outputFile = new File(SpringUtils.getProperty(env, outputFileKey));
+		File baseDir = new File(SpringUtils.getProperty(env, "project.build.outputDirectory"));
+
+		MetaInfContext context = new MetaInfContext();
+		context.setBaseDir(baseDir);
+		context.setOutputFile(outputFile);
+		context.setIncludes(includes);
+		return context;
+	}
+}
