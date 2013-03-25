2c089cc220dca9098511026c05d63eb3623c87c3
jcaddel <jcaddel@kuali.org>
2013-03-24 22:32:28 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@18912 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/test/java/org/kuali/common/jdbc/spring/OleResetConfig.java
+++ src/test/java/org/kuali/common/jdbc/spring/OleResetConfig.java
@@ -1,14 +1,17 @@
 package org.kuali.common.jdbc.spring;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
 import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.Project;
 import org.kuali.common.util.PropertyUtils;
 import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.execute.SpringContextLoaderExecutable;
 import org.kuali.common.util.property.ProjectProperties;
+import org.kuali.common.util.property.PropertiesContext;
 import org.kuali.common.util.service.DefaultSpringService;
 import org.kuali.common.util.service.PropertySourceContext;
 import org.kuali.common.util.service.SpringContext;
@@ -22,7 +25,7 @@ import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 
 @Configuration
-@Import({ JdbcPropertiesConfig.class, OlePropertiesConfig.class })
+@Import({ JdbcPropertiesConfig.class })
 public class OleResetConfig {
 
 	@Autowired
@@ -31,8 +34,23 @@ public class OleResetConfig {
 	@Autowired
 	JdbcPropertiesConfig jdbcPropertiesConfig;
 
-	@Autowired
-	OlePropertiesConfig olePropertiesConfig;
+	@Bean
+	public ProjectProperties oleProjectProperties() {
+		Project project = new Project();
+		project.setArtifactId("ole-fs");
+		project.setGroupId("org.kuali.ole");
+		project.setVersion("0.8.1-s-r11074");
+		project.setEncoding("UTF-8");
+
+		PropertiesContext pc = new PropertiesContext();
+		pc.setEncoding(project.getEncoding());
+		pc.setLocations(Arrays.asList("classpath:ole-fs.properties"));
+
+		ProjectProperties pp = new ProjectProperties();
+		pp.setProject(project);
+		pp.setPropertiesContext(pc);
+		return pp;
+	}
 
 	@Bean(initMethod = "execute")
 	public Executable springExecutable() {
@@ -64,7 +82,7 @@ public class OleResetConfig {
 		String name = "springPropertySource";
 		List<ProjectProperties> pps = new ArrayList<ProjectProperties>();
 		pps.add(jdbcPropertiesConfig.jdbcProjectProperties());
-		pps.add(olePropertiesConfig.oleProjectProperties());
+		pps.add(oleProjectProperties());
 		Properties source = PropertyUtils.load(pps);
 		return new PropertiesPropertySource(name, source);
 	}
