6b927091cffc8bffeb0f77f65bff816a5e40ad27
psisson <psisson@kuali.org>
2013-03-26 18:41:25 -0400

    3-26
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@18989 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/make_wiki_table.pl
+++ kuali-dns/make_wiki_table.pl
@@ -1,147 +1,182 @@
 #!/usr/bin/perl
 
-sub build_fn_lst
+$HOME=$ENV{'HOME'};
+
+sub build_ec2_lst
 {
 $cmd = "ec2-describe-instances";
-$key = `ls ~/.ssh/fn-pk*`;
-$cert = `ls ~/.ssh/fn-cert*`;
-chomp($key);
-chomp($cert);
-#print "\nkey:$key.";
-#print "cert:$cert.";
-$command_instance = "$cmd -K $key -C $cert | grep \"INSTANCE\" > instance.lst";
-$command_tag = "$cmd -K $key -C $cert | grep \"TAG\" > tag.lst";
-$size = `wc -l instance.lst`;
-print "\nfn: $size";
-`$command_instance`;
-`$command_tag`;
-$key = `ls ~/.ssh/kr-pk*`;
-$cert = `ls ~/.ssh/kr-cert*`;
-chomp($key);
-chomp($cert);
-$command_instance = "$cmd -K $key -C $cert | grep \"INSTANCE\" >> instance.lst";
-$command_tag = "$cmd -K $key -C $cert | grep \"TAG\" >> tag.lst";
-`$command_instance`;
-`$command_tag`;
-$size = `wc -l instance.lst`;
-print "\nkr: $size";
 
-$key = `ls ~/.ssh/ks-pk*`;
-$cert = `ls ~/.ssh/ks-cert*`;
-chomp($key);
-chomp($cert);
-$command_instance = "$cmd -K $key -C $cert | grep \"INSTANCE\" >> instance.lst";
-$size = `wc -l instance.lst`;
-print "\nks: $size";
-$command_tag = "$cmd -K $key -C $cert | grep \"TAG\" >> tag.lst";
-`$command_instance`;
-`$command_tag`;
 
-open ( FN,  "<instance.lst"); (@INSTANCE =<FN>); close (FN);
-`rm FN.lst`;
-open FNLST, ">>FN.lst" or die "FN.lst : $!\n" ;
+my @projects = ("fn","ks","kr");
+my $pj;
+system("rm ./instance.lst;touch ./instance.lst");
+system("rm ./tag.lst;touch ./tag.lst");
+
+foreach  $pj (@projects)
+{ 
+  $pk_key =  "$HOME/.ssh/$pj-pk*";
+  $cert_key = "$HOME/.ssh/$pj-cert*";
+  $key = `ls $pk_key`;
+  $cert = `ls $cert_key`;
+  chomp($key);
+  chomp($cert);
+   $command_tag = "$cmd -K $key -C $cert | grep \"TAG\" >> tag.lst";
+   $command_instance = "$cmd -K $key -C $cert | grep \"INSTANCE\" >> instance.lst";
+   `$command_instance`;
+   `$command_tag`;
+}
+
+#make a file with one line for each instance, including the tag name 
+open ( EC2,  "<instance.lst"); (@INSTANCE =<EC2>); close (EC2);
+`rm EC2.lst`;
+open EC2LST, ">>EC2.lst" or die "EC2.lst : $!\n" ;
 foreach $instance ( @INSTANCE)
-{
+ {
   my @temp = split(/\t/, $instance);
   my $url = $temp[3];
   my $status = $temp[5];
   my $instance_id = $temp[1];
   my @TAGS = `grep $instance_id tag.lst`;
   my @tags = ();
+  #there could be more than one tag. So lets combine them with ":" 
   foreach $tagline ( @TAGS)
   { chomp($tagline); @tagname = split(/\t/, $tagline ); 
-    push( @tags, $tagname[4]); }
+    push( @tags, $tagname[4]); 
+  }
+
   $tags = join ( ":", @tags);
+  #done with tag for this instance
+
   if ( $url eq "" ){ $temp[3] = "n/a"; }
-  print FNLST  $instance_id," ",$url," ", $status," ", $tags,"\n";
-
-#  foreach $element (@temp) {
-#   print "\n",$i," $element\n";
-#    $i++; }
+  
+ #let's create a file combining the instance info and tag info.  
+  print EC2LST  $instance_id," ",$url," ", $status," ", $tags,"\n";
+ }
+ close(EC2LST);
 }
-}
 
-sub foundation_env_status
+################################################
+sub project_env_status 
+################################################
 {
- $project = @_[0];
 
+ my $env_no = "";
+ my $name_url = "";
+ my $server =  "";
+ my $status = "";
+ my $size = "";
+ my $tag = "";
+
+ my $project = @_[0];
+ my $skip_ec2_list = @_[1];
+ chomp ($project);
+ chomp ($skip_ec2_list);
+ #lets setup the individual project comma delimited files
  $projectfile = "dns_"."$project".".csv";
  `rm $projectfile`;
+ #add the headers, primative, but effective
  `echo \"DNS Name,EC2 Name,uptime or status, .. , no users,avg load for 1 , for 5 min,15 min,disk,size,GB,used,%\" > $projectfile`;
+
+ #Ok, let put the file in append mode
   open WIKI,  ">>$projectfile" or die "$projectfile $!\n";
-  build_fn_lst();
+
+ #using ec2-describe-instances, let build a file with all info about fn, rice and student.  I don't have key for ole.
+ if (( $project ne "ole" )  && ($skip_ec2_list eq "" ))
+ { build_ec2_lst(); }
+
+ #ok, now we'll do some dnsme and find out what's in our dns tables.
  $sourcefile = "dns.$project".".txt";
- $cmd = "mvn dnsme:showrecords | grep \">\" | grep -v ole | grep -v ks | grep -v rice > $sourcefile";
+ 
+ if ( $project eq "fn" )
+ { $cmd = "mvn dnsme:showrecords | grep \">\" | grep -v ole | grep -v ks | grep -v rice > $sourcefile"; }
+ else
+ { $cmd = "mvn dnsme:showrecords -Ddnsme.recordNameContains=$project > $sourcefile"; }
+
+ `echo \"DNS Name,EC2 Name,uptime or status, .. , no users,avg load for 1 , for 5 min,15 min,disk,size,GB,used,%\" > $projectfile`;
+ #print "\n", $cmd;
  `$cmd`;
- print "\n", $cmd;
  open( dns,  "<$sourcefile"); (@DNS =<dns>); close (dns);
  foreach $line (@DNS)
  {
    chomp($line);
-  $no_ping = "";
+   if ( $line !~ /\->/ ){ next; }
+   $no_ping = "";
+
+   #weed out the known situation that will not need query
    if (( $line =~ "cloudfront") ){ $no_ping = "cloudfront-no check";  }
    if (( $line =~ "rds") ){ $no_ping = "RDS-no check"; }
    if (( lc($line) =~ "s3") ){ $no_ping = "S3 Data Archiver, no check"; }
    if (( lc($line) =~ "ghs") ){ $no_ping = "Google Hosted Services(ghs), no check"; }
-   #print "\n",$line;
-   @parts = split(/\s|\->|,/,$line);
-    ($toss,$url,$ec2,$CNAME,$ttl) = split(/\s|\->|,/,$line);
+   #if (( $line =~ "env7") && ($project eq "ole")){ $no_ping = "time-out"; }
+   if (( $line =~ "rds") ){ $no_ping = "RDS-no check"; }
+   if (( $line =~ "cloudfront") ){ $no_ping = "cloudfront-no check";  }
+
+
+   #so I have information, lets parse and clean it up
+   ($toss,$url,$ec2,$CNAME,$ttl) = split(/\s|\->|,/,$line);
    print "\n(toss:$toss,url:$url,ec2:$ec2,cname:$CNAME,ttl:$ttl)";
-   #$name = $parts[1].".kuali.org";
-   $name = $url.".kuali.org";
-   if ($name eq ""){next;}
-   #print "\nname: $name";
-   #@temp = split(//,$parts[2]);
-   @temp = split(//,$ec2);
-   pop(@temp); #there's a period there
-   $ec2 = join "", @temp;
-   if ( $name =~ "ci.fn" )
-   {
-   print "\ngrep $name FN.lst";
-   print "\ngrep $ec2 FN.lst" ;
-   }
-   $result = `grep $url FN.lst`;
-   $result_ec2 = `grep $ec2 FN.lst`;
-    
-   chomp($result);
-  # print "\nresult 0:", $result[0];
-  # print " result 1:", $result[1];
-   chomp($result_ec2);
-   if ( $result_ec2 ne "" )
-   {   
-      ($instance_id, $server, $status, $tags) = split (/\s/, $result_ec2);
-       print WIKI "$name, $server, $status,$tags\n";
-   }
-   else{
-     if ( $result ne "" )
-     {   
-        ($instance_id, $server, $status, $tags) = split (/\s/, $result);
-     
-        print WIKI "$name, $server, $status,$tags\n";
-     }
+   $name_url = $url.".kuali.org";
+
     if ( $no_ping ne "" )
-    { print WIKI "$name,$url , $no_ping\n"; }
-   if ( ( $result_ec2 eq "" ) && ( $result eq "" ) && ($no_ping eq ""  ) )
-      { @result = dead_or_alive($name,$no_ping);
-      chomp(@result);
-      $status = $result[0];
-      $size = $result[1];
-      print WIKI "$name, $server, $status, $size\n"; }
-      #print WIKI "$name,$url , no ec2 server\n"; }
+    { print WIKI "$name_url,$url , $no_ping\n"; next; }
+   #get rid of that dot at the end of amazon.com name
+   @temp = split(//,$ec2);
+
+   pop(@temp); #there's a period there
+   $ec2 = join "", @temp; #put it back together
+
+
+   #if the amazon.com name match in the EC2 List, lets use that info 
+   #query ec2 list for the amazon DNS name 
+   $outcome_ec2com = `grep $ec2 EC2.lst`;
+   chomp($outcome_ec2com);
+
+
+   #query ec2 list for the tag name 
+   #print "\ngrep $url EC2.lst";
+   $results_ec2tag = `grep $url EC2.lst`; 
+   chomp($results_ec2tag);
+   
+   #if the ec2 query has returned outcome lets get that info first
+   if ( $outcome_ec2com ne "" )
+   {   
+      ($instance_id, $server, $status, $tags) = split (/\s/, $outcome_ec2com);
+   }
+   else #let's use the tag query
+   { ($instance_id, $server, $status, $tags) = split (/\s/, $results_ec2tag);}
+
+   #only ping if the server is running
+   if (( $status eq "running") || ( $project eq "ole" ))
+      {  
+        if (( $line =~ "env2") && ($project eq "ole"))
+         { @results_ec2tag = dead_or_alive($server);  }
+        else
+        { @results_ec2tag = dead_or_alive($name_url); }
+      chomp(@results_ec2tag);
+      $status = $results_ec2tag[0];
+      $size = $results_ec2tag[1];
+      }
+    
+    #let look up the index, probably write a routine for this
+    $domainservers = "./domainsvr_lookup.txt";
+    $env_no = "";
+   #print "grep $name_url $domainservers\n";
+   ($env_no, $env_name, $projectx) = split(/,/,`grep $name_url $domainservers`);
+   #I only are about env_no for this effort
+
+   #take the info and print it to the project file
+   print WIKI "$env_no,$name_url, $server, $status, $size,$tag\n"; 
+   }
   }
-  }
-}
 
 sub dead_or_alive
 {
-use warnings;
-my $name = $_[0];
-my $default_value = $_[1];
-my $output = "";
-my @out = ();
-my $value = "";
+ use warnings;
+ my $name = $_[0];
+ my $output = "";
+ my @out = ();
+ my $value = "";
 
-if ( $default_value ne ""){$out[0]=$name; $out[1]=$default_value; return(@out);}
 eval {
     local $SIG{ALRM} = sub {die "alarm\n"};
     alarm 5;
@@ -170,64 +205,14 @@ if ($?) {
 }
 
 
-sub project_env_status {
- my $project = @_[0];
- $projectfile = "dns_"."$project".".csv";
- `rm $projectfile`;
- $sourcefile = "dns.$project".".txt";
- $cmd = "mvn dnsme:showrecords -Ddnsme.recordNameContains=$project > $sourcefile";
- `echo \"DNS Name,EC2 Name,uptime or status, .. , no users,avg load for 1 , for 5 min,15 min,disk,size,GB,used,%\" > $projectfile`;
- `$cmd`;
- open( dns,  "<$sourcefile"); (@DNS =<dns>); close (dns);
- open WIKI,  ">>$projectfile" or die "$projectfile $!\n";
- $i =0;
- foreach $line (@DNS) {
- chomp($line);
- print "\n",$line;
- $no_ping = "";
- if (( $line =~ "env2") && ($project eq "ole")){ $no_ping = "time-out"; }
- if (( $line =~ "env7") && ($project eq "ole")){ $no_ping = "time-out"; }
- if (( $line =~ "rds") ){ $no_ping = "RDS-no check"; }
- if (( $line =~ "cloudfront") ){ $no_ping = "cloudfront-no check";  }
- if (($line =~ $project) ) {
-   @parts = split(/\s|\->|,/,$line);
-   $name = $parts[1].".kuali.org";
-   if ($name eq ""){next;}
-   @temp = split(//,$parts[2]);
-   pop(@temp);
-   $server = join "", @temp;
-   @result = dead_or_alive($name,$no_ping);
-   chomp(@result);
-   $status = $result[0];
-   $size = $result[1];
-   $i++;
-   print WIKI "$name, $server, $status, $size\n";
- }
-#$i=0;
-#foreach $item (@parts)
-#{
-#  print "\n";
-#  print $i++;
-#  print $item;
-#}
-}
-} #project_env_status
-
-
-sub get_status
-{
-my $project = @_[0];
-if (( $project eq "rice") || ( $project eq "ks") ||( $project eq "ole") ){ project_env_status($project)};
-if ( $project eq "fn" ){ foundation_env_status($project) ;}
-#sleep 60; #let the pings timeout
-}
-
 sub main
 {
   $project=$ARGV[0];
+  $skip_ec2_list = @_[1];
   chomp($project);
+  chomp($skip_ec2_list);
   if ( $project eq "" ){ print "\n\tPlease include a project: ole, rice, ks. Try again.\n\n"; exit;}
-  get_status( $project);
+  project_env_status(  $project, $skip_ec2_list );
 }
 
 &main();
