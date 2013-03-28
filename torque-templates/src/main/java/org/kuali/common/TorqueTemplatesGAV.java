39a9db7abc05e02c4bac64feb156b633c231f0d5
jcaddel <jcaddel@kuali.org>
2013-03-27 20:39:55 -0400

    GAV info
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@19051 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ torque-templates/src/main/java/org/kuali/common/TorqueTemplatesGAV.java
@@ -0,0 +1,16 @@
+package org.kuali.common;
+
+/**
+ * ############################# WARNING ##############################
+ * 
+ * This is a generated file. Do NOT edit.
+ * 
+ * Automated process keeps this source file in sync with the Maven POM.
+ */
+public abstract class TorqueTemplatesGAV {
+
+	public static final String GROUP_ID = "org.kuali.common";
+	public static final String ARTIFACT_ID = "torque-templates";
+	public static final String VERSION = "2.1.0-SNAPSHOT";
+
+}
