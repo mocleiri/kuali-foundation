df48b1ea150b1d97af687da462333162af9302af
jcaddel <jcaddel@kuali.org>
2013-03-27 21:20:41 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@19060 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/spring/ConfigUtils.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.util.spring;
+
+import java.util.List;
+
+import org.kuali.common.util.Project;
+import org.kuali.common.util.property.ProjectProperties;
+import org.kuali.common.util.property.PropertiesContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(ConfigUtils.class);
+
+	public static ProjectProperties getProjectProperties(Project project, List<String> locations) {
+		logger.debug(project.getGroupId());
+		PropertiesContext pc = new PropertiesContext();
+		pc.setEncoding(project.getEncoding());
+		pc.setLocations(locations);
+
+		ProjectProperties pp = new ProjectProperties();
+		pp.setProject(project);
+		pp.setPropertiesContext(pc);
+		return pp;
+	}
+}
