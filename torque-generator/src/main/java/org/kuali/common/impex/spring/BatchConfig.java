8ee6e3a0787542c277b73c8393f3ea7f97f2963e
jcaddel <jcaddel@kuali.org>
2013-03-27 21:33:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@19064 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ torque-generator/src/main/java/org/kuali/common/impex/spring/BatchConfig.java
@@ -0,0 +1,21 @@
+package org.kuali.common.impex.spring;
+
+import org.kuali.common.util.FormatUtils;
+import org.kuali.common.util.spring.SpringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class BatchConfig {
+
+	@Autowired
+	Environment env;
+
+	@Bean
+	public long impexBatchSize() {
+		String size = SpringUtils.getProperty(env, "impex.batch.size");
+		return FormatUtils.getBytes(size);
+	}
+}
