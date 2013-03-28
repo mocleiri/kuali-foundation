0054329b43352438eb50f3503e4b3ecc5904135c
jcaddel <jcaddel@kuali.org>
2013-03-27 21:50:25 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@19068 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-executor/src/main/java/org/kuali/common/impex/spring/MpxSupplierConfig.java
+++ torque-executor/src/main/java/org/kuali/common/impex/spring/MpxSupplierConfig.java
@@ -1,10 +1,14 @@
 package org.kuali.common.impex.spring;
 
+import org.apache.torque.engine.EngineException;
 import org.apache.torque.engine.platform.Platform;
 import org.apache.torque.engine.platform.PlatformFactory;
+import org.kuali.common.impex.KualiDatabase;
 import org.kuali.common.impex.service.SqlProducer;
 import org.kuali.common.util.spring.SpringUtils;
+import org.kuali.core.db.torque.KualiXmlToAppData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
@@ -19,6 +23,7 @@ public class MpxSupplierConfig {
 	@Autowired
 	BatchConfig batchConfig;
 
+	@Bean
 	public SqlProducer impexProducer() {
 		String vendor = SpringUtils.getProperty(env, "db.vendor");
 		Platform platform = PlatformFactory.getPlatformFor(vendor);
@@ -26,5 +31,17 @@ public class MpxSupplierConfig {
 		producer.setBatchDataSizeLimit(batchConfig.impexBatchSize());
 		producer.setBatchRowCountLimit(batchConfig.impexBatchRows());
 		return producer;
+	}
+
+	@Bean
+	public KualiDatabase impexDatabase() {
+		String vendor = SpringUtils.getProperty(env, "db.vendor");
+		String location = SpringUtils.getProperty(env, "impex.schema.location");
+		KualiXmlToAppData parser = new KualiXmlToAppData(vendor);
+		try {
+			return parser.parseResource(location);
+		} catch (EngineException e) {
+			throw new IllegalStateException(e);
+		}
 	}
 }
