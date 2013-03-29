9c5adb90aae0e3b110252873d03104d187508b56
jcaddel <jcaddel@kuali.org>
2013-03-29 19:15:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@19110 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/ResetSchemaConfig.java
+++ src/main/java/org/kuali/common/jdbc/spring/ResetSchemaConfig.java
@@ -14,6 +14,7 @@ import org.springframework.core.env.Environment;
 @Configuration
 @Import({ JdbcCommonConfig.class, JdbcDataSourceConfig.class })
 public class ResetSchemaConfig {
+
 	public static final String TYPE = "schema";
 	public static final String SKIP_KEY = "jdbc.schema.skip";
 
