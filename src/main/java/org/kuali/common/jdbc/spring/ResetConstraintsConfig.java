fd29b6daafd3f9e5d304317b109810ee2e66fa7a
jcaddel <jcaddel@kuali.org>
2013-03-30 12:55:31 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@19128 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/ResetConstraintsConfig.java
+++ src/main/java/org/kuali/common/jdbc/spring/ResetConstraintsConfig.java
@@ -1,13 +1,9 @@
 package org.kuali.common.jdbc.spring;
 
-import java.util.List;
-
-import javax.sql.DataSource;
-
 import org.kuali.common.jdbc.JdbcExecutable;
 import org.kuali.common.jdbc.context.JdbcContext;
+import org.kuali.common.jdbc.context.SqlMode;
 import org.kuali.common.jdbc.listener.SummaryListener;
-import org.kuali.common.jdbc.supplier.SqlSupplier;
 import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.context.annotation.Bean;
@@ -16,33 +12,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ResetConstraintsConfig extends ResetBaseConfig {
 
+	public static final String TYPE = "constraints";
+	public static final String SKIP_KEY = "jdbc.constraints.skip";
+
 	@Bean
 	public Executable jdbcConstraintsExecutable() {
-		String skip = SpringUtils.getProperty(env, "jdbc.constraints.skip", "false");
+		JdbcConfigContext jcc = new JdbcConfigContext(env, TYPE, SqlMode.CONCURRENT, commonConfig, dataSourceConfig);
+		JdbcContext ctx = JdbcConfigUtils.getConcurrentJdbcContext(jcc);
+		ctx.setListener(new SummaryListener(false));
 
 		JdbcExecutable exec = new JdbcExecutable();
-		exec.setSkip(new Boolean(skip));
+		exec.setSkip(SpringUtils.getBoolean(env, SKIP_KEY, false));
 		exec.setService(commonConfig.jdbcService());
-		exec.setContext(getJdbcContext());
+		exec.setContext(ctx);
 		return exec;
-	}
-
-	protected JdbcContext getJdbcContext() {
-		String skip = SpringUtils.getProperty(env, "sql.constraints.skip", "false");
-		String threads = SpringUtils.getProperty(env, "sql.threads");
-		String message = SpringUtils.getProperty(env, "sql.constraints.concurrent.message");
-		List<SqlSupplier> suppliers = commonConfig.getSqlSuppliers("sql.constraints.concurrent");
-		DataSource dataSource = dataSourceConfig.jdbcDataSource();
-
-		JdbcContext ctx = new JdbcContext();
-		ctx.setMultithreaded(true);
-		ctx.setThreads(new Integer(threads));
-		ctx.setMessage(message);
-		ctx.setSkip(new Boolean(skip));
-		ctx.setDataSource(dataSource);
-		ctx.setSuppliers(suppliers);
-		ctx.setListener(new SummaryListener(false));
-		return ctx;
 	}
 
 }
