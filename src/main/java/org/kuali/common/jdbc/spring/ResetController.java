203804047f203e0ca24475e7afd2066a2aeb456f
jcaddel <jcaddel@kuali.org>
2013-03-31 16:36:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@19166 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/ResetController.java
+++ src/main/java/org/kuali/common/jdbc/spring/ResetController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 /**
  * Default database reset controller class. It displays the JDBC configuration, then executes dba SQL followed by schema->data->constraints->other, in that order.
@@ -20,7 +20,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 public class ResetController {
 
 	@Autowired
-	ConfigurableEnvironment env;
+	Environment env;
 
 	@Autowired
 	JdbcDataSourceConfig dataSourceConfig;
