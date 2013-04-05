e2f91a59bcb233ddc97e34c53dd1746b36c2087d
jcaddel <jcaddel@kuali.org>
2013-04-05 19:29:40 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@19237 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/main/java/org/kuali/common/impex/spring/ImpexDumpConfig.java
+++ torque-generator/src/main/java/org/kuali/common/impex/spring/ImpexDumpConfig.java
@@ -1,11 +1,17 @@
 package org.kuali.common.impex.spring;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
+import org.kuali.common.impex.DumpExecutable;
 import org.kuali.common.impex.ImpexContextFactoryBean;
+import org.kuali.common.impex.service.DefaultImpexGeneratorService;
 import org.kuali.common.impex.service.ImpexContext;
+import org.kuali.common.util.Assert;
 import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -20,6 +26,10 @@ public class ImpexDumpConfig {
 	@Autowired
 	Properties mavenProperties;
 
+	@Autowired
+	@Qualifier("dumpContexts")
+	List<ImpexContext> dumpContexts;
+
 	@Bean
 	public ImpexContext impexSourceContext() {
 		DriverManagerDataSource dmds = new DriverManagerDataSource();
@@ -32,6 +42,18 @@ public class ImpexDumpConfig {
 		factory.setDataSource(dmds);
 		factory.setProperties(mavenProperties);
 		return factory.getObject();
+	}
+
+	@Bean(initMethod = "execute")
+	public DumpExecutable dumpExecutable() {
+		Assert.notNull(dumpContexts, "dumpContexts is null");
+		DumpExecutable executable = new DumpExecutable();
+		executable.setSourceContext(impexSourceContext());
+		executable.setDatabaseVendors(Arrays.asList("oracle", "mysql"));
+		executable.setService(new DefaultImpexGeneratorService());
+		executable.setSkip(SpringUtils.getBoolean(env, "impex.dump.skip", false));
+		executable.setContexts(dumpContexts);
+		return executable;
 	}
 
 	@Bean
