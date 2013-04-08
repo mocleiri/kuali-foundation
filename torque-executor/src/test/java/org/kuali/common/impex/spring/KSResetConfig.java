e4568c1ce99c8950d7b281fe82799b6cc7023c4b
jcaddel <jcaddel@kuali.org>
2013-04-08 18:32:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@19289 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-executor/src/test/java/org/kuali/common/impex/spring/KSResetConfig.java
+++ torque-executor/src/test/java/org/kuali/common/impex/spring/KSResetConfig.java
@@ -1,13 +1,16 @@
 package org.kuali.common.impex.spring;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.kuali.common.jdbc.spring.AbstractMavenResetConfig;
 import org.kuali.common.jdbc.spring.JdbcPropertiesConfig;
 import org.kuali.common.jdbc.spring.ResetConfig;
 import org.kuali.common.jdbc.spring.ResetController;
+import org.kuali.common.util.Project;
 import org.kuali.common.util.property.ProjectProperties;
+import org.kuali.common.util.property.PropertiesContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -21,10 +24,22 @@ public class KSResetConfig extends AbstractMavenResetConfig {
 
 	@Override
 	protected List<ProjectProperties> getProjectPropertiesList() {
+		Project project = new Project();
+		project.setGroupCode("org.kuali.student");
+		project.setArtifactId("ks-with-rice-bundled");
+		project.setVersion("1.0.0");
+
+		PropertiesContext pc = new PropertiesContext();
+		pc.setLocations(Arrays.asList("classpath:ks-with-rice-bundled.properties"));
+
+		ProjectProperties pp = new ProjectProperties();
+		pp.setProject(project);
+		pp.setPropertiesContext(pc);
+
 		List<ProjectProperties> pps = new ArrayList<ProjectProperties>();
 		pps.add(jdbcProperties.jdbcProjectProperties());
 		pps.add(generatorProperties.generatorProjectProperties());
-		pps.add(mavenProjectProperties());
+		pps.add(pp);
 		return pps;
 	}
 
