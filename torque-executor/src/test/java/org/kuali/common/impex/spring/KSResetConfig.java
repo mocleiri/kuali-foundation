94eab17943ac42089b96b82bcd19e16a07edb15b
jcaddel <jcaddel@kuali.org>
2013-04-08 18:16:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@19286 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ torque-executor/src/test/java/org/kuali/common/impex/spring/KSResetConfig.java
@@ -0,0 +1,40 @@
+package org.kuali.common.impex.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.common.jdbc.spring.AbstractMavenResetConfig;
+import org.kuali.common.jdbc.spring.JdbcPropertiesConfig;
+import org.kuali.common.jdbc.spring.ResetConfig;
+import org.kuali.common.jdbc.spring.ResetController;
+import org.kuali.common.util.property.ProjectProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ JdbcPropertiesConfig.class, GeneratorPropertiesConfig.class })
+public class KSResetConfig extends AbstractMavenResetConfig {
+
+	@Autowired
+	GeneratorPropertiesConfig generatorProperties;
+
+	@Override
+	protected List<ProjectProperties> getProjectPropertiesList() {
+		List<ProjectProperties> pps = new ArrayList<ProjectProperties>();
+		pps.add(jdbcProperties.jdbcProjectProperties());
+		pps.add(generatorProperties.generatorProjectProperties());
+		pps.add(mavenProjectProperties());
+		return pps;
+	}
+
+	@Override
+	protected List<Class<?>> getAnnotatedClasses() {
+		List<Class<?>> annotatedClasses = new ArrayList<Class<?>>();
+		annotatedClasses.add(MpxSupplierConfig.class);
+		annotatedClasses.add(ResetConfig.class);
+		annotatedClasses.add(ResetController.class);
+		return annotatedClasses;
+
+	}
+}
