51300abf03b39aded67b9350f89c0f2b7c426ddf
jcaddel <jcaddel@kuali.org>
2013-04-09 20:10:11 -0400

    Automatically generated java source code - Maven GAV
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@19355 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-generator/src/main/java/org/kuali/common/TorqueGeneratorGAV.java
+++ torque-generator/src/main/java/org/kuali/common/TorqueGeneratorGAV.java
@@ -11,6 +11,6 @@ public abstract class TorqueGeneratorGAV {
 
 	public static final String GROUP_ID = "org.kuali.common";
 	public static final String ARTIFACT_ID = "torque-generator";
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "2.1.1-SNAPSHOT";
 
 }
