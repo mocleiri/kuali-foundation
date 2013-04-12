86b2bf7fe20838ae11504466f4db1f63ff5db4e2
jcaddel <jcaddel@kuali.org>
2013-04-12 09:22:07 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@19439 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DeployConfig.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DeployConfig.java
@@ -1,6 +1,15 @@
 package org.kuali.common.deploy.spring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.kuali.common.deploy.AppServerController;
+import org.kuali.common.deploy.DefaultTomcatController;
+import org.kuali.common.deploy.DeployContext;
 import org.kuali.common.util.Artifact;
+import org.kuali.common.util.secure.DefaultSecureChannel;
+import org.kuali.common.util.secure.SecureChannel;
 import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -32,6 +41,69 @@ public class DeployConfig {
 		a.setClassifier(SpringUtils.getProperty(env, "project.version", "NONE"));
 		a.setType("war");
 		return a;
+	}
+
+	@Bean
+	public DeployContext kdoContext() {
+		DeployContext ctx = new DeployContext();
+		ctx.setEnvironment(SpringUtils.getProperty(env, "deploy.env"));
+		ctx.setHostname(SpringUtils.getProperty(env, "deploy.hostname"));
+		ctx.setUsername(SpringUtils.getProperty(env, "deploy.username"));
+		ctx.setJdbcDriver(kdoJdbcDriverArtifact());
+		ctx.setApplication(kdoApplicationArtifact());
+
+		// TODO Set this appropriately
+		ctx.setConfig(null);
+
+		return ctx;
+	}
+
+	@Bean
+	public SecureChannel kdoSecureChannel() {
+		DeployContext ctx = kdoContext();
+		DefaultSecureChannel dsc = new DefaultSecureChannel();
+		dsc.setUsername(ctx.getUsername());
+		dsc.setHostname(ctx.getHostname());
+		dsc.setStrictHostKeyChecking(SpringUtils.getBoolean(env, "kdo.channel.strictHostKeyChecking", false));
+		dsc.setUseConfigFile(SpringUtils.getBoolean(env, "kdo.channel.useConfigFile", false));
+		dsc.setIncludeDefaultPrivateKeyLocations(SpringUtils.getBoolean(env, "kdo.channel.useConfigFile", false));
+		dsc.setPrivateKeyStrings(Arrays.asList(SpringUtils.getProperty(env, "kdo.channel.privateKey")));
+		return dsc;
+	}
+
+	@Bean
+	public AppServerController kdoController() {
+		DefaultTomcatController dtc = new DefaultTomcatController();
+		dtc.setChannel(kdoSecureChannel());
+		dtc.setUsername(SpringUtils.getProperty(env, "tomcat.user"));
+		dtc.setShutdown(SpringUtils.getProperty(env, "tomcat.shutdown"));
+		dtc.setStartup(SpringUtils.getProperty(env, "tomcat.startup"));
+		return dtc;
+	}
+
+	@Bean
+	public List<String> kdoFilesToDelete() {
+		String lib = SpringUtils.getProperty(env, "tomcat.lib");
+		List<String> list = new ArrayList<String>();
+		list.add(lib + "/mysql*.jar");
+		list.add(lib + "/ojdbc*.jar");
+		list.add(lib + "/classes12*.jar");
+		list.add(lib + "/orai18n*.jar");
+		list.add(SpringUtils.getProperty(env, "tomcat.setenv"));
+		list.add(SpringUtils.getProperty(env, "appdynamics.controller"));
+		list.add(SpringUtils.getProperty(env, "tomcat.home.kuali"));
+		return list;
+	}
+
+	@Bean
+	public List<String> kdoDirectoriesToDelete() {
+		List<String> list = new ArrayList<String>();
+		list.add(SpringUtils.getProperty(env, "tomcat.logs"));
+		list.add(SpringUtils.getProperty(env, "tomcat.webapps"));
+		list.add(SpringUtils.getProperty(env, "tomcat.home.kuali"));
+		list.add(SpringUtils.getProperty(env, "tomcat.work"));
+		list.add(SpringUtils.getProperty(env, "tomcat.conf.catalina"));
+		return list;
 	}
 
 }
