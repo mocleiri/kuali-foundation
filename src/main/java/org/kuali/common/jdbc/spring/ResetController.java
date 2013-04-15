00f0ccf631b1aa8eb4060e77b1759f6b93df7d3b
jcaddel <jcaddel@kuali.org>
2013-04-14 23:24:41 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@19681 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/kuali/common/jdbc/spring/ResetController.java
+++ src/main/java/org/kuali/common/jdbc/spring/ResetController.java
@@ -15,63 +15,19 @@
  */
 package org.kuali.common.jdbc.spring;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.kuali.common.util.execute.Executable;
-import org.kuali.common.util.execute.ExecutablesExecutable;
-import org.kuali.common.util.spring.SpringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 
 /**
  * Default database reset controller class. It displays the JDBC configuration, then executes a series of SQL statements in order [dba->schema->data->constraints->other].
  */
 @Configuration
-@Import({ JdbcCommonConfig.class, JdbcDataSourceConfig.class, ResetDbaConfig.class, ResetSchemaConfig.class, ResetConstraintsConfig.class, ResetOtherConfig.class })
-public class ResetController {
-
-	@Autowired
-	Environment env;
-
-	@Autowired
-	JdbcDataSourceConfig dataSourceConfig;
-
-	@Autowired
-	ResetDbaConfig dbaConfig;
-
-	@Autowired
-	ResetSchemaConfig schemaConfig;
-
-	@Autowired
-	ResetDataConfig dataConfig;
-
-	@Autowired
-	ResetConstraintsConfig constraintsConfig;
-
-	@Autowired
-	ResetOtherConfig otherConfig;
+public class ResetController extends AbstractResetController {
 
 	@Bean(initMethod = "execute")
 	public Executable jdbcResetExecutable() {
-		List<Executable> executables = new ArrayList<Executable>();
-		executables.add(dataSourceConfig.jdbcShowConfigExecutable());
-		executables.add(dbaConfig.jdbcDbaExecutable());
-		executables.add(schemaConfig.jdbcSchemaExecutable());
-		executables.add(dataConfig.jdbcDataConcurrentExecutable());
-		executables.add(dataConfig.jdbcDataSequentialExecutable());
-		executables.add(constraintsConfig.jdbcConstraintsConcurrentExecutable());
-		executables.add(constraintsConfig.jdbcConstraintsSequentialExecutable());
-		executables.add(otherConfig.jdbcOtherConcurrentExecutable());
-		executables.add(otherConfig.jdbcOtherSequentialExecutable());
-
-		ExecutablesExecutable exec = new ExecutablesExecutable();
-		exec.setSkip(SpringUtils.getBoolean(env, "jdbc.reset.skip", false));
-		exec.setTimed(SpringUtils.getBoolean(env, "jdbc.reset.timed", true));
-		exec.setExecutables(executables);
-		return exec;
+		return getResetExecutable();
 	}
+
 }
