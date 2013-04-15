4e70415fd4fffd9a815c7301a0dc930a1b2e5f21
jcaddel <jcaddel@kuali.org>
2013-04-14 23:24:52 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@19682 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/spring/ResetController.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/spring/ResetController.java
@@ -15,69 +15,20 @@
  */
 package org.kuali.common.deploy.spring;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.kuali.common.jdbc.spring.JdbcCommonConfig;
-import org.kuali.common.jdbc.spring.JdbcDataSourceConfig;
-import org.kuali.common.jdbc.spring.ResetConstraintsConfig;
-import org.kuali.common.jdbc.spring.ResetDataConfig;
-import org.kuali.common.jdbc.spring.ResetDbaConfig;
-import org.kuali.common.jdbc.spring.ResetOtherConfig;
-import org.kuali.common.jdbc.spring.ResetSchemaConfig;
+import org.kuali.common.jdbc.spring.AbstractResetController;
 import org.kuali.common.util.execute.Executable;
-import org.kuali.common.util.execute.ExecutablesExecutable;
-import org.kuali.common.util.spring.SpringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 
 /**
  * Default database reset controller class. It displays the JDBC configuration, then executes a series of SQL statements in order [dba->schema->data->constraints->other].
  */
 @Configuration
-@Import({ JdbcCommonConfig.class, JdbcDataSourceConfig.class, ResetDbaConfig.class, ResetSchemaConfig.class, ResetConstraintsConfig.class, ResetOtherConfig.class })
-public class ResetController {
-
-	@Autowired
-	Environment env;
-
-	@Autowired
-	JdbcDataSourceConfig dataSourceConfig;
-
-	@Autowired
-	ResetDbaConfig dbaConfig;
-
-	@Autowired
-	ResetSchemaConfig schemaConfig;
-
-	@Autowired
-	ResetDataConfig dataConfig;
-
-	@Autowired
-	ResetConstraintsConfig constraintsConfig;
-
-	@Autowired
-	ResetOtherConfig otherConfig;
+public class ResetController extends AbstractResetController {
 
 	@Bean
 	public Executable jdbcResetExecutable() {
-		List<Executable> executables = new ArrayList<Executable>();
-		executables.add(dataSourceConfig.jdbcShowConfigExecutable());
-		executables.add(dbaConfig.jdbcDbaExecutable());
-		executables.add(schemaConfig.jdbcSchemaExecutable());
-		executables.add(dataConfig.jdbcDataConcurrentExecutable());
-		executables.add(dataConfig.jdbcDataSequentialExecutable());
-		executables.add(constraintsConfig.jdbcConstraintsConcurrentExecutable());
-		executables.add(constraintsConfig.jdbcConstraintsSequentialExecutable());
-		executables.add(otherConfig.jdbcOtherConcurrentExecutable());
-		executables.add(otherConfig.jdbcOtherSequentialExecutable());
-
-		ExecutablesExecutable exec = new ExecutablesExecutable(executables);
-		exec.setSkip(SpringUtils.getBoolean(env, "jdbc.reset.skip", false));
-		exec.setTimed(SpringUtils.getBoolean(env, "jdbc.reset.timed", true));
-		return exec;
+		return getResetExecutable();
 	}
+
 }
