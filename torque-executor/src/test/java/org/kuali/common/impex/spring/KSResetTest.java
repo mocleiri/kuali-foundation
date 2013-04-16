362af12a41ec3ff6486483cf78e10c5df063bc5f
jcaddel <jcaddel@kuali.org>
2013-04-16 17:44:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@19705 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-executor/src/test/java/org/kuali/common/impex/spring/KSResetTest.java
+++ torque-executor/src/test/java/org/kuali/common/impex/spring/KSResetTest.java
@@ -1,7 +1,12 @@
 package org.kuali.common.impex.spring;
 
 import org.junit.Test;
+import org.kuali.common.jdbc.spring.ResetConfig;
+import org.kuali.common.jdbc.spring.ResetController;
+import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.MavenUtils;
 import org.kuali.common.util.service.DefaultSpringService;
+import org.kuali.common.util.service.SpringContext;
 import org.kuali.common.util.service.SpringService;
 
 public class KSResetTest {
@@ -9,8 +14,17 @@ public class KSResetTest {
 	@Test
 	public void test() {
 		try {
+			// Default Spring service will do what we need
 			SpringService ss = new DefaultSpringService();
-			ss.load(KSResetConfig.class);
+
+			// Setup a Spring context that uses maven properties for placeholder resolution
+			SpringContext context = MavenUtils.getMavenizedSpringContext(ss, KSPropertiesConfig.KS_MAVEN_PROPS, KSMavenPropertySourceConfig.class);
+
+			// Reset the db using annotated config
+			context.setAnnotatedClasses(CollectionUtils.asList(ResetConfig.class, ResetController.class));
+
+			// Execute Spring
+			ss.load(context);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
