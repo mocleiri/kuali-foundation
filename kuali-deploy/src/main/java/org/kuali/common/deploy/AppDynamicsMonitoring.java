aeb34bb2da345a9427887cf48b7d6d8a5c3cae67
jcaddel <jcaddel@kuali.org>
2013-04-26 16:37:24 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20045 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/AppDynamicsMonitoring.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/AppDynamicsMonitoring.java
@@ -7,6 +7,9 @@ public class AppDynamicsMonitoring implements Monitoring {
 
 	private static final Logger logger = LoggerFactory.getLogger(AppDynamicsMonitoring.class);
 
+	String machineAgentCommand;
+	String user;
+
 	@Override
 	public void stop() {
 		logger.info("Shutting down AppDynamics");
@@ -20,6 +23,22 @@ public class AppDynamicsMonitoring implements Monitoring {
 	@Override
 	public void start() {
 		logger.info("Starting AppDynamics");
+	}
+
+	public String getMachineAgentCommand() {
+		return machineAgentCommand;
+	}
+
+	public void setMachineAgentCommand(String machineAgentCommand) {
+		this.machineAgentCommand = machineAgentCommand;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
 	}
 
 }
