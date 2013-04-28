a94eb92f5652b010da63b019a065e94125095119
jcaddel <jcaddel@kuali.org>
2013-04-28 11:31:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20144 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/TomcatApplicationServer.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/TomcatApplicationServer.java
@@ -64,7 +64,7 @@ public class TomcatApplicationServer implements ApplicationServer {
 		DeployUtils.mkdirs(channel, dirsToCreate);
 		// Copy files to the remote server
 		if (!skipFiles) {
-			// If monitoring is turned on we need to alter a property used when filtering setenv.sh so it includes
+			// If monitoring is turned on, we need to alter a property used when filtering setenv.sh so it includes
 			// monitoring related java startup options
 			if (monitoring.isEnabled()) {
 				PropertyUtils.appendToOrSetProperty(filterProperties, "setenv.env.content", "\n" + monitoring.getJavaStartupOptions());
