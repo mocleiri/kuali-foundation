20dc01d0212bdf5c6759413f95e5d9b594b33f1e
jcaddel <jcaddel@kuali.org>
2013-04-29 10:36:04 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20206 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/FileSystemUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/FileSystemUtils.java
@@ -31,7 +31,7 @@ public class FileSystemUtils {
 	private static final Logger logger = LoggerFactory.getLogger(FileSystemUtils.class);
 
 	/**
-	 * Examine the contents of a text file, stopping as soon as it contains <code>token</code> or <code>timeout</code> is exceeded, whichever comes first.
+	 * Examine the contents of a text file, stopping as soon as it contains <code>token</code>, or <code>timeout</code> is exceeded, whichever comes first.
 	 */
 	public static MonitorTextFileResult monitorTextFile(File file, String token, int interval, int timeout, String encoding) {
 
