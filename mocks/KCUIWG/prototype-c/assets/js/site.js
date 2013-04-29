3ea230557511981afe57bc7d536c1007cab7aa8d
thrclark <thrclark@kuali.org>
2013-04-29 13:32:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@20229 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/prototype-c/assets/js/site.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".expandControl").toggle(function () {
-        $(this).removeClass("closed");
-        $(this).next(".expandTarget").slideDown(600);
-    }, function () {
-        $(this).addClass("closed");
-        $(this).next(".expandTarget").slideUp(600);
-    });
-});
