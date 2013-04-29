018a4dbf70c009258179279c3fd3a8e0a4d266c2
thrclark <thrclark@kuali.org>
2013-04-29 13:18:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@20221 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/KFS/assets/inc/portal-footer.php
@@ -0,0 +1,17 @@
+<footer>
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="foundation">
+            &copy; 2013
+            <a href="#" target="_blank">Kuali Foundation</a>
+          </div>
+          <div class="credit"><span>site built with KRAD</span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</body></html>
