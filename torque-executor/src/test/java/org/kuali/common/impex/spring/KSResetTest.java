264a3ceef149b58a4679cf49b1c45521730ba341
andrewlubbers <andrewlubbers@kuali.org>
2013-05-02 16:10:55 -0400

    Updated KSResetTest to work with latest version of kuali-jdbc
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/impex-2.0@20374 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- torque-executor/src/test/java/org/kuali/common/impex/spring/KSResetTest.java
+++ torque-executor/src/test/java/org/kuali/common/impex/spring/KSResetTest.java
@@ -1,13 +1,14 @@
 package org.kuali.common.impex.spring;
 
 import org.junit.Test;
-import org.kuali.common.jdbc.spring.ResetConfig;
-import org.kuali.common.jdbc.spring.ResetController;
+import org.kuali.common.jdbc.spring.SqlControllerConfig;
 import org.kuali.common.util.CollectionUtils;
 import org.kuali.common.util.MavenUtils;
 import org.kuali.common.util.service.DefaultSpringService;
 import org.kuali.common.util.service.SpringContext;
 import org.kuali.common.util.service.SpringService;
+
+import static org.junit.Assert.fail;
 
 public class KSResetTest {
 
@@ -21,12 +22,13 @@ public class KSResetTest {
 			SpringContext context = MavenUtils.getMavenizedSpringContext(ss, KSPropertiesConfig.KS_MAVEN_PROPS, KSMavenPropertySourceConfig.class);
 
 			// Reset the db using annotated config
-			context.setAnnotatedClasses(CollectionUtils.asList(MpxSupplierConfig.class, ResetConfig.class, ResetController.class));
+			context.setAnnotatedClasses(CollectionUtils.asList(MpxSupplierConfig.class, SqlControllerConfig.class));
 
 			// Execute Spring
 			ss.load(context);
 		} catch (Exception e) {
 			e.printStackTrace();
+            fail("Exception during test: " + e.getMessage());
 		}
 
 	}
