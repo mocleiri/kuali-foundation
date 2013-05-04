af8d1f40346e4ef8cc443cb9c053bfcb78fe41fe
jcaddel <jcaddel@kuali.org>
2013-05-04 18:23:14 -0400

    $MSG
    git-svn-id: https://svn.kuali.org/repos/foundation/sandbox/branches/impex-2.0-embedded@20641 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ torque-parser/src/main/java/org/kuali/common/impex/spring/ImpexContextDumpConfig.java
@@ -0,0 +1,11 @@
+package org.kuali.common.impex.spring;
+
+import java.util.List;
+
+import org.kuali.common.impex.service.ImpexContext;
+
+public interface ImpexContextDumpConfig {
+
+	public List<ImpexContext> dumpContexts();
+
+}
