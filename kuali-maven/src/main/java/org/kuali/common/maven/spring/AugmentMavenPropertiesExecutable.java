4ea6ac239202645778fa4f3f144baed50b30875f
jcaddel <jcaddel@kuali.org>
2013-05-10 12:23:14 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20692 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-maven/src/main/java/org/kuali/common/maven/spring/AugmentMavenPropertiesExecutable.java
+++ kuali-maven/src/main/java/org/kuali/common/maven/spring/AugmentMavenPropertiesExecutable.java
@@ -3,6 +3,7 @@ package org.kuali.common.maven.spring;
 import java.util.Properties;
 
 import org.apache.maven.project.MavenProject;
+import org.kuali.common.util.FormatUtils;
 import org.kuali.common.util.execute.Executable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,20 +19,31 @@ public class AugmentMavenPropertiesExecutable implements Executable {
 
 	@Override
 	public void execute() {
+
+		// Are we skipping execution altogether?
 		if (skip) {
 			logger.info("Skipping execution");
 			return;
 		}
+
 		// This is the original Maven project object created by Maven and injected into the context by spring-maven-plugin
 		Assert.notNull(mavenProject, "mavenProject is null");
 
-		// This is a derived set of properties mostly populated by Maven, but augmented by other logic
-		// It adds in a few timestamp related properties, adds orgId, adds properties for paths based on orgId, groupId
+		// This is a derived set of properties mostly populated by Maven, but augmented by other logic that enhances the default set of Maven properties
 		// eg project.groupId.path=org/kuali/rice
+		// eg project.version.snapshot=true
+		// eg project.build.timestamp.millis=123456789
 		Assert.notNull(augmentedMavenProperties, "augmentedMavenProperties is null");
 
-		// The augmented set of properties is 90% the same with 10% new properties added
+		// The augmented set of properties is mostly the same as the original with ~5% new properties added
 		mavenProject.getProperties().putAll(augmentedMavenProperties);
+
+		// Print something useful if we are in debug mode
+		int size1 = mavenProject.getProperties().size();
+		int size2 = augmentedMavenProperties.size();
+		String diff = FormatUtils.getCount(size2 - size1);
+		logger.debug("Added {} properties", diff);
+
 	}
 
 	public MavenProject getMavenProject() {
