6dbfaad1901720a5c9d6ea07d43dabd85ca5477f
jcaddel <jcaddel@kuali.org>
2013-05-17 18:57:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20915 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-s3/src/main/java/org/kuali/common/aws/s3/AmazonS3Service.java
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/AmazonS3Service.java
@@ -7,7 +7,8 @@ import com.amazonaws.services.s3.model.ObjectListing;
 public interface AmazonS3Service {
 
 	/**
-	 * Recursively examine a bucket starting at <code>prefix</code>. The list returned by this method will contain
+	 * Recursively examine a bucket starting at <code>prefix</code>. The list returned by this method contains an object listing for every "directory" and "sub-directory" rooted at
+	 * <code>prefix</code>
 	 */
 	List<ObjectListing> getObjectListings(BucketContext context);
 
