5f7f8acb24908aea9238010fc24636e18314334e
jcaddel <jcaddel@kuali.org>
2013-05-19 18:26:23 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20987 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/DefaultCloudFrontService.java
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/DefaultCloudFrontService.java
@@ -1,9 +1,23 @@
 package org.kuali.common.aws.s3.cloudfront;
 
+import org.codehaus.plexus.util.StringUtils;
+
 public class DefaultCloudFrontService implements CloudFrontService {
 
 	@Override
-	public void createOrUpdateBrowsableIndexObjects(DirectoryContext context) {
+	public void createOrUpdateBrowsableIndexObjects(DirectoryContext ctx) {
+
+		String bucket = ctx.getBucket();
+		String dirPrefix = ctx.getListing().getPrefix();
+		String dirNoSlashPrefix = StringUtils.substring(dirPrefix, 0, dirPrefix.length() - 1);
+
+		String welcomeFileKey = CloudFrontUtils.getWelcomeFileKey(ctx.getListing(), ctx.getWelcomeFiles());
+		if (welcomeFileKey == null) {
+			CloudFrontUtils.getPutIndexObjectRequest(bucket, ctx.getCacheControl(), ctx.getIndexHtml(), dirPrefix);
+		} else {
+			CloudFrontUtils.getCopyObjectRequest(bucket, welcomeFileKey, dirPrefix);
+		}
+
 	}
 
 }
--- kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/DirectoryContext.java
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/DirectoryContext.java
@@ -2,12 +2,18 @@ package org.kuali.common.aws.s3.cloudfront;
 
 import java.util.List;
 
+import org.kuali.common.aws.s3.ObjectListingRequest;
+
 import com.amazonaws.services.s3.model.ObjectListing;
 
 public class DirectoryContext {
 
-	ObjectListing listing;
+	String cacheControl = CloudFrontUtils.DEFAULT_CACHE_CONTROL;
+	String delimiter = ObjectListingRequest.DEFAULT_DELIMITER;
 	List<String> welcomeFiles;
+	String bucket;
+	ObjectListing listing;
+	String indexHtml;
 
 	public ObjectListing getListing() {
 		return listing;
@@ -23,6 +29,38 @@ public class DirectoryContext {
 
 	public void setWelcomeFiles(List<String> welcomeFiles) {
 		this.welcomeFiles = welcomeFiles;
+	}
+
+	public String getIndexHtml() {
+		return indexHtml;
+	}
+
+	public void setIndexHtml(String indexHtml) {
+		this.indexHtml = indexHtml;
+	}
+
+	public String getBucket() {
+		return bucket;
+	}
+
+	public void setBucket(String bucket) {
+		this.bucket = bucket;
+	}
+
+	public String getCacheControl() {
+		return cacheControl;
+	}
+
+	public void setCacheControl(String cacheControl) {
+		this.cacheControl = cacheControl;
+	}
+
+	public String getDelimiter() {
+		return delimiter;
+	}
+
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
 	}
 
 }
