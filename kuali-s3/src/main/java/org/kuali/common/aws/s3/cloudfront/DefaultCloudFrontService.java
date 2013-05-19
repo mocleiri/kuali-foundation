bb5d8bc132362ed05a9faa2ac8fd180bf3a8ef12
jcaddel <jcaddel@kuali.org>
2013-05-19 17:33:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20986 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/DefaultCloudFrontService.java
@@ -0,0 +1,9 @@
+package org.kuali.common.aws.s3.cloudfront;
+
+public class DefaultCloudFrontService implements CloudFrontService {
+
+	@Override
+	public void createOrUpdateBrowsableIndexObjects(DirectoryContext context) {
+	}
+
+}
