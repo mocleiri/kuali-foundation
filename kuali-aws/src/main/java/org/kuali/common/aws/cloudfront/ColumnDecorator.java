b02107d46a2f14f4e09914b9f04a0266865df02b
jcaddel <jcaddel@kuali.org>
2013-05-20 19:26:45 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21077 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/cloudfront/ColumnDecorator.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/cloudfront/ColumnDecorator.java
@@ -20,43 +20,42 @@ package org.kuali.common.aws.cloudfront;
  */
 public class ColumnDecorator {
 
-    String tableDataClass;
-    String spanClass;
-    String columnTitle;
+	String tableDataClass;
+	String spanClass;
+	String columnTitle;
 
-    public ColumnDecorator() {
-        this(null, null, null);
-    }
+	public ColumnDecorator() {
+		this(null, null, null);
+	}
 
-    public ColumnDecorator(final String tableDataClass, final String spanClass,
-            final String columnTitle) {
-        super();
-        this.tableDataClass = tableDataClass;
-        this.spanClass = spanClass;
-        this.columnTitle = columnTitle;
-    }
+	public ColumnDecorator(String tableDataClass, String spanClass, String columnTitle) {
+		super();
+		this.tableDataClass = tableDataClass;
+		this.spanClass = spanClass;
+		this.columnTitle = columnTitle;
+	}
 
-    public String getTableDataClass() {
-        return tableDataClass;
-    }
+	public String getTableDataClass() {
+		return tableDataClass;
+	}
 
-    public void setTableDataClass(final String tableDataClass) {
-        this.tableDataClass = tableDataClass;
-    }
+	public void setTableDataClass(final String tableDataClass) {
+		this.tableDataClass = tableDataClass;
+	}
 
-    public String getSpanClass() {
-        return spanClass;
-    }
+	public String getSpanClass() {
+		return spanClass;
+	}
 
-    public void setSpanClass(final String spanClass) {
-        this.spanClass = spanClass;
-    }
+	public void setSpanClass(final String spanClass) {
+		this.spanClass = spanClass;
+	}
 
-    public String getColumnTitle() {
-        return columnTitle;
-    }
+	public String getColumnTitle() {
+		return columnTitle;
+	}
 
-    public void setColumnTitle(final String columnTitle) {
-        this.columnTitle = columnTitle;
-    }
+	public void setColumnTitle(final String columnTitle) {
+		this.columnTitle = columnTitle;
+	}
 }
