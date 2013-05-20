8612de3731a0ec4083127dc6f6fe1f48841086bd
jcaddel <jcaddel@kuali.org>
2013-05-20 09:58:13 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20994 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/TypedRequest.java
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/TypedRequest.java
@@ -7,4 +7,20 @@ public class TypedRequest {
 	AmazonWebServiceRequestType type;
 	AmazonWebServiceRequest request;
 
+	public AmazonWebServiceRequestType getType() {
+		return type;
+	}
+
+	public void setType(AmazonWebServiceRequestType type) {
+		this.type = type;
+	}
+
+	public AmazonWebServiceRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(AmazonWebServiceRequest request) {
+		this.request = request;
+	}
+
 }
