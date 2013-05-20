0f506410895804d23e572c03419d83d2ede4a673
jcaddel <jcaddel@kuali.org>
2013-05-20 19:16:33 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21071 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/cloudfront/CloudFrontConstants.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/cloudfront/CloudFrontConstants.java
@@ -9,10 +9,11 @@ public abstract class CloudFrontConstants {
 
 	public static final String DATE_DISPLAY_FORMAT = "EEE, dd MMM yyyy HH:mm:ss z";
 	public static final String DATE_DISPLAY_TIMEZONE = "UTC";
-	public static final String GOOGLE_ANALYTICS_ACCOUNT = "UA-16781661-1";
 	public static final String ENCODING = "UTF-8";
 	public static final String DEFAULT_CACHE_CONTROL = "max-age=3600, must-revalidate";
 	public static final CannedAccessControlList DEFAULT_ACL = CannedAccessControlList.PublicRead;
 	public static final List<String> DEFAULT_WELCOME_FILES = Arrays.asList("index.html", "welcome.html", "portal.html");
 
+	// This is Kuali specific. Should move this elsewhere
+	public static final String GOOGLE_ANALYTICS_ACCOUNT = "UA-16781661-1";
 }
