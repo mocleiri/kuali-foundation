4b6fece037197ea478dc9c3b131e964bfc03ec3b
jcaddel <jcaddel@kuali.org>
2013-05-20 12:41:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21000 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/DefaultCloudFrontService.java
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/DefaultCloudFrontService.java
@@ -15,27 +15,30 @@ public class DefaultCloudFrontService implements CloudFrontService {
 	public List<TypedRequest> getIndexObjectRequests(CloudFrontContext context, List<ObjectListing> listings) {
 
 		String bucket = context.getBucketContext().getName();
-		String delimiter = context.getBucketContext().getDelimiter();
 
 		List<TypedRequest> requests = new ArrayList<TypedRequest>();
 
 		for (ObjectListing listing : listings) {
-			String dirKey = listing.getPrefix();
-			String dirNoSlashKey = removeTrailingSuffix(dirKey, delimiter);
-
-			PutObjectRequest request = CloudFrontUtils.getPutIndexObjectRequest(bucket, context.getCacheControl(), null, dirNoSlashKey);
-			requests.add(new TypedRequest(request, AmazonWebServiceRequestType.PUT));
-
+			addDirNoSlash(context, listing, requests);
 			String welcomeFileKey = CloudFrontUtils.getWelcomeFileKey(listing, context.getWelcomeFiles());
 			if (welcomeFileKey == null) {
 				// Create s3://bucket/foo/bar/
-				CloudFrontUtils.getPutIndexObjectRequest(bucket, context.getCacheControl(), null, dirKey);
+				PutObjectRequest request2 = CloudFrontUtils.getPutIndexObjectRequest(bucket, context.getCacheControl(), null, listing.getPrefix());
+				requests.add(new TypedRequest(request2, AmazonWebServiceRequestType.PUT));
 			} else {
 				// Copy s3://bucket/foo/bar/index.html -> s3://bucket/foo/bar/
-				CloudFrontUtils.getCopyObjectRequest(bucket, welcomeFileKey, dirKey);
+				CloudFrontUtils.getCopyObjectRequest(bucket, welcomeFileKey, listing.getPrefix());
 			}
 		}
 		return requests;
+	}
+
+	protected void addDirNoSlash(CloudFrontContext context, ObjectListing listing, List<TypedRequest> requests) {
+		String bucket = context.getBucketContext().getName();
+		String delimiter = context.getBucketContext().getDelimiter();
+		String dirNoSlashKey = removeTrailingSuffix(listing.getPrefix(), delimiter);
+		PutObjectRequest request = CloudFrontUtils.getPutIndexObjectRequest(bucket, context.getCacheControl(), null, dirNoSlashKey);
+		requests.add(new TypedRequest(request, AmazonWebServiceRequestType.PUT));
 	}
 
 	protected String removeTrailingSuffix(String s, String suffix) {
