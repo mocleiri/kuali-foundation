f618a23e55c2e6f173012ece051cecee6321cd9a
jcaddel <jcaddel@kuali.org>
2013-05-20 09:56:59 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@20993 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/TypedRequest.java
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/TypedRequest.java
@@ -2,7 +2,7 @@ package org.kuali.common.aws.s3.cloudfront;
 
 import com.amazonaws.AmazonWebServiceRequest;
 
-public class CopyOrPutRequest {
+public class TypedRequest {
 
 	AmazonWebServiceRequestType type;
 	AmazonWebServiceRequest request;
