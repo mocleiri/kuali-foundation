91e5493a051900c3ffd9e80491afccb936f5c57f
jcaddel <jcaddel@kuali.org>
2013-05-20 16:51:35 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21034 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/CloudFrontConstants.java
+++ kuali-s3/src/main/java/org/kuali/common/aws/s3/cloudfront/CloudFrontConstants.java
@@ -3,5 +3,6 @@ package org.kuali.common.aws.s3.cloudfront;
 public abstract class CloudFrontConstants {
 
 	public static final String DATE_DISPLAY_FORMAT = "EEE, dd MMM yyyy HH:mm:ss z";
+	public static final String DATE_DISPLAY_TIMEZONE = "UTC";
 
 }
