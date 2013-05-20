84f07621f4e057f69a32a71a3a6af127f58c44a9
jcaddel <jcaddel@kuali.org>
2013-05-20 19:44:08 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21079 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-aws/src/main/java/org/kuali/common/aws/cloudfront/IndexContext.java
@@ -0,0 +1,46 @@
+package org.kuali.common.aws.cloudfront;
+
+import java.util.List;
+
+import com.amazonaws.services.s3.model.ObjectListing;
+
+public class IndexContext {
+
+	ObjectListing listing;
+	List<String> indexData;
+	String indexHtml;
+	String welcomeFileKey;
+
+	public ObjectListing getListing() {
+		return listing;
+	}
+
+	public void setListing(ObjectListing listing) {
+		this.listing = listing;
+	}
+
+	public List<String> getIndexData() {
+		return indexData;
+	}
+
+	public void setIndexData(List<String> indexData) {
+		this.indexData = indexData;
+	}
+
+	public String getIndexHtml() {
+		return indexHtml;
+	}
+
+	public void setIndexHtml(String indexHtml) {
+		this.indexHtml = indexHtml;
+	}
+
+	public String getWelcomeFileKey() {
+		return welcomeFileKey;
+	}
+
+	public void setWelcomeFileKey(String welcomeFileKey) {
+		this.welcomeFileKey = welcomeFileKey;
+	}
+
+}
