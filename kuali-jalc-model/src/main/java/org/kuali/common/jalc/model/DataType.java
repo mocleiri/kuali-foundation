4df2fd271382d65ee819364caa228adfe14f182c
andrewlubbers <andrewlubbers@kuali.org>
2013-05-23 18:45:50 -0400

    KSENROLL-6764 Adding new modules for module name refactor
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jalc-3.0@21155 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-jalc-model/src/main/java/org/kuali/common/jalc/model/DataType.java
@@ -0,0 +1,5 @@
+package org.kuali.common.jalc.model;
+
+public enum DataType {
+    DATE, TIMESTAMP, CLOB, STRING, FLOAT, INTEGER, BIT, BLOB
+}
---
+++ kuali-jalc-model/src/main/java/org/kuali/common/jalc/model/ForeignKeyConstraintType.java
@@ -0,0 +1,9 @@
+package org.kuali.common.jalc.model;
+
+public enum ForeignKeyConstraintType {
+    CASCADE,
+    SET_NULL,
+    SET_DEFAULT,
+    RESTRICT,
+    NO_ACTION
+}
