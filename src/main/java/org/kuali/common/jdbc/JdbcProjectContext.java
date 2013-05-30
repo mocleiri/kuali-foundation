e9488149c0e19438a9225a116b828ecfec735305
jcaddel <jcaddel@kuali.org>
2013-05-30 14:28:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-3.0@21262 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/org/kuali/common/jdbc/JdbcProjectContext.java
@@ -0,0 +1,33 @@
+package org.kuali.common.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.common.util.ProjectContext;
+import org.kuali.common.util.ProjectUtils;
+
+public class JdbcProjectContext implements ProjectContext {
+
+	@Override
+	public String getGroupId() {
+		return ProjectUtils.KUALI_COMMON_GROUP_ID;
+	}
+
+	@Override
+	public String getArtifactId() {
+		return "kuali-jdbc";
+	}
+
+	@Override
+	public List<String> getPropertyLocations() {
+		List<String> locations = new ArrayList<String>();
+		locations.add("classpath:org/kuali/common/jdbc/jdbc.properties");
+		locations.add("classpath:org/kuali/common/sql/sql.xml");
+		locations.add("classpath:org/kuali/common/sql/mysql.xml");
+		locations.add("classpath:org/kuali/common/sql/oracle.xml");
+		locations.add("classpath:org/kuali/common/sql/h2.xml");
+		locations.add("classpath:org/kuali/common/sql/derby.xml");
+		return locations;
+	}
+
+}
