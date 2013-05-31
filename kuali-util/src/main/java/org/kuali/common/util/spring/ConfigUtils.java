5ba195a56bf6d7d48785e721c751c6aab81d737d
jcaddel <jcaddel@kuali.org>
2013-05-30 23:03:02 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21301 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/ConfigUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/ConfigUtils.java
@@ -15,13 +15,36 @@
  */
 package org.kuali.common.util.spring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.kuali.common.util.Project;
+import org.kuali.common.util.ProjectContext;
+import org.kuali.common.util.ProjectUtils;
 import org.kuali.common.util.property.ProjectProperties;
 import org.kuali.common.util.property.PropertiesContext;
 
 public class ConfigUtils {
+
+	public static List<ProjectProperties> getProjectProperties(List<ProjectContext> contexts) {
+		List<ProjectProperties> list = new ArrayList<ProjectProperties>();
+		for (ProjectContext context : contexts) {
+			Project project = ProjectUtils.loadProject(context);
+			ProjectProperties pp = getProjectProperties(project, context.getPropertyLocations());
+			list.add(pp);
+		}
+		return list;
+	}
+
+	public static List<ProjectProperties> getProjectProperties(ProjectContext... contexts) {
+		return getProjectProperties(Arrays.asList(contexts));
+	}
+
+	public static ProjectProperties getProjectProperties(ProjectContext context) {
+		Project project = ProjectUtils.loadProject(context);
+		return getProjectProperties(project, context.getPropertyLocations());
+	}
 
 	public static ProjectProperties getProjectProperties(Project project, List<String> locations) {
 		PropertiesContext pc = new PropertiesContext();
