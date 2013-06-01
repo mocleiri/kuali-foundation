1333fa7d547e56d9c907084cb3892f0bb34995bb
jcaddel <jcaddel@kuali.org>
2013-05-31 20:20:35 -0400

    Revert to version 2.0.8 of kuali-common and revert mojo changes as they cause an NPE for some reason.  The 2.0.8 version of kuali-common can no longer publish stuff to Maven Central, so we are just going to publish version 1.1.14 of maven-impex-plugin internally.  Should have Rice completely off of maven-impex-plugin soon
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/maven-impex-plugin-1.0@21383 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/org/apache/torque/mojo/DataSqlMojo.java
+++ src/main/java/org/apache/torque/mojo/DataSqlMojo.java
@@ -25,8 +25,9 @@ import org.apache.torque.util.SimpleScanner;
 import org.kuali.core.db.torque.KualiTorqueDataSQLTask;
 
 /**
- * Generates platform specific SQL from database agnostic XML files. Each SQL file created by this goal contains data for a single table. The database platform to generate SQL for
- * is determined by ${targetDatabase}. See also <code>impex:schemasql</code>
+ * Generates platform specific SQL from database agnostic XML files. Each SQL file created by this goal contains data
+ * for a single table. The database platform to generate SQL for is determined by ${targetDatabase}. See also
+ * <code>impex:schemasql</code>
  * 
  * @goal datasql
  * @phase generate-sources
@@ -35,7 +36,7 @@ public class DataSqlMojo extends DataModelTaskMojo {
 	/**
 	 * The directory in which the SQL will be generated.
 	 * 
-	 * @parameter expression="${outputDir}" default-value="${project.build.directory}/classes/sql"
+	 * @parameter property="outputDir" expression="${outputDir}" default-value="${project.build.directory}/classes/sql"
 	 * @required
 	 */
 	@SuppressWarnings("unused")
@@ -44,7 +45,8 @@ public class DataSqlMojo extends DataModelTaskMojo {
 	/**
 	 * The location where the SQL file will be generated.
 	 * 
-	 * @parameter expression="${reportFile}" default-value="../../../reports/report.${project.artifactId}-data.sql"
+	 * @parameter property="reportFile" expression="${reportFile}"
+	 *            default-value="../../../reports/report.${project.artifactId}-data.sql"
 	 */
 	@SuppressWarnings("unused")
 	private String dummy2;
@@ -52,7 +54,8 @@ public class DataSqlMojo extends DataModelTaskMojo {
 	/**
 	 * The location where the context property file for velocity will be generated.
 	 * 
-	 * @parameter expression="${contextPropertiesPath}" default-value="${project.build.directory}/reports/context.datasql.properties"
+	 * @parameter property="contextPropertiesPath" expression="${contextPropertiesPath}"
+	 *            default-value="${project.build.directory}/reports/context.datasql.properties"
 	 */
 	@SuppressWarnings("unused")
 	private String dummy3;
@@ -99,7 +102,8 @@ public class DataSqlMojo extends DataModelTaskMojo {
 	/**
 	 * The DTD for the data XML files
 	 * 
-	 * @parameter expression="${dataDTD}" default-value="${project.build.directory}/generated-impex/${project.artifactId}.dtd"
+	 * @parameter expression="${dataDTD}"
+	 *            default-value="${project.build.directory}/generated-impex/${project.artifactId}.dtd"
 	 * @required
 	 */
 	private File dataDTD;
@@ -136,7 +140,6 @@ public class DataSqlMojo extends DataModelTaskMojo {
 	 * 
 	 * @return The PropertiesConfiguration containing all context properties, not null.
 	 */
-	@Override
 	protected PropertiesConfiguration getMojoContextProperties() {
 		PropertiesConfiguration configuration = new PropertiesConfiguration();
 		configuration.addProperty(TARGET_DATABASE_CONTEXT_PROPERTY, super.getTargetDatabase());
@@ -146,7 +149,6 @@ public class DataSqlMojo extends DataModelTaskMojo {
 	/**
 	 * Configures the Texen task wrapped by this mojo
 	 */
-	@Override
 	protected void configureTask() throws MojoExecutionException {
 		KualiTorqueDataSQLTask task = new KualiTorqueDataSQLTask();
 		setAntTask(task);
@@ -162,7 +164,6 @@ public class DataSqlMojo extends DataModelTaskMojo {
 	 * 
 	 * @return "sql/load/Control.vm"
 	 */
-	@Override
 	protected String getControlTemplate() {
 		return "sql/load/Control.vm";
 	}
