825fd13c0de726da43eefb105dce0a1df8a2da96
jcaddel <jcaddel@kuali.org>
2013-06-02 18:21:06 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21456 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/DeployProjectContext.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/DeployProjectContext.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.kuali.common.util.DefaultProjectContext;
+import org.kuali.common.util.MavenConstants;
+import org.kuali.common.util.Str;
 
 public class DeployProjectContext extends DefaultProjectContext {
 
@@ -16,8 +18,8 @@ public class DeployProjectContext extends DefaultProjectContext {
 
 	private static final List<String> getLocations() {
 		List<String> locations = new ArrayList<String>();
-		locations.add("classpath:org/kuali/common/deploy/deploy.properties");
-		locations.add("classpath:org/kuali/common/deploy/driver.properties");
+		locations.add("classpath:" + Str.getPath(MavenConstants.KUALI_COMMON_GROUP_ID) + "/deploy/deploy.properties");
+		locations.add("classpath:" + Str.getPath(MavenConstants.KUALI_COMMON_GROUP_ID) + "/deploy/driver.properties");
 		locations.add("classpath:${project.groupId.path}/deploy.properties");
 		locations.add("classpath:${project.groupId.path}/${project.artifactId}.properties");
 		locations.add("classpath:${project.groupId.path}/env${deploy.env}.properties");
