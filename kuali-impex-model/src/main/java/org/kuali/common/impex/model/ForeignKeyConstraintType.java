cc4a9c723f3e2ebdfa7f069d54575e11f160e22e
jcaddel <jcaddel@kuali.org>
2013-06-14 18:17:51 -0400

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jalc-3.0@21681 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex-model/src/main/java/org/kuali/common/impex/model/ForeignKeyConstraintType.java
+++ kuali-impex-model/src/main/java/org/kuali/common/impex/model/ForeignKeyConstraintType.java
@@ -1,9 +1,7 @@
 package org.kuali.common.impex.model;
 
 public enum ForeignKeyConstraintType {
-    CASCADE,
-    SET_NULL,
-    SET_DEFAULT,
-    RESTRICT,
-    NO_ACTION
+
+	CASCADE, SET_NULL, SET_DEFAULT, RESTRICT, NO_ACTION
+
 }
