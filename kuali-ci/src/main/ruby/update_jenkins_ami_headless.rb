3420ad325d05f0b4bbb045b2230f72d3af490af9
jcaddel <jcaddel@kuali.org>
2013-06-18 19:27:30 -0400

    Add a comment for explaining what the id "yui-gen78-button" is for
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21732 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ci/src/main/ruby/update_jenkins_ami_headless.rb
+++ kuali-ci/src/main/ruby/update_jenkins_ami_headless.rb
@@ -42,6 +42,8 @@ puts @browser.text_field(:name => "_.ami").value
 puts "updating ami value to #{ami_value}"
 
 @browser.text_field(:name => "_.ami").set ami_value
+
+# This is the id for the "Save" button
 @browser.button(:id => "yui-gen78-button").click
 
 @browser.link(:text => "log out").click
