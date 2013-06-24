1939f0ac3ba3ba8716b194a02829376d3b99d25c
jcaddel <jcaddel@kuali.org>
2013-06-24 17:28:29 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21867 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/test/java/org/kuali/common/impex/schema/service/impl/MockSchemaExtractionService.java
+++ kuali-impex/kuali-impex-export/src/test/java/org/kuali/common/impex/schema/service/impl/MockSchemaExtractionService.java
@@ -1,6 +1,9 @@
 package org.kuali.common.impex.schema.service.impl;
 
+import java.util.Arrays;
+
 import org.kuali.common.impex.model.Schema;
+import org.kuali.common.impex.model.Table;
 import org.kuali.common.impex.schema.service.SchemaExtractionContext;
 import org.kuali.common.impex.schema.service.SchemaExtractionService;
 
@@ -9,6 +12,14 @@ public class MockSchemaExtractionService implements SchemaExtractionService {
 	@Override
 	public Schema getSchema(SchemaExtractionContext context) {
 		Schema schema = new Schema();
+
+		Table foo = new Table();
+		foo.setName("FOO");
+
+		Table bar = new Table();
+		bar.setName("BAR");
+
+		schema.setTables(Arrays.asList(foo, bar));
 
 		return schema;
 	}
