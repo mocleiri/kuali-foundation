a8fa1d9e69730abee32ad41c4a0f03d8b5f1e212
jcaddel <jcaddel@kuali.org>
2013-06-26 09:23:32 -0400

    Just formatting.  No functional changes
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21891 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/schema/service/impl/ExtractSchemaBucketHandler.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/schema/service/impl/ExtractSchemaBucketHandler.java
@@ -30,56 +30,55 @@ import org.kuali.common.threads.ListIteratorContext;
 
 public class ExtractSchemaBucketHandler implements ElementHandler<ExtractSchemaBucket> {
 
-    SchemaExtractionService service;
+	SchemaExtractionService service;
 
-    public ExtractSchemaBucketHandler(SchemaExtractionService service) {
-        this.service = service;
-    }
+	public ExtractSchemaBucketHandler(SchemaExtractionService service) {
+		this.service = service;
+	}
 
-    @Override
-    public void handleElement(ListIteratorContext<ExtractSchemaBucket> context, int index, ExtractSchemaBucket element) {
-        Schema schema = element.getSchema();
-        SchemaExtractionContext extractionContext = element.getContext();
+	@Override
+	public void handleElement(ListIteratorContext<ExtractSchemaBucket> context, int index, ExtractSchemaBucket element) {
+		Schema schema = element.getSchema();
+		SchemaExtractionContext extractionContext = element.getContext();
 
-        if(element instanceof ExtractViewsAndSequencesBucket) {
-            Collection<View> views;
-            Collection<Sequence> sequences;
+		if (element instanceof ExtractViewsAndSequencesBucket) {
+			Collection<View> views;
+			Collection<Sequence> sequences;
 
-            try {
-                views = service.extractViews(extractionContext);
-            } catch (SQLException e) {
-                throw new RuntimeException("Exception thrown by extraction service attempting to extract view metadata: " + e.getMessage(), e);
-            }
+			try {
+				views = service.extractViews(extractionContext);
+			} catch (SQLException e) {
+				throw new RuntimeException("Exception thrown by extraction service attempting to extract view metadata: " + e.getMessage(), e);
+			}
 
-            try {
-                sequences = service.extractSequences(extractionContext);
-            } catch (SQLException e) {
-                throw new RuntimeException("Exception thrown by extraction service attempting to extract sequence metadata: " + e.getMessage(), e);
-            }
+			try {
+				sequences = service.extractSequences(extractionContext);
+			} catch (SQLException e) {
+				throw new RuntimeException("Exception thrown by extraction service attempting to extract sequence metadata: " + e.getMessage(), e);
+			}
 
-            schema.getViews().addAll(views);
-            schema.getSequences().addAll(sequences);
-        }
-        else {
+			schema.getViews().addAll(views);
+			schema.getSequences().addAll(sequences);
+		} else {
 
-            Collection<Table> tables;
-            
-            Collection<ForeignKey> foreignKeys;
-            
-            try {
-                tables = service.extractTables(element.getTableNames(), extractionContext);
-            } catch (SQLException e) {
-                throw new RuntimeException("Exception thrown by extraction service attempting to extract table metadata: " + e.getMessage(), e);
-            }
+			Collection<Table> tables;
 
-            try {
-                foreignKeys = service.extractForeignKeys(element.getTableNames(), extractionContext);
-            } catch (SQLException e) {
-                throw new RuntimeException("Exception thrown by extraction service attempting to extract foreign key metadata: " + e.getMessage(), e);
-            }
+			Collection<ForeignKey> foreignKeys;
 
-            schema.getTables().addAll(tables);
-            schema.getForeignKeys().addAll(foreignKeys);
-        }
-    }
+			try {
+				tables = service.extractTables(element.getTableNames(), extractionContext);
+			} catch (SQLException e) {
+				throw new RuntimeException("Exception thrown by extraction service attempting to extract table metadata: " + e.getMessage(), e);
+			}
+
+			try {
+				foreignKeys = service.extractForeignKeys(element.getTableNames(), extractionContext);
+			} catch (SQLException e) {
+				throw new RuntimeException("Exception thrown by extraction service attempting to extract foreign key metadata: " + e.getMessage(), e);
+			}
+
+			schema.getTables().addAll(tables);
+			schema.getForeignKeys().addAll(foreignKeys);
+		}
+	}
 }
