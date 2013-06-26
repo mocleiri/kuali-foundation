e89c50ed0b4ce98e92e09be474d1fed1ba9272ef
jcaddel <jcaddel@kuali.org>
2013-06-26 10:20:31 -0400

    Refactor things into smaller methods.  Simplify control flow.  Get all the sequences at once, then filter out the ones we don't care about, then sort them, then return the list.
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21897 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/schema/OracleSequenceFinder.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/schema/OracleSequenceFinder.java
@@ -21,11 +21,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.kuali.common.impex.model.Sequence;
 import org.kuali.common.impex.model.util.NamedElementComparator;
 import org.kuali.common.impex.util.ExtractionUtils;
+import org.kuali.common.jdbc.JdbcUtils;
+import org.kuali.common.util.Assert;
 import org.kuali.common.util.StringFilter;
 
 public class OracleSequenceFinder implements SequenceFinder {
@@ -50,36 +53,92 @@ public class OracleSequenceFinder implements SequenceFinder {
 
 	@Override
 	public List<Sequence> findSequences(StringFilter nameFilter, Connection connection) throws SQLException {
-		String query = SEQUENCE_QUERY_PREFIX + schemaName + SEQUENCE_QUERY_SUFFIX;
 
-		Statement stmt = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-		ResultSet rs = null;
-		List<Sequence> results = new ArrayList<Sequence>();
+		// Connect to the database and get Sequence objects
+		List<Sequence> sequences = getSequences(connection, schemaName);
 
-		try {
-			rs = stmt.executeQuery(query);
+		// Filter out sequences we don't care about
+		filterSequences(sequences, nameFilter);
 
-			while (rs.next()) {
-				String name = rs.getString(SEQUENCE_NAME_KEY);
-				if (isNameExcluded(name, nameFilter)) {
-					continue;
-				}
+		// Sort the sequences by name
+		Collections.sort(sequences, NamedElementComparator.getInstance());
 
-				String value = rs.getString(SEQUENCE_VALUE_KEY);
-
-				results.add(new Sequence(name, value));
-			}
-
-			stmt.close();
-		} finally {
-			ExtractionUtils.closeQuietly(rs);
-		}
-
-		Collections.sort(results, NamedElementComparator.getInstance());
-
-		return results;
+		// Return the sorted list of Sequence objects
+		return sequences;
 	}
 
+	/**
+	 * Remove Sequence objects from the list that don't match the filter
+	 */
+	protected void filterSequences(List<Sequence> sequences, StringFilter filter) {
+		Iterator<Sequence> itr = sequences.iterator();
+		while (itr.hasNext()) {
+			Sequence sequence = itr.next();
+			String name = sequence.getName();
+			if (isNameExcluded(name, filter)) {
+				itr.remove();
+			}
+		}
+	}
+
+	/**
+	 * Given a schema name and a connection to a database, return all the sequences for that schema
+	 */
+	protected List<Sequence> getSequences(Connection connection, String schemaName) throws SQLException {
+
+		// Make sure we are configured correctly
+		Assert.hasText(schemaName, "schemaName has no text");
+
+		// Setup the sequence query based on the schema we've been provided
+		String query = SEQUENCE_QUERY_PREFIX + schemaName + SEQUENCE_QUERY_SUFFIX;
+
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			// Initialize a statement
+			stmt = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+
+			// Connect to the database and extract the sequence information
+			rs = stmt.executeQuery(query);
+
+			// Setup some storage for the sequence objects
+			List<Sequence> sequences = new ArrayList<Sequence>();
+
+			// Iterate over the result set creating Sequence objects as we go
+			while (rs.next()) {
+
+				// Convert each row in the result set into a Sequence object
+				Sequence sequence = getSequence(rs);
+
+				// Add each sequence to the list
+				sequences.add(sequence);
+			}
+
+			// Return what we've found
+			return sequences;
+
+		} finally {
+
+			// TODO Move this method to JdbcUtils
+			ExtractionUtils.closeQuietly(rs);
+
+			// Close the Statement
+			JdbcUtils.closeQuietly(stmt);
+		}
+	}
+
+	/**
+	 * Convert the current row in the ResultSet into a Sequence object
+	 */
+	protected Sequence getSequence(ResultSet rs) throws SQLException {
+		String name = rs.getString(SEQUENCE_NAME_KEY);
+		String value = rs.getString(SEQUENCE_VALUE_KEY);
+		return new Sequence(name, value);
+	}
+
+	/**
+	 * Return true if we've been provided and nameFilter and the name is excluded by that nameFilter
+	 */
 	protected boolean isNameExcluded(String name, StringFilter nameFilter) {
 		if (nameFilter == null) {
 			return false;
