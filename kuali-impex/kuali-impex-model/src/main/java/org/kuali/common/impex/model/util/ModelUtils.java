31b04323e07ae683a4b7caf602cf727fc7a097fd
jcaddel <jcaddel@kuali.org>
2013-06-26 11:28:59 -0400

    javadoc
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@21905 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/model/util/ModelUtils.java
+++ kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/model/util/ModelUtils.java
@@ -82,7 +82,7 @@ public class ModelUtils {
 	}
 
 	/**
-	 * Alter the elements passed in by removing any elements that don't belong and then sort the list by name
+	 * Alter the elements passed in by removing any elements that don't belong and then sort the ones that remain by name
 	 */
 	public static <T extends NamedElement> void filterAndSortElements(List<T> elements, StringFilter filter) {
 
