c77419ed81a47bab5a2ebe88f51fe5d5eb10518f
jcaddel <jcaddel@kuali.org>
2013-07-02 13:47:38 -0400

    Refactor the project.properties generation out into it's own config that sets up the executable.  Make MetaInfProjectPropertiesConfig extend ExectuableConfig to run the executable
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22005 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/MetaInfProjectPropertiesConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/MetaInfProjectPropertiesConfig.java
@@ -15,93 +15,23 @@
  */
 package org.kuali.common.util.spring;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
-import org.kuali.common.util.CollectionUtils;
 import org.kuali.common.util.execute.Executable;
-import org.kuali.common.util.execute.ExecutablesExecutable;
-import org.kuali.common.util.execute.StorePropertiesExecutable;
-import org.kuali.common.util.execute.StoreRicePropertiesExecutable;
-import org.kuali.common.util.property.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.util.Assert;
+import org.springframework.context.annotation.Import;
 
 /**
  * Create project.properties and embed it inside META-INF for jar's/war's
  */
 @Configuration
-public class MetaInfProjectPropertiesConfig {
+@Import(MetaInfProjectPropertiesSetupConfig.class)
+public class MetaInfProjectPropertiesConfig extends ExecutableConfig {
 
 	@Autowired
-	ConfigurableEnvironment env;
+	MetaInfProjectPropertiesSetupConfig metaInfProjectPropertiesSetupConfig;
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer pspc() {
-		return new PropertySourcesPlaceholderConfigurer();
+	@Override
+	protected Executable getExecutable() {
+		return metaInfProjectPropertiesSetupConfig.storePropertiesExecutablesExectuable();
 	}
-
-	@Value(Constants.PROJECT_PROPERTIES_OUTPUTFILE)
-	File outputFile;
-
-	@Value(Constants.RICE_PROJECT_PROPERTIES_OUTPUTFILE)
-	File riceOutputFile;
-
-	@Bean
-	public Properties springProperties() {
-		return SpringUtils.getAllEnumerableProperties(env);
-	}
-
-	@Bean(initMethod = "execute")
-	public Executable storePropertiesExecutablesExectuable() {
-
-		// Extract property values from the environment
-		String encoding = SpringUtils.getProperty(env, "project.encoding");
-		String includesCSV = SpringUtils.getProperty(env, "project.metainf.includes");
-		String excludesCSV = SpringUtils.getProperty(env, "project.metainf.excludes");
-
-		// Make sure we are configured right
-		Assert.hasText(encoding);
-
-		// Convert the lists to CSV
-		List<String> includes = CollectionUtils.getTrimmedListFromCSV(includesCSV);
-		List<String> excludes = CollectionUtils.getTrimmedListFromCSV(excludesCSV);
-
-		// Get the list of all properties spring knows about
-		Properties properties = springProperties();
-
-		// Setup the regular properties file executable
-		StorePropertiesExecutable spe = new StorePropertiesExecutable();
-		spe.setSkip(SpringUtils.getBoolean(env, "project.properties.skip", false));
-		spe.setEncoding(encoding);
-		spe.setOutputFile(outputFile);
-		spe.setProperties(properties);
-		spe.setIncludes(includes);
-		spe.setExcludes(excludes);
-
-		// Setup the Rice style properties file executable
-		StoreRicePropertiesExecutable srpe = new StoreRicePropertiesExecutable();
-		srpe.setSkip(SpringUtils.getBoolean(env, "project.properties.rice.skip", false));
-		srpe.setEncoding(encoding);
-		srpe.setOutputFile(riceOutputFile);
-		srpe.setProperties(properties);
-		srpe.setIncludes(includes);
-		srpe.setExcludes(excludes);
-
-		// Create an executables list
-		List<Executable> executables = new ArrayList<Executable>();
-		executables.add(spe);
-		executables.add(srpe);
-
-		// Return an executable that executes the list
-		return new ExecutablesExecutable(executables);
-	}
-
 }
