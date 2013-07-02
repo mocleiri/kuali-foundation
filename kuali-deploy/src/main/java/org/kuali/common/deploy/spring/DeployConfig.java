464f3606b75970352c314208ae94aa8b68060eab
jcaddel <jcaddel@kuali.org>
2013-07-02 17:38:02 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22024 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DeployConfig.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DeployConfig.java
@@ -19,6 +19,7 @@ import org.kuali.common.deploy.SysAdminExecutable;
 import org.kuali.common.deploy.TomcatApplicationServer;
 import org.kuali.common.http.HttpContext;
 import org.kuali.common.http.HttpWaitExecutable;
+import org.kuali.common.jdbc.spring.AbstractSqlController;
 import org.kuali.common.util.Artifact;
 import org.kuali.common.util.LocationUtils;
 import org.kuali.common.util.PropertyUtils;
@@ -35,14 +36,14 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @Configuration
-@Import({ SqlControllerConfig.class })
+@Import({ AbstractSqlController.class })
 public class DeployConfig {
 
 	@Autowired
 	ConfigurableEnvironment env;
 
 	@Autowired
-	SqlControllerConfig sqlController;
+	AbstractSqlController sqlController;
 
 	protected Artifact getJdbcDriverArtifact() {
 		Artifact a = new Artifact();
