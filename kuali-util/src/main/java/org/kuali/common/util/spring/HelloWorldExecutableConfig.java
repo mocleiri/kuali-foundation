419d8b2758cc982bccafc201dfe5c6b2b2e2b9d2
jcaddel <jcaddel@kuali.org>
2013-07-02 16:13:38 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22007 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/HelloWorldExecutableConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/HelloWorldExecutableConfig.java
@@ -16,16 +16,20 @@
 package org.kuali.common.util.spring;
 
 import org.kuali.common.util.execute.Executable;
-import org.kuali.common.util.execute.HelloWorldExecutable;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-public class HelloWorldConfig {
+@Import(HelloWorldConfig.class)
+public class HelloWorldExecutableConfig extends ExecutableConfig {
 
-	@Bean
-	public Executable helloWorldExecutable() {
-		return new HelloWorldExecutable();
+	@Autowired
+	HelloWorldConfig helloWorldConfig;
+
+	@Override
+	protected Executable getExecutable() {
+		return helloWorldConfig.helloWorldExecutable();
 	}
 
 }
