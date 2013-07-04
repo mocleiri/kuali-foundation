0a42e57416a694dbfbdd16a46bce999978abdc4f
jcaddel <jcaddel@kuali.org>
2013-07-04 07:38:52 -0400

    Non-functional refactoring
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22063 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/util/ExportUtils.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/util/ExportUtils.java
@@ -34,9 +34,10 @@ public class ExportUtils {
 
 	protected static final String SIZE_PROPERTY_SUFFIX = ".size";
 	protected static final String ROWS_PROPERTY_SUFFIX = ".rows";
-	public static final Integer DEFAULT_DATA_THREADS = 15;
-	public static final Integer DEFAULT_ROW_INTERVAL = 50;
-	public static final Integer DEFAULT_DATA_INTERVAL = 50 * 1024;
+
+	public static final int DEFAULT_DATA_THREADS = 15;
+	public static final int DEFAULT_ROW_INTERVAL = 50;
+	public static final String DEFAULT_DATA_INTERVAL = "50k";
 
 	public static <T extends NamedElement> Collection<T> getIncludedElements(StringFilter stringFilter, List<T> elements) {
 
@@ -52,7 +53,7 @@ public class ExportUtils {
 	}
 
 	/**
-	 * Populate table data size and row count from properties.  Use the lowercase table name as the prefix for each key
+	 * Populate table data size and row count from properties. Use the lowercase table name as the prefix for each key
 	 * 
 	 * @param tableStatistics
 	 *            a properties object
@@ -63,42 +64,43 @@ public class ExportUtils {
 	 */
 	public static void populateTableStatistics(Properties tableStatistics, Table table, ExportTableContext context) {
 		if (tableStatistics != null) {
-            String tableNameKeyFormat = table.getName().toLowerCase();
-            String sizeVal = tableStatistics.getProperty(tableNameKeyFormat + SIZE_PROPERTY_SUFFIX);
-            String rowCountVal = tableStatistics.getProperty(tableNameKeyFormat + ROWS_PROPERTY_SUFFIX);
+			String tableNameKeyFormat = table.getName().toLowerCase();
+			String sizeVal = tableStatistics.getProperty(tableNameKeyFormat + SIZE_PROPERTY_SUFFIX);
+			String rowCountVal = tableStatistics.getProperty(tableNameKeyFormat + ROWS_PROPERTY_SUFFIX);
 
-            if (StringUtils.isNotBlank(sizeVal)) {
-                context.setSize(Long.parseLong(sizeVal));
-            }
-            if (StringUtils.isNotBlank(rowCountVal)) {
-                context.setRowCount(Long.parseLong(rowCountVal));
-            }
-        }
+			if (StringUtils.isNotBlank(sizeVal)) {
+				context.setSize(Long.parseLong(sizeVal));
+			}
+			if (StringUtils.isNotBlank(rowCountVal)) {
+				context.setRowCount(Long.parseLong(rowCountVal));
+			}
+		}
 	}
 
-    /**
-     * Store table data size and row count to a file.  Use the lowercase table name as the prefix for each key
-     * This method should append to any existing statistics found at the location, if any already exist
-     *
-     * @param tableResults list of exported table data results
-     * @param location file path defining where to write the property file
-     */
-    public static void storeTableStatistics(List<ExportTableResult> tableResults, String location) {
-        Properties tableStatistics;
+	/**
+	 * Store table data size and row count to a file. Use the lowercase table name as the prefix for each key This method should append to any existing statistics found at the
+	 * location, if any already exist
+	 * 
+	 * @param tableResults
+	 *            list of exported table data results
+	 * @param location
+	 *            file path defining where to write the property file
+	 */
+	public static void storeTableStatistics(List<ExportTableResult> tableResults, String location) {
+		Properties tableStatistics;
 
-        if (LocationUtils.exists(location)) {
-            tableStatistics = PropertyUtils.load(location);
-        }
-        else {
-            tableStatistics = new Properties();
-        }
+		if (LocationUtils.exists(location)) {
+			tableStatistics = PropertyUtils.loadSilently(location);
+		} else {
+			tableStatistics = new Properties();
+		}
 
-        for (ExportTableResult result : tableResults) {
-            String nameKey = result.getTableContext().getTable().getName().toLowerCase();
-            tableStatistics.setProperty(nameKey + SIZE_PROPERTY_SUFFIX, Long.toString(result.getSize()));
-            tableStatistics.setProperty(nameKey + ROWS_PROPERTY_SUFFIX, Long.toString(result.getRows()));
-        }
+		for (ExportTableResult result : tableResults) {
+			String nameKey = result.getTableContext().getTable().getName().toLowerCase();
+			tableStatistics.setProperty(nameKey + SIZE_PROPERTY_SUFFIX, Long.toString(result.getSize()));
+			tableStatistics.setProperty(nameKey + ROWS_PROPERTY_SUFFIX, Long.toString(result.getRows()));
+		}
 
-        PropertyUtils.store(tableStatistics, new File(location));
-    }
+		PropertyUtils.store(tableStatistics, new File(location));
+	}
 }
