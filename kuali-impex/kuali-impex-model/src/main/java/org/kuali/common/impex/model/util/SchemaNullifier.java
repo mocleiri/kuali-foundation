1eacff0be6a9a07c1b6b828a70974afe3b952722
jcaddel <jcaddel@kuali.org>
2013-07-05 13:54:20 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22183 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/model/util/SchemaNullifier.java
+++ kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/model/util/SchemaNullifier.java
@@ -5,13 +5,12 @@ import org.kuali.common.impex.model.Schema;
 import org.kuali.common.impex.model.Table;
 import org.kuali.common.impex.model.TypeSize;
 import org.kuali.common.util.CollectionUtils;
-import org.kuali.common.util.nullify.Nullify;
 import org.springframework.util.Assert;
 
 /**
  * Null out values in a <code>schema</code> that are not needed when converting it to XML
  */
-public class SchemaNullifier implements Nullify {
+public class SchemaNullifier implements Nullifier {
 
 	Schema schema;
 
