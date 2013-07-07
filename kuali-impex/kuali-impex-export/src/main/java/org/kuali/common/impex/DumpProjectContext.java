ef7611e32c70779b7c939c3ced321b9d838c7b90
jcaddel <jcaddel@kuali.org>
2013-07-07 12:37:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22242 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/DumpProjectContext.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/DumpProjectContext.java
@@ -1,13 +1,12 @@
 package org.kuali.common.impex;
 
 import org.kuali.common.util.DefaultProjectContext;
-import org.kuali.common.util.MavenConstants;
-import org.kuali.common.util.Str;
+import org.kuali.common.util.ProjectUtils;
 
 public class DumpProjectContext extends DefaultProjectContext {
 
 	private static final String ARTIFACT_ID = "kuali-impex-export";
-	private static final String LOCATION = "classpath:" + Str.getPath(MavenConstants.KUALI_COMMON_GROUP_ID) + ARTIFACT_ID + "/dump.properties";
+	private static final String LOCATION = ProjectUtils.getCommonClassPathPrefix(ARTIFACT_ID) + "/dump.properties";
 
 	public DumpProjectContext() {
 		super(ARTIFACT_ID, LOCATION);
