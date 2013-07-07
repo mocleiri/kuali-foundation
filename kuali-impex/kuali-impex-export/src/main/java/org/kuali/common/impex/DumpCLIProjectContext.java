3432674c76c862668d7f025754d334df53495514
jcaddel <jcaddel@kuali.org>
2013-07-07 14:27:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22258 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/DumpCLIProjectContext.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/DumpCLIProjectContext.java
@@ -6,7 +6,7 @@ import org.kuali.common.util.DefaultProjectContext;
 
 public class DumpCLIProjectContext extends DefaultProjectContext {
 
-	public static final String ARTIFACT_ID = "kuali-impex-export";
+	public static final String ARTIFACT_ID = DumpProjectContext.ARTIFACT_ID;
 	private static final String LOCATION = "${user.home}/.impex/dump.properties";
 
 	public DumpCLIProjectContext() {
