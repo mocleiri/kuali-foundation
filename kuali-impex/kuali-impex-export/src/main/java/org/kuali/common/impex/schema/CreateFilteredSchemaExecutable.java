f5d7a695bbbdb0bb4b954c28cacb6ee6f813def4
jcaddel <jcaddel@kuali.org>
2013-07-07 22:38:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22304 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/schema/CreateFilteredSchemaExecutable.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/schema/CreateFilteredSchemaExecutable.java
@@ -7,32 +7,45 @@ import java.util.List;
 import org.kuali.common.impex.model.Schema;
 import org.kuali.common.impex.model.util.ModelUtils;
 import org.kuali.common.impex.util.DumpConstants;
+import org.kuali.common.util.FileSystemUtils;
 import org.kuali.common.util.LocationUtils;
 import org.kuali.common.util.StringFilter;
 import org.kuali.common.util.execute.Executable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
 public class CreateFilteredSchemaExecutable implements Executable {
 
+	private static final Logger logger = LoggerFactory.getLogger(CreateFilteredSchemaExecutable.class);
+
 	DumpSchemaService service = DumpSchemaExecutable.DEFAULT_EXPORT_SCHEMA_SERVICE;
 	List<String> includes = Arrays.asList(DumpConstants.DEFAULT_INCLUDE);
 	List<String> excludes = Arrays.asList(DumpConstants.DEFAULT_EXCLUDE);
-	File inputSchemaFile;
-	File outputSchemaFile;
+	File schemaInputFile;
+	File schemaOutputFile;
+	File relativeDir;
 
 	@Override
 	public void execute() {
 		// Make sure we are configured correctly
 		Assert.notNull(service, "service is null");
-		Assert.notNull(inputSchemaFile, "inputSchemaFile is null");
-		Assert.notNull(outputSchemaFile, "outputSchemaFile is null");
-		Assert.isTrue(LocationUtils.exists(inputSchemaFile), "inputSchemaFile does not exist");
+		Assert.notNull(schemaInputFile, "inputSchemaFile is null");
+		Assert.notNull(schemaOutputFile, "outputSchemaFile is null");
+		Assert.isTrue(LocationUtils.exists(schemaInputFile), "inputSchemaFile does not exist");
 
 		// Load an existing schema, filter it, persist it back to the file system as XML
-		Schema schema = service.getSchema(inputSchemaFile);
+		Schema schema = service.getSchema(schemaInputFile);
 		StringFilter filter = StringFilter.getInstance(includes, excludes);
 		ModelUtils.filter(schema, filter);
-		service.dumpSchema(schema, outputSchemaFile);
+
+		if (FileSystemUtils.isParent(relativeDir, schemaOutputFile)) {
+			logger.info("Creating - [{}]", FileSystemUtils.getRelativePath(relativeDir, schemaOutputFile));
+		} else {
+			logger.info("Creating - [{}]", LocationUtils.getCanonicalPath(schemaOutputFile));
+		}
+
+		service.dumpSchema(schema, schemaOutputFile);
 	}
 
 	public DumpSchemaService getService() {
@@ -59,20 +72,28 @@ public class CreateFilteredSchemaExecutable implements Executable {
 		this.excludes = excludes;
 	}
 
-	public File getInputSchemaFile() {
-		return inputSchemaFile;
+	public File getSchemaInputFile() {
+		return schemaInputFile;
 	}
 
-	public void setInputSchemaFile(File inputSchemaFile) {
-		this.inputSchemaFile = inputSchemaFile;
+	public void setSchemaInputFile(File inputSchemaFile) {
+		this.schemaInputFile = inputSchemaFile;
 	}
 
-	public File getOutputSchemaFile() {
-		return outputSchemaFile;
+	public File getSchemaOutputFile() {
+		return schemaOutputFile;
 	}
 
-	public void setOutputSchemaFile(File outputSchemaFile) {
-		this.outputSchemaFile = outputSchemaFile;
+	public void setSchemaOutputFile(File outputSchemaFile) {
+		this.schemaOutputFile = outputSchemaFile;
+	}
+
+	public File getRelativeDir() {
+		return relativeDir;
+	}
+
+	public void setRelativeDir(File relativeDir) {
+		this.relativeDir = relativeDir;
 	}
 
 }
