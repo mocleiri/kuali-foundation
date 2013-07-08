b8b285848cd8a1a3be1b86979c514c74c5e9db29
jcaddel <jcaddel@kuali.org>
2013-07-07 20:41:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22281 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/staging/CreateFilteredSchemaExecutable.java
@@ -0,0 +1,38 @@
+package org.kuali.common.impex.staging;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import org.kuali.common.impex.model.Schema;
+import org.kuali.common.impex.model.util.ModelUtils;
+import org.kuali.common.impex.schema.DumpSchemaExecutable;
+import org.kuali.common.impex.schema.DumpSchemaService;
+import org.kuali.common.impex.util.DumpConstants;
+import org.kuali.common.util.LocationUtils;
+import org.kuali.common.util.StringFilter;
+import org.kuali.common.util.execute.Executable;
+import org.springframework.util.Assert;
+
+public class CreateFilteredSchemaExecutable implements Executable {
+
+	DumpSchemaService service = DumpSchemaExecutable.DEFAULT_EXPORT_SCHEMA_SERVICE;
+	List<String> includes = Arrays.asList(DumpConstants.DEFAULT_INCLUDE);
+	List<String> excludes = Arrays.asList(DumpConstants.DEFAULT_EXCLUDE);
+	File inputSchemaFile;
+	File outputSchemaFile;
+
+	@Override
+	public void execute() {
+		Assert.notNull(service, "service is null");
+		Assert.notNull(inputSchemaFile, "inputSchemaFile is null");
+		Assert.isTrue(LocationUtils.exists(inputSchemaFile), "inputSchemaFile does not exist");
+		Assert.notNull(outputSchemaFile, "outputSchemaFile is null");
+
+		Schema schema = service.getSchema(inputSchemaFile);
+		StringFilter filter = StringFilter.getInstance(includes, excludes);
+		ModelUtils.filter(schema, filter);
+		service.dumpSchema(schema, outputSchemaFile);
+	}
+
+}
