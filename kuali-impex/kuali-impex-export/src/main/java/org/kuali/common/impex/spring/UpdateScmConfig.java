3e37c90a3ad58fa7c917f704d98dc17ddaf4bd95
jcaddel <jcaddel@kuali.org>
2013-07-09 21:38:03 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22428 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/UpdateScmConfig.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/UpdateScmConfig.java
@@ -1,7 +1,9 @@
 package org.kuali.common.impex.spring;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.kuali.common.util.ScmRequest;
 import org.kuali.common.util.execute.BuildScmExecutable;
 import org.kuali.common.util.execute.PrepareScmDirExecutable;
 import org.kuali.common.util.spring.SpringUtils;
@@ -17,6 +19,8 @@ public class UpdateScmConfig {
 
 	private static final String UPDATE_KEY = "impex.scm.update.skip";
 	private static final String MESSAGE_KEY = "impex.scm.update.commitMessage";
+	private static final String SCM_URL_KEY = "project.scm.developerConnection";
+	private static final String STATS_KEY = DumpDataConfig.STATS_LOCATION_KEY;
 
 	@Autowired
 	Environment env;
@@ -30,12 +34,14 @@ public class UpdateScmConfig {
 		boolean skip = SpringUtils.getBoolean(env, UPDATE_KEY);
 		String commitMessage = SpringUtils.getProperty(env, MESSAGE_KEY);
 		List<PrepareScmDirExecutable> preparers = projectPrepareScmConfig.prepareScmDirExecutables();
+		ScmRequest request = new ScmRequest();
+		request.setCommits(Arrays.asList(SpringUtils.getFile(env, STATS_KEY)));
 
 		BuildScmExecutable exec = new BuildScmExecutable();
 		exec.setExecutables(preparers);
 		exec.setSkip(skip);
 		exec.setCommitMessage(commitMessage);
+		exec.setRequest(request);
 		return exec;
 	}
-
 }
