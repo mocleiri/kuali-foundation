b4af98e6366282235e3024527e74d8e403826052
andrewlubbers <andrewlubbers@kuali.org>
2013-07-10 14:06:22 -0400

    KSENROLL-7622 Updated extract config to find view and sequence finders through properties
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22463 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/ExtractSchemaConfig.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/ExtractSchemaConfig.java
@@ -15,16 +15,9 @@
 
 package org.kuali.common.impex.spring;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import javax.sql.DataSource;
 
-import org.kuali.common.impex.schema.MySqlSequenceFinder;
-import org.kuali.common.impex.schema.MySqlViewFinder;
-import org.kuali.common.impex.schema.OracleSequenceFinder;
-import org.kuali.common.impex.schema.OracleViewFinder;
 import org.kuali.common.impex.schema.SequenceFinder;
 import org.kuali.common.impex.schema.ViewFinder;
 import org.kuali.common.impex.schema.service.ExtractSchemaContext;
@@ -51,17 +44,14 @@ public class ExtractSchemaConfig {
 	public static final String THREADS_KEY = "impex.extract.schema.threads";
 	public static final String SERVICE_KEY = "impex.extract.schema.service";
 
-	protected static final String ORACLE_SEQUENCE_FINDER_KEY = "impex.extract.schema.oracle.sequence.finder";
-	protected static final String ORACLE_VIEW_FINDER_KEY = "impex.extract.schema.oracle.view.finder";
-
-	protected static final String MYSQL_SEQUENCE_FINDER_KEY = "impex.extract.schema.mysql.sequence.finder";
-	protected static final String MYSQL_VIEW_FINDER_KEY = "impex.extract.schema.mysql.view.finder";
-
 	// By default, include everything, exclude nothing
 	protected static final String DEFAULT_INCLUDES = DumpConstants.DEFAULT_INCLUDE;
 	protected static final String DEFAULT_EXCLUDES = DumpConstants.DEFAULT_EXCLUDE;
 
 	protected static final String SKIP_KEY = "impex.extract.schema.skip";
+
+    protected static final String VIEW_FINDER_KEY = "impex.extract.schema.viewfinder";
+    protected static final String SEQUENCE_FINDER_KEY = "impex.extract.schema.sequencefinder";
 
 	@Autowired
 	Environment env;
@@ -96,15 +86,12 @@ public class ExtractSchemaConfig {
 		// DataSource for obtaining connections to the database
 		DataSource dataSource = dataSourceConfig.jdbcDataSource();
 
-		// The type of database we are connecting to
-		String vendor = SpringUtils.getProperty(env, DumpConstants.DB_VENDOR_KEY);
-
 		// This is used to filter out tables/views/sequences/foreign keys
 		StringFilter nameFilter = getNameFilter();
 
 		// Get database specific finders for sequences and views
-		SequenceFinder sequenceFinder = getSequenceFinderMap().get(vendor);
-		ViewFinder viewFinder = getViewFinderMap().get(vendor);
+        SequenceFinder sequenceFinder = SpringUtils.getInstance(env, SEQUENCE_FINDER_KEY);
+        ViewFinder viewFinder = SpringUtils.getInstance(env, VIEW_FINDER_KEY);
 
 		// Setup our context with pojo's aggregated from the Spring configuration
 		ExtractSchemaContext context = new ExtractSchemaContext();
@@ -125,28 +112,6 @@ public class ExtractSchemaConfig {
 
 		// Setup the name filter
 		return StringFilter.getInstance(includes, excludes);
-	}
-
-	protected Map<String, SequenceFinder> getSequenceFinderMap() {
-
-		SequenceFinder oracle = SpringUtils.getInstance(env, ORACLE_SEQUENCE_FINDER_KEY, OracleSequenceFinder.class);
-		SequenceFinder mysql = SpringUtils.getInstance(env, MYSQL_SEQUENCE_FINDER_KEY, MySqlSequenceFinder.class);
-
-		Map<String, SequenceFinder> result = new HashMap<String, SequenceFinder>();
-		result.put(OracleSequenceFinder.SUPPORTED_VENDOR, oracle);
-		result.put(MySqlSequenceFinder.SUPPORTED_VENDOR, mysql);
-		return result;
-	}
-
-	protected Map<String, ViewFinder> getViewFinderMap() {
-
-		ViewFinder oracle = SpringUtils.getInstance(env, ORACLE_VIEW_FINDER_KEY, OracleViewFinder.class);
-		ViewFinder mysql = SpringUtils.getInstance(env, MYSQL_VIEW_FINDER_KEY, MySqlViewFinder.class);
-
-		Map<String, ViewFinder> result = new HashMap<String, ViewFinder>();
-		result.put(OracleViewFinder.SUPPORTED_VENDOR, oracle);
-		result.put(MySqlViewFinder.SUPPORTED_VENDOR, mysql);
-		return result;
 	}
 
 }
