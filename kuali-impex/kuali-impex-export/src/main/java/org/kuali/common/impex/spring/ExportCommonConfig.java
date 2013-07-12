57cde9d8182348d09015ec8954409165dac1588b
jcaddel <jcaddel@kuali.org>
2013-07-12 18:51:08 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22595 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/ExportCommonConfig.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/ExportCommonConfig.java
@@ -6,24 +6,34 @@ import org.kuali.common.impex.schema.DumpSchemaService;
 import org.kuali.common.impex.schema.execute.DumpSchemasExecutable;
 import org.kuali.common.impex.schema.service.ExtractSchemaExecutable;
 import org.kuali.common.impex.schema.service.ExtractSchemaService;
+import org.kuali.common.util.spring.SpringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class ExportCommonConfig {
 
+	private static final String DUMP_SCHEMA_SERVICE_KEY = "impex.dump.schema.service";
+	private static final String DUMP_DATA_SERVICE_KEY = "impex.dump.data.service";
+	private static final String EXTRACT_SCHEMA_SERVICE_KEY = "impex.extract.schema.service";
+
+	@Autowired
+	Environment env;
+
 	@Bean
 	public DumpSchemaService exportDumpSchemaService() {
-		return DumpSchemasExecutable.DEFAULT_SERVICE;
+		return SpringUtils.getInstance(env, DUMP_SCHEMA_SERVICE_KEY, DumpSchemasExecutable.DEFAULT_SERVICE.getClass());
 	}
 
 	@Bean
 	public ExtractSchemaService exportExtractSchemaService() {
-		return ExtractSchemaExecutable.DEFAULT_SERVICE;
+		return SpringUtils.getInstance(env, EXTRACT_SCHEMA_SERVICE_KEY, ExtractSchemaExecutable.DEFAULT_SERVICE.getClass());
 	}
 
 	@Bean
 	public DumpDataService exportDumpDataService() {
-		return DumpDataExecutable.DEFAULT_SERVICE;
+		return SpringUtils.getInstance(env, DUMP_DATA_SERVICE_KEY, DumpDataExecutable.DEFAULT_SERVICE.getClass());
 	}
 }
