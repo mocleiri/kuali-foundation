a285920940079dc895201d4ac3e609781f90ace8
jcaddel <jcaddel@kuali.org>
2013-07-12 11:28:51 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22570 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/UpdateScmConfig.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/UpdateScmConfig.java
@@ -9,9 +9,11 @@ import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 @Configuration
+@Import({ ProjectStagingConfig.class })
 public class UpdateScmConfig {
 
 	private static final String SKIP_KEY = "impex.scm.skip";
