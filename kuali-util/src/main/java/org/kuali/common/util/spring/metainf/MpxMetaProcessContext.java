57ed5ed65c4186362629f57982bb45c024a3a9e8
andrewlubbers <andrewlubbers@kuali.org>
2013-07-12 17:05:04 -0400

    KSENROLL-7622 Created process context classes for mpx and sql resource metadata
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22593 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/spring/metainf/MpxMetaProcessContext.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2011 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.kuali.common.util.spring.metainf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.common.util.DefaultProjectContext;
+import org.kuali.common.util.ProjectConstants;
+
+/**
+ * This class represents a property context for the process of producing META-INF resource files
+ * specific to the standard Kuali structure for .mpx resources
+ */
+public class MpxMetaProcessContext extends DefaultProjectContext {
+
+    public MpxMetaProcessContext() {
+        super(ProjectConstants.COMMON_GROUP_ID, ProjectConstants.UTIL_ARTIFACT_ID);
+
+        // build property locations
+        List<String> sqlPropertyLocations = new ArrayList<String>();
+        sqlPropertyLocations.add("classpath:org/kuali/common/kuali-util/metainf/common.properties");
+        sqlPropertyLocations.add("classpath:org/kuali/common/kuali-util/metainf/mpx.properties");
+        setPropertyLocations(sqlPropertyLocations);
+    }
+
+}
