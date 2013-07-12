c72e3501996db1dc225d5945e0d0fbc02cd8617a
jcaddel <jcaddel@kuali.org>
2013-07-12 11:22:51 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22569 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/UpdateScmConfig.java
@@ -0,0 +1,34 @@
+package org.kuali.common.impex.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.common.util.execute.Executable;
+import org.kuali.common.util.execute.ExecutablesExecutable;
+import org.kuali.common.util.spring.SpringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class UpdateScmConfig {
+
+	private static final String SKIP_KEY = "impex.scm.skip";
+
+	@Autowired
+	Environment env;
+
+	@Autowired
+	ProjectStagingConfig projectStagingConfig;
+
+	@Bean
+	public Executable updateScmExecutable() {
+		boolean skip = SpringUtils.getBoolean(env, SKIP_KEY, false);
+		List<Executable> execs = new ArrayList<Executable>();
+		execs.add(projectStagingConfig.copySchemaFilesExecutable());
+		execs.add(projectStagingConfig.copyProjectDataFilesExecutable());
+		return new ExecutablesExecutable(execs, skip);
+	}
+
+}
