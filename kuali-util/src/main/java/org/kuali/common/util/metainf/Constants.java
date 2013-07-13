eedec818d7a40a274a4b914e308b4277089e65a9
jcaddel <jcaddel@kuali.org>
2013-07-13 11:49:21 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22630 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/Constants.java
@@ -0,0 +1,13 @@
+package org.kuali.common.util.metainf;
+
+public final class Constants {
+
+	private Constants() {
+		super();
+	}
+
+	public static final String DEFAULT_PREFIX = "classpath:";
+	public static final boolean DEFAULT_SORT = true;
+	public static final String METAINF = "metainf";
+
+}
