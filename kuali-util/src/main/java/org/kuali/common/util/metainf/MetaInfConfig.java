bb928e91deafd3dc7e03df4f5363857c455d307c
jcaddel <jcaddel@kuali.org>
2013-07-13 11:22:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22619 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/MetaInfConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/MetaInfConfig.java
@@ -31,79 +31,65 @@ import org.springframework.core.env.Environment;
 @Configuration
 public class MetaInfConfig {
 
-    protected static final String EXECUTABLE_SKIP_KEY = "util.metainf.skip";
+	protected static final String EXECUTABLE_SKIP_KEY = "metainf.skip";
+	protected static final String METAINF_CONTEXTS_KEY = "metainf.contexts";
+	protected static final String ADD_LINE_COUNT_KEY = ".linecount";
+	protected static final boolean DEFAULT_ADD_LINE_COUNT = false;
+	protected static final String ADD_PROPERTIES_FILE_KEY = ".propertiesfile";
+	protected static final boolean DEFAULT_ADD_PROPERTIES_FILE = false;
+	protected static final String SORT_KEY = ".sort";
+	protected static final boolean DEFAULT_SORT = MetaInfContext.DEFAULT_SORT;
+	protected static final String BASE_DIR_KEY = ".basedir";
+	protected static final String OUTPUT_FILE_KEY = ".output";
+	protected static final String INCLUDES_KEY = ".includes";
+	protected static final String EXCLUDES_KEY = ".excludes";
+	protected static final String DEFAULT_EXCLUDES = Constants.NONE;
+	protected static final String PREFIX_KEY = ".prefix";
+	protected static final String DEFAULT_PREFIX = MetaInfContext.DEFAULT_PREFIX;
 
-    protected static final String METAINF_CONTEXTS_KEY = "util.metainf.contexts";
+	@Autowired
+	Environment env;
 
-    protected static final String ADD_LINE_COUNT_KEY = ".linecount";
+	@Bean
+	public Executable metaInfExecutable() {
 
-    protected static final boolean DEFAULT_ADD_LINE_COUNT = false;
+		String prefixes = SpringUtils.getProperty(env, METAINF_CONTEXTS_KEY);
 
-    protected static final String ADD_PROPERTIES_FILE_KEY = ".propertiesfile";
+		List<String> contextKeys = CollectionUtils.getTrimmedListFromCSV(prefixes);
 
-    protected static final boolean DEFAULT_ADD_PROPERTIES_FILE = false;
+		List<MetaInfContext> contexts = new ArrayList<MetaInfContext>();
 
-    protected static final String SORT_KEY = ".sort";
+		for (String contextKey : contextKeys) {
+			MetaInfContext context = new MetaInfContext();
 
-    protected static final boolean DEFAULT_SORT = MetaInfContext.DEFAULT_SORT;
+			context.setAddLineCount(getDefaultedBoolean(contextKey, ADD_LINE_COUNT_KEY, DEFAULT_ADD_LINE_COUNT));
+			context.setAddPropertiesFile(getDefaultedBoolean(contextKey, ADD_PROPERTIES_FILE_KEY, DEFAULT_ADD_PROPERTIES_FILE));
+			context.setSort(getDefaultedBoolean(contextKey, SORT_KEY, DEFAULT_SORT));
 
-    protected static final String BASE_DIR_KEY = ".basedir";
+			context.setBaseDir(SpringUtils.getFile(env, buildContextKey(contextKey, BASE_DIR_KEY)));
+			context.setOutputFile(SpringUtils.getFile(env, buildContextKey(contextKey, OUTPUT_FILE_KEY)));
 
-    protected static final String OUTPUT_FILE_KEY = ".output";
+			context.setIncludes(SpringUtils.getIncludes(env, buildContextKey(contextKey, INCLUDES_KEY)));
+			context.setExcludes(SpringUtils.getExcludes(env, buildContextKey(contextKey, EXCLUDES_KEY), DEFAULT_EXCLUDES));
 
-    protected static final String INCLUDES_KEY = ".includes";
+			context.setPrefix(SpringUtils.getProperty(env, buildContextKey(contextKey, PREFIX_KEY), DEFAULT_PREFIX));
 
-    protected static final String EXCLUDES_KEY = ".excludes";
+			contexts.add(context);
+		}
 
-    protected static final String DEFAULT_EXCLUDES = Constants.NONE;
+		MetaInfExecutable mie = new MetaInfExecutable();
+		mie.setSkip(SpringUtils.getBoolean(env, EXECUTABLE_SKIP_KEY, MetaInfExecutable.DEFAULT_SKIP));
+		mie.setContexts(contexts);
+		return mie;
+	}
 
-    protected static final String PREFIX_KEY = ".prefix";
+	private boolean getDefaultedBoolean(String contextKey, String keySuffix, boolean defaultVal) {
+		String key = buildContextKey(contextKey, keySuffix);
+		return SpringUtils.getBoolean(env, key, defaultVal);
+	}
 
-    protected static final String DEFAULT_PREFIX = MetaInfContext.DEFAULT_PREFIX;
-
-    @Autowired
-    Environment env;
-
-    @Bean
-    public Executable metaInfExecutable() {
-
-        String prefixes = SpringUtils.getProperty(env, METAINF_CONTEXTS_KEY);
-
-        List<String> contextKeys = CollectionUtils.getTrimmedListFromCSV(prefixes);
-
-        List<MetaInfContext> contexts = new ArrayList<MetaInfContext>();
-
-        for (String contextKey : contextKeys) {
-            MetaInfContext context = new MetaInfContext();
-
-            context.setAddLineCount(getDefaultedBoolean(contextKey, ADD_LINE_COUNT_KEY, DEFAULT_ADD_LINE_COUNT));
-            context.setAddPropertiesFile(getDefaultedBoolean(contextKey, ADD_PROPERTIES_FILE_KEY, DEFAULT_ADD_PROPERTIES_FILE));
-            context.setSort(getDefaultedBoolean(contextKey, SORT_KEY, DEFAULT_SORT));
-
-            context.setBaseDir(SpringUtils.getFile(env, buildContextKey(contextKey, BASE_DIR_KEY)));
-            context.setOutputFile(SpringUtils.getFile(env, buildContextKey(contextKey, OUTPUT_FILE_KEY)));
-
-            context.setIncludes(SpringUtils.getIncludes(env, buildContextKey(contextKey, INCLUDES_KEY)));
-            context.setExcludes(SpringUtils.getExcludes(env, buildContextKey(contextKey, EXCLUDES_KEY), DEFAULT_EXCLUDES));
-
-            context.setPrefix(SpringUtils.getProperty(env, buildContextKey(contextKey, PREFIX_KEY), DEFAULT_PREFIX));
-
-            contexts.add(context);
-        }
-
-        MetaInfExecutable mie = new MetaInfExecutable();
-        mie.setSkip(SpringUtils.getBoolean(env, EXECUTABLE_SKIP_KEY, MetaInfExecutable.DEFAULT_SKIP));
-        mie.setContexts(contexts);
-        return mie;
-    }
-
-    private boolean getDefaultedBoolean(String contextKey, String keySuffix, boolean defaultVal) {
-        String key = buildContextKey(contextKey, keySuffix);
-        return SpringUtils.getBoolean(env, key, defaultVal);
-    }
-
-    private String buildContextKey(String contextKey, String keySuffix) {
-        return contextKey + keySuffix;
-    }
+	private String buildContextKey(String contextKey, String keySuffix) {
+		return contextKey + keySuffix;
+	}
 
 }
