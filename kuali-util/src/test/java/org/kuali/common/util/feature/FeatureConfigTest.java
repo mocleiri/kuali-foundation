d769384964ab9a7cf429e9e4dc4f2df6963ab2a4
jcaddel <jcaddel@kuali.org>
2013-07-14 00:35:48 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22654 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/feature/FeatureConfigTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/feature/FeatureConfigTest.java
@@ -1,7 +1,6 @@
 package org.kuali.common.util.feature;
 
 import org.junit.Test;
-import org.kuali.common.util.feature.spring.FeatureConfig;
 import org.kuali.common.util.service.DefaultSpringService;
 import org.kuali.common.util.service.SpringService;
 
@@ -13,7 +12,7 @@ public class FeatureConfigTest {
 		try {
 			System.setProperty("feature.ids", "org.kuali.common:kuali-util:metainf:mpx");
 			SpringService ss = new DefaultSpringService();
-			ss.load(FeatureConfig.class);
+			ss.load(FeatureTestConfig.class);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
