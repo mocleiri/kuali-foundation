4bb85698b9181946679e71669541d6a9cad74a36
jcaddel <jcaddel@kuali.org>
2013-07-14 15:15:02 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22699 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/config/FeatureTestConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/config/FeatureTestConfig.java
@@ -15,34 +15,8 @@
  */
 package org.kuali.common.util.config;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ ConfigMetadataConfig.class })
 public class FeatureTestConfig {
-
-	@Autowired
-	ConfigMetadataConfig featureConfig;
-
-	@Bean
-	public Object whatup() {
-		ConfigMetadataService service = featureConfig.utilConfigMetadataService();
-		Map<String, ConfigMetadata> map = featureConfig.utilConfigMetadataMap();
-		System.out.println(map.size());
-		for (ConfigMetadata metadata : map.values()) {
-			List<Location> contexts = metadata.getLocations();
-			System.out.println(service.getId(metadata));
-			for (Location context : contexts) {
-				System.out.println(context.getValue());
-			}
-			System.out.println();
-		}
-		return null;
-	}
 }
