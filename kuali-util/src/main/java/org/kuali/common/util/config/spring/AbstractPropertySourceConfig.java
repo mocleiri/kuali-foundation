bc44e77cd2721d4e54492650a3a3f783fc4b1003
jcaddel <jcaddel@kuali.org>
2013-07-14 20:56:53 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22720 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/config/spring/AbstractPropertySourceConfig.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.util.config.spring;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import org.kuali.common.util.LocationUtils;
+import org.kuali.common.util.ModeUtils;
+import org.kuali.common.util.PropertyUtils;
+import org.kuali.common.util.config.Location;
+import org.kuali.common.util.property.Constants;
+import org.kuali.common.util.spring.SpringUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.PropertySource;
+import org.springframework.util.PropertyPlaceholderHelper;
+
+/**
+ * 
+ */
+@Configuration
+public abstract class AbstractPropertySourceConfig {
+
+	private static final PropertyPlaceholderHelper HELPER = Constants.DEFAULT_PROPERTY_PLACEHOLDER_HELPER;
+
+	/**
+	 * 
+	 */
+	protected List<Location> getLocations() {
+		return Collections.emptyList();
+	}
+
+	protected abstract Properties getProjectProperties();
+
+	public PropertySource<?> getPropertySource() {
+		// Do "something" to get project properties
+		Properties project = getProjectProperties();
+		// Do "something" to get the list of locations holding properties we need to load
+		List<Location> locations = getLocations();
+		// Allocate some storage
+		Properties properties = new Properties();
+		// Get system/environment properties
+		Properties global = PropertyUtils.getGlobalProperties();
+		// Cycle through our list of locations
+		for (Location location : locations) {
+			// Combine properties we've already loaded with project and global properties
+			Properties resolver = PropertyUtils.combine(properties, project, global);
+			// Use the combined properties to resolve any placeholders in the location
+			String resolvedLocation = HELPER.replacePlaceholders(location.getValue(), resolver);
+			// If the location exists, load it
+			if (LocationUtils.exists(resolvedLocation)) {
+				Properties loaded = PropertyUtils.load(resolvedLocation, location.getEncoding());
+				properties.putAll(loaded);
+			} else {
+				// Take appropriate action for missing locations (ignore, inform, warn, or error out)
+				ModeUtils.validate(location.getMissingMode(), "Non-existent location [" + resolvedLocation + "]");
+			}
+		}
+		// Override loaded properties with project properties
+		properties.putAll(project);
+		// Override everything with system/environment properties
+		properties.putAll(global);
+		// Decrypt them
+		PropertyUtils.decrypt(properties);
+		// Make sure every single placeholder is fully resolved and throw an exception if that is not the case
+		// *MUCH* better to immediately error out as opposed to allowing things to proceed only to discover we've got some random unresolved property that causes issues at runtime
+		PropertyUtils.resolve(properties);
+		// Convert the properties into a PropertySource<?> for Spring
+		return SpringUtils.getGlobalPropertySource(properties);
+	}
+
+	@Bean
+	public PropertySource<?> springPropertySource() {
+		return getPropertySource();
+	}
+
+}
