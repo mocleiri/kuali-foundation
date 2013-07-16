ebb2167af9593c5085b5f029901ed1722ba13f63
jcaddel <jcaddel@kuali.org>
2013-07-16 14:00:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22818 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/config/JdbcConfigRequest.java
@@ -0,0 +1,12 @@
+package org.kuali.common.jdbc.config;
+
+import org.kuali.common.util.ProjectConstants;
+import org.kuali.common.util.config.KualiCommonConfigRequest;
+
+public class JdbcConfigRequest extends KualiCommonConfigRequest {
+
+	public JdbcConfigRequest() {
+		super(ProjectConstants.JDBC_ARIFACT_ID);
+	}
+
+}
---
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/config/SqlConfigRequest.java
@@ -0,0 +1,12 @@
+package org.kuali.common.jdbc.config;
+
+import org.kuali.common.util.ProjectConstants;
+import org.kuali.common.util.config.KualiCommonConfigRequest;
+
+public class SqlConfigRequest extends KualiCommonConfigRequest {
+
+	public SqlConfigRequest() {
+		super(ProjectConstants.SQL_ARTIFACT_ID);
+	}
+
+}
