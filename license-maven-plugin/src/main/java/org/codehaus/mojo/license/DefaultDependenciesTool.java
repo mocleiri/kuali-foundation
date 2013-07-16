5b6e3754dcdca74f414bd8735fe78493b68c6289
jcaddel <jcaddel@kuali.org>
2013-07-16 18:45:56 -0400

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22840 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- license-maven-plugin/src/main/java/org/codehaus/mojo/license/DefaultDependenciesTool.java
+++ license-maven-plugin/src/main/java/org/codehaus/mojo/license/DefaultDependenciesTool.java
@@ -35,7 +35,7 @@ import org.codehaus.plexus.logging.Logger;
 
 /**
  * Default implementation of the {@link DependenciesTool}.
- *
+ * 
  * @author tchemit <chemit@codelutin.com>
  * @version $Id: DefaultDependenciesTool.java 14411 2011-08-10 22:09:34Z tchemit $
  * @plexus.component role="org.codehaus.mojo.license.DependenciesTool" role-hint="default"
@@ -43,215 +43,211 @@ import org.codehaus.plexus.logging.Logger;
  */
 public class DefaultDependenciesTool extends AbstractLogEnabled implements DependenciesTool {
 
-    public static final String INVALID_PATTERN_MESSAGE = "The pattern specified by expression <%s> seems to be invalid.";
+	public static final String INVALID_PATTERN_MESSAGE = "The pattern specified by expression <%s> seems to be invalid.";
 
-    /**
-     * Project builder.
-     *
-     * @plexus.requirement
-     */
-    private MavenProjectBuilder mavenProjectBuilder;
+	/**
+	 * Project builder.
+	 * 
+	 * @plexus.requirement
+	 */
+	private MavenProjectBuilder mavenProjectBuilder;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public SortedMap<String, MavenProject> loadProjectDependencies(MavenProject project,
-            MavenProjectDependenciesConfigurator configuration, ArtifactRepository localRepository,
-            List<ArtifactRepository> remoteRepositories, SortedMap<String, MavenProject> cache) {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public SortedMap<String, MavenProject> loadProjectDependencies(MavenProject project, MavenProjectDependenciesConfigurator configuration, ArtifactRepository localRepository,
+			List<ArtifactRepository> remoteRepositories, SortedMap<String, MavenProject> cache) {
 
-        boolean haveNoIncludedGroups = StringUtils.isEmpty(configuration.getIncludedGroups());
-        boolean haveNoIncludedArtifacts = StringUtils.isEmpty(configuration.getIncludedArtifacts());
+		boolean haveNoIncludedGroups = StringUtils.isEmpty(configuration.getIncludedGroups());
+		boolean haveNoIncludedArtifacts = StringUtils.isEmpty(configuration.getIncludedArtifacts());
 
-        boolean haveExcludedGroups = StringUtils.isNotEmpty(configuration.getExcludedGroups());
-        boolean haveExcludedArtifacts = StringUtils.isNotEmpty(configuration.getExcludedArtifacts());
-        boolean haveExclusions = haveExcludedGroups || haveExcludedArtifacts;
+		boolean haveExcludedGroups = StringUtils.isNotEmpty(configuration.getExcludedGroups());
+		boolean haveExcludedArtifacts = StringUtils.isNotEmpty(configuration.getExcludedArtifacts());
+		boolean haveExclusions = haveExcludedGroups || haveExcludedArtifacts;
 
-        Pattern includedGroupPattern = null;
-        Pattern includedArtifactPattern = null;
-        Pattern excludedGroupPattern = null;
-        Pattern excludedArtifactPattern = null;
+		Pattern includedGroupPattern = null;
+		Pattern includedArtifactPattern = null;
+		Pattern excludedGroupPattern = null;
+		Pattern excludedArtifactPattern = null;
 
-        if (!haveNoIncludedGroups) {
-            includedGroupPattern = Pattern.compile(configuration.getIncludedGroups());
-        }
-        if (!haveNoIncludedArtifacts) {
-            includedArtifactPattern = Pattern.compile(configuration.getIncludedArtifacts());
-        }
-        if (haveExcludedGroups) {
-            excludedGroupPattern = Pattern.compile(configuration.getExcludedGroups());
-        }
-        if (haveExcludedArtifacts) {
-            excludedArtifactPattern = Pattern.compile(configuration.getExcludedArtifacts());
-        }
+		if (!haveNoIncludedGroups) {
+			includedGroupPattern = Pattern.compile(configuration.getIncludedGroups());
+		}
+		if (!haveNoIncludedArtifacts) {
+			includedArtifactPattern = Pattern.compile(configuration.getIncludedArtifacts());
+		}
+		if (haveExcludedGroups) {
+			excludedGroupPattern = Pattern.compile(configuration.getExcludedGroups());
+		}
+		if (haveExcludedArtifacts) {
+			excludedArtifactPattern = Pattern.compile(configuration.getExcludedArtifacts());
+		}
 
-        Set<?> depArtifacts;
+		Set<?> depArtifacts;
 
-        if (configuration.isIncludeTransitiveDependencies()) {
-            // All project dependencies
-            depArtifacts = project.getArtifacts();
-        } else {
-            // Only direct project dependencies
-            depArtifacts = project.getDependencyArtifacts();
-        }
+		if (configuration.isIncludeTransitiveDependencies()) {
+			// All project dependencies
+			depArtifacts = project.getArtifacts();
+		} else {
+			// Only direct project dependencies
+			depArtifacts = project.getDependencyArtifacts();
+		}
 
-        List<String> includedScopes = configuration.getIncludedScopes();
-        List<String> excludeScopes = configuration.getExcludedScopes();
+		List<String> includedScopes = configuration.getIncludedScopes();
+		List<String> excludeScopes = configuration.getExcludedScopes();
 
-        boolean verbose = configuration.isVerbose();
+		boolean verbose = configuration.isVerbose();
 
-        SortedMap<String, MavenProject> result = new TreeMap<String, MavenProject>();
+		SortedMap<String, MavenProject> result = new TreeMap<String, MavenProject>();
 
-        for (Object o : depArtifacts) {
-            Artifact artifact = (Artifact) o;
+		for (Object o : depArtifacts) {
+			Artifact artifact = (Artifact) o;
 
-            String scope = artifact.getScope();
-            if (CollectionUtils.isNotEmpty(includedScopes) && !includedScopes.contains(scope)) {
-                // not in included scopes
-                continue;
-            }
-            if (excludeScopes.contains(scope)) {
-                // in excluded scopes
-                continue;
-            }
+			String scope = artifact.getScope();
+			if (CollectionUtils.isNotEmpty(includedScopes) && !includedScopes.contains(scope)) {
+				// not in included scopes
+				continue;
+			}
+			if (excludeScopes.contains(scope)) {
+				// in excluded scopes
+				continue;
+			}
 
-            Logger log = getLogger();
+			Logger log = getLogger();
 
-            String id = MojoHelper.getArtifactId(artifact);
+			String id = MojoHelper.getArtifactId(artifact);
 
-            if (verbose) {
-                log.info("detected artifact " + id);
-            }
+			if (verbose) {
+				log.info("detected artifact " + id);
+			}
 
-            // Check if the project should be included
-            // If there is no specified artifacts and group to include, include all
-            boolean isToInclude = haveNoIncludedArtifacts && haveNoIncludedGroups
-                    || isIncludable(artifact, includedGroupPattern, includedArtifactPattern);
+			// Check if the project should be included
+			// If there is no specified artifacts and group to include, include all
+			boolean isToInclude = haveNoIncludedArtifacts && haveNoIncludedGroups || isIncludable(artifact, includedGroupPattern, includedArtifactPattern);
 
-            // Check if the project should be excluded
-            boolean isToExclude = isToInclude && haveExclusions
-                    && isExcludable(artifact, excludedGroupPattern, excludedArtifactPattern);
+			// Check if the project should be excluded
+			boolean isToExclude = isToInclude && haveExclusions && isExcludable(artifact, excludedGroupPattern, excludedArtifactPattern);
 
-            if (!isToInclude || isToExclude) {
-                if (verbose) {
-                    log.info("skip artifact " + id);
-                }
-                continue;
-            }
+			if (!isToInclude || isToExclude) {
+				if (verbose) {
+					log.info("skip artifact " + id);
+				}
+				continue;
+			}
 
-            MavenProject depMavenProject = null;
+			MavenProject depMavenProject = null;
 
-            if (cache != null) {
+			if (cache != null) {
 
-                // try to get project from cache
-                depMavenProject = cache.get(id);
-            }
+				// try to get project from cache
+				depMavenProject = cache.get(id);
+			}
 
-            if (depMavenProject != null) {
-                if (verbose) {
-                    log.info("add dependency [" + id + "] (from cache)");
-                }
-            } else {
-                // build project
-                try {
-                    depMavenProject = mavenProjectBuilder.buildFromRepository(artifact, remoteRepositories,
-                            localRepository, true);
-                } catch (ProjectBuildingException e) {
-                    log.warn("Unable to obtain POM for artifact : " + artifact, e);
-                    continue;
-                }
+			if (depMavenProject != null) {
+				if (verbose) {
+					log.info("add dependency [" + id + "] (from cache)");
+				}
+			} else {
+				// build project
+				try {
+					depMavenProject = mavenProjectBuilder.buildFromRepository(artifact, remoteRepositories, localRepository, true);
+				} catch (ProjectBuildingException e) {
+					log.warn("Unable to obtain POM for artifact : " + artifact, e);
+					continue;
+				}
 
-                if (verbose) {
-                    log.info("add dependency [" + id + "]");
-                }
-                if (cache != null) {
-                    // store it also in cache
-                    cache.put(id, depMavenProject);
-                }
-            }
+				if (verbose) {
+					log.info("add dependency [" + id + "]");
+				}
+				if (cache != null) {
+					// store it also in cache
+					cache.put(id, depMavenProject);
+				}
+			}
 
-            // keep the project
-            result.put(id, depMavenProject);
-        }
+			// keep the project
+			result.put(id, depMavenProject);
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    protected boolean isIncludable(Artifact project, Pattern includedGroupPattern, Pattern includedArtifactPattern) {
+	protected boolean isIncludable(Artifact project, Pattern includedGroupPattern, Pattern includedArtifactPattern) {
 
-        Logger log = getLogger();
+		Logger log = getLogger();
 
-        // check if the groupId of the project should be included
-        if (includedGroupPattern != null) {
-            // we have some defined license filters
-            try {
-                Matcher matchGroupId = includedGroupPattern.matcher(project.getGroupId());
-                if (matchGroupId.find()) {
-                    if (log.isDebugEnabled()) {
-                        log.debug("Include " + project.getGroupId());
-                    }
-                    return true;
-                }
-            } catch (PatternSyntaxException e) {
-                log.warn(String.format(INVALID_PATTERN_MESSAGE, includedGroupPattern.pattern()));
-            }
-        }
+		// check if the groupId of the project should be included
+		if (includedGroupPattern != null) {
+			// we have some defined license filters
+			try {
+				Matcher matchGroupId = includedGroupPattern.matcher(project.getGroupId());
+				if (matchGroupId.find()) {
+					if (log.isDebugEnabled()) {
+						log.debug("Include " + project.getGroupId());
+					}
+					return true;
+				}
+			} catch (PatternSyntaxException e) {
+				log.warn(String.format(INVALID_PATTERN_MESSAGE, includedGroupPattern.pattern()));
+			}
+		}
 
-        // check if the artifactId of the project should be included
-        if (includedArtifactPattern != null) {
-            // we have some defined license filters
-            try {
-                Matcher matchGroupId = includedArtifactPattern.matcher(project.getArtifactId());
-                if (matchGroupId.find()) {
-                    if (log.isDebugEnabled()) {
-                        log.debug("Include " + project.getArtifactId());
-                    }
-                    return true;
-                }
-            } catch (PatternSyntaxException e) {
-                log.warn(String.format(INVALID_PATTERN_MESSAGE, includedArtifactPattern.pattern()));
-            }
-        }
+		// check if the artifactId of the project should be included
+		if (includedArtifactPattern != null) {
+			// we have some defined license filters
+			try {
+				Matcher matchGroupId = includedArtifactPattern.matcher(project.getArtifactId());
+				if (matchGroupId.find()) {
+					if (log.isDebugEnabled()) {
+						log.debug("Include " + project.getArtifactId());
+					}
+					return true;
+				}
+			} catch (PatternSyntaxException e) {
+				log.warn(String.format(INVALID_PATTERN_MESSAGE, includedArtifactPattern.pattern()));
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    protected boolean isExcludable(Artifact project, Pattern excludedGroupPattern, Pattern excludedArtifactPattern) {
+	protected boolean isExcludable(Artifact project, Pattern excludedGroupPattern, Pattern excludedArtifactPattern) {
 
-        Logger log = getLogger();
+		Logger log = getLogger();
 
-        // check if the groupId of the project should be included
-        if (excludedGroupPattern != null) {
-            // we have some defined license filters
-            try {
-                Matcher matchGroupId = excludedGroupPattern.matcher(project.getGroupId());
-                if (matchGroupId.find()) {
-                    if (log.isDebugEnabled()) {
-                        log.debug("Exclude " + project.getGroupId());
-                    }
-                    return true;
-                }
-            } catch (PatternSyntaxException e) {
-                log.warn(String.format(INVALID_PATTERN_MESSAGE, excludedGroupPattern.pattern()));
-            }
-        }
+		// check if the groupId of the project should be included
+		if (excludedGroupPattern != null) {
+			// we have some defined license filters
+			try {
+				Matcher matchGroupId = excludedGroupPattern.matcher(project.getGroupId());
+				if (matchGroupId.find()) {
+					if (log.isDebugEnabled()) {
+						log.debug("Exclude " + project.getGroupId());
+					}
+					return true;
+				}
+			} catch (PatternSyntaxException e) {
+				log.warn(String.format(INVALID_PATTERN_MESSAGE, excludedGroupPattern.pattern()));
+			}
+		}
 
-        // check if the artifactId of the project should be included
-        if (excludedArtifactPattern != null) {
-            // we have some defined license filters
-            try {
-                Matcher matchGroupId = excludedArtifactPattern.matcher(project.getArtifactId());
-                if (matchGroupId.find()) {
-                    if (log.isDebugEnabled()) {
-                        log.debug("Exclude " + project.getArtifactId());
-                    }
-                    return true;
-                }
-            } catch (PatternSyntaxException e) {
-                log.warn(String.format(INVALID_PATTERN_MESSAGE, excludedArtifactPattern.pattern()));
-            }
-        }
+		// check if the artifactId of the project should be included
+		if (excludedArtifactPattern != null) {
+			// we have some defined license filters
+			try {
+				Matcher matchGroupId = excludedArtifactPattern.matcher(project.getArtifactId());
+				if (matchGroupId.find()) {
+					if (log.isDebugEnabled()) {
+						log.debug("Exclude " + project.getArtifactId());
+					}
+					return true;
+				}
+			} catch (PatternSyntaxException e) {
+				log.warn(String.format(INVALID_PATTERN_MESSAGE, excludedArtifactPattern.pattern()));
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 }
