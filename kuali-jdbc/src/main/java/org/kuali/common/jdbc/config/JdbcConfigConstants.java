f1802f9b67a2907bd11b5a368f69d3d1ba93f896
jcaddel <jcaddel@kuali.org>
2013-07-16 19:21:27 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22846 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/config/JdbcConfigConstants.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/config/JdbcConfigConstants.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.kuali.common.util.config.ConfigRequest;
+import org.kuali.common.util.config.ConfigUtils;
 
 public abstract class JdbcConfigConstants {
 
-	public static final List<? extends ConfigRequest> JDBC_CONFIG = Arrays.asList(new SqlConfigRequest(), new JdbcConfigRequest());
+	public static final List<? extends ConfigRequest> CONFIG_REQUESTS = Arrays.asList(new SqlConfigRequest(), new JdbcConfigRequest());
+	public static final List<String> CONFIG_IDS = ConfigUtils.getConfigIds(CONFIG_REQUESTS);
 
 }
