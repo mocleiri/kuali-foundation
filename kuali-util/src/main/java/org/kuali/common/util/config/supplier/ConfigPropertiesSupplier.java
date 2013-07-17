ed98a3cc9803841a5220d1f851d2458941d6af8d
jcaddel <jcaddel@kuali.org>
2013-07-16 20:02:35 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22853 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/config/supplier/ConfigPropertiesSupplier.java
+++ kuali-util/src/main/java/org/kuali/common/util/config/supplier/ConfigPropertiesSupplier.java
@@ -3,13 +3,19 @@ package org.kuali.common.util.config.supplier;
 import java.util.List;
 import java.util.Properties;
 
+import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.PropertyUtils;
 import org.kuali.common.util.config.ConfigService;
+import org.kuali.common.util.config.DefaultConfigService;
 import org.springframework.util.Assert;
 
 public class ConfigPropertiesSupplier implements PropertiesSupplier {
 
-	ConfigService service;
+	public static final ConfigService DEFAULT_CONFIG_SERVICE = new DefaultConfigService();
+
+	ConfigService service = DEFAULT_CONFIG_SERVICE;
 	List<String> configIds;
+	List<String> locations;
 
 	@Override
 	public Properties getProperties() {
@@ -17,7 +23,17 @@ public class ConfigPropertiesSupplier implements PropertiesSupplier {
 		Assert.notNull(service, "service is null");
 		Assert.notNull(configIds, "configIds is null");
 
-		return service.getProperties(configIds);
+		Properties overrides = getOverrides(locations);
+		return service.getProperties(configIds, overrides);
+	}
+
+	protected Properties getOverrides(List<String> locations) {
+		Properties overrides = new Properties();
+		for (String location : CollectionUtils.toEmptyList(locations)) {
+			Properties loaded = PropertyUtils.load(location);
+			overrides.putAll(loaded);
+		}
+		return overrides;
 	}
 
 	public ConfigService getService() {
