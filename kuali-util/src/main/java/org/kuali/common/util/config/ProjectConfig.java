462d2a3cf4963d3ec8132dd71c015fe62e83e1ca
jcaddel <jcaddel@kuali.org>
2013-07-17 23:26:43 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22918 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/config/ProjectConfig.java
@@ -0,0 +1,13 @@
+package org.kuali.common.util.config;
+
+public interface ProjectConfig {
+
+	String getGroupId();
+
+	String getArtifactId();
+
+	String getContextId();
+
+	String getConfigId();
+
+}
