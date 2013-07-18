0340da28bf6ccde674aa92f9b8fefb135e8b17c7
jcaddel <jcaddel@kuali.org>
2013-07-18 03:03:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@22934 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/config/DefaultProjectConfigServiceTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/config/DefaultProjectConfigServiceTest.java
@@ -25,7 +25,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.kuali.common.util.ProjectUtils;
 import org.kuali.common.util.PropertyUtils;
-import org.kuali.common.util.metainf.SqlMetaInfConstants;
 
 public class DefaultProjectConfigServiceTest {
 
@@ -39,9 +38,7 @@ public class DefaultProjectConfigServiceTest {
 			Properties project = ProjectUtils.loadProject(groupId, artifactId).getProperties();
 			ConfigService service = new DefaultConfigService();
 			Properties properties1 = service.getProperties(configId, project);
-			Properties properties2 = service.getProperties(SqlMetaInfConstants.CONFIG_IDS, project);
 			PropertyUtils.info(properties1);
-			PropertyUtils.info(properties2);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
