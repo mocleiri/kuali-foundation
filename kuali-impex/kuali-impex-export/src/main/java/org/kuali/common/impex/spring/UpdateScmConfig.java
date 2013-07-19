baa006e5980ce7af0897a6608c13870c5b5cd9f6
jcaddel <jcaddel@kuali.org>
2013-07-19 02:03:43 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23024 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/UpdateScmConfig.java
+++ kuali-impex/kuali-impex-export/src/main/java/org/kuali/common/impex/spring/UpdateScmConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.execute.ExecutablesExecutable;
+import org.kuali.common.util.scm.BuildScmConfig;
 import org.kuali.common.util.scm.ScmConfig;
 import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@Import({ ScmConfig.class, ProjectStagingConfig.class })
+@Import({ ScmConfig.class, ProjectStagingConfig.class, BuildScmConfig.class })
 public class UpdateScmConfig {
 
 	private static final String SKIP_KEY = "impex.scm.skip";
@@ -25,11 +26,15 @@ public class UpdateScmConfig {
 	@Autowired
 	ProjectStagingConfig projectStagingConfig;
 
+	@Autowired
+	BuildScmConfig buildScmConfig;
+
 	@Bean
 	public Executable updateScmExecutable() {
 		boolean skip = SpringUtils.getBoolean(env, SKIP_KEY, false);
 		List<Executable> execs = new ArrayList<Executable>();
 		execs.add(projectStagingConfig.projectStagingExecutable());
+		execs.add(buildScmConfig.updateScmExecutable());
 		return new ExecutablesExecutable(execs, skip);
 	}
 
