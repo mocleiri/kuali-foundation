13f7e9c42bf43ae4ada66068ce51be7114f05768
jcaddel <jcaddel@kuali.org>
2013-07-20 16:51:29 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23107 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/profile/DatabaseConfigTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/profile/DatabaseConfigTest.java
@@ -1,19 +1,16 @@
 package org.kuali.common.util.spring.profile;
 
 import org.junit.Test;
-import org.kuali.common.util.service.DefaultSpringService;
-import org.kuali.common.util.service.SpringService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class DatabaseConfigTest {
 
 	@Test
 	public void test() {
-		try {
-			SpringService ss = new DefaultSpringService();
-			ss.load(ShowDatabaseExecutableConfig.class);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.getEnvironment().setActiveProfiles(DatabaseConstants.ORACLE);
+		ctx.register(ShowDatabaseExecutableConfig.class);
+		ctx.refresh();
 	}
 
 }
