226c760ce897c81eab229962f95ee8ac80d92012
jcaddel <jcaddel@kuali.org>
2013-07-20 16:22:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23102 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/profile/DatabaseConstants.java
@@ -0,0 +1,9 @@
+package org.kuali.common.util.spring.profile;
+
+public class DatabaseConstants {
+
+	public static final String ORACLE = "oracle";
+	public static final String MYSQL = "mysql";
+	public static final String VENDOR_BEAN_NAME = "db.vendor";
+
+}
