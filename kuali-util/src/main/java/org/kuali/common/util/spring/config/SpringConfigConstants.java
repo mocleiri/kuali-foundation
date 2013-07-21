c6cdd5d7e6b720c152f94d6ed74b0e11fb1af36c
jcaddel <jcaddel@kuali.org>
2013-07-21 19:09:43 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23222 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/config/SpringConfigConstants.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/config/SpringConfigConstants.java
@@ -2,7 +2,7 @@ package org.kuali.common.util.spring.config;
 
 public abstract class SpringConfigConstants {
 
-	public static final String PROJECT_ID_BEAN_NAME = "projectId";
+	public static final String PROJECT_ID_BEAN_NAME = "project.id";
 	public static final String DEFAULT_PROFILE_ID = "default";
 
 }
