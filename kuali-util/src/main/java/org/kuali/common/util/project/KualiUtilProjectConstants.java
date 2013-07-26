1b6e4efbfaef4466808656327005a9346bab7554
jcaddel <jcaddel@kuali.org>
2013-07-26 15:42:59 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23423 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/project/KualiUtilProjectConstants.java
+++ kuali-util/src/main/java/org/kuali/common/util/project/KualiUtilProjectConstants.java
@@ -2,6 +2,10 @@ package org.kuali.common.util.project;
 
 public abstract class KualiUtilProjectConstants {
 
-	public static final ProjectIdentifier PROJECT_IDENTIFIER = new ImmutableProjectIdentifier(KualiProjectConstants.COMMON_GROUP_ID, "kuali-util");
+	// These 2 must always exactly match what is in the Maven pom
+	private static final String GROUP_ID = KualiProjectConstants.COMMON_GROUP_ID;
+	private static final String ARTIFACT_ID = "kuali-util";
+
+	public static final ProjectIdentifier PROJECT_IDENTIFIER = new ImmutableProjectIdentifier(GROUP_ID, ARTIFACT_ID);
 
 }
