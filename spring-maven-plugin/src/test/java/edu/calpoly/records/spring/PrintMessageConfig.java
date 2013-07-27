465c43debc07154e7fe4235ac48a6772d9dc3150
jcaddel <jcaddel@kuali.org>
2013-07-27 16:28:44 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23498 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintMessageConfig.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintMessageConfig.java
@@ -3,10 +3,8 @@ package edu.calpoly.records.spring;
 import org.kuali.common.util.execute.PrintMessageExecutable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("default")
 public class PrintMessageConfig {
 
 	@Bean
