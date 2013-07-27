ca5a89c9398362bb8b07479eac6307d89a19844b
jcaddel <jcaddel@kuali.org>
2013-07-27 17:36:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23514 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/SpringMojoServiceConfig.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/SpringMojoServiceConfig.java
@@ -1,6 +1,6 @@
 package org.kuali.maven.plugins.spring.config;
 
-import org.kuali.common.util.spring.service.PropertySourceServiceConfig;
+import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.kuali.maven.plugins.spring.DefaultSpringMojoService;
 import org.kuali.maven.plugins.spring.SpringMojoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +9,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ PropertySourceServiceConfig.class })
+@Import({ SpringServiceConfig.class })
 public class SpringMojoServiceConfig {
 
 	@Autowired
-	PropertySourceServiceConfig propertySourceServiceConfig;
+	SpringServiceConfig springServiceConfig;
 
 	@Bean
 	public SpringMojoService springMojoService() {
 		DefaultSpringMojoService service = new DefaultSpringMojoService();
-		service.setPropertySourceService(propertySourceServiceConfig.propertySourceService());
+		service.setPropertySourceService(springServiceConfig.propertySourceService());
 		return service;
 	}
 }
