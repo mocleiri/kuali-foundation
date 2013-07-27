e96b71ef6d6223660844edfe51b9c60e727bb03d
jcaddel <jcaddel@kuali.org>
2013-07-27 14:48:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23492 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintMavenMessageConfig.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintMavenMessageConfig.java
@@ -1,6 +1,5 @@
 package edu.calpoly.records.spring;
 
-import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.execute.PrintMessageExecutable;
 import org.kuali.common.util.spring.config.annotation.Maven;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +9,11 @@ import org.springframework.context.annotation.Configuration;
 @Maven
 public class PrintMavenMessageConfig {
 
-	@Bean(name = "printMessageConfig")
-	public Executable printMessageExecutable() {
-		PrintMessageExecutable pme = new PrintMessageExecutable();
-		pme.setMessage("maven override message");
-		return pme;
+	@Bean
+	public PrintMessageExecutable printMessageExecutable() {
+		PrintMessageExecutable exec = new PrintMessageExecutable();
+		exec.setMessage("maven override message");
+		return exec;
 	}
 
 }
--- spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintMessageConfig.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintMessageConfig.java
@@ -1,25 +1,19 @@
 package edu.calpoly.records.spring;
 
-import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.execute.PrintMessageExecutable;
-import org.kuali.common.util.spring.SpringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.kuali.common.util.spring.config.annotation.Default;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Configuration
+@Default
 public class PrintMessageConfig {
 
-	@Autowired
-	Environment env;
-
-	@Bean(name = "printMessageConfig")
-	public Executable printMessageExecutable() {
-		String message = SpringUtils.getProperty(env, "print.message", "NONE");
-		PrintMessageExecutable pme = new PrintMessageExecutable();
-		pme.setMessage(message);
-		return pme;
+	@Bean
+	public PrintMessageExecutable printMessageExecutable() {
+		PrintMessageExecutable exec = new PrintMessageExecutable();
+		exec.setMessage("hello world");
+		return exec;
 	}
 
 }
