33ff8fb9d2eefe45c63951ceac84818309e3348d
jcaddel <jcaddel@kuali.org>
2013-07-27 14:43:42 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23491 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/BeanConfig.java
@@ -0,0 +1,25 @@
+package org.kuali.common.util;
+
+@Deprecated
+public class BeanConfig {
+
+	String name;
+	Object object;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Object getObject() {
+		return object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+}
