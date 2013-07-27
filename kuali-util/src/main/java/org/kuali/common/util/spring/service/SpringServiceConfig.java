e37ca749544ed1361675952f11babf661cdd813a
jcaddel <jcaddel@kuali.org>
2013-07-27 14:39:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23489 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/service/SpringServiceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/service/SpringServiceConfig.java
@@ -1,15 +1,10 @@
 package org.kuali.common.util.spring.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Configuration
 public class SpringServiceConfig {
-
-	@Autowired
-	Environment env;
 
 	@Bean
 	public SpringService springService() {
