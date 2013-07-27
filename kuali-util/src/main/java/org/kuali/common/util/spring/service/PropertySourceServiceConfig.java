a5b03f7a8e4078f42d112fce376c3b81dc6a1c01
jcaddel <jcaddel@kuali.org>
2013-07-27 14:40:14 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23490 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/service/PropertySourceServiceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/service/PropertySourceServiceConfig.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @Import({ SpringServiceConfig.class })
 public class PropertySourceServiceConfig {
 
 	@Autowired
-	Environment env;
-
 	SpringServiceConfig springServiceConfig;
 
 	@Bean
