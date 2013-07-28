618635b35e16a0136cb7575f8e82325f6d36dee3
jcaddel <jcaddel@kuali.org>
2013-07-28 10:21:02 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23558 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintRuntimeMessage.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintRuntimeMessage.java
@@ -1,12 +1,12 @@
 package edu.calpoly.records.spring;
 
 import org.kuali.common.util.execute.PrintMessageExecutable;
-import org.kuali.common.util.spring.config.annotation.Runtime;
+import org.kuali.common.util.spring.config.annotation.NotMaven;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Runtime
+@NotMaven
 public class PrintRuntimeMessage {
 
 	@Bean
