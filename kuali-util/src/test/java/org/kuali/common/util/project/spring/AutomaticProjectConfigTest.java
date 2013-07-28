dc2d5908a90e7efc9e9fe19ae642b6f5efc2fbbe
jcaddel <jcaddel@kuali.org>
2013-07-28 10:41:13 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23564 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/project/spring/AutomaticProjectConfigTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/project/spring/AutomaticProjectConfigTest.java
@@ -1,5 +1,22 @@
 package org.kuali.common.util.project.spring;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kuali.common.util.project.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = KualiUtilProjectConfig.class)
 public class AutomaticProjectConfigTest {
+
+	@Autowired
+	private Project project;
+
+	@Test
+	public void test() {
+		System.out.println("hello world");
+	}
 
 }
