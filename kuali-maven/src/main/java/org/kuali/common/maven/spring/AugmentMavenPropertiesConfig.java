2e5413531b4f0bc161553e95961ada23c05ea9a9
jcaddel <jcaddel@kuali.org>
2013-07-28 10:03:59 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23554 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-maven/src/main/java/org/kuali/common/maven/spring/AugmentMavenPropertiesConfig.java
+++ kuali-maven/src/main/java/org/kuali/common/maven/spring/AugmentMavenPropertiesConfig.java
@@ -17,11 +17,9 @@ package org.kuali.common.maven.spring;
 
 import org.apache.maven.project.MavenProject;
 import org.kuali.common.util.execute.Executable;
-import org.kuali.common.util.maven.MavenConstants;
-import org.kuali.common.util.project.ProjectServiceConfig;
+import org.kuali.common.util.project.spring.ProjectServiceConfig;
 import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -35,11 +33,10 @@ public class AugmentMavenPropertiesConfig {
 	Environment env;
 
 	@Autowired
-	ProjectServiceConfig projectServiceConfig;
+	MavenProject mavenProject;
 
 	@Autowired
-	@Qualifier(MavenConstants.PROJECT_BEAN_NAME)
-	MavenProject mavenProject;
+	ProjectServiceConfig projectServiceConfig;
 
 	@Bean(initMethod = "execute")
 	public Executable augmentMavenProperties() {
