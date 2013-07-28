c9ab534255f0262599f731b0af8da1e197d153c3
jcaddel <jcaddel@kuali.org>
2013-07-28 10:25:10 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23562 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintMavenMessageConfig.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintMavenMessageConfig.java
@@ -1,6 +1,7 @@
 package edu.calpoly.records.spring;
 
 import org.kuali.common.util.execute.PrintMessageExecutable;
+import org.kuali.common.util.maven.MavenConstants;
 import org.kuali.common.util.spring.config.annotation.Maven;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,7 @@ public class PrintMavenMessageConfig {
 	@Bean
 	public PrintMessageExecutable printMessageExecutable() {
 		PrintMessageExecutable exec = new PrintMessageExecutable();
-		exec.setMessage("maven");
+		exec.setMessage(MavenConstants.SPRING_PROFILE_NAME);
 		return exec;
 	}
 
--- spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintNotMavenMessage.java
+++ spring-maven-plugin/src/test/java/edu/calpoly/records/spring/PrintNotMavenMessage.java
@@ -1,6 +1,7 @@
 package edu.calpoly.records.spring;
 
 import org.kuali.common.util.execute.PrintMessageExecutable;
+import org.kuali.common.util.maven.MavenConstants;
 import org.kuali.common.util.spring.config.annotation.NotMaven;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,7 @@ public class PrintNotMavenMessage {
 	@Bean
 	public PrintMessageExecutable printMessageExecutable() {
 		PrintMessageExecutable exec = new PrintMessageExecutable();
-		exec.setMessage("!maven");
+		exec.setMessage(MavenConstants.NEGATED_SPRING_PROFILE_NAME);
 		return exec;
 	}
 
