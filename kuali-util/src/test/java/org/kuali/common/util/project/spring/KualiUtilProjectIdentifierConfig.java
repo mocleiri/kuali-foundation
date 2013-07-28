f8d9a94e00bd17f9edbc6d5f79fe7765f4f98fe6
jcaddel <jcaddel@kuali.org>
2013-07-28 10:45:24 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23565 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/project/spring/KualiUtilProjectIdentifierConfig.java
@@ -0,0 +1,25 @@
+package org.kuali.common.util.project.spring;
+
+import org.kuali.common.util.project.KualiUtilProjectConstants;
+import org.kuali.common.util.project.ProjectIdentifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KualiUtilProjectIdentifierConfig implements ProjectIdentifierConfig {
+
+	protected static final ProjectIdentifier IDENTIFIER = KualiUtilProjectConstants.PROJECT_IDENTIFIER;
+
+	@Override
+	@Bean
+	public String projectGroupId() {
+		return IDENTIFIER.getGroupId();
+	}
+
+	@Override
+	@Bean
+	public String projectArtifactId() {
+		return IDENTIFIER.getGroupId();
+	}
+
+}
