ff6266af77fb5ebe2185e268e629ea3191df0174
jcaddel <jcaddel@kuali.org>
2013-07-28 11:11:20 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23567 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/project/spring/AutomaticProjectConfigTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/project/spring/AutomaticProjectConfigTest.java
@@ -1,8 +1,15 @@
 package org.kuali.common.util.project.spring;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kuali.common.util.PropertyUtils;
+import org.kuali.common.util.project.KualiUtilProjectConstants;
 import org.kuali.common.util.project.Project;
+import org.kuali.common.util.project.ProjectIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,12 +18,20 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(classes = KualiUtilProjectConfig.class)
 public class AutomaticProjectConfigTest {
 
+	private static final Logger logger = LoggerFactory.getLogger(AutomaticProjectConfigTest.class);
+
+	protected static final ProjectIdentifier ID = KualiUtilProjectConstants.PROJECT_IDENTIFIER;
+
 	@Autowired
 	private Project project;
 
 	@Test
 	public void test() {
-		System.out.println("hello world");
+		logger.info("Constants: [" + ID.getGroupId() + ":" + ID.getArtifactId() + "]");
+		logger.info("   Loaded: [" + project.getGroupId() + ":" + project.getArtifactId() + "]");
+		Assert.assertEquals(ID.getGroupId(), project.getGroupId());
+		Assert.assertEquals(ID.getArtifactId(), project.getArtifactId());
+		PropertyUtils.info(project.getProperties());
 	}
 
 }
