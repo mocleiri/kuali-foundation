6524fd301d04dd277d131ec66eb8ff66b6c0c873
jcaddel <jcaddel@kuali.org>
2013-07-29 15:05:28 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23652 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/Log4JContext.java
@@ -0,0 +1,11 @@
+package org.kuali.common.util.log4j;
+
+import java.util.Map;
+
+import org.apache.log4j.Appender;
+
+public class Log4JContext {
+
+	Map<String, ? extends Appender> appenders;
+
+}
