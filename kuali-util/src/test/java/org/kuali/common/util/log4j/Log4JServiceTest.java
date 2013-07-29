30bb3878a62e5325aa838090742990e79c4c6abc
jcaddel <jcaddel@kuali.org>
2013-07-29 09:58:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23647 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/log4j/Log4JServiceTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/log4j/Log4JServiceTest.java
@@ -1,13 +1,15 @@
 package org.kuali.common.util.log4j;
 
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kuali.common.util.log4j.spring.Log4JCommonConfig;
 import org.kuali.common.util.log4j.spring.Log4JServiceConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,7 +18,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(classes = { Log4JServiceConfig.class, Log4JCommonConfig.class })
 public class Log4JServiceTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(Log4JServiceTest.class);
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Log4JServiceTest.class);
 
 	@Autowired
 	Log4JServiceConfig log4JServiceConfig;
@@ -26,10 +28,30 @@ public class Log4JServiceTest {
 
 	@Test
 	public void test() {
-		logger.info("before");
-		Log4JService service = log4JServiceConfig.log4jService();
-		List<LoggerContext> contexts = log4JCommonConfig.log4JDefault();
-		service.configure(contexts);
-		logger.info("after");
+		try {
+			logger.info("before");
+			Log4JService service = log4JServiceConfig.log4jService();
+			System.out.println(getLoggers().size());
+			service.reset();
+			System.out.println(getLoggers().size());
+			// List<LoggerContext> contexts = log4JCommonConfig.log4JMaven();
+			// service.configure(contexts);
+			logger.info("after");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected List<Logger> getLoggers() {
+		return Collections.<Logger> list(LogManager.getCurrentLoggers());
+	}
+
+	protected void showLoggers() {
+		Enumeration<?> e = LogManager.getCurrentLoggers();
+		while (e.hasMoreElements()) {
+			Logger logger = (Logger) e.nextElement();
+			System.out.println(logger.getName());
+		}
 	}
 }
