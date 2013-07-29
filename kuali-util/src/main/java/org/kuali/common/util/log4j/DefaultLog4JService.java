31df3182f605d5c4440096b2e66613c6d067f521
jcaddel <jcaddel@kuali.org>
2013-07-29 19:34:13 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23694 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log4j/DefaultLog4JService.java
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/DefaultLog4JService.java
@@ -1,20 +1,34 @@
 package org.kuali.common.util.log4j;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.kuali.common.util.Assert;
+import org.kuali.common.util.LocationUtils;
+import org.kuali.common.util.PropertyUtils;
 import org.kuali.common.util.log4j.model.Log4JContext;
+import org.kuali.common.util.nullify.Nullifier;
+import org.kuali.common.util.xml.XmlService;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 import com.sun.org.apache.xerces.internal.impl.xs.opti.DefaultDocument;
 
 public class DefaultLog4JService implements Log4JService {
 
-	public static final String RESET_KEY = "log4.reset";
-	public static final String THRESHOLD_KEY = "log4.threshold";
+	public static final String ENCODING = "UTF-8";
+
+	XmlService xmlService;
 
 	@Override
 	public void reset() {
@@ -23,7 +37,14 @@ public class DefaultLog4JService implements Log4JService {
 
 	@Override
 	public void configure(String location) {
-		// Do something here
+		Assert.isTrue(LocationUtils.exists(location), "[" + location + "] does not exist");
+		if (StringUtils.endsWithIgnoreCase(location, ".properties")) {
+			configure(PropertyUtils.load(location, ENCODING));
+		} else if (StringUtils.endsWithIgnoreCase(location, ".xml")) {
+			DOMConfigurator.configure(location);
+		} else {
+			throw new IllegalArgumentException("Only .properties and .xml files are supported");
+		}
 	}
 
 	@Override
@@ -33,7 +54,32 @@ public class DefaultLog4JService implements Log4JService {
 
 	@Override
 	public void configure(Log4JContext context) {
-		// Do something smart
+		String xml = getXml(context);
+		Document document = getDocument(xml);
+		DOMConfigurator.configure(document.getDocumentElement());
+	}
+
+	protected Document getDocument(String xml) {
+		try {
+			ByteArrayInputStream in = new ByteArrayInputStream(xml.getBytes(ENCODING));
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder parser = dbf.newDocumentBuilder();
+			return parser.parse(in);
+		} catch (IOException e) {
+			throw new IllegalStateException("Unexpected IO error", e);
+		} catch (SAXException e) {
+			throw new IllegalStateException("Unexpected SAX error", e);
+		} catch (ParserConfigurationException e) {
+			throw new IllegalStateException("Unexpected parser config error", e);
+		}
+	}
+
+	protected String getXml(Log4JContext context) {
+		Log4JContext clone = new Log4JContext(context);
+		Nullifier nullifier = new Log4JContextNullifier(clone);
+		nullifier.nullify();
+		Assert.notNull(xmlService, "xmlService is null");
+		return xmlService.toString(clone, ENCODING);
 	}
 
 	@Override
@@ -44,6 +90,14 @@ public class DefaultLog4JService implements Log4JService {
 	protected Element getElement(Log4JContext context) {
 		Document document = new DefaultDocument();
 		return document.getDocumentElement();
+	}
+
+	public XmlService getXmlService() {
+		return xmlService;
+	}
+
+	public void setXmlService(XmlService xmlService) {
+		this.xmlService = xmlService;
 	}
 
 }
