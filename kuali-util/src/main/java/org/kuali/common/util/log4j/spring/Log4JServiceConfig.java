bb91eeddfb33c4f8ad91e9db2c93050c94002857
jcaddel <jcaddel@kuali.org>
2013-07-29 19:35:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23695 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log4j/spring/Log4JServiceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/spring/Log4JServiceConfig.java
@@ -2,15 +2,24 @@ package org.kuali.common.util.log4j.spring;
 
 import org.kuali.common.util.log4j.DefaultLog4JService;
 import org.kuali.common.util.log4j.Log4JService;
+import org.kuali.common.util.xml.spring.XmlServiceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({ XmlServiceConfig.class })
 public class Log4JServiceConfig {
+
+	@Autowired
+	XmlServiceConfig xmlServiceConfig;
 
 	@Bean
 	public Log4JService log4jService() {
-		return new DefaultLog4JService();
+		DefaultLog4JService service = new DefaultLog4JService();
+		service.setXmlService(xmlServiceConfig.xmlService());
+		return service;
 	}
 
 }
