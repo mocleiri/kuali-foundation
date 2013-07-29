f14e8541a66fc3f177db24a9ffd3f7f0457b452c
jcaddel <jcaddel@kuali.org>
2013-07-28 20:06:41 -0400

    updates
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23609 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/project/spring/AutowiredProjectConfigTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/project/spring/AutowiredProjectConfigTest.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2010-2013 The Kuali Foundation
- *
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.opensource.org/licenses/ecl2.php
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.kuali.common.util.project.spring;
 
 import junit.framework.Assert;
@@ -30,9 +15,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = KualiUtilProjectConfig.class)
-public class AutomaticProjectConfigTest {
+public class AutowiredProjectConfigTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(AutomaticProjectConfigTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(AutowiredProjectConfigTest.class);
 
 	protected static final ProjectIdentifier ID = KualiUtilProjectConstants.PROJECT_IDENTIFIER;
 
