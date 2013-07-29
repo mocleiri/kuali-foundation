cb0eedc177612e048003df3250561c4dccabc647
jcaddel <jcaddel@kuali.org>
2013-07-29 16:08:55 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23669 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/log4j/Log4JTestConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/log4j/Log4JTestConfig.java
@@ -35,7 +35,7 @@ public class Log4JTestConfig {
 
 	@Bean
 	public Executable log4jResetExecutable() {
-		Log4JConfigurationExecutable exec = new Log4JConfigurationExecutable();
+		Log4JConfigExecutable exec = new Log4JConfigExecutable();
 		exec.setService(log4JServiceConfig.log4jService());
 		return exec;
 	}
