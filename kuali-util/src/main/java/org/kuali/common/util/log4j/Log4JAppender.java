85095b9420b8f8c296674898ffa2aa8809908b91
jcaddel <jcaddel@kuali.org>
2013-07-29 15:14:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23654 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log4j/Log4JAppender.java
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/Log4JAppender.java
@@ -9,4 +9,36 @@ public class Log4JAppender {
 	Log4JLayout layout;
 	Map<String, String> params;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Class<?> getJavaClass() {
+		return javaClass;
+	}
+
+	public void setJavaClass(Class<?> javaClass) {
+		this.javaClass = javaClass;
+	}
+
+	public Log4JLayout getLayout() {
+		return layout;
+	}
+
+	public void setLayout(Log4JLayout layout) {
+		this.layout = layout;
+	}
+
+	public Map<String, String> getParams() {
+		return params;
+	}
+
+	public void setParams(Map<String, String> params) {
+		this.params = params;
+	}
+
 }
