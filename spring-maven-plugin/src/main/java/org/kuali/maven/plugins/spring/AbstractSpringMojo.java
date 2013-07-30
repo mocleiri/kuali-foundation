fc2f01d3a5ef75be83741a246956c513f221cc07
jcaddel <jcaddel@kuali.org>
2013-07-29 20:32:59 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23704 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/AbstractSpringMojo.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/AbstractSpringMojo.java
@@ -75,7 +75,7 @@ public abstract class AbstractSpringMojo extends AbstractMojo {
 	String springService = MavenConstants.DEFAULT_SPRING_SERVICE.getName();
 
 	/**
-	 * The annotated Spring configuration class used to execute this mojo. This defaults to <code>org.kuali.maven.plugins.spring.config.MojoExecutableConfig</code>
+	 * The Spring config class execution is delegated to. This defaults to <code>org.kuali.maven.plugins.spring.config.MojoExecutableConfig</code>
 	 */
 	@Parameter(property = "spring.mojoExecutableConfig")
 	String mojoExecutableConfig = MojoExecutableConfig.class.getName();
