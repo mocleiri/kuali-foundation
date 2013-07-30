c7dd3913774b59f7a41a585f7fdab061a7f0ecb5
jcaddel <jcaddel@kuali.org>
2013-07-30 11:33:07 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23738 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log4j/model/LevelValue.java
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/model/LevelValue.java
@@ -3,8 +3,12 @@ package org.kuali.common.util.log4j.model;
 public enum LevelValue {
 
 	ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF, NULL;
-	// The string "null" (lowercase) is actually a supported value inside log4j.xml
-	// However, the log4j parser can't deal with uppercase "NULL". (It issues an ominous sounding WARN message)
-	// It quite happily parses all of the other uppercase values
+	// Log4j only supports "null" (lowercase) as a value inside log4j.xml
+	// "NULL" (uppercase) is not supported and causes log4j to emit a WARN level logging message as an unknown level.
+	// A proper solution here would be to get JAXB to translate "NULL" to "null" and vice versa when reading/writing XML.
+	// What happens at the moment, is we detect values set to "NULL" and set them to null on the way out.
+	// This causes JAXB to omit them when creating XML from an object.
+	// When creating an object from the XML "NULL" is the default value when nothing is present in the XML.
+	// This is a tad bit crazy and quite brittle...
 
 }
