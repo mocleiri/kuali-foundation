9a74f5ec044a24b79ac1469968ad555daee07438
jcaddel <jcaddel@kuali.org>
2013-07-30 13:32:03 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23748 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log4j/model/Value.java
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/model/Value.java
@@ -3,7 +3,7 @@ package org.kuali.common.util.log4j.model;
 public enum Value {
 
 	ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF, NULL;
-	// Log4j only supports "null" (lowercase) as a value inside log4j.xml
+	// Log4j only supports "null" (lowercase) as a text value inside log4j.xml
 	// "NULL" (uppercase) is not supported and causes log4j to emit a WARN level logging message as an unknown level.
 	// The best solution would be to get log4j to recognize "NULL" as a synonym for "null"
 	// Failing that, another solution would be to get JAXB to translate "NULL" to "null" and vice versa when writing/reading XML.
