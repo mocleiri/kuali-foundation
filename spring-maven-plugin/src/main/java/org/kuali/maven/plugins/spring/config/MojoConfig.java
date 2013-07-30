f65e7d1d7640c8f38e6cef0a05be71ac8d09ccb3
jcaddel <jcaddel@kuali.org>
2013-07-29 20:07:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23699 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/MojoConfig.java
+++ spring-maven-plugin/src/main/java/org/kuali/maven/plugins/spring/config/MojoConfig.java
@@ -1,6 +1,9 @@
 package org.kuali.maven.plugins.spring.config;
 
 import org.kuali.common.util.execute.Executable;
+import org.kuali.common.util.log4j.Log4JExecutable;
+import org.kuali.common.util.log4j.spring.Log4JCommonConfig;
+import org.kuali.common.util.log4j.spring.Log4JServiceConfig;
 import org.kuali.maven.plugins.spring.AbstractSpringMojo;
 import org.kuali.maven.plugins.spring.MavenConstants;
 import org.kuali.maven.plugins.spring.MojoExecutable;
@@ -11,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ SpringMojoServiceConfig.class })
+@Import({ SpringMojoServiceConfig.class, Log4JServiceConfig.class, Log4JCommonConfig.class })
 public class MojoConfig {
 
 	// The mojo gets wired in by registering it as a bean in the parent context
@@ -23,11 +26,25 @@ public class MojoConfig {
 	@Autowired
 	SpringMojoServiceConfig springMojoServiceConfig;
 
+	@Autowired
+	Log4JServiceConfig log4JServiceConfig;
+
+	@Autowired
+	Log4JCommonConfig log4JCommonConfig;
+
 	@Bean
 	public Executable mojoExecutable() {
 		MojoExecutable executable = new MojoExecutable();
 		executable.setService(springMojoServiceConfig.springMojoService());
 		executable.setMojo(mojo);
 		return executable;
+	}
+
+	@Bean
+	public Executable log4JExecutable() {
+		Log4JExecutable exec = new Log4JExecutable();
+		exec.setService(log4JServiceConfig.log4jService());
+		exec.setContext(log4JCommonConfig.log4JContextMaven());
+		return exec;
 	}
 }
