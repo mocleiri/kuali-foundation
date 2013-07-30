35db65aca0103d607ff80d92497c9b97d6d2ad1f
jcaddel <jcaddel@kuali.org>
2013-07-30 11:41:15 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23739 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log4j/model/LevelValue.java
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/model/LevelValue.java
@@ -5,10 +5,12 @@ public enum LevelValue {
 	ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF, NULL;
 	// Log4j only supports "null" (lowercase) as a value inside log4j.xml
 	// "NULL" (uppercase) is not supported and causes log4j to emit a WARN level logging message as an unknown level.
-	// A proper solution here would be to get JAXB to translate "NULL" to "null" and vice versa when reading/writing XML.
-	// What happens at the moment, is we detect values set to "NULL" and set them to null on the way out.
-	// This causes JAXB to omit them when creating XML from an object.
-	// When creating an object from the XML "NULL" is the default value when nothing is present in the XML.
-	// This is a tad bit crazy and quite brittle...
+	// The best solution would be to get log4j to recognize "NULL" as a synonym for "null"
+	// Failing that, another solution would be to get JAXB to translate "NULL" to "null" and vice versa when writing/reading XML.
+	// What happens at the moment, is we detect "NULL" and set it to null when creating XML from an object.
+	// This causes JAXB to omit the "value" attribute entirely from the XML.
+	// When creating an object from the XML "NULL" is the default value if the "value" attribute is not present.
+	// Thus we have a method that works in both directions for dealing with "NULL" vs "null"
+	// Granted, this is a tad bit crazy and quite brittle...
 
 }
