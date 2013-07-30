1ca729150370db6611e2b4a71df4c92da9997e82
jcaddel <jcaddel@kuali.org>
2013-07-30 16:18:50 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23754 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log4j/model/Value.java
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/model/Value.java
@@ -6,11 +6,11 @@ public enum Value {
 	// Log4j only supports "null" (lowercase) as a text value inside log4j.xml
 	// "NULL" (uppercase) is not supported and causes log4j to emit a WARN level logging message as an unknown level.
 	// The best solution would be to get log4j to recognize "NULL" as a synonym for "null"
-	// Failing that, another solution would be to get JAXB to translate "NULL" to "null" and vice versa when writing/reading XML.
-	// What happens at the moment, is we detect "NULL" and set it to null when creating XML from an object.
-	// This causes JAXB to omit the "value" attribute entirely from the XML.
-	// When creating an object from the XML "NULL" is the default value if the "value" attribute is not present.
+	// Failing that, another solution would be to get JAXB to translate "NULL" to "null" and vice versa when writing/reading xml.
+	// What happens at the moment, is we detect "NULL" and set it to null when creating xml from an object.
+	// This causes JAXB to omit the "value" attribute entirely from the xml.
+	// When going the other way (ie creating an object from xml) "NULL" is the default value if the "value" attribute is not present.
 	// Thus we have a method that works in both directions for dealing with "NULL" vs "null"
-	// Granted, this is a tad bit crazy and quite brittle...
+	// Granted, this is a tad bit crazy and supremely brittle...
 
 }
