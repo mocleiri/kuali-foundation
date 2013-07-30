beb464c97b328fbb516e464c96d516d17193d6ad
jcaddel <jcaddel@kuali.org>
2013-07-30 09:19:43 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23721 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log4j/model/LevelValue.java
+++ kuali-util/src/main/java/org/kuali/common/util/log4j/model/LevelValue.java
@@ -3,9 +3,8 @@ package org.kuali.common.util.log4j.model;
 public enum LevelValue {
 
 	ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF; // NULL
-	// The string "null" (lowercase) is actually a supported value inside log4j
-	// However, the log4j parser can't deal with uppercase "NULL".
-	// Log4J issues a WARN message if you set value = "NULL"
-	// The log4j behavior if value == null, is the same as if you set it to "null"
+	// The string "null" (lowercase) is actually a supported value inside log4j.xml
+	// However, the log4j parser can't deal with uppercase "NULL". (It issues an ominous sounding WARN message)
+	// It quite happily parses all of the other uppercase values
 
 }
