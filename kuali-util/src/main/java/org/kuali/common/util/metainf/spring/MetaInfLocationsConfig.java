2d22e5adb5eb82f45d07cd1341a2d969b9389ff5
jcaddel <jcaddel@kuali.org>
2013-08-01 00:22:25 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23825 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MetaInfLocationsConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MetaInfLocationsConfig.java
@@ -1,22 +1,31 @@
 package org.kuali.common.util.metainf.spring;
 
+import org.kuali.common.util.project.KualiUtilProjectConstants;
 import org.kuali.common.util.project.Project;
+import org.kuali.common.util.project.ProjectService;
 import org.kuali.common.util.project.ProjectUtils;
-import org.kuali.common.util.project.spring.AutowiredProjectConfig;
+import org.kuali.common.util.project.spring.ProjectServiceConfig;
 import org.kuali.common.util.properties.ImmutableLocation;
 import org.kuali.common.util.properties.Location;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ AutowiredProjectConfig.class })
+@Import({ ProjectServiceConfig.class })
 public class MetaInfLocationsConfig {
 
 	protected static String METAINF = "metainf";
 
 	@Autowired
-	Project project;
+	ProjectServiceConfig projectServiceConfig;
+
+	@Bean
+	public Project kualiUtilProject() {
+		ProjectService service = projectServiceConfig.projectService();
+		return service.getProject(KualiUtilProjectConstants.PROJECT_IDENTIFIER);
+	}
 
 	public Location getMetaInfCommon() {
 		return getLocation("/common.properties");
@@ -27,8 +36,8 @@ public class MetaInfLocationsConfig {
 	}
 
 	public Location getLocation(String suffix) {
-		String encoding = ProjectUtils.getEncoding(project);
-		String classpathPrefix = ProjectUtils.getClasspathPrefix(project.getGroupId(), project.getArtifactId());
+		String encoding = ProjectUtils.getEncoding(kualiUtilProject());
+		String classpathPrefix = ProjectUtils.getClasspathPrefix(kualiUtilProject().getGroupId(), kualiUtilProject().getArtifactId());
 		String prefix = classpathPrefix + "/" + METAINF;
 		return new ImmutableLocation(prefix + suffix, encoding);
 	}
