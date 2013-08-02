7aa717788a7de6a954831af3b96eb6af46235cae
jcaddel <jcaddel@kuali.org>
2013-08-02 06:36:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23887 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/properties/spring/PropertiesServiceConfig.java
@@ -0,0 +1,9 @@
+package org.kuali.common.util.properties.spring;
+
+import org.kuali.common.util.properties.PropertiesService;
+
+public interface PropertiesServiceConfig {
+
+	public PropertiesService propertiesService();
+
+}
---
+++ kuali-util/src/main/java/org/kuali/common/util/properties/spring/PropertyLocationsConfig.java
@@ -0,0 +1,11 @@
+package org.kuali.common.util.properties.spring;
+
+import java.util.List;
+
+import org.kuali.common.util.properties.Location;
+
+public interface PropertyLocationsConfig {
+
+	List<Location> propertyLocations();
+
+}
