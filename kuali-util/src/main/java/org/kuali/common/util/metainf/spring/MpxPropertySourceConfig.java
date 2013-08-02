c77c037256c46822aef7e4cfafee241d13ae8ac1
jcaddel <jcaddel@kuali.org>
2013-08-02 07:53:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23894 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MpxPropertySourceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MpxPropertySourceConfig.java
@@ -1,35 +1,12 @@
 package org.kuali.common.util.metainf.spring;
 
-import java.util.List;
-import java.util.Properties;
-
-import org.kuali.common.util.properties.Location;
-import org.kuali.common.util.properties.PropertiesService;
+import org.kuali.common.util.properties.spring.LocationPropertySourceConfig;
 import org.kuali.common.util.properties.spring.ProjectPropertiesServiceConfig;
-import org.kuali.common.util.spring.service.PropertySourceConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.env.PropertySource;
 
 @Configuration
 @Import({ MpxLocationsConfig.class, ProjectPropertiesServiceConfig.class })
-public class MpxPropertySourceConfig implements PropertySourceConfig {
+public class MpxPropertySourceConfig extends LocationPropertySourceConfig {
 
-	@Autowired
-	MpxLocationsConfig mpxLocationsConfig;
-
-	@Autowired
-	ProjectPropertiesServiceConfig projectPropertiesServiceConfig;
-
-	@Override
-	@Bean
-	public PropertySource<?> propertySource() {
-		List<Location> locations = mpxLocationsConfig.metaInfMpxBuildLocations();
-		PropertiesService service = projectPropertiesServiceConfig.propertiesService();
-		Properties properties = service.getProperties(locations);
-		return new PropertiesPropertySource("propertiesPropertySource", properties);
-	}
 }
