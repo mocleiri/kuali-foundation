1544f68a65e5caaea158b52d735d1e68b7478b83
jcaddel <jcaddel@kuali.org>
2013-08-02 19:06:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23947 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MpxBuildConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MpxBuildConfig.java
@@ -13,30 +13,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ MpxCommonConfig.class })
+@Import({ MetaInfCommonConfig.class })
 public class MpxBuildConfig implements MetaInfContextsConfig {
 
 	public static final List<String> RECURSIVE_MPX_INCLUDES = Arrays.asList("**/*.mpx");
 
 	@Autowired
-	MpxCommonConfig mpxCommonConfig;
+	MetaInfCommonConfig metaInfCommonConfig;
 
 	@Autowired
 	Project project;
-
-	protected File getResourceBase() {
-		File outputDir = ProjectUtils.getBuildOutputDirectory(project);
-		String subDirectory = mpxCommonConfig.getResourcePrefix();
-		return new File(outputDir, subDirectory);
-	}
 
 	@Override
 	@Bean
 	public List<MetaInfContext> metaInfContexts() {
 		MetaInfContext context = new MetaInfContext();
-		context.setOutputFile(new File(getResourceBase() + MpxCommonConfig.DATA_FILENAME));
+		context.setOutputFile(new File(getResourceBase() + MetaInfCommonConfig.DATA_FILENAME));
 		context.setIncludes(RECURSIVE_MPX_INCLUDES);
 		return Arrays.asList(context);
+	}
+
+	protected File getResourceBase() {
+		File outputDir = ProjectUtils.getBuildOutputDirectory(project);
+		String subDirectory = metaInfCommonConfig.getResourcePrefix();
+		return new File(outputDir, subDirectory);
+	}
+
+	protected File getOutputFile() {
+		File dir = getResourceBase();
+		return new File(dir, MetaInfCommonConfig.DATA_FILENAME);
 	}
 
 }
