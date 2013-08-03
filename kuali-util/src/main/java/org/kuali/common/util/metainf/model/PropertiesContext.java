8af8a22d80f2cbb0abfa9116cd5df2fd9f3e7630
jcaddel <jcaddel@kuali.org>
2013-08-03 12:49:10 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@23976 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/model/PropertiesContext.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/model/PropertiesContext.java
@@ -35,32 +35,4 @@ public final class PropertiesContext {
 		return includeLineCounts;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (includeFileSizes ? 1231 : 1237);
-		result = prime * result + (includeLineCounts ? 1231 : 1237);
-		result = prime * result + (includePropertiesFile ? 1231 : 1237);
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PropertiesContext other = (PropertiesContext) obj;
-		if (includeFileSizes != other.includeFileSizes)
-			return false;
-		if (includeLineCounts != other.includeLineCounts)
-			return false;
-		if (includePropertiesFile != other.includePropertiesFile)
-			return false;
-		return true;
-	}
-
 }
