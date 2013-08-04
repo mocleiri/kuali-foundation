8a074c4e5e38965e51055b0dd03bd2eb447b99a1
jcaddel <jcaddel@kuali.org>
2013-08-04 09:30:43 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24026 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/project/FeatureIdentifier.java
+++ kuali-util/src/main/java/org/kuali/common/util/project/FeatureIdentifier.java
@@ -4,23 +4,67 @@ import org.kuali.common.util.Assert;
 
 public final class FeatureIdentifier {
 
-	private final ProjectIdentifier projectId;
+	private final ProjectIdentifier project;
 	private final String featureId;
+	private final String identifier;
+	private final int hashCode;
 
-	public FeatureIdentifier(ProjectIdentifier projectId, String featureId) {
-		super();
-		Assert.notNull(projectId, "projectId is null");
+	public FeatureIdentifier(ProjectIdentifier project, String featureId) {
+		// Make sure we are being configured correctly
+		Assert.notNull(project, "project is null");
 		Assert.notBlank(featureId, "featureId is required");
-		this.projectId = projectId;
+
+		// Store the project identifier and featureId
+		this.project = project;
 		this.featureId = featureId;
+
+		// Cache a reference to the fully qualified identifier string
+		this.identifier = project.getIdentifier() + ":" + featureId;
+
+		// Cache the hash code of the identifier string
+		this.hashCode = identifier.hashCode();
 	}
 
-	public ProjectIdentifier getProjectId() {
-		return projectId;
+	public ProjectIdentifier getProject() {
+		return project;
+	}
+
+	@Override
+	public String toString() {
+		return identifier;
 	}
 
 	public String getFeatureId() {
 		return featureId;
+	}
+
+	@Override
+	public int hashCode() {
+		return hashCode;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+
+		// They are the exact same physical object
+		if (this == object) {
+			return true;
+		}
+
+		// If object is null or a different runtime type, it's not equal
+		if (object == null || getClass() != object.getClass()) {
+			return false;
+		}
+
+		// Cast object to a FeatureIdentifier
+		FeatureIdentifier other = (FeatureIdentifier) object;
+
+		// The hashcode's being equal AND the identifier strings being equal, constitutes equality
+		return hashCode == other.hashCode && identifier.equals(other.identifier);
+	}
+
+	public String getIdentifier() {
+		return identifier;
 	}
 
 }
