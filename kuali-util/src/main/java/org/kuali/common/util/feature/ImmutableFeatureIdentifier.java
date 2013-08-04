1a7d2efc2cca4428b47355634720e88a8ccee33f
jcaddel <jcaddel@kuali.org>
2013-08-04 11:32:46 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24051 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/feature/ImmutableFeatureIdentifier.java
+++ kuali-util/src/main/java/org/kuali/common/util/feature/ImmutableFeatureIdentifier.java
@@ -1,16 +1,17 @@
 package org.kuali.common.util.feature;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.identifier.AbstractIdentifier;
 import org.kuali.common.util.identifier.ImmutableIdentifier;
 import org.kuali.common.util.project.ProjectIdentifier;
 
-public final class ImmutableFeatureIdentifier implements FeatureIdentifier {
+public final class ImmutableFeatureIdentifier extends AbstractIdentifier implements FeatureIdentifier {
 
 	private final ProjectIdentifier project;
 	private final String featureId;
-	private final ImmutableIdentifier identifier;
 
 	public ImmutableFeatureIdentifier(ProjectIdentifier project, String featureId) {
+		super(new ImmutableIdentifier(project.getIdentifier() + ":" + featureId));
 
 		// Make sure we are being configured correctly
 		Assert.notNull(project, "project is null");
@@ -19,9 +20,6 @@ public final class ImmutableFeatureIdentifier implements FeatureIdentifier {
 		// Store the project identifier and featureId
 		this.project = project;
 		this.featureId = featureId;
-
-		// Cache a reference to the fully qualified feature identifier string
-		this.identifier = new ImmutableIdentifier(project.getIdentifier() + ":" + featureId);
 	}
 
 	@Override
@@ -37,26 +35,6 @@ public final class ImmutableFeatureIdentifier implements FeatureIdentifier {
 	@Override
 	public String getFeatureId() {
 		return featureId;
-	}
-
-	@Override
-	public String toString() {
-		return identifier.toString();
-	}
-
-	@Override
-	public int hashCode() {
-		return identifier.hashCode();
-	}
-
-	@Override
-	public boolean equals(Object object) {
-		return identifier.equals(object);
-	}
-
-	@Override
-	public String getIdentifier() {
-		return identifier.getIdentifier();
 	}
 
 }
