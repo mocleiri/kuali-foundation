89b3404bf8c776c13979227acf75615665856d0d
jcaddel <jcaddel@kuali.org>
2013-08-04 09:06:43 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24020 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/project/ProjectIdentifier.java
+++ kuali-util/src/main/java/org/kuali/common/util/project/ProjectIdentifier.java
@@ -17,6 +17,33 @@ package org.kuali.common.util.project;
 
 import org.kuali.common.util.Assert;
 
+/**
+ * The project identifier concept is based on two facts:
+ * 
+ * <p>
+ * 1 - All Kuali projects produce only one artifact containing executable java code and associated resources.<br>
+ * 2 - There is only one version of any given Kuali project in the java classpath.<br>
+ * </p>
+ * 
+ * <p>
+ * Thus, groupId + artifactId is a simple way to uniquely namespace project resources at runtime.
+ * 
+ * For example, files residing in the project at the following locations:
+ * 
+ * <pre>
+ *   src/main/resources/org/kuali/common/kuali-util/foo.txt
+ *   src/main/resources/org/kuali/common/kuali-util/bar.txt
+ * </pre>
+ * 
+ * Can be referenced at runtime as:
+ * 
+ * <pre>
+ *   classpath:org/kuali/common/kuali-util/foo.txt
+ *   classpath:org/kuali/common/kuali-util/bar.txt
+ * </pre>
+ * 
+ * </p>
+ */
 public final class ProjectIdentifier implements ProjectIdentifierInterface {
 
 	private final String groupId;
