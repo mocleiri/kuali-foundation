3654a2fb5248b6aa1e48ddd68758c2069063585e
jcaddel <jcaddel@kuali.org>
2013-08-05 09:54:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24117 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/model/RelativeContext.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/model/RelativeContext.java
@@ -3,32 +3,42 @@ package org.kuali.common.util.metainf.model;
 import java.io.File;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.file.CanonicalFile;
 import org.springframework.util.ResourceUtils;
 
 public class RelativeContext {
 
 	public static final String DEFAULT_RELATIVE_URL_PREFIX = ResourceUtils.CLASSPATH_URL_PREFIX;
 
-	private final File directory;
+	private final File parent;
+	private final File child;
 	private final String urlPrefix;
 
-	public RelativeContext(File directory) {
-		this(directory, DEFAULT_RELATIVE_URL_PREFIX);
+	public RelativeContext(File parent) {
+		this(parent, parent, DEFAULT_RELATIVE_URL_PREFIX);
 	}
 
-	public RelativeContext(File directory, String urlPrefix) {
-		Assert.notNull(directory, "directory is null");
-		Assert.notNull(urlPrefix, "urlPrefix is null");
-		this.directory = directory;
+	public RelativeContext(File parent, File child) {
+		this(parent, child, DEFAULT_RELATIVE_URL_PREFIX);
+	}
+
+	public RelativeContext(File parent, File child, String urlPrefix) {
+		Assert.noNulls("parent, child, and urlPrefix are required", parent, child, urlPrefix);
+		this.parent = new CanonicalFile(parent);
+		this.child = new CanonicalFile(parent);
 		this.urlPrefix = urlPrefix;
-	}
-
-	public File getDirectory() {
-		return directory;
 	}
 
 	public String getUrlPrefix() {
 		return urlPrefix;
+	}
+
+	public File getParent() {
+		return parent;
+	}
+
+	public File getChild() {
+		return child;
 	}
 
 }
