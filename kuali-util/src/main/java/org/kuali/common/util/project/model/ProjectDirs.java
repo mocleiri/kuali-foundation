7e809fd8cb70f93ba8179851743d09394a0863f7
jcaddel <jcaddel@kuali.org>
2013-08-05 17:54:02 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24142 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/project/model/ProjectDirs.java
+++ kuali-util/src/main/java/org/kuali/common/util/project/model/ProjectDirs.java
@@ -2,18 +2,16 @@ package org.kuali.common.util.project.model;
 
 import java.io.File;
 
+import org.kuali.common.util.Assert;
+
 public class ProjectDirs {
 
-	private final File base;
-	private final File build;
-	private final File buildOutput;
-	private final File buildSource;
-	private final File buildScriptSource;
-	private final File buildTestOutput;
-	private final File buildTestSource;
+	public ProjectDirs(File base, File build, File buildOutput) {
+		this(base, build, buildOutput, null, null, null, null);
+	}
 
 	public ProjectDirs(File base, File build, File buildOutput, File buildSource, File buildScriptSource, File buildTestOutput, File buildTestSource) {
-		super();
+		Assert.noNulls("base, build, and buildOutput are required", base, build, buildOutput);
 		this.base = base;
 		this.build = build;
 		this.buildOutput = buildOutput;
@@ -22,6 +20,14 @@ public class ProjectDirs {
 		this.buildTestOutput = buildTestOutput;
 		this.buildTestSource = buildTestSource;
 	}
+
+	private final File base;
+	private final File build;
+	private final File buildOutput;
+	private final File buildSource;
+	private final File buildScriptSource;
+	private final File buildTestOutput;
+	private final File buildTestSource;
 
 	public File getBase() {
 		return base;
