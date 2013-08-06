a275b306839eb67ea194059576eebb27353076bb
jcaddel <jcaddel@kuali.org>
2013-08-06 17:36:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24186 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/project/model/Build.java
+++ kuali-util/src/main/java/org/kuali/common/util/project/model/Build.java
@@ -6,27 +6,27 @@ import org.kuali.common.util.Assert;
 
 public class Build {
 
-	public Build(Project project, File projectDir, File baseDir, File buildOutput) {
-		this(project, projectDir, baseDir, buildOutput, null, null, null, null);
+	public Build(Project project, File projectDir, File baseDir, File outputDir) {
+		this(project, projectDir, baseDir, outputDir, null, null, null, null);
 	}
 
-	public Build(Project project, File projectDir, File baseDir, File buildOutput, File buildSource, File buildScriptSource, File buildTestOutput, File buildTestSource) {
-		Assert.noNulls(project, projectDir, baseDir, buildOutput);
+	public Build(Project project, File projectDir, File baseDir, File outputDir, File sourceDir, File scriptSourceDir, File testOutPutDir, File testSourceDir) {
+		Assert.noNulls(project, projectDir, baseDir, outputDir);
 		this.project = project;
 		this.projectDir = projectDir;
 		this.baseDir = baseDir;
-		this.outputDir = buildOutput;
-		this.buildSource = buildSource;
-		this.scriptSourceDir = buildScriptSource;
-		this.testOutputDir = buildTestOutput;
-		this.testSourceDir = buildTestSource;
+		this.outputDir = outputDir;
+		this.sourceDir = sourceDir;
+		this.scriptSourceDir = scriptSourceDir;
+		this.testOutputDir = testOutPutDir;
+		this.testSourceDir = testSourceDir;
 	}
 
 	private final Project project;
 	private final File projectDir;
 	private final File baseDir;
 	private final File outputDir;
-	private final File buildSource;
+	private final File sourceDir;
 	private final File scriptSourceDir;
 	private final File testOutputDir;
 	private final File testSourceDir;
@@ -43,8 +43,8 @@ public class Build {
 		return outputDir;
 	}
 
-	public File getBuildSource() {
-		return buildSource;
+	public File getSourceDir() {
+		return sourceDir;
 	}
 
 	public File getScriptSourceDir() {
