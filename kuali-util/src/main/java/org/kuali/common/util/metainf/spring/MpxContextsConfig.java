d679a1cdff3866c700d7ed1d47007920d8f7e44c
jcaddel <jcaddel@kuali.org>
2013-08-06 17:28:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24183 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MpxContextsConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MpxContextsConfig.java
@@ -1,18 +1,45 @@
 package org.kuali.common.util.metainf.spring;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import org.kuali.common.util.file.CanonicalFile;
 import org.kuali.common.util.metainf.model.MetaInfContext;
+import org.kuali.common.util.metainf.service.MetaInfUtils;
+import org.kuali.common.util.project.ProjectUtils;
+import org.kuali.common.util.project.model.Project;
+import org.kuali.common.util.project.model.ProjectDirectories;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({ MetaInfCommonConfig.class })
 public class MpxContextsConfig implements MetaInfContextsConfig {
+
+	public static final List<String> RECURSIVE_MPX_INCLUDES = Arrays.asList("**/*.mpx");
+
+	@Autowired
+	MetaInfCommonConfig metaInfCommonConfig;
+
+	@Autowired
+	Project project;
+
+	@Autowired
+	ProjectDirectories projectDirectories;
 
 	@Override
 	@Bean
 	public List<MetaInfContext> metaInfContexts() {
-		return null;
+		File buildOutputDir = projectDirectories.getBuildOutput();
+		String outputPath = MetaInfUtils.getResourcePrefix(project) + MetaInfCommonConfig.DATA_FILENAME;
+		File outputFile = new CanonicalFile(buildOutputDir, outputPath);
+		String encoding = ProjectUtils.getEncoding(project);
+		File scanDir = buildOutputDir;
+		return Collections.singletonList(new MetaInfContext(outputFile, encoding, scanDir, RECURSIVE_MPX_INCLUDES));
 	}
 
 }
