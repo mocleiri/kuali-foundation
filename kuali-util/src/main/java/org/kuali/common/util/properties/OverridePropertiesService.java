38c6c002465b17f97a571a7d425452f537c8690b
jcaddel <jcaddel@kuali.org>
2013-08-06 12:27:15 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24161 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/properties/OverridePropertiesService.java
+++ kuali-util/src/main/java/org/kuali/common/util/properties/OverridePropertiesService.java
@@ -1,8 +1,6 @@
 package org.kuali.common.util.properties;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
@@ -44,7 +42,7 @@ public class OverridePropertiesService implements PropertiesService {
 
 	public OverridePropertiesService(List<Properties> overrides, Mode overrideMode, int indent) {
 		Assert.noNulls(overrides, overrideMode);
-		this.overrides = Collections.unmodifiableList(new ArrayList<Properties>(overrides));
+		this.overrides = PropertyUtils.toImmutable(overrides);
 		this.overrideMode = overrideMode;
 		this.logMessageIndent = indent;
 	}
