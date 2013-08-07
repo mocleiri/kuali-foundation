2034974951be203b8be011f7297f8c633c5fba0a
jcaddel <jcaddel@kuali.org>
2013-08-07 18:44:09 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24246 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/main/MainExecutableConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/main/MainExecutableConfig.java
@@ -4,10 +4,8 @@ import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.spring.ExecutableConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ MainConfig.class })
 public class MainExecutableConfig extends ExecutableConfig {
 
 	@Autowired
