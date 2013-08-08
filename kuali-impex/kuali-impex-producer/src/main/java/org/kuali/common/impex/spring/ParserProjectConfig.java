b99f42f99868c92273f68f50c65efd53bcf4289a
jcaddel <jcaddel@kuali.org>
2013-08-08 08:02:22 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24284 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-producer/src/main/java/org/kuali/common/impex/spring/ParserProjectConfig.java
+++ kuali-impex/kuali-impex-producer/src/main/java/org/kuali/common/impex/spring/ParserProjectConfig.java
@@ -1,20 +1,21 @@
 package org.kuali.common.impex.spring;
 
-import org.kuali.common.util.Project;
-import org.kuali.common.util.ProjectUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/**
+ * @deprecated
+ */
 @Deprecated
+@Configuration
 public class ParserProjectConfig {
 
 	public static final String GROUP_ID = org.kuali.common.util.MavenConstants.KUALI_COMMON_GROUP_ID;
 	public static final String ARTIFACT_ID = "torque-parser";
 
 	@Bean
-	public Project generatorProject() {
-		return ProjectUtils.loadProject(GROUP_ID + ":" + ARTIFACT_ID);
+	public org.kuali.common.util.Project generatorProject() {
+		return org.kuali.common.util.ProjectUtils.loadProject(GROUP_ID + ":" + ARTIFACT_ID);
 	}
 
 }
