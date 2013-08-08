f9f4cc611000649a305d7da9de384b18b7a227ba
jcaddel <jcaddel@kuali.org>
2013-08-08 09:29:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24314 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-cli/src/main/java/org/kuali/common/impex/cli/project/ImpexCLIProjectIdConfig.java
+++ kuali-impex/kuali-impex-cli/src/main/java/org/kuali/common/impex/cli/project/ImpexCLIProjectIdConfig.java
@@ -1,11 +1,14 @@
 package org.kuali.common.impex.cli.project;
 
 import org.kuali.common.util.project.model.ProjectIdentifier;
+import org.kuali.common.util.project.spring.AutowiredProjectConfig;
 import org.kuali.common.util.project.spring.ProjectIdentifierConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({ AutowiredProjectConfig.class })
 public class ImpexCLIProjectIdConfig implements ProjectIdentifierConfig {
 
 	@Override
