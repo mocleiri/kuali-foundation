5aadbe363e58df3ed13f2e2152d96e42a2fe2181
jcaddel <jcaddel@kuali.org>
2013-08-07 20:50:33 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24264 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/spring/SqlLocationsConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/spring/SqlLocationsConfig.java
@@ -3,8 +3,10 @@ package org.kuali.common.util.metainf.spring;
 import java.util.Collections;
 import java.util.List;
 
+import org.kuali.common.util.project.KualiUtilProjectConstants;
+import org.kuali.common.util.project.ProjectService;
 import org.kuali.common.util.project.model.Project;
-import org.kuali.common.util.project.spring.AutowiredProjectConfig;
+import org.kuali.common.util.project.spring.ProjectServiceConfig;
 import org.kuali.common.util.properties.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +14,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ AutowiredProjectConfig.class })
+@Import({ ProjectServiceConfig.class })
 public class SqlLocationsConfig {
 
 	@Autowired
-	Project project;
+	ProjectService projectService;
 
 	@Bean
 	public List<Location> metaInfSqlLocations() {
+		Project project = projectService.getProject(KualiUtilProjectConstants.PROJECT_ID);
 		Location location = MetaInfCommonConfig.getLocation(MetaInfCommonConfig.FEATURE_ID, project, "sql.properties");
 		return Collections.singletonList(location);
 	}
