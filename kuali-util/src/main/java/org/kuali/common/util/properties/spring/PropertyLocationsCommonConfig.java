3cf2fb006309b7584814c6575a679f1182a0ab2c
jcaddel <jcaddel@kuali.org>
2013-08-07 22:37:03 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24278 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/properties/spring/PropertyLocationsCommonConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/properties/spring/PropertyLocationsCommonConfig.java
@@ -1,5 +1,9 @@
 package org.kuali.common.util.properties.spring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.kuali.common.util.project.ProjectService;
 import org.kuali.common.util.project.ProjectUtils;
 import org.kuali.common.util.project.model.Feature;
@@ -25,6 +29,18 @@ public class PropertyLocationsCommonConfig {
 
 	@Autowired
 	ProjectService projectService;
+
+	public List<Location> getLocations(ProjectIdentifier identifier, List<String> filenames) {
+		List<Location> locations = new ArrayList<Location>();
+		for (String filename : filenames) {
+			locations.add(getLocation(identifier, filename));
+		}
+		return locations;
+	}
+
+	public List<Location> getLocations(ProjectIdentifier identifier, String... filenames) {
+		return getLocations(identifier, Arrays.asList(filenames));
+	}
 
 	public Location getLocation(ProjectIdentifier identifier, String filename) {
 		boolean cache = SpringUtils.getBoolean(env, PROPERTIES_CACHE_KEY, DEFAULT_CACHE_PROPERTIES_VALUE);
