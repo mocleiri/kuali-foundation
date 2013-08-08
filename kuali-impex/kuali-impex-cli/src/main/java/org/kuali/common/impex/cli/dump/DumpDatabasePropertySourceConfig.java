85f20e4f3d837089c2bbfaf0e1d35680abcc2b85
jcaddel <jcaddel@kuali.org>
2013-08-08 09:49:28 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24318 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-cli/src/main/java/org/kuali/common/impex/cli/dump/DumpDatabasePropertySourceConfig.java
+++ kuali-impex/kuali-impex-cli/src/main/java/org/kuali/common/impex/cli/dump/DumpDatabasePropertySourceConfig.java
@@ -15,20 +15,29 @@
 
 package org.kuali.common.impex.cli.dump;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 import org.kuali.common.impex.cli.project.ImpexCLIProjectIdConfig;
+import org.kuali.common.jdbc.spring.JdbcPropertyLocationsConfig;
+import org.kuali.common.util.project.ProjectUtils;
 import org.kuali.common.util.project.model.Project;
-import org.kuali.common.util.properties.spring.PropertiesLocationServiceConfig;
+import org.kuali.common.util.properties.Location;
+import org.kuali.common.util.properties.PropertiesService;
+import org.kuali.common.util.properties.spring.PropertiesServiceConfig;
 import org.kuali.common.util.spring.main.MainContext;
 import org.kuali.common.util.spring.service.PropertySourceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.util.Assert;
 
 @Configuration
-@Import({ ImpexCLIProjectIdConfig.class, PropertiesLocationServiceConfig.class })
+@Import({ ImpexCLIProjectIdConfig.class, JdbcPropertyLocationsConfig.class, PropertiesServiceConfig.class })
 public class DumpDatabasePropertySourceConfig implements PropertySourceConfig {
 
 	@Autowired
@@ -37,16 +46,34 @@ public class DumpDatabasePropertySourceConfig implements PropertySourceConfig {
 	@Autowired
 	Project project;
 
+	@Autowired
+	PropertiesService propertiesService;
+
+	@Autowired
+	JdbcPropertyLocationsConfig jdbcPropertyLocationsConfig;
+
 	@Override
 	@Bean
 	public PropertySource<?> propertySource() {
-		String[] args = context.getArgs();
+		List<Location> locations = getLocations();
+		Properties properties = propertiesService.getProperties(locations);
+		return new PropertiesPropertySource("dumpDatabasePropertySource", properties);
+	}
+
+	protected List<Location> getLocations() {
+		Location location = getLocation(context.getArgs());
+		List<Location> locations = new ArrayList<Location>();
+		locations.addAll(jdbcPropertyLocationsConfig.jdbcPropertyLocations());
+		locations.add(location);
+		return locations;
+	}
+
+	protected Location getLocation(String[] args) {
 		Assert.notNull(args, "args is null");
 		Assert.isTrue(args.length > 0, "args.length <= 0");
-
-		String location = args[0];
-
-		return null;
+		String value = args[0];
+		String encoding = ProjectUtils.getEncoding(project);
+		return new Location(value, encoding);
 	}
 
 }
