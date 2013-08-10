a63b0d0eb457878cd12691528fef708541ebb9fd
jcaddel <jcaddel@kuali.org>
2013-08-10 04:30:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24463 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/spring/SqlConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/spring/SqlConfig.java
@@ -2,7 +2,6 @@ package org.kuali.common.util.metainf.spring;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,24 +38,25 @@ public class SqlConfig implements MetaInfContextsConfig {
 	@Override
 	@Bean
 	public List<MetaInfContext> metaInfContexts() {
-		Map<MetaInfGroup, String> includesMap = getIncludesMapping();
+		Map<MetaInfGroup, String> defaultIncludes = getDefaultIncludes();
 		List<MetaInfContext> contexts = new ArrayList<MetaInfContext>();
 		for (MetaInfGroup group : MetaInfGroup.values()) {
-			MetaInfContext context = getMetaInfContext(group, includesMap);
+			MetaInfContext context = getMetaInfContext(group, defaultIncludes);
 			contexts.add(context);
 		}
 		return contexts;
 	}
 
-	protected MetaInfContext getMetaInfContext(MetaInfGroup group, Map<MetaInfGroup, String> includesMap) {
+	protected MetaInfContext getMetaInfContext(MetaInfGroup group, Map<MetaInfGroup, String> defaultIncludes) {
 		String databaseVendor = SpringUtils.getProperty(env, DB_VENDOR_KEY);
 		boolean generateRelativePaths = SpringUtils.getBoolean(env, RELATIVE_KEY, DEFAULT_GENERATE_RELATIVE_PATHS);
-		List<String> includes = Arrays.asList(includesMap.get(group));
+		String includesKey = MetaInfUtils.PROPERTY_PREFIX + ".sql." + group.name().toLowerCase() + ".includes";
+		List<String> includes = SpringUtils.getNoneSensitiveListFromCSV(env, includesKey, defaultIncludes.get(group));
 		File outputFile = MetaInfUtils.getOutputFile(project, build, databaseVendor, group);
 		return new MetaInfContext(outputFile, build.getEncoding(), build.getOutputDir(), includes, generateRelativePaths);
 	}
 
-	protected Map<MetaInfGroup, String> getIncludesMapping() {
+	protected Map<MetaInfGroup, String> getDefaultIncludes() {
 		Map<MetaInfGroup, String> map = new HashMap<MetaInfGroup, String>();
 		map.put(MetaInfGroup.SCHEMA, "**/initial-db/**/*create-schema.sql");
 		map.put(MetaInfGroup.DATA, "**/initial-db/**/data/*.sql");
