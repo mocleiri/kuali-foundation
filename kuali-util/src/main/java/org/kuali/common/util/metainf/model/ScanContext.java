1cdf46081be2ca9af1034ad3ffa6f36e0e71e4f8
jcaddel <jcaddel@kuali.org>
2013-08-10 02:56:55 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24443 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/model/ScanContext.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/model/ScanContext.java
@@ -1,12 +1,12 @@
 package org.kuali.common.util.metainf.model;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.CollectionUtils;
 
 public class ScanContext {
 
@@ -27,10 +27,10 @@ public class ScanContext {
 	}
 
 	public ScanContext(File directory, List<String> includes, List<String> excludes) {
-		Assert.noNulls("directory, includes, and excludes are required", directory, includes, excludes);
+		Assert.noNulls(directory, includes, excludes);
 		this.directory = directory;
-		this.includes = Collections.unmodifiableList(new ArrayList<String>(includes));
-		this.excludes = Collections.unmodifiableList(new ArrayList<String>(excludes));
+		this.includes = CollectionUtils.immutableCopy(includes);
+		this.excludes = CollectionUtils.immutableCopy(excludes);
 	}
 
 	public File getDirectory() {
