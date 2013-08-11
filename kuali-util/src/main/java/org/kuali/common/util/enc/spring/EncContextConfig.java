6fc04ea18b3bace7912053d7ee241102e9a96e9f
jcaddel <jcaddel@kuali.org>
2013-08-11 13:04:52 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24487 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/enc/spring/EncContextConfig.java
@@ -0,0 +1,9 @@
+package org.kuali.common.util.enc.spring;
+
+import org.kuali.common.util.enc.EncryptionContext;
+
+public interface EncContextConfig {
+
+	EncryptionContext encryptionContext();
+
+}
---
+++ kuali-util/src/main/java/org/kuali/common/util/enc/spring/SystemEncContextConfig.java
@@ -0,0 +1,28 @@
+package org.kuali.common.util.enc.spring;
+
+import org.kuali.common.util.enc.EncryptionContext;
+import org.kuali.common.util.sys.SystemService;
+import org.kuali.common.util.sys.spring.SystemServiceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@Import({ SystemServiceConfig.class })
+public class SystemEncContextConfig implements EncContextConfig {
+
+	@Autowired
+	Environment env;
+
+	@Autowired
+	SystemService service;
+
+	@Override
+	@Bean
+	public EncryptionContext encryptionContext() {
+		return null;
+	}
+
+}
