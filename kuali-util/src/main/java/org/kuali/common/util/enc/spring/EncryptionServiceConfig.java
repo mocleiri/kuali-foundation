bb06bd46a3f11a824bb46f84478cfe99ba908e6a
jcaddel <jcaddel@kuali.org>
2013-08-11 18:21:31 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24526 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/enc/spring/EncryptionServiceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/enc/spring/EncryptionServiceConfig.java
@@ -6,21 +6,30 @@ import org.kuali.common.util.enc.EncUtils;
 import org.kuali.common.util.enc.EncryptionService;
 import org.kuali.common.util.enc.EncryptionServiceContext;
 import org.kuali.common.util.enc.NoOpEncryptionService;
+import org.kuali.common.util.spring.service.SpringServiceConfig;
+import org.kuali.common.util.sys.SystemService;
+import org.kuali.common.util.sys.spring.SystemServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({ SystemServiceConfig.class, SpringServiceConfig.class })
 public class EncryptionServiceConfig {
 
 	@Autowired
 	EncryptionServiceContextConfig contextConfig;
+
+	@Autowired
+	SystemService service;
 
 	@Bean
 	public EncryptionService encryptionService() {
 		EncryptionServiceContext context = contextConfig.encryptionServiceContext();
 		if (context.isEnabled()) {
 			TextEncryptor encryptor = EncUtils.getTextEncryptor(context.getPassword(), context.getStrength());
+			service.removeSystemProperty(contextConfig.encryptionPasswordKey());
 			return new DefaultEncryptionService(encryptor);
 		} else {
 			return new NoOpEncryptionService();
