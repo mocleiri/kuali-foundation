a077939136dce6e954d6029753fe1a0f0f0f564b
jcaddel <jcaddel@kuali.org>
2013-08-11 16:12:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24500 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/enc/spring/EncServiceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/enc/spring/EncServiceConfig.java
@@ -6,13 +6,9 @@ import org.kuali.common.util.enc.EncryptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Configuration
 public class EncServiceConfig {
-
-	@Autowired
-	Environment env;
 
 	@Autowired
 	EncContext context;
