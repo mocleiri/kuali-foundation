d4cebb632adc833749659fe33acf3b448509b25c
jcaddel <jcaddel@kuali.org>
2013-08-12 00:22:48 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24559 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/properties/spring/PropertiesLocationServiceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/properties/spring/PropertiesLocationServiceConfig.java
@@ -4,29 +4,29 @@ import org.kuali.common.util.project.ProjectService;
 import org.kuali.common.util.project.spring.ProjectServiceConfig;
 import org.kuali.common.util.properties.DefaultPropertiesLocationService;
 import org.kuali.common.util.properties.PropertiesLocationService;
-import org.kuali.common.util.spring.SpringUtils;
+import org.kuali.common.util.spring.env.EnvironmentService;
+import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@Import({ ProjectServiceConfig.class })
+@Import({ SpringServiceConfig.class, ProjectServiceConfig.class })
 public class PropertiesLocationServiceConfig {
 
 	private static final String CACHE_KEY = "properties.cache";
 	private static final boolean DEFAULT_CACHE_PROPERTIES_VALUE = true;
 
 	@Autowired
-	Environment env;
+	EnvironmentService env;
 
 	@Autowired
 	ProjectService projectService;
 
 	@Bean
 	public PropertiesLocationService propertiesLocationService() {
-		boolean cache = SpringUtils.getBoolean(env, CACHE_KEY, DEFAULT_CACHE_PROPERTIES_VALUE);
+		boolean cache = env.getBoolean(CACHE_KEY, DEFAULT_CACHE_PROPERTIES_VALUE);
 		return new DefaultPropertiesLocationService(projectService, cache);
 	}
 
