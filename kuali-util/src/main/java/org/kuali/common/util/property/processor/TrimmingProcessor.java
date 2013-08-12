081cbd835f1985e3b878dce30ef131ea4d08beac
jcaddel <jcaddel@kuali.org>
2013-08-11 22:05:58 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24543 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/property/processor/TrimmingProcessor.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.util.property.processor;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import org.kuali.common.util.Assert;
+import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.PropertyUtils;
+
+public final class TrimmingProcessor implements PropertyProcessor {
+
+	private final List<String> includes;
+	private final List<String> excludes;
+
+	public TrimmingProcessor(String include) {
+		this(CollectionUtils.noNullsSingletonList(include), Collections.<String> emptyList());
+	}
+
+	public TrimmingProcessor(List<String> includes) {
+		this(includes, Collections.<String> emptyList());
+	}
+
+	public TrimmingProcessor(List<String> includes, List<String> excludes) {
+		Assert.noNulls(includes, excludes);
+		this.includes = includes;
+		this.excludes = excludes;
+	}
+
+	@Override
+	public void process(Properties properties) {
+		PropertyUtils.trim(properties, includes, excludes);
+	}
+
+	public List<String> getIncludes() {
+		return includes;
+	}
+
+	public List<String> getExcludes() {
+		return excludes;
+	}
+}
