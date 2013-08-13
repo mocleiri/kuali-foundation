cec000127b5418fc7d7050b5351b574982388665
jcaddel <jcaddel@kuali.org>
2013-08-13 13:20:42 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24589 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/property/processor/OverridingProcessor.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.util.property.processor;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import org.kuali.common.util.Assert;
+import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.Mode;
+
+public final class OverridingProcessor implements PropertyProcessor {
+
+	public static final int DEFAULT_INDENT = 2;
+	public static final Mode DEFAULT_OVERRIDE_MODE = Mode.INFORM;
+
+	private final Mode overrideMode;
+	private final Properties overrides;
+	private final List<String> includes;
+	private final List<String> excludes;
+	private final int indent;
+
+	public OverridingProcessor(Properties overrides) {
+		this(overrides, DEFAULT_OVERRIDE_MODE, Collections.<String> emptyList(), Collections.<String> emptyList(), DEFAULT_INDENT);
+	}
+
+	public OverridingProcessor(Properties overrides, List<String> includes) {
+		this(overrides, DEFAULT_OVERRIDE_MODE, includes, Collections.<String> emptyList(), DEFAULT_INDENT);
+	}
+
+	public OverridingProcessor(Properties overrides, List<String> includes, List<String> excludes) {
+		this(overrides, DEFAULT_OVERRIDE_MODE, includes, excludes, DEFAULT_INDENT);
+	}
+
+	public OverridingProcessor(Properties overrides, Mode overrideMode, List<String> includes, List<String> excludes, int indent) {
+		Assert.noNulls(overrides, overrideMode, includes, excludes);
+		Assert.isTrue(indent >= 0, "indent is negative");
+		this.overrides = overrides;
+		this.overrideMode = overrideMode;
+		this.includes = CollectionUtils.unmodifiableCopy(includes);
+		this.excludes = CollectionUtils.unmodifiableCopy(excludes);
+		this.indent = indent;
+	}
+
+	@Override
+	public void process(Properties properties) {
+	}
+
+	public Mode getOverrideMode() {
+		return overrideMode;
+	}
+
+	public Properties getOverrides() {
+		return overrides;
+	}
+
+	public List<String> getIncludes() {
+		return includes;
+	}
+
+	public List<String> getExcludes() {
+		return excludes;
+	}
+
+	public int getIndent() {
+		return indent;
+	}
+}
