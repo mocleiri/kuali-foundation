3418dc527e5eb950b5bf6fe0243d775599ae229a
jcaddel <jcaddel@kuali.org>
2013-08-13 14:39:40 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24597 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/properties/DefaultPropertiesService.java
+++ kuali-util/src/main/java/org/kuali/common/util/properties/DefaultPropertiesService.java
@@ -4,12 +4,11 @@ import java.util.List;
 import java.util.Properties;
 
 import org.kuali.common.util.Assert;
-import org.kuali.common.util.Mode;
 import org.kuali.common.util.PropertyUtils;
 import org.kuali.common.util.cache.Cache;
 import org.kuali.common.util.cache.SimpleCache;
 import org.kuali.common.util.property.processor.NoOpProcessor;
-import org.kuali.common.util.property.processor.OverrideProcessor;
+import org.kuali.common.util.property.processor.OverridingProcessor;
 import org.kuali.common.util.property.processor.PropertyProcessor;
 import org.kuali.common.util.resolver.PropertiesValueResolver;
 import org.kuali.common.util.resolver.ValueResolver;
@@ -18,40 +17,18 @@ public final class DefaultPropertiesService implements PropertiesService {
 
 	private static final Cache<String, Properties> CACHE = new SimpleCache<String, Properties>();
 
-	private static final Mode DEFAULT_OVERRIDE_MODE = Mode.INFORM;
-	private static int DEFAULT_LOG_MESSAGE_INDENT = 2;
 	private static final PropertyProcessor DEFAULT_POST_PROCESSOR = NoOpProcessor.INSTANCE;
 
 	private final Properties overrides;
-	private final Mode overrideMode;
-	private final int logMessageIndent;
 	private final PropertyProcessor postProcessor;
 
-	public DefaultPropertiesService() {
-		this(PropertyUtils.EMPTY);
-	}
-
 	public DefaultPropertiesService(Properties overrides) {
-		this(overrides, DEFAULT_OVERRIDE_MODE);
+		this(overrides, DEFAULT_POST_PROCESSOR);
 	}
 
 	public DefaultPropertiesService(Properties overrides, PropertyProcessor postProcessor) {
-		this(overrides, DEFAULT_OVERRIDE_MODE, postProcessor);
-	}
-
-	public DefaultPropertiesService(Properties overrides, Mode overrideMode) {
-		this(overrides, overrideMode, DEFAULT_POST_PROCESSOR, DEFAULT_LOG_MESSAGE_INDENT);
-	}
-
-	public DefaultPropertiesService(Properties overrides, Mode overrideMode, PropertyProcessor postProcessor) {
-		this(overrides, overrideMode, postProcessor, DEFAULT_LOG_MESSAGE_INDENT);
-	}
-
-	public DefaultPropertiesService(Properties overrides, Mode overrideMode, PropertyProcessor postProcessor, int indent) {
-		Assert.noNulls(overrides, overrideMode, postProcessor);
+		Assert.noNulls(overrides, postProcessor);
 		this.overrides = PropertyUtils.toImmutable(overrides);
-		this.overrideMode = overrideMode;
-		this.logMessageIndent = indent;
 		this.postProcessor = postProcessor;
 	}
 
@@ -83,7 +60,7 @@ public final class DefaultPropertiesService implements PropertiesService {
 			Properties loaded = loader.load();
 
 			// Override what we've got so far with what we just loaded
-			override(properties, loaded);
+			new OverridingProcessor(loaded).process(properties);
 		}
 
 		// Do any post processing as needed
@@ -95,11 +72,6 @@ public final class DefaultPropertiesService implements PropertiesService {
 
 	public void clearCache() {
 		CACHE.clear();
-	}
-
-	protected void override(Properties existing, Properties overrides) {
-		PropertyProcessor processor = new OverrideProcessor(overrideMode, overrides, logMessageIndent);
-		processor.process(existing);
 	}
 
 }
