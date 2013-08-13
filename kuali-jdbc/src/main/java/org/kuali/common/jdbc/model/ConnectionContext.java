45bbf90a8569d003a80088bdc72c0a1374e0525e
jcaddel <jcaddel@kuali.org>
2013-08-13 17:32:15 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24618 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/model/ConnectionContext.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/model/ConnectionContext.java
@@ -5,7 +5,7 @@ import org.kuali.common.util.Assert;
 public final class ConnectionContext {
 
 	private ConnectionContext(String url, String username, String password) {
-		Assert.noBlanks(url);
+		Assert.noBlanks(url, username, password);
 		this.url = url;
 		this.username = username;
 		this.password = password;
