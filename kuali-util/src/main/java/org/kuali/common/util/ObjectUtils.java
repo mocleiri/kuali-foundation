08e1978ad1899f790c3888d3190fbba847b0d275
jcaddel <jcaddel@kuali.org>
2013-08-13 10:46:13 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24569 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/ObjectUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/ObjectUtils.java
@@ -29,23 +29,77 @@ public class ObjectUtils {
 	 */
 	public static boolean equalsByToString(Object main, Object other) {
 
+		// They are the same object
+		if (sameObject(main, other)) {
+			return true;
+		}
+
+		// Only bother comparing the toString() methods if they are the exact same runtime type
+		if (sameType(main, other)) {
+			return main.toString().equals(other.toString());
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * <p>
+	 * This method returns <code>true</code> if both <code>hashCode()</code> methods return matching values AND both objects are the exact same runtime type.
+	 * </p>
+	 * 
+	 * <p>
+	 * Returns <code>true</code> immediately if <code>main==other</code> (ie they are the same object).
+	 * </p>
+	 * 
+	 * <p>
+	 * Returns <code>false</code> immediately if <code>other==null</code> or is a different runtime type than <code>main</code>.
+	 * </p>
+	 * 
+	 * <p>
+	 * If neither one is <code>null</code>, and both are the exact same runtime type, then compare their respective <code>hashCode()</code> values.
+	 * </p>
+	 * 
+	 * @param main
+	 *            The object <code>other</code> is being compared to.
+	 * @param other
+	 *            The object being examined for equality with <code>main</code>.
+	 * 
+	 * @throws NullPointerException
+	 *             If <code>main</cod> is <code>null</code>
+	 */
+	public static boolean equalsByHashcode(Object main, Object other) {
+
+		// They are the same object
+		if (sameObject(main, other)) {
+			return true;
+		}
+
+		// Only bother comparing the hashcode's if they are the exact same runtime type
+		if (sameType(main, other)) {
+			return main.hashCode() == other.hashCode();
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean sameObject(Object main, Object other) {
+
 		// Main can't be null
 		if (main == null) {
 			throw new NullPointerException("main is null");
 		}
 
 		// They are the same object
-		if (main == other) {
-			return true;
-		}
-
-		if (other == null || main.getClass() != other.getClass()) {
-			// other being null or a different runtime type means they are not equal
-			return false;
-		} else {
-			// Both toString() methods producing the exact same string constitutes equality
-			return main.toString().equals(other.toString());
-		}
+		return main == other;
 	}
 
+	public static boolean sameType(Object main, Object other) {
+
+		// Main can't be null
+		if (main == null) {
+			throw new NullPointerException("main is null");
+		}
+
+		return other != null && main.getClass() == other.getClass();
+	}
 }
