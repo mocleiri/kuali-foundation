4a461cd8646f3bd5273e55afcb63b054284d2599
jcaddel <jcaddel@kuali.org>
2013-08-14 13:54:03 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24712 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/service/spring/SqlContextConfig.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/service/spring/SqlContextConfig.java
@@ -1,8 +1,23 @@
 package org.kuali.common.jdbc.service.spring;
 
+import org.kuali.common.jdbc.model.DatabaseVendor;
 import org.kuali.common.jdbc.model.sql.SqlContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-public interface SqlContextConfig {
+@Configuration
+@Import({ DatabaseVendorsConfig.class })
+public class SqlContextConfig {
 
-	SqlContext sqlContext();
+	@Autowired
+	DatabaseVendor databaseVendor;
+
+	@Bean
+	public SqlContext sqlContext() {
+
+		return null;
+	}
+
 }
