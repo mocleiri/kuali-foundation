74f71595167211e332b6ee7ee156619a37b0fb29
jcaddel <jcaddel@kuali.org>
2013-08-14 12:45:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24704 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/model/sql/SqlContext.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/model/sql/SqlContext.java
@@ -7,20 +7,19 @@ public final class SqlContext {
 	public static final int DEFAULT_THREADS = 5;
 	public static final String DEFAULT_ENCODING = "UTF-8";
 
-	public SqlContext(DbaSql dba, AdminSql admin, Credentials credentials, String schema) {
-		this(dba, admin, credentials, schema, DEFAULT_ENCODING, DEFAULT_THREADS);
+	public SqlContext(DbaSql dba, Credentials credentials, String schema) {
+		this(dba, credentials, schema, DEFAULT_ENCODING, DEFAULT_THREADS);
 	}
 
 	public SqlContext(DbaSql dba, AdminSql admin, Credentials credentials, String schema, String encoding) {
-		this(dba, admin, credentials, schema, encoding, DEFAULT_THREADS);
+		this(dba, credentials, schema, encoding, DEFAULT_THREADS);
 	}
 
-	public SqlContext(DbaSql dba, AdminSql admin, Credentials credentials, String schema, String encoding, int threads) {
-		Assert.noNulls(dba, admin, credentials);
+	public SqlContext(DbaSql dba, Credentials credentials, String schema, String encoding, int threads) {
+		Assert.noNulls(dba, credentials);
 		Assert.noBlanks(schema, encoding);
 		Assert.isTrue(threads > 0, "threads must be a positive integer");
 		this.dba = dba;
-		this.admin = admin;
 		this.credentials = credentials;
 		this.schema = schema;
 		this.encoding = encoding;
@@ -28,7 +27,6 @@ public final class SqlContext {
 	}
 
 	private final DbaSql dba;
-	private final AdminSql admin;
 	private final Credentials credentials;
 	private final String schema;
 	private final String encoding;
@@ -36,10 +34,6 @@ public final class SqlContext {
 
 	public DbaSql getDba() {
 		return dba;
-	}
-
-	public AdminSql getAdmin() {
-		return admin;
 	}
 
 	public String getSchema() {
