a9dd486277a63561e8f8dae196bd2c434e969526
jcaddel <jcaddel@kuali.org>
2013-08-14 07:40:22 -0400

    updates
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24669 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/service/spring/JdbcVendorsConfig.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/service/spring/JdbcVendorsConfig.java
@@ -2,46 +2,78 @@ package org.kuali.common.jdbc.service.spring;
 
 import java.sql.Driver;
 
+import oracle.jdbc.driver.OracleDriver;
+
+import org.kuali.common.jdbc.model.DatabaseVendor;
 import org.kuali.common.jdbc.model.VendorName;
 import org.kuali.common.jdbc.model.context.ConnectionContext;
-import org.kuali.common.jdbc.model.vendor.DatabaseVendor;
-import org.kuali.common.jdbc.model.vendor.Vendors;
+import org.kuali.common.util.nullify.NullUtils;
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Import({ SpringServiceConfig.class })
 public class JdbcVendorsConfig {
 
-	@Autowired
-	EnvironmentService env;
+	@Import({ SpringServiceConfig.class })
+	@Profile("oracle")
+	public static class OracleDatabaseVendor {
 
-	@Bean
-	public DatabaseVendor databaseVendorOracle() {
-		VendorName name = VendorName.ORACLE;
-		String dbaUsr = env.getString("oracle.dba.username", Vendors.Oracle.Dba.USERNAME);
-		String dbaPwd = env.getString("oracle.dba.password", Vendors.Oracle.Dba.PASSWORD);
-		String dbaUrl = env.getString("oracle.dba.url", Vendors.Oracle.Dba.URL);
-		ConnectionContext dba = new ConnectionContext(dbaUrl, dbaUsr, dbaPwd);
-		Class<? extends Driver> driver = env.getClass("oracle.driver", Vendors.Oracle.DRIVER);
-		String url = env.getString("oracle.url", Vendors.Oracle.URL);
-		return new DatabaseVendor(name, dba, url, driver);
+		@Autowired
+		EnvironmentService env;
+
+		@Bean
+		public DatabaseVendor databaseVendor() {
+			VendorName name = VendorName.ORACLE;
+			String dbaUsr = env.getString("oracle.dba.username", Dba.USERNAME);
+			String dbaPwd = env.getString("oracle.dba.password", Dba.PASSWORD);
+			String dbaUrl = env.getString("oracle.dba.url", Dba.URL);
+			ConnectionContext dba = new ConnectionContext(dbaUrl, dbaUsr, dbaPwd);
+			Class<? extends Driver> driver = env.getClass("oracle.driver", DRIVER);
+			String url = env.getString("oracle.url", URL);
+			return new DatabaseVendor(name, dba, url, driver);
+		}
+
+		public static final Class<? extends Driver> DRIVER = OracleDriver.class;
+		public static final String URL = Dba.URL;
+
+		public static class Dba {
+			public static final String USERNAME = "system";
+			public static final String PASSWORD = "manager";
+			public static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+		}
 	}
 
-	@Bean
-	public DatabaseVendor databaseVendorMySql() {
-		VendorName name = VendorName.MYSQL;
-		String dbaUsr = env.getString("mysql.dba.username", Vendors.MySql.Dba.USERNAME);
-		String dbaPwd = env.getString("mysql.dba.password", Vendors.MySql.Dba.PASSWORD);
-		String dbaUrl = env.getString("mysql.dba.url", Vendors.MySql.Dba.URL);
-		ConnectionContext dba = new ConnectionContext(dbaUrl, dbaUsr, dbaPwd);
-		Class<? extends Driver> driver = env.getClass("mysql.driver", Vendors.MySql.DRIVER);
-		String url = env.getString("mysql.url", Vendors.MySql.URL);
-		return new DatabaseVendor(name, dba, url, driver);
-	}
+	@Import({ SpringServiceConfig.class })
+	@Profile("mysql")
+	public static class MySqlDatabaseVendor {
 
+		@Autowired
+		EnvironmentService env;
+
+		@Bean
+		public DatabaseVendor databaseVendor() {
+			VendorName name = VendorName.MYSQL;
+			String dbaUsr = env.getString("mysql.dba.username", Dba.USERNAME);
+			String dbaPwd = env.getString("mysql.dba.password", Dba.PASSWORD);
+			String dbaUrl = env.getString("mysql.dba.url", Dba.URL);
+			ConnectionContext dba = new ConnectionContext(dbaUrl, dbaUsr, dbaPwd);
+			Class<? extends Driver> driver = env.getClass("mysql.driver", DRIVER);
+			String url = env.getString("mysql.url", URL);
+			return new DatabaseVendor(name, dba, url, driver);
+		}
+
+		public static final Class<? extends Driver> DRIVER = com.mysql.jdbc.Driver.class;
+		public static final String URL = Dba.URL + "/${jdbc.username}";
+
+		public static class Dba {
+			public static final String USERNAME = "root";
+			public static final String PASSWORD = NullUtils.NONE;
+			public static final String URL = "jdbc:mysql://localhost";
+		}
+	}
 }
