4c79ef58aaa30271a3d4dfd2fb6b52c646390088
jcaddel <jcaddel@kuali.org>
2013-08-15 11:54:45 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24793 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/service/spring/annotation/MySql.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/service/spring/annotation/MySql.java
@@ -20,12 +20,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.kuali.common.jdbc.vendor.model.VendorCodes;
+import org.kuali.common.jdbc.vendor.model.Vendors;
 import org.springframework.context.annotation.Profile;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Profile(VendorCodes.MYSQL)
+@Profile(Vendors.Profiles.MYSQL)
 public @interface MySql {
 
 }
--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/VendorCodes.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.common.jdbc.vendor.model;
-
-public final class VendorCodes {
-
-	public static final String ORACLE = "oracle";
-	public static final String MYSQL = "mysql";
-
-}
