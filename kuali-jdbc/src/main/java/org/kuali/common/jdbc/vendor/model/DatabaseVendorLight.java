577e4a9664b171bbe931509ed6e423421881ccbd
jcaddel <jcaddel@kuali.org>
2013-08-15 12:56:57 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24799 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/DatabaseVendorLight.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/DatabaseVendorLight.java
@@ -3,32 +3,26 @@ package org.kuali.common.jdbc.vendor.model;
 import org.kuali.common.jdbc.model.context.ConnectionContext;
 import org.kuali.common.util.Assert;
 
-public final class SimpleDatabaseVendor {
+public final class DatabaseVendorLight {
 
-	public SimpleDatabaseVendor(String name, ConnectionContext dba, String url, String driver) {
+	public DatabaseVendorLight(String code, ConnectionContext dba, String driver) {
 		Assert.noNulls(dba);
-		Assert.noBlanks(name, url, driver);
-		this.name = name;
+		Assert.noBlanks(code, driver);
+		this.code = code;
 		this.dba = dba;
-		this.url = url;
 		this.driver = driver;
 	}
 
-	private final String name;
+	private final String code;
 	private final ConnectionContext dba;
-	private final String url;
 	private final String driver;
 
-	public String getName() {
-		return name;
+	public String getCode() {
+		return code;
 	}
 
 	public ConnectionContext getDba() {
 		return dba;
-	}
-
-	public String getUrl() {
-		return url;
 	}
 
 	public String getDriver() {
