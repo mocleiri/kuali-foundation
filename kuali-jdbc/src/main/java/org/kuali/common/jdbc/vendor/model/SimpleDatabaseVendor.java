5fbb271c228084a554cd9d7cc32baf31bb70a408
jcaddel <jcaddel@kuali.org>
2013-08-15 13:18:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24800 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/SimpleDatabaseVendor.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/SimpleDatabaseVendor.java
@@ -3,9 +3,9 @@ package org.kuali.common.jdbc.vendor.model;
 import org.kuali.common.jdbc.model.context.ConnectionContext;
 import org.kuali.common.util.Assert;
 
-public final class DatabaseVendorLight {
+public final class SimpleDatabaseVendor {
 
-	public DatabaseVendorLight(String code, ConnectionContext dba, String driver) {
+	public SimpleDatabaseVendor(String code, ConnectionContext dba, String driver) {
 		Assert.noNulls(dba);
 		Assert.noBlanks(code, driver);
 		this.code = code;
