a948f6d0cf7a705dce1f49a83ec39b9399c47780
jcaddel <jcaddel@kuali.org>
2013-08-15 08:40:24 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24769 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendor.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendor.java
@@ -1,12 +1,12 @@
 package org.kuali.common.jdbc.vendor.model;
 
+import org.kuali.common.jdbc.model.Credentials;
 import org.kuali.common.jdbc.model.context.ConnectionContext;
-import org.kuali.common.util.nullify.NullUtils;
 
 public enum Vendor {
 
 	ORACLE(VendorConstants.ORACLE_CODE, "system", "manager", "jdbc:oracle:thin:@localhost:1521:XE"), //
-	MYSQL(VendorConstants.MYSQL_CODE, "root", NullUtils.NONE, "jdbc:mysql://localhost");
+	MYSQL(VendorConstants.MYSQL_CODE, "root", Credentials.NO_PASSWORD, "jdbc:mysql://localhost");
 
 	private Vendor(String code, String username, String password, String url) {
 		this.code = code;
