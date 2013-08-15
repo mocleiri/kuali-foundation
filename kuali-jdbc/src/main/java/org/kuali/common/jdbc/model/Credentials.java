1eb3c15098071d8fbd0b305c27c10423a81b8813
jcaddel <jcaddel@kuali.org>
2013-08-15 08:20:50 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24765 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/model/Credentials.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/model/Credentials.java
@@ -1,8 +1,17 @@
 package org.kuali.common.jdbc.model;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.nullify.NullUtils;
 
 public final class Credentials {
+
+	public Credentials() {
+		this(NullUtils.NONE, NullUtils.NONE);
+	}
+
+	public Credentials(String username) {
+		this(username, NullUtils.NONE);
+	}
 
 	public Credentials(String username, String password) {
 		Assert.noBlanks(username, password);
