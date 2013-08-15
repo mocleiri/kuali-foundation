b3a24eec8cfb87536bee95197f9f71cef920b5d0
jcaddel <jcaddel@kuali.org>
2013-08-15 10:36:09 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24779 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/VendorDefaults.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/VendorDefaults.java
@@ -1,18 +1,17 @@
 package org.kuali.common.jdbc.vendor.model;
 
-import org.kuali.common.jdbc.model.Credentials;
 import org.kuali.common.jdbc.model.context.ConnectionContext;
 import org.kuali.common.util.Assert;
 
 public enum VendorDefaults {
 
-	ORACLE(Vendor.ORACLE, "jdbc:oracle:thin:@localhost:1521:XE", "system", "manager"), //
-	MYSQL(Vendor.MYSQL, "jdbc:mysql://localhost", "root", Credentials.NO_PASSWORD);
+	ORACLE(Vendor.ORACLE, new ConnectionContext("jdbc:oracle:thin:@localhost:1521:XE", "system", "manager"), ""), //
+	MYSQL(Vendor.MYSQL, new ConnectionContext("jdbc:mysql://localhost", "root"), "");
 
-	private VendorDefaults(Vendor vendor, String defaultDbaUrl, String defaultDbaUsername, String defaultDbaPassword) {
+	private VendorDefaults(Vendor vendor, ConnectionContext dba, String defaultDriver) {
 		Assert.noNulls(vendor);
 		this.vendor = vendor;
-		this.dba = new ConnectionContext(defaultDbaUrl, defaultDbaUsername, defaultDbaPassword);
+		this.dba = dba;
 	}
 
 	private final Vendor vendor;
