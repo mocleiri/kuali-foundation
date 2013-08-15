a3564bc9b9ca93db7cb47a542af882b08731d818
jcaddel <jcaddel@kuali.org>
2013-08-15 12:06:46 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24797 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/DatabaseVendor.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/DatabaseVendor.java
@@ -8,17 +8,17 @@ import org.kuali.common.util.PropertyUtils;
 
 public final class DatabaseVendor {
 
-	public DatabaseVendor(VendorCode code, ConnectionContext dba, String url, String driver, Properties sql) {
-		Assert.noNulls(code, dba, sql);
-		Assert.noBlanks(url, driver);
-		this.code = code;
+	public DatabaseVendor(String name, ConnectionContext dba, String url, String driver, Properties sql) {
+		Assert.noNulls(dba, sql);
+		Assert.noBlanks(name, url, driver);
+		this.name = name;
 		this.dba = dba;
 		this.url = url;
 		this.driver = driver;
 		this.sql = PropertyUtils.toImmutable(sql);
 	}
 
-	private final VendorCode code;
+	private final String name;
 	private final ConnectionContext dba;
 	private final String url;
 	private final String driver;
@@ -28,8 +28,8 @@ public final class DatabaseVendor {
 		return sql;
 	}
 
-	public VendorCode getCode() {
-		return code;
+	public String getName() {
+		return name;
 	}
 
 	public ConnectionContext getDba() {
