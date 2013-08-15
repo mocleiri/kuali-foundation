ac7e35bf4e94f36e3c4785d5444c6b3534f61ce1
jcaddel <jcaddel@kuali.org>
2013-08-15 14:41:56 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24813 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendors.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendors.java
@@ -23,25 +23,21 @@ public final class Vendors {
 	public static final class Oracle {
 
 		public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
-		public static final ConnectionContext DBA = new ConnectionContext(Dba.URL, Dba.USERNAME, Dba.PASSWORD);
+		public static final String USERNAME = "system";
+		public static final String PASSWORD = "manager";
+		public static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+		public static final ConnectionContext DBA = new ConnectionContext(URL, USERNAME, PASSWORD);
 
-		public static final class Dba {
-			private static final String USERNAME = "system";
-			private static final String PASSWORD = "manager";
-			private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
-		}
 	}
 
 	public static final class MySql {
 
 		public static final String DRIVER = "com.mysql.jdbc.Driver";
-		public static final ConnectionContext DBA = new ConnectionContext(Dba.URL, Dba.USERNAME, Dba.PASSWORD);
+		public static final String USERNAME = "root";
+		public static final String PASSWORD = Credentials.NO_PASSWORD;
+		public static final String URL = "jdbc:mysql://localhost";
+		public static final ConnectionContext DBA = new ConnectionContext(URL, USERNAME, PASSWORD);
 
-		public static final class Dba {
-			public static final String USERNAME = "root";
-			public static final String PASSWORD = Credentials.NO_PASSWORD;
-			public static final String URL = "jdbc:mysql://localhost";
-		}
 	}
 
 	public static final class Codes {
