cc8205d8583d495fc500de671c9ec4e5c1a9b421
jcaddel <jcaddel@kuali.org>
2013-08-15 08:44:04 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24771 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendor.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendor.java
@@ -2,6 +2,7 @@ package org.kuali.common.jdbc.vendor.model;
 
 import org.kuali.common.jdbc.model.Credentials;
 import org.kuali.common.jdbc.model.context.ConnectionContext;
+import org.kuali.common.util.Assert;
 
 public enum Vendor {
 
@@ -9,6 +10,7 @@ public enum Vendor {
 	MYSQL(VendorConstants.MYSQL_CODE, "root", Credentials.NO_PASSWORD, "jdbc:mysql://localhost");
 
 	private Vendor(String code, String defaultDbaUsername, String defaultDbaPassword, String url) {
+		Assert.noBlanks(code);
 		this.code = code;
 		this.dba = new ConnectionContext(url, defaultDbaUsername, defaultDbaPassword);
 	}
