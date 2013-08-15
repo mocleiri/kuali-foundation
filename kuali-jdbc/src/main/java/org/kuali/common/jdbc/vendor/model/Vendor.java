7dcd9c366eebd4270c1474412605004c03c92752
jcaddel <jcaddel@kuali.org>
2013-08-15 14:24:55 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24806 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendor.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendor.java
@@ -2,6 +2,16 @@ package org.kuali.common.jdbc.vendor.model;
 
 public enum Vendor {
 
-	ORACLE, MYSQL;
+	ORACLE(Vendors.Codes.ORACLE), MYSQL(Vendors.Codes.MYSQL);
+
+	private Vendor(String code) {
+		this.code = code;
+	}
+
+	private final String code;
+
+	public String getCode() {
+		return code;
+	}
 
 }
