e884d8af360c3b7af6205233f8b56775d85ab7df
jcaddel <jcaddel@kuali.org>
2013-08-15 10:40:40 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24780 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/VendorDefaults.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/VendorDefaults.java
@@ -5,17 +5,20 @@ import org.kuali.common.util.Assert;
 
 public enum VendorDefaults {
 
-	ORACLE(Vendor.ORACLE, new ConnectionContext("jdbc:oracle:thin:@localhost:1521:XE", "system", "manager"), ""), //
-	MYSQL(Vendor.MYSQL, new ConnectionContext("jdbc:mysql://localhost", "root"), "");
+	ORACLE(Vendor.ORACLE, new ConnectionContext("jdbc:oracle:thin:@localhost:1521:XE", "system", "manager"), "oracle.jdbc.driver.OracleDriver"), //
+	MYSQL(Vendor.MYSQL, new ConnectionContext("jdbc:mysql://localhost", "root"), "com.mysql.jdbc.Driver");
 
-	private VendorDefaults(Vendor vendor, ConnectionContext dba, String defaultDriver) {
-		Assert.noNulls(vendor);
+	private VendorDefaults(Vendor vendor, ConnectionContext dba, String driver) {
+		Assert.noNulls(vendor, dba);
+		Assert.noBlanks(driver);
 		this.vendor = vendor;
 		this.dba = dba;
+		this.driver = driver;
 	}
 
 	private final Vendor vendor;
 	private final ConnectionContext dba;
+	private final String driver;
 
 	public Vendor getVendor() {
 		return vendor;
@@ -23,6 +26,10 @@ public enum VendorDefaults {
 
 	public ConnectionContext getDba() {
 		return dba;
+	}
+
+	public String getDriver() {
+		return driver;
 	}
 
 }
