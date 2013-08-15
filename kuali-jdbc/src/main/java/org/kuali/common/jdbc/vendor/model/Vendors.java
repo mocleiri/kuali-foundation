6bc1a01179ee63eceac7dbd0332f89cf91155ce5
jcaddel <jcaddel@kuali.org>
2013-08-15 14:33:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24808 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendors.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/model/Vendors.java
@@ -1,5 +1,6 @@
 package org.kuali.common.jdbc.vendor.model;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,15 +9,15 @@ import org.kuali.common.jdbc.model.context.ConnectionContext;
 
 public final class Vendors {
 
-	public static final Map<Vendor, VendorBase> DEFAULTS = getSimpleVendors();
+	public static final Map<Vendor, VendorBase> DEFAULTS = getBase();
 
-	public static final Map<Vendor, VendorBase> getSimpleVendors() {
+	public static final Map<Vendor, VendorBase> getBase() {
 		VendorBase oracle = new VendorBase(Vendor.ORACLE, Oracle.DBA, Oracle.DRIVER);
 		VendorBase mysql = new VendorBase(Vendor.MYSQL, MySql.DBA, MySql.DRIVER);
 		Map<Vendor, VendorBase> map = new HashMap<Vendor, VendorBase>();
 		map.put(Vendor.ORACLE, oracle);
 		map.put(Vendor.MYSQL, mysql);
-		return map;
+		return Collections.unmodifiableMap(map);
 	}
 
 	public static final class Oracle {
@@ -24,7 +25,7 @@ public final class Vendors {
 		public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
 		public static final ConnectionContext DBA = new ConnectionContext(Dba.URL, Dba.USERNAME, Dba.PASSWORD);
 
-		private static final class Dba {
+		public static final class Dba {
 			private static final String USERNAME = "system";
 			private static final String PASSWORD = "manager";
 			private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
@@ -36,10 +37,10 @@ public final class Vendors {
 		public static final String DRIVER = "com.mysql.jdbc.Driver";
 		public static final ConnectionContext DBA = new ConnectionContext(Dba.URL, Dba.USERNAME, Dba.PASSWORD);
 
-		private static final class Dba {
-			private static final String USERNAME = "root";
-			private static final String PASSWORD = Credentials.NO_PASSWORD;
-			private static final String URL = "jdbc:mysql://localhost";
+		public static final class Dba {
+			public static final String USERNAME = "root";
+			public static final String PASSWORD = Credentials.NO_PASSWORD;
+			public static final String URL = "jdbc:mysql://localhost";
 		}
 	}
 
