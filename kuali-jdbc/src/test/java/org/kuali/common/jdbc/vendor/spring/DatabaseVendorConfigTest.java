eb67f193ad389907142204821461cd034f8f1591
jcaddel <jcaddel@kuali.org>
2013-08-16 01:14:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24853 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-jdbc/src/test/java/org/kuali/common/jdbc/vendor/spring/DatabaseVendorConfigTest.java
@@ -0,0 +1,42 @@
+package org.kuali.common.jdbc.vendor.spring;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kuali.common.jdbc.vendor.model.Vendor;
+import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.spring.SpringExecUtils;
+import org.kuali.common.util.spring.service.SpringContext;
+import org.kuali.common.util.spring.service.SpringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DatabaseVendorPropertySourceConfig.class)
+public class DatabaseVendorConfigTest {
+
+	@Autowired
+	PropertySource<?> source;
+
+	@Autowired
+	SpringService service;
+
+	@Test
+	public void test() {
+		try {
+			String vendorString = (String) source.getProperty("db.vendor");
+			Vendor vendor = Vendor.valueOf(vendorString.toUpperCase());
+			String profile = vendor.getCode();
+			SpringContext context = SpringExecUtils.getSinglePropertySourceContext(source);
+			context.setAnnotatedClasses(CollectionUtils.asList(DatabaseVendorConfig.class));
+			context.setActiveProfiles(Arrays.asList(profile));
+			service.load(context);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
