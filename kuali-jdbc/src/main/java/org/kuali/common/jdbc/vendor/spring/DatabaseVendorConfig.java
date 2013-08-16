c4acd79452e806a0c0dc121a31cad0df13355979
jcaddel <jcaddel@kuali.org>
2013-08-15 23:12:05 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24834 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/spring/DatabaseVendorConfig.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/spring/DatabaseVendorConfig.java
@@ -1,10 +1,13 @@
 package org.kuali.common.jdbc.vendor.spring;
 
+import java.sql.Driver;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import org.kuali.common.jdbc.model.context.ConnectionContext;
 import org.kuali.common.jdbc.vendor.model.DatabaseVendor;
 import org.kuali.common.jdbc.vendor.model.Vendor;
 import org.kuali.common.jdbc.vendor.model.VendorBase;
@@ -14,6 +17,7 @@ import org.kuali.common.jdbc.vendor.model.keys.KeySuffix;
 import org.kuali.common.jdbc.vendor.model.keys.Liquibase;
 import org.kuali.common.jdbc.vendor.model.keys.Oracle;
 import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.ReflectionUtils;
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +42,38 @@ public class DatabaseVendorConfig {
 
 	@Bean
 	public DatabaseVendor databaseVendor() {
-		VendorBase base = vendorBaseMap().get(databaseVendorEnum());
-		return new DatabaseVendor(null, null, null, null, null);
+		Vendor vendor = databaseVendorEnum();
+		VendorBase base = vendorBaseMap().get(vendor);
+		ConnectionContext dba = getDba(base);
+		Class<? extends Driver> driver = getDriver(base);
+		Properties sql = getSql(base);
+		return new DatabaseVendor(databaseVendorEnum(), dba, null, driver, sql);
+	}
+
+	protected Properties getSql(VendorBase base) {
+		Vendor vendor = base.getVendor();
+		List<KeySuffix> suffixes = vendorSqlKeysMap().get(vendor);
+		Properties properties = new Properties();
+		for (KeySuffix suffix : suffixes) {
+			String key = vendor.getCode() + "." + suffix.getKeySuffix();
+			String sql = env.getString(key);
+			properties.setProperty(key, sql);
+		}
+		return properties;
+	}
+
+	protected Class<? extends Driver> getDriver(VendorBase base) {
+		String prefix = base.getVendor().getCode();
+		String driver = env.getString(prefix + ".driver", base.getDriver());
+		return ReflectionUtils.newInstance(driver);
+	}
+
+	protected ConnectionContext getDba(VendorBase base) {
+		String prefix = base.getVendor().getCode();
+		String dbaUrl = env.getString(prefix + ".dba.url", base.getDba().getUrl());
+		String dbaUsr = env.getString(prefix + ".dba.username", base.getDba().getCredentials().getUsername());
+		String dbaPwd = env.getString(prefix + ".dba.password", base.getDba().getCredentials().getPassword());
+		return new ConnectionContext(dbaUrl, dbaUsr, dbaPwd);
 	}
 
 	@Bean
@@ -54,11 +88,11 @@ public class DatabaseVendorConfig {
 
 	@Bean
 	public Map<Vendor, List<KeySuffix>> vendorSqlKeysMap() {
-		List<KeySuffix> oracle = Collections.unmodifiableList(CollectionUtils.combine(Admin.asList(), Liquibase.asList(), Oracle.asList()));
-		List<KeySuffix> mysql = Collections.unmodifiableList(CollectionUtils.combine(Admin.asList(), Liquibase.asList()));
+		List<KeySuffix> oracle = CollectionUtils.combine(Admin.asList(), Liquibase.asList(), Oracle.asList());
+		List<KeySuffix> mysql = CollectionUtils.combine(Admin.asList(), Liquibase.asList());
 		Map<Vendor, List<KeySuffix>> map = new HashMap<Vendor, List<KeySuffix>>();
-		map.put(Vendor.ORACLE, oracle);
-		map.put(Vendor.MYSQL, mysql);
+		map.put(Vendor.ORACLE, Collections.unmodifiableList(oracle));
+		map.put(Vendor.MYSQL, Collections.unmodifiableList(mysql));
 		return Collections.unmodifiableMap(map);
 	}
 
