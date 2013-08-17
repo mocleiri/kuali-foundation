deca2253a91d30b31e06947554696f2de793c811
jcaddel <jcaddel@kuali.org>
2013-08-16 20:34:53 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24952 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/main/MainUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/main/MainUtils.java
@@ -17,14 +17,25 @@ public class MainUtils {
 	 */
 	public static void runAndExit(Class<?> mainClass, String[] args) {
 		try {
+			// Preserve the context info for the class where main(String[] args) was invoked
 			MainContext mainContext = new MainContext(mainClass, args);
+
+			// Create beans for the Spring context
 			Map<String, Object> beans = Collections.singletonMap(MAIN_CONTEXT_BEAN_NAME, (Object) mainContext);
+
+			// Create a SpringContext with the beans and an active profile called "main"
 			SpringContext context = new SpringContext(beans, mainClass, MAIN_PROFILE_NAME);
+
+			// DefaultSpringService will do what we need it to do
 			SpringService service = SpringExecutable.DEFAULT_SPRING_SERVICE;
-			SpringExecutable exec = new SpringExecutable(service, context);
-			exec.execute();
+
+			// SpringExecutable will load the @Configuration annotated main class
+			new SpringExecutable(service, context).execute();
+
+			// As long as no exception was thrown we assume everything finished ok
 			System.exit(Status.SUCCESS.getValue());
 		} catch (Exception e) {
+			// Otherwise print the message from the exception and return a non-zero exit code
 			System.err.print(e.getMessage());
 			System.exit(Status.FAILURE.getValue());
 		}
