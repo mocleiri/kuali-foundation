5a8742149c9ef92fdef20d8dd3441c633edefe22
jcaddel <jcaddel@kuali.org>
2013-08-17 12:46:29 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24975 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/spring/env/EnvironmentKey.java
@@ -0,0 +1,7 @@
+package org.kuali.common.util.spring.env;
+
+public interface EnvironmentKey {
+
+	String getValue();
+
+}
---
+++ kuali-util/src/main/java/org/kuali/common/util/spring/env/EnvironmentKeySuffix.java
@@ -0,0 +1,7 @@
+package org.kuali.common.util.spring.env;
+
+public interface EnvironmentKeySuffix {
+
+	String getValue();
+
+}
