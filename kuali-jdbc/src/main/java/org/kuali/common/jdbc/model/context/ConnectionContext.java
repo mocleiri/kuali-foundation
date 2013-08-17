bd5b5a207cb1d07f87a3c09e835548e0f4e342e4
jcaddel <jcaddel@kuali.org>
2013-08-17 10:23:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24957 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/model/context/ConnectionContext.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/model/context/ConnectionContext.java
@@ -1,30 +1,38 @@
 package org.kuali.common.jdbc.model.context;
 
+import java.util.Properties;
+
 import org.kuali.common.jdbc.model.Credentials;
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.PropertyUtils;
 
 public final class ConnectionContext {
 
+	private static final Properties DEFAULT_PROPERTIES = PropertyUtils.EMPTY;
+
 	public ConnectionContext(String url) {
-		this(url, Credentials.EMPTY);
+		this(url, Credentials.NO_USERNAME);
 	}
 
 	public ConnectionContext(String url, String username) {
-		this(url, new Credentials(username));
+		this(url, Credentials.NO_USERNAME, Credentials.NO_PASSWORD);
 	}
 
 	public ConnectionContext(String url, String username, String password) {
-		this(url, new Credentials(username, password));
+		this(url, new Credentials(username, password), DEFAULT_PROPERTIES);
 	}
 
-	public ConnectionContext(String url, Credentials credentials) {
+	public ConnectionContext(String url, Credentials credentials, Properties properties) {
+		Assert.noNulls(credentials, properties);
 		Assert.noBlanks(url);
 		this.url = url;
 		this.credentials = credentials;
+		this.properties = PropertyUtils.toImmutable(properties);
 	}
 
 	private final String url;
 	private final Credentials credentials;
+	private final Properties properties;
 
 	public String getUrl() {
 		return url;
@@ -32,5 +40,9 @@ public final class ConnectionContext {
 
 	public Credentials getCredentials() {
 		return credentials;
+	}
+
+	public Properties getProperties() {
+		return properties;
 	}
 }
