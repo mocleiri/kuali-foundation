96ef7f90c82502ebc98a3a22243dd567dcc64a40
jcaddel <jcaddel@kuali.org>
2013-08-17 10:47:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@24961 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/service/spring/DataSourceConfig.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/service/spring/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package org.kuali.common.jdbc.service.spring;
 
 import java.sql.Driver;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -8,15 +9,21 @@ import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.jdbc.model.Credentials;
 import org.kuali.common.jdbc.model.context.ConnectionContext;
 import org.kuali.common.jdbc.model.context.DatabaseProcessContext;
+import org.kuali.common.util.Assert;
+import org.kuali.common.util.property.ImmutableProperties;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 @Import({ DatabaseProcessContextConfig.class })
 public class DataSourceConfig {
+
+	private static final String USERNAME_KEY = "user";
+	private static final String PASSWORD_KEY = "password";
 
 	@Autowired
 	DatabaseProcessContext context;
@@ -24,13 +31,13 @@ public class DataSourceConfig {
 	@Bean
 	public DataSource dataSource() {
 		ConnectionContext normal = context.getConnections().getNormal();
-		return getDataSource(normal, jdbcDriverClass());
+		return getDataSource(normal, jdbcDriver());
 	}
 
 	@Bean
 	public DataSource dataSourceDba() {
 		ConnectionContext dba = context.getConnections().getDba();
-		return getDataSource(dba, jdbcDriverClass());
+		return getDataSource(dba, jdbcDriver());
 	}
 
 	@Bean
@@ -38,16 +45,32 @@ public class DataSourceConfig {
 		return context.getConnections().getDriver();
 	}
 
-	protected DataSource getDataSource(ConnectionContext context, Class<? extends Driver> driver) {
+	@Bean
+	public Driver jdbcDriver() {
+		return BeanUtils.instantiate(jdbcDriverClass());
+	}
+
+	protected DataSource getDataSource(ConnectionContext context, Driver driver) {
+		Properties properties = getProperties(context);
+		return new SimpleDriverDataSource(driver, context.getUrl(), properties);
+	}
+
+	protected Properties getProperties(ConnectionContext context) {
 		Credentials creds = context.getCredentials();
 		String username = toNull(creds.getUsername(), Credentials.NO_USERNAME);
 		String password = toNull(creds.getPassword(), Credentials.NO_PASSWORD);
-		DriverManagerDataSource dmds = new DriverManagerDataSource(context.getUrl(), username, password);
-		dmds.setDriverClassName(driver.getName());
-		return dmds;
+		Properties properties = new Properties(context.getProperties());
+		if (username != null) {
+			properties.setProperty(USERNAME_KEY, username);
+		}
+		if (password != null) {
+			properties.setProperty(PASSWORD_KEY, password);
+		}
+		return new ImmutableProperties(properties);
 	}
 
 	protected String toNull(String token, String nullToken) {
+		Assert.noNulls(nullToken);
 		return StringUtils.equals(token, nullToken) ? null : token;
 	}
 }
