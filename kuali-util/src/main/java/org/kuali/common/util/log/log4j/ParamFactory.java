cc8bd73b2e49f67407f90925c1660ad85939df16
jcaddel <jcaddel@kuali.org>
2013-08-19 16:14:07 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25196 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/log/log4j/ParamFactory.java
@@ -0,0 +1,31 @@
+package org.kuali.common.util.log.log4j;
+
+import java.io.File;
+
+import org.kuali.common.util.file.CanonicalFile;
+import org.kuali.common.util.log.log4j.model.Param;
+import org.kuali.common.util.log4j.model.Value;
+
+public class ParamFactory {
+
+	public static final String CONVERSION_PATTERN = "ConversionPattern";
+	public static final String FILE = "File";
+	public static final String THRESHOLD = "Threshold";
+
+	public static Param getConversionPattern(String pattern) {
+		return new Param(CONVERSION_PATTERN, pattern);
+	}
+
+	public static Param getFile(String filename) {
+		return new Param(FILE, filename);
+	}
+
+	public static Param getFile(File file) {
+		return getFile(new CanonicalFile(file).getAbsolutePath());
+	}
+
+	public static Param getThreshold(Value value) {
+		return new Param(THRESHOLD, value.name());
+	}
+
+}
