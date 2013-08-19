94c30bd01ad41a5b6d5b185381ab8cd60c3af8a2
jcaddel <jcaddel@kuali.org>
2013-08-19 14:02:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25173 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log/log4j/model/Level.java
+++ kuali-util/src/main/java/org/kuali/common/util/log/log4j/model/Level.java
@@ -6,32 +6,34 @@ import org.kuali.common.util.Assert;
 
 public final class Level {
 
-	public static final Class<org.apache.log4j.Level> DEFAULT_JAVA_CLASS = org.apache.log4j.Level.class;
-	public static final Value DEFAULT_VALUE = Value.INFO;
+	public static final Class<org.apache.log4j.Level> DEFAULT_CLASS = org.apache.log4j.Level.class;
+	public static final Value DEFAULT_VALUE = Value.NULL;
 
-	private final Class<?> javaClass;
+	@XmlAttribute(name = "class")
+	private final Class<?> levelClass;
+
+	@XmlAttribute
 	private final Value value;
 
-	public Level() {
-		this(DEFAULT_JAVA_CLASS, Value.INFO);
+	@SuppressWarnings("unused")
+	private Level() {
+		this(DEFAULT_CLASS, DEFAULT_VALUE);
 	}
 
 	public Level(Value value) {
-		this(DEFAULT_JAVA_CLASS, value);
+		this(DEFAULT_CLASS, value);
 	}
 
-	public Level(Class<?> javaClass, Value value) {
-		Assert.noNulls(javaClass, value);
-		this.javaClass = javaClass;
+	public Level(Class<?> levelClass, Value value) {
+		Assert.noNulls(levelClass, value);
+		this.levelClass = levelClass;
 		this.value = value;
 	}
 
-	@XmlAttribute(name = "class")
-	public Class<?> getJavaClass() {
-		return javaClass;
+	public Class<?> getLevelClass() {
+		return levelClass;
 	}
 
-	@XmlAttribute
 	public Value getValue() {
 		return value;
 	}
