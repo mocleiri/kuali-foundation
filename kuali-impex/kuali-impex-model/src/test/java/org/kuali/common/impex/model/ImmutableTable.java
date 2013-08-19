56f3111fad003e878a20d7305077dd9aa879dc7a
jcaddel <jcaddel@kuali.org>
2013-08-19 09:11:13 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25147 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/model/ImmutableTable.java
+++ kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/model/ImmutableTable.java
@@ -1,27 +1,16 @@
 package org.kuali.common.impex.model;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
+import org.kuali.common.impex.model.adapter.TableAdapter;
 import org.kuali.common.util.Assert;
 import org.kuali.common.util.nullify.NullUtils;
 
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.PROPERTY)
+@XmlJavaTypeAdapter(TableAdapter.class)
 public final class ImmutableTable implements NamedElement {
 
-	@XmlAttribute
 	private final String name;
-
-	@XmlAttribute
 	private final String description;
-
-	@SuppressWarnings("unused")
-	private ImmutableTable() {
-		this(NullUtils.NONE);
-	}
 
 	public ImmutableTable(String name) {
 		this(name, NullUtils.NONE);
