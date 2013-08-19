6bdad5bf69322ea9123ff178ce58f2fbde580711
jcaddel <jcaddel@kuali.org>
2013-08-18 22:37:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25126 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/suppliers/SuppliersFactory.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/suppliers/SuppliersFactory.java
@@ -8,15 +8,15 @@ import org.kuali.common.util.Assert;
 import org.kuali.common.util.CollectionUtils;
 import org.kuali.common.util.LocationUtils;
 
-public final class ResourcesFactory {
+public final class SuppliersFactory {
 
 	public static final String DEFAULT_EXTENSION = "resources";
 
-	public ResourcesFactory(List<SupplierFactory> factories) {
+	public SuppliersFactory(List<SupplierFactory> factories) {
 		this(DEFAULT_EXTENSION, factories);
 	}
 
-	public ResourcesFactory(String extension, List<SupplierFactory> factories) {
+	public SuppliersFactory(String extension, List<SupplierFactory> factories) {
 		Assert.noBlanks(extension);
 		Assert.isFalse(CollectionUtils.isEmpty(factories), "Must provide at least one factory");
 		this.factories = CollectionUtils.unmodifiableCopy(factories);
