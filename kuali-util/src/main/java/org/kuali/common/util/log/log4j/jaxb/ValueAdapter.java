150cbdbb4e9493260eff0b21c7036715f7041e9a
jcaddel <jcaddel@kuali.org>
2013-08-20 01:55:57 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25246 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/log/log4j/jaxb/ValueAdapter.java
@@ -0,0 +1,28 @@
+package org.kuali.common.util.log.log4j.jaxb;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import org.kuali.common.util.log.log4j.model.Value;
+
+public abstract class ValueAdapter extends XmlAdapter<String, String> {
+
+	@Override
+	public final String marshal(String value) {
+		Value v = Value.valueOf(value.toUpperCase());
+		if (Value.DEFAULT_VALUE.equals(v)) {
+			return null;
+		} else {
+			return v.name().toLowerCase();
+		}
+	}
+
+	@Override
+	public final String unmarshal(String value) {
+		if (value == null) {
+			return Value.DEFAULT_VALUE.name().toLowerCase();
+		} else {
+			return value;
+		}
+	}
+
+}
