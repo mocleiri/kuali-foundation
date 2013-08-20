8eb251f3c509aa87996da4a7e6495b2b784e7371
jcaddel <jcaddel@kuali.org>
2013-08-20 00:42:07 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25230 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/DropFalseAdapter.java
@@ -0,0 +1,24 @@
+package org.kuali.common.util.xml.jaxb;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class DropFalseAdapter extends XmlAdapter<Boolean, Boolean> {
+
+	@Override
+	public Boolean unmarshal(Boolean value) {
+		if (Boolean.FALSE.equals(value)) {
+			return null;
+		} else {
+			return value;
+		}
+	}
+
+	@Override
+	public Boolean marshal(Boolean value) {
+		if (value == null) {
+			return Boolean.FALSE;
+		} else {
+			return value;
+		}
+	}
+}
---
+++ kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/DropTrueAdapter.java
@@ -0,0 +1,24 @@
+package org.kuali.common.util.xml.jaxb;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class DropTrueAdapter extends XmlAdapter<Boolean, Boolean> {
+
+	@Override
+	public Boolean unmarshal(Boolean value) {
+		if (Boolean.TRUE.equals(value)) {
+			return null;
+		} else {
+			return value;
+		}
+	}
+
+	@Override
+	public Boolean marshal(Boolean value) {
+		if (value == null) {
+			return Boolean.TRUE;
+		} else {
+			return value;
+		}
+	}
+}
