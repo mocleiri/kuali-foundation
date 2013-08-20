b7b1e1de3682015f31fa733fdb6e38fb8b74f0d7
jcaddel <jcaddel@kuali.org>
2013-08-20 14:01:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25285 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log/log4j/jaxb/UnmodifiableAppenderListAdapter.java
+++ kuali-util/src/main/java/org/kuali/common/util/log/log4j/jaxb/UnmodifiableAppenderListAdapter.java
@@ -1,34 +1,17 @@
 package org.kuali.common.util.log.log4j.jaxb;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
-import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 import org.kuali.common.util.log.log4j.model.Appender;
 
-public class UnmodifiableAppenderListAdapter extends XmlAdapter<Appender[], List<Appender>> {
-
-	private static final List<Appender> EMPTY_LIST = Collections.<Appender> emptyList();
-	private static final Appender[] EMPTY_ARRAY = {};
+public class UnmodifiableAppenderListAdapter extends UnmodifiableListAdapter<Appender> {
 
 	@Override
-	public Appender[] marshal(List<Appender> list) {
-		if (list == null || list.size() == 0) {
-			return EMPTY_ARRAY;
+	protected Appender[] getArray(List<Appender> list) {
+		if (list == null) {
+			return new Appender[0];
 		} else {
 			return list.toArray(new Appender[list.size()]);
 		}
 	}
-
-	@Override
-	public List<Appender> unmarshal(Appender[] array) {
-		if (array == null || array.length == 0) {
-			return EMPTY_LIST;
-		} else {
-			return Arrays.asList(array);
-		}
-	}
-
 }
---
+++ kuali-util/src/main/java/org/kuali/common/util/log/log4j/jaxb/UnmodifiableListAdapter.java
@@ -0,0 +1,32 @@
+package org.kuali.common.util.log.log4j.jaxb;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public abstract class UnmodifiableListAdapter<T> extends XmlAdapter<T[], List<T>> {
+
+	private final List<T> EMPTY_LIST = Collections.<T> emptyList();
+
+	protected abstract T[] getArray(List<T> list);
+
+	@Override
+	public T[] marshal(List<T> list) {
+		if (list == null || list.size() == 0) {
+			return null;
+		} else {
+			return getArray(list);
+		}
+	}
+
+	@Override
+	public List<T> unmarshal(T[] array) {
+		if (array == null || array.length == 0) {
+			return EMPTY_LIST;
+		} else {
+			return Collections.unmodifiableList(Arrays.asList(array));
+		}
+	}
+}
