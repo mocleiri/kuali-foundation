d2a320baa17043d641be2d9d348903c23d0205bc
jcaddel <jcaddel@kuali.org>
2013-08-20 01:20:10 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25241 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/log/log4j/model/Level.java
+++ kuali-util/src/main/java/org/kuali/common/util/log/log4j/model/Level.java
@@ -1,25 +1,26 @@
 package org.kuali.common.util.log.log4j.model;
 
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.kuali.common.util.Assert;
-import org.kuali.common.util.nullify.Null;
+import org.kuali.common.util.log.log4j.jaxb.DropLevelClassAdapter;
 
 public final class Level {
 
 	public static final Class<org.apache.log4j.Level> DEFAULT_CLASS = org.apache.log4j.Level.class;
-	public static final Class<Null> NO_CLASS = Null.class;
 	public static final Value NO_VALUE = Value.NULL;
 	public static final Level NO_LEVEL = new Level();
 
 	@XmlAttribute(name = "class")
+	@XmlJavaTypeAdapter(DropLevelClassAdapter.class)
 	private final Class<?> levelClass;
 
 	@XmlAttribute
 	private final Value value;
 
 	private Level() {
-		this(NO_CLASS, NO_VALUE);
+		this(DEFAULT_CLASS, NO_VALUE);
 	}
 
 	public Level(Value value) {
