004d6e3f965d34234ebfb0500fe5816637e37813
jcaddel <jcaddel@kuali.org>
2013-08-21 12:45:28 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25342 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/Student.java
+++ kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/Student.java
@@ -1,5 +1,27 @@
 package org.kuali.common.util.xml.jaxb;
 
+import java.util.List;
+
+import org.kuali.common.util.Assert;
+
 public final class Student {
+
+	public Student(String name, List<Class> classes) {
+		Assert.noBlanks(name);
+		Assert.noNulls(classes);
+		this.name = name;
+		this.classes = classes;
+	}
+
+	private final String name;
+	private final List<Class> classes;
+
+	public String getName() {
+		return name;
+	}
+
+	public List<Class> getClasses() {
+		return classes;
+	}
 
 }
