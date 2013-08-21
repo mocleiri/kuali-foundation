852279e9d4764e255e1b7f3b3e13766741c1dad1
jcaddel <jcaddel@kuali.org>
2013-08-21 15:10:59 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25358 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/Student.java
+++ kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/Student.java
@@ -19,7 +19,7 @@ public final class Student {
 	private final String name;
 
 	@XmlElement
-	@XmlJavaTypeAdapter(UnmodifiableListAdapter.class)
+	@XmlJavaTypeAdapter(ImmutableListArrayAdapter.class)
 	private final List<Club> clubs;
 
 	public String getName() {
