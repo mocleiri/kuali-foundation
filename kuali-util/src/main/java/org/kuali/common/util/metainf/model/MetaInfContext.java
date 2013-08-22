52cf906dc062224fdc7fb67add4f547b38568e47
jcaddel <jcaddel@kuali.org>
2013-08-22 12:30:31 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25389 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/model/MetaInfContext.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/model/MetaInfContext.java
@@ -16,86 +16,68 @@
 package org.kuali.common.util.metainf.model;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.List;
 
 import org.kuali.common.util.Assert;
-import org.kuali.common.util.CollectionUtils;
 import org.kuali.common.util.file.CanonicalFile;
 
 public class MetaInfContext {
 
-	public static final boolean DEFAULT_SORT = true;
-	public static final boolean DEFAULT_GENERATE_RELATIVE_PATHS = true;
-
+	private final CanonicalFile outputFile;
+	private final File scanDir;
 	private final boolean sort;
 	private final String encoding;
-	private final PropertiesContext propertiesContext;
-	private final CanonicalFile outputFile;
-	private final ScanContext scanContext;
-	private final RelativeContext relativeContext;
+	private final boolean includePropertiesFile;
+	private final boolean includeFileSizes;
+	private final boolean includeLineCounts;
+	private final List<String> includes;
+	private final List<String> excludes;
+	private final boolean generateRelativePaths;
+	private final CanonicalFile parent;
+	private final String urlPrefix;
 
-	public MetaInfContext(File outputFile, String encoding, File scanDir, String includes) {
-		this(outputFile, encoding, scanDir, CollectionUtils.singletonList(includes));
+	public boolean isSort() {
+		return sort;
 	}
 
-	public MetaInfContext(File outputFile, String encoding, File scanDir, String includes, boolean generateRelativePaths) {
-		this(outputFile, encoding, scanDir, CollectionUtils.singletonList(includes), generateRelativePaths);
+	public CanonicalFile getOutputFile() {
+		return outputFile;
 	}
 
-	public MetaInfContext(File outputFile, String encoding, File scanDir, String... includes) {
-		this(outputFile, encoding, scanDir, Arrays.asList(includes));
-	}
-
-	public MetaInfContext(File outputFile, String encoding, File scanDir, List<String> includes) {
-		this(outputFile, encoding, new ScanContext(scanDir, includes), new RelativeContext(scanDir));
-	}
-
-	public MetaInfContext(File outputFile, String encoding, File scanDir, ScanContext scanContext) {
-		this(outputFile, encoding, scanContext, new RelativeContext(scanDir));
-	}
-
-	public MetaInfContext(File outputFile, String encoding, File scanDir, ScanContext scanContext, boolean generateRelativePaths) {
-		this(outputFile, encoding, scanContext, new RelativeContext(scanDir, generateRelativePaths));
-	}
-
-	public MetaInfContext(File outputFile, String encoding, File scanDir, List<String> includes, boolean generateRelativePaths) {
-		this(outputFile, encoding, new ScanContext(scanDir, includes), new RelativeContext(scanDir, generateRelativePaths));
-	}
-
-	public MetaInfContext(File outputFile, String encoding, ScanContext scanContext, RelativeContext relativeContext) {
-		this(outputFile, encoding, scanContext, relativeContext, DEFAULT_SORT, PropertiesContext.DEFAULT_PROPERTIES_CONTEXT);
-	}
-
-	public MetaInfContext(File outputFile, String encoding, ScanContext scanContext, RelativeContext relativeContext, boolean sort, PropertiesContext propertiesContext) {
-		Assert.noNulls(outputFile, scanContext, relativeContext, propertiesContext);
-		Assert.noBlanks(encoding);
-		this.outputFile = new CanonicalFile(outputFile);
-		this.encoding = encoding;
-		this.scanContext = scanContext;
-		this.relativeContext = relativeContext;
-		this.sort = sort;
-		this.propertiesContext = propertiesContext;
+	public String getEncoding() {
+		return encoding;
 	}
 
 	public static class Builder {
 
 		private final CanonicalFile outputFile;
 		private final String encoding;
-		private final ScanContext scanContext;
 
 		private boolean sort = true;
-		private PropertiesContext propertiesContext = PropertiesContext.DEFAULT_PROPERTIES_CONTEXT;
-		private RelativeContext relativeContext;
 
-		public Builder(File outputFile, String encoding, ScanContext scanContext) {
+		public Builder(File outputFile, String encoding, File scanDir) {
 			this.outputFile = new CanonicalFile(outputFile);
 			this.encoding = encoding;
-			this.scanContext = scanContext;
+			this.relativeContext = new RelativeContext(scanContext.getDirectory());
+		}
+
+		public Builder sort(boolean sort) {
+			this.sort = sort;
+			return this;
+		}
+
+		public Builder relative(RelativeContext context) {
+			this.relativeContext = context;
+			return this;
+		}
+
+		public Builder properties(PropertiesContext context) {
+			this.propertiesContext = context;
+			return this;
 		}
 
 		public MetaInfContext build() {
-			Assert.noNulls(outputFile, scanContext, propertiesContext);
+			Assert.noNulls(outputFile, scanContext, relativeContext, propertiesContext);
 			Assert.noBlanks(encoding);
 			return new MetaInfContext(this);
 		}
@@ -109,30 +91,6 @@ public class MetaInfContext {
 		this.outputFile = builder.outputFile;
 		this.scanContext = builder.scanContext;
 		this.relativeContext = builder.relativeContext;
-	}
-
-	public boolean isSort() {
-		return sort;
-	}
-
-	public CanonicalFile getOutputFile() {
-		return outputFile;
-	}
-
-	public ScanContext getScanContext() {
-		return scanContext;
-	}
-
-	public RelativeContext getRelativeContext() {
-		return relativeContext;
-	}
-
-	public PropertiesContext getPropertiesContext() {
-		return propertiesContext;
-	}
-
-	public String getEncoding() {
-		return encoding;
 	}
 
 }
