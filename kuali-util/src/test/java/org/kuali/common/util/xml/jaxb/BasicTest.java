2daf03c50dbde75780d8146d81b93e22c7f7afef
jcaddel <jcaddel@kuali.org>
2013-08-23 18:31:36 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25466 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/BasicTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/BasicTest.java
@@ -12,7 +12,7 @@ public class BasicTest {
 			String encoding = "UTF-8";
 			DefaultXmlService service = new DefaultXmlService();
 			List<Club> clubs = Arrays.asList(new Club("dairy"), new Club("archery", 15.0));
-			Student student = new Student.Builder("joe").clubs(clubs).sport(new Sport("soccer")).build();
+			Student student = new Student.Builder("joe").clubs(clubs).sport(new Sport("soccer")).ethnicity("caucasian").build();
 			Student student2 = new Student.Builder("joe").build();
 			String xml = service.toXml(student, encoding);
 			System.out.println(xml);
