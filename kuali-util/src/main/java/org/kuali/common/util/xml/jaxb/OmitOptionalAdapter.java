25224ed458c00d1a9cb07e48e5d7693d0309033e
jcaddel <jcaddel@kuali.org>
2013-08-23 18:16:58 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25462 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/OmitOptionalAdapter.java
@@ -0,0 +1,27 @@
+package org.kuali.common.util.xml.jaxb;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import com.google.common.base.Optional;
+
+public class OmitOptionalAdapter<T> extends XmlAdapter<T, Optional<T>> {
+
+	@Override
+	public T marshal(Optional<T> optional) {
+		if (optional.isPresent()) {
+			return optional.get();
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public Optional<T> unmarshal(T value) {
+		if (value == null) {
+			return Optional.<T> absent();
+		} else {
+			return Optional.<T> of(value);
+		}
+	}
+
+}
