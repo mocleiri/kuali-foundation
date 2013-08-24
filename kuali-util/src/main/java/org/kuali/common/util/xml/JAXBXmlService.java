0a0082757e4b5d859f37d481d5dc4be7df2b768f
jcaddel <jcaddel@kuali.org>
2013-08-24 14:04:47 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25535 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/xml/JAXBXmlService.java
+++ kuali-util/src/main/java/org/kuali/common/util/xml/JAXBXmlService.java
@@ -214,13 +214,26 @@ public class JAXBXmlService implements XmlService {
 	public static class Builder {
 
 		private static final Map<String, ?> EMPTY_MAP = Collections.unmodifiableMap(new HashMap<String, Object>());
+
 		public static final boolean FORMAT_OUTPUT = true;
 		public static final boolean USE_NAMESPACE_AWARE_PARSER = true;
+		public static final boolean USE_ECLIPSE_LINK_MOXY_PROVIDER = true;
 
 		// Optional fields with default values
 		private boolean formatOutput = FORMAT_OUTPUT;
-		private boolean useNamespaceAwareParser = USE_NAMESPACE_AWARE_PARSER;
 		private Map<String, ?> properties = EMPTY_MAP;
+
+		// Set this to false if you need to parse a log4j.xml file (log4j.xml has an attribute containing the colon character)
+		private boolean useNamespaceAwareParser = USE_NAMESPACE_AWARE_PARSER;
+
+		// This flag switches the service to use EclipseLink MOXy instead of the JAXB reference implementation that ships with the JDK
+		// The *reason* for defaulting the service to MOXy is there is a pretty serious bug in the reference implementation.
+		// The RI throws an NPE anytime an adapter adapts a non-null bound value to null.
+		// The jira for this issue is -> https://java.net/jira/browse/JAXB-415 (As of Aug 24 2013
+		// There are two pretty detailed discussions of this issue and how to get around it on stackoverflow
+		// http://stackoverflow.com/questions/11894193/jaxb-marshal-empty-string-to-null-globally
+		// http://stackoverflow.com/questions/6110612/how-to-prevent-marshalling-empty-tags-in-jaxb-when-string-is-empty-but-not-null
+		private boolean useEclipseLinkMoxyProvider = USE_ECLIPSE_LINK_MOXY_PROVIDER;
 
 		public Builder formatOutput(boolean formatOutput) {
 			this.formatOutput = formatOutput;
--- kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/issue415/JAXBIssue415Test.java
+++ kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/issue415/JAXBIssue415Test.java
@@ -9,33 +9,28 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
+import org.junit.Test;
+
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class JAXBIssue415Test {
 
 	public JAXBIssue415Test() {
-		this(null);
-	}
-
-	public JAXBIssue415Test(String string) {
-		this.string = string;
+		this.string = "foo";
 	}
 
 	@XmlElement
 	@XmlJavaTypeAdapter(JAXBIssue415TestAdapter.class)
 	private String string;
 
-	public static void main(String[] args) {
-		try {
-			Marshaller m = JAXBContext.newInstance(JAXBIssue415Test.class).createMarshaller();
-			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			m.marshal(new JAXBIssue415Test("foo"), System.out);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	@Test
+	public void testIssue415() throws Exception {
+		Marshaller m = JAXBContext.newInstance(JAXBIssue415Test.class).createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		m.marshal(new JAXBIssue415Test(), System.out);
 	}
 
-	public class JAXBIssue415TestAdapter extends XmlAdapter<String, String> {
+	public static class JAXBIssue415TestAdapter extends XmlAdapter<String, String> {
 
 		@Override
 		public String unmarshal(String v) {
