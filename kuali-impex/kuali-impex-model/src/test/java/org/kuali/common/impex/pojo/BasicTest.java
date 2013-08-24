caed165bde40b5972e47bbd0f77c8b12ac3bfc5f
jcaddel <jcaddel@kuali.org>
2013-08-24 15:17:44 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25552 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/pojo/BasicTest.java
+++ kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/pojo/BasicTest.java
@@ -6,8 +6,10 @@ public class BasicTest {
 
 	public static void main(String[] args) {
 		try {
-			String version = System.getProperty("java.runtime.version");
-			System.out.println(version);
+			String os = System.getProperty("os.name") + ", " + System.getProperty("os.version");
+			String jdk = System.getProperty("java.vm.name") + ", " + System.getProperty("java.runtime.version");
+			System.out.println(os);
+			System.out.println(jdk);
 			String encoding = "UTF-8";
 			JAXBXmlService service = new JAXBXmlService.Builder().useEclipseLinkMoxyProvider(true).build();
 			Column column = new Column.Builder("column", DataType.FLOAT, new DataTypeSize(10, 2)).build();
