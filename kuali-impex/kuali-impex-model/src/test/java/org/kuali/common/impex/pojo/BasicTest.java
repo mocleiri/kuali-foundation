cc16635f5538fc0b33ae5218c1a4b53aef97687b
jcaddel <jcaddel@kuali.org>
2013-08-24 01:53:15 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25508 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/pojo/BasicTest.java
+++ kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/pojo/BasicTest.java
@@ -2,6 +2,8 @@ package org.kuali.common.impex.pojo;
 
 import org.kuali.common.util.xml.DefaultXmlService;
 
+import com.google.common.base.Optional;
+
 public class BasicTest {
 
 	public static void main(String[] args) {
@@ -10,7 +12,8 @@ public class BasicTest {
 			System.out.println(version);
 			String encoding = "UTF-8";
 			DefaultXmlService service = new DefaultXmlService();
-			Column column = new Column.Builder("column", DataType.BIT, new DataTypeSize(0)).build();
+			Optional<Integer> scale = Optional.<Integer> of(2);
+			Column column = new Column.Builder("column", DataType.FLOAT, new DataTypeSize(10, scale)).build();
 			Table table = new Table("table", column);
 			Schema schema = new Schema.Builder("schema").table(table).build();
 			String xml = service.toXml(schema, encoding);
@@ -19,5 +22,4 @@ public class BasicTest {
 			e.printStackTrace();
 		}
 	}
-
 }
