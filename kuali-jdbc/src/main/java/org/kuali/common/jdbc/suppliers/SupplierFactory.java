959a0ae38fc224f5f439fc8d8b899736a3252455
jcaddel <jcaddel@kuali.org>
2013-08-24 16:06:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25555 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/suppliers/SupplierFactory.java
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/suppliers/SupplierFactory.java
@@ -1,6 +1,8 @@
 package org.kuali.common.jdbc.suppliers;
 
-
+/**
+ * Get an appropriate supplier based on information contained in the location (eg file extension)
+ */
 public interface SupplierFactory {
 
 	SqlSupplier getSupplier(String location);
