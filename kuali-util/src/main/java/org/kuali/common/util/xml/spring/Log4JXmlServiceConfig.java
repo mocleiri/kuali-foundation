85a244d15149843b300efb6a813103703a2d7d47
jcaddel <jcaddel@kuali.org>
2013-08-24 10:39:45 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25519 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/xml/spring/Log4JXmlServiceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/xml/spring/Log4JXmlServiceConfig.java
@@ -5,6 +5,10 @@ import org.kuali.common.util.xml.XmlService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * The XML log4j knows how to parse has element and attribute names that contain the colon character. A colon is almost universally used to represent a namespace prefix, but in the
+ * XML for log4j, the colon is part of the node name.
+ */
 @Configuration
 public class Log4JXmlServiceConfig {
 
