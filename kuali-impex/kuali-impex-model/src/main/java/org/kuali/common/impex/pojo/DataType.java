6d41353665ea9c40ece304a3706a78279b1a88bf
jcaddel <jcaddel@kuali.org>
2013-08-25 10:49:52 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25611 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/DataType.java
+++ kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/DataType.java
@@ -4,4 +4,21 @@ public enum DataType {
 
 	DATE, TIMESTAMP, CLOB, STRING, CHAR, FLOAT, INTEGER, BIT, BLOB;
 
+	public boolean isSizeable(DataType type) {
+		switch (type) {
+		case DATE:
+		case TIMESTAMP:
+		case BIT:
+			return false;
+		default:
+			return true;
+		}
+	}
+
+	// scale == decimal digits, aka number of digits after the decimal point.
+	// In simplified data types used in Kuali apps, scale only has meaning with FLOAT
+	public boolean isScalable(DataType type) {
+		return FLOAT.equals(type);
+	}
+
 }
--- kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/pojo/BasicTest.java
+++ kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/pojo/BasicTest.java
@@ -1,7 +1,5 @@
 package org.kuali.common.impex.pojo;
 
-import java.util.Arrays;
-
 import org.kuali.common.util.Assert;
 import org.kuali.common.util.xml.jaxb.JAXBXmlService;
 
@@ -18,9 +16,9 @@ public class BasicTest {
 			String desc = "<metadata>\n<yo>12</yo>\n</metadata>";
 			Column column1 = new Column.Builder("ID", DataType.FLOAT).size(10).scale(2).defaultValue("99").primaryKey(true).nullable(false).description(desc).build();
 			Column column2 = new Column.Builder("CREATED", DataType.TIMESTAMP).defaultValue("NOW()").nullable(false).description(desc).build();
-			UniqueConstraint uc1 = new UniqueConstraint("KS_VERSION_U1", Arrays.asList(column1.getName(), column2.getName()));
-			UniqueConstraint uc2 = new UniqueConstraint("KS_VERSION_U1", Arrays.asList(column1.getName(), column2.getName()));
-			Table table = new Table.Builder("VERSION").columns(Arrays.asList(column1, column2)).uniqueConstraints(uc1).build();
+			UniqueConstraint uc1 = new UniqueConstraint("KS_VERSION_U1", column1.getName(), column2.getName());
+			UniqueConstraint uc2 = new UniqueConstraint("KS_VERSION_U2", column1.getName(), column2.getName());
+			Table table = new Table.Builder("VERSION").columns(column1, column2).uniqueConstraints(uc1, uc2).build();
 			Schema schema = new Schema.Builder("KS").table(table).build();
 			String xml = service.toXml(schema, encoding);
 			System.out.println(xml);
