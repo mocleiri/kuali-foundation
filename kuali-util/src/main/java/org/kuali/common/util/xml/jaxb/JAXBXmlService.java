b3946db712a8fe5872609879af77ef2329be2760
jcaddel <jcaddel@kuali.org>
2013-08-24 20:20:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25573 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/JAXBXmlService.java
+++ kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/JAXBXmlService.java
@@ -286,4 +286,19 @@ public class JAXBXmlService implements XmlService {
 		return useEclipseLinkMoxyProvider;
 	}
 
+	private static class UseEclipseLinkMoxyProvider {
+
+		/**
+		 * <p>
+		 * The only purpose for this class is to make JAXB bootstrap itself with the jaxb.properties file in this directory and thus switch from the reference implementation to the
+		 * EclipseLink MOXy implementation.
+		 * </p>
+		 * 
+		 * <p>
+		 * If issue https://java.net/jira/browse/JAXB-415 gets resolved, this class and the corresponding jaxb.properties file can be removed, and the useEclipseLinkMoxyProvider
+		 * flag can be defaulted to false.
+		 * </p>
+		 */
+	}
+
 }
