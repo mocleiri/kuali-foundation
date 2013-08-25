ed7c08658f7c5fc092ddde568887bc5079821675
jcaddel <jcaddel@kuali.org>
2013-08-25 19:29:05 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25690 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/adapter/ImmutableListAdapter.java
+++ kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/adapter/ImmutableListAdapter.java
@@ -7,6 +7,8 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 import org.kuali.common.util.xml.jaxb.wrapper.ListWrapper;
 
+import com.google.common.collect.ImmutableList;
+
 public class ImmutableListAdapter<T> extends XmlAdapter<ListWrapper<T>, List<T>> {
 
 	private final List<T> EMPTY_LIST = Collections.emptyList();
@@ -14,25 +16,19 @@ public class ImmutableListAdapter<T> extends XmlAdapter<ListWrapper<T>, List<T>>
 
 	@Override
 	public ListWrapper<T> marshal(List<T> list) {
-		System.out.println("1");
 		if (isEmpty(list)) {
-			System.out.println("2");
 			return EMPTY_WRAPPER;
 		} else {
-			System.out.println("3");
 			return new ListWrapper<T>(list);
 		}
 	}
 
 	@Override
 	public List<T> unmarshal(ListWrapper<T> wrapper) {
-		System.out.println("4");
 		if (isEmpty(wrapper.getList())) {
-			System.out.println("5");
 			return EMPTY_LIST;
 		} else {
-			System.out.println("6");
-			return Collections.unmodifiableList(wrapper.getList());
+			return ImmutableList.copyOf(wrapper.getList());
 		}
 	}
 
