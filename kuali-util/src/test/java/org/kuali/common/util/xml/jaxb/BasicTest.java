b2cf61b500108b5f47a0251853343e9c82a6cdc7
jcaddel <jcaddel@kuali.org>
2013-08-25 13:22:57 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25640 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/BasicTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/BasicTest.java
@@ -12,15 +12,18 @@ public class BasicTest {
 			System.out.println(jdk);
 			String encoding = "UTF-8";
 			JAXBXmlService service = new JAXBXmlService.Builder().useEclipseLinkMoxyProvider(true).build();
-			Sport soccer = new Sport("soccer", Arrays.asList("galaxy"));
-			Sport football = new Sport("football", Arrays.asList("raiders"));
-			Team galaxy = new Team("galaxy", "soccer", "Jeff", "Joe");
-			Team raiders = new Team("raiders", "football", "Jeff");
-			Student jeff = new Student("Jeff", Arrays.asList(galaxy.getName(), raiders.getName()), Arrays.asList(soccer.getName(), football.getName()));
-			Student joe = new Student("Joe", Arrays.asList(galaxy.getName()), Arrays.asList(soccer.getName()));
-			University uni = new University(Arrays.asList(joe, jeff), Arrays.asList(soccer, football), Arrays.asList(galaxy, raiders));
-			String xml = service.toXml(uni, encoding);
-			System.out.println(xml);
+			Sport soccer = new Sport("soccer", "galaxy");
+			Sport football = new Sport("football", "raiders");
+			Team galaxy = new Team("galaxy", soccer.getName(), "Jeff", "Joe");
+			Team raiders = new Team("raiders", football.getName(), "Jeff");
+			Student jeff = new Student("Jeff", Arrays.asList(galaxy.getName(), raiders.getName()), soccer.getName(), football.getName());
+			Student joe = new Student("Joe", Arrays.asList(galaxy.getName()), soccer.getName());
+			University original = new University(Arrays.asList(joe, jeff), Arrays.asList(soccer, football), galaxy, raiders);
+			String originalMarshalled = service.toXml(original, encoding);
+			System.out.println(originalMarshalled);
+			University derived = service.getObjectFromXml(originalMarshalled, encoding, University.class);
+			String derivedMarshalled = service.toXml(derived, encoding);
+			System.out.println(derivedMarshalled);
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
