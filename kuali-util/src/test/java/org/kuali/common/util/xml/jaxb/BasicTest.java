bf54d51bf7533d2e847082b69781f040eefe04aa
jcaddel <jcaddel@kuali.org>
2013-08-25 12:53:31 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25635 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/BasicTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/BasicTest.java
@@ -1,6 +1,5 @@
 package org.kuali.common.util.xml.jaxb;
 
-import org.springframework.util.Assert;
 
 public class BasicTest {
 
@@ -12,15 +11,6 @@ public class BasicTest {
 			System.out.println(jdk);
 			String encoding = "UTF-8";
 			JAXBXmlService service = new JAXBXmlService.Builder().useEclipseLinkMoxyProvider(true).build();
-			Club club = new Club("Delta Tau", 15.0);
-			Sport soccer = new Sport("Soccer", 125, true, new Team("Manchester United", "blue", "joe", "jeff"));
-			Student student1 = new Student.Builder("joe").club(club).sport(soccer).ethnicity("caucasian").build();
-			String xml1 = service.toXml(student1, encoding);
-			System.out.println(xml1);
-			Student derived = service.getObjectFromXml(xml1, encoding, Student.class);
-			String xml2 = service.toXml(derived, "UTF-8");
-			System.out.println(xml2);
-			Assert.isTrue(xml1.equals(xml2), "xml is different");
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
--- kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/Sport.java
+++ kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/Sport.java
@@ -1,21 +1,13 @@
 package org.kuali.common.util.xml.jaxb;
 
-import java.util.List;
-
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import com.google.common.collect.ImmutableList;
 
 @XmlRootElement
 public final class Sport {
 
 	@XmlAttribute
 	private final String name;
-
-	@XmlElement
-	private final List<Team> teams;
 
 	public String getName() {
 		return name;
@@ -27,20 +19,7 @@ public final class Sport {
 	}
 
 	public Sport(String name) {
-		this(name, ImmutableList.<Team> of());
-	}
-
-	public Sport(String name, Team team) {
-		this(name, ImmutableList.of(team));
-	}
-
-	public Sport(String name, Team... teams) {
-		this(name, ImmutableList.copyOf(teams));
-	}
-
-	public Sport(String name, List<Team> teams) {
 		this.name = name;
-		this.teams = teams;
 	}
 
 }
