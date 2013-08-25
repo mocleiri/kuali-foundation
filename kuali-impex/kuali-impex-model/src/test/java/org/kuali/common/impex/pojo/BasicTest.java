b0fd326a85230cd5b96c32fa09edf0bf7fa422a8
jcaddel <jcaddel@kuali.org>
2013-08-25 09:36:05 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25608 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/pojo/BasicTest.java
+++ kuali-impex/kuali-impex-model/src/test/java/org/kuali/common/impex/pojo/BasicTest.java
@@ -1,5 +1,7 @@
 package org.kuali.common.impex.pojo;
 
+import java.util.Arrays;
+
 import org.kuali.common.util.Assert;
 import org.kuali.common.util.xml.jaxb.JAXBXmlService;
 
@@ -14,9 +16,10 @@ public class BasicTest {
 			String encoding = "UTF-8";
 			JAXBXmlService service = new JAXBXmlService.Builder().useEclipseLinkMoxyProvider(true).build();
 			String desc = "<metadata>\n<yo>12</yo>\n</metadata>";
-			Column column = new Column.Builder("ID", DataType.FLOAT).size(10).scale(2).defaultValue("99").primaryKey(true).nullable(false).description(desc).build();
-			UniqueConstraint uniqueConstraint = new UniqueConstraint("KS_VERSION_U1", column.getName());
-			Table table = new Table.Builder("VERSION").column(column).uniqueConstraint(uniqueConstraint).build();
+			Column column1 = new Column.Builder("ID", DataType.FLOAT).size(10).scale(2).defaultValue("99").primaryKey(true).nullable(false).description(desc).build();
+			Column column2 = new Column.Builder("CREATED", DataType.TIMESTAMP).defaultValue("NOW()").nullable(false).description(desc).build();
+			UniqueConstraint uniqueConstraint = new UniqueConstraint("KS_VERSION_U1", Arrays.asList(column1.getName(), column2.getName()));
+			Table table = new Table.Builder("VERSION").columns(Arrays.asList(column1, column2)).uniqueConstraint(uniqueConstraint).build();
 			Schema schema = new Schema.Builder("KS").table(table).build();
 			String xml = service.toXml(schema, encoding);
 			System.out.println(xml);
