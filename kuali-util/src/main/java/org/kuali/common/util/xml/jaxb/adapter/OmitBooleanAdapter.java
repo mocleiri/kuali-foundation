3386b9527d5208f16bec34d5942cb6ab4faac0af
jcaddel <jcaddel@kuali.org>
2013-08-25 14:22:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25660 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/adapter/OmitBooleanAdapter.java
+++ kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/adapter/OmitBooleanAdapter.java
@@ -2,7 +2,7 @@ package org.kuali.common.util.xml.jaxb.adapter;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public abstract class OmitBooleanAdapter extends XmlAdapter<Boolean, Boolean> {
+public abstract class OmitBooleanAdapter extends XmlAdapter<String, Boolean> {
 
 	public OmitBooleanAdapter(boolean omitValue) {
 		this.omitValue = omitValue;
@@ -11,20 +11,20 @@ public abstract class OmitBooleanAdapter extends XmlAdapter<Boolean, Boolean> {
 	private final Boolean omitValue;
 
 	@Override
-	public final Boolean marshal(Boolean value) {
+	public final String marshal(Boolean value) {
 		if (omitValue.equals(value)) {
 			return null;
 		} else {
-			return value;
+			return value.toString();
 		}
 	}
 
 	@Override
-	public final Boolean unmarshal(Boolean value) {
+	public final Boolean unmarshal(String value) {
 		if (value == null) {
 			return omitValue;
 		} else {
-			return value;
+			return Boolean.parseBoolean(value);
 		}
 	}
 
