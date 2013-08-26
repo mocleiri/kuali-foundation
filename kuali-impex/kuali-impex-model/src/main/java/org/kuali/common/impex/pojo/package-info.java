cd01936e8e56b69dd1c7da10a0fc6c12586ae839
jcaddel <jcaddel@kuali.org>
2013-08-26 09:24:45 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25739 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/package-info.java
+++ kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/package-info.java
@@ -2,7 +2,9 @@
  * Model POJO's for database constructs
  */
 @XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
+@XmlAccessorOrder(javax.xml.bind.annotation.XmlAccessOrder.ALPHABETICAL)
 package org.kuali.common.impex.pojo;
 
+import javax.xml.bind.annotation.XmlAccessorOrder;
 import javax.xml.bind.annotation.XmlAccessorType;
 
