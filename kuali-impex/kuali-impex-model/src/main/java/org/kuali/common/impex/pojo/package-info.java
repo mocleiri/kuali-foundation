15ca84d39288bc1f463b4ea5c50f9a06a20e64ac
jcaddel <jcaddel@kuali.org>
2013-08-26 10:54:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25761 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/package-info.java
+++ kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/package-info.java
@@ -2,6 +2,7 @@
  * Model POJO's for database constructs
  */
 @XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
+// JAXB's default is UNDEFINED.  This annotation makes the XML marshalling deterministic
 @XmlAccessorOrder(javax.xml.bind.annotation.XmlAccessOrder.ALPHABETICAL)
 package org.kuali.common.impex.pojo;
 
