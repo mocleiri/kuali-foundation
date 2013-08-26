4a9645bdf5ecf875431b0db7085fb6850656f776
jcaddel <jcaddel@kuali.org>
2013-08-26 11:47:41 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25762 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/package-info.java
+++ kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/package-info.java
@@ -3,6 +3,7 @@
  */
 @XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
 // JAXB's default is UNDEFINED.  This annotation makes the XML marshalling deterministic
+// The Schema object uses @XmlType + propOrder to override alphabetical for the top level database objects
 @XmlAccessorOrder(javax.xml.bind.annotation.XmlAccessOrder.ALPHABETICAL)
 package org.kuali.common.impex.pojo;
 
