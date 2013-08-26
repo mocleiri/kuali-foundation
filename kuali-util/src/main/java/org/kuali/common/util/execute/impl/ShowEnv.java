3610d66496a600ce1a7b298b25d863f13bbe90b0
jcaddel <jcaddel@kuali.org>
2013-08-26 09:53:57 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25742 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/execute/impl/ShowEnv.java
+++ kuali-util/src/main/java/org/kuali/common/util/execute/impl/ShowEnv.java
@@ -1,6 +1,10 @@
 package org.kuali.common.util.execute.impl;
 
+import java.nio.charset.Charset;
+import java.util.Locale;
+
 import org.kuali.common.util.execute.Executable;
+import org.kuali.common.util.file.CanonicalFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,27 +14,31 @@ public class ShowEnv implements Executable {
 		this(false);
 	}
 
-	public ShowEnv(boolean skip) {
-		this.skip = skip;
+	public ShowEnv(boolean automatic) {
+		this.automatic = automatic;
+		if (automatic) {
+			execute();
+		}
 	}
 
 	private static final Logger logger = LoggerFactory.getLogger(ShowEnv.class);
 
-	private final boolean skip;
+	private final boolean automatic;
 
 	@Override
 	public void execute() {
-
-		if (skip) {
-			return;
-		}
-
-		Object[] jdk = { System.getProperty("java.runtime.version"), System.getProperty("java.vm.vendor"), System.getProperty("java.vm.name") };
-		logger.info("Java version: {}, vendor: {}, name: {}", jdk);
+		Object[] java = { System.getProperty("java.runtime.version"), System.getProperty("java.vm.vendor"), System.getProperty("java.vm.name") };
+		Object[] javaHome = { new CanonicalFile(System.getProperty("java.home")) };
+		Object[] other = { Locale.getDefault().getDisplayName(), Charset.defaultCharset().displayName() };
+		Object[] os = { System.getProperty("os.name"), System.getProperty("os.version"), System.getProperty("os.arch") };
+		logger.info("Java version: {}, vendor: {}, name: {}", java);
+		logger.info("Java home: {}", javaHome);
+		logger.info("Default locale: {}, platform encoding: {}", other);
+		logger.info("OS name: {}, version: {}, arch: {}", os);
 	}
 
-	public boolean isSkip() {
-		return skip;
+	public boolean isAutomatic() {
+		return automatic;
 	}
 
 }
