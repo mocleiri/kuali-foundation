a0ee910d679fb3f3581434d2da4c9198e121c5b6
jcaddel <jcaddel@kuali.org>
2013-08-27 17:40:44 -0400

    [maven-release-plugin]  copy for tag kuali-pom-3.3.8
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-pom-3.3.8@25840 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/vendor/service/DatabaseVendorService.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common.jdbc.vendor.service;
-
-import org.kuali.common.jdbc.vendor.model.DatabaseVendor;
-
-public interface DatabaseVendorService {
-
-	DatabaseVendor getDatabaseVendor();
-
-}
