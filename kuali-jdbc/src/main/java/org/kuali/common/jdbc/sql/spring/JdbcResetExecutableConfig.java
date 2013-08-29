477598af34fdbdb837131da17a0c2742a2ab90af
jcaddel <jcaddel@kuali.org>
2013-08-28 20:12:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25895 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-jdbc/src/main/java/org/kuali/common/jdbc/sql/spring/JdbcResetExecutableConfig.java
@@ -0,0 +1,9 @@
+package org.kuali.common.jdbc.sql.spring;
+
+import org.kuali.common.util.execute.Executable;
+
+public interface JdbcResetExecutableConfig {
+
+	Executable jdbcResetExecutable();
+
+}
