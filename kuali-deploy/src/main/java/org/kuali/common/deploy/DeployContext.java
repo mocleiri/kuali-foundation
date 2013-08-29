66eead59642ab11c0d89f04bd9bab0b1dc15de5f
jcaddel <jcaddel@kuali.org>
2013-08-28 20:58:45 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25899 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/DeployContext.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/DeployContext.java
@@ -3,62 +3,50 @@ package org.kuali.common.deploy;
 import java.util.List;
 
 import org.kuali.common.util.Artifact;
+import org.kuali.common.util.Assert;
 
 public class DeployContext {
 
-	String environment;
-	String username;
-	String hostname;
-	Artifact application;
-	Artifact jdbcDriver;
-	List<Deployable> configFiles;
+	public DeployContext(String environment, String username, String hostname, Artifact application, Artifact jdbcDriver, List<Deployable> configFiles) {
+		Assert.noBlanks(environment, username, hostname);
+		Assert.noNulls(application, jdbcDriver, configFiles);
+		this.environment = environment;
+		this.username = username;
+		this.hostname = hostname;
+		this.application = application;
+		this.jdbcDriver = jdbcDriver;
+		this.configFiles = configFiles;
+	}
+
+	private final String environment;
+	private final String username;
+	private final String hostname;
+	private final Artifact application;
+	private final Artifact jdbcDriver;
+	private final List<Deployable> configFiles;
 
 	public String getEnvironment() {
 		return environment;
-	}
-
-	public void setEnvironment(String environment) {
-		this.environment = environment;
 	}
 
 	public String getUsername() {
 		return username;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
 	public String getHostname() {
 		return hostname;
-	}
-
-	public void setHostname(String hostname) {
-		this.hostname = hostname;
 	}
 
 	public Artifact getApplication() {
 		return application;
 	}
 
-	public void setApplication(Artifact application) {
-		this.application = application;
-	}
-
 	public Artifact getJdbcDriver() {
 		return jdbcDriver;
 	}
 
-	public void setJdbcDriver(Artifact jdbcDriver) {
-		this.jdbcDriver = jdbcDriver;
-	}
-
 	public List<Deployable> getConfigFiles() {
 		return configFiles;
-	}
-
-	public void setConfigFiles(List<Deployable> configFiles) {
-		this.configFiles = configFiles;
 	}
 
 }
