6b149f770686a3cd8dc44277d28685371804d3eb
jcaddel <jcaddel@kuali.org>
2013-08-30 18:10:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25967 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-maven/src/main/java/org/kuali/common/maven/spring/AugmentMavenPropertiesConfig.java
+++ kuali-maven/src/main/java/org/kuali/common/maven/spring/AugmentMavenPropertiesConfig.java
@@ -33,7 +33,7 @@ public class AugmentMavenPropertiesConfig {
 	@Autowired
 	MavenProject mavenProject;
 
-	@Autowired
+	@Autowired(required = false)
 	Settings settings;
 
 	@Bean(initMethod = "execute")
