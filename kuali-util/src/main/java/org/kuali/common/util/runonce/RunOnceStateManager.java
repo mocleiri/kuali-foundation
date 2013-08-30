91c773a91e62d2f38b1ee568523e4803f5fd5f2f
jcaddel <jcaddel@kuali.org>
2013-08-30 09:05:14 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25943 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/runonce/RunOnceStateManager.java
+++ kuali-util/src/main/java/org/kuali/common/util/runonce/RunOnceStateManager.java
@@ -2,10 +2,19 @@ package org.kuali.common.util.runonce;
 
 public interface RunOnceStateManager {
 
+	/**
+	 * Do whatever needs to be done in order for <code>isRunOnce()</code> to return the right value.
+	 */
 	void initialize();
 
+	/**
+	 * Return true if it is safe to run something once, false otherwise.
+	 */
 	boolean isRunOnce();
 
+	/**
+	 * Permanently set the run once state in such a way that it will survive a JVM re-start.
+	 */
 	void updateState(RunOnceState state);
 
 }
