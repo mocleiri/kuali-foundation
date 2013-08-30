cca8525bf761d448622d17205eb698724b5fa9b0
jcaddel <jcaddel@kuali.org>
2013-08-29 21:37:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@25928 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DeployPropertyLocationsConfig.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DeployPropertyLocationsConfig.java
@@ -1,45 +1,72 @@
 package org.kuali.common.deploy.spring;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import org.kuali.common.jdbc.project.JdbcProjectConstants;
-import org.kuali.common.util.project.model.ProjectIdentifier;
+import org.kuali.common.deploy.project.DeployProjectConstants;
+import org.kuali.common.util.project.ProjectService;
+import org.kuali.common.util.project.ProjectUtils;
+import org.kuali.common.util.project.model.Project;
+import org.kuali.common.util.project.spring.ProjectServiceConfig;
 import org.kuali.common.util.properties.Location;
-import org.kuali.common.util.properties.PropertiesLocationService;
-import org.kuali.common.util.properties.spring.PropertiesLocationServiceConfig;
+import org.kuali.common.util.spring.env.EnvironmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import com.google.common.collect.ImmutableList;
+
 @Configuration
-@Import({ PropertiesLocationServiceConfig.class })
+@Import({ ProjectServiceConfig.class })
 public class DeployPropertyLocationsConfig {
 
-	private static final ProjectIdentifier SQL = JdbcProjectConstants.KUALI_SQL;
-	private static final ProjectIdentifier JDBC = JdbcProjectConstants.PROJECT_ID;
+	@Autowired
+	EnvironmentService env;
 
 	@Autowired
-	PropertiesLocationService service;
+	ProjectService service;
 
 	@Bean
-	public List<Location> jdbcPropertyLocations() {
-		List<Location> locations = new ArrayList<Location>();
-		locations.addAll(service.getLocations(SQL, getSqlFilenames()));
-		locations.addAll(service.getLocations(JDBC, "config.properties"));
-		return Collections.unmodifiableList(locations);
+	public List<Location> deployPropertyLocations() {
+		Location kualiDefaults = getDeployDefaults();
+		Location groupDefaults = getGroupDefaults();
+		Location appDefaults = getApplicationDefaults();
+		Location envDefaults = getEnvDefaults();
+		return ImmutableList.of(kualiDefaults, groupDefaults, appDefaults, envDefaults);
 	}
 
-	protected List<String> getSqlFilenames() {
-		List<String> filenames = new ArrayList<String>();
-		filenames.add("derby.xml");
-		filenames.add("h2.xml");
-		filenames.add("mysql.xml");
-		filenames.add("oracle.xml");
-		filenames.add("sql.xml");
-		return filenames;
+	@Bean
+	public String kualiDeployEncoding() {
+		Project project = service.getProject(DeployProjectConstants.ID);
+		return ProjectUtils.getEncoding(project);
+	}
+
+	protected Location getEnvDefaults() {
+		String environmentNumber = env.getString("deploy.env");
+		String path = getGroupPrefix() + "/env" + environmentNumber + ".properties";
+		return new Location(path, kualiDeployEncoding(), true);
+	}
+
+	protected Location getApplicationDefaults() {
+		String artifactId = env.getString("project.artifactId");
+		String path = getGroupPrefix() + "/" + artifactId + ".properties";
+		return new Location(path, kualiDeployEncoding(), true);
+	}
+
+	protected String getGroupPrefix() {
+		String groupId = env.getString("project.groupId");
+		return ProjectUtils.getClasspathPrefix(groupId);
+	}
+
+	protected Location getGroupDefaults() {
+		String path = getGroupPrefix() + "/deploy.properties";
+		return new Location(path, kualiDeployEncoding(), true);
+	}
+
+	protected Location getDeployDefaults() {
+		String prefix = ProjectUtils.getClasspathPrefix(DeployProjectConstants.ID.getGroupId());
+		String path = prefix + "/deploy/deploy.properties";
+		return new Location(path, kualiDeployEncoding(), true);
 	}
 
 }
