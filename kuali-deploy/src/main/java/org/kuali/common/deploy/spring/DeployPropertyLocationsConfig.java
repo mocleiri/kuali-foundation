5563a87b0eb9a575e7df3ef2e13f8ac49bdd85c3
jcaddel <jcaddel@kuali.org>
2013-08-30 20:45:41 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26010 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DeployPropertyLocationsConfig.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DeployPropertyLocationsConfig.java
@@ -2,12 +2,7 @@ package org.kuali.common.deploy.spring;
 
 import java.util.List;
 
-import org.kuali.common.deploy.project.DeployProjectConstants;
-import org.kuali.common.util.project.ProjectService;
-import org.kuali.common.util.project.ProjectUtils;
 import org.kuali.common.util.project.model.Project;
-import org.kuali.common.util.project.spring.AutowiredProjectConfig;
-import org.kuali.common.util.project.spring.ProjectServiceConfig;
 import org.kuali.common.util.properties.Location;
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,62 +13,31 @@ import org.springframework.context.annotation.Import;
 import com.google.common.collect.ImmutableList;
 
 @Configuration
-@Import({ AutowiredProjectConfig.class, ProjectServiceConfig.class })
+@Import({ BasePropertyLocationsConfig.class })
 public class DeployPropertyLocationsConfig {
 
 	@Autowired
 	EnvironmentService env;
 
 	@Autowired
-	ProjectService service;
+	BasePropertyLocationsConfig config;
 
 	@Autowired
 	Project project;
 
 	@Bean
 	public List<Location> deployPropertyLocations() {
-		Location kualiDefaults = kualiDeployDefaults();
-		Location groupDefaults = kualiDeployGroupDefaults();
-		Location appDefaults = kualiDeployApplicationDefaults();
+		Location kualiDefaults = config.kualiDeployDefaults();
+		Location groupDefaults = config.kualiDeployGroupDefaults();
+		Location appDefaults = config.kualiDeployApplicationDefaults();
 		Location envDefaults = kualiDeployEnvDefaults();
 		return ImmutableList.of(kualiDefaults, groupDefaults, appDefaults, envDefaults);
 	}
 
 	@Bean
-	public String kualiDeployEncoding() {
-		Project project = service.getProject(DeployProjectConstants.ID);
-		return ProjectUtils.getEncoding(project);
-	}
-
-	@Bean
 	public Location kualiDeployEnvDefaults() {
 		String environmentNumber = env.getString("deploy.env");
-		String path = kualiDeployGroupPrefix() + "/env" + environmentNumber + ".properties";
-		return new Location(path, kualiDeployEncoding(), true);
+		String path = config.kualiDeployGroupPrefix() + "/env" + environmentNumber + ".properties";
+		return new Location(path, config.kualiDeployEncoding(), true);
 	}
-
-	@Bean
-	public Location kualiDeployApplicationDefaults() {
-		String path = kualiDeployGroupPrefix() + "/" + project.getArtifactId() + ".properties";
-		return new Location(path, kualiDeployEncoding(), true);
-	}
-
-	@Bean
-	public String kualiDeployGroupPrefix() {
-		return ProjectUtils.getClasspathPrefix(project.getGroupId());
-	}
-
-	@Bean
-	public Location kualiDeployGroupDefaults() {
-		String path = kualiDeployGroupPrefix() + "/deploy.properties";
-		return new Location(path, kualiDeployEncoding(), true);
-	}
-
-	@Bean
-	public Location kualiDeployDefaults() {
-		String prefix = ProjectUtils.getClasspathPrefix(DeployProjectConstants.ID.getGroupId());
-		String path = prefix + "/deploy/deploy.properties";
-		return new Location(path, kualiDeployEncoding(), true);
-	}
-
 }
