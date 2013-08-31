41d4faf4af87f9ee136063672cc0d8bd1740199f
jcaddel <jcaddel@kuali.org>
2013-08-31 18:42:33 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26041 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/spring/DefaultSecureChannelConfig.java
@@ -0,0 +1,38 @@
+package org.kuali.common.deploy.spring;
+
+import java.util.Arrays;
+
+import org.kuali.common.deploy.DeployContext;
+import org.kuali.common.util.secure.DefaultSecureChannel;
+import org.kuali.common.util.secure.SecureChannel;
+import org.kuali.common.util.spring.env.EnvironmentService;
+import org.kuali.common.util.spring.service.SpringServiceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ SpringServiceConfig.class, DefaultDeployContextConfig.class })
+public class DefaultSecureChannelConfig implements SecureChannelConfig {
+
+	@Autowired
+	DeployContext ctx;
+
+	@Autowired
+	EnvironmentService env;
+
+	@Override
+	@Bean
+	public SecureChannel secureChannel() {
+		DefaultSecureChannel dsc = new DefaultSecureChannel();
+		dsc.setUsername(ctx.getUsername());
+		dsc.setHostname(ctx.getHostname());
+		dsc.setStrictHostKeyChecking(env.getBoolean("kdo.channel.strictHostKeyChecking", false));
+		dsc.setUseConfigFile(env.getBoolean("kdo.channel.useConfigFile", false));
+		dsc.setIncludeDefaultPrivateKeyLocations(env.getBoolean("kdo.channel.includeDefaultPrivateKeyLocations", false));
+		dsc.setPrivateKeyStrings(Arrays.asList(env.getString("kdo.channel.privateKey")));
+		return dsc;
+	}
+
+}
---
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/spring/SecureChannelConfig.java
@@ -0,0 +1,9 @@
+package org.kuali.common.deploy.spring;
+
+import org.kuali.common.util.secure.SecureChannel;
+
+public interface SecureChannelConfig {
+
+	SecureChannel secureChannel();
+
+}
