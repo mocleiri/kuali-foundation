7d58917560c40bbc956f3d035b3c16c884e14421
jcaddel <jcaddel@kuali.org>
2013-08-31 18:13:14 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26036 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/spring/ApplicationServerConfig.java
@@ -0,0 +1,9 @@
+package org.kuali.common.deploy.spring;
+
+import org.kuali.common.deploy.ApplicationServer;
+
+public interface ApplicationServerConfig {
+
+	ApplicationServer applicationServer();
+
+}
