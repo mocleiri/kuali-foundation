e7a3aa905d5334f1bf2af67aa998cc620fc7df08
jcaddel <jcaddel@kuali.org>
2013-08-31 23:59:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26058 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/secure/spring/DefaultSecureChannelConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/secure/spring/DefaultSecureChannelConfig.java
@@ -1,6 +1,5 @@
 package org.kuali.common.util.secure.spring;
 
-import org.kuali.common.util.nullify.NullUtils;
 import org.kuali.common.util.secure.DefaultSecureChannel;
 import org.kuali.common.util.secure.SecureChannel;
 import org.kuali.common.util.spring.SpringUtils;
@@ -25,12 +24,24 @@ public class DefaultSecureChannelConfig implements SecureChannelConfig {
 	@Bean
 	public SecureChannel secureChannel() {
 		DefaultSecureChannel dsc = new DefaultSecureChannel();
+
+		// Setup username/hostname
 		dsc.setUsername(env.getString(USERNAME_KEY));
 		dsc.setHostname(env.getString(HOSTNAME_KEY));
+
+		// Turn off strict host key checking
 		dsc.setStrictHostKeyChecking(env.getBoolean("channel.strictHostKeyChecking", false));
+
+		// Turn off use of ~/.ssh/config
 		dsc.setUseConfigFile(env.getBoolean("channel.useConfigFile", false));
+
+		// Do not check file system locations for private keys
 		dsc.setIncludeDefaultPrivateKeyLocations(env.getBoolean("channel.includeDefaultPrivateKeyLocations", false));
-		dsc.setPrivateKeyStrings(SpringUtils.getNoneSensitiveListFromCSV(env, "channel.privateKeyStrings", NullUtils.NONE));
+
+		// They must supply at least one string representing a private key
+		dsc.setPrivateKeyStrings(SpringUtils.getNoneSensitiveListFromCSV(env, "channel.privateKeyStrings"));
+
+		// Return the configured channel
 		return dsc;
 	}
 
