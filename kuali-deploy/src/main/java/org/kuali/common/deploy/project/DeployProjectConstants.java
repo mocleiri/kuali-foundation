72d3bdc974bf6d80d55ca1b8fb1f875eded46309
jcaddel <jcaddel@kuali.org>
2013-09-01 17:14:38 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26089 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/project/DeployProjectConstants.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/project/DeployProjectConstants.java
@@ -1,12 +1,10 @@
 package org.kuali.common.deploy.project;
 
-import org.kuali.common.util.project.KualiProjectConstants;
+import org.kuali.common.util.project.KualiGroup;
 import org.kuali.common.util.project.model.ProjectIdentifier;
 
 public abstract class DeployProjectConstants {
 
-	private static final String GROUP_ID = KualiProjectConstants.COMMON_GROUP_ID;
-
-	public static final ProjectIdentifier ID = new ProjectIdentifier(GROUP_ID, "kuali-deploy");
+	public static final ProjectIdentifier ID = new ProjectIdentifier(KualiGroup.COMMON.getId(), "kuali-deploy");
 
 }
