0280eafc95a5a9f8ea4e032bcbfc330f0c9384ca
jcaddel <jcaddel@kuali.org>
2013-09-01 22:18:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26114 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/deploy/spring/DeployEnvironmentConfig.java
@@ -0,0 +1,9 @@
+package org.kuali.common.deploy.spring;
+
+import org.kuali.common.deploy.model.DeployEnvironment;
+
+public interface DeployEnvironmentConfig {
+
+	DeployEnvironment deployEnvironment();
+
+}
