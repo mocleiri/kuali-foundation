43c0f3579cb71c0494cc8b192fd365ccfe21960c
jcaddel <jcaddel@kuali.org>
2013-09-01 23:39:36 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26134 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/aws/spring/DefaultAwsContextConfig.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/aws/spring/DefaultAwsContextConfig.java
@@ -1,6 +1,8 @@
 package org.kuali.common.deploy.aws.spring;
 
 import org.kuali.common.deploy.aws.model.AwsContext;
+import org.kuali.common.deploy.aws.model.EC2Context;
+import org.kuali.common.deploy.aws.model.S3Context;
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,19 @@ public class DefaultAwsContextConfig implements AwsContextConfig {
 	@Bean
 	public AwsContext awsContext() {
 		return null;
+	}
+
+	protected EC2Context getEC2Context() {
+		String ami = env.getString("ec2.ami");
+		String securityGroup = env.getString("ec2.securityGroup");
+		String type = env.getString("ec2.type");
+		return new EC2Context(ami, securityGroup, type);
+	}
+
+	protected S3Context getS3Context() {
+		String accessKey = env.getString("s3.accessKey");
+		String secretKey = env.getString("s3.secretKey");
+		return new S3Context(accessKey, secretKey);
 	}
 
 }
