23a4f257bcf0e4c47bb030f1dd2a8dce95f0db35
jcaddel <jcaddel@kuali.org>
2013-09-02 13:26:39 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26153 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/dns/model/DnsUtils.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/dns/model/DnsUtils.java
@@ -4,6 +4,11 @@ import org.kuali.common.util.Assert;
 
 public class DnsUtils {
 
+	/**
+	 * <pre>
+	 *  env1, ks, kuali.org -> env1.ks.kuali.org
+	 * </pre>
+	 */
 	public static String getHostname(String prefix, String subdomain, String domain) {
 		Assert.noBlanks(prefix, subdomain, domain);
 		return prefix + "." + subdomain + "." + domain;
