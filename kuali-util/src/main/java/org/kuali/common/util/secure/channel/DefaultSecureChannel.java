dfd46afdeed601137f71cd559d949f551fb7ecb7
jcaddel <jcaddel@kuali.org>
2013-09-02 13:33:16 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26154 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/secure/channel/DefaultSecureChannel.java
+++ kuali-util/src/main/java/org/kuali/common/util/secure/channel/DefaultSecureChannel.java
@@ -111,7 +111,7 @@ public final class DefaultSecureChannel implements SecureChannel {
 
 	@Override
 	public Result executeCommand(String command, String stdin) {
-		Assert.notBlank(command);
+		Assert.noBlanks(command);
 		ChannelExec exec = null;
 		InputStream stdoutStream = null;
 		ByteArrayOutputStream stderrStream = null;
