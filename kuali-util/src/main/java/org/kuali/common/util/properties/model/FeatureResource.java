6ee9d178cd997e481861e395267b500848b1e4bf
jcaddel <jcaddel@kuali.org>
2013-09-03 11:55:08 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26201 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/properties/model/FeatureResource.java
@@ -0,0 +1,11 @@
+package org.kuali.common.util.properties.model;
+
+import org.kuali.common.util.project.model.FeatureIdentifier;
+
+public interface FeatureResource {
+
+	FeatureIdentifier getFeatureIdentifier();
+
+	String getFilename();
+
+}
---
+++ kuali-util/src/main/java/org/kuali/common/util/properties/model/ProjectResource.java
@@ -0,0 +1,11 @@
+package org.kuali.common.util.properties.model;
+
+import org.kuali.common.util.project.model.ProjectIdentifier;
+
+public interface ProjectResource {
+
+	ProjectIdentifier getProjectIdentifier();
+
+	String getFilename();
+
+}
