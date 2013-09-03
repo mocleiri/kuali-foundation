5deba6424780f2ef06cc37e18cf594068b2c70f0
thrclark <thrclark@kuali.org>
2013-09-03 11:22:53 -0400

    UXI-8 adding menu icons
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@26199 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b/themes/kc/inc/doc-subnav.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b/themes/kc/inc/doc-subnav.php
@@ -1,6 +1,6 @@
 <div class="uif-navigation">
 	<ul>
-		<li <?php if ($section == "basics") { echo 'class="expanded"'; } ?>><a href="#">Basics</a>
+		<li <?php if ($section == "basics") { echo 'class="expanded"'; } ?>> <a href="#"><span aria-hidden="true" class="icon-file-alt"></span> Basics</a>
 			<ul>
 				<li <?php if ($page == "basics-details") { echo 'class="active"'; } ?>><a href="prop.basics.details.php">Proposal Details</a></li>
 				<li <?php if ($page == "basics-search") { echo 'class="active"'; } ?>><a href="prop.basics.oppsearch-search.php">Opportunity Search</a></li>
@@ -10,15 +10,15 @@
 				<li <?php if ($page == "basics-keywords") { echo 'class="active"'; } ?>><a href="prop.basics.keywords.php">Keywords</a></li>
 			</ul>
 		</li>
-		<li <?php if ($section == "keypersonnel") { echo 'class="expanded"'; } ?>><a href="#">Key Pesonnel</a>
+		<li <?php if ($section == "keypersonnel") { echo 'class="expanded"'; } ?>><a href="#"><span aria-hidden="true" class="icon-user"></span> Key Pesonnel</a>
 			<ul>
 				<li <?php if ($page == "keypersonnel-start") { echo 'class="active"'; } ?>><a href="prop.keypersonnel.start.php">Personnel</a></li>
 				<li <?php if ($page == "keypersonnel-intelcredit") { echo 'class="active"'; } ?>><a href="prop.keypersonnel.creditintel.php">Intellectual Credit</a></li>
 				<li <?php if ($page == "keypersonnel-facredit") { echo 'class="active"'; } ?>><a href="prop.keypersonnel.creditfa.php">F&amp:A Credit</a></li>
 			</ul>
 		</li>
-		<li <?php if ($page == "compliance") { echo 'class="active"'; } ?>><a href="prop.compliance.php">Compliance</a></li>
-		<li <?php if ($section == "attachments") { echo 'class="expanded"'; } ?>><a href="#">Attachments</a>
+		<li <?php if ($page == "compliance") { echo 'class="active"'; } ?>><a href="prop.compliance.php"><span aria-hidden="true" class="icon-ok"></span> Compliance</a></li>
+		<li <?php if ($section == "attachments") { echo 'class="expanded"'; } ?>><a href="#"><span aria-hidden="true" class="icon-paper-clip"></span> Attachments</a>
 			<ul>
 				<li <?php if ($page == "attach-proposal") { echo 'class="active"'; } ?>><a href="prop.attachments.proposal.php">Proposal</a></li>
 				<li <?php if ($page == "attach-personnel") { echo 'class="active"'; } ?>><a href="prop.attachments.personnel.php">Personnel</a></li>
@@ -27,8 +27,8 @@
 				<li <?php if ($page == "attach-notes") { echo 'class="active"'; } ?>><a href="prop.attachments.notes.php">Notes</a></li>
 			</ul>
 		</li>
-		<li <?php if ($page == "questions") { echo 'class="active"'; } ?>><a href="prop.questionnaire.php">Questionnaire</a></li>
-		<li <?php if ($section == "budget") { echo 'class="expanded"'; } ?>><a href="#">Budget</a>
+		<li <?php if ($page == "questions") { echo 'class="active"'; } ?>><a href="prop.questionnaire.php"><span aria-hidden="true" class="icon-question"></span> Questionnaire</a></li>
+		<li <?php if ($section == "budget") { echo 'class="expanded"'; } ?>><a href="#"><span aria-hidden="true" class="icon-money"></span> Budget</a>
 			<ul>
 				<li <?php if ($page == "budget-summary") { echo 'class="active"'; } ?>><a href="prop.budget.summary.php">Summary</a></li>
 				<li <?php if ($page == "budget-params") { echo 'class="active"'; } ?>><a href="prop.budget.params.php">Parameters</a></li>
@@ -40,13 +40,13 @@
 				<li <?php if ($page == "budget-actions") { echo 'class="active"'; } ?>><a href="prop.budget.actions.php">Actions</a></li>
 			</ul>
 		</li>
-		<li <?php if ($section == "instdata") { echo 'class="expanded"'; } ?>><a href="#">Institution Data</a>
+		<li <?php if ($section == "instdata") { echo 'class="expanded"'; } ?>><a href="#"><span aria-hidden="true" class="icon-bell"></span> Institution Data</a>
 			<ul>
 				<li <?php if ($page == "institution-page1") { echo 'class="active"'; } ?>><a href="prop.inst.1.php">Page 1</a></li>
 				<li <?php if ($page == "institution-page2") { echo 'class="active"'; } ?>><a href="prop.inst.2.php">Page 2</a></li>
 				<li <?php if ($page == "institution-page3") { echo 'class="active"'; } ?>><a href="prop.inst.3.php">Page 3</a></li>
 			</ul>
 		</li>
-		<li <?php if ($page == "prop-summary") { echo 'class="active"'; } ?>><a href="prop.summary.php">Summary/Submit</a></li>
+		<li <?php if ($page == "prop-summary") { echo 'class="active"'; } ?>><a href="prop.summary.php"><span aria-hidden="true" class="icon-signout"></span> Summary/Submit</a></li>
 	</ul>
 </div>
