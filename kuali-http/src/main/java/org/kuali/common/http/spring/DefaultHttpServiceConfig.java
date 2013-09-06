a3c38ee15d909e9b56fa9295630e92e468584a0d
jcaddel <jcaddel@kuali.org>
2013-09-05 20:43:47 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26237 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-http/src/main/java/org/kuali/common/http/spring/DefaultHttpServiceConfig.java
+++ kuali-http/src/main/java/org/kuali/common/http/spring/DefaultHttpServiceConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DefaultHttpServiceConfig {
+public class DefaultHttpServiceConfig implements HttpServiceConfig {
 
+	@Override
 	@Bean
 	public HttpService httpService() {
 		return new DefaultHttpService();
