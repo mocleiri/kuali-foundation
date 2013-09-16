1aee924604acefbb7a2a36dc87520c33952ffb9c
jcaddel <jcaddel@kuali.org>
2013-09-16 19:13:39 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26340 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/pojo/Instance.java
@@ -0,0 +1,10 @@
+package org.kuali.common.util.pojo;
+
+import java.util.Map;
+
+public class Instance {
+
+	Reservation reservation;
+	Map<String, String> properties;
+
+}
---
+++ kuali-util/src/test/java/org/kuali/common/util/pojo/Reservation.java
@@ -0,0 +1,9 @@
+package org.kuali.common.util.pojo;
+
+import java.util.Map;
+
+public class Reservation {
+
+	Map<String, String> properties;
+
+}
