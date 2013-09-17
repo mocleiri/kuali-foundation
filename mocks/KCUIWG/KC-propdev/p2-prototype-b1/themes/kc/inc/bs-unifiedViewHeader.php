808865f0ff6c274da574e0e8fa3e4dbe44c9a8ce
thrclark <thrclark@kuali.org>
2013-09-17 11:28:07 -0400

    KULRICE-10292 Metadata dropdown widget added to prototypes
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@26350 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/bs-unifiedViewHeader.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/bs-unifiedViewHeader.php
@@ -14,7 +14,39 @@
           <span class="icon-text">help</span></span></a> 
           <span class="supportTitle">Custom Page Title</span> </h1>
         </div>
-        <div class="col-md-6"> <span class="metadata"> metadata information </span> </div>
+        <div class="col-md-6"> <div class="metadata">
+              <div class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">metadata</a>
+                <div class="dropdown-menu" >
+                  <h4>Header</h4>
+                  <table class="table table-condensed">
+                    <tr>
+                      <th>Label</th>
+                      <td>Item </td>
+                    </tr>
+                    <tr>
+                      <th>Label</th>
+                      <td>Item </td>
+                    </tr>
+                    <tr>
+                      <th>Label</th>
+                      <td>Item </td>
+                    </tr>
+                    <tr>
+                      <th>Label</th>
+                      <td>Item </td>
+                    </tr>
+                    <tr>
+                      <th>Label</th>
+                      <td>Item </td>
+                    </tr>
+                    <tr>
+                      <th>Label</th>
+                      <td>Item </td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
+            </div> </div>
       </div>
     </header>
   </div>
