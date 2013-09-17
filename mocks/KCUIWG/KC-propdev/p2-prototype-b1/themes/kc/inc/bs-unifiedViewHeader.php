683a376029862ff9007961c3f8bc7ed4fa3ead1b
thrclark <thrclark@kuali.org>
2013-09-17 15:47:55 -0400

    KULRICE-10291 - Document info section refactored
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@26362 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/bs-unifiedViewHeader.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/bs-unifiedViewHeader.php
@@ -1,57 +1,61 @@
 
-  
-  
-
-  
-  <div class="container-fluid"> <!--KRAD - needs to be configurable between 'container-fluid' and 'container'-->
-    <header class="viewHeader">
-      <div class="row">
-        <div class="col-md-6">
-          <h1> 
-          <span class="areaTitle">Proposal Development</span> 
-          <span class="headerText"> Proposal: #23533 </span> 
-          <a href="#"><span aria-hidden="false" class="icon-question-sign">
-          <span class="icon-text">help</span></span></a> 
-          <span class="supportTitle">Custom Page Title</span> </h1>
-        </div>
-        <div class="col-md-6"> <div class="metadata">
-              <div class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">metadata</a>
+<div class="container-fluid"> <!--KRAD - needs to be configurable between 'container-fluid' and 'container'-->
+  <header class="viewHeader">
+    <div class="row">
+      <div class="col-md-6">
+        <h1> <span class="areaTitle">Proposal Development</span> <span class="headerText"> Proposal: #23533 </span> <a href="#"><span aria-hidden="false" class="icon-question-sign"> <span class="icon-text">help</span></span></a> <span class="supportTitle">Custom Page Title</span> </h1>
+      </div>
+      <div class="col-md-6">
+        <div class="metadata">
+              <div class="dropdown"><strong>Document Info</strong> (<a href="#" class="dropdown-toggle" data-toggle="dropdown">more...</a>)
                 <div class="dropdown-menu" >
-                  <h4>Header</h4>
+                  <h4> Document Info</h4>
                   <table class="table table-condensed">
-                    <tr>
-                      <th>Label</th>
-                      <td>Item </td>
-                    </tr>
-                    <tr>
-                      <th>Label</th>
-                      <td>Item </td>
-                    </tr>
-                    <tr>
-                      <th>Label</th>
-                      <td>Item </td>
-                    </tr>
-                    <tr>
-                      <th>Label</th>
-                      <td>Item </td>
-                    </tr>
-                    <tr>
-                      <th>Label</th>
-                      <td>Item </td>
-                    </tr>
-                    <tr>
-                      <th>Label</th>
-                      <td>Item </td>
-                    </tr>
+                    <tbody>
+                      <tr>
+                        <th scope="row">PI</th>
+                        <td>Ken Graves</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Progress</th>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Created</th>
+                        <td>04:27pm 07/09/2013</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Updated</th>
+                        <td>12:22pm 07/12/2013</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Doc Nbr</th>
+                        <td>2743</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Proposal Nbr</th>
+                        <td>4</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Initiator</th>
+                        <td><a href="#">thrclark</a></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Sponsor Name</th>
+                        <td>NIH</td>
+                      </tr>
+                    </tbody>
                   </table>
                 </div>
               </div>
-            </div> </div>
+              <div class="metadata-featured" > PI: Ken Graves<br>
+                Initiator: <a href="#">thrclark</a><br>
+                Created:
+                04:27pm 07/09/2013<br>
+                Updated: 12:22pm 07/12/2013<br>
+              </div>
+            </div>
       </div>
-    </header>
-  </div>
-  
-  
-  
-  
-  
+    </div>
+  </header>
+</div>
