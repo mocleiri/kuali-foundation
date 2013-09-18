1447eeaefe0d33da94e8d3998cc72edc086989db
jcaddel <jcaddel@kuali.org>
2013-09-18 19:50:07 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26379 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/properties/model/rice/ConfigTest.java
@@ -0,0 +1,16 @@
+package org.kuali.common.util.properties.model.rice;
+
+import org.junit.Test;
+
+public class ConfigTest {
+
+	@Test
+	public void test() {
+		try {
+			System.out.println("hello world");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
