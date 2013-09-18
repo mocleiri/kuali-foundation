ea47d1cc3546874e7e02542d0c4317d368fd13bc
jcaddel <jcaddel@kuali.org>
2013-09-18 19:55:47 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26380 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/properties/model/rice/ConfigTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/properties/model/rice/ConfigTest.java
@@ -1,13 +1,21 @@
 package org.kuali.common.util.properties.model.rice;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+import org.kuali.common.util.xml.jaxb.JAXBXmlService;
+import org.kuali.common.util.xml.service.XmlService;
 
 public class ConfigTest {
 
 	@Test
 	public void test() {
 		try {
-			System.out.println("hello world");
+			Param param = new Param.Builder("foo").value("bar").build();
+			Config config = new Config(Arrays.asList(param));
+			XmlService service = new JAXBXmlService.Builder().build();
+			String xml = service.toXml(config, "UTF-8");
+			System.out.println(xml);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
