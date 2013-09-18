e5c3b07b91cc53c1c5501f4c6584b9a916c91a31
jcaddel <jcaddel@kuali.org>
2013-09-18 18:48:21 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26373 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/properties/model/rice/Param.java
@@ -0,0 +1,92 @@
+package org.kuali.common.util.properties.model.rice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+import org.kuali.common.util.Assert;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = { "value" })
+public class Param {
+
+	@XmlAttribute(required = true)
+	private final String name;
+
+	@XmlAttribute
+	private final boolean override;
+
+	@XmlAttribute
+	private final boolean random;
+
+	@XmlAttribute
+	private final boolean system;
+
+	@XmlValue
+	private final String value;
+
+	public String getName() {
+		return name;
+	}
+
+	public static class Builder {
+
+		// Required
+		private final String name;
+
+		// Optional
+		private boolean override = true;
+		private boolean random = false;
+		private boolean system = false;
+		private String value;
+
+		public Builder(String name) {
+			this.name = name;
+		}
+
+		public Builder override(boolean override) {
+			this.override = override;
+			return this;
+		}
+
+		public Builder random(boolean random) {
+			this.random = random;
+			return this;
+		}
+
+		public Builder system(boolean system) {
+			this.system = system;
+			return this;
+		}
+
+		public Builder value(String value) {
+			this.value = value;
+			return this;
+		}
+
+		private Builder initialized() {
+			return this;
+		}
+
+		public Param build() {
+			Assert.noBlanks(name);
+			return new Param(this);
+		}
+
+	}
+
+	private Param() {
+		this(new Builder(null).initialized());
+	}
+
+	private Param(Builder builder) {
+		this.name = builder.name;
+		this.override = builder.override;
+		this.random = builder.random;
+		this.system = builder.system;
+		this.value = builder.value;
+	}
+
+}
