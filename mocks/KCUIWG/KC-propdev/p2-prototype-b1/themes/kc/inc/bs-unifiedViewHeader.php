c2d0d0cfd75e3a075e6abf3a5014221aa98c5fd8
thrclark <thrclark@kuali.org>
2013-09-20 17:14:45 -0400

    UXI-2 unified view header cleanup
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@26396 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/bs-unifiedViewHeader.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/bs-unifiedViewHeader.php
@@ -1,60 +1,61 @@
 
-<div class="container-fluid"> <!--KRAD - needs to be configurable between 'container-fluid' and 'container'-->
   <header class="viewHeader">
     <div class="row">
       <div class="col-md-6">
-        <h1> <span class="areaTitle">Proposal Development</span> <span class="headerText"> Proposal: #23533 </span> <a href="#"><span aria-hidden="false" class="icon-question-sign"> <span class="icon-text">help</span></span></a> <span class="supportTitle">Custom Page Title</span> </h1>
+        <h1> <span class="areaTitle">Proposal Development</span> <span class="headerText"> Proposal: #23533 </span> <a href="#"><span aria-hidden="false" class="icon-question-sign"> <span class="icon-text">help</span></span></a> <span class="supportTitle">PI: Ken Graves</span> </h1>
       </div>
       <div class="col-md-6">
-        <div class="metadata">
-              <div class="dropdown"><strong>Document Info</strong> (<a href="#" class="dropdown-toggle" data-toggle="dropdown">more...</a>)
-                <div class="dropdown-menu" >
-                  <h4> Document Info</h4>
-                  <table class="table table-condensed">
-                    <tbody>
-                      <tr>
-                        <th scope="row">PI</th>
-                        <td>Ken Graves</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Progress</th>
-                        <td></td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Created</th>
-                        <td>04:27pm 07/09/2013</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Updated</th>
-                        <td>12:22pm 07/12/2013</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Doc Nbr</th>
-                        <td>2743</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Proposal Nbr</th>
-                        <td>4</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Initiator</th>
-                        <td><a href="#">thrclark</a></td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Sponsor Name</th>
-                        <td>NIH</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-              <div class="metadata-featured" > PI: Ken Graves<br>
-                Initiator: <a href="#">thrclark</a><br>
-                Created: 04:27pm 07/09/2013<br>
-             
+        <div class="metadata"><strong>Document Info</strong>
+          
+          <div class="metadata-featured" > 
+            Doc Nbr: 2743<br>
+            Initiator: thrclark<br>
+            Status: In Progress<br>
+            <div class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">more...</a>
+              <div class="dropdown-menu" >
+                <h4> Document Info</h4>
+                <table class="table table-condensed">
+                  <tbody>    <tr>
+                      <th scope="row">Doc Nbr</th>
+                      <td>2743</td>
+                    </tr><tr>
+                      <th scope="row">Initiator</th>
+                      <td><a href="#">thrclark</a></td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Status</th>
+                      <td>In Progress</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">PI</th>
+                      <td>Ken Graves</td>
+                    </tr>
+                    
+                    <tr>
+                      <th scope="row">Created</th>
+                      <td>04:27pm 07/09/2013</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Updated</th>
+                      <td>12:22pm 07/12/2013</td>
+                    </tr>
+                
+                    <tr>
+                      <th scope="row">Proposal Nbr</th>
+                      <td>#23533</td>
+                    </tr>
+                    
+                    <tr>
+                      <th scope="row">Sponsor Name</th>
+                      <td>NIH</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
+          </div>
+        </div>
       </div>
     </div>
   </header>
-</div>
+
