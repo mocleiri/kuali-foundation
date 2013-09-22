4fd3fa8474a74b75b5215a038fdb3ea4dfb384b2
jcaddel <jcaddel@kuali.org>
2013-09-22 16:56:53 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26408 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-liquibase/src/test/java/org/kuali/common/liquibase/SpringIntegrationTest.java
@@ -0,0 +1,16 @@
+package org.kuali.common.liquibase;
+
+import org.junit.Test;
+
+public class SpringIntegrationTest {
+
+	@Test
+	public void test() {
+		try {
+			System.out.println("hello world");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
