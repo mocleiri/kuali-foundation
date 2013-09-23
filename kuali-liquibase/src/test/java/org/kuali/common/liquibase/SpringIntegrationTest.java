05616bfb7b7b87ee30e630f313c9934ae7f92659
jcaddel <jcaddel@kuali.org>
2013-09-23 13:18:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26422 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-liquibase/src/test/java/org/kuali/common/liquibase/SpringIntegrationTest.java
+++ kuali-liquibase/src/test/java/org/kuali/common/liquibase/SpringIntegrationTest.java
@@ -1,13 +1,25 @@
 package org.kuali.common.liquibase;
 
-import org.junit.Test;
+import liquibase.integration.spring.SpringLiquibase;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { DefaultSpringLiquibaseConfig.class })
 public class SpringIntegrationTest {
+
+	@Autowired
+	SpringLiquibase springLiquibase;
 
 	@Test
 	public void test() {
 		try {
 			System.out.println("hello world");
+			springLiquibase.afterPropertiesSet();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
