0a5f7f7b9cff99598dc178a645e8afe5f012f5d1
jcaddel <jcaddel@kuali.org>
2013-09-23 14:00:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26429 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-liquibase/src/main/java/org/kuali/common/liquibase/spring/LiquibaseServiceConfig.java
+++ kuali-liquibase/src/main/java/org/kuali/common/liquibase/spring/LiquibaseServiceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class LiquibaseConfig {
+public class LiquibaseServiceConfig {
 
 	@Bean
 	public LiquibaseService liquibaseService() {
