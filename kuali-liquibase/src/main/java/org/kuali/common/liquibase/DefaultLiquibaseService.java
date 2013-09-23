b69ffa31362f3a3f82914dad2cb7737f7ce00d73
jcaddel <jcaddel@kuali.org>
2013-09-23 16:35:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26448 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-liquibase/src/main/java/org/kuali/common/liquibase/DefaultLiquibaseService.java
+++ kuali-liquibase/src/main/java/org/kuali/common/liquibase/DefaultLiquibaseService.java
@@ -8,31 +8,16 @@ import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
-import liquibase.integration.spring.SpringLiquibase;
 import liquibase.resource.ResourceAccessor;
 
 import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.jdbc.service.JdbcUtils;
 import org.kuali.common.util.CollectionUtils;
-import org.springframework.core.io.DefaultResourceLoader;
 
 public class DefaultLiquibaseService implements LiquibaseService {
 
 	@Override
 	public void update(LiquibaseContext context) {
-		SpringLiquibase springLiquibase = new SpringLiquibase();
-		springLiquibase.setChangeLog(context.getChangeLog());
-		springLiquibase.setDataSource(context.getDataSource());
-		springLiquibase.setContexts(CollectionUtils.asCSV(context.getContexts()));
-		springLiquibase.setResourceLoader(new DefaultResourceLoader());
-		try {
-			springLiquibase.afterPropertiesSet();
-		} catch (LiquibaseException e) {
-			throw new IllegalStateException("Unexpected Liquibase error", e);
-		}
-	}
-
-	protected void foo(LiquibaseContext context) {
 		String csv = StringUtils.trimToNull(CollectionUtils.asCSV(context.getContexts()));
 		Connection connection = null;
 		Liquibase liquibase = null;
