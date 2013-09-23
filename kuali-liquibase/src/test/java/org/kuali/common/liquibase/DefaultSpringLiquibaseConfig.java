644ff011f0d554c484e5c3ffc85c6a1ce56ad7c0
jcaddel <jcaddel@kuali.org>
2013-09-23 12:42:55 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26414 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-liquibase/src/test/java/org/kuali/common/liquibase/DefaultSpringLiquibaseConfig.java
+++ kuali-liquibase/src/test/java/org/kuali/common/liquibase/DefaultSpringLiquibaseConfig.java
@@ -2,10 +2,13 @@ package org.kuali.common.liquibase;
 
 import liquibase.integration.spring.SpringLiquibase;
 
+import org.kuali.common.jdbc.service.spring.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(DataSourceConfig.class)
 public class DefaultSpringLiquibaseConfig implements LiquibaseConfig {
 
 	@Override
