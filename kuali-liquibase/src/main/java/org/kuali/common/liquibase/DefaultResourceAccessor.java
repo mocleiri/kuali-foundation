b89b916b5b12d8e98e968dfc88dfaa7a1b10f8a1
jcaddel <jcaddel@kuali.org>
2013-09-23 16:34:11 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26446 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-liquibase/src/main/java/org/kuali/common/liquibase/DefaultResourceAccessor.java
+++ kuali-liquibase/src/main/java/org/kuali/common/liquibase/DefaultResourceAccessor.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.Vector;
 
 import liquibase.resource.ResourceAccessor;
 
 import org.kuali.common.util.LocationUtils;
 import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 public final class DefaultResourceAccessor implements ResourceAccessor {
@@ -22,7 +24,11 @@ public final class DefaultResourceAccessor implements ResourceAccessor {
 
 	@Override
 	public Enumeration<URL> getResources(String packageName) throws IOException {
-		return null;
+		Resource resource = LocationUtils.getResource(packageName);
+		URL url = resource.getURL();
+		Vector<URL> tmp = new Vector<URL>();
+		tmp.add(url);
+		return tmp.elements();
 	}
 
 	@Override
