417abe586423190e64759f05f423b9773fdcbe7f
clrux <clrux@kuali.org>
2013-09-25 16:26:22 -0400

    Questionnaire modal
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@26544 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b2/prop.questionnaire.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b2/prop.questionnaire.php
@@ -10,52 +10,50 @@ require_once( 'themes/kc/inc/toolbar.php' );
 
 <section id="main">
 
-		
 	<?php require_once( 'themes/kc/inc/uif-unifiedViewHeader.php' ); ?>
     <?php require_once( 'themes/kc/inc/uif-documentFunctions.php' ); ?>
-<div class="container"><div class="row-temp-disabled">
-
+	
+	<div class="container">
+		<div class="row-temp-disabled">
 			<div id="Uif-ViewContentWrapper" class="uif-viewContentWrapper">
 				<?php require_once( 'themes/kc/inc/doc-subnav.php' ); ?>
 			</div>
 
-		  <div id="Uif-PageContentWrapper" class="uif-pageContentWrapper closed"><div id="content" class="uif-page" tabindex="-1">
-				<!-- Main content goes here -->
-        
-        <div class="section-title">
-          <h3>Questionnaire</h3>
-        </div>
-        
-        
-        
-        <table class="table table-condensed">
-                <tr>
-                  <th>Source</th>
-                  <th>Status</th>
-                  <th>Action</th>
-          </tr>
-                <tr>
-                  <td>Grants.gov Questions</td>
-                  <td>completed</td>
-                  <td>  
-                  <a href="#" class="launch-modal" data-modal-page="modal/questionnaire.html">edit</a></td>
-                </tr>
-                <tr>
-                  <td>Proposal Questions</td>
-                  <td>incomplete</td>
-                 <td>  
-                  <a href="#" class="launch-modal" data-modal-page="modal/questionnaire.html">edit</a></td>
-                </tr>
-              </table>
+		  	<div id="Uif-PageContentWrapper" class="uif-pageContentWrapper closed">
+		  		<div id="content" class="uif-page" tabindex="-1">
+					<div class="section-title">
+          				<h3>Questionnaire</h3>
+        			</div>
 
-        <!-- // --> 
+        			<div class="well">
+        				<button class="btn btn-default launch-modal" href="#" data-modal-page="modal/questionnaire.html">Add source</button>
+        			</div>
         
-      </div>
-
+					<table class="table table-condensed">
+						<tr>
+							<th>Source</th>
+							<th>Status</th>
+							<th>Action</th>
+						</tr>
+						<tr>
+							<td>Grants.gov Questions</td>
+							<td>Completed</td>
+							<td>  
+								<a href="#" class="launch-modal" data-modal-page="modal/questionnaire.html">Edit</a>
+							</td>
+						</tr>
+						<tr>
+							<td>Proposal Questions</td>
+							<td>Incomplete</td>
+							<td>  
+								<a href="#" class="launch-modal" data-modal-page="modal/questionnaire.html">Edit</a>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
 		</div>
-
 	</div>
-
 </section>
 
 <div class="page-controls clearfix">
