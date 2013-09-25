1740c10fc88bb554aa4bcdb283a7d2908ddd8cdd
jcaddel <jcaddel@kuali.org>
2013-09-24 20:07:41 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26502 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/file/ListTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/file/ListTest.java
@@ -1,12 +1,24 @@
 package org.kuali.common.util.file;
 
+import java.io.File;
+import java.util.List;
+
 import org.junit.Test;
+import org.kuali.common.util.SimpleScanner;
 
 public class ListTest {
 
 	@Test
 	public void test() {
-		System.out.println("yo");
+		try {
+			File basedir = new File("/usr/local/sonatype-work/nexus/storage/ow2");
+			String includes = "**/**";
+			SimpleScanner scanner = new SimpleScanner(basedir, includes, null);
+			List<File> files = scanner.getFiles();
+			System.out.println(files.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
