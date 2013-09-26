d88d63de8b410b529a3f4ef1c8e31e832947b0d5
jcaddel <jcaddel@kuali.org>
2013-09-25 21:13:44 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26551 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/file/model/Repository.java
+++ kuali-util/src/test/java/org/kuali/common/util/file/model/Repository.java
@@ -13,10 +13,20 @@ public class Repository {
 		Assert.noNulls(files);
 		this.name = name;
 		this.files = ImmutableList.copyOf(files);
+		this.size = getTotalSize(files);
+	}
+
+	private long getTotalSize(List<RepoFile> files) {
+		long size = 0;
+		for (RepoFile file : files) {
+			size += file.getSize();
+		}
+		return size;
 	}
 
 	private final String name;
 	private List<RepoFile> files;
+	private long size;
 
 	public List<RepoFile> getFiles() {
 		return files;
@@ -28,6 +38,14 @@ public class Repository {
 
 	public String getName() {
 		return name;
+	}
+
+	public long getSize() {
+		return size;
+	}
+
+	public void setSize(long size) {
+		this.size = size;
 	}
 
 }
