8ca0aad941af78a5cd5a0299efc583a3006b453c
jcaddel <jcaddel@kuali.org>
2013-09-26 14:29:37 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26576 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/file/model/RepoArtifacts.java
+++ kuali-util/src/test/java/org/kuali/common/util/file/model/RepoArtifacts.java
@@ -4,15 +4,25 @@ import java.util.List;
 
 import com.google.common.collect.ImmutableList;
 
-public class RepoArtifacts {
+public final class RepoArtifacts {
 
 	public RepoArtifacts(Repository repository, List<Artifact> artifacts) {
 		this.repository = repository;
 		this.artifacts = ImmutableList.copyOf(artifacts);
+		this.size = getSize(artifacts);
+	}
+
+	private static long getSize(List<Artifact> artifacts) {
+		long size = 0;
+		for (Artifact artifact : artifacts) {
+			size += artifact.getFile().getSize();
+		}
+		return size;
 	}
 
 	private final Repository repository;
 	private final List<Artifact> artifacts;
+	private final long size;
 
 	public Repository getRepository() {
 		return repository;
@@ -20,5 +30,9 @@ public class RepoArtifacts {
 
 	public List<Artifact> getArtifacts() {
 		return artifacts;
+	}
+
+	public long getSize() {
+		return size;
 	}
 }
