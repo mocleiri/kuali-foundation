ea8b21e3ac1dc98da9c322be55bcd581c7f7f071
jcaddel <jcaddel@kuali.org>
2013-09-25 21:34:02 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26556 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/file/ListTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/file/ListTest.java
@@ -6,9 +6,9 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -29,42 +29,60 @@ public class ListTest {
 	@Test
 	public void getRepoListTest() {
 		try {
-			List<String> names = getRepoNames();
-			List<Repository> repos = new ArrayList<Repository>();
-			Set<String> paths = new HashSet<String>();
-			for (String name : names) {
-				String location = "classpath:repos/" + name + ".txt";
-				List<String> lines = LocationUtils.readLines(location);
-				List<RepoFile> files = getRepoFiles(lines);
-				for (RepoFile file : files) {
-					paths.add(file.getPath());
-				}
-				Repository repo = new Repository(name, files);
-				repos.add(repo);
-			}
-			Collections.sort(repos);
-			Collections.reverse(repos);
-			List<String> columns = ImmutableList.of("repo", "files", "size");
-			List<Object[]> rows = new ArrayList<Object[]>();
-			long totalFiles = 0;
-			long totalSize = 0;
-			for (Repository repo : repos) {
-				String count = FormatUtils.getCount(repo.getFiles().size());
-				String size = FormatUtils.getSize(repo.getSize());
-				totalSize += repo.getSize();
-				totalFiles += repo.getFiles().size();
-				Object[] row = { repo.getName(), count, size };
-				rows.add(row);
-			}
-			String tc = FormatUtils.getCount(totalFiles);
-			String ts = FormatUtils.getSize(totalSize);
-			rows.add(new Object[] { " ", " ", " " });
-			rows.add(new Object[] { "totals", tc, ts });
-			LoggerUtils.logTable("Repo Summary", columns, rows);
-			System.out.println("Unique Paths: " + FormatUtils.getCount(paths.size()));
+			List<Repository> repos = getRepoList();
+			logRepos(repos);
+			Set<String> paths = getPaths(repos);
+			System.out.println("Unique paths: " + FormatUtils.getCount(paths.size()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected Set<String> getPaths(List<Repository> repos) {
+		Set<String> paths = new TreeSet<String>();
+		for (Repository repo : repos) {
+			List<RepoFile> files = repo.getFiles();
+			for (RepoFile file : files) {
+				String path = file.getPath();
+				paths.add(path);
+			}
+		}
+		return paths;
+	}
+
+	protected List<Repository> getRepoList() {
+		List<String> names = getRepoNames();
+		List<Repository> repos = new ArrayList<Repository>();
+		for (String name : names) {
+			String location = "classpath:repos/" + name + ".txt";
+			List<String> lines = LocationUtils.readLines(location);
+			List<RepoFile> files = getRepoFiles(lines);
+			Repository repo = new Repository(name, files);
+			repos.add(repo);
+		}
+		return repos;
+	}
+
+	protected void logRepos(List<Repository> repos) {
+		Collections.sort(repos);
+		Collections.reverse(repos);
+		List<String> columns = ImmutableList.of("repo", "files", "size");
+		List<Object[]> rows = new ArrayList<Object[]>();
+		long totalFiles = 0;
+		long totalSize = 0;
+		for (Repository repo : repos) {
+			String count = FormatUtils.getCount(repo.getFiles().size());
+			String size = FormatUtils.getSize(repo.getSize());
+			totalSize += repo.getSize();
+			totalFiles += repo.getFiles().size();
+			Object[] row = { repo.getName(), count, size };
+			rows.add(row);
+		}
+		String tc = FormatUtils.getCount(totalFiles);
+		String ts = FormatUtils.getSize(totalSize);
+		rows.add(new Object[] { " ", " ", " " });
+		rows.add(new Object[] { "totals", tc, ts });
+		LoggerUtils.logTable("Repo Summary", columns, rows);
 	}
 
 	protected List<RepoFile> getRepoFiles(List<String> lines) {
