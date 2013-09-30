10d3cffa600deb5422d8a3df2fd8e897ef0d490d
jcaddel <jcaddel@kuali.org>
2013-09-30 15:01:59 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-impex-3.0.x@26607 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex-producer/src/test/java/org/kuali/common/impex/schema/impl/TestOracleTableSqlProducer.java
+++ kuali-impex-producer/src/test/java/org/kuali/common/impex/schema/impl/TestOracleTableSqlProducer.java
@@ -15,6 +15,9 @@
 
 package org.kuali.common.impex.schema.impl;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,62 +27,45 @@ import org.junit.Test;
 import org.kuali.common.impex.model.Table;
 import org.kuali.common.impex.schema.impl.oracle.OracleTableSqlProducer;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.fail;
-
 public class TestOracleTableSqlProducer {
 
-    private final static String[] EXPECTED_SIMPLE_SQL = {"DECLARE temp NUMBER;\n" +
-            "BEGIN\n" +
-            "\tSELECT COUNT(*) INTO temp FROM user_tables WHERE table_name = 'FOO_T';\n" +
-            "\tIF temp > 0 THEN EXECUTE IMMEDIATE 'DROP TABLE FOO_T CASCADE CONSTRAINTS PURGE'; END IF;\n" +
-            "END;\n",
+	private final static String[] EXPECTED_SIMPLE_SQL = {
+			"DECLARE temp NUMBER;\n" + "BEGIN\n" + "\tSELECT COUNT(*) INTO temp FROM user_tables WHERE table_name = 'FOO_T';\n"
+					+ "\tIF temp > 0 THEN EXECUTE IMMEDIATE 'DROP TABLE FOO_T CASCADE CONSTRAINTS PURGE'; END IF;\n" + "END;\n",
 
-            "CREATE TABLE FOO_T\n" +
-            "(\n" +
-            "\tID VARCHAR2(36),\n" +
-            "\tCREATETIME TIMESTAMP NOT NULL,\n" +
-            "\tFOO_COUNT NUMBER(10),\n" +
-            "\tNAME VARCHAR2(255),\n" +
-            "\tCONSTRAINT FOO_U1_NAME UNIQUE (NAME)\n" +
-            ")\n",
+			"CREATE TABLE FOO_T\n" + "(\n" + "\tID VARCHAR2(36),\n" + "\tCREATETIME TIMESTAMP NOT NULL,\n" + "\tFOO_COUNT NUMBER(10),\n" + "\tNAME VARCHAR2(255),\n"
+					+ "\tCONSTRAINT FOO_U1_NAME UNIQUE (NAME)\n" + ")\n",
 
-            "ALTER TABLE FOO_T\n" +
-            "\tADD CONSTRAINT FOO_TP1\n" +
-            "PRIMARY KEY (ID)\n"
-    };
+			"ALTER TABLE FOO_T\n" + "\tADD CONSTRAINT FOO_TP1\n" + "PRIMARY KEY (ID)\n" };
 
+	@Test
+	public void simpleTableTest() {
 
+		OracleTableSqlProducer producer = new OracleTableSqlProducer();
 
-    @Test
-    public void simpleTableTest() {
+		producer.setMappingProvider(new NoOpProvider());
 
-        OracleTableSqlProducer producer = new OracleTableSqlProducer();
+		Table table = MockDataUtil.buildSimpleTable();
 
-        producer.setMappingProvider(new NoOpProvider());
+		List<String> results = producer.getTablesSql(Collections.singletonList(table));
 
-        Table table = MockDataUtil.buildSimpleTable();
+		assertEquals(EXPECTED_SIMPLE_SQL.length, results.size());
 
-        List<String> results = producer.getTablesSql(Collections.singletonList(table));
+		List<String> expected = Arrays.asList(EXPECTED_SIMPLE_SQL);
 
-        assertEquals(EXPECTED_SIMPLE_SQL.length, results.size());
+		List<String> foundExpected = new ArrayList<String>(expected);
+		for (String e : expected) {
+			if (results.contains(e)) {
+				foundExpected.remove(e);
+			} else {
+				fail("Expected sql statment **" + e + "** not found in generated statements.");
+			}
+		}
 
-        List<String> expected = Arrays.asList(EXPECTED_SIMPLE_SQL);
+		if (!foundExpected.isEmpty()) {
+			fail("Following expected sql statements not found in generated statements: \n" + foundExpected.toString());
+		}
 
-        List<String> foundExpected = new ArrayList<String>(expected);
-        for (String e : expected) {
-            if(results.contains(e)) {
-                foundExpected.remove(e);
-            }
-            else {
-                fail("Expected sql statment **" + e + "** not found in generated statements.");
-            }
-        }
-
-        if(!foundExpected.isEmpty()) {
-            fail("Following expected sql statements not found in generated statements: \n" + foundExpected.toString());
-        }
-
-    }
+	}
 
 }
