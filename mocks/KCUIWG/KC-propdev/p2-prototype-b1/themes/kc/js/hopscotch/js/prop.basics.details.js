c6a486f645103ec1c7956208026e11de4eb05364
thrclark <thrclark@kuali.org>
2013-10-03 10:39:51 -0400

    UXI-2 Guided tour demo page & resources
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@26663 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/js/hopscotch/js/prop.basics.details.js
@@ -0,0 +1,87 @@
+// Define the tour!
+var tour = {
+  id: "hello-hopscotch",
+  steps: [
+    {
+      title: "Proposal Type",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour1",
+      placement: "right"
+    },
+	
+	    {
+      title: "Lead Unit",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour2",
+      placement: "right"
+    },
+	
+	
+	    {
+      title: "Activity Type",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour3",
+      placement: "right"
+    },
+	
+	
+	    {
+      title: "Project Dates",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour4",
+      placement: "right"
+    },
+	
+	
+	    {
+      title: "Sponsor Code",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour5",
+      placement: "right"
+    },
+	
+	
+	    {
+      title: "Project Title",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour6",
+      placement: "right"
+    },
+	
+	
+	    {
+      title: "Award ID",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour7",
+      placement: "right"
+    },
+	
+	
+	    {
+      title: "Original Institutional ID",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour8",
+      placement: "right"
+    },
+	
+	
+	    {
+      title: "Keywords",
+      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      target: "#tour9",
+      placement: "right"
+    },
+	
+	
+	    {
+      title: "Let's move on...",
+      content: "Good job. you can do this!,",
+      target: "#tour10",
+      placement: "top"
+    }
+	
+  ]
+};
+
+// Start the tour!
+hopscotch.startTour(tour);
