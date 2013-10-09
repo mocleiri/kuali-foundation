91c50e6ee4fdc5694632f66032e980a663d55c31
thrclark <thrclark@kuali.org>
2013-10-09 13:08:23 -0400

    UXI-16 adding missing fields
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@26718 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/prop.keypersonnel.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/prop.keypersonnel.php
@@ -31,27 +31,127 @@ require_once( 'themes/kc/inc/toolbar.php' );
 								</div>
 							</div>
 
-							<p>Use this page to identify the faculty member or senior researcher who is the Principal Investigator (PI) of the proposal, and any additional Co-Investigators (Co-I), and project Key Persons (other Key Personnel).
+							<p>Use this page to identify the faculty member or senior researcher who is the Principal Investigator (PI) of the proposal, and any additional Co-Investigators (Co-I), and project Key Persons (other Key Personnel).</p>
 
-							<div class="key-personnel">
-								<div class="panel-group" id="accordion">
-									<div class="panel">
-										<div class="panel-heading">
-											<h6 class="panel-title">
-												<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#prop1">Edward Haskell (haskelldw)</a>
-											</h6>
-										</div>
-										<div id="prop1" class="panel-collapse collapse">
-											<div class="panel-body">
-												Stuff here...
+
+
+
+
+<div class="panel-group" id="accordion1" style="margin-bottom:20px;">
+								<div class="panel panel-default">
+									
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-md-6">
+												<h4 class="panel-title"><a class="accordion-toggle pull-left" data-toggle="collapse" data-parent="#accordion" href="#collapse2"> <span aria-hidden="true" class="icon-caret-down"></span> Edward H Haskell </a></h4>
 											</div>
+											<div class="col-md-6">
+												<a class="pull-right" href="#"><span aria-hidden="true" class="icon-remove"></span></a>
+											</div>
+										</div>
+									</div>
+
+									<div id="collapse2" class="panel-collapse in" style="height: auto;">
+										<div class="panel-body">
+											<table class="table table-collapsed">
+												<tbody><tr>
+													<th width="25%">Person role</th>
+													<td>PI</td>
+												</tr>
+												<tr>
+													<th>Full name</th>
+													<td>Edward R. Haskell</td>
+												</tr>
+												<tr>
+													<th>Username</th>
+													<td>erhaskell</td>
+												</tr>
+												<tr>
+													<th>Email address</th>
+													<td>erhaskell@email.com</td>
+												</tr>
+												<tr>
+													<th>Office phone</th>
+													<td>321-321-1214</td>
+												</tr>
+												<tr>
+													<th>Office fax</th>
+													<td>321-321-1121</td>
+												</tr>
+												<tr>
+													<th>Pager</th>
+													<td>321-333-1656</td>
+												</tr>
+												<tr>
+													<th>Mobile</th>
+													<td>321-321-1221</td>
+												</tr>
+												<tr>
+													<th>Primary title</th>
+													<td>Acme Primary Title</td>
+												</tr>
+												<tr>
+													<th>Directory title</th>
+													<td>Acme Directory Title</td>
+												</tr>
+												<tr>
+													<th>Home unit</th>
+													<td>UA-VPIT</td>
+												</tr>
+												<tr>
+													<th>Division</th>
+													<td>University Administration</td>
+												</tr>
+												<tr>
+													<th>eRA Commons username</th>
+													<td>erhaskell</td>
+												</tr>
+												<tr>
+													<th>Office address</th>
+													<td>Kuali Foundation, 1135 Kuali Drive, Kuali, KU</td>
+												</tr>
+												<tr>
+													<th>Country</th>
+													<td>United States</td>
+												</tr>
+												<tr>
+													<th>Percentage effort</th>
+													<td>50%</td>
+												</tr>
+												<tr>
+													<th>Faculty</th>
+													<td>-</td>
+												</tr>
+											</tbody></table>
 										</div>
 									</div>
 								</div>
 							</div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+							
+
 							<div class="form-group clearfix">
-								<button class="btn btn-default launch-modal" data-modal-page="modal/modal-addpersonnel/start.html">Add personnel</button>
+								<div class="well"><button class="btn btn-sm btn-default launch-modal" data-modal-page="modal/modal-addpersonnel/start.html">Add personnel</button></div>
 							</div>
 						</fieldset>
 					</form>
