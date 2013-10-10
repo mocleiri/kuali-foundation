3de5db47d614345916899f057a05185a6b5217bc
jcaddel <jcaddel@kuali.org>
2013-10-10 13:11:15 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26733 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/appserver/tomcat/TomcatFiles.java
@@ -0,0 +1,83 @@
+package org.kuali.common.deploy.appserver.tomcat;
+
+import java.util.List;
+
+import org.kuali.common.deploy.Deployable;
+import org.kuali.common.util.Assert;
+
+import com.google.common.collect.ImmutableList;
+
+public final class TomcatFiles {
+
+	private final String rootWar;
+	private final String serverXml;
+	private final String webXml;
+	private final List<Deployable> logsDirJsps;
+
+	public static class Builder {
+
+		private String rootWar;
+		private String serverXml;
+		private String webXml;
+		private List<Deployable> logsDirJsps = ImmutableList.of();
+
+		public Builder(TomcatDirs dirs) {
+			this.rootWar = dirs.getWebapps() + "/ROOT.war";
+			this.serverXml = dirs.getConf() + "/server.xml";
+			this.webXml = dirs.getConf() + "/web.xml";
+			this.logsDirJsps = getLogsDirJsps(dirs);
+		}
+
+		private List<Deployable> getLogsDirJsps(TomcatDirs dirs) {
+			Deployable env = new Deployable.Builder("classpath:org/kuali/common/deploy/jsp/env.jsp", dirs.getLib() + "/env.jsp").build();
+			Deployable tail = new Deployable.Builder("classpath:org/kuali/common/deploy/jsp/tail.jsp", dirs.getLib() + "/tail.jsp").build();
+			return ImmutableList.of(env, tail);
+		}
+
+		public Builder rootWar(String rootWar) {
+			this.rootWar = rootWar;
+			return this;
+		}
+
+		public Builder serverXml(String serverXml) {
+			this.serverXml = serverXml;
+			return this;
+		}
+
+		public Builder webXml(String webXml) {
+			this.webXml = webXml;
+			return this;
+		}
+
+		public TomcatFiles build() {
+			Assert.noBlanks(rootWar, serverXml, webXml);
+			Assert.notNull(logsDirJsps);
+			return new TomcatFiles(this);
+		}
+
+	}
+
+	private TomcatFiles(Builder builder) {
+		this.rootWar = builder.rootWar;
+		this.serverXml = builder.serverXml;
+		this.webXml = builder.webXml;
+		this.logsDirJsps = builder.logsDirJsps;
+	}
+
+	public String getRootWar() {
+		return rootWar;
+	}
+
+	public String getServerXml() {
+		return serverXml;
+	}
+
+	public String getWebXml() {
+		return webXml;
+	}
+
+	public List<Deployable> getLogsDirJsps() {
+		return logsDirJsps;
+	}
+
+}
