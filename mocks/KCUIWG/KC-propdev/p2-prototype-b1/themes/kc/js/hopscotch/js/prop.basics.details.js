b18e0c4f71b60891f7e469cea98936569395d5a8
thrclark <thrclark@kuali.org>
2013-10-11 13:51:35 -0400

    Guided tour page
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@26759 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/js/hopscotch/js/prop.basics.details.js
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/js/hopscotch/js/prop.basics.details.js
@@ -4,78 +4,78 @@ var tour = {
   steps: [
     {
       title: "Proposal Type",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      content: "Required.    An identifier for the kind of research project being submitted (i.e., new, resubmission, continuing, etc...).",
       target: "#tour1",
-      placement: "right"
+      placement: "top"
     },
 	
 	    {
       title: "Lead Unit",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      content: "Required.  The designated unit responsible for the application and administration of the proposed sponsored project; the lead unit is directly connected to the lead principal investigator. The proposal's fixed home unit that will be used for routing and business rule purposes. AKA Owned by Unit.  The designated unit responsible for the application and administration of the proposed sponsored project. ",
       target: "#tour2",
-      placement: "right"
+      placement: "top"
     },
 	
 	
 	    {
       title: "Activity Type",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      content: "Required.  A word or phrase to define the kind of endeavor being proposed (i.e., research, instruction, etc.).  Select from the list to specify the type of activity proposed, (for example, organized research or instruction).  Activity type refers to the major functions of an institution based on OMB Circular A-21. Each account in an institution's financial system must be identified by its activity type.",
       target: "#tour3",
-      placement: "right"
+      placement: "top"
     },
 	
 	
 	    {
       title: "Project Dates",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      content: "The proposed month, day and year in which the research will begin and end. In the Proposal Module, this is the requested project start and end dates for the project in the proposal.",
       target: "#tour4",
-      placement: "right"
-    },
-	
-	
-	    {
-      title: "Sponsor Code",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
-      target: "#tour5",
-      placement: "right"
+      placement: "top"
     },
 	
 	
 	    {
       title: "Project Title",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
+      content: "Required.  The name created to identify the research proposal.  The proposed title of the project. ",
+      target: "#tour5",
+      placement: "top"
+    },
+	
+	
+	    {
+      title: "Sponsor Code",
+      content: "Required.  A predefined code directly associated with the sponsoring organization (i.e. 000500 = National Science Foundation, 000340 = National Institutes of Health, etc.). The identification number of the organization or agency that is providing support for the sponsored project.  ",
       target: "#tour6",
-      placement: "right"
+      placement: "top"
+    },
+	
+	
+	    {
+      title: "Prime Sponsor Code",
+      content: "Content coming soon...",
+      target: "#tour7",
+      placement: "top"
     },
 	
 	
 	    {
       title: "Award ID",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
-      target: "#tour7",
-      placement: "right"
+      content: "A unique identifier assigned by an institution to identify an award (e.g. used when submitting a continuing type of activity.). A unique institutionally assigned number of a previously funded application.  A unique ID automatically assigned by the system (based on institution specifications) to identify an award.   An award is defined as:  To grant or declare as merited or due. If, after consideration, an outside agency/sponsor decides a proposal has merit and worth, an agreement is drawn between the funding agency and Institution for services. This agreement is called an award. An award is a legal, binding document for services in lieu of funds to perform the original outline of the proposal submitted.",
+      target: "#tour8",
+      placement: "top"
     },
 	
 	
 	    {
-      title: "Original Institutional ID",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
-      target: "#tour8",
-      placement: "right"
+      title: "Institutional Proposal ID",
+      content: "The unique identifier originally assigned to the  previously-submitted proposal record; used if the document is being revised and/or resubmitted. The number assigned by KC Proposal Development module for the previously submitted proposal record currently being revised.  A unique institutionally assigned proposal number of a previously submitted proposal. This is applicable when submitting a revised (changed/corrected) application.",
+      target: "#tour9",
+      placement: "top"
     },
 	
 	
 	    {
       title: "Keywords",
-      content: "Ea ullum quidam integre eos, eu pro nihil conceptam consequuntur, an intellegat scripserit voluptatibus his. An enim senserit urbanitas.,",
-      target: "#tour9",
-      placement: "right"
-    },
-	
-	
-	    {
-      title: "Let's move on...",
-      content: "Good job. you can do this!,",
+      content: "Specific words selected from a predefined list that can be used in database lookups and reports (e.g. diabetes, shikimic acid, etc.).  Select from the lookup a term that describes the discipline of a sponsored project.  The actual keyword(s) specific to the proposal that can be used in database lookups and reports. ",
       target: "#tour10",
       placement: "top"
     }
@@ -83,5 +83,3 @@ var tour = {
   ]
 };
 
-// Start the tour!
-hopscotch.startTour(tour);
