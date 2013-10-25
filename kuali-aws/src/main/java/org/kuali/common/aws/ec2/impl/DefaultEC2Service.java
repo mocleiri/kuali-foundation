b7b97e10b8ca42b7bd4f514c02a56f5192a8f3eb
jcaddel <jcaddel@kuali.org>
2013-10-25 16:24:38 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26928 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/impl/DefaultEC2Service.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/impl/DefaultEC2Service.java
@@ -1,7 +1,8 @@
-package org.kuali.common.aws.ec2;
+package org.kuali.common.aws.ec2.impl;
 
 import java.util.List;
 
+import org.kuali.common.aws.ec2.api.EC2Service;
 import org.kuali.common.util.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,25 +15,27 @@ import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
 
-public final class EC2Service {
+public final class DefaultEC2Service implements EC2Service {
 
-	private static final Logger logger = LoggerFactory.getLogger(EC2Service.class);
+	private static final Logger logger = LoggerFactory.getLogger(DefaultEC2Service.class);
 
 	private final AmazonEC2Client client;
 
-	public EC2Service(AWSCredentials credentials) {
+	public DefaultEC2Service(AWSCredentials credentials) {
 		Assert.noNulls(credentials);
 		this.client = new AmazonEC2Client(credentials);
 	}
 
-	public EC2Service(String accessKey, String secretKey) {
+	public DefaultEC2Service(String accessKey, String secretKey) {
 		this(new BasicAWSCredentials(accessKey, secretKey));
 	}
 
-	public Instance getSingleEC2Instance(RunInstancesRequest request) {
+	@Override
+	public Instance launchSingleInstance(RunInstancesRequest request) {
 		RunInstancesResult result = client.runInstances(request);
 		Reservation r = result.getReservation();
 		List<Instance> instances = r.getInstances();
+		Assert.isTrue(instances.size() == 1);
 		return instances.get(0);
 	}
 
