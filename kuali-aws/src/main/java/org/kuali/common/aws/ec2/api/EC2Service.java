ff2f77adc60ccd475d55a571d06f4ed432f90276
jcaddel <jcaddel@kuali.org>
2013-10-25 18:44:10 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26957 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/api/EC2Service.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/api/EC2Service.java
@@ -9,10 +9,19 @@ import com.amazonaws.services.ec2.model.Tag;
 
 public interface EC2Service {
 
+	/**
+	 * Launch a single Amazon EC2 instance
+	 */
 	public Instance launchInstance(LaunchInstanceRequest request);
 
+	/**
+	 * Return an Amazon EC2 instance given an instance id
+	 */
 	public Instance getInstance(String id);
 
+	/**
+	 * Create tags on the indicated Amazon resource
+	 */
 	public void tag(String resourceId, List<Tag> tags);
 
 }
