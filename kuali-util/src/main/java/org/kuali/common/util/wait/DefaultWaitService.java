6b80c88203de869a07fa9c44d3acd73727d42775
jcaddel <jcaddel@kuali.org>
2013-10-26 15:37:04 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27048 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/wait/DefaultWaitService.java
+++ kuali-util/src/main/java/org/kuali/common/util/wait/DefaultWaitService.java
@@ -1,10 +1,28 @@
 package org.kuali.common.util.wait;
 
+import org.kuali.common.util.Assert;
+import org.kuali.common.util.FormatUtils;
+import org.kuali.common.util.ThreadUtils;
+import org.kuali.common.util.condition.Condition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DefaultWaitService implements WaitService {
+
+	private static final Logger logger = LoggerFactory.getLogger(DefaultWaitService.class);
 
 	@Override
 	public WaitResult wait(WaitContext context) {
 		long start = System.currentTimeMillis();
+		long timeout = start + context.getTimeoutMillis();
+		Condition condition = context.getCondition();
+		ThreadUtils.sleep(context.getInitialPauseMillis());
+		while (!condition.isTrue()) {
+			long now = System.currentTimeMillis();
+			Assert.isTrue(now <= timeout, "Timed out waiting");
+			logger.info("[elapsed: {}  timeout: {}]", FormatUtils.getTime(now - start), FormatUtils.getTime(timeout - now));
+			ThreadUtils.sleep(context.getSleepMillis());
+		}
 		return new WaitResult.Builder(start, System.currentTimeMillis() - start).build();
 	}
 
