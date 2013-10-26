5b8ed7d57d92aaccdf4764a3ff00c17914730928
jcaddel <jcaddel@kuali.org>
2013-10-26 18:49:26 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27066 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/Reachability.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/Reachability.java
@@ -12,7 +12,17 @@ public final class Reachability {
 		this.instance = instance;
 	}
 
+	/**
+	 * This string indicates the ability of AWS to get network packets to the instance. If this check fails, there may be an issue with the infrastructure hosting the instance
+	 * (such as AWS power, networking or software systems). The instance may need to be restarted or replaced or it may require waiting for AWS systems to resolve an issue. This
+	 * check does not validate that the operating system and applications are accepting traffic.
+	 */
 	private final String system;
+
+	/**
+	 * This string indicates the ability of the instance's operating system is accepting traffic. If this check fails, the instance may need to be rebooted or have modifications
+	 * made to the operating system configuration.
+	 */
 	private final String instance;
 
 	public String getSystem() {
