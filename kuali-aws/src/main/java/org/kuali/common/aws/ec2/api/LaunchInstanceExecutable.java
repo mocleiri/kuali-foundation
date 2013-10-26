f1185b33935d7f5f86c1459a82c70b98010450f3
jcaddel <jcaddel@kuali.org>
2013-10-25 20:17:24 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@26984 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/api/LaunchInstanceExecutable.java
@@ -0,0 +1,39 @@
+package org.kuali.common.aws.ec2.api;
+
+import org.kuali.common.aws.ec2.model.LaunchInstanceRequest;
+import org.kuali.common.util.Assert;
+import org.kuali.common.util.execute.Executable;
+
+public class LaunchInstanceExecutable implements Executable {
+
+	private final EC2Service service;
+	private final LaunchInstanceRequest request;
+	private final boolean skip;
+
+	public LaunchInstanceExecutable(EC2Service service, LaunchInstanceRequest request) {
+		this(service, request, false);
+	}
+
+	public LaunchInstanceExecutable(EC2Service service, LaunchInstanceRequest request, boolean skip) {
+		Assert.noNulls(service, request);
+		this.service = service;
+		this.request = request;
+		this.skip = skip;
+	}
+
+	@Override
+	public void execute() {
+		if (!skip) {
+			service.launchInstance(request);
+		}
+	}
+
+	public EC2Service getService() {
+		return service;
+	}
+
+	public LaunchInstanceRequest getRequest() {
+		return request;
+	}
+
+}
