33e1cf95466d76dc50f6ae892ec3a8a6ba96e53b
jcaddel <jcaddel@kuali.org>
2013-10-27 08:07:26 -0400

    Add the alsa-lib-devel package to avoid:
    XPCOMGlueLoad error for file /usr/local/firefox/libxul.so:
    libasound.so.2: cannot open shared object file: No such file or directory
    Couldn't load XPCOM.
    
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27122 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/main/resources/org/kuali/common/kuali-ec2/amazon-linux/gtk-firefox.sh
+++ kuali-ec2/src/main/resources/org/kuali/common/kuali-ec2/amazon-linux/gtk-firefox.sh
@@ -19,7 +19,7 @@ yum --assumeyes install make libjpeg-devel libpng-devel \
 libtiff-devel gcc libffi-devel gettext-devel libmpc-devel \
 libstdc++46-devel xauth gcc-c++ libtool libX11-devel \
 libXext-devel libXinerama-devel libXi-devel libxml2-devel \
-libXrender-devel libXrandr-devel libXt dbus-glib
+libXrender-devel libXrandr-devel libXt dbus-glib alsa-lib-devel
 mkdir -p $workpath
 mkdir -p $installroot
 cd $installroot
