e42ac3ce63536a3ce8dde089ffc4f54851720980
jcaddel <jcaddel@kuali.org>
2013-10-26 20:01:19 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27077 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/Reachability.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/Reachability.java
@@ -5,6 +5,7 @@ import org.kuali.common.util.Assert;
 public final class Reachability {
 
 	public static final String STATUS_NAME = "reachability";
+	public static final String PASSED = "passed";
 
 	public Reachability(String system, String instance) {
 		Assert.noBlanks(system, instance);
