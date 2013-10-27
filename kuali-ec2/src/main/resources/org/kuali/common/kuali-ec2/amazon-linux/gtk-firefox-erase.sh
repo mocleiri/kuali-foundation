c8f3e0bbcd81772d97e2981053123ca5f4605be4
jcaddel <jcaddel@kuali.org>
2013-10-27 08:46:55 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27134 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/main/resources/org/kuali/common/kuali-ec2/amazon-linux/gtk-firefox-erase.sh
+++ kuali-ec2/src/main/resources/org/kuali/common/kuali-ec2/amazon-linux/gtk-firefox-erase.sh
@@ -2,9 +2,3 @@
  
 rm -rf /usr/local/src
 rm -rf /etc/ld.so.conf.d/firefox.conf
-
-yum --assumeyes erase make libjpeg-devel libpng-devel \
-libtiff-devel gcc libffi-devel gettext-devel libmpc-devel \
-libstdc++46-devel xauth gcc-c++ libtool libX11-devel \
-libXext-devel libXinerama-devel libXi-devel libxml2-devel \
-libXrender-devel libXrandr-devel libXt dbus-glib alsa-lib-devel
