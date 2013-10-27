388b8918b0861ab1f4395cb2710e42f9ab82ec6d
jcaddel <jcaddel@kuali.org>
2013-10-27 18:59:41 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27187 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/test/java/org/kuali/common/aws/ec2/LaunchInstanceConfig.java
+++ kuali-aws/src/test/java/org/kuali/common/aws/ec2/LaunchInstanceConfig.java
@@ -62,7 +62,9 @@ public class LaunchInstanceConfig {
 		InstanceType type = InstanceType.fromValue(env.getString("ec2.type"));
 		List<Tag> tags = getTags();
 		List<String> securityGroups = SpringUtils.getNoneSensitiveListFromCSV(env, "ec2.securityGroups");
-		return new LaunchInstanceContext.Builder(ami, keyName).type(type).availabilityZone(availabilityZone).tags(tags).securityGroups(securityGroups).build();
+		boolean enableTerminationProtection = env.getBoolean("ec2.enableTerminationProtection", false);
+		return new LaunchInstanceContext.Builder(ami, keyName).type(type).availabilityZone(availabilityZone).tags(tags).securityGroups(securityGroups)
+				.enableTerminationProtection(enableTerminationProtection).build();
 	}
 
 	protected List<Tag> getTags() {
