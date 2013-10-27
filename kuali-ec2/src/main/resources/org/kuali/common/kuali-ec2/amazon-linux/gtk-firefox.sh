f80a6c5dcb223b400a45cd940c7db15c9b16c39a
jcaddel <jcaddel@kuali.org>
2013-10-27 08:14:27 -0400

    Commenting out everything but the first install
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27125 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/main/resources/org/kuali/common/kuali-ec2/amazon-linux/gtk-firefox.sh
+++ kuali-ec2/src/main/resources/org/kuali/common/kuali-ec2/amazon-linux/gtk-firefox.sh
@@ -65,16 +65,16 @@ cd ..
  
 init
 install ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.xz
-install http://download.savannah.gnu.org/releases/freetype/freetype-2.4.9.tar.gz
-install http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.9.0.tar.gz
-install http://ftp.gnome.org/pub/gnome/sources/glib/2.32/glib-2.32.3.tar.xz
-install http://cairographics.org/releases/pixman-0.26.0.tar.gz
-install http://cairographics.org/releases/cairo-1.12.2.tar.xz
-install http://ftp.gnome.org/pub/gnome/sources/pango/1.30/pango-1.30.0.tar.xz
-install http://ftp.gnome.org/pub/gnome/sources/atk/2.4/atk-2.4.0.tar.xz
-install http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.26/gdk-pixbuf-2.26.1.tar.xz
-install http://ftp.gnome.org/pub/gnome/sources/gtk+/2.24/gtk+-2.24.10.tar.xz
-finish
+#install http://download.savannah.gnu.org/releases/freetype/freetype-2.4.9.tar.gz
+#install http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.9.0.tar.gz
+#install http://ftp.gnome.org/pub/gnome/sources/glib/2.32/glib-2.32.3.tar.xz
+#install http://cairographics.org/releases/pixman-0.26.0.tar.gz
+#install http://cairographics.org/releases/cairo-1.12.2.tar.xz
+#install http://ftp.gnome.org/pub/gnome/sources/pango/1.30/pango-1.30.0.tar.xz
+#install http://ftp.gnome.org/pub/gnome/sources/atk/2.4/atk-2.4.0.tar.xz
+#install http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.26/gdk-pixbuf-2.26.1.tar.xz
+#install http://ftp.gnome.org/pub/gnome/sources/gtk+/2.24/gtk+-2.24.10.tar.xz
+#finish
  
  
 # adds the /usr/local/bin to your path by updating your .bashrc file.
