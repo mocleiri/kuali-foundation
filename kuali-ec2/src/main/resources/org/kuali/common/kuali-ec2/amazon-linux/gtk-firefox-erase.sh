5d1d3fbe75f219551bcf98fbd91412b84005e19b
jcaddel <jcaddel@kuali.org>
2013-10-27 08:30:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27130 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/main/resources/org/kuali/common/kuali-ec2/amazon-linux/gtk-firefox-erase.sh
+++ kuali-ec2/src/main/resources/org/kuali/common/kuali-ec2/amazon-linux/gtk-firefox-erase.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# GTK+ and Firefox for Amazon Linux
-# Written by Joseph Lawson 2012-06-03
-# http://joekiller.com
-# http://joekiller.com/2012/06/03/install-firefox-on-amazon-linux-x86_64-compiling-gtk/
- 
-# chmod 755 ./gtk-firefox.sh
-# sudo ./gtk-firefox.sh
  
 rm -rf /usr/local/src
 rm -rf /etc/ld.so.conf.d/firefox.conf
