63565636d277b9c333fd9b65f2cf9d6bc14a7dfc
jcaddel <jcaddel@kuali.org>
2013-10-28 13:41:57 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27207 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/impl/DefaultEC2Service.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/impl/DefaultEC2Service.java
@@ -184,7 +184,7 @@ public final class DefaultEC2Service implements EC2Service {
 		Instance instance = getInstance(context);
 		// Was getting some flaky behavior from AWS without a small delay after the RunInstancesRequest returned
 		// Granted, this was in early 2011 and it may no longer be an issue
-		// Since it generally takes a few minutes for the instance to spin up, pausing for 1 second here shouldn't pose much of an issue
+		// Since it generally takes a few minutes for the instance to spin up, pausing here for 1 second shouldn't pose much of an issue
 		ThreadUtils.sleep(initialPauseMillis);
 		tag(instance.getInstanceId(), context.getTags());
 		if (context.isPreventTermination()) {
