446aee47cd237a4a182d2b67ad8291077b44b5d1
jcaddel <jcaddel@kuali.org>
2013-10-29 16:58:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27319 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/DevOpsAwsConstants.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/DevOpsAwsConstants.java
@@ -5,8 +5,8 @@ import org.kuali.common.aws.model.AwsAccount;
 public abstract class DevOpsAwsConstants {
 
 	public static final AwsAccount FOUNDATION = new AwsAccount.Builder("3627-3510-8948").accessKeyId("AKIAJFD5IM7IPVVUEBNA").keyName("kuali-key").build();
-	public static final AwsAccount KS = new AwsAccount.Builder("0523-3819-3506").accessKeyId("AKIAJZ72UQ5ZCVEDMAPQ").keyName("kuali-key").build();
-	public static final AwsAccount RICE = new AwsAccount.Builder("7898-1396-8323").accessKeyId("AKIAIZFPMJVCNOYYAZ2Q").keyName("kuali-key").build();
-	public static final AwsAccount OLE = new AwsAccount.Builder("7867-4615-1229").accessKeyId("AKIAI453FI76LUZ7T7CA").keyName("kuali-key").build();
+	public static final AwsAccount KS = new AwsAccount.Builder("0523-3819-3506").accessKeyId("AKIAJZ72UQ5ZCVEDMAPQ").keyName("ks-key").build();
+	public static final AwsAccount RICE = new AwsAccount.Builder("7898-1396-8323").accessKeyId("AKIAIZFPMJVCNOYYAZ2Q").keyName("kr-key").build();
+	public static final AwsAccount OLE = new AwsAccount.Builder("7867-4615-1229").accessKeyId("AKIAI453FI76LUZ7T7CA").keyName("ko-key").build();
 
 }
