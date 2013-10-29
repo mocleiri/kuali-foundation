4ed9f236e170fcacb9a4f75927dfebd279975f82
jcaddel <jcaddel@kuali.org>
2013-10-29 18:56:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27338 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/ci/CreateMasterConfig.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/ci/CreateMasterConfig.java
@@ -22,11 +22,9 @@ import org.kuali.common.aws.ec2.api.EC2Service;
 import org.kuali.common.aws.ec2.model.LaunchInstanceContext;
 import org.kuali.common.aws.ec2.model.RootVolume;
 import org.kuali.common.aws.ec2.util.LaunchUtils;
-import org.kuali.common.aws.model.AwsAccount;
 import org.kuali.common.aws.spring.AwsServiceConfig;
 import org.kuali.common.devops.aws.DevOpsAwsConstants;
 import org.kuali.common.devops.aws.SecurityGroupName;
-import org.kuali.common.util.spring.SpringUtils;
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,11 +41,6 @@ import com.google.common.collect.ImmutableList;
 @Import({ AwsServiceConfig.class, SpringServiceConfig.class })
 public class CreateMasterConfig {
 
-	private static final int DEFAULT_ROOT_VOLUME_SIZE_IN_GIGABYTES = 25;
-	private static final boolean DEFAULT_PREVENT_TERMINATION = true;
-	private static final List<String> DEFAULT_SECURITY_GROUPS = SecurityGroupName.getValues(SecurityGroupName.SSH, SecurityGroupName.HTTP, SecurityGroupName.HTTPS);
-	private static final List<Tag> DEFAULT_MASTER_TAGS = getDefaultMasterTags();
-
 	@Autowired
 	EC2Service service;
 
@@ -56,27 +49,26 @@ public class CreateMasterConfig {
 
 	@Bean
 	public Object launchAndThenTerminate() {
-		LaunchInstanceContext context = LaunchUtils.getLaunchInstanceContext(env);
+		LaunchInstanceContext context = LaunchUtils.getLaunchInstanceContext(env, getDefaultMasterLaunchContext());
 		Instance instance = service.launchInstance(context);
 		// service.terminateInstance(instance.getInstanceId());
 		return null;
 	}
 
 	private LaunchInstanceContext getDefaultMasterLaunchContext() {
-		AwsAccount account = DevOpsAwsConstants.FOUNDATION;
-		String ami = env.getString(LaunchUtils.AMI_KEY, DevOpsAwsConstants.AMAZON_LINUX_64_BIT_MINIMAL_AMI_2013_09);
-		String keyName = env.getString(LaunchUtils.KEY_NAME_KEY, account.getKeyName());
-		InstanceType type = LaunchUtils.getType(env, InstanceType.M1Large);
-		String zone = env.getString(LaunchUtils.AVAILABILITY_ZONE_KEY, DevOpsAwsConstants.US_EAST_1D);
-		List<String> securityGroups = SpringUtils.getStrings(env, LaunchUtils.SECURITY_GROUPS_KEY, DEFAULT_SECURITY_GROUPS);
-		List<Tag> tags = LaunchUtils.getTags(env, DEFAULT_MASTER_TAGS);
-		RootVolume rootVolume = new RootVolume(DEFAULT_ROOT_VOLUME_SIZE_IN_GIGABYTES);
-		boolean preventTermination = env.getBoolean(LaunchUtils.PREVENT_TERMINATION_KEY, DEFAULT_PREVENT_TERMINATION);
+		String ami = DevOpsAwsConstants.AMAZON_LINUX_64_BIT_MINIMAL_AMI_2013_09;
+		String keyName = DevOpsAwsConstants.FOUNDATION.getKeyName();
+		InstanceType type = InstanceType.M1Large;
+		String zone = DevOpsAwsConstants.US_EAST_1D;
+		List<String> securityGroups = SecurityGroupName.getValues(SecurityGroupName.SSH, SecurityGroupName.HTTP, SecurityGroupName.HTTPS);
+		List<Tag> tags = getTags();
+		RootVolume rootVolume = new RootVolume(25);
+		boolean preventTermination = true;
 		return new LaunchInstanceContext.Builder(ami, keyName).type(type).availabilityZone(zone).tags(tags).securityGroups(securityGroups).preventTermination(preventTermination)
 				.rootVolume(rootVolume).build();
 	}
 
-	private static final List<Tag> getDefaultMasterTags() {
+	private final List<Tag> getTags() {
 		List<Tag> tags = new ArrayList<Tag>();
 		tags.add(new Tag("Name", "ci-test-server-ok-to-delete"));
 		tags.add(new Tag("Vendor", "jenkins"));
