8bd84ae7c951955671a85cb449e38e86ce45da37
jcaddel <jcaddel@kuali.org>
2013-10-29 18:19:27 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27336 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/DevOpsAwsConstants.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/DevOpsAwsConstants.java
@@ -9,6 +9,7 @@ public abstract class DevOpsAwsConstants {
 	public static final AwsAccount RICE = new AwsAccount.Builder("7898-1396-8323").accessKeyId("AKIAIZFPMJVCNOYYAZ2Q").keyName("kr-key").build();
 	public static final AwsAccount OLE = new AwsAccount.Builder("7867-4615-1229").accessKeyId("AKIAI453FI76LUZ7T7CA").keyName("ole").build();
 
+	// This is the availability zone both the master and the build slaves are pinned to
 	public static final String US_EAST_1D = "us-east-1d";
 
 	// amzn-ami-minimal-pv-2013.09 - This is a bare bones Amazon Linux box with virtually nothing installed on it
