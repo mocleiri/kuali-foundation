000c3e8e6114dbee20aa18b466cc41d29505dbe2
jcaddel <jcaddel@kuali.org>
2013-10-29 17:48:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27328 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/main/java/org/kuali/common/devops/project/spring/DevOpsProjectConfig.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.devops.project.spring;
+
+import org.kuali.common.aws.project.KualiAwsProjectConstants;
+import org.kuali.common.util.project.model.ProjectIdentifier;
+import org.kuali.common.util.project.spring.AutowiredProjectConfig;
+import org.kuali.common.util.project.spring.ProjectIdentifierConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ AutowiredProjectConfig.class })
+public class DevOpsProjectConfig implements ProjectIdentifierConfig {
+
+	@Override
+	@Bean
+	public ProjectIdentifier projectIdentifier() {
+		return KualiAwsProjectConstants.PROJECT_ID;
+	}
+}
