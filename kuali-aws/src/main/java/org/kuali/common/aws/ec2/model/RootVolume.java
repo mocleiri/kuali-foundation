0e37242634544c8c528d772155749adcfa6d8953
jcaddel <jcaddel@kuali.org>
2013-10-29 19:37:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27343 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/RootVolume.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/RootVolume.java
@@ -9,6 +9,10 @@ public final class RootVolume {
 	// Default is to delete the root volume when its associated instance is terminated
 	private static final boolean DEFAULT_DELETE_ON_TERMINATION = true;
 
+	public RootVolume() {
+		this(DEFAULT_DELETE_ON_TERMINATION);
+	}
+
 	public RootVolume(int sizeInGigabytes) {
 		this(sizeInGigabytes, DEFAULT_DELETE_ON_TERMINATION);
 	}
