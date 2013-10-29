bbdb463f1df1babd44c9f8d00e7af378f63e457c
jcaddel <jcaddel@kuali.org>
2013-10-29 17:48:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27329 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/ci/LaunchMasterPSC.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/ci/LaunchMasterPSC.java
@@ -3,8 +3,8 @@ package org.kuali.common.devops.ci;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.kuali.common.aws.project.KualiAwsProjectConstants;
 import org.kuali.common.aws.project.spring.AwsProjectConfig;
+import org.kuali.common.devops.project.KualiDevOpsProjectConstants;
 import org.kuali.common.util.project.ProjectUtils;
 import org.kuali.common.util.project.model.Project;
 import org.kuali.common.util.properties.Location;
@@ -32,10 +32,9 @@ public class LaunchMasterPSC implements PropertySourceConfig {
 	@Bean
 	public PropertySource<?> propertySource() {
 		String encoding = ProjectUtils.getEncoding(project);
-		String prefix = ProjectUtils.getClasspathPrefix(KualiAwsProjectConstants.PROJECT_ID);
+		String prefix = ProjectUtils.getClasspathPrefix(KualiDevOpsProjectConstants.PROJECT_ID);
 		List<Location> locations = new ArrayList<Location>();
 		locations.add(new Location(prefix + "/foundation.properties", encoding));
-		locations.add(new Location(prefix + "/ci.properties", encoding));
 		return PropertySourceUtils.getPropertySource(service, locations);
 	}
 }
