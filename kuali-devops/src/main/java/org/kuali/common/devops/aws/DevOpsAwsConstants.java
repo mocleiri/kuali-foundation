e80b9601a0b7522041d5fad08f2f16ab76d6e76a
jcaddel <jcaddel@kuali.org>
2013-10-29 16:50:09 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27316 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/DevOpsAwsConstants.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/DevOpsAwsConstants.java
@@ -6,7 +6,9 @@ import org.kuali.common.aws.model.AwsAccount;
 
 public abstract class DevOpsAwsConstants {
 
-	private static final List<String> GROUPS = SecurityGroupName.getSecurityGroups(SecurityGroupName.SSH, SecurityGroupName.HTTP, SecurityGroupName.HTTPS);
-	public static final AwsAccount FOUNDATION = new AwsAccount.Builder("3627-3510-8948").accessKeyId("AKIAJFD5IM7IPVVUEBNA").keyName("kuali-key").securityGroups(GROUPS).build();
+	private static final List<String> DEFAULT_GROUPS = SecurityGroupName.getSecurityGroups(SecurityGroupName.SSH, SecurityGroupName.HTTP, SecurityGroupName.HTTPS);
+
+	public static final AwsAccount FOUNDATION = new AwsAccount.Builder("3627-3510-8948").accessKeyId("AKIAJFD5IM7IPVVUEBNA").keyName("kuali-key").securityGroups(DEFAULT_GROUPS)
+			.build();
 
 }
