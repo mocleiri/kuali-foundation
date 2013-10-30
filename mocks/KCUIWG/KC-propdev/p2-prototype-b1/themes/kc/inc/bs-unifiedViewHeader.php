9bc9ca255b755536efe491984a190b95ed5317b8
thrclark <thrclark@kuali.org>
2013-10-30 16:34:53 -0400

    DOM cleanup
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@27392 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/bs-unifiedViewHeader.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/bs-unifiedViewHeader.php
@@ -5,55 +5,54 @@
     
     <header class="viewHeader">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6"> 
           <!-- <h1> <span class="areaTitle">Proposal Development</span> <span class="headerText"> Proposal: #23533 </span> <a href="#"><span aria-hidden="false" class="icon-question-sign"> <span class="icon-text">help</span></span></a> <span class="supportTitle">PI: Ken Graves</span> </h1> -->
           <h1> <span class="areaTitle">Proposal Development</span> <span class="headerText"> Proposal: #23533 </span> <span class="supportTitle">PI: <em>Not yet assigned</em></span> </h1>
         </div>
         <div class="col-md-6 visible-lg visible-md">
-          <div class="metadata"><strong>Document Info</strong>
-            <div class="metadata-featured" > Doc Nbr: 2743<br>
-              Initiator: thrclark<br>
-              Status: In Progress<br>
-              <div class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">more...</a>
-                <div class="dropdown-menu" >
-                  <h4> Document Info</h4>
-                  <table class="table table-condensed">
-                    <tbody>
-                      <tr>
-                        <th scope="row">Doc Nbr</th>
-                        <td>2743</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Initiator</th>
-                        <td><a href="#">thrclark</a></td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Status</th>
-                        <td>In Progress</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">PI</th>
-                        <td>Ken Graves</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Created</th>
-                        <td>04:27pm 07/09/2013</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Updated</th>
-                        <td>12:22pm 07/12/2013</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Proposal Nbr</th>
-                        <td>#23533</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Sponsor Name</th>
-                        <td>NIH</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+          <div class="metadata"><strong>Document Info</strong> <br>
+            Doc Nbr: 2743<br>
+            Initiator: thrclark<br>
+            Status: In Progress<br>
+            <div class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">more...</a>
+              <div class="dropdown-menu" >
+                <h4> Document Info</h4>
+                <table class="table table-condensed">
+                  <tbody>
+                    <tr>
+                      <th scope="row">Doc Nbr</th>
+                      <td>2743</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Initiator</th>
+                      <td><a href="#">thrclark</a></td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Status</th>
+                      <td>In Progress</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">PI</th>
+                      <td>Ken Graves</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Created</th>
+                      <td>04:27pm 07/09/2013</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Updated</th>
+                      <td>12:22pm 07/12/2013</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Proposal Nbr</th>
+                      <td>#23533</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Sponsor Name</th>
+                      <td>NIH</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
@@ -66,8 +65,8 @@
           <li><i class="icon icon-copy"></i> <a class="various fancybox.ajax" data-fancybox-type="ajax" href="modal/copy-document.html">Copy</a></li>
           <li><i class="icon icon-tasks"></i> <a href="#">Medusa</a></li>
           <li><i class="icon icon-list-ol"></i> <a href="#">Hierarchy</a></li>
-          <li><i class="icon icon-lock"></i> <a href="#">Access</a></li>
-          <li><i class="icon icon-question-sign"></i> <a href="#">Help</a></li>
+        <li><i class="icon icon-lock"></i> <a href="#">Access</a></li>
+          <!-- <li><i class="icon icon-question-sign"></i> <a href="#">Help</a></li>-->
         </ul>
       </div>
     </header>
