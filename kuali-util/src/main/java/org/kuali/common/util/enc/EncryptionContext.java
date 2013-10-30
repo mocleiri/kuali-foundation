d8921ad91fd2ca40166ad9888c71988129573fe1
jcaddel <jcaddel@kuali.org>
2013-10-30 12:55:56 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27371 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/enc/EncryptionContext.java
@@ -0,0 +1,32 @@
+package org.kuali.common.util.enc;
+
+import org.kuali.common.util.Assert;
+
+import com.google.common.base.Optional;
+
+public final class EncryptionContext {
+
+	public EncryptionContext(boolean enabled, Optional<String> password, EncStrength strength) {
+		Assert.noNulls(password, strength);
+		this.enabled = enabled;
+		this.password = password;
+		this.strength = strength;
+	}
+
+	private final boolean enabled;
+	private final Optional<String> password;
+	private final EncStrength strength;
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public Optional<String> getPassword() {
+		return password;
+	}
+
+	public EncStrength getStrength() {
+		return strength;
+	}
+
+}
