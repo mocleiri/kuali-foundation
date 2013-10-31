7a5e2498b6f7d087471707867e5596aff5011348
jcaddel <jcaddel@kuali.org>
2013-10-31 19:49:43 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27501 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/spring/DefaultAwsCredentialsConfig.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/spring/DefaultAwsCredentialsConfig.java
@@ -16,28 +16,53 @@
 package org.kuali.common.aws.spring;
 
 import org.kuali.common.aws.model.ImmutableAwsCredentials;
+import org.kuali.common.util.enc.EncUtils;
+import org.kuali.common.util.enc.EncryptionService;
+import org.kuali.common.util.enc.spring.DefaultEncryptionServiceConfig;
+import org.kuali.common.util.spring.SpringUtils;
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.google.common.base.Optional;
 
 @Configuration
+@Import({ DefaultEncryptionServiceConfig.class })
 public class DefaultAwsCredentialsConfig implements AwsCredentialsConfig {
 
 	private static final String ACCESS_KEY = "aws.accessKeyId";
+	private static final String ACCESS_ENV_KEY = "AWS_ACCESS_KEY_ID";
+
 	private static final String SECRET_KEY = "aws.secretKey";
+	private static final String SECRET_ENV_KEY = "AWS_SECRET_KEY";
 
 	@Autowired
 	EnvironmentService env;
 
+	@Autowired
+	EncryptionService service;
+
 	@Override
 	@Bean
 	public AWSCredentials awsCredentials() {
-		String accessKey = env.getString(ACCESS_KEY);
-		String secretKey = env.getString(SECRET_KEY);
+		String accessKey = getValue(ACCESS_KEY, ACCESS_ENV_KEY);
+		String secretKey = getValue(SECRET_KEY, SECRET_ENV_KEY);
+		if (EncUtils.isEncrypted(secretKey)) {
+			secretKey = service.decrypt(secretKey);
+		}
 		return new ImmutableAwsCredentials(accessKey, secretKey);
+	}
+
+	protected String getValue(String key, String envKey) {
+		Optional<String> value = SpringUtils.getOptionalString(env, key);
+		if (value.isPresent()) {
+			return value.get();
+		} else {
+			return env.getString(envKey);
+		}
 	}
 
 }
