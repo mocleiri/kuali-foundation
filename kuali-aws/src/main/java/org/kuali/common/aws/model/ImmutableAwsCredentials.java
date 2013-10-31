40887b2e1485132f677ac4a33df93d50e85897d3
jcaddel <jcaddel@kuali.org>
2013-10-31 17:44:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27478 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-aws/src/main/java/org/kuali/common/aws/model/ImmutableAwsCredentials.java
@@ -0,0 +1,28 @@
+package org.kuali.common.aws.model;
+
+import org.kuali.common.util.Assert;
+
+import com.amazonaws.auth.AWSCredentials;
+
+public class ImmutableAwsCredentials implements AWSCredentials {
+
+	public ImmutableAwsCredentials(String accessKey, String secretKey) {
+		Assert.noBlanks(accessKey, secretKey);
+		this.accessKey = accessKey;
+		this.secretKey = secretKey;
+	}
+
+	private final String accessKey;
+	private final String secretKey;
+
+	@Override
+	public String getAWSAccessKeyId() {
+		return accessKey;
+	}
+
+	@Override
+	public String getAWSSecretKey() {
+		return secretKey;
+	}
+
+}
