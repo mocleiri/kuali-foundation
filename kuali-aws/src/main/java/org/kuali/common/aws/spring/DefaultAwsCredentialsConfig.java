40ed8d84737033f3cab02f9ef544f3c125c6615a
jcaddel <jcaddel@kuali.org>
2013-10-31 19:23:35 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27498 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-aws/src/main/java/org/kuali/common/aws/spring/DefaultAwsCredentialsConfig.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.aws.spring;
+
+import org.kuali.common.aws.model.ImmutableAwsCredentials;
+import org.kuali.common.util.spring.env.EnvironmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+
+@Configuration
+public class DefaultAwsCredentialsConfig implements AwsCredentialsConfig {
+
+	private static final String ACCESS_KEY = "aws.accessKeyId";
+	private static final String SECRET_KEY = "aws.secretKey";
+
+	@Autowired
+	EnvironmentService env;
+
+	@Override
+	@Bean
+	public AWSCredentials awsCredentials() {
+		String accessKey = env.getString(ACCESS_KEY);
+		String secretKey = env.getString(SECRET_KEY);
+		return new ImmutableAwsCredentials(accessKey, secretKey);
+	}
+
+}
