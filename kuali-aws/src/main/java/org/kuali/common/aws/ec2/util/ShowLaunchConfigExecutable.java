9e66290223f27ad93290e1c4bae9a35adb118622
jcaddel <jcaddel@kuali.org>
2013-11-01 17:44:18 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27587 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/util/ShowLaunchConfigExecutable.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/util/ShowLaunchConfigExecutable.java
@@ -40,12 +40,14 @@ public class ShowLaunchConfigExecutable implements Executable {
 			String regionLocation = getRegionLocation(serviceContext);
 			String availabilityZone = getAvailabilityZone(instanceContext);
 
+			logger.info("---------- Launching EC2 Instance ----------");
 			logger.info("AWS Access Key ID: {}", accessKey);
 			logger.info("Region: [{} {}]", regionName, regionLocation);
 			logger.info("Availabilty Zone: {}", availabilityZone);
 			logger.info("AMI: {}", instanceContext.getAmi());
 			logger.info("Key: {}", instanceContext.getKeyName());
 			logger.info("Type: {}", instanceContext.getType().toString());
+			logger.info("--------------------------------------------");
 		}
 	}
 
