3a1b663685a4d5d9648db948e8801197b9366d80
jcaddel <jcaddel@kuali.org>
2013-10-31 22:48:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27543 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/model/AvailabilityZones.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/model/AvailabilityZones.java
@@ -2,7 +2,7 @@ package org.kuali.common.aws.model;
 
 public enum AvailabilityZones {
 
-	// This zone is in the us-east region
+	// This is a zone in the us-east region
 	US_EAST_1D("us-east-1d");
 
 	private final String value;
