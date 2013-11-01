47ff6cee13377c9643fc07038c680e4b3fe633aa
jcaddel <jcaddel@kuali.org>
2013-10-31 20:10:41 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27505 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/model/util/CredentialUtils.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/model/util/CredentialUtils.java
@@ -25,18 +25,21 @@ public class CredentialUtils {
 	}
 
 	public static AWSCredentials getCredentials(EnvironmentService env, EncryptionService enc, AWSCredentials provided) {
-		String accessKey = NullUtils.trimToNull(getValue(env, ACCESS_KEY, ACCESS_ENV_KEY));
-		String rawSecretKey = NullUtils.trimToNull(getValue(env, SECRET_KEY, SECRET_ENV_KEY));
+		String accessKey = NullUtils.trimToNull(getValue(env, ACCESS_KEY, ACCESS_ENV_KEY, provided.getAWSAccessKeyId()));
+		String rawSecretKey = NullUtils.trimToNull(getValue(env, SECRET_KEY, SECRET_ENV_KEY, provided.getAWSSecretKey()));
 		String secretKey = EncUtils.isEncrypted(rawSecretKey) ? enc.decrypt(rawSecretKey) : rawSecretKey;
 		return new ImmutableAwsCredentials(accessKey, secretKey);
 	}
 
-	protected static String getValue(EnvironmentService env, String key, String envKey) {
+	protected static String getValue(EnvironmentService env, String key, String envKey, String provided) {
 		Optional<String> value = SpringUtils.getOptionalString(env, key);
+		Optional<String> envValue = SpringUtils.getOptionalString(env, envKey);
 		if (value.isPresent()) {
 			return value.get();
+		} else if (envValue.isPresent()) {
+			return envValue.get();
 		} else {
-			return env.getString(envKey);
+			return provided;
 		}
 	}
 
