0462f561819ad0ba659b0d2f3c25b11bffe2b934
jcaddel <jcaddel@kuali.org>
2013-11-01 14:41:52 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27553 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/model/AvailabilityZones.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/model/AvailabilityZones.java
@@ -1,9 +1,12 @@
 package org.kuali.common.aws.model;
 
+import org.kuali.common.util.nullify.NullUtils;
+
 public enum AvailabilityZones {
 
 	// This is a zone in the us-east region
-	US_EAST_1D("us-east-1d");
+	US_EAST_1D("us-east-1d"), //
+	NO_PREFERENCE(NullUtils.NONE);
 
 	private final String value;
 
