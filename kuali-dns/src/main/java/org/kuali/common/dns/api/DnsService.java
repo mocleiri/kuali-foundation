a47af8d319c4214d3a708450ffa904abbb9554f7
jcaddel <jcaddel@kuali.org>
2013-11-01 21:03:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27649 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/api/DnsService.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/api/DnsService.java
@@ -1,12 +1,25 @@
 package org.kuali.common.dns.api;
 
+import java.util.List;
+
 import org.kuali.common.dns.dnsme.model.Domain;
 import org.kuali.common.dns.dnsme.model.Record;
+import org.kuali.common.dns.dnsme.model.Search;
 
 public interface DnsService {
 
 	Record addRecord(Domain domain, Record record);
 
 	Domain getDomain(String domainName);
+
+	Record getRecord(Domain domain, int recordId);
+
+	Record getRecord(Domain domain, String name);
+
+	Record getRecord(Domain domain, Search search);
+
+	List<Record> getRecords(Domain domain);
+
+	List<Record> getRecords(Domain domain, Search search);
 
 }
