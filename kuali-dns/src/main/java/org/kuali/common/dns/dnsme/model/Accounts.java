cbf6d1c4132d1e210f8836e3c6ecb5ed6fa76d68
jcaddel <jcaddel@kuali.org>
2013-11-01 20:17:41 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27632 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Accounts.java
@@ -0,0 +1,22 @@
+package org.kuali.common.dns.dnsme.model;
+
+import org.kuali.common.util.Assert;
+
+import com.google.common.collect.ImmutableList;
+
+public enum Accounts {
+
+	SANDBOX("", "");
+
+	private final Account account;
+
+	private Accounts(String apiKey, String secretKey) {
+		Assert.noBlanks(apiKey, secretKey);
+		this.account = new Account(apiKey, secretKey, ImmutableList.<Domain> of());
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+}
