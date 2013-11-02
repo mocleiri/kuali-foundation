df3089fe614a3e8b0e1d3099389e81b1d26be36a
jcaddel <jcaddel@kuali.org>
2013-11-01 20:46:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27644 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Accounts.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Accounts.java
@@ -4,7 +4,8 @@ import org.kuali.common.util.Assert;
 
 public enum Accounts {
 
-	SANDBOX("549de3da-8eae-4350-b20e-3d5c31f7117e", "ENC(tOH/wJCMfs8WrgL2TR+6zIDqvvmXvQpaN7td5WohonWW1mp5XoN+1LmZowNIn7X9)"), //
+	// Sandbox credentials expire after 90 days or so
+	SANDBOX("cce722ad-25a7-4a6d-a6d7-fceae7c0bc0c", "ENC(9jpu7hzzCx7rDbp8qxqH9GGX1bTTCkJuRuaKYWGpVpYV45IMO3SSFwXmNreQ/btT)"), //
 	PRODUCTION("454f2836-81c0-4379-b8dd-2cc6495131b5", "ENC(/Fxj5aXSbOe3AWJ5mAg1M8Lo03+ytqybZuYq3CwUWaZwBnqxEPzpR+/8n19MeRBX)");
 
 	private final Account account;
