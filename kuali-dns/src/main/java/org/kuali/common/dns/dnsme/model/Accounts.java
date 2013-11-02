cf692bd91a3aadea7e1b6f2a43f67a8c6e18241f
jcaddel <jcaddel@kuali.org>
2013-11-01 20:24:40 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27636 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Accounts.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Accounts.java
@@ -6,7 +6,8 @@ import com.google.common.collect.ImmutableList;
 
 public enum Accounts {
 
-	SANDBOX("", ""), PRODUCTION("", "");
+	SANDBOX("549de3da-8eae-4350-b20e-3d5c31f7117e", "ENC(tOH/wJCMfs8WrgL2TR+6zIDqvvmXvQpaN7td5WohonWW1mp5XoN+1LmZowNIn7X9)"), //
+	PRODUCTION("454f2836-81c0-4379-b8dd-2cc6495131b5", "ENC(/Fxj5aXSbOe3AWJ5mAg1M8Lo03+ytqybZuYq3CwUWaZwBnqxEPzpR+/8n19MeRBX)");
 
 	private final Account account;
 
