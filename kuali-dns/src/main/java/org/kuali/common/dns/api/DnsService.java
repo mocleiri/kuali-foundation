5cf818012be782a01c1951156d3fccbe76151278
jcaddel <jcaddel@kuali.org>
2013-11-02 21:16:27 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27667 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/api/DnsService.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/api/DnsService.java
@@ -2,39 +2,21 @@ package org.kuali.common.dns.api;
 
 import java.util.List;
 
-import org.kuali.common.dns.dnsme.model.Domain;
-import org.kuali.common.dns.dnsme.model.Record;
 import org.kuali.common.dns.dnsme.model.Search;
+import org.kuali.common.dns.model.DnsRecord;
 
 public interface DnsService {
 
 	boolean exists(String fqdn);
 
 	void createCNAME(String aliasDnsName, String canonicalDnsName, int timeToLiveInSeconds);
-	
+
 	void delete(String fqdn);
-	
+
 	DnsRecord getRecord(String fqdn);
-	
+
 	List<DnsRecord> getRecords();
-	
-	List<DnsRecord> getRecords(String)
-	
 
-	void deleteRecord(Domain domain, String name);
-
-	void deleteRecord(Domain domain, int recordId);
-
-	Domain getDomain(String domainName);
-
-	Record getRecord(Domain domain, int recordId);
-
-	Record getRecord(Domain domain, String name);
-
-	Record getRecord(Domain domain, Search search);
-
-	List<Record> getRecords(Domain domain);
-
-	List<Record> getRecords(Domain domain, Search search);
+	List<DnsRecord> getRecords(Search search);
 
 }
