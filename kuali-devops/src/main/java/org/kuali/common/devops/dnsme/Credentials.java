4a5ca8325f849d1435912001fa480c24334ebf7a
jcaddel <jcaddel@kuali.org>
2013-11-03 17:19:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27751 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/main/java/org/kuali/common/devops/dnsme/Credentials.java
@@ -0,0 +1,23 @@
+package org.kuali.common.devops.dnsme;
+
+import org.kuali.common.dns.dnsme.model.DNSMadeEasyCredentials;
+import org.kuali.common.util.Assert;
+
+public enum Credentials {
+
+	// Sandbox credentials expire after 90 days or so
+	SANDBOX("cce722ad-25a7-4a6d-a6d7-fceae7c0bc0c", "ENC(9jpu7hzzCx7rDbp8qxqH9GGX1bTTCkJuRuaKYWGpVpYV45IMO3SSFwXmNreQ/btT)"), //
+	PRODUCTION("454f2836-81c0-4379-b8dd-2cc6495131b5", "ENC(/Fxj5aXSbOe3AWJ5mAg1M8Lo03+ytqybZuYq3CwUWaZwBnqxEPzpR+/8n19MeRBX)");
+
+	private final DNSMadeEasyCredentials credentials;
+
+	private Credentials(String apiKey, String secretKey) {
+		Assert.noBlanks(apiKey, secretKey);
+		this.credentials = new DNSMadeEasyCredentials(apiKey, secretKey);
+	}
+
+	public DNSMadeEasyCredentials getCredentials() {
+		return credentials;
+	}
+
+}
