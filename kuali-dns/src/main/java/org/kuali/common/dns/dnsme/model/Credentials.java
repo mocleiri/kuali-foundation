f60d7c490e1b95389deda186b686503b3e1e3014
jcaddel <jcaddel@kuali.org>
2013-11-03 13:07:09 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27690 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Credentials.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Credentials.java
@@ -1,13 +1,11 @@
 package org.kuali.common.dns.dnsme.model;
 
 import org.kuali.common.util.Assert;
-import org.kuali.common.util.nullify.NullUtils;
 
 public enum Credentials {
 
 	// Sandbox credentials expire after 90 days or so
 	SANDBOX("cce722ad-25a7-4a6d-a6d7-fceae7c0bc0c", "ENC(9jpu7hzzCx7rDbp8qxqH9GGX1bTTCkJuRuaKYWGpVpYV45IMO3SSFwXmNreQ/btT)"), //
-	NONE(NullUtils.NONE, NullUtils.NONE), //
 	PRODUCTION("454f2836-81c0-4379-b8dd-2cc6495131b5", "ENC(/Fxj5aXSbOe3AWJ5mAg1M8Lo03+ytqybZuYq3CwUWaZwBnqxEPzpR+/8n19MeRBX)");
 
 	private final DNSMadeEasyCredentials credentials;
