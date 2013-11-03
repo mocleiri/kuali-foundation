33ebab3a7b0785e7cb3c4da1004a12187788edbf
jcaddel <jcaddel@kuali.org>
2013-11-03 10:11:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27671 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/api/DnsService.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/api/DnsService.java
@@ -2,21 +2,89 @@ package org.kuali.common.dns.api;
 
 import java.util.List;
 
+import org.kuali.common.dns.dnsme.model.Search;
 import org.kuali.common.dns.model.DnsRecord;
-import org.kuali.common.dns.model.Search;
 
 public interface DnsService {
 
-	DnsRecord createCNAMERecord(String alias, String canonical, int timeToLiveInSeconds);
+	/**
+	 * The domain this service performs operations on eg <code>kuali.org</code>
+	 */
+	String getDomain();
 
+	/**
+	 * Create a CNAME record from <code>aliasFQDN</code> to <code>fqdn</code>.
+	 * 
+	 * @param aliasFQDN
+	 *            The fully qualified domain name being aliased to <code>fqdn</code>
+	 * @param fqdn
+	 *            The fully qualified domain name <code>aliasFQDN</code> points to after this method returns.
+	 * @param timeToLiveInSeconds
+	 *            The amount of time (in seconds) DNS servers are allowed to cache the new CNAME record. A TTL value of 300 means if the CNAME record is changed, DNS servers around
+	 *            the world may still be showing the old value for up to 5 minutes.
+	 * 
+	 * @see getDomain
+	 * 
+	 * @throws IllegalArgumentException
+	 *             <ul>
+	 *             <li>If either <code>aliasFQDN</code> or <code>fqdn</code> are blank or are not correctly formatted fully qualified domain names.
+	 *             <li>If <code>aliasFQDN</code> does not end with the domain this service is performing operations on.</li>
+	 *             <li>If <code>timeToLiveInSeconds</code> is negative.</li>
+	 *             </ul>
+	 */
+	DnsRecord createCNAMERecord(String aliasFQDN, String fqdn, int timeToLiveInSeconds);
+
+	/**
+	 * Return true if a DNS record for <code>fqdn</code> exists, false otherwise.
+	 * 
+	 * @see getDomain
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If <code>fqdn</code> is blank, is not a property formatted DNS name, or does not end with the domain this service is performing operations on.
+	 */
 	boolean exists(String fqdn);
 
+	/**
+	 * Delete the DNS record for <code>fqdn</code>, if one exists.
+	 * 
+	 * @see getDomain
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If <code>fqdn</code> is blank, is not a property formatted DNS name, or does not end with the domain this service is performing operations on.
+	 */
 	void delete(String fqdn);
 
+	/**
+	 * Return the DNS record for <code>fqdn</code>.
+	 * 
+	 * @see getDomain
+	 * 
+	 * @throws IllegalArgumentException
+	 *             <ul>
+	 *             <li>If <code>fqdn</code> is blank, is not a property formatted DNS name, or does not end with the domain this service is performing operations on.</li>
+	 *             <li>If there is no corresponding DNS record for <code>fqdn</code></li>
+	 *             </ul>
+	 */
 	DnsRecord getRecord(String fqdn);
 
+	/**
+	 * Return a list of all the DNS records for the domain this service is performing operations on.
+	 * 
+	 * @see getDomain
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If <code>fqdn</code> is blank, is not a property formatted DNS name, or does not end with the domain this service is performing operations on.
+	 */
 	List<DnsRecord> getRecords();
 
+	/**
+	 * Return a list of any DNS records matching the search criteria for the domain this service is performing operations on.
+	 * 
+	 * @see getDomain
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If <code>searchCritera</code> is null.
+	 */
 	List<DnsRecord> getRecords(Search search);
 
 }
--- kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Search.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/model/Search.java
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.common.dns.model;
+package org.kuali.common.dns.dnsme.model;
 
-import org.kuali.common.dns.dnsme.model.GTDLocation;
+import org.kuali.common.dns.model.DnsRecordType;
 
 public class Search {
 
