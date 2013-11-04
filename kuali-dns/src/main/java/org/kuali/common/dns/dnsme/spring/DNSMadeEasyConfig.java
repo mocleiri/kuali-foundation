a1e4d218497e8794acf450ac983e4e6ceee8a65a
jcaddel <jcaddel@kuali.org>
2013-11-04 11:54:48 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27804 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/dnsme/spring/DNSMadeEasyConfig.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/spring/DNSMadeEasyConfig.java
@@ -4,9 +4,6 @@ import org.kuali.common.dns.api.DnsService;
 import org.kuali.common.dns.dnsme.DNSMadeEasyDnsService;
 import org.kuali.common.dns.dnsme.model.DNSMadeEasyServiceContext;
 import org.kuali.common.dns.spring.DnsServiceConfig;
-import org.kuali.common.util.enc.EncryptionService;
-import org.kuali.common.util.enc.spring.DefaultEncryptionServiceConfig;
-import org.kuali.common.util.spring.env.EnvironmentService;
 import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,14 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ SpringServiceConfig.class, DefaultEncryptionServiceConfig.class })
+@Import({ SpringServiceConfig.class })
 public class DNSMadeEasyConfig implements DnsServiceConfig {
-
-	@Autowired
-	EnvironmentService env;
-
-	@Autowired
-	EncryptionService enc;
 
 	@Autowired
 	DNSMadeEasyServiceContext context;
