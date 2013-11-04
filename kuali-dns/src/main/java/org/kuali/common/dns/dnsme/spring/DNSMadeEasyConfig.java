ab679cbdacef4aecf2dee5eb936fdb1da3813b1f
jcaddel <jcaddel@kuali.org>
2013-11-04 11:56:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27805 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/dnsme/spring/DNSMadeEasyConfig.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/spring/DNSMadeEasyConfig.java
@@ -4,22 +4,20 @@ import org.kuali.common.dns.api.DnsService;
 import org.kuali.common.dns.dnsme.DNSMadeEasyDnsService;
 import org.kuali.common.dns.dnsme.model.DNSMadeEasyServiceContext;
 import org.kuali.common.dns.spring.DnsServiceConfig;
-import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ SpringServiceConfig.class })
 public class DNSMadeEasyConfig implements DnsServiceConfig {
 
 	@Autowired
-	DNSMadeEasyServiceContext context;
+	DNSMadeEasyContextConfig config;
 
 	@Override
 	@Bean
 	public DnsService dnsService() {
+		DNSMadeEasyServiceContext context = config.dnsMadeEasyServiceContext();
 		return new DNSMadeEasyDnsService(context);
 	}
 
