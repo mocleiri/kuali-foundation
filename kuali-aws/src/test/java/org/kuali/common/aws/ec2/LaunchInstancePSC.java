6dc61d7c9e5d55ecd476aa98531a8e92886f4229
jcaddel <jcaddel@kuali.org>
2013-11-04 13:14:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27827 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/test/java/org/kuali/common/aws/ec2/LaunchInstancePSC.java
+++ kuali-aws/src/test/java/org/kuali/common/aws/ec2/LaunchInstancePSC.java
@@ -1,41 +1,17 @@
 package org.kuali.common.aws.ec2;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.kuali.common.aws.project.KualiAwsProjectConstants;
-import org.kuali.common.aws.project.spring.AwsProjectConfig;
-import org.kuali.common.util.project.ProjectUtils;
-import org.kuali.common.util.project.model.Project;
-import org.kuali.common.util.properties.Location;
-import org.kuali.common.util.properties.PropertiesService;
-import org.kuali.common.util.properties.spring.DefaultPropertiesServiceConfig;
 import org.kuali.common.util.spring.PropertySourceUtils;
 import org.kuali.common.util.spring.service.PropertySourceConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.env.PropertySource;
 
 @Configuration
-@Import({ AwsProjectConfig.class, DefaultPropertiesServiceConfig.class })
 public class LaunchInstancePSC implements PropertySourceConfig {
-
-	@Autowired
-	Project project;
-
-	@Autowired
-	PropertiesService service;
 
 	@Override
 	@Bean
 	public PropertySource<?> propertySource() {
-		String encoding = ProjectUtils.getEncoding(project);
-		String prefix = ProjectUtils.getClasspathPrefix(KualiAwsProjectConstants.PROJECT_ID);
-		List<Location> locations = new ArrayList<Location>();
-		locations.add(new Location(prefix + "/foundation.properties", encoding));
-		locations.add(new Location(prefix + "/ci.properties", encoding));
-		return PropertySourceUtils.getPropertySource(service, locations);
+		return PropertySourceUtils.getDefaultPropertySource();
 	}
 }
