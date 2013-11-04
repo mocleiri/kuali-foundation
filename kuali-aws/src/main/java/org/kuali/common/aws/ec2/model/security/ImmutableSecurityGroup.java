4fd1f1e58105cba0e1473ca0903c979a09aee98e
jcaddel <jcaddel@kuali.org>
2013-11-04 15:50:42 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27845 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/security/ImmutableSecurityGroup.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/security/ImmutableSecurityGroup.java
@@ -1,14 +1,18 @@
 package org.kuali.common.aws.ec2.model.security;
 
+import java.util.List;
+
 import org.kuali.common.util.Assert;
 import org.kuali.common.util.nullify.NullUtils;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 
 public final class ImmutableSecurityGroup {
 
 	private final String name;
 	private final Optional<String> description;
+	private final List<ImmutableIpPermission> permissions;
 
 	public static class Builder {
 
@@ -17,6 +21,7 @@ public final class ImmutableSecurityGroup {
 
 		// Optional
 		private Optional<String> description = Optional.absent();
+		private List<ImmutableIpPermission> permissions = ImmutableList.of();
 
 		public Builder(String name) {
 			this.name = name;
@@ -27,9 +32,15 @@ public final class ImmutableSecurityGroup {
 			return this;
 		}
 
+		public Builder permissions(List<ImmutableIpPermission> permissions) {
+			this.permissions = permissions;
+			return this;
+		}
+
 		public ImmutableSecurityGroup build() {
 			Assert.noBlanks(name);
-			Assert.noNulls(description);
+			Assert.noNulls(description, permissions);
+			this.permissions = ImmutableList.copyOf(permissions);
 			return new ImmutableSecurityGroup(this);
 		}
 	}
@@ -37,6 +48,7 @@ public final class ImmutableSecurityGroup {
 	private ImmutableSecurityGroup(Builder builder) {
 		this.name = builder.name;
 		this.description = builder.description;
+		this.permissions = builder.permissions;
 	}
 
 	public String getName() {
@@ -45,6 +57,10 @@ public final class ImmutableSecurityGroup {
 
 	public Optional<String> getDescription() {
 		return description;
+	}
+
+	public List<ImmutableIpPermission> getPermissions() {
+		return permissions;
 	}
 
 }
