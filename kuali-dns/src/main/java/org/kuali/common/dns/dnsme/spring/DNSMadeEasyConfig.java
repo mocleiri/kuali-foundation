2fcf7f4946611fc41b0471eb09948f54e5d52cf8
jcaddel <jcaddel@kuali.org>
2013-11-03 22:23:09 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27771 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/dnsme/spring/DNSMadeEasyConfig.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/spring/DNSMadeEasyConfig.java
@@ -2,13 +2,8 @@ package org.kuali.common.dns.dnsme.spring;
 
 import org.kuali.common.dns.api.DnsService;
 import org.kuali.common.dns.dnsme.DNSMadeEasyService;
-import org.kuali.common.dns.dnsme.URLS;
-import org.kuali.common.dns.dnsme.model.Credentials;
-import org.kuali.common.dns.dnsme.model.DNSMadeEasyCredentials;
 import org.kuali.common.dns.dnsme.model.DNSMadeEasyServiceContext;
 import org.kuali.common.dns.spring.DnsServiceConfig;
-import org.kuali.common.dns.spring.DomainNameConfig;
-import org.kuali.common.util.enc.EncUtils;
 import org.kuali.common.util.enc.EncryptionService;
 import org.kuali.common.util.enc.spring.DefaultEncryptionServiceConfig;
 import org.kuali.common.util.spring.env.EnvironmentService;
@@ -22,11 +17,6 @@ import org.springframework.context.annotation.Import;
 @Import({ SpringServiceConfig.class, DefaultEncryptionServiceConfig.class })
 public class DNSMadeEasyConfig implements DnsServiceConfig {
 
-	private static final String API_KEY = "dnsme.apiKey";
-	private static final String SECRET_KEY = "dnsme.secretKey";
-	private static final String URL_KEY = "dnsme.url";
-	private static final String DOMAIN_KEY = "dnsme.domain";
-
 	@Autowired
 	EnvironmentService env;
 
@@ -34,26 +24,12 @@ public class DNSMadeEasyConfig implements DnsServiceConfig {
 	EncryptionService enc;
 
 	@Autowired
-	DomainNameConfig config;
+	DNSMadeEasyServiceContext context;
 
 	@Override
 	@Bean
 	public DnsService dnsService() {
-		DNSMadeEasyServiceContext context = dnsMadeEasyServiceContext();
 		return new DNSMadeEasyService(context);
 	}
 
-	@Bean
-	public DNSMadeEasyServiceContext dnsMadeEasyServiceContext() {
-		DNSMadeEasyCredentials encrypted = Credentials.PRODUCTION.getCredentials();
-		String url = env.getString(URL_KEY, URLS.PRODUCTION);
-		String apiKey = env.getString(API_KEY, encrypted.getApiKey());
-		String secretKey = env.getString(SECRET_KEY, encrypted.getSecretKey());
-		if (EncUtils.isEncrypted(secretKey)) {
-			secretKey = enc.decrypt(secretKey);
-		}
-		DNSMadeEasyCredentials credentials = new DNSMadeEasyCredentials(apiKey, secretKey);
-		String domainName = env.getString(DOMAIN_KEY, config.domainName());
-		return new DNSMadeEasyServiceContext(credentials, url, domainName);
-	}
 }
---
+++ kuali-dns/src/main/java/org/kuali/common/dns/dnsme/spring/DNSMadeEasyContextConfig.java
@@ -0,0 +1,9 @@
+package org.kuali.common.dns.dnsme.spring;
+
+import org.kuali.common.dns.dnsme.model.DNSMadeEasyServiceContext;
+
+public interface DNSMadeEasyContextConfig {
+
+	DNSMadeEasyServiceContext dnsMadeEasyServiceContext();
+
+}
