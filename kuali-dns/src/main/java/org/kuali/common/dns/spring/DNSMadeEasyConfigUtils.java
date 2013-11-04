03b261b6ca4f97d6205d8fb420f1d54f00d46695
jcaddel <jcaddel@kuali.org>
2013-11-03 22:16:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27765 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-dns/src/main/java/org/kuali/common/dns/spring/DNSMadeEasyConfigUtils.java
+++ kuali-dns/src/main/java/org/kuali/common/dns/spring/DNSMadeEasyConfigUtils.java
@@ -3,11 +3,11 @@ package org.kuali.common.dns.spring;
 import org.kuali.common.dns.api.DnsService;
 import org.kuali.common.dns.dnsme.DNSMadeEasyService;
 import org.kuali.common.dns.dnsme.URLS;
-import org.kuali.common.dns.dnsme.model.Credentials;
 import org.kuali.common.dns.dnsme.model.DNSMadeEasyCredentials;
 import org.kuali.common.dns.dnsme.model.DNSMadeEasyServiceContext;
 import org.kuali.common.util.enc.EncUtils;
 import org.kuali.common.util.enc.EncryptionService;
+import org.kuali.common.util.nullify.NullUtils;
 import org.kuali.common.util.spring.env.EnvironmentService;
 
 public class DNSMadeEasyConfigUtils {
@@ -17,21 +17,23 @@ public class DNSMadeEasyConfigUtils {
 	private static final String URL_KEY = "dnsme.url";
 	private static final String DOMAIN_KEY = "dnsme.domain";
 
-	public static DnsService getService(EnvironmentService env, EncryptionService enc, String domainName) {
-		DNSMadeEasyServiceContext context = getServiceContext(env, enc, domainName);
+	private static final DNSMadeEasyServiceContext NONE = new DNSMadeEasyServiceContext(new DNSMadeEasyCredentials(NullUtils.NONE, NullUtils.NONE), URLS.PRODUCTION, NullUtils.NONE);
+
+	public static DnsService getService(EnvironmentService env, EncryptionService enc) {
+		DNSMadeEasyServiceContext context = getServiceContext(env, enc, NONE);
 		return new DNSMadeEasyService(context);
 	}
 
-	public static DNSMadeEasyServiceContext getServiceContext(EnvironmentService env, EncryptionService enc, String defaultDomainName) {
-		DNSMadeEasyCredentials encrypted = Credentials.PRODUCTION.getCredentials();
-		String url = env.getString(URL_KEY, URLS.PRODUCTION);
-		String apiKey = env.getString(API_KEY, encrypted.getApiKey());
-		String secretKey = env.getString(SECRET_KEY, encrypted.getSecretKey());
+	public static DNSMadeEasyServiceContext getServiceContext(EnvironmentService env, EncryptionService enc, DNSMadeEasyServiceContext provided) {
+		String url = NullUtils.trimToNull(env.getString(URL_KEY, provided.getRestApiUrl()));
+		DNSMadeEasyCredentials encrypted = provided.getCredentials();
+		String apiKey = NullUtils.trimToNull(env.getString(API_KEY, encrypted.getApiKey()));
+		String secretKey = NullUtils.trimToNull(env.getString(SECRET_KEY, encrypted.getSecretKey()));
 		if (EncUtils.isEncrypted(secretKey)) {
 			secretKey = enc.decrypt(secretKey);
 		}
 		DNSMadeEasyCredentials credentials = new DNSMadeEasyCredentials(apiKey, secretKey);
-		String domainName = env.getString(DOMAIN_KEY, defaultDomainName);
+		String domainName = NullUtils.trimToNull(env.getString(DOMAIN_KEY, provided.getDomainName()));
 		return new DNSMadeEasyServiceContext(credentials, url, domainName);
 	}
 }
