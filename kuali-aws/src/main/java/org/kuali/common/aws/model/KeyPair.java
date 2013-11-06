6023daf7474cb7e34c20bfd496cc7efcbc9f770e
jcaddel <jcaddel@kuali.org>
2013-11-05 21:10:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@27969 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/model/KeyPair.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/model/KeyPair.java
@@ -2,10 +2,24 @@ package org.kuali.common.aws.model;
 
 import org.kuali.common.util.Assert;
 
+import com.google.common.base.Optional;
+
 public final class KeyPair {
 
+	public KeyPair(String name, String publicKey) {
+		this(name, publicKey, Optional.<String> absent());
+	}
+
 	public KeyPair(String name, String publicKey, String privateKey) {
-		Assert.noBlanks(name, publicKey, privateKey);
+		this(name, publicKey, Optional.of(privateKey));
+	}
+
+	public KeyPair(String name, String publicKey, Optional<String> privateKey) {
+		Assert.noBlanks(name, publicKey);
+		Assert.noNulls(privateKey);
+		if (privateKey.isPresent()) {
+			Assert.noBlanks(privateKey.get());
+		}
 		this.name = name;
 		this.publicKey = publicKey;
 		this.privateKey = privateKey;
@@ -13,7 +27,7 @@ public final class KeyPair {
 
 	private final String name;
 	private final String publicKey;
-	private final String privateKey;
+	private final Optional<String> privateKey;
 
 	public String getName() {
 		return name;
@@ -23,7 +37,7 @@ public final class KeyPair {
 		return publicKey;
 	}
 
-	public String getPrivateKey() {
+	public Optional<String> getPrivateKey() {
 		return privateKey;
 	}
 
