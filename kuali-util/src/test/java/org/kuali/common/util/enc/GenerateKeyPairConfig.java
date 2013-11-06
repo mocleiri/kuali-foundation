01411984a087fe9758439adf5bc97d24510c3788
jcaddel <jcaddel@kuali.org>
2013-11-06 13:53:21 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28023 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/enc/GenerateKeyPairConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/enc/GenerateKeyPairConfig.java
@@ -15,7 +15,8 @@
  */
 package org.kuali.common.util.enc;
 
-import java.security.KeyPairGenerator;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.kuali.common.util.spring.service.SpringServiceConfig;
@@ -23,6 +24,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.KeyPair;
 
 @Configuration
 @Import({ SpringServiceConfig.class })
@@ -35,12 +39,29 @@ public class GenerateKeyPairConfig {
 	@Bean
 	public Object execute() {
 		try {
-			KeyPairGenerator kpg = KeyPairGenerator.getInstance("SSH-2", "RSA");
-			System.out.println(kpg);
+			int type = KeyPair.RSA;
+			JSch jsch = new JSch();
+			KeyPair keyPair = KeyPair.genKeyPair(jsch, type);
+			String name = "winnie-the-pooh";
+			String publicKey = getPublicKey(keyPair, name);
+			String privateKey = getPrivateKey(keyPair);
+			System.out.println(publicKey);
+			System.out.println(privateKey);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
+	protected String getPrivateKey(KeyPair keyPair) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		keyPair.writePrivateKey(out);
+		return out.toString("UTF-8");
+	}
+
+	protected String getPublicKey(KeyPair keyPair, String name) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		keyPair.writePublicKey(out, name);
+		return out.toString("UTF-8");
+	}
 }
