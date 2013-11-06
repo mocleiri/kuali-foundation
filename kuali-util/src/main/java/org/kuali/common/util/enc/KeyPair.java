82a2d8dca72eff6bbae05d8e5d9c9153fd831774
jcaddel <jcaddel@kuali.org>
2013-11-06 14:21:47 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28031 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/enc/KeyPair.java
+++ kuali-util/src/main/java/org/kuali/common/util/enc/KeyPair.java
@@ -1,44 +1,66 @@
 package org.kuali.common.util.enc;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.nullify.NullUtils;
 
 import com.google.common.base.Optional;
 
 public final class KeyPair {
 
-	public KeyPair(String name) {
-		this(name, Optional.<String> absent());
-	}
-
-	public KeyPair(String name, String publicKey) {
-		this(name, Optional.of(publicKey));
-	}
-
-	public KeyPair(String name, Optional<String> publicKey) {
-		this(name, publicKey, Optional.<String> absent());
-	}
-
-	public KeyPair(String name, String publicKey, String privateKey) {
-		this(name, Optional.of(publicKey), Optional.of(privateKey));
-	}
-
-	public KeyPair(String name, Optional<String> publicKey, Optional<String> privateKey) {
-		Assert.noBlanks(name);
-		Assert.noNulls(publicKey, privateKey);
-		if (publicKey.isPresent()) {
-			Assert.noBlanks(publicKey.get());
-		}
-		if (privateKey.isPresent()) {
-			Assert.noBlanks(privateKey.get());
-		}
-		this.name = name;
-		this.publicKey = publicKey;
-		this.privateKey = privateKey;
-	}
-
 	private final String name;
 	private final Optional<String> publicKey;
 	private final Optional<String> privateKey;
+	private final Optional<String> fingerprint;
+
+	public static class Builder {
+
+		// Required
+		private final String name;
+
+		// Optional
+		private Optional<String> publicKey = Optional.absent();
+		private Optional<String> privateKey = Optional.absent();
+		private Optional<String> fingerprint = Optional.absent();
+
+		public Builder(String name) {
+			this.name = name;
+		}
+
+		public Builder publicKey(String publicKey) {
+			this.publicKey = Optional.fromNullable(NullUtils.trimToNull(publicKey));
+			return this;
+		}
+
+		public Builder fingerprint(String fingerprint) {
+			this.fingerprint = Optional.fromNullable(NullUtils.trimToNull(fingerprint));
+			return this;
+		}
+
+		public Builder privateKey(String privateKey) {
+			this.privateKey = Optional.fromNullable(NullUtils.trimToNull(privateKey));
+			return this;
+		}
+
+		public KeyPair build() {
+			Assert.noBlanks(name);
+			Assert.noNulls(publicKey, privateKey);
+			if (publicKey.isPresent()) {
+				Assert.noBlanks(publicKey.get());
+			}
+			if (privateKey.isPresent()) {
+				Assert.noBlanks(privateKey.get());
+			}
+			return new KeyPair(this);
+		}
+
+	}
+
+	private KeyPair(Builder builder) {
+		this.name = builder.name;
+		this.publicKey = builder.publicKey;
+		this.privateKey = builder.privateKey;
+		this.fingerprint = builder.fingerprint;
+	}
 
 	public String getName() {
 		return name;
@@ -50,6 +72,10 @@ public final class KeyPair {
 
 	public Optional<String> getPrivateKey() {
 		return privateKey;
+	}
+
+	public Optional<String> getFingerprint() {
+		return fingerprint;
 	}
 
 }
