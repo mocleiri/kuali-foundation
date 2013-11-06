0214a6358d77864fd505b514a9b5032eb9d26880
jcaddel <jcaddel@kuali.org>
2013-11-06 13:25:11 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28017 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/properties/spring/ProjectPropertySourceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/properties/spring/ProjectPropertySourceConfig.java
@@ -1,26 +1,26 @@
 package org.kuali.common.util.properties.spring;
 
-import org.kuali.common.util.properties.PropertiesService;
+import org.kuali.common.util.project.model.Project;
+import org.kuali.common.util.project.spring.AutowiredProjectConfig;
 import org.kuali.common.util.spring.PropertySourceUtils;
 import org.kuali.common.util.spring.service.PropertySourceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.PropertySource;
 
 @Configuration
+@Import({ AutowiredProjectConfig.class })
 public class ProjectPropertySourceConfig implements PropertySourceConfig {
 
 	@Autowired
-	PropertyLocationsConfig locationsConfig;
-
-	@Autowired
-	PropertiesService service;
+	Project project;
 
 	@Override
 	@Bean
 	public PropertySource<?> propertySource() {
-		return PropertySourceUtils.getPropertySource(service, locationsConfig.propertyLocations());
+		return PropertySourceUtils.getPropertySource(project.getProperties());
 	}
 
 }
