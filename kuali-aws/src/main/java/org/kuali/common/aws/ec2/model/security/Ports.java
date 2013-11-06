575fd656561d94ba33eb05189303b61e52727db0
jcaddel <jcaddel@kuali.org>
2013-11-06 11:48:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28003 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/security/Ports.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/security/Ports.java
@@ -1,5 +1,7 @@
 package org.kuali.common.aws.ec2.model.security;
 
+import org.kuali.common.util.Assert;
+
 public enum Ports {
 
 	SSH(22), HTTP(80), HTTPS(443);
@@ -7,6 +9,7 @@ public enum Ports {
 	private final int value;
 
 	private Ports(int value) {
+		Assert.isPort(value);
 		this.value = value;
 	}
 
