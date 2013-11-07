404b96405932decdb2b58e857d450fe91559a5fb
jcaddel <jcaddel@kuali.org>
2013-11-07 17:46:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28201 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMIs.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMIs.java
@@ -3,6 +3,7 @@ package org.kuali.common.aws.ec2.model;
 public enum AMIs {
 
 	// This is a bare bones Amazon Linux box with virtually nothing except yum installed on it
+	// amzn-ami-minimal-pv-2013.09.0.x86_64-ebs
 	AMAZON_LINUX_64_BIT_MINIMAL_AMI_2013_09("ami-65792c0c");
 
 	private final String id;
