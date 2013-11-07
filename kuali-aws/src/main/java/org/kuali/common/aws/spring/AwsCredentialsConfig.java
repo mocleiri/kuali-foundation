079c6a73836121cbd07bfafca37686d62f1bea8b
jcaddel <jcaddel@kuali.org>
2013-11-07 16:52:25 -0500

    [maven-release-plugin]  copy for tag kuali-pom-3.3.31
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-pom-3.3.31@28184 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/spring/AwsCredentialsConfig.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common.aws.spring;
-
-import com.amazonaws.auth.AWSCredentials;
-
-public interface AwsCredentialsConfig {
-
-	AWSCredentials awsCredentials();
-
-}
--- kuali-util/src/main/java/org/kuali/common/util/channel/spring/SecureChannelConfig.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common.util.channel.spring;
-
-import org.kuali.common.util.channel.api.SecureChannelService;
-
-public interface SecureChannelConfig {
-
-	SecureChannelService secureChannelService();
-
-}
