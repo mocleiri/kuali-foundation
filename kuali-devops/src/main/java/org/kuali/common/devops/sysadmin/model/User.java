26d19fd48ed3236f64317105864fb052742e58ce
jcaddel <jcaddel@kuali.org>
2013-11-07 18:29:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28209 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/main/java/org/kuali/common/devops/sysadmin/model/User.java
@@ -0,0 +1,34 @@
+package org.kuali.common.devops.sysadmin.model;
+
+import org.kuali.common.util.Assert;
+
+public final class User {
+
+	public User(String login, String home) {
+		this(login, home, home + "/.ssh/authorized_keys");
+	}
+
+	public User(String login, String home, String authorizedKeys) {
+		Assert.noBlanks(login, home, authorizedKeys);
+		this.login = login;
+		this.home = home;
+		this.authorizedKeys = authorizedKeys;
+	}
+
+	private final String login;
+	private final String home;
+	private final String authorizedKeys;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getHome() {
+		return home;
+	}
+
+	public String getAuthorizedKeys() {
+		return authorizedKeys;
+	}
+
+}
