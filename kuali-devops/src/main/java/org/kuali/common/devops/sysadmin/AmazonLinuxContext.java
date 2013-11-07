43c58473948e5b9600bf4eec3de79a8f8b6dc4dc
jcaddel <jcaddel@kuali.org>
2013-11-07 18:43:21 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28214 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/sysadmin/AmazonLinuxContext.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/sysadmin/AmazonLinuxContext.java
@@ -3,6 +3,7 @@ package org.kuali.common.devops.sysadmin;
 import org.kuali.common.devops.sysadmin.model.User;
 import org.kuali.common.util.Assert;
 import org.kuali.common.util.channel.api.SecureChannelService;
+import org.kuali.common.util.enc.KeyPair;
 
 public final class AmazonLinuxContext {
 
@@ -11,25 +12,30 @@ public final class AmazonLinuxContext {
 	private final User root;
 	private final String dnsName;
 	private final String rootVolumeDeviceName;
+	private final KeyPair keyPair;
 
 	public static class Builder {
 
 		// Required
 		private final SecureChannelService service;
 		private final String dnsName;
+		private final KeyPair keyPair;
 
 		// Optional
 		private User ec2User = new User("ec2-user", "/home/ec2-user");
 		private User root = new User("root", "/root");
 		private String rootVolumeDeviceName = "/dev/xvda1";
 
-		public Builder(SecureChannelService service, String dnsName) {
+		public Builder(SecureChannelService service, String dnsName, KeyPair keyPair) {
 			this.service = service;
 			this.dnsName = dnsName;
+			this.keyPair = keyPair;
 		}
 
 		public AmazonLinuxContext build() {
-			Assert.noNulls(service);
+			Assert.noNulls(service, keyPair);
+			Assert.isTrue(keyPair.getPublicKey().isPresent(), "Public key is required");
+			Assert.isTrue(keyPair.getPrivateKey().isPresent(), "Private key is required");
 			return new AmazonLinuxContext(this);
 		}
 	}
@@ -40,6 +46,7 @@ public final class AmazonLinuxContext {
 		this.root = builder.root;
 		this.dnsName = builder.dnsName;
 		this.rootVolumeDeviceName = builder.rootVolumeDeviceName;
+		this.keyPair = builder.keyPair;
 	}
 
 	public SecureChannelService getService() {
@@ -60,6 +67,10 @@ public final class AmazonLinuxContext {
 
 	public String getRootVolumeDeviceName() {
 		return rootVolumeDeviceName;
+	}
+
+	public KeyPair getKeyPair() {
+		return keyPair;
 	}
 
 }
--- kuali-devops/src/main/java/org/kuali/common/devops/sysadmin/AmazonLinuxService.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/sysadmin/AmazonLinuxService.java
@@ -1,10 +1,19 @@
 package org.kuali.common.devops.sysadmin;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.channel.api.SecureChannel;
+import org.kuali.common.util.channel.model.ChannelContext;
+import org.kuali.common.util.channel.model.RemoteFile;
+import org.kuali.common.util.channel.util.ChannelUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class AmazonLinuxService implements SysAdminService {
+
+	private static final Logger logger = LoggerFactory.getLogger(AmazonLinuxService.class);
 
 	public AmazonLinuxService(AmazonLinuxContext context) {
 		Assert.noNulls(context);
@@ -15,6 +24,29 @@ public final class AmazonLinuxService implements SysAdminService {
 
 	@Override
 	public void enableRootSSH() {
+		SecureChannel channel = null;
+		try {
+			String src = "classpath:org/kuali/common/kuali-devops/amazon-linux/2013.09/etc/ssh/sshd_config";
+			String path = "/home/ec2-user/sshd_config";
+			String command1 = "sudo cp /home/ec2-user/.ssh/authorized_keys /root/.ssh/authorized_keys";
+			String command2 = "sudo cp " + path + " /etc/ssh/sshd_config";
+			String command3 = "sudo service sshd restart";
+
+			ChannelContext cc = getEC2UserContext(instance, context.getKeyPair().getPrivateKey().get());
+			channel = context.getService().getChannel(cc);
+			RemoteFile dst = new RemoteFile.Builder(path).build();
+			exec(channel, command1); // copy authorized_keys from ec2-user to root as that version does not have the header commands blocking ssh
+			logger.info("cp {} {}", src, dst.getAbsolutePath());
+			channel.copyLocationToFile(src, dst); // copy the updated sshd_config file into the ec2-users home directory
+			exec(channel, command2); // copy the updated sshd_config file to /etc/ssh/sshd_config
+			exec(channel, command3); // restart the sshd service
+			logger.info("rm {}", path);
+			channel.deleteFile(path); // delete the sshd_config file we left in the ec2-users home directory
+		} catch (IOException e) {
+			throw new IllegalStateException("Unexpected IO error", e);
+		} finally {
+			ChannelUtils.closeQuietly(channel);
+		}
 	}
 
 	@Override
