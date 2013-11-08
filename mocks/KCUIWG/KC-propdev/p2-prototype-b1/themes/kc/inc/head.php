a185b048d63289389b0d701a785ea06a5003ba2d
thrclark <thrclark@kuali.org>
2013-11-08 12:28:45 -0500

    Root path fix
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@28277 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/head.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/head.php
@@ -18,13 +18,13 @@ session_start();
 <meta name="author" content="" />
 
 <!--  Styles -->
-<link href="<?php echo ROOT ?>themes/bootstrap/css/bootstrap.css" rel="stylesheet" />
-<link href="<?php echo ROOT ?>themes/kc/js/fancybox/jquery.fancybox.css" rel="stylesheet" media="screen" />
-<link href="<?php echo ROOT ?>themes/kc/css/smoothness/jquery-ui-1.9.2.custom.min.css" rel="stylesheet" media="screen" />
-<link href="<?php echo ROOT ?>themes/kc/css/jquery.multiselect.css" rel="stylesheet" media="screen" />
-<link href="<?php echo ROOT ?>themes/kc/css/jquery.multiselect.filter.css" rel="stylesheet" media="screen" />
-<link href="<?php echo ROOT ?>themes/kc/css/custom.css" rel="stylesheet" />
-<link href="<?php echo ROOT ?>themes/kc/icons/style.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $ROOT ?>themes/bootstrap/css/bootstrap.css" rel="stylesheet" />
+<link href="<?php echo $ROOT ?>themes/kc/js/fancybox/jquery.fancybox.css" rel="stylesheet" media="screen" />
+<link href="<?php echo $ROOT ?>themes/kc/css/smoothness/jquery-ui-1.9.2.custom.min.css" rel="stylesheet" media="screen" />
+<link href="<?php echo $ROOT ?>themes/kc/css/jquery.multiselect.css" rel="stylesheet" media="screen" />
+<link href="<?php echo $ROOT ?>themes/kc/css/jquery.multiselect.filter.css" rel="stylesheet" media="screen" />
+<link href="<?php echo $ROOT ?>themes/kc/css/custom.css" rel="stylesheet" />
+<link href="<?php echo $ROOT ?>themes/kc/icons/style.css" rel="stylesheet" type="text/css" />
 
 <!-- Scripts, ideally we'd load these in the footer and not use in-line scripting -->
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
