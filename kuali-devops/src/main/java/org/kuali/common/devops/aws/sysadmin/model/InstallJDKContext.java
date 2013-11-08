1f66c24597791573a119d5f4b04e922911a87735
jcaddel <jcaddel@kuali.org>
2013-11-08 14:04:50 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28301 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/InstallJDKContext.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/InstallJDKContext.java
@@ -4,12 +4,17 @@ import java.io.File;
 
 import org.kuali.common.util.Assert;
 import org.kuali.common.util.channel.api.SecureChannelService;
+import org.kuali.common.util.channel.model.ChannelContext;
 import org.kuali.common.util.maven.RepositoryUtils;
 import org.kuali.common.util.maven.model.Artifact;
 
 public final class InstallJDKContext {
 
+	private final SecureChannelService service;
+	private final ChannelContext context;
 	private final Artifact artifact;
+	private final File localRepositoryDir;
+	private final String remoteJavaDir;
 
 	public static class Builder {
 
@@ -19,38 +24,63 @@ public final class InstallJDKContext {
 		private static final String TYPE = "zip";
 
 		// Required
-		private final Artifact artifact;
 		private final SecureChannelService service;
-		private final String privateKey;
-		private final String hostname;
+		private final ChannelContext context;
+		private final Artifact artifact;
 
 		// Optional
 		private File localRepositoryDir = RepositoryUtils.getDefaultLocalRepository();
 		private String remoteJavaDir = "/usr/local/java";
-		private String username = "root";
 
-		public Builder(JDKLevel level, String version) {
-			this.artifact = new Artifact.Builder(GROUP_ID, ARTIFACT_ID_PREFIX + level.getVersion(), version).classifier(CLASSIFIER).type(TYPE).build();
+		public Builder(SecureChannelService service, ChannelContext context, JDKLevel level, String version) {
+			this(service, context, getDefaultArtifact(level, version));
 		}
 
-		public Builder(Artifact artifact) {
+		public Builder(SecureChannelService service, ChannelContext context, Artifact artifact) {
 			this.artifact = artifact;
+			this.service = service;
+			this.context = context;
+		}
+
+		private static Artifact getDefaultArtifact(JDKLevel level, String version) {
+			Assert.noNulls(level);
+			Assert.noBlanks(version);
+			return new Artifact.Builder(GROUP_ID, ARTIFACT_ID_PREFIX + level.getVersion(), version).classifier(CLASSIFIER).type(TYPE).build();
 		}
 
 		public InstallJDKContext build() {
 			Assert.noNulls(artifact, localRepositoryDir);
-			Assert.exists(localRepositoryDir);
-			Assert.isTrue(RepositoryUtils.exists(localRepositoryDir, artifact));
+			Assert.noBlanks(remoteJavaDir);
 			return new InstallJDKContext(this);
 		}
 	}
 
 	private InstallJDKContext(Builder builder) {
 		this.artifact = builder.artifact;
+		this.service = builder.service;
+		this.context = builder.context;
+		this.localRepositoryDir = builder.localRepositoryDir;
+		this.remoteJavaDir = builder.remoteJavaDir;
 	}
 
 	public Artifact getArtifact() {
 		return artifact;
+	}
+
+	public SecureChannelService getService() {
+		return service;
+	}
+
+	public ChannelContext getContext() {
+		return context;
+	}
+
+	public File getLocalRepositoryDir() {
+		return localRepositoryDir;
+	}
+
+	public String getRemoteJavaDir() {
+		return remoteJavaDir;
 	}
 
 }
