3044fdfb6837d1992a2ffb94adc3555e1dce7a89
jcaddel <jcaddel@kuali.org>
2013-11-08 11:59:54 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28271 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/Regions.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/Regions.java
@@ -49,7 +49,7 @@ public enum Regions {
 	 * </p>
 	 * 
 	 * <p>
-	 * The AWS Management Console uses US West (Oregon) Region as its default. Therefore, when using both be sure to explicitly specify the same region.
+	 * The AWS Management Console uses US West (Oregon) Region as its default. Therefore, when using both be sure to explicitly specify the region you want.
 	 * </p>
 	 */
 	public static final Regions DEFAULT_REGION = US_EAST_1;
