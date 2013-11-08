8a4f84734c426bd47ebd6bbf36e4e00d96c921f4
jcaddel <jcaddel@kuali.org>
2013-11-08 11:39:06 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28264 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/Users.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/Users.java
@@ -2,17 +2,23 @@ package org.kuali.common.aws.ec2.model;
 
 public enum Users {
 
-	EC2USER("ec2-user"), //
-	ROOT("root"); //
+	EC2USER("ec2-user", "/home/ec2-user"), //
+	ROOT("root", "/root"); //
 
 	private final String login;
+	private final String home;
 
-	private Users(String login) {
+	private Users(String login, String home) {
 		this.login = login;
+		this.home = home;
 	}
 
 	public String getLogin() {
 		return login;
+	}
+
+	public String getHome() {
+		return home;
 	}
 
 }
