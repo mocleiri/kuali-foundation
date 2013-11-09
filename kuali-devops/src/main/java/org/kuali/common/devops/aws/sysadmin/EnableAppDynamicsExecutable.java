f449c1ef0379b3e718787ba920f46163fa88b3a0
jcaddel <jcaddel@kuali.org>
2013-11-09 10:08:30 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28464 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/EnableAppDynamicsExecutable.java
@@ -0,0 +1,42 @@
+package org.kuali.common.devops.aws.sysadmin;
+
+import org.kuali.common.util.Assert;
+import org.kuali.common.util.channel.api.SecureChannel;
+import org.kuali.common.util.channel.util.ChannelExecutable;
+import org.kuali.common.util.channel.util.ChannelUtils;
+
+/**
+ * AppDynamics needs to find tools.jar in the JRE directory in order for advanced metrics to work right.
+ */
+public final class EnableAppDynamicsExecutable implements ChannelExecutable {
+
+	public EnableAppDynamicsExecutable(String jdkDir) {
+		this(jdkDir, false);
+	}
+
+	public EnableAppDynamicsExecutable(String jdkDir, boolean skip) {
+		Assert.noBlanks(jdkDir);
+		this.jdkDir = jdkDir;
+		this.skip = skip;
+	}
+
+	private final String jdkDir;
+	private final boolean skip;
+
+	@Override
+	public void execute(SecureChannel channel) {
+		if (skip) {
+			return;
+		}
+		// /usr/local/jdk[6,7]
+		String src = jdkDir + "/lib/tools.jar";
+		String dst = jdkDir + "/jre/lib/ext/tools.jar";
+		String command = "cp " + src + " " + dst;
+		ChannelUtils.exec(channel, command);
+	}
+
+	public boolean isSkip() {
+		return skip;
+	}
+
+}
