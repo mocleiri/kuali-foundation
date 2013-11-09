6e6e7e24e38cf03700f8c4ac9286e851797b9808
jcaddel <jcaddel@kuali.org>
2013-11-09 07:36:08 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28428 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/ci/ProvisionCIMasterConfig.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/ci/ProvisionCIMasterConfig.java
@@ -127,8 +127,8 @@ public class ProvisionCIMasterConfig {
 		executables.add(new InstallZipPackageExecutable(jdk6));
 		executables.add(new InstallZipPackageExecutable(jdk7));
 		executables.add(new InstallZipPackageExecutable(tomcat));
-		// new ConcurrentExecutables(executables).execute();
-		new ExecutablesExecutable(executables).execute();
+		// new ConcurrentExecutables(executables,true).execute();
+		new ExecutablesExecutable(executables, true).execute();
 		long elapsed = System.currentTimeMillis() - start;
 		logger.info("Elapsed: {}", FormatUtils.getTime(elapsed));
 		return null; // new ExecutablesExecutable(show);
