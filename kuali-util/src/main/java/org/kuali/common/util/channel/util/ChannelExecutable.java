9fe202b11f0110f73a894fdc473cdbd6015d1a6b
jcaddel <jcaddel@kuali.org>
2013-11-09 09:15:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28459 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/channel/util/ChannelExecutable.java
+++ kuali-util/src/main/java/org/kuali/common/util/channel/util/ChannelExecutable.java
@@ -2,7 +2,7 @@ package org.kuali.common.util.channel.util;
 
 import org.kuali.common.util.channel.api.SecureChannel;
 
-public interface SecureChannelExecutable {
+public interface ChannelExecutable {
 
 	void execute(SecureChannel channel);
 
