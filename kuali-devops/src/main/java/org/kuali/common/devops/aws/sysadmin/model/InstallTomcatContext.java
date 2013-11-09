97d5162d78a66872db01d6b716965fb2b10c946e
jcaddel <jcaddel@kuali.org>
2013-11-08 20:22:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28397 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/InstallTomcatContext.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/InstallTomcatContext.java
@@ -1,10 +1,15 @@
 package org.kuali.common.devops.aws.sysadmin.model;
 
+import java.util.List;
+
 import org.kuali.common.util.Assert;
+
+import com.google.common.collect.ImmutableList;
 
 public final class InstallTomcatContext {
 
 	private final InstallZipPackageContext zipPackage;
+	private final List<Deployable> deployables;
 
 	public static class Builder {
 
@@ -12,19 +17,21 @@ public final class InstallTomcatContext {
 		private final InstallZipPackageContext zipPackage;
 
 		// Optional
+		private final List<Deployable> deployables = ImmutableList.of();
 
 		public Builder(InstallZipPackageContext zipPackage) {
 			this.zipPackage = zipPackage;
 		}
 
 		public InstallTomcatContext build() {
-			Assert.noNulls(zipPackage);
+			Assert.noNulls(zipPackage, deployables);
 			return new InstallTomcatContext(this);
 		}
 	}
 
 	private InstallTomcatContext(Builder builder) {
 		this.zipPackage = builder.zipPackage;
+		this.deployables = builder.deployables;
 	}
 
 	public InstallZipPackageContext getZipPackage() {
