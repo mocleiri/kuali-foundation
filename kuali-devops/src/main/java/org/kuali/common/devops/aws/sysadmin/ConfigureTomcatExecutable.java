74f5c4a3281949ab98f3951c5b4a65b4b6e5483b
jcaddel <jcaddel@kuali.org>
2013-11-09 11:51:58 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28483 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/ConfigureTomcatExecutable.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/ConfigureTomcatExecutable.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import org.kuali.common.devops.aws.sysadmin.model.Deployable;
 import org.kuali.common.devops.aws.sysadmin.model.InstallZipPackageContext;
+import org.kuali.common.devops.project.DevOpsProjectConstants;
 import org.kuali.common.util.Assert;
 import org.kuali.common.util.VersionUtils;
 import org.kuali.common.util.channel.api.SecureChannel;
 import org.kuali.common.util.channel.model.RemoteFile;
 import org.kuali.common.util.channel.util.ChannelExecutable;
 import org.kuali.common.util.channel.util.ChannelUtils;
+import org.kuali.common.util.project.ProjectUtils;
 
 import com.google.common.collect.ImmutableList;
 
@@ -22,12 +24,14 @@ public final class ConfigureTomcatExecutable implements ChannelExecutable {
 	private final InstallZipPackageContext context;
 	private final boolean skip;
 	private final String majorVersion;
+	private final String classpathPrefix;
 
 	public static class Builder {
 
 		// Required
 		private final InstallZipPackageContext context;
 		private final String majorVersion;
+		private final String classpathPrefix;
 
 		// Optional
 		private boolean skip = false;
@@ -35,6 +39,7 @@ public final class ConfigureTomcatExecutable implements ChannelExecutable {
 		public Builder(InstallZipPackageContext context) {
 			this.context = context;
 			this.majorVersion = VersionUtils.getVersion(context.getZipPackage().getArtifact().getVersion()).getMajor();
+			this.classpathPrefix = ProjectUtils.getClasspathPrefix(DevOpsProjectConstants.PROJECT_ID) + "/tomcat";
 		}
 
 		public Builder skip(boolean skip) {
@@ -54,6 +59,7 @@ public final class ConfigureTomcatExecutable implements ChannelExecutable {
 		this.context = builder.context;
 		this.skip = builder.skip;
 		this.majorVersion = builder.majorVersion;
+		this.classpathPrefix = builder.classpathPrefix;
 	}
 
 	@Override
@@ -85,7 +91,7 @@ public final class ConfigureTomcatExecutable implements ChannelExecutable {
 		String[] resources = { "env.jsp", "tail.jsp" };
 		List<Deployable> list = new ArrayList<Deployable>();
 		for (String resource : resources) {
-			String src = "classpath:org/kuali/common/kuali-deploy/tomcat/jsps/" + resource;
+			String src = classpathPrefix + "/jsps/" + resource;
 			String absolutePath = context.getInstallDir() + "/logs/" + resource;
 			RemoteFile dst = new RemoteFile.Builder(absolutePath).build();
 			list.add(new Deployable(src, dst));
@@ -97,7 +103,7 @@ public final class ConfigureTomcatExecutable implements ChannelExecutable {
 		String[] resources = { "cleanup.sh", "forced-shutdown.sh" };
 		List<Deployable> list = new ArrayList<Deployable>();
 		for (String resource : resources) {
-			String src = "classpath:org/kuali/common/kuali-deploy/tomcat/bin/" + resource;
+			String src = classpathPrefix + "/bin/" + resource;
 			String absolutePath = context.getInstallDir() + "/bin/" + resource;
 			RemoteFile dst = new RemoteFile.Builder(absolutePath).build();
 			list.add(new Deployable(src, dst));
@@ -109,7 +115,7 @@ public final class ConfigureTomcatExecutable implements ChannelExecutable {
 		String[] resources = { "server.xml", "web.xml" };
 		List<Deployable> list = new ArrayList<Deployable>();
 		for (String resource : resources) {
-			String src = "classpath:org/kuali/common/kuali-deploy/tomcat/" + majorVersion + "/conf/" + resource;
+			String src = classpathPrefix + "/" + majorVersion + "/conf/" + resource;
 			String absolutePath = context.getInstallDir() + "/conf/" + resource;
 			RemoteFile dst = new RemoteFile.Builder(absolutePath).build();
 			list.add(new Deployable(src, dst));
