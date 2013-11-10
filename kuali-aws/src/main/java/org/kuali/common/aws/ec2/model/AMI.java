d38ea5de880741d5573c5eef6cb3144cfb992e9e
jcaddel <jcaddel@kuali.org>
2013-11-09 20:48:18 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28532 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMI.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMI.java
@@ -1,16 +1,19 @@
 package org.kuali.common.aws.ec2.model;
 
+import org.kuali.common.util.Assert;
+
 public enum AMI {
 
 	// This is a bare bones Amazon Linux box with virtually nothing except yum installed on it
 	// amzn-ami-minimal-pv-2013.09.0.x86_64-ebs
 	AMAZON_LINUX_64_BIT_MINIMAL_AMI_2013_09("amazon-linux", "2013.09", "ami-65792c0c");
 
-	private final String id;
 	private final String operatingSystem;
 	private final String version;
+	private final String id;
 
 	private AMI(String operatingSystem, String version, String id) {
+		Assert.noBlanks(operatingSystem, version, id);
 		this.operatingSystem = operatingSystem;
 		this.version = version;
 		this.id = id;
