f16d82345ec9a708607b1e7dcd2c4d6fa8709580
jcaddel <jcaddel@kuali.org>
2013-11-09 21:32:29 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28547 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/ci/ProvisionCIMasterConfig.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/ci/ProvisionCIMasterConfig.java
@@ -107,8 +107,8 @@ public class ProvisionCIMasterConfig {
 		long start = System.currentTimeMillis();
 		LaunchInstanceContext context = launchInstanceContext();
 		new ShowLaunchConfigExecutable(serviceContext, context).execute();
-		Instance instance = ec2.launchInstance(context);
-		// Instance instance = ec2.getInstance("i-072be77e");
+		// Instance instance = ec2.launchInstance(context);
+		Instance instance = ec2.getInstance("i-9c9e38e1");
 		String privateKey = context.getKeyPair().getPrivateKey().get();
 		BootstrapContext bc = new BootstrapContext.Builder(scs, instance.getPublicDnsName(), privateKey).build();
 		new BootstrapExecutable(bc).execute();
