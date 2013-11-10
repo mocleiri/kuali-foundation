f3e2d4d7fb37094d9f99ffb2fe9b44b57e4ba719
jcaddel <jcaddel@kuali.org>
2013-11-09 21:05:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28538 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMI.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMI.java
@@ -8,19 +8,19 @@ public enum AMI {
 	// amzn-ami-minimal-pv-2013.09.0.x86_64-ebs
 	AMAZON_LINUX_64_BIT_MINIMAL_AMI_2013_09("amazon-linux", "2013.09", "ami-65792c0c");
 
-	private final String operatingSystem;
+	private final String distro;
 	private final String version;
 	private final String id;
 
-	private AMI(String operatingSystem, String version, String id) {
-		Assert.noBlanks(operatingSystem, version, id);
-		this.operatingSystem = operatingSystem;
+	private AMI(String distro, String version, String id) {
+		Assert.noBlanks(distro, version, id);
+		this.distro = distro;
 		this.version = version;
 		this.id = id;
 	}
 
-	public String getOperatingSystem() {
-		return operatingSystem;
+	public String getDistro() {
+		return distro;
 	}
 
 	public String getVersion() {
