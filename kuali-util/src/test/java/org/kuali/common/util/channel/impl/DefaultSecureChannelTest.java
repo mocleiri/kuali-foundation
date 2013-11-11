89f890fdaee38ba5c11989fc4236d01aecb2b5b5
jcaddel <jcaddel@kuali.org>
2013-11-11 16:07:46 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28726 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/channel/impl/DefaultSecureChannelTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/channel/impl/DefaultSecureChannelTest.java
@@ -1,12 +1,23 @@
 package org.kuali.common.util.channel.impl;
 
+import org.jasypt.util.text.TextEncryptor;
 import org.junit.Test;
+import org.kuali.common.util.Assert;
+import org.kuali.common.util.PropertyUtils;
+import org.kuali.common.util.enc.DefaultEncryptionService;
+import org.kuali.common.util.enc.EncUtils;
+import org.kuali.common.util.enc.EncryptionService;
 
 public class DefaultSecureChannelTest {
 
 	@Test
 	public void doIt() {
 		try {
+			String password = PropertyUtils.getGlobalProperty("enc.password");
+			Assert.noBlanks(password);
+			TextEncryptor encryptor = EncUtils.getTextEncryptor(password);
+			EncryptionService enc = new DefaultEncryptionService(encryptor);
+			System.out.println(enc.encrypt("foo"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
