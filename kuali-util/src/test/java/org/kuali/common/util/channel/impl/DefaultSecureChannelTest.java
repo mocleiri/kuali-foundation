88b5118575bd6b94a75ce1b20594c43260c82802
jcaddel <jcaddel@kuali.org>
2013-11-11 16:00:49 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28724 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/channel/impl/DefaultSecureChannelTest.java
@@ -0,0 +1,16 @@
+package org.kuali.common.util.channel.impl;
+
+import org.junit.Test;
+
+public class DefaultSecureChannelTest {
+
+	@Test
+	public void doIt() {
+		try {
+			System.out.println("hello world");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
