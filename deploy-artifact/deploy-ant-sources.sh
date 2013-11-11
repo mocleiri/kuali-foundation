1bc979a648cebfd933efccf2808f1153379e64d5
jcaddel <jcaddel@kuali.org>
2013-11-10 19:58:51 -0500

    [maven-release-plugin]  copy for tag kuali-pom-3.3.33
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-pom-3.3.33@28638 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- deploy-artifact/deploy-ant-sources.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ARGS="$ARGS -Ddp.groupId=org.apache.ant"
-ARGS="$ARGS -Ddp.artifactId=ant"
-ARGS="$ARGS -Ddp.version=1.8.2"
-ARGS="$ARGS -Ddp.classifier=sources"
-ARGS="$ARGS -Ddp.file=/root/.ant/apache-ant-1.8.2-sources.jar"
-mvn process-resources -Ppublic $ARGS
--- kuali-util/src/main/java/org/kuali/common/util/channel/model/CopyDirection.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.common.util.channel.model;
-
-public enum CopyDirection {
-
-	TO_REMOTE, // We copied something (local file system, another url) to the remote server
-	FROM_REMOTE; // We copied something from the remote sever to somewhere else (local file system, an output stream)
-
-}
