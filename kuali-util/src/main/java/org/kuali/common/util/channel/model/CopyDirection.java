6d6a2c4483c5310542fe2c6cb9450e3b897fee75
jcaddel <jcaddel@kuali.org>
2013-11-11 21:56:13 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28762 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/channel/model/CopyDirection.java
+++ kuali-util/src/main/java/org/kuali/common/util/channel/model/CopyDirection.java
@@ -1,8 +1,10 @@
 package org.kuali.common.util.channel.model;
 
 public enum CopyDirection {
+	// We copied something (a file on the local file system, the contents of a url, a java.lang.String object) TO the file system on a remote server
+	TO_REMOTE, //
 
-	TO_REMOTE, // We copied something (a file on the local file system, the contents of a url, a java.lang.String object) to a file on the remote server
-	FROM_REMOTE; // We copied a file from the remote sever to somewhere (our local file system, an output stream, a java.lang.String object)
+	// We copied a file FROM the file system on a remote sever to somewhere (our local file system, an output stream, a java.lang.String object)
+	FROM_REMOTE;
 
 }
