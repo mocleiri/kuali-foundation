0b9dcd9d723a030d9581fda2b95e3d4ac2983ed0
jcaddel <jcaddel@kuali.org>
2013-11-20 16:39:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28821 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/PackageGroups.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/PackageGroups.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import com.google.common.collect.ImmutableList;
 
-public enum ServerPackageGroups {
+public enum PackageGroups {
 
 	BASIC(of("man", "zip", "unzip", "wget", "rsync", "openssh-clients")), //
 	SCM(of("subversion", "git")), //
@@ -35,7 +35,7 @@ public enum ServerPackageGroups {
 
 	private final List<String> names;
 
-	private ServerPackageGroups(List<String> names) {
+	private PackageGroups(List<String> names) {
 		this.names = names;
 	}
 
@@ -43,20 +43,20 @@ public enum ServerPackageGroups {
 		return names;
 	}
 
-	private static List<String> of(ServerPackageGroups packages, String... names) {
+	private static List<String> of(PackageGroups packages, String... names) {
 		return of(ImmutableList.of(packages), names);
 	}
 
-	private static List<String> of(List<ServerPackageGroups> packages, String... names) {
+	private static List<String> of(List<PackageGroups> packages, String... names) {
 		List<String> newList = new ArrayList<String>();
-		for (ServerPackageGroups pkg : packages) {
+		for (PackageGroups pkg : packages) {
 			newList.addAll(pkg.getNames());
 		}
 		newList.addAll(ImmutableList.copyOf(names));
 		return newList;
 	}
 
-	private static List<ServerPackageGroups> of(ServerPackageGroups... packages) {
+	private static List<PackageGroups> of(PackageGroups... packages) {
 		return ImmutableList.copyOf(packages);
 	}
 
