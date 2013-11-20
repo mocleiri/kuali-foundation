29d354096c46d4e0c9b1a9b1319499828b2148fb
jcaddel <jcaddel@kuali.org>
2013-11-20 16:35:26 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28820 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/ServerPackageGroups.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/ServerPackageGroups.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import com.google.common.collect.ImmutableList;
 
-public enum Blabbity {
+public enum ServerPackageGroups {
 
 	BASIC(of("man", "zip", "unzip", "wget", "rsync", "openssh-clients")), //
 	SCM(of("subversion", "git")), //
@@ -35,7 +35,7 @@ public enum Blabbity {
 
 	private final List<String> names;
 
-	private Blabbity(List<String> names) {
+	private ServerPackageGroups(List<String> names) {
 		this.names = names;
 	}
 
@@ -43,20 +43,20 @@ public enum Blabbity {
 		return names;
 	}
 
-	private static List<String> of(Blabbity packages, String... names) {
+	private static List<String> of(ServerPackageGroups packages, String... names) {
 		return of(ImmutableList.of(packages), names);
 	}
 
-	private static List<String> of(List<Blabbity> packages, String... names) {
+	private static List<String> of(List<ServerPackageGroups> packages, String... names) {
 		List<String> newList = new ArrayList<String>();
-		for (Blabbity pkg : packages) {
+		for (ServerPackageGroups pkg : packages) {
 			newList.addAll(pkg.getNames());
 		}
 		newList.addAll(ImmutableList.copyOf(names));
 		return newList;
 	}
 
-	private static List<Blabbity> of(Blabbity... packages) {
+	private static List<ServerPackageGroups> of(ServerPackageGroups... packages) {
 		return ImmutableList.copyOf(packages);
 	}
 
