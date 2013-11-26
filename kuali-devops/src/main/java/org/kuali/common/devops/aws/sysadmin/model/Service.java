600af2b81d1eeabdb2d7a996825d43eb4435c4b5
jcaddel <jcaddel@kuali.org>
2013-11-26 15:53:12 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28872 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/Service.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/Service.java
@@ -2,20 +2,31 @@ package org.kuali.common.devops.aws.sysadmin.model;
 
 import org.kuali.common.util.Assert;
 
+import com.google.common.base.Optional;
+
 public final class Service {
 
+	private final String name;
 	private final String instanceId;
+	private final Optional<String> fqdn;
 
 	public static class Builder {
 
+		// Required
+		private final String name;
 		private final String instanceId;
 
-		public Builder(String instanceId) {
+		// Optional
+		private Optional<String> fqdn = Optional.absent(); // Defaults to the public DNS name supplied by Amazon if no custom FQDN is supplied
+
+		public Builder(String name, String instanceId) {
 			this.instanceId = instanceId;
+			this.name = name;
 		}
 
 		public Service build() {
-			Assert.noBlanks(instanceId);
+			Assert.noBlanks(instanceId, name);
+			Assert.noNulls(fqdn);
 			return new Service(this);
 		}
 
@@ -23,10 +34,20 @@ public final class Service {
 
 	private Service(Builder builder) {
 		this.instanceId = builder.instanceId;
+		this.name = builder.name;
+		this.fqdn = builder.fqdn;
 	}
 
 	public String getInstanceId() {
 		return instanceId;
+	}
+
+	public Optional<String> getFqdn() {
+		return fqdn;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 }
