cbebfd529dda57962b53b989ec83c4f83fa7dbfc
jcaddel <jcaddel@kuali.org>
2013-11-26 15:26:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28862 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/Service.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/Service.java
@@ -1,54 +1,32 @@
 package org.kuali.common.devops.aws.sysadmin.model;
 
-import java.io.File;
-
 import org.kuali.common.util.Assert;
 
 public final class Service {
 
-	private final String name;
-	private final String configFileAbsolutePath;
-	private final String configFileName;
+	private final String instanceId;
 
 	public static class Builder {
 
-		private final String name;
-		private final String configFileAbsolutePath;
-		private final String configFileName;
+		private final String instanceId;
 
-		public Builder(String name, String configFileAbsolutePath) {
-			this(name, configFileAbsolutePath, new File(configFileAbsolutePath).getName());
-		}
-
-		public Builder(String name, String configFileAbsolutePath, String configFileName) {
-			this.name = name;
-			this.configFileAbsolutePath = configFileAbsolutePath;
-			this.configFileName = configFileName;
+		public Builder(String instanceId) {
+			this.instanceId = instanceId;
 		}
 
 		public Service build() {
-			Assert.noBlanks(name, configFileAbsolutePath, configFileName);
+			Assert.noBlanks(instanceId);
 			return new Service(this);
 		}
 
 	}
 
 	private Service(Builder builder) {
-		this.name = builder.name;
-		this.configFileAbsolutePath = builder.configFileAbsolutePath;
-		this.configFileName = builder.configFileName;
+		this.instanceId = builder.instanceId;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String getConfigFileAbsolutePath() {
-		return configFileAbsolutePath;
-	}
-
-	public String getConfigFileName() {
-		return configFileName;
+	public String getInstanceId() {
+		return instanceId;
 	}
 
 }
