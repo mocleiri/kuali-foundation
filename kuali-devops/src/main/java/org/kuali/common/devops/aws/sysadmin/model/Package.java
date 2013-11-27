f1e3dd3bee31c9be3df8639a295ebff018c94c64
jcaddel <jcaddel@kuali.org>
2013-11-26 21:12:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28914 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/Package.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/Package.java
@@ -1,17 +1,24 @@
 package org.kuali.common.devops.aws.sysadmin.model;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.nullify.NullUtils;
+
+import com.google.common.base.Optional;
 
 public final class Package {
 
 	private final String title;
 	private final String name;
+	private final Optional<String> description;
 
 	public static class Builder {
 
 		// Required
 		private final String title;
 		private final String name;
+
+		// Optional
+		private Optional<String> description = Optional.absent();
 
 		public Builder(String name) {
 			this(name, name);
@@ -20,6 +27,11 @@ public final class Package {
 		public Builder(String title, String name) {
 			this.title = title;
 			this.name = name;
+		}
+
+		public Builder description(String description) {
+			this.description = Optional.fromNullable(NullUtils.trimToNull(description));
+			return this;
 		}
 
 		public Package build() {
@@ -31,6 +43,7 @@ public final class Package {
 	private Package(Builder builder) {
 		this.title = builder.title;
 		this.name = builder.name;
+		this.description = builder.description;
 	}
 
 	public String getTitle() {
@@ -39,6 +52,10 @@ public final class Package {
 
 	public String getName() {
 		return name;
+	}
+
+	public Optional<String> getDescription() {
+		return description;
 	}
 
 }
