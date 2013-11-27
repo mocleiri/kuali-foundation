cca946146c5cda46e4d6307638f878fb790c4c22
jcaddel <jcaddel@kuali.org>
2013-11-27 18:00:38 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28953 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/builder/BuilderUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/builder/BuilderUtils.java
@@ -1,7 +1,11 @@
 package org.kuali.common.util.builder;
 
 import org.kuali.common.util.FormatUtils;
+import org.kuali.common.util.enc.EncUtils;
+import org.kuali.common.util.spring.SpringUtils;
 import org.kuali.common.util.spring.env.EnvironmentService;
+
+import com.google.common.base.Optional;
 
 public class BuilderUtils {
 
@@ -15,4 +19,23 @@ public class BuilderUtils {
 		}
 	}
 
+	/**
+	 * Return a decrypted value by searching the environment under both keys. Default to the provided value if nothing is found
+	 */
+	public static String getValue(BuilderContext ctx, String key1, String key2, String provided) {
+		String value = getValue(ctx.getEnv(), key1, key2, provided);
+		return EncUtils.decrypt(ctx.getEnc(), value);
+	}
+
+	protected static String getValue(EnvironmentService env, String key1, String key2, String provided) {
+		Optional<String> value1 = SpringUtils.getOptionalString(env, key1);
+		Optional<String> value2 = SpringUtils.getOptionalString(env, key2);
+		if (value1.isPresent()) {
+			return value1.get();
+		} else if (value2.isPresent()) {
+			return value2.get();
+		} else {
+			return provided;
+		}
+	}
 }
