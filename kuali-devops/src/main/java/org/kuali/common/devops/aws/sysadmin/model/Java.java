e863756c87c3edc489792c862a3d059690d7afff
jcaddel <jcaddel@kuali.org>
2013-11-26 19:35:13 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28891 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/Java.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/sysadmin/model/Java.java
@@ -3,6 +3,7 @@ package org.kuali.common.devops.aws.sysadmin.model;
 import java.util.List;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.nullify.NullUtils;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
@@ -16,12 +17,29 @@ public final class Java {
 	public static class Builder {
 
 		// Optional
-		private final Optional<String> home = Optional.absent();
-		private final List<String> options = ImmutableList.of();
-		private final List<String> args = ImmutableList.of();
+		private Optional<String> home = Optional.absent();
+		private List<String> options = ImmutableList.of();
+		private List<String> args = ImmutableList.of();
+
+		public Builder home(String home) {
+			this.home = Optional.fromNullable(NullUtils.trimToNull(home));
+			return this;
+		}
+
+		public Builder options(List<String> options) {
+			this.options = options;
+			return this;
+		}
+
+		public Builder args(List<String> args) {
+			this.args = args;
+			return this;
+		}
 
 		public Java build() {
 			Assert.noNulls(home, options, args);
+			this.options = ImmutableList.copyOf(options);
+			this.args = ImmutableList.copyOf(args);
 			return new Java(this);
 		}
 	}
