616df3d10c0fa9bc5ac3180dd3630d5839de1244
jcaddel <jcaddel@kuali.org>
2013-11-26 21:47:03 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28919 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/main/java/org/kuali/common/devops/model/Artifacts.java
@@ -0,0 +1,36 @@
+package org.kuali.common.devops.model;
+
+import org.kuali.common.util.maven.model.Artifact;
+
+public enum Artifacts {
+
+	JDK6(HotSpot.GID, HotSpot.JDK6, HotSpot.JDK6_LATEST, HotSpot.LINUX_X64), //
+	JDK7(HotSpot.GID, HotSpot.JDK7, HotSpot.JDK7_LATEST, HotSpot.LINUX_X64); //
+
+	private final Artifact artifact;
+
+	private Artifacts(String groupId, String artifactId, String version) {
+		this.artifact = new Artifact.Builder(groupId, artifactId, version).build();
+	}
+
+	private Artifacts(String groupId, String artifactId, String version, String classifier) {
+		this.artifact = new Artifact.Builder(groupId, artifactId, version).classifier(classifier).build();
+	}
+
+	public Artifact getArtifact() {
+		return artifact;
+	}
+
+	private static final class HotSpot {
+
+		private static final String JDK6_LATEST = "1.6.0-u45";
+		private static final String JDK7_LATEST = "1.7.0-u40";
+
+		private static final String GID = "com.oracle";
+		private static final String JDK6 = "jdk6";
+		private static final String JDK7 = "jdk7";
+		private static final String LINUX_X64 = "linux-x64";
+
+	}
+
+}
