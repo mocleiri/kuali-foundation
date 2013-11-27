cb417524ee9e6c98a1992a85b9520761328cd148
jcaddel <jcaddel@kuali.org>
2013-11-27 17:53:17 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28952 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/model/AwsAccount.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/model/AwsAccount.java
@@ -1,22 +1,74 @@
 package org.kuali.common.aws.model;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.nullify.NullUtils;
+import org.kuali.common.util.spring.SpringUtils;
+import org.kuali.common.util.spring.env.EnvironmentService;
 
 import com.google.common.base.Optional;
 
 public final class AwsAccount {
 
-	public AwsAccount(String name, String accountNumber, Optional<String> description) {
-		Assert.noBlanks(name, accountNumber);
-		Assert.noNulls(description);
-		this.name = name;
-		this.accountNumber = accountNumber;
-		this.description = description;
-	}
-
 	private final String name;
 	private final String accountNumber;
 	private final Optional<String> description;
+
+	public static class Builder {
+
+		// Required
+		private final String name;
+		private final String accountNumber;
+
+		// Optional
+		private Optional<String> description = Optional.absent();
+
+		// This provides a simple way for system properties / environment variables to override configuration
+		private Optional<EnvironmentService> env = Optional.absent();
+		private static final String NAME_KEY = "aws.account";
+		private static final String ACCOUNT_NUMBER_KEY = "aws.accountNumber";
+		private static final String ACCOUNT_DESCRIPTION_KEY = "aws.accountDescription";
+
+		public Builder(String name, String accountNumber) {
+			this(Optional.<EnvironmentService> absent(), name, accountNumber);
+		}
+
+		public Builder(EnvironmentService env, String name, String accountNumber) {
+			this(Optional.of(env), name, accountNumber);
+		}
+
+		private Builder(Optional<EnvironmentService> env, String name, String accountNumber) {
+			this.env = env;
+			if (env.isPresent()) {
+				EnvironmentService service = env.get();
+				this.name = service.getString(NAME_KEY, name);
+				this.accountNumber = service.getString(ACCOUNT_NUMBER_KEY, accountNumber);
+			} else {
+				this.name = name;
+				this.accountNumber = accountNumber;
+			}
+		}
+
+		public Builder description(String description) {
+			this.description = Optional.fromNullable(NullUtils.trimToNull(description));
+			return this;
+		}
+
+		public AwsAccount build() {
+			Assert.noBlanks(name, accountNumber);
+			if (env.isPresent()) {
+				this.description = SpringUtils.getString(env.get(), ACCOUNT_DESCRIPTION_KEY, description);
+			}
+			Assert.noNulls(description);
+			return new AwsAccount(this);
+		}
+
+	}
+
+	private AwsAccount(Builder builder) {
+		this.name = builder.name;
+		this.accountNumber = builder.accountNumber;
+		this.description = builder.description;
+	}
 
 	public String getName() {
 		return name;
--- kuali-aws/src/main/java/org/kuali/common/aws/model/ImmutableAwsCredentials.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/model/ImmutableAwsCredentials.java
@@ -1,23 +1,65 @@
 package org.kuali.common.aws.model;
 
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.builder.BuilderContext;
+import org.kuali.common.util.enc.EncUtils;
+import org.kuali.common.util.enc.EncryptionService;
+import org.kuali.common.util.spring.env.EnvironmentService;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.google.common.base.Optional;
 
 public class ImmutableAwsCredentials implements AWSCredentials {
 
-	public ImmutableAwsCredentials(AWSCredentials credentials) {
-		this(credentials.getAWSAccessKeyId(), credentials.getAWSSecretKey());
-	}
-
-	public ImmutableAwsCredentials(String accessKey, String secretKey) {
-		Assert.noBlanks(accessKey, secretKey);
-		this.accessKey = accessKey;
-		this.secretKey = secretKey;
-	}
-
 	private final String accessKey;
 	private final String secretKey;
+
+	public static class Builder {
+
+		// Required
+		private final String accessKey;
+		private final String secretKey;
+
+		public Builder(AWSCredentials credentials) {
+			this(BuilderContext.ABSENT, credentials.getAWSAccessKeyId(), credentials.getAWSSecretKey());
+		}
+
+		public Builder(BuilderContext ctx, AWSCredentials credentials) {
+			this(Optional.of(ctx), credentials.getAWSAccessKeyId(), credentials.getAWSSecretKey());
+		}
+
+		public Builder(String accessKey, String secretKey) {
+			this(BuilderContext.ABSENT, accessKey, secretKey);
+		}
+
+		public Builder(BuilderContext ctx, String accessKey, String secretKey) {
+			this(Optional.of(ctx), accessKey, secretKey);
+		}
+
+		private Builder(Optional<BuilderContext> ctx, String accessKey, String secretKey) {
+			if (ctx.isPresent()) {
+				EnvironmentService env = ctx.get().getEnv();
+				EncryptionService enc = ctx.get().getEnc();
+				this.accessKey = env.getString("aws.accessKey", accessKey);
+				this.secretKey = EncUtils.decrypt(enc, env.getString("aws.secretKey", secretKey));
+			} else {
+				this.accessKey = accessKey;
+				this.secretKey = secretKey;
+			}
+		}
+
+		public ImmutableAwsCredentials build() {
+			Assert.noBlanks(accessKey, secretKey);
+			Assert.decrypted(secretKey);
+			return new ImmutableAwsCredentials(this);
+		}
+
+	}
+
+	private ImmutableAwsCredentials(Builder builder) {
+		this.accessKey = builder.accessKey;
+		this.secretKey = builder.secretKey;
+	}
 
 	@Override
 	public String getAWSAccessKeyId() {
