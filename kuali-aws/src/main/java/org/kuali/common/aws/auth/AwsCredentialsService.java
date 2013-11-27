7ced6a0ff3a59129e866c8abd1dbad87ecfe131c
jcaddel <jcaddel@kuali.org>
2013-11-27 18:39:28 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28958 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-aws/src/main/java/org/kuali/common/aws/auth/AwsCredentialsService.java
@@ -0,0 +1,9 @@
+package org.kuali.common.aws.auth;
+
+import com.amazonaws.auth.AWSCredentials;
+
+public interface AwsCredentialsService {
+
+	AWSCredentials getAWSCredentials();
+
+}
---
+++ kuali-aws/src/main/java/org/kuali/common/aws/auth/DefaultAwsCredentialsProviderChain.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.aws.auth;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSCredentialsProviderChain;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+
+/**
+ * This chain searches for AWS credentials in system properties -> environment variables -> Amazon's EC2 Instance Metadata Service
+ */
+public final class DefaultAwsCredentialsProviderChain extends AWSCredentialsProviderChain {
+
+	public DefaultAwsCredentialsProviderChain() {
+		super(getProviders());
+	}
+
+	private static AWSCredentialsProvider[] getProviders() {
+		List<AWSCredentialsProvider> providers = new ArrayList<AWSCredentialsProvider>();
+
+		// System properties always win
+		providers.add(new SystemPropertiesCredentialsProvider());
+
+		// Then fall through to environment variables
+		providers.add(new EnvironmentVariableCredentialsProvider());
+
+		// Then fall through to Amazon's EC2 Instance Metadata Service
+		// http://docs.aws.amazon.com/AWSSdkDocsJava/latest/DeveloperGuide/java-dg-roles.html
+		// This allows you setup an IAM role, attach that role to an EC2 Instance at launch time,
+		// and thus automatically provide the wagon with the credentials it needs
+		providers.add(new InstanceProfileCredentialsProvider());
+
+		return providers.toArray(new AWSCredentialsProvider[providers.size()]);
+	}
+
+}
