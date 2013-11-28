f1531a988cc9f52792e6c8618b0478f84b00fad3
jcaddel <jcaddel@kuali.org>
2013-11-28 14:07:07 -0500

    Step 6
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28991 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/FooBar.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/FooBar.java
@@ -1,72 +1,109 @@
 package org.kuali.common.util.builder;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-/** {@code FooBar} that supports validation, complete with builder. */
+/** Now with more builders! */
 public class FooBar {
 	private final int foo;
-	private final String[] bar;
+	private final List<String> bar;
 
 	private FooBar(Builder builder) {
 		this.foo = builder.foo;
-		this.bar = copy(builder.bar);
+		this.bar = Collections.unmodifiableList(new ArrayList<String>(builder.bar));
 	}
 
 	public int getFoo() {
 		return foo;
 	}
 
-	public String[] getBar() {
-		return copy(bar);
+	/** @return {@code bar} as an unmodifiable list */
+	public List<String> getBar() {
+		return bar;
 	}
 
-	private static String[] copy(String[] array) {
-		return Arrays.copyOf(array, array.length);
+	public static DefaultBuilder builder() {
+		return new DefaultBuilder();
 	}
 
-	public static Builder builder() {
-		return new Builder();
+	public static NullBarBuilder nullBarBuilder() {
+		return new NullBarBuilder();
 	}
 
-	public static Builder nullBarBuilder() {
-		return new Builder().withFoo(30).withBar(null);
-	}
-
-	public static class Builder extends AbstractBuilder<FooBar> {
-		public static final int FOO_DEFAULT = 0;
-		public static final String[] BAR_DEFAULT = new String[] { "Hello, world!" };
-
+	public static abstract class Builder extends AbstractBuilder<FooBar> {
 		private int foo;
-		private String[] bar;
+		private List<String> bar;
 
 		private Builder() {
 		}
 
-		public Builder withFoo(int foo) {
+		public final Builder withFoo(int foo) {
 			this.foo = foo;
 			return this;
 		}
 
-		public Builder withBar(String[] bar) {
+		public final Builder withBar(List<String> bar) {
 			this.bar = bar;
 			return this;
 		}
 
+		protected abstract int defaultFoo();
+
+		protected abstract List<String> defaultBar();
+
 		@Override
-		public boolean isValid() {
+		public final boolean isValid() {
 			int fooMin = bar != null ? 0 : 30;
 			int fooMax = bar != null ? 45 : 60;
 			return fooMin <= foo && foo <= fooMax;
 		}
 
 		@Override
-		protected void defaults() {
-			withFoo(FOO_DEFAULT).withBar(BAR_DEFAULT);
+		protected final void defaults() {
+			withFoo(defaultFoo()).withBar(defaultBar());
 		}
 
 		@Override
-		protected FooBar construct() {
+		protected final FooBar construct() {
 			return new FooBar(this);
+		}
+	}
+
+	public static class DefaultBuilder extends Builder {
+		public static final int FOO_DEFAULT = 0;
+		public static final List<String> BAR_DEFAULT = Collections.unmodifiableList(Arrays.asList(new String[] { "Hello, world!" }));
+
+		private DefaultBuilder() {
+		}
+
+		@Override
+		protected int defaultFoo() {
+			return FOO_DEFAULT;
+		}
+
+		@Override
+		protected List<String> defaultBar() {
+			return BAR_DEFAULT;
+		}
+	}
+
+	public static class NullBarBuilder extends Builder {
+		public static final int FOO_DEFAULT = 30;
+		public static final List<String> BAR_DEFAULT = null;
+
+		private NullBarBuilder() {
+		}
+
+		@Override
+		protected int defaultFoo() {
+			return FOO_DEFAULT;
+		}
+
+		@Override
+		protected List<String> defaultBar() {
+			return BAR_DEFAULT;
 		}
 	}
 }
