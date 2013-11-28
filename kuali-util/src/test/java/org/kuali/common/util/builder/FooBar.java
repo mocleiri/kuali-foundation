1e15bb15ea21b47b0b7bd93c2314b71b10a85eb1
jcaddel <jcaddel@kuali.org>
2013-11-28 13:54:36 -0500

    Step 2
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28987 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/FooBar.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/FooBar.java
@@ -1,13 +1,26 @@
 package org.kuali.common.util.builder;
 
+import java.util.Arrays;
+
 public class FooBar {
+	private final int foo;
+	private final String[] bar;
 
-	protected int foo;
-	protected String[] bar;
-
+	/**
+	 * Constructs a valid {@code FooBar} instance.
+	 * 
+	 * ...doc specifying preconditions here...
+	 * 
+	 * @throw IllegalArgumentException if a valid instance cannot be created based on provided values
+	 */
 	public FooBar(int foo, String[] bar) {
+		int fooMin = bar != null ? 0 : 30;
+		int fooMax = bar != null ? 45 : 60;
+		if (foo < fooMin || foo > fooMax) {
+			throw new IllegalArgumentException();
+		}
 		this.foo = foo;
-		this.bar = bar;
+		this.bar = copy(bar);
 	}
 
 	public int getFoo() {
@@ -15,6 +28,11 @@ public class FooBar {
 	}
 
 	public String[] getBar() {
-		return bar;
+		return copy(bar);
+	}
+
+	/** Convenience method that returns a copy of {@code array}. */
+	private static String[] copy(String[] array) {
+		return Arrays.copyOf(array, array.length);
 	}
 }
