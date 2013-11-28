d7f2bb9bc1150dd8c7b67f76fa3bdb86f9600203
jcaddel <jcaddel@kuali.org>
2013-11-28 13:55:36 -0500

    Step 3
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@28988 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/FooBar.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/FooBar.java
@@ -3,24 +3,14 @@ package org.kuali.common.util.builder;
 import java.util.Arrays;
 
 public class FooBar {
+
 	private final int foo;
 	private final String[] bar;
 
-	/**
-	 * Constructs a valid {@code FooBar} instance.
-	 * 
-	 * ...doc specifying preconditions here...
-	 * 
-	 * @throw IllegalArgumentException if a valid instance cannot be created based on provided values
-	 */
-	public FooBar(int foo, String[] bar) {
-		int fooMin = bar != null ? 0 : 30;
-		int fooMax = bar != null ? 45 : 60;
-		if (foo < fooMin || foo > fooMax) {
-			throw new IllegalArgumentException();
-		}
-		this.foo = foo;
-		this.bar = copy(bar);
+	/** Prevent direct instantiation. */
+	private FooBar(Builder builder) {
+		this.foo = builder.foo;
+		this.bar = copy(builder.bar);
 	}
 
 	public int getFoo() {
@@ -31,8 +21,40 @@ public class FooBar {
 		return copy(bar);
 	}
 
-	/** Convenience method that returns a copy of {@code array}. */
 	private static String[] copy(String[] array) {
 		return Arrays.copyOf(array, array.length);
+	}
+
+	public static class Builder {
+
+		private int foo;
+		private String[] bar;
+
+		public Builder withFoo(int foo) {
+			this.foo = foo;
+			return this;
+		}
+
+		public Builder withBar(String[] bar) {
+			this.bar = bar;
+			return this;
+		}
+
+		/** @return {@code true} if a valid {@code FooBar} instance can be built */
+		public boolean isValid() {
+			int fooMin = bar != null ? 0 : 30;
+			int fooMax = bar != null ? 45 : 60;
+			return fooMin <= foo && foo <= fooMax;
+		}
+
+		public void validate() {
+			if (!isValid())
+				throw new IllegalArgumentException();
+		}
+
+		public FooBar build() {
+			validate();
+			return new FooBar(this);
+		}
 	}
 }
