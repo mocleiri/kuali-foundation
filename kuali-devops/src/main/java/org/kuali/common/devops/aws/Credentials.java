e88ccd317d942533c0dda00de73daa487de6bec4
jcaddel <jcaddel@kuali.org>
2013-11-28 21:05:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29002 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/Credentials.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/Credentials.java
@@ -2,7 +2,9 @@ package org.kuali.common.devops.aws;
 
 import org.kuali.common.util.Assert;
 
-public enum Credentials {
+import com.amazonaws.auth.AWSCredentials;
+
+public enum Credentials implements AWSCredentials {
 
 	FOUNDATION("AKIAJFD5IM7IPVVUEBNA", "ENC(uXNCzc6efcKz1zvp4t5Fj4wyR9oGw2GZ2VOB3SXZaoXaV1BA1Gao2d2vWXnjqUA1oKzg+0s9NAM=)"), //
 	STUDENT("AKIAJZ72UQ5ZCVEDMAPQ", "ENC(fXH7NKtbCDnn17aoTOPL707itdwlz6VKyHJaKCHAuBk//XGTyGef+2CeM89GqpIPZYk4ewKia2Y=)"), //
@@ -10,20 +12,22 @@ public enum Credentials {
 	OLE("AKIAI453FI76LUZ7T7CA", "ENC(2UP9ztdMy32DdN62ZMjD+K0jMYCrKkYzI2xies0asEU4cc7sWOF47a2CDu314ojHoSrz/CtFaAs=)");
 
 	private final String accessKey;
-	private final String encryptedSecretKey;
+	private final String secretKey;
 
-	private Credentials(String accessKey, String encryptedSecretKey) {
-		Assert.noBlanks(accessKey, encryptedSecretKey);
+	private Credentials(String accessKey, String secretKey) {
+		Assert.noBlanks(accessKey, secretKey);
 		this.accessKey = accessKey;
-		this.encryptedSecretKey = encryptedSecretKey;
+		this.secretKey = secretKey;
 	}
 
-	public String getAccessKey() {
+	@Override
+	public String getAWSAccessKeyId() {
 		return accessKey;
 	}
 
-	public String getEncryptedSecretKey() {
-		return encryptedSecretKey;
+	@Override
+	public String getAWSSecretKey() {
+		return secretKey;
 	}
 
 }
