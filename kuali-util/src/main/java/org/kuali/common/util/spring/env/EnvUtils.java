99a29e403461163056caedf4a608a67d6c2f6a52
jcaddel <jcaddel@kuali.org>
2013-11-29 10:26:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29024 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/env/EnvUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/env/EnvUtils.java
@@ -1,10 +1,19 @@
 package org.kuali.common.util.spring.env;
 
+import java.util.Properties;
+
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.PropertyUtils;
+import org.springframework.core.env.Environment;
 
 public class EnvUtils {
 
 	private static final String ENV_PREFIX = "env";
+
+	public static Environment getDefaultEnvironment() {
+		Properties global = PropertyUtils.getGlobalProperties();
+		return new PropertiesEnvironment(global);
+	}
 
 	/**
 	 * <pre>
@@ -19,15 +28,20 @@ public class EnvUtils {
 		char prevChar = 0;
 		for (char c : chars) {
 			if (c == '.') {
+				// Convert dots into dashes
 				sb.append('_');
 			} else if (isUpperCase(c) && isLowerCase(prevChar)) {
+				// Insert an underscore wherever there is a transition from a lower case char to an upper case char
 				sb.append('_');
 				sb.append(c);
 			} else {
+				// Just append the char
 				sb.append(c);
 			}
+			// Keep track of the previous char
 			prevChar = c;
 		}
+		// Add a prefix, change to upper case and return
 		return ENV_PREFIX + "." + sb.toString().toUpperCase();
 	}
 
