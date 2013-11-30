d91fe129e262a283f73929572169b8f5ecaf8417
jcaddel <jcaddel@kuali.org>
2013-11-29 23:28:28 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29078 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/properties/spring/DefaultPropertiesServiceConfig.java
+++ kuali-util/src/main/java/org/kuali/common/util/properties/spring/DefaultPropertiesServiceConfig.java
@@ -3,18 +3,18 @@ package org.kuali.common.util.properties.spring;
 import java.util.Properties;
 
 import org.kuali.common.util.PropertyUtils;
-import org.kuali.common.util.enc.EncUtils;
-import org.kuali.common.util.enc.EncryptionContext;
+import org.kuali.common.util.enc.EncContext;
 import org.kuali.common.util.project.model.Project;
 import org.kuali.common.util.project.spring.AutowiredProjectConfig;
 import org.kuali.common.util.properties.DefaultPropertiesService;
 import org.kuali.common.util.properties.PropertiesService;
 import org.kuali.common.util.property.ImmutableProperties;
-import org.kuali.common.util.property.processor.ContextDecryptingProcessor;
+import org.kuali.common.util.property.processor.EncContextDecryptingProcessor;
 import org.kuali.common.util.property.processor.OverridingProcessor;
 import org.kuali.common.util.property.processor.ProcessorsProcessor;
 import org.kuali.common.util.property.processor.PropertyProcessor;
 import org.kuali.common.util.property.processor.ResolvingProcessor;
+import org.kuali.common.util.spring.env.BasicEnvironmentService;
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +29,6 @@ public class DefaultPropertiesServiceConfig implements PropertiesServiceConfig {
 	@Autowired
 	Project project;
 
-	@Autowired
-	EnvironmentService env;
-
 	@Override
 	@Bean
 	public PropertiesService propertiesService() {
@@ -43,7 +40,8 @@ public class DefaultPropertiesServiceConfig implements PropertiesServiceConfig {
 		Properties overrides = new ImmutableProperties(PropertyUtils.combine(project.getProperties(), global));
 
 		// Setup an encryption context from the overrides properties
-		EncryptionContext context = EncUtils.getEncryptionContext(overrides);
+		EnvironmentService basic = new BasicEnvironmentService(overrides);
+		EncContext context = new EncContext.Builder().env(basic).build();
 
 		// Setup a processor that gets invoked on the properties *after* they have all been loaded
 		PropertyProcessor processor = getPostProcessor(overrides, context);
@@ -51,18 +49,20 @@ public class DefaultPropertiesServiceConfig implements PropertiesServiceConfig {
 		// Setup a service with the overrides and post processor we've configured
 		PropertiesService service = new DefaultPropertiesService(overrides, processor);
 
-		// Now that the service is setup, we can remove the password as a system property (if it has been set there)
-		if (context.getPasswordKey().isPresent() && context.isRemovePasswordSystemProperty()) {
-			PropertyUtils.removeSystemProperty(context.getPasswordKey().get());
+		// Now that the service is setup, we can remove any system properties that may contain the encryption password
+		if (context.isRemovePasswordSystemProperty()) {
+			for (String key : context.getPasswordKeys()) {
+				PropertyUtils.removeSystemProperty(key);
+			}
 		}
 
 		// Return the configured service
 		return service;
 	}
 
-	protected PropertyProcessor getPostProcessor(Properties overrides, EncryptionContext context) {
+	protected PropertyProcessor getPostProcessor(Properties overrides, EncContext context) {
 		PropertyProcessor override = new OverridingProcessor(overrides);
-		PropertyProcessor decrypt = new ContextDecryptingProcessor(context);
+		PropertyProcessor decrypt = new EncContextDecryptingProcessor(context);
 		PropertyProcessor resolve = new ResolvingProcessor();
 		return new ProcessorsProcessor(override, decrypt, resolve);
 	}
