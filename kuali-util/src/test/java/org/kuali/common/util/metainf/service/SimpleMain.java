84c756cf358842b1c72af3f65426ca45cf450dc0
jcaddel <jcaddel@kuali.org>
2013-11-29 23:54:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29084 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/metainf/service/SimpleMain.java
+++ kuali-util/src/test/java/org/kuali/common/util/metainf/service/SimpleMain.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class SimpleMain {
 
 	public static void main(String[] args) {
-		MainUtils.runAndExit(SimpleMain.class, args);
+		MainUtils.run(SimpleMain.class, args);
 	}
 
 	@Autowired
