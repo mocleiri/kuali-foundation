9cc2a0b3e6eba6aecc164eba62ab73d4b09c6bba
jcaddel <jcaddel@kuali.org>
2013-11-30 00:07:29 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29087 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/project/SimpleTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/project/SimpleTest.java
@@ -9,18 +9,14 @@ public class SimpleTest {
 
 	@Test
 	public void test() {
-		try {
-			ProjectIdentifier p1 = new ProjectIdentifier("org.kuali.common", "kuali-util");
-			ProjectIdentifier p2 = new ProjectIdentifier("org.kuali.common", "kuali-util");
-			ProjectIdentifier p3 = new ProjectIdentifier("org.kuali.common", "kuali-jdbc");
-			System.out.println(p1.hashCode());
-			System.out.println(p2.hashCode());
-			System.out.println(p3.hashCode());
-			Assert.assertTrue(p1.equals(p2));
-			Assert.assertFalse(p1.equals(p3));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		ProjectIdentifier p1 = new ProjectIdentifier("org.kuali.common", "kuali-util");
+		ProjectIdentifier p2 = new ProjectIdentifier("org.kuali.common", "kuali-util");
+		ProjectIdentifier p3 = new ProjectIdentifier("org.kuali.common", "kuali-jdbc");
+		System.out.println(p1.hashCode());
+		System.out.println(p2.hashCode());
+		System.out.println(p3.hashCode());
+		Assert.assertTrue(p1.equals(p2));
+		Assert.assertFalse(p1.equals(p3));
 	}
 
 }
