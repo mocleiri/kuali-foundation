da5addfe49a172dbedc0e14cf0a8e724e3165ae2
jcaddel <jcaddel@kuali.org>
2013-12-01 17:06:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29186 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/env/EnvUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/env/EnvUtils.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 
 import org.kuali.common.util.Ascii;
 import org.kuali.common.util.Assert;
+import org.kuali.common.util.FormatUtils;
 import org.kuali.common.util.PropertyUtils;
 import org.springframework.core.env.Environment;
 
@@ -14,6 +15,24 @@ public class EnvUtils {
 	public static final Optional<EnvironmentService> ABSENT = Optional.absent();
 
 	private static final String ENV_PREFIX = "env";
+
+	/**
+	 * If the environment contains a string under this key, convert it into a long signifying bytes
+	 * 
+	 * <pre>
+	 *   file.size=10m   (file that is 10 megabytes)
+	 *   disk.size=100g  (disk that is 100 gigabytes)
+	 * </pre>
+	 */
+	public static long getBytes(EnvironmentService env, String key, long provided) {
+		if (env.containsProperty(key)) {
+			String size = env.getString(key);
+			long bytes = FormatUtils.getBytes(size);
+			return bytes;
+		} else {
+			return provided;
+		}
+	}
 
 	/**
 	 * Return an environment that uses system properties / environment variables
