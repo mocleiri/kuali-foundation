3a895f7a824e133864a17638d769dd16befafa69
thrclark <thrclark@kuali.org>
2013-12-03 11:54:16 -0500

    UXI-45 Submit page set to represent pre-submission view
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@29208 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/doc-subnav.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/inc/doc-subnav.php
@@ -49,7 +49,7 @@
 				<li <?php if ($page == "institution-page3") { echo 'class="active"'; } ?>><a href="prop.inst.3.php">Page 3</a></li>
 			</ul>
 		</li>
-		<li <?php if ($page == "prop-summary") { echo 'class="active"'; } ?>><a href="prop.summary.php"><i class="icon-signout"></i><span>Summary/Submit</span></a></li>
+		<li <?php if ($page == "prop-summary") { echo 'class="active"'; } ?>><a href="prop.summary.php"><i class="icon-signout"></i><span>Submit</span></a></li>
 	</ul>
 </div>
 
