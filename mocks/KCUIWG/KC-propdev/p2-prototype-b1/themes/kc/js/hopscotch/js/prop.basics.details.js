1ab179d3fa57ac8d8ec07804355bdc6ac328b0f4
thrclark <thrclark@kuali.org>
2013-12-05 11:15:54 -0500

    UXI-44 Guided tour updates
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@29230 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/js/hopscotch/js/prop.basics.details.js
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/themes/kc/js/hopscotch/js/prop.basics.details.js
@@ -4,56 +4,49 @@ var tour = {
   steps: [
     {
       title: "Proposal Type",
-      content: "Required.    An identifier for the kind of research project being submitted (i.e., new, resubmission, continuing, etc...).",
-      target: "#tour1",
-      placement: "top"
+      content: "An identifier for the kind of research project being submitted (i.e., new, resubmission, etc...).",
+      target: "#tour1", placement: "top"
     },
 	
 	    {
       title: "Lead Unit",
-      content: "Required.  The designated unit responsible for the application and administration of the proposed sponsored project; the lead unit is directly connected to the lead principal investigator. The proposal's fixed home unit that will be used for routing and business rule purposes. AKA Owned by Unit.  The designated unit responsible for the application and administration of the proposed sponsored project. ",
-      target: "#tour2",
-      placement: "top"
+      content: "The designated unit responsible for the application and administration of the proposed sponsored project; the lead unit is directly connected to the lead principal investigator. ",
+      target: "#tour2", placement: "top"
     },
 	
 	
 	    {
       title: "Activity Type",
-      content: "Required.  A word or phrase to define the kind of endeavor being proposed (i.e., research, instruction, etc.).  Select from the list to specify the type of activity proposed, (for example, organized research or instruction).  Activity type refers to the major functions of an institution based on OMB Circular A-21. Each account in an institution's financial system must be identified by its activity type.",
-      target: "#tour3",
-      placement: "top"
+      content: "A word or phrase to define the kind of endeavor being proposed (i.e., research, instruction, etc.).  Select from the list to specify the type of activity proposed, (for example, organized research or instruction).",
+      target: "#tour3", placement: "top"
     },
 	
 	
 	    {
       title: "Project Dates",
       content: "The proposed month, day and year in which the research will begin and end. In the Proposal Module, this is the requested project start and end dates for the project in the proposal.",
-      target: "#tour4",
-      placement: "top"
+      target: "#tour4", placement: "top"
     },
 	
 	
 	    {
       title: "Project Title",
-      content: "Required.  The name created to identify the research proposal.  The proposed title of the project. ",
-      target: "#tour5",
-      placement: "top"
+      content: "The name created to identify the research proposal.  The proposed title of the project. ",
+      target: "#tour5", placement: "top"
     },
 	
 	
 	    {
       title: "Sponsor Code",
-      content: "Required.  A predefined code directly associated with the sponsoring organization (i.e. 000500 = National Science Foundation, 000340 = National Institutes of Health, etc.). The identification number of the organization or agency that is providing support for the sponsored project.  ",
-      target: "#tour6",
-      placement: "top"
+      content: "A predefined code directly associated with the sponsoring organization (i.e. 000500 = National Science Foundation, 000340 = National Institutes of Health, etc.). The identification number of the organization or agency that is providing support for the sponsored project.  ",
+      target: "#tour6", placement: "top"
     },
 	
 	
 	    {
-      title: "Prime Sponsor",
-      content: "Content coming soon...",
-      target: "#tour7",
-      placement: "top"
+      title: "Keywords",
+      content: "A keyword is a term that describes the discipline of a sponsored project (for example, sustainable agriculture or children's literature).",
+      target: "#tour7", placement: "top"
     },
 	
 	
