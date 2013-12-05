ba05780f741e5c8476008f6971f23ea0673ab003
thrclark <thrclark@kuali.org>
2013-12-05 10:53:46 -0500

    Height adjustment for initial 'access' screen
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@29227 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p2-prototype-b1/modal/access.php
+++ mocks/KCUIWG/KC-propdev/p2-prototype-b1/modal/access.php
@@ -10,7 +10,7 @@ require_once( 'inc/head.php' );
       <h3 id="myModalLabel">Access</h3>
     </div>
     <form  class="form-horizontal" method="get" action="">
-      <div class="modal-body" style="min-height:300px"><div class="uif-toolbar" style="padding-top:0px">
+      <div class="modal-body" style="min-height:400px"><div class="uif-toolbar" style="padding-top:0px">
           <a href="access.adduser.php" class="btn btn-default btn-xs"><i class="icon icon-user"></i> Add User</a>
         </div>
          <table class="table table-condensed">
@@ -47,6 +47,20 @@ require_once( 'inc/head.php' );
               </select></td>
                      <td style=""> <a href="" class="btn btn-xs btn-default">Remove</a></td>
           </tr>
+          <tr>
+           <td>Jane Smith</td>
+            <td><select name="keywords" id="roles3" class="form-control input-sm col-md-8" multiple>
+               <option >Reviewer</option>
+               <option >Budget Creator</option>
+               <option>Narritive Writer</option>
+               <option >Aggregator</option>
+               <option >Delete Proposal</option>
+               <option >View Institutional Salaries</option>
+               <option >View Proposal Salaries</option>
+               <option >Approver</option>
+             </select></td>
+           <td style=""> <a href="" class="btn btn-xs btn-default">Remove</a></td>
+         </tr>
         </table>
       </div>
       
