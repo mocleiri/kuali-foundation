f81eb412ba2f25df29b5ca113dec6a8238b11efb
jcaddel <jcaddel@kuali.org>
2013-12-10 18:16:14 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29300 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/env/EnvironmentService.java
+++ kuali-util/src/main/java/org/kuali/common/util/env/EnvironmentService.java
@@ -1,7 +1,5 @@
 package org.kuali.common.util.env;
 
-import java.io.File;
-
 import com.google.common.base.Optional;
 
 public interface EnvironmentService {
@@ -23,53 +21,5 @@ public interface EnvironmentService {
 	<T> Optional<Class<T>> getOptionalPropertyAsClass(String key);
 
 	<T> Optional<Class<T>> getOptionalPropertyAsClass(String key, Optional<Class<T>> provided);
-
-	String getString(String key);
-
-	String getString(String key, String provided);
-
-	Optional<String> getOptionalString(String key);
-
-	Optional<String> getOptionalString(String key, Optional<String> provided);
-
-	boolean getBoolean(String key);
-
-	boolean getBoolean(String key, boolean provided);
-
-	Optional<Boolean> getOptionalBoolean(String key);
-
-	Optional<Boolean> getOptionalBoolean(String key, Optional<Boolean> provided);
-
-	File getFile(String key);
-
-	File getFile(String key, File provided);
-
-	Optional<File> getOptionalFile(String key);
-
-	Optional<File> getOptionalFile(String key, Optional<File> provided);
-
-	int getInteger(String key);
-
-	int getInteger(String key, int provided);
-
-	Optional<Integer> getOptionalInteger(String key);
-
-	Optional<Integer> getOptionalInteger(String key, Optional<Integer> provided);
-
-	double getDouble(String key);
-
-	double getDouble(String key, double provided);
-
-	Optional<Double> getOptionalDouble(String key);
-
-	Optional<Double> getOptionalDouble(String key, Optional<Double> provided);
-
-	long getLong(String key);
-
-	long getLong(String key, long provided);
-
-	Optional<Long> getOptionalLong(String key);
-
-	Optional<Long> getOptionalLong(String key, Optional<Long> provided);
 
 }
