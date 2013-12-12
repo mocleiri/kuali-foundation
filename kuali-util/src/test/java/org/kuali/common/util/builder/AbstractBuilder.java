16ebc3cdfdc8fb9d0951fea484ab2810454b47ac
jcaddel <jcaddel@kuali.org>
2013-12-12 11:41:15 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29426 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/builder/AbstractBuilder.java
@@ -0,0 +1,17 @@
+package org.kuali.common.util.builder;
+
+
+public abstract class AbstractBuilder<T> implements Builder<T> {
+
+	@Override
+	public final T build() {
+		T instance = getInstance();
+		validate(instance);
+		return instance;
+	}
+
+	protected abstract T getInstance();
+
+	protected abstract void validate(T instance);
+
+}
---
+++ kuali-util/src/test/java/org/kuali/common/util/builder/Builder.java
@@ -0,0 +1,7 @@
+package org.kuali.common.util.builder;
+
+public interface Builder<T> {
+
+	T build();
+
+}
