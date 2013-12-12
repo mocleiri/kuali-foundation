710195394e470cc4c74c5cd770213a7f80758001
jcaddel <jcaddel@kuali.org>
2013-12-12 12:39:03 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29435 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/AbstractBuilder.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/AbstractBuilder.java
@@ -1,5 +1,8 @@
 package org.kuali.common.util.builder;
 
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
@@ -20,8 +23,23 @@ public abstract class AbstractBuilder<T> implements Builder<T> {
 	@Override
 	public final T build() {
 		T instance = getInstance();
-		validator.validate(instance);
+		Set<ConstraintViolation<T>> violations = validator.validate(instance);
+		validate(violations);
 		return instance;
+	}
+
+	protected void validate(Set<ConstraintViolation<T>> violations) {
+		if (violations.size() > 0) {
+			StringBuilder sb = new StringBuilder();
+			int i = 0;
+			for (ConstraintViolation<T> violation : violations) {
+				if (i++ != 0) {
+					sb.append(", ");
+				}
+				sb.append("[" + violation.getMessage() + "]");
+			}
+			throw new IllegalArgumentException(sb.toString());
+		}
 	}
 
 	protected abstract T getInstance();
