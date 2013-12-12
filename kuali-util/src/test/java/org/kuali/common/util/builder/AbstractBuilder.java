22251b7ce96e58ba4c7b127b89e99990baef70ae
jcaddel <jcaddel@kuali.org>
2013-12-12 12:40:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29436 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/AbstractBuilder.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/AbstractBuilder.java
@@ -29,17 +29,18 @@ public abstract class AbstractBuilder<T> implements Builder<T> {
 	}
 
 	protected void validate(Set<ConstraintViolation<T>> violations) {
-		if (violations.size() > 0) {
-			StringBuilder sb = new StringBuilder();
-			int i = 0;
-			for (ConstraintViolation<T> violation : violations) {
-				if (i++ != 0) {
-					sb.append(", ");
-				}
-				sb.append("[" + violation.getMessage() + "]");
-			}
-			throw new IllegalArgumentException(sb.toString());
+		if (violations.size() == 0) {
+			return;
 		}
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
+		for (ConstraintViolation<T> violation : violations) {
+			if (i++ != 0) {
+				sb.append(", ");
+			}
+			sb.append("[" + violation.getMessage() + "]");
+		}
+		throw new IllegalArgumentException(sb.toString());
 	}
 
 	protected abstract T getInstance();
