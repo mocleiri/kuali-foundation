e6ac6736daffdc85d1849416bf95f38d5e79b59a
jcaddel <jcaddel@kuali.org>
2013-12-11 22:42:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29375 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/Errors.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/Errors.java
@@ -1,48 +1,22 @@
 package org.kuali.common.util.validate;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import com.google.common.base.Preconditions;
+import org.springframework.util.Assert;
+
 import com.google.common.collect.ImmutableList;
 
 public final class Errors {
 
+	public Errors(List<String> messages) {
+		Assert.notNull(messages, "'messages' cannot be null");
+		this.messages = ImmutableList.copyOf(messages);
+	}
+
+	public List<String> getMessages() {
+		return messages;
+	}
+
 	private final List<String> messages;
-	private final int count;
-
-	private Errors(Builder builder) {
-		this.messages = builder.messages;
-		this.count = builder.count;
-	}
-
-	public static class Builder {
-
-		private List<String> messages = new ArrayList<String>();
-		private int count;
-
-		public Builder reject(String message) {
-			messages.add(message);
-			return this;
-		}
-
-		public Builder withMessages(List<String> messages) {
-			this.messages = messages;
-			return this;
-		}
-
-		public Errors build() {
-			this.messages = ImmutableList.copyOf(messages);
-			this.count = messages.size();
-			Errors instance = new Errors(this);
-			validate(instance);
-			return instance;
-		}
-
-		private void validate(Errors instance) {
-			Preconditions.checkArgument(instance.messages != null, "'messages' cannot be null");
-			Preconditions.checkArgument(instance.count == messages.size(), "count must always equal size");
-		}
-	}
 
 }
