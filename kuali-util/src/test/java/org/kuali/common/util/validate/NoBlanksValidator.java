6243a3353e75261b560697e1a62dbaec96f28ebb
jcaddel <jcaddel@kuali.org>
2013-12-12 09:28:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29419 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/NoBlanksValidator.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/NoBlanksValidator.java
@@ -18,12 +18,12 @@ public class NoBlanksValidator extends AbstractObjectValidator implements Constr
 
 	@Override
 	protected Optional<String> validate(Field field, Object instance) {
-		if (field.getType() != String.class && field.getType() != CharSequence.class) {
+		if (field.getType() != String.class) {
 			return Optional.absent();
 		}
 		Optional<?> value = get(field, instance);
-		CharSequence cs = (CharSequence) value.orNull();
-		if (StringUtils.isBlank(cs)) {
+		String string = (String) value.orNull();
+		if (StringUtils.isBlank(string)) {
 			return Optional.of(getErrorMessage(field, instance, "cannot be blank"));
 		} else {
 			return Optional.absent();
