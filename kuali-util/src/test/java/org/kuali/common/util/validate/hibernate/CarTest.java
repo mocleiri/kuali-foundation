f946f74f6fc44505145089ce1494f2ae189ade44
jcaddel <jcaddel@kuali.org>
2013-12-12 09:37:14 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29421 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/CarTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/CarTest.java
@@ -45,7 +45,7 @@ public class CarTest {
 		Set<ConstraintViolation<Car>> constraintViolations = validator.validate(car);
 
 		assertEquals(1, constraintViolations.size());
-		assertEquals("may not be null", constraintViolations.iterator().next().getMessage());
+		assertEquals("[Car.manufacturer] cannot be null", constraintViolations.iterator().next().getMessage());
 	}
 
 	@Test
