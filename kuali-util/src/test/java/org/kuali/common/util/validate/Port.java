082e2b84c6d48d7e7c586c6eb09aafcbabe95275
jcaddel <jcaddel@kuali.org>
2013-12-12 13:15:48 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29444 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/Port.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/Port.java
@@ -1,6 +1,9 @@
 package org.kuali.common.util.validate;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -10,7 +13,7 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-@Target({ TYPE })
+@Target({ FIELD, METHOD, PARAMETER, ANNOTATION_TYPE })
 @Retention(RUNTIME)
 @Constraint(validatedBy = PortValidator.class)
 @Documented
