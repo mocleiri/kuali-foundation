4334bb4a4a790cfbcbb8572caec7d505d5f11a3e
jcaddel <jcaddel@kuali.org>
2013-12-12 14:51:38 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29457 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/car/Car.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/car/Car.java
@@ -1,9 +1,13 @@
 package org.kuali.common.util.builder.car;
 
-import org.springframework.core.env.Environment;
+import org.kuali.common.util.builder.AbstractBuilder;
+import org.kuali.common.util.validate.NoBlanks;
+import org.kuali.common.util.validate.NoNulls;
 
 import com.google.common.base.Optional;
 
+@NoNulls
+@NoBlanks
 public final class Car {
 
 	public String getMake() {
@@ -22,30 +26,13 @@ public final class Car {
 		this.description = builder.description;
 	}
 
-	public static class Builder {
+	public static class Builder extends AbstractBuilder<Car> {
 
 		private final String make; // Required
 		private Optional<String> description = Optional.absent(); // Optional
 
-		// Spring's environment abstraction
-		private final Optional<Environment> environment;
-
-		public Builder(String make) {
-			this(Optional.<Environment> absent(), make);
-		}
-
-		public Builder(Environment environment, String make) {
-			this(Optional.of(environment), make);
-		}
-
-		private Builder(Optional<Environment> environment, String make) {
-			if (environment.isPresent()) {
-				Environment env = environment.get();
-				this.make = env.getProperty("car.make", make);
-			} else {
-				this.make = make;
-			}
-			this.environment = environment;
+		private Builder(String make) {
+			this.make = make;
 		}
 
 		public Builder description(String description) {
@@ -53,28 +40,10 @@ public final class Car {
 			return this;
 		}
 
-		private void override() {
-			if (environment.isPresent()) {
-				Environment env = environment.get();
-				String description = env.getProperty("car.description");
-				this.description = Optional.fromNullable(description);
-			}
+		@Override
+		protected Car getInstance() {
+			return new Car(this);
 		}
 
-		private void validate(Car instance) {
-			if (instance.getMake() == null) {
-				throw new IllegalArgumentException("make is null");
-			}
-			if (instance.getDescription() == null) {
-				throw new IllegalArgumentException("description is null");
-			}
-		}
-
-		public Car build() {
-			override();
-			Car instance = new Car(this);
-			validate(instance);
-			return instance;
-		}
 	}
 }
