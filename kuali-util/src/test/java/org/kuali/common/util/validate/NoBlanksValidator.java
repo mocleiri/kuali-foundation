7d83e6c1be65b57036753aef9ffcb88cf06a8a2d
jcaddel <jcaddel@kuali.org>
2013-12-12 09:03:06 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29412 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/NoBlanksValidator.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/NoBlanksValidator.java
@@ -1,24 +1,78 @@
 package org.kuali.common.util.validate;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.kuali.common.util.CollectionUtils;
+import org.kuali.common.util.validate.annotation.NoBlanks;
 
 import com.google.common.base.Optional;
 
-public class NoBlanksValidator extends ClassValidator {
+public class NoBlanksValidator implements ConstraintValidator<NoBlanks, Object> {
+
+	private boolean skip;
 
 	@Override
-	protected Optional<String> validate(Field field, Object instance) {
-		if (field.getType() != String.class) {
-			return Optional.absent();
+	public void initialize(NoBlanks constraintAnnotation) {
+		this.skip = constraintAnnotation.skip();
+	}
+
+	@Override
+	public boolean isValid(Object instance, ConstraintValidatorContext constraintContext) {
+		if (skip) {
+			return true;
 		}
-		Optional<?> value = get(field, instance);
-		String string = (String) value.orNull();
-		if (StringUtils.isBlank(string)) {
-			return Optional.of("'" + field.getName() + "' cannot be blank");
+		if (instance == null) {
+			throw new IllegalStateException("instance cannot be null");
+		}
+		Field[] fields = instance.getClass().getDeclaredFields();
+		List<String> errors = new ArrayList<String>();
+		for (Field field : fields) {
+			Optional<String> error = validate(field, instance);
+			if (error.isPresent()) {
+				errors.add(error.get());
+			}
+		}
+
+		if (errors.size() == 0) {
+			return true;
 		} else {
-			return Optional.absent();
+			constraintContext.disableDefaultConstraintViolation();
+			constraintContext.buildConstraintViolationWithTemplate(CollectionUtils.asCSV(errors)).addConstraintViolation();
+			return false;
 		}
 	}
+
+	protected Optional<?> get(Field field, Object instance) {
+		try {
+			synchronized (field) {
+				boolean accessible = field.isAccessible();
+				if (!accessible) {
+					field.setAccessible(true);
+				}
+				Object value = field.get(instance);
+				if (!accessible) {
+					field.setAccessible(false);
+				}
+				return Optional.fromNullable(value);
+			}
+		} catch (IllegalAccessException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	protected Optional<String> validate(Field field, Object instance) {
+		Optional<?> value = get(field, instance);
+		if (value.isPresent()) {
+			return Optional.absent();
+		} else {
+			String className = instance.getClass().getSimpleName();
+			return Optional.of("[" + className + "." + field.getName() + "] cannot be null");
+		}
+	}
+
 }
--- kuali-util/src/test/java/org/kuali/common/util/validate/annotation/NoBlanks.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/annotation/NoBlanks.java
@@ -1,13 +1,29 @@
 package org.kuali.common.util.validate.annotation;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ ElementType.TYPE })
-@Retention(RetentionPolicy.RUNTIME)
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import org.kuali.common.util.validate.NoBlanksValidator;
+
+@Target({ TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = NoBlanksValidator.class)
 @Documented
 public @interface NoBlanks {
+
+	String message() default "blank strings not allowed";
+
+	Class<?>[] groups() default {};
+
+	boolean skip() default false;
+
+	Class<? extends Payload>[] payload() default {};
+
 }
