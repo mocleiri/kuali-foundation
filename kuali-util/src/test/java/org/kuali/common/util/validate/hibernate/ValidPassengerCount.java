44bdb7373cee2f05be8c2987cef313ba09a2c5e9
jcaddel <jcaddel@kuali.org>
2013-12-12 07:34:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29407 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/ValidPassengerCount.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/ValidPassengerCount.java
@@ -1,5 +1,9 @@
 package org.kuali.common.util.validate.hibernate;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
