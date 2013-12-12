92e05688eb4d39054f738448c8b9f71125f7ab33
jcaddel <jcaddel@kuali.org>
2013-12-12 15:15:39 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29458 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/validate/NotBlankIfPresent.java
@@ -0,0 +1,25 @@
+package org.kuali.common.util.validate;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = NoBlanksValidator.class)
+@Documented
+public @interface NotBlankIfPresent {
+
+	String message() default "blank strings not allowed";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
