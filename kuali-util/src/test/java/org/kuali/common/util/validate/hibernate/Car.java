f0485022529f88a5a93056283769a50b59847360
jcaddel <jcaddel@kuali.org>
2013-12-12 09:35:48 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29420 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/Car.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/Car.java
@@ -3,22 +3,18 @@ package org.kuali.common.util.validate.hibernate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.kuali.common.util.validate.annotation.NoNulls;
 
 import com.google.common.collect.ImmutableList;
 
 @ValidPassengerCount
+@NoNulls
 public class Car {
 
-	public List<String> getPassengers() {
-		return passengers;
-	}
-
-	@NotNull
 	private final String manufacturer;
 
-	@NotNull
 	@Size(min = 2, max = 14)
 	@CheckCase(value = CaseMode.UPPER, message = "{CheckCase.Car.licensePlate}")
 	private String licensePlate;
@@ -26,7 +22,6 @@ public class Car {
 	@Min(2)
 	private final int seatCount;
 
-	@NotNull
 	private final List<String> passengers;
 
 	public Car(String manufacturer, String licensePlate, int seatCount) {
@@ -54,6 +49,10 @@ public class Car {
 
 	public int getSeatCount() {
 		return seatCount;
+	}
+
+	public List<String> getPassengers() {
+		return passengers;
 	}
 
 }
