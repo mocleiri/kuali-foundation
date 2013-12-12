4032970d91ce223f6e5641c3296fdc2c8fd87a02
jcaddel <jcaddel@kuali.org>
2013-12-12 05:52:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29383 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/Car.java
@@ -0,0 +1,25 @@
+package org.kuali.common.util.validate.hibernate;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Car {
+
+	@NotNull
+	private final String manufacturer;
+
+	@NotNull
+	@Size(min = 2, max = 14)
+	private final String licensePlate;
+
+	@Min(2)
+	private final int seatCount;
+
+	public Car(String manufacturer, String licencePlate, int seatCount) {
+		this.manufacturer = manufacturer;
+		this.licensePlate = licencePlate;
+		this.seatCount = seatCount;
+	}
+
+}
