499dd50d8edc5e551987ee024c0c0374b0377a1d
jcaddel <jcaddel@kuali.org>
2013-12-13 17:31:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/sandbox/maven-spring-mvc@29585 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/test/java/springapp/web/InventoryControllerTest.java
+++ src/test/java/springapp/web/InventoryControllerTest.java
@@ -3,18 +3,24 @@ package springapp.web;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Map;
+
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
+
+import springapp.domain.SimpleProductManager;
 
 public class InventoryControllerTest {
 
 	@Test
 	public void testHandleRequestView() throws Exception {
 		InventoryController controller = new InventoryController();
+		controller.setProductManager(new SimpleProductManager());
 		ModelAndView modelAndView = controller.handleRequest(null, null);
 		assertEquals("hello", modelAndView.getViewName());
 		assertNotNull(modelAndView.getModel());
-		String nowValue = (String) modelAndView.getModel().get("now");
+		Map<?, ?> modelMap = (Map<?, ?>) modelAndView.getModel().get("model");
+		String nowValue = (String) modelMap.get("now");
 		assertNotNull(nowValue);
 	}
 }
