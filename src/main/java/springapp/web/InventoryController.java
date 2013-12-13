aa8a9e8216803bdc121db5b109b3ad845818975f
jcaddel <jcaddel@kuali.org>
2013-12-13 17:29:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/sandbox/maven-spring-mvc@29584 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/springapp/web/InventoryController.java
+++ src/main/java/springapp/web/InventoryController.java
@@ -2,6 +2,8 @@ package springapp.web;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -32,7 +34,10 @@ public class InventoryController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String now = new Date().toString();
 		logger.info("Returning hello view with " + now);
-		return new ModelAndView("hello", "now", now);
+		Map<String, Object> myModel = new HashMap<String, Object>();
+		myModel.put("now", now);
+		myModel.put("products", this.productManager.getProducts());
+		return new ModelAndView("hello", "model", myModel);
 	}
 
 }
