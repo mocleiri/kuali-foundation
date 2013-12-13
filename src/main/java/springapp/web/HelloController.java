92516b0498c8833a0bbc07a62c422a3094003da8
jcaddel <jcaddel@kuali.org>
2013-12-13 13:45:31 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/sandbox/maven-spring-mvc@29559 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/springapp/web/HelloController.java
@@ -0,0 +1,26 @@
+package springapp.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		logger.info("Returning hello view");
+
+		return new ModelAndView("hello.jsp");
+	}
+
+}
