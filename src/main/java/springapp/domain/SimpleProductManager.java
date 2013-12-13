d189ecf759194245d1c1511294bb5cbdf58f9817
jcaddel <jcaddel@kuali.org>
2013-12-13 17:26:06 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/sandbox/maven-spring-mvc@29581 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/java/springapp/domain/SimpleProductManager.java
+++ src/main/java/springapp/domain/SimpleProductManager.java
@@ -5,18 +5,25 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class SimpleProductManager implements ProductManager {
 
+	private List<Product> products;
+
 	@Override
 	public List<Product> getProducts() {
-		throw new UnsupportedOperationException();
+		return products;
 	}
 
 	@Override
 	public void increasePrice(int percentage) {
-		throw new UnsupportedOperationException();
+		if (products != null) {
+			for (Product product : products) {
+				double newPrice = product.getPrice().doubleValue() * (100 + percentage) / 100;
+				product.setPrice(newPrice);
+			}
+		}
 	}
 
 	public void setProducts(List<Product> products) {
-		throw new UnsupportedOperationException();
+		this.products = products;
 	}
 
 }
