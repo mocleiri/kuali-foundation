e97c7a0d5b163d8ba7b3817a797ea80603e2805b
jcaddel <jcaddel@kuali.org>
2013-12-13 17:51:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/sandbox/maven-spring-mvc@29596 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/web/PriceIncreaseFormController.java
@@ -0,0 +1,51 @@
+package web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import springapp.service.PriceIncrease;
+import springapp.service.ProductManager;
+import springapp.web.InventoryController;
+
+@SuppressWarnings("deprecation")
+public class PriceIncreaseFormController extends SimpleFormController {
+
+	private static final Logger logger = LoggerFactory.getLogger(InventoryController.class);
+
+	private ProductManager productManager;
+
+	@Override
+	public ModelAndView onSubmit(Object command) throws ServletException {
+
+		int increase = ((PriceIncrease) command).getPercentage();
+		logger.info("Increasing prices by " + increase + "%.");
+
+		productManager.increasePrice(increase);
+
+		logger.info("returning from PriceIncreaseForm view to " + getSuccessView());
+
+		return new ModelAndView(new RedirectView(getSuccessView()));
+	}
+
+	@Override
+	protected Object formBackingObject(HttpServletRequest request) throws ServletException {
+		PriceIncrease priceIncrease = new PriceIncrease();
+		priceIncrease.setPercentage(20);
+		return priceIncrease;
+	}
+
+	public void setProductManager(ProductManager productManager) {
+		this.productManager = productManager;
+	}
+
+	public ProductManager getProductManager() {
+		return productManager;
+	}
+
+}
