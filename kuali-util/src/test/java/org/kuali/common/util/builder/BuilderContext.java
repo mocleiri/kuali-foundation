922277dd1c2c6d83d1090f04b8c0d76e20c6a4ba
jcaddel <jcaddel@kuali.org>
2013-12-12 19:22:28 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29509 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/builder/BuilderContext.java
@@ -0,0 +1,49 @@
+package org.kuali.common.util.builder;
+
+import javax.validation.Validator;
+
+import org.kuali.common.util.env.OverrideService;
+
+import com.google.common.base.Preconditions;
+
+public final class BuilderContext {
+
+	public Validator getValidator() {
+		return validator;
+	}
+
+	public OverrideService getOverrider() {
+		return overrider;
+	}
+
+	private final Validator validator;
+	private final OverrideService overrider;
+
+	private BuilderContext(Builder builder) {
+		this.validator = builder.validator;
+		this.overrider = builder.overrider;
+	}
+
+	public static class Builder {
+
+		private final Validator validator;
+		private final OverrideService overrider;
+
+		public Builder(Validator validator, OverrideService overrider) {
+			this.validator = validator;
+			this.overrider = overrider;
+		}
+
+		public BuilderContext build() {
+			BuilderContext instance = new BuilderContext(this);
+			validate(instance);
+			return new BuilderContext(this);
+		}
+
+		private void validate(BuilderContext instance) {
+			Preconditions.checkNotNull(instance.validator, "validator may not be null");
+			Preconditions.checkNotNull(instance.overrider, "overrider may not be null");
+		}
+	}
+
+}
