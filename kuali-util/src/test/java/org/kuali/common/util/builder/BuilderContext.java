952af4026666ca699f66a28fe7c053e79e75461d
jcaddel <jcaddel@kuali.org>
2013-12-12 19:58:17 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29516 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/BuilderContext.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/BuilderContext.java
@@ -2,11 +2,17 @@ package org.kuali.common.util.builder;
 
 import javax.validation.Validator;
 
+import org.kuali.common.util.enc.EncryptionService;
 import org.kuali.common.util.env.OverrideService;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 
 public final class BuilderContext {
+
+	public Optional<EncryptionService> getEnc() {
+		return enc;
+	}
 
 	public Validator getValidator() {
 		return validator;
@@ -18,20 +24,31 @@ public final class BuilderContext {
 
 	private final Validator validator;
 	private final OverrideService overrider;
+	private final Optional<EncryptionService> enc;
 
 	private BuilderContext(Builder builder) {
 		this.validator = builder.validator;
 		this.overrider = builder.overrider;
+		this.enc = builder.enc;
 	}
 
 	public static class Builder {
 
+		// Required
 		private final Validator validator;
 		private final OverrideService overrider;
+
+		// Optional
+		private Optional<EncryptionService> enc;
 
 		public Builder(Validator validator, OverrideService overrider) {
 			this.validator = validator;
 			this.overrider = overrider;
+		}
+
+		public Builder enc(EncryptionService enc) {
+			this.enc = Optional.of(enc);
+			return this;
 		}
 
 		public BuilderContext build() {
@@ -43,6 +60,7 @@ public final class BuilderContext {
 		private void validate(BuilderContext instance) {
 			Preconditions.checkNotNull(instance.validator, "'validator' cannot be null");
 			Preconditions.checkNotNull(instance.overrider, "'overrider' cannot be null");
+			Preconditions.checkNotNull(instance.enc, "'enc' cannot be null");
 		}
 	}
 
