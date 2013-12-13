7be195b5a7e54106851b112849c0249d4e0d60c1
jcaddel <jcaddel@kuali.org>
2013-12-13 17:17:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/sandbox/maven-spring-mvc@29575 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/java/springapp/domain/ProductManager.java
@@ -0,0 +1,12 @@
+package springapp.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface ProductManager extends Serializable {
+
+	public void increasePrice(int percentage);
+
+	public List<Product> getProducts();
+
+}
