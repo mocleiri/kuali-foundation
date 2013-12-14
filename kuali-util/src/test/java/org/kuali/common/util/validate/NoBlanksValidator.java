3bb76e2b4b9b62995e5cd8cf8b202a4dbfc04291
jcaddel <jcaddel@kuali.org>
2013-12-14 18:15:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29644 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/NoBlanksValidator.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/NoBlanksValidator.java
@@ -2,17 +2,16 @@ package org.kuali.common.util.validate;
 
 import java.lang.reflect.Field;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Map;
 
 import javax.validation.ConstraintValidator;
 
 import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.util.ReflectionUtils;
+import org.kuali.common.util.collect.CollectionUtils;
 import org.kuali.common.util.collect.MapUtils;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.Maps;
 
 public class NoBlanksValidator extends AbstractFieldsValidator implements ConstraintValidator<NoBlanks, Object> {
 
@@ -32,14 +31,15 @@ public class NoBlanksValidator extends AbstractFieldsValidator implements Constr
 	@Override
 	protected Optional<String> validate(Field field, Object instance) {
 
-		if (!isValidatableField(field)) {
+		if (!isValidatableType(field)) {
 			return Optional.absent();
 		}
 
 		Optional<?> fieldValue = ReflectionUtils.get(field, instance);
 
 		if (isCharSequence(field)) {
-			return validateCharSequence(field, fieldValue);
+			CharSequence charSequence = (CharSequence) fieldValue.orNull();
+			return getOptionalErrorMessage(field, charSequence);
 		}
 
 		if (!fieldValue.isPresent()) {
@@ -47,22 +47,25 @@ public class NoBlanksValidator extends AbstractFieldsValidator implements Constr
 		}
 
 		if (checkOptionals && isOptional(field)) {
-			return validateOptional(field, fieldValue);
+			Optional<?> optional = (Optional<?>) fieldValue.get();
+			return validateOptional(field, optional);
 		} else if (checkCollections && isCollection(field)) {
-			return validateCollection(field, instance);
+			Collection<?> collection = (Collection<?>) fieldValue.get();
+			return validateCollection(field, collection);
 		} else if (checkMaps && isMap(field)) {
-			return validateMap(field, (Map<?, ?>) fieldValue.get());
+			Map<?, ?> map = (Map<?, ?>) fieldValue.get();
+			return validateMap(field, map);
 		} else {
 			return Optional.absent();
 		}
 	}
 
-	protected boolean isValidatableField(Field field) {
+	protected boolean isValidatableType(Field field) {
 		if (isCharSequence(field)) {
 			return true;
-		} else if (isCollection(field)) {
-			return true;
 		} else if (isOptional(field)) {
+			return true;
+		} else if (CollectionUtils.isCollection(field.getClass())) {
 			return true;
 		} else {
 			return MapUtils.isMap(field.getClass());
@@ -70,11 +73,62 @@ public class NoBlanksValidator extends AbstractFieldsValidator implements Constr
 
 	}
 
+	protected Optional<String> validateMap(Field field, Map<?, ?> map) {
+		Map<?, ?> blanks = MapUtils.getBlankEntries(map);
+		if (blanks.size() > 0) {
+			return Optional.of(getErrorMessage(field, "contains " + blanks.size() + " entries with a blank key or value"));
+		} else {
+			return Optional.absent();
+		}
+	}
+
+	protected Optional<String> validateCollection(Field field, Collection<?> collection) {
+		// Examine the collection for blanks
+		Collection<String> blanks = CollectionUtils.getBlanks(collection);
+		if (blanks.size() > 0) {
+			return Optional.of(getErrorMessage(field, "contains " + blanks.size() + " blank elements"));
+		} else {
+			return Optional.absent();
+		}
+	}
+
+	protected Optional<String> validateOptional(Field field, Optional<?> optional) {
+
+		// The optional does not contain a value
+		if (!optional.isPresent()) {
+			return Optional.absent();
+		}
+
+		// Extract whatever value the optional contains
+		Object value = optional.get();
+
+		// If it's a CharSequence we need to examine it further
+		if (value instanceof CharSequence) {
+			// Cast to a CharSequence
+			CharSequence charSequence = (CharSequence) value;
+			// If the CharSequence is blank, return an error message
+			return getOptionalErrorMessage(field, charSequence);
+		} else {
+			// If it's not a char sequence, we don't care what it is, always return the absence of an error message
+			return Optional.absent();
+		}
+	}
+
+	/**
+	 * If the charSequence is blank, return an error message, otherwise return Optional.absent()
+	 */
+	protected Optional<String> getOptionalErrorMessage(Field field, CharSequence charSequence) {
+		if (StringUtils.isBlank(charSequence)) {
+			return Optional.of(getErrorMessage(field, "cannot be blank"));
+		} else {
+			return Optional.absent();
+		}
+	}
+
 	/**
 	 * Return true if this field is a Collection
 	 */
 	protected boolean isCollection(Field field) {
-		Maps.asMap(null, null);
 		return Collection.class.isAssignableFrom(field.getClass());
 	}
 
@@ -97,104 +151,6 @@ public class NoBlanksValidator extends AbstractFieldsValidator implements Constr
 	 */
 	protected boolean isOptional(Field field) {
 		return Optional.class.isAssignableFrom(field.getClass());
-	}
-
-	protected Optional<String> validateCharSequence(Field field, Optional<?> fieldValue) {
-		// It must be a CharSequence at this point
-		CharSequence charSequence = (CharSequence) fieldValue.orNull();
-
-		// If it's blank return an error message
-		return getBlankCharSequenceErrorMessage(field, charSequence);
-	}
-
-	protected Optional<String> validateOptional(Field field, Object instance) {
-
-		// Determine if there is a value for this field on this object
-		Optional<?> fieldValue = ReflectionUtils.get(field, instance);
-		if (!fieldValue.isPresent()) {
-			// If not, return the absence of an error message
-			return Optional.absent();
-		} else {
-			// The field contains a non-null optional that we need to examine
-			Optional<?> optional = (Optional<?>) fieldValue.get();
-
-			// Examine the optional to see if it contains a blank string
-			return handleOptionalField(field, optional);
-		}
-
-	}
-
-	protected Optional<String> validateMap(Field field, Map<?, ?> map) {
-		Map<?, ?> blanks = MapUtils.getBlankEntries(map);
-		if (blanks.size() > 0) {
-			return Optional.of(getErrorMessage(field, "contains " + blanks.size() + " entries with a blank key or value"));
-		} else {
-			return Optional.absent();
-		}
-	}
-
-	protected Optional<String> validateCollection(Field field, Object instance) {
-
-		// Determine if there is a value for this field on this object
-		Optional<?> fieldValue = ReflectionUtils.get(field, instance);
-		if (!fieldValue.isPresent()) {
-			// If not, return the absence of an error message
-			return Optional.absent();
-		} else {
-			// The field contains a non-null optional that we need to examine
-			Collection<?> collection = (Collection<?>) fieldValue.get();
-
-			Iterator<?> iterator = collection.iterator();
-			int count = 0;
-			while (iterator.hasNext()) {
-				Object value = iterator.next();
-				if (value instanceof CharSequence) {
-					count = StringUtils.isBlank((CharSequence) value) ? count++ : count;
-				} else {
-					return Optional.absent();
-				}
-			}
-
-			if (count > 0) {
-				return Optional.of(getErrorMessage(field, "contains " + count + " blank values"));
-			} else {
-				return Optional.absent();
-			}
-		}
-
-	}
-
-	protected Optional<String> handleOptionalField(Field field, Optional<?> optional) {
-
-		// The optional does not contain a value
-		if (!optional.isPresent()) {
-			return Optional.absent();
-		}
-
-		// Extract whatever value the optional contains
-		Object value = optional.get();
-
-		// If it's a CharSequence we need to examine it further
-		if (value instanceof CharSequence) {
-			// Cast to a CharSequence
-			CharSequence charSequence = (CharSequence) value;
-			// If the CharSequence is blank, return an error message
-			return getBlankCharSequenceErrorMessage(field, charSequence);
-		} else {
-			// If it's not a char sequence, we don't care what it is, always return the absence of an error message
-			return Optional.absent();
-		}
-	}
-
-	/**
-	 * If the charSequence is blank, return an error message, otherwise return Optional.absent()
-	 */
-	protected Optional<String> getBlankCharSequenceErrorMessage(Field field, CharSequence charSequence) {
-		if (StringUtils.isBlank(charSequence)) {
-			return Optional.of(getErrorMessage(field, "cannot be blank"));
-		} else {
-			return Optional.absent();
-		}
 	}
 
 }
