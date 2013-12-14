59103491d0fa717db980c0bf2ee7786fc57f2679
jcaddel <jcaddel@kuali.org>
2013-12-14 10:20:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29613 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/NoBlanksValidator.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/NoBlanksValidator.java
@@ -29,25 +29,45 @@ public class NoBlanksValidator extends AbstractFieldsValidator implements Constr
 	}
 
 	protected Optional<String> handleString(Field field, Object instance) {
+
+		// Extract the value of this field on this object
 		Optional<?> optional = ReflectionUtils.get(field, instance);
+
+		// It can only be a string at this point
 		String string = (String) optional.orNull();
+
+		// If it's blank return an error message
 		return getBlankStringErrorMessage(field, string);
 	}
 
 	protected Optional<String> handleOptional(Field field, Object instance) {
+
+		// Extract the value of this field on this object
 		Optional<?> optional = ReflectionUtils.get(field, instance);
+
+		// If this field on this object is null, we are done
 		if (!optional.isPresent()) {
 			return Optional.absent();
 		}
-		Object value = optional.get();
-		if (value instanceof String) {
-			String string = (String) value;
-			return getBlankStringErrorMessage(field, string);
-		} else {
+
+		// The value of this field can only be an Optional at this point
+		Optional<?> value = (Optional<?>) optional.get();
+
+		// If the optional does not contain a value, we are done
+		if (!value.isPresent()) {
 			return Optional.absent();
 		}
+
+		// We've located an optional that contains a non-null string value
+		String string = (String) value.get();
+
+		// If that string is blank, return an error message
+		return getBlankStringErrorMessage(field, string);
 	}
 
+	/**
+	 * If the string is blank, return an error message, otherwise return Optional.absent()
+	 */
 	protected Optional<String> getBlankStringErrorMessage(Field field, String string) {
 		if (StringUtils.isBlank(string)) {
 			return Optional.of(getErrorMessage(field, "cannot be blank"));
