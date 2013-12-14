223b6f7d0796a8eb77f7a68209e2302fef2b0640
jcaddel <jcaddel@kuali.org>
2013-12-14 17:09:45 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29638 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/collect/CollectionUtils.java
+++ kuali-util/src/main/java/org/kuali/common/util/collect/CollectionUtils.java
@@ -2,6 +2,8 @@ package org.kuali.common.util.collect;
 
 import java.util.Collection;
 
+import com.google.common.collect.ImmutableCollection;
+
 public class CollectionUtils {
 
 	private CollectionUtils() {
@@ -9,5 +11,9 @@ public class CollectionUtils {
 
 	public static boolean isCollection(Class<?> type) {
 		return Collection.class.isAssignableFrom(type);
+	}
+
+	public static boolean isImmutable(Class<?> type) {
+		return ImmutableCollection.class.isAssignableFrom(type);
 	}
 }
---
+++ kuali-util/src/test/java/org/kuali/common/util/validate/ImmutableCollectionsValidator.java
@@ -0,0 +1,48 @@
+package org.kuali.common.util.validate;
+
+import java.lang.reflect.Field;
+
+import javax.validation.ConstraintValidator;
+
+import org.kuali.common.util.ReflectionUtils;
+import org.kuali.common.util.collect.CollectionUtils;
+import org.kuali.common.util.collect.MapUtils;
+
+import com.google.common.base.Optional;
+
+public class ImmutableCollectionsValidator extends AbstractFieldsValidator implements ConstraintValidator<NoNulls, Object> {
+
+	@Override
+	public void initialize(NoNulls constraintAnnotation) {
+		this.skip = constraintAnnotation.skip();
+		this.includeInheritedFields = constraintAnnotation.includeInheritedFields();
+	}
+
+	@Override
+	protected Optional<String> validate(Field field, Object instance) {
+
+		// This field doesn't implement the Map interface, nothing to do
+		if (!CollectionUtils.isCollection(field.getClass())) {
+			return Optional.absent();
+		}
+
+		// Extract the value of the field into an Optional
+		Optional<?> fieldValue = ReflectionUtils.get(field, instance);
+
+		// There is no value for this field (ie it was null)
+		if (!fieldValue.isPresent()) {
+			return Optional.absent();
+		}
+
+		// Get the actual object reference
+		Object value = fieldValue.get();
+
+		if (MapUtils.isImmutable(value.getClass())) {
+			// If it's immutable, we are good to go
+			return Optional.absent();
+		} else {
+			// If not, return an error message
+			return Optional.of(getErrorMessage(field, "is not an immutable map"));
+		}
+	}
+}
