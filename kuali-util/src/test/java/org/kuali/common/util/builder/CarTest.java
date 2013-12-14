9686e75cf35f61aa3b394936ebc4177071528b85
jcaddel <jcaddel@kuali.org>
2013-12-14 18:42:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29650 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/CarTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/CarTest.java
@@ -8,9 +8,9 @@ public class CarTest {
 	@Test
 	public void test() {
 		try {
-			System.setProperty("car.description", "        ");
-			System.setProperty("car.make", "ford");
-			Car car = new Car.Builder(null).description("sucks").build();
+			// System.setProperty("car.description", "        ");
+			// System.setProperty("car.make", "ford");
+			Car car = new Car.Builder(null).description("   ").build();
 			String description = NullUtils.toNone(car.getDescription());
 			System.out.println(car.getMake());
 			System.out.println(description);
