d6e3df672b0900703eceae5c1c270a197990001f
jcaddel <jcaddel@kuali.org>
2013-12-15 11:12:18 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29663 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/binder/ImmutableCarDataBinderTest.java
@@ -0,0 +1,58 @@
+package org.kuali.common.util.spring.binder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.kuali.common.util.log.LoggerUtils;
+import org.slf4j.Logger;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DataBinder;
+import org.springframework.validation.ObjectError;
+
+public class ImmutableCarDataBinderTest {
+
+	private static final Logger logger = LoggerUtils.make();
+
+	@Test
+	public void test() {
+		try {
+			ImmutableCar car = new ImmutableCar.Builder().build();
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("manufacturer", "ford");
+			map.put("color", "red");
+			map.put("year", "bar");
+			map.put("stickerPrice", "foo");
+
+			MutablePropertyValues pvs = new MutablePropertyValues(map);
+			DataBinder binder = new DataBinder(car);
+			binder.bind(pvs);
+			BindingResult result = binder.getBindingResult();
+			if (result.hasErrors()) {
+				throw new IllegalStateException(getErrorMessage(result.getAllErrors()));
+			}
+			logger.info("car.manufacturer=[{}]", car.getManufacturer());
+			logger.info("car.color=[{}]", car.getColor());
+			logger.info("car.stickerPrice=[{}]", car.getStickerPrice());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected String getErrorMessage(List<ObjectError> errors) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Unexpected binding error(s):\n\n");
+		for (int i = 0; i < errors.size(); i++) {
+			if (i != 0) {
+				sb.append("\n");
+			}
+			ObjectError error = errors.get(i);
+			sb.append("[" + error.getDefaultMessage() + "]");
+		}
+		sb.append("\n");
+		return sb.toString();
+	}
+
+}
