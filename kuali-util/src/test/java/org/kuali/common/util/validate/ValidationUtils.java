aa01fb3ee79a3d474d3a6e55bedad2fb3f47e6fe
jcaddel <jcaddel@kuali.org>
2013-12-17 15:11:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29743 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/ValidationUtils.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/ValidationUtils.java
@@ -1,11 +1,15 @@
 package org.kuali.common.util.validate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+
+import org.kuali.common.util.CollectionUtils;
 
 public class ValidationUtils {
 
@@ -23,17 +27,19 @@ public class ValidationUtils {
 		if (violations.size() == 0) {
 			return;
 		}
-		StringBuilder sb = new StringBuilder();
-		int i = 0;
+		List<String> errorMessages = new ArrayList<String>();
 		for (ConstraintViolation<T> violation : violations) {
-			if (i++ != 0) {
-				sb.append(", ");
-			}
-			String name = violation.getRootBeanClass().getSimpleName();
-			String property = violation.getPropertyPath() + "";
-			sb.append("[" + name + "." + property + " " + violation.getMessage() + "]");
+			String errorMessage = getErrorMessage(violation);
+			errorMessages.add(errorMessage);
 		}
-		throw new IllegalArgumentException(sb.toString());
+		String errorMessage = CollectionUtils.asCSV(errorMessages);
+		throw new IllegalArgumentException(errorMessage);
+	}
+
+	public static <T> String getErrorMessage(ConstraintViolation<T> violation) {
+		String name = violation.getRootBeanClass().getSimpleName();
+		String property = violation.getPropertyPath() + "";
+		return "[" + name + "." + property + " " + violation.getMessage() + "]";
 	}
 
 }
