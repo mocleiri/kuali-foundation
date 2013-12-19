c1ee37ba2681311b3620dc80f74fe9d66d8132a7
jcaddel <jcaddel@kuali.org>
2013-12-19 14:46:03 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29811 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/validate/Positive.java
@@ -0,0 +1,29 @@
+package org.kuali.common.util.validate;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.Min;
+
+@Min(1)
+@Target({ FIELD, METHOD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
+public @interface Positive {
+
+	String message() default "must be greater than or equal to one";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
