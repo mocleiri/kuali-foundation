783ed6dd703ff7b0ee2a12698bb66360ae03691d
jcaddel <jcaddel@kuali.org>
2013-12-20 13:41:29 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29849 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/ValidPort.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/ValidPort.java
@@ -1,8 +1,10 @@
 package org.kuali.common.util.validate;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -18,7 +20,7 @@ import org.kuali.common.util.Ports;
 
 @Min(Ports.MIN)
 @Max(Ports.MAX)
-@Target({ FIELD, METHOD, ANNOTATION_TYPE })
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
 @Retention(RUNTIME)
 @Constraint(validatedBy = {})
 @Documented
