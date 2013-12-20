bd778dbdf5a7db69ccaef027d2185bd851dfda3d
jcaddel <jcaddel@kuali.org>
2013-12-20 18:14:54 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29871 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/programmatic/A.java
+++ kuali-util/src/test/java/org/kuali/common/util/validate/hibernate/programmatic/A.java
@@ -1,10 +1,16 @@
 package org.kuali.common.util.validate.hibernate.programmatic;
 
+import java.lang.annotation.ElementType;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
 import javax.validation.Validator;
 
+import org.hibernate.validator.HibernateValidator;
+import org.hibernate.validator.HibernateValidatorConfiguration;
+import org.hibernate.validator.cfg.ConstraintMapping;
+import org.hibernate.validator.cfg.defs.NotNullDef;
 import org.kuali.common.util.validate.NoNullFields;
 import org.kuali.common.util.validate.ValidationUtils;
 
@@ -25,12 +31,23 @@ public class A {
 
 		private String foo;
 
+		private void validate(Builder builder) {
+			HibernateValidatorConfiguration configuration = Validation.byProvider(HibernateValidator.class).configure();
+			ConstraintMapping cm = configuration.createConstraintMapping();
+			cm.type(Builder.class).property("foo", ElementType.FIELD).constraint(new NotNullDef());
+			Validator validator = configuration.addMapping(cm).buildValidatorFactory().getValidator();
+			check(validator.validate(builder));
+		}
+
 		private void validate(A instance) {
 			Validator validator = ValidationUtils.getDefaultValidator();
-			Set<ConstraintViolation<A>> violations = validator.validate(instance);
+			check(validator.validate(instance));
+		}
+
+		private static <T> void check(Set<ConstraintViolation<T>> violations) {
 			if (violations.size() > 0) {
 				StringBuilder sb = new StringBuilder();
-				for (ConstraintViolation<A> violation : violations) {
+				for (ConstraintViolation<T> violation : violations) {
 					sb.append(violation.getMessage());
 				}
 				throw new IllegalArgumentException(sb.toString());
@@ -38,6 +55,7 @@ public class A {
 		}
 
 		public A build() {
+			validate(this);
 			A instance = new A(this);
 			validate(instance);
 			return instance;
