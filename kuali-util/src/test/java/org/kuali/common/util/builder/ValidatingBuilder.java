7c8d2eadc784178243379dc0419c3c056fd519e8
jcaddel <jcaddel@kuali.org>
2013-12-20 14:15:34 -0500

    [maven-release-plugin]  copy for tag externals-maven-plugin-1.0.11
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/externals-maven-plugin-1.0.11@29863 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/ValidatingBuilder.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.common.util.builder;
-
-public interface ValidatingBuilder<T> extends Builder<T> {
-
-	void validate(T instance);
-
-}
--- kuali-util/src/test/java/org/kuali/common/util/env/adapter/EnvAdapter.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.common.util.env.adapter;
-
-public interface EnvAdapter<S, T> {
-
-	Class<S> getSourceType();
-
-	T convert(S source);
-}
