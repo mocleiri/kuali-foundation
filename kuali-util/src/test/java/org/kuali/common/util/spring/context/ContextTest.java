d0548cfb47dc93b71a3c9cd98b9805aae4e388e1
jcaddel <jcaddel@kuali.org>
2013-12-29 13:12:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29985 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/context/ContextTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/context/ContextTest.java
@@ -1,9 +1,15 @@
 package org.kuali.common.util.spring.context;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Test;
 import org.kuali.common.util.log.LoggerUtils;
 import org.slf4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.google.common.collect.ImmutableList;
 
 public class ContextTest {
 
@@ -15,10 +21,17 @@ public class ContextTest {
 			AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 			ctx.setId("foo");
 			ctx.setDisplayName("bar");
+			// ctx.register(CerealConfig.class, MilkConfig.class);
 			ctx.register(BreakfastConfig.class);
 			ctx.refresh();
-			logger.info("Id: [{}]", ctx.getId());
-			logger.info("Name: [{}]", ctx.getDisplayName());
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			logger.info("{} beans in this application context", beanNames.length);
+			List<String> names = new ArrayList<String>(ImmutableList.copyOf(beanNames));
+			Collections.sort(names);
+			for (String name : names) {
+				Object bean = ctx.getBean(name);
+				logger.info("[{}]=[{}]", name, bean.getClass().getCanonicalName());
+			}
 			ctx.close();
 		} catch (Exception e) {
 			e.printStackTrace();
