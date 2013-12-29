f56b801947108e965785a9cafe211c242046c4dd
jcaddel <jcaddel@kuali.org>
2013-12-29 12:56:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29981 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/context/ContextTest.java
@@ -0,0 +1,28 @@
+package org.kuali.common.util.spring.context;
+
+import org.junit.Test;
+import org.kuali.common.util.log.LoggerUtils;
+import org.slf4j.Logger;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ContextTest {
+
+	private static final Logger logger = LoggerUtils.make();
+
+	@Test
+	public void test() {
+		try {
+			AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+			ctx.setId("foo");
+			ctx.setDisplayName("bar");
+			ctx.register(BreakfastConfig.class);
+			ctx.refresh();
+			logger.info("Id: [{}]", ctx.getId());
+			logger.info("Name: [{}]", ctx.getDisplayName());
+			ctx.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
