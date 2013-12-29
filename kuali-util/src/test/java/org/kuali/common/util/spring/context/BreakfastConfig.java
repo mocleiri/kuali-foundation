a12ace57d8baa3b5186fabb0c5f8049a489af9e4
jcaddel <jcaddel@kuali.org>
2013-12-29 12:45:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@29979 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/context/BreakfastConfig.java
@@ -0,0 +1,17 @@
+package org.kuali.common.util.spring.context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BreakfastConfig {
+
+	@Bean
+	public Cereal cereal() {
+		Cereal c = new Cereal();
+		c.setName("chocolate cheerios");
+		c.setPrice(2.50);
+		return c;
+	}
+
+}
