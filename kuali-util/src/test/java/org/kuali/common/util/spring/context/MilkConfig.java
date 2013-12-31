b6012c014e64109d1afc440b32f0ba85c095f7db
jcaddel <jcaddel@kuali.org>
2013-12-30 21:23:53 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30000 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/context/MilkConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/context/MilkConfig.java
@@ -2,8 +2,10 @@ package org.kuali.common.util.spring.context;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:org/kuali/common/util/spring/context/breakfast.properties")
 @ImportResource("classpath:org/kuali/common/util/spring/context/milk-context.xml")
 public class MilkConfig {
 
