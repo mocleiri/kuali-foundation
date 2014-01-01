6f960fe845a60e27133dfc6d3cf9f64c98fa4d69
jcaddel <jcaddel@kuali.org>
2014-01-01 00:12:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30033 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/properties/Location.java
+++ kuali-util/src/main/java/org/kuali/common/util/properties/Location.java
@@ -1,8 +1,11 @@
 package org.kuali.common.util.properties;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.nio.charset.Charset;
 
-import org.kuali.common.util.Assert;
+import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.util.Mode;
 import org.kuali.common.util.property.PropertyFormat;
 
@@ -36,13 +39,12 @@ public final class Location {
 	}
 
 	public Location(String value, String encoding, Mode missingMode, PropertyFormat format, boolean cacheable) {
-		Assert.noBlanks(value, encoding);
-		Assert.noNulls(missingMode, format);
 		this.value = value;
 		this.encoding = encoding;
 		this.missingMode = missingMode;
 		this.format = format;
 		this.cacheable = cacheable;
+		Builder.validate(this);
 	}
 
 	public Mode getMissingMode() {
@@ -63,6 +65,104 @@ public final class Location {
 
 	public boolean isCacheable() {
 		return cacheable;
+	}
+
+	private Location(Builder builder) {
+		this.missingMode = builder.missingMode;
+		this.encoding = builder.encoding;
+		this.format = builder.format;
+		this.value = builder.value;
+		this.cacheable = builder.cacheable;
+	}
+
+	public static Builder builder(String value) {
+		return new Builder(value);
+	}
+
+	public static Builder builder(Location loc, String newValue) {
+		return new Builder(newValue).cacheable(loc.isCacheable()).encoding(loc.getEncoding()).format(loc.getFormat()).missingMode(loc.getMissingMode());
+	}
+
+	public static class Builder {
+
+		private final String value;
+		private Mode missingMode = DEFAULT_MISSING_MODE;
+		private String encoding = DEFAULT_ENCODING;
+		private PropertyFormat format = DEFAULT_PROPERTY_FORMAT;
+		private boolean cacheable = DEFAULT_CACHEABLE;
+
+		public Builder(String value) {
+			this.value = value;
+		}
+
+		public Builder missingMode(Mode missingMode) {
+			this.missingMode = missingMode;
+			return this;
+		}
+
+		public Builder encoding(String encoding) {
+			this.encoding = encoding;
+			return this;
+		}
+
+		public Builder format(PropertyFormat format) {
+			this.format = format;
+			return this;
+		}
+
+		public Builder cacheable(boolean cacheable) {
+			this.cacheable = cacheable;
+			return this;
+		}
+
+		public Location build() {
+			Location instance = new Location(this);
+			validate(instance);
+			return instance;
+		}
+
+		private static void validate(Location instance) {
+			checkArgument(!StringUtils.isBlank(instance.value), "value cannot be blank");
+			checkArgument(!StringUtils.isBlank(instance.encoding), "encoding cannot be blank");
+			checkNotNull(instance.format, "format cannot be null");
+			checkNotNull(instance.missingMode, "missingMode cannot be null");
+		}
+
+		public Mode getMissingMode() {
+			return missingMode;
+		}
+
+		public void setMissingMode(Mode missingMode) {
+			this.missingMode = missingMode;
+		}
+
+		public String getEncoding() {
+			return encoding;
+		}
+
+		public void setEncoding(String encoding) {
+			this.encoding = encoding;
+		}
+
+		public PropertyFormat getFormat() {
+			return format;
+		}
+
+		public void setFormat(PropertyFormat format) {
+			this.format = format;
+		}
+
+		public boolean isCacheable() {
+			return cacheable;
+		}
+
+		public void setCacheable(boolean cacheable) {
+			this.cacheable = cacheable;
+		}
+
+		public String getValue() {
+			return value;
+		}
 	}
 
 }
