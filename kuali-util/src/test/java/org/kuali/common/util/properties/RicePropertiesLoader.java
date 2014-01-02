404094291e4c6b312790eaa7cb054c27da22df8c
jcaddel <jcaddel@kuali.org>
2014-01-01 21:59:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30079 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/properties/RicePropertiesLoader.java
+++ kuali-util/src/test/java/org/kuali/common/util/properties/RicePropertiesLoader.java
@@ -17,8 +17,8 @@ package org.kuali.common.util.properties;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.xml.bind.JAXBContext;
@@ -48,6 +48,7 @@ import org.xml.sax.XMLReader;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
 
 /**
  * <p>
@@ -80,12 +81,13 @@ public class RicePropertiesLoader {
 		Preconditions.checkArgument(!StringUtils.isBlank(location), "'location' cannot be blank");
 		Preconditions.checkArgument(LocationUtils.exists(location), "[%s] does not exist", location);
 		Unmarshaller unmarshaller = getUnmarshaller();
-		Properties properties = new Properties();
-		load(location, unmarshaller, 0, properties);
+		Map<String, Param> params = Maps.newHashMap();
+		load(location, unmarshaller, 0, params);
+		Properties properties = convert(params);
 		return properties;
 	}
 
-	protected void load(String location, Unmarshaller unmarshaller, int depth, Properties properties) {
+	protected void load(String location, Unmarshaller unmarshaller, int depth, Map<String, Param> params) {
 
 		// Setup an indentation prefix based on the recursive depth
 		final String prefix = StringUtils.repeat(" ", depth);
@@ -99,7 +101,7 @@ public class RicePropertiesLoader {
 		InputStream in = null;
 		try {
 			in = LocationUtils.getInputStream(location);
-			load(prefix, location, in, properties, depth, unmarshaller);
+			load(prefix, location, in, params, depth, unmarshaller);
 		} catch (IOException e) {
 			throw new IllegalStateException(e);
 		} finally {
@@ -107,30 +109,30 @@ public class RicePropertiesLoader {
 		}
 	}
 
-	protected void load(String prefix, String location, InputStream in, Properties properties, int depth, Unmarshaller unmarshaller) throws IOException {
+	protected void load(String prefix, String location, InputStream in, Map<String, Param> params, int depth, Unmarshaller unmarshaller) throws IOException {
 		if (isPropertiesFile(location)) {
-			loadJavaProperties(prefix, location, in, properties, depth);
+			loadJavaProperties(prefix, location, in, params, depth);
 		} else {
-			loadRiceProperties(prefix, location, in, properties, depth, unmarshaller);
+			loadRiceProperties(prefix, location, in, params, depth, unmarshaller);
 		}
 	}
 
-	protected void loadRiceProperties(String prefix, String location, InputStream in, Properties properties, int depth, Unmarshaller unmarshaller) throws IOException {
+	protected void loadRiceProperties(String prefix, String location, InputStream in, Map<String, Param> params, int depth, Unmarshaller unmarshaller) throws IOException {
 		logger.info("{}+ loading - [{}]", prefix, location);
 		Config config = unmarshal(unmarshaller, in);
 		for (Param p : config.getParams()) {
-			handleParam(p, depth, unmarshaller, properties, prefix);
+			handleParam(p, depth, unmarshaller, params, prefix);
 		}
 		logger.info("{}- loaded  - [{}]", prefix, location);
 	}
 
-	protected void handleParam(Param p, int depth, Unmarshaller unmarshaller, Properties properties, String prefix) {
+	protected void handleParam(Param p, int depth, Unmarshaller unmarshaller, Map<String, Param> params, String prefix) {
 
 		// This is a reference to a nested config file
 		if (p.getName().equalsIgnoreCase(magicNestedConfigKey)) {
 			String originalLocation = p.getValue();
-			String resolvedLocation = getResolvedValue(originalLocation, properties);
-			load(resolvedLocation, unmarshaller, depth + 1, properties);
+			String resolvedLocation = getResolvedValue(originalLocation, params);
+			load(resolvedLocation, unmarshaller, depth + 1, params);
 			return;
 		}
 
@@ -138,68 +140,68 @@ public class RicePropertiesLoader {
 		checkParam(p);
 
 		// Update the properties object with this parameter
-		update(properties, p, prefix);
+		update(params, p, prefix);
 
 	}
 
-	protected void loadJavaProperties(String prefix, String location, InputStream in, Properties properties, int depth) throws IOException {
+	protected void loadJavaProperties(String prefix, String location, InputStream in, Map<String, Param> params, int depth) throws IOException {
 		logger.info("{}+ loading - [{}]", prefix, location);
 		Properties loaded = new Properties();
 		loaded.load(in);
-		List<Param> params = getParams(loaded);
-		for (Param p : params) {
-			update(properties, p, prefix);
+		Map<String, Param> newMap = getParamMap(loaded);
+		for (Param p : newMap.values()) {
+			update(params, p, prefix);
 		}
 		logger.info("{}- loaded  - [{}]", prefix, location);
 	}
 
-	protected void update(Properties properties, Param p, String prefix) {
+	protected void update(Map<String, Param> params, Param p, String prefix) {
 		Preconditions.checkNotNull(p.getValue(), "parameter value cannot be null");
 
-		// Extract the old value (it it's present)
-		Optional<String> oldValue = Optional.fromNullable(properties.getProperty(p.getName()));
+		// Extract the old value (if it's present)
+		Optional<Param> oldParam = Optional.fromNullable(params.get(p.getName()));
 
 		// Get a log friendly value
-		String newLogValue = getLogValue(p.getName(), p.getValue());
+		String newLogValue = getLogValue(p);
 
 		// If there is no previous value, just add it
-		if (!oldValue.isPresent()) {
+		if (!oldParam.isPresent()) {
 			Object[] args = { prefix, p.getName(), newLogValue };
 			logger.debug("{}~ add - [{}]=[{}]", args);
-			properties.setProperty(p.getName(), p.getValue());
+			params.put(p.getName(), p);
 			return;
 		}
 
 		// The new value is the same as the old value. Nothing more to do
-		if (oldValue.get().equals(p.getValue())) {
+		if (oldParam.get().getValue().equals(p.getValue())) {
 			Object[] args = { prefix, p.getName(), newLogValue };
 			logger.debug("{}~ duplicate - [{}]=[{}]", args);
 			return;
 		}
 
 		// Get a log friendly value
-		String oldLogValue = getLogValue(p.getName(), oldValue.get());
+		String oldLogValue = getLogValue(oldParam.get());
 
 		// There is a new value for this property and it's different than the old value
 		Object[] args = { prefix, p.getName(), oldLogValue, newLogValue };
 		if (p.isOverride()) {
 			// Change it, and log the fact that we are changing it
 			logger.info("{}* override - [{}]=[{}] -> [{}]", args);
-			properties.setProperty(p.getName(), p.getValue());
+			params.put(p.getName(), p);
 		} else {
 			// Ignore it, and log the fact that we are ignoring the new value from the config file
 			logger.info("{}~ ignore - [{}]=[{}] -> ignoring [{}]", args);
 		}
 	}
 
-	protected String getLogValue(String key, String value) {
-		String lcase = key.toLowerCase();
+	protected String getLogValue(Param param) {
+		String lcase = param.getName().toLowerCase();
 		for (String obscurePattern : obscureTokens) {
 			if (lcase.contains(obscurePattern)) {
-				return Str.flatten(obscurer.obscure(value));
+				return Str.flatten(obscurer.obscure(param.getValue()));
 			}
 		}
-		return Str.flatten(value);
+		return Str.flatten(param.getValue());
 	}
 
 	protected void checkParam(Param param) {
@@ -235,8 +237,8 @@ public class RicePropertiesLoader {
 		}
 	}
 
-	protected List<Param> getParams(Properties properties) {
-		List<Param> params = new ArrayList<Param>();
+	protected Map<String, Param> getParamMap(Properties properties) {
+		Map<String, Param> params = Maps.newHashMap();
 		List<String> keys = PropertyUtils.getSortedKeys(properties);
 		for (String key : keys) {
 			String value = properties.getProperty(key);
@@ -245,7 +247,7 @@ public class RicePropertiesLoader {
 			// Normal .properties files don't have a way to toggle an "override" attribute at the individual property level (nor should they)
 			// Thus, the default value of "true"
 			Param param = Param.builder(key, value).override(true).build();
-			params.add(param);
+			params.put(param.getName(), param);
 		}
 		return params;
 	}
@@ -255,7 +257,16 @@ public class RicePropertiesLoader {
 		return StringUtils.endsWith(lower, ".properties");
 	}
 
-	protected String getResolvedValue(String value, Properties properties) {
+	protected Properties convert(Map<String, Param> params) {
+		Properties properties = new Properties();
+		for (Param p : params.values()) {
+			properties.setProperty(p.getName(), p.getValue());
+		}
+		return properties;
+	}
+
+	protected String getResolvedValue(String value, Map<String, Param> params) {
+		Properties properties = convert(params);
 		Properties global = PropertyUtils.getGlobalProperties(properties);
 		return propertyPlaceholderHelper.replacePlaceholders(value, global);
 	}
