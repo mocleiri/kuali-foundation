b4d9d2b7d942b9c4d4323fd272ac8457993c5224
jcaddel <jcaddel@kuali.org>
2014-01-08 03:46:06 -0500

    Better comment
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30114 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/metainf/model/MetaInfResourcePathComparator.java
+++ kuali-util/src/main/java/org/kuali/common/util/metainf/model/MetaInfResourcePathComparator.java
@@ -25,7 +25,7 @@ public class MetaInfResourcePathComparator implements Comparator<MetaInfResource
 		String[] tokens1 = one.getLocation().split("/");
 		String[] tokens2 = two.getLocation().split("/");
 
-		// Compare each token
+		// Compare path tokens from both locations
 		for (int i = 0; i < tokens1.length && i < tokens2.length; i++) {
 
 			// We hit the end of 'one' but 'two' still has more tokens
