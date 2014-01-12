91c81681857818fe8bf9743523f2fbf2acd396f1
jcaddel <jcaddel@kuali.org>
2014-01-12 18:26:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30262 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/helloworld/HelloWorldTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/helloworld/HelloWorldTest.java
@@ -1,11 +1,14 @@
 package org.kuali.common.util.helloworld;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.ServiceLoader;
 
 import org.junit.Test;
 import org.kuali.common.util.log.LoggerUtils;
 import org.slf4j.Logger;
+
+import com.google.common.collect.Lists;
 
 public class HelloWorldTest {
 
@@ -16,9 +19,15 @@ public class HelloWorldTest {
 		try {
 			ServiceLoader<HelloWorldService> loader = ServiceLoader.load(HelloWorldService.class);
 			Iterator<HelloWorldService> itr = loader.iterator();
+			List<HelloWorldService> services = Lists.newArrayList();
 			while (itr.hasNext()) {
-				HelloWorldService service = itr.next();
+				services.add(itr.next());
+			}
+			for (HelloWorldService service : services) {
 				logger.info(String.format("service=%s", service.getClass().getCanonicalName()));
+			}
+			for (HelloWorldService service : services) {
+				service.sayHello();
 			}
 		} catch (Throwable t) {
 			t.printStackTrace();
