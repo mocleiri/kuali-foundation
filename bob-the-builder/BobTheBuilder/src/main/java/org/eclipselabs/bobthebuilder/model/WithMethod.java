26fa7e48b42338fd12b39298068260f5faec1a7d
jcaddel <jcaddel@kuali.org>
2014-01-12 16:41:44 -0500

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30248 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- bob-the-builder/BobTheBuilder/src/main/java/org/eclipselabs/bobthebuilder/model/WithMethod.java
+++ bob-the-builder/BobTheBuilder/src/main/java/org/eclipselabs/bobthebuilder/model/WithMethod.java
@@ -8,75 +8,72 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class WithMethod implements Comparable<WithMethod> {
 
-  private final String name;
+	private final String name;
 
-  private final Field field;
+	private final Field field;
 
-  private WithMethod(Builder builder) {
-    this.name = builder.name;
-    this.field = builder.field;
-  }
+	private WithMethod(Builder builder) {
+		this.name = builder.name;
+		this.field = builder.field;
+	}
 
-  public static class Builder {
+	public static class Builder {
 
-    private String name;
+		private String name;
 
-    private Field field;
+		private Field field;
 
-    public Builder withName(String name) {
-      this.name = name;
-      return this;
-    }
+		public Builder withName(String name) {
+			this.name = name;
+			return this;
+		}
 
-    public Builder withField(Field field) {
-      this.field = field;
-      return this;
-    }
+		public Builder withField(Field field) {
+			this.field = field;
+			return this;
+		}
 
-    public WithMethod build() {
-      validate();
-      return new WithMethod(this);
-    }
+		public WithMethod build() {
+			validate();
+			return new WithMethod(this);
+		}
 
-    private void validate() {
-      Validate.isTrue(!StringUtils.isBlank(name), "name may not be blank");
-      Validate.notNull(field, "field may not be null");
-    }
-  }
+		private void validate() {
+			Validate.isTrue(!StringUtils.isBlank(name), "name may not be blank");
+			Validate.notNull(field, "field may not be null");
+		}
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public Field getField() {
-    return field;
-  }
+	public Field getField() {
+		return field;
+	}
 
-  public static WithMethod getInstanceFromField(Field field) {
-    Validate.notNull(field, "field may not be null");
-    return new WithMethod.Builder()
-        .withName("with" + StringUtils.capitalize(field.getName()))
-        .withField(field)
-        .build();
-  }
+	public static WithMethod getInstanceFromField(Field field) {
+		Validate.notNull(field, "field may not be null");
+		return new WithMethod.Builder().withName("with" + StringUtils.capitalize(field.getName())).withField(field).build();
+	}
 
-  @Override
-  public int hashCode() {
-    return HashCodeBuilder.reflectionHashCode(this);
-  }
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    return EqualsBuilder.reflectionEquals(this, obj);
-  }
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
 
-  @Override
-  public String toString() {
-    return ToStringBuilder.reflectionToString(this);
-  }
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 
-  @Override
-  public int compareTo(WithMethod o) {
-    return this.field.compareTo(o.field);
-  }
+	@Override
+	public int compareTo(WithMethod o) {
+		return this.field.compareTo(o.field);
+	}
 }
