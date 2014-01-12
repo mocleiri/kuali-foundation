8299c68d5a1471a41f2541ca5682e5cee2a48313
jcaddel <jcaddel@kuali.org>
2014-01-12 16:20:38 -0500

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30242 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- bob-the-builder/BobTheBuilder/src/main/java/org/eclipselabs/bobthebuilder/ValidationFramework.java
+++ bob-the-builder/BobTheBuilder/src/main/java/org/eclipselabs/bobthebuilder/ValidationFramework.java
@@ -6,93 +6,70 @@ import org.eclipselabs.bobthebuilder.model.Field;
 
 //TODO StringUtils import needs to be added as well
 public enum ValidationFramework {
-  GOOGLE_GUAVA(
-      "com.google.common.base.Preconditions",
-      "Preconditions.checkNotNull",
-      "Preconditions.checkArgument"),
-  COMMONS_LANG3(
-      "org.apache.commons.lang3.Validate",
-      "Validate.notNull",
-      "Validate.isTrue"),
-  COMMONS_LANG2(
-      "org.apache.commons.lang.Validate",
-      COMMONS_LANG3.checkNotNull,
-      COMMONS_LANG3.checkArgument);
 
-  private static final String checkNotNullTemplateEnding = "(%1$s, \"%1$s may not be null\");";
+	GOOGLE_GUAVA("com.google.common.base.Preconditions", "Preconditions.checkNotNull", "Preconditions.checkArgument"), COMMONS_LANG3("org.apache.commons.lang3.Validate",
+			"Validate.notNull", "Validate.isTrue"), COMMONS_LANG2("org.apache.commons.lang.Validate", COMMONS_LANG3.checkNotNull, COMMONS_LANG3.checkArgument);
 
-  private static final String checkBlankStringTemplateEnding =
-      "(!StringUtils.isBlank(%1$s), \"%1$s may not be blank\");";
+	private static final String checkNotNullTemplateEnding = "(%1$s, \"%1$s may not be null\");";
 
-  private static final String checkNotDefaultTemplateEnding = "(%1$s > %2$s, \"%1$s should be set\");";
+	private static final String checkBlankStringTemplateEnding = "(!StringUtils.isBlank(%1$s), \"%1$s may not be blank\");";
 
-  private static final String checkNotEmptyCollectionTemplateEnding =
-      "(!%1$s.isEmpty(), \"%1$s may not be empty\");";
+	private static final String checkNotDefaultTemplateEnding = "(%1$s > %2$s, \"%1$s should be set\");";
 
-  public String fullClassName;
+	private static final String checkNotEmptyCollectionTemplateEnding = "(!%1$s.isEmpty(), \"%1$s may not be empty\");";
 
-  private String checkArgument;
+	public String fullClassName;
 
-  private String checkNotNull;
+	private String checkArgument;
 
-  private ValidationFramework(String fullClassName, String checkNotNull, String checkArgument) {
-    this.fullClassName = fullClassName;
-    this.checkArgument = checkArgument;
-    this.checkNotNull = checkNotNull;
-  }
+	private String checkNotNull;
 
-  public String getFullClassName() {
-    return fullClassName;
-  }
+	private ValidationFramework(String fullClassName, String checkNotNull, String checkArgument) {
+		this.fullClassName = fullClassName;
+		this.checkArgument = checkArgument;
+		this.checkNotNull = checkNotNull;
+	}
 
-  public String getCheckArgument() {
-    return checkArgument;
-  }
+	public String getFullClassName() {
+		return fullClassName;
+	}
 
-  public String getCheckNotNull() {
-    return checkNotNull;
-  }
+	public String getCheckArgument() {
+		return checkArgument;
+	}
 
-  public String getReadableName() {
-    return StringUtils.lowerCase(this.name()).replace('_', ' ');
-  }
-  
-  public String composeFieldValidation(Field field) {
-    Validate.notNull(field, "field may not be null");
-    String signature = field.getSignature();
-    String fieldName = field.getName();
-    if (signature.equals("byte")) {
-      return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0");
-    }
-    else if (signature.equals("char")) {
-      return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "'\u0000'");
-    }
-    else if (signature.equals("double")) {
-      return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0L");
-    }
-    else if (signature.equals("float")) {
-      return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0.0f");
-    }
-    else if (signature.equals("int")) {
-      return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0");
-    }
-    else if (signature.equals("long")) {
-      return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0L");
-    }
-    else if (signature.equals("short")) {
-      return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0");
-    }
-    else if (signature.contains("String") || signature.contains("java.lang.String")) {
-      return String.format(checkArgument + checkBlankStringTemplateEnding, fieldName);
-    }
-    else if (signature.contains("Map")
-        || signature.contains("Set")
-        || signature.contains("List")) {
-      return String.format(checkNotNull + checkNotNullTemplateEnding, fieldName) + " " +
-        String.format(checkArgument + checkNotEmptyCollectionTemplateEnding, fieldName);
-    }
-    else {
-      return String.format(checkNotNull + checkNotNullTemplateEnding, fieldName);
-    }
-  }
+	public String getCheckNotNull() {
+		return checkNotNull;
+	}
+
+	public String getReadableName() {
+		return StringUtils.lowerCase(this.name()).replace('_', ' ');
+	}
+
+	public String composeFieldValidation(Field field) {
+		Validate.notNull(field, "field may not be null");
+		String signature = field.getSignature();
+		String fieldName = field.getName();
+		if (signature.equals("byte")) {
+			return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0");
+		} else if (signature.equals("char")) {
+			return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "'\u0000'");
+		} else if (signature.equals("double")) {
+			return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0L");
+		} else if (signature.equals("float")) {
+			return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0.0f");
+		} else if (signature.equals("int")) {
+			return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0");
+		} else if (signature.equals("long")) {
+			return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0L");
+		} else if (signature.equals("short")) {
+			return String.format(checkArgument + checkNotDefaultTemplateEnding, fieldName, "0");
+		} else if (signature.contains("String") || signature.contains("java.lang.String")) {
+			return String.format(checkArgument + checkBlankStringTemplateEnding, fieldName);
+		} else if (signature.contains("Map") || signature.contains("Set") || signature.contains("List")) {
+			return String.format(checkNotNull + checkNotNullTemplateEnding, fieldName) + " " + String.format(checkArgument + checkNotEmptyCollectionTemplateEnding, fieldName);
+		} else {
+			return String.format(checkNotNull + checkNotNullTemplateEnding, fieldName);
+		}
+	}
 }
