362fe9770b12430bf9b8da06269686b52fd41160
jcaddel <jcaddel@kuali.org>
2014-01-13 01:48:14 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30283 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/AbstractValidatingBuilder.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/AbstractValidatingBuilder.java
@@ -3,19 +3,47 @@ package org.kuali.common.util.builder;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
 import org.kuali.common.util.validate.Validation;
+
+import com.google.common.base.Preconditions;
 
 public abstract class AbstractValidatingBuilder<T> implements ValidatingBuilder<T> {
 
 	protected abstract T getInstance();
 
+	private final Validator validator;
+
 	@Override
 	public final T build() {
 		T instance = getInstance();
-		Set<ConstraintViolation<T>> violations = Validation.getDefaultValidator().validate(instance);
+		Set<ConstraintViolation<T>> violations = validate(instance);
 		Validation.check(violations);
 		return instance;
+	}
+
+	@Override
+	public Set<ConstraintViolation<T>> validate(T instance) {
+		return validator.validate(instance);
+	}
+
+	private AbstractValidatingBuilder(Builder<T> builder) {
+		this.validator = builder.validator;
+	}
+
+	public abstract static class Builder<T> implements ValidatingBuilder<AbstractValidatingBuilder<T>> {
+
+		private Validator validator = Validation.getDefaultValidator();
+
+		public Builder<T> validator(Validator validator) {
+			this.validator = validator;
+			return this;
+		}
+
+		protected static <T> void validate(AbstractValidatingBuilder<T> instance) {
+			Preconditions.checkNotNull(instance.validator, "'validator' cannot be null");
+		}
 	}
 
 }
