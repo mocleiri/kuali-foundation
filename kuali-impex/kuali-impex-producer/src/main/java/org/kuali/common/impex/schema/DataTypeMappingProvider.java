de16ffd40461cc4c57063f99b29436c2b31896df
jcaddel <jcaddel@kuali.org>
2014-01-14 16:04:16 -0500

    [maven-release-plugin]  copy for tag kuali-deploy-3.0.6
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-deploy-3.0.6@30361 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-producer/src/main/java/org/kuali/common/impex/schema/DataTypeMappingProvider.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common.impex.schema;
-
-import org.kuali.common.impex.model.Column;
-
-public interface DataTypeMappingProvider {
-
-    DataTypeMapping getDataTypeMapping(Column column);
-
-}
