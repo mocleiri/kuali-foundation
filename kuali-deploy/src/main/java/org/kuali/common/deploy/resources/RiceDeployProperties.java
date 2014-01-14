ea6a9d4d74a5c1a70c835eacfea0a93a86560383
jcaddel <jcaddel@kuali.org>
2014-01-13 21:34:28 -0500

    Better abstraction for locating project resources
    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30303 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-deploy/src/main/java/org/kuali/common/deploy/resources/RiceDeployProperties.java
+++ kuali-deploy/src/main/java/org/kuali/common/deploy/resources/RiceDeployProperties.java
@@ -1,33 +1,29 @@
 package org.kuali.common.deploy.resources;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
-import org.apache.commons.lang3.StringUtils;
 import org.kuali.common.deploy.project.DeployProjectConstants;
-import org.kuali.common.util.project.model.ProjectIdentifier;
-import org.kuali.common.util.properties.model.ProjectResource;
+import org.kuali.common.util.LocationUtils;
+import org.kuali.common.util.project.ProjectUtils;
+import org.kuali.common.util.project.model.ProjectResource;
 
-public enum RiceDeployProperties implements ProjectResource {
+public enum RiceDeployProperties {
 
-	DB("rice/db.properties"), //
-	INIT_SOURCE_DB("rice/initialize-source-db.properties"); //
+	DB(ProjectResource.create(DeployProjectConstants.ID, "rice/db.properties")), //
+	INIT_SOURCE_DB(ProjectResource.create(DeployProjectConstants.ID, "rice/initialize-source-db.properties")); //
 
-	private RiceDeployProperties(String filename) {
-		checkArgument(!StringUtils.isBlank(filename));
-		this.filename = filename;
+	private RiceDeployProperties(ProjectResource resource) {
+		checkNotNull(resource, "'resource' cannot be null");
+		this.resource = resource;
+		String path = ProjectUtils.getPath(resource);
+		checkArgument(LocationUtils.exists(path), "[%s] does not exist", path);
 	}
 
-	private final ProjectIdentifier project = DeployProjectConstants.ID;
-	private final String filename;
+	private final ProjectResource resource;
 
-	@Override
-	public ProjectIdentifier getProject() {
-		return project;
-	}
-
-	@Override
-	public String getFilename() {
-		return filename;
+	public ProjectResource getResource() {
+		return resource;
 	}
 
 }
