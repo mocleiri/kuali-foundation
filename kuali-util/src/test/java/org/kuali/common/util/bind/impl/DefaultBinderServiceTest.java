7d34dad993f68752e5632bdf4f12d2b71efcfb9c
jcaddel <jcaddel@kuali.org>
2014-01-15 18:48:30 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30425 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/impl/DefaultBinderServiceTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/impl/DefaultBinderServiceTest.java
@@ -1,5 +1,6 @@
 package org.kuali.common.util.bind.impl;
 
+import org.codehaus.plexus.util.StringUtils;
 import org.junit.Test;
 import org.kuali.common.util.bind.api.BinderService;
 import org.kuali.common.util.bind.model.User;
@@ -16,9 +17,10 @@ public class DefaultBinderServiceTest {
 			BinderService service = new DefaultBinderService();
 			User user = new User();
 			service.bind(user);
-			logger.info(String.format("name=%s", user.getName()));
-			logger.info(String.format("home=%s", user.getHome()));
-			logger.info(String.format("dir=%s", user.getDir()));
+			String prefix = StringUtils.uncapitalise(User.class.getSimpleName());
+			logger.info(String.format("%s.name=%s", prefix, user.getName()));
+			logger.info(String.format("%s.home=%s", prefix, user.getHome()));
+			logger.info(String.format("%s.dir=%s", prefix, user.getDir()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
