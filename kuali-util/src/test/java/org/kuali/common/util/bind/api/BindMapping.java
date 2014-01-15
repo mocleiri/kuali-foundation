6c4f6cc41b240f12474dda9742342e4702d473c9
jcaddel <jcaddel@kuali.org>
2014-01-15 16:03:17 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30412 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/bind/api/BindMapping.java
@@ -0,0 +1,17 @@
+package org.kuali.common.util.bind.api;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({ FIELD })
+@Retention(RUNTIME)
+@Documented
+public @interface BindMapping {
+
+	String[] values() default {};
+
+}
