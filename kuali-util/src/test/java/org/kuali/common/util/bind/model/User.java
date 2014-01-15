f4abf42f9214ecb3a566eb2afb52b2149b33bd16
jcaddel <jcaddel@kuali.org>
2014-01-15 17:48:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30420 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/model/User.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/model/User.java
@@ -1,61 +1,14 @@
 package org.kuali.common.util.bind.model;
 
 import java.io.File;
-import java.util.TimeZone;
 
 import org.kuali.common.util.bind.api.Bound;
-import org.kuali.common.util.bind.api.BindMappings;
 
 @Bound
 public class User {
 
-	String country;
+	String name;
 	File home;
 	File dir;
-
-	@BindMappings("timezone")
-	TimeZone timeZone;
-
-	String name;
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public File getHome() {
-		return home;
-	}
-
-	public void setHome(File home) {
-		this.home = home;
-	}
-
-	public File getDir() {
-		return dir;
-	}
-
-	public void setDir(File dir) {
-		this.dir = dir;
-	}
-
-	public TimeZone getTimeZone() {
-		return timeZone;
-	}
-
-	public void setTimeZone(TimeZone timeZone) {
-		this.timeZone = timeZone;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 }
