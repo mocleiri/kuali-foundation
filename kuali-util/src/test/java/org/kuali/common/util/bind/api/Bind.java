21f5c8cc4387b80525149299f616f765bec87628
jcaddel <jcaddel@kuali.org>
2014-01-14 20:18:28 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30398 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/bind/api/Bind.java
@@ -0,0 +1,17 @@
+package org.kuali.common.util.bind.api;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({ TYPE })
+@Retention(RUNTIME)
+@Documented
+public @interface Bind {
+
+	String prefix() default "";
+
+}
