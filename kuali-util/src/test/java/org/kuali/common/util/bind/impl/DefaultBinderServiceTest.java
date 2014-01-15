04b4b378dac9a7e0012db81cfc5b1be0ea05f05a
jcaddel <jcaddel@kuali.org>
2014-01-15 14:38:39 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30410 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/impl/DefaultBinderServiceTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/impl/DefaultBinderServiceTest.java
@@ -1,17 +1,10 @@
 package org.kuali.common.util.bind.impl;
 
-import java.util.SortedSet;
-
-import org.apache.commons.io.FileUtils;
 import org.junit.Test;
-import org.kuali.common.util.Str;
 import org.kuali.common.util.bind.api.BinderService;
 import org.kuali.common.util.bind.model.User;
-import org.kuali.common.util.file.CanonicalFile;
 import org.kuali.common.util.log.LoggerUtils;
 import org.slf4j.Logger;
-
-import com.google.common.collect.Sets;
 
 public class DefaultBinderServiceTest {
 
@@ -20,20 +13,12 @@ public class DefaultBinderServiceTest {
 	@Test
 	public void test() {
 		try {
-			SortedSet<String> keys = Sets.newTreeSet(System.getProperties().stringPropertyNames());
-			StringBuilder sb = new StringBuilder();
-			for (String key : keys) {
-				String value = System.getProperty(key);
-				String flattened = Str.flatten(value, "CR", "LF");
-				sb.append(key + "=" + flattened + "\n");
-			}
-			FileUtils.write(new CanonicalFile("/tmp/system.txt"), sb.toString());
 			BinderService service = new DefaultBinderService();
 			User user = new User();
 			service.bind(user);
 			logger.info(String.format("name=%s", user.getName()));
 			logger.info(String.format("home=%s", user.getHome()));
-			logger.info(String.format("timeZone=%s", user.getTimezone()));
+			logger.info(String.format("timeZone=%s", user.getTimezone().getDisplayName()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- kuali-util/src/test/java/org/kuali/common/util/bind/impl/PrintSystem.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/impl/PrintSystem.java
@@ -6,14 +6,10 @@ import java.util.TreeSet;
 public class PrintSystem {
 
 	public static void main(String[] args) {
-		try {
-			SortedSet<String> keys = new TreeSet<String>(System.getProperties().stringPropertyNames());
-			for (String key : keys) {
-				String value = System.getProperty(key).replace("\r", "CR").replace("\n", "LF");
-				System.out.println(key + "=" + value);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		SortedSet<String> keys = new TreeSet<String>(System.getProperties().stringPropertyNames());
+		for (String key : keys) {
+			String value = System.getProperty(key).replace("\r", "CR").replace("\n", "LF");
+			System.out.println(key + "=" + value);
 		}
 	}
 
