f561f002d347b3a53e7db0bdf27189cb0e4d331c
jcaddel <jcaddel@kuali.org>
2014-01-15 19:08:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30426 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/bind/impl/FieldMapping.java
@@ -0,0 +1,65 @@
+package org.kuali.common.util.bind.impl;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.lang.reflect.Field;
+import java.util.SortedSet;
+
+import com.google.common.collect.Sets;
+
+public final class FieldMapping {
+
+	private final Field field;
+	private final SortedSet<String> mappings;
+
+	private FieldMapping(Builder builder) {
+		this.field = builder.field;
+		this.mappings = builder.mappings;
+	}
+
+	public static FieldMapping create(Field field) {
+		return builder(field).build();
+	}
+
+	public static Builder builder(Field field) {
+		return new Builder(field);
+	}
+
+	public static class Builder {
+
+		public Builder(Field field) {
+			this.field = field;
+		}
+
+		// Required
+		private final Field field;
+
+		// Optional
+		private SortedSet<String> mappings = Sets.newTreeSet();
+
+		public Builder mappings(SortedSet<String> mappings) {
+			this.mappings = mappings;
+			return this;
+		}
+
+		public FieldMapping build() {
+			FieldMapping instance = new FieldMapping(this);
+			validate(instance);
+			return instance;
+		}
+
+		private static void validate(FieldMapping instance) {
+			checkNotNull(instance.field, "'field' cannot be null");
+			checkNotNull(instance.mappings, "'mappings' cannot be null");
+		}
+	}
+
+	public Field getField() {
+		return field;
+	}
+
+	public SortedSet<String> getMappings() {
+		return mappings;
+	}
+
+}
