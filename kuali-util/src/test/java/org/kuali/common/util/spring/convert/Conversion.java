c06160db38a0937f5229e0aea550e6470198f322
jcaddel <jcaddel@kuali.org>
2014-01-17 18:09:55 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30540 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/convert/Conversion.java
@@ -0,0 +1,17 @@
+package org.kuali.common.util.spring.convert;
+
+import org.kuali.common.util.serviceloader.ServiceLoaderUtils;
+import org.springframework.core.convert.ConversionService;
+
+public class Conversion {
+
+	private static ConversionService instance;
+
+	public synchronized static ConversionService getDefaultConversionService() {
+		if (instance == null) {
+			instance = ServiceLoaderUtils.getFirst(ConversionService.class);
+		}
+		return instance;
+	}
+
+}
