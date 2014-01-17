95587441da471484253f3d72b02c0e1da6193abc
jcaddel <jcaddel@kuali.org>
2014-01-17 13:30:46 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30532 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/serviceloader/ServiceLoaderUtils.java
+++ kuali-util/src/test/java/org/kuali/common/util/serviceloader/ServiceLoaderUtils.java
@@ -3,16 +3,27 @@ package org.kuali.common.util.serviceloader;
 import static com.google.common.base.Preconditions.checkState;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.ServiceLoader;
+
+import com.google.common.collect.Lists;
 
 public class ServiceLoaderUtils {
 
 	public static <T> T getFirst(Class<T> type) {
+		List<T> list = getAll(type);
+		checkState(list.size() > 0, "ServiceLoader could not find service for type [%s]", type.getCanonicalName());
+		return list.get(0);
+	}
+
+	public static <T> List<T> getAll(Class<T> type) {
 		ServiceLoader<T> loader = ServiceLoader.load(type);
 		Iterator<T> itr = loader.iterator();
-		checkState(itr.hasNext(), "ServiceLoader could not find service for type [%s]", type.getCanonicalName());
-		T service = itr.next();
-		return service;
+		List<T> list = Lists.newArrayList();
+		while (itr.hasNext()) {
+			list.add(itr.next());
+		}
+		return list;
 	}
 
 }
