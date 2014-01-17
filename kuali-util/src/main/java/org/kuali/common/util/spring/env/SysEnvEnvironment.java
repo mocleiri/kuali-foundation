bafb97d2c61c92b975374429161fbd537a8b4ec6
jcaddel <jcaddel@kuali.org>
2014-01-17 12:25:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30520 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/env/SysEnvEnvironment.java
+++ kuali-util/src/main/java/org/kuali/common/util/spring/env/SysEnvEnvironment.java
@@ -2,15 +2,21 @@ package org.kuali.common.util.spring.env;
 
 import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertySource;
 
 /**
- * Automatically check both system properties and environment variables for values
+ * This environment contains both system properties and environment variables and automatically checks both whenever a property is requested.
+ * 
+ * <pre>
+ *   foo.barBaz -> env.FOO_BAR_BAZ
+ *                 FOO_BAR_BAZ
+ *                 env.foo_bar_baz
+ *                 foo_bar_baz
+ * </pre>
  */
 public class SysEnvEnvironment extends AbstractEnvironment {
 
 	public SysEnvEnvironment() {
-		PropertySource<?> source = new SysEnvPropertySource();
+		SysEnvPropertySource source = new SysEnvPropertySource();
 		MutablePropertySources sources = getPropertySources();
 		sources.addFirst(source);
 	}
