af8f30361d4ecfa4badada0be005871a85fcbb33
jcaddel <jcaddel@kuali.org>
2014-01-16 21:03:48 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30502 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/impl/EnvironmentPropertyValues.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/impl/EnvironmentPropertyValues.java
@@ -13,13 +13,13 @@ public final class EnvironmentPropertyValues<T> extends MutablePropertyValues {
 
 	private static final long serialVersionUID = 6864548343005636397L;
 
-	private final Class<T> target;
+	private final Class<T> type;
 	private final Environment env;
 	private final ImmutableMap<String, String> values;
 
 	private EnvironmentPropertyValues(Builder<T> builder) {
 		super(builder.values);
-		this.target = builder.target;
+		this.type = builder.type;
 		this.env = builder.env;
 		this.values = ImmutableMap.copyOf(builder.values);
 	}
@@ -28,34 +28,34 @@ public final class EnvironmentPropertyValues<T> extends MutablePropertyValues {
 		return builder(target, env).build();
 	}
 
-	public static <T> Builder<T> builder(Class<T> target, Environment env) {
-		return new Builder<T>(target, env);
+	public static <T> Builder<T> builder(Class<T> type, Environment env) {
+		return new Builder<T>(type, env);
 	}
 
 	public static class Builder<T> implements org.kuali.common.util.builder.Builder<EnvironmentPropertyValues<T>> {
 
 		// Required
-		private final Class<T> target;
+		private final Class<T> type;
 		private final Environment env;
 
 		// Filled in by the build() method
 		private Map<String, String> values;
 
 		public Builder(Class<T> target, Environment env) {
-			this.target = target;
+			this.type = target;
 			this.env = env;
 		}
 
 		@Override
 		public EnvironmentPropertyValues<T> build() {
-			this.values = BindUtils.getMap(target, env);
+			this.values = BindUtils.getMap(type, env);
 			EnvironmentPropertyValues<T> instance = new EnvironmentPropertyValues<T>(this);
 			validate(instance);
 			return instance;
 		}
 
 		private static <T> void validate(EnvironmentPropertyValues<T> instance) {
-			checkNotNull(instance.target, "'target' cannot be null");
+			checkNotNull(instance.type, "'type' cannot be null");
 			checkNotNull(instance.env, "'env' cannot be null");
 			checkNotNull(instance.values, "'values' cannot be null");
 		}
