5d9120af6d133b8ee053a442098955cc4744f653
jcaddel <jcaddel@kuali.org>
2014-01-17 18:08:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30539 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/convert/DefaultConversionService.java
@@ -0,0 +1,17 @@
+package org.kuali.common.util.spring.convert;
+
+import org.kohsuke.MetaInfServices;
+import org.kuali.common.util.spring.binder.BytesFormatAnnotationFormatterFactory;
+import org.kuali.common.util.spring.binder.TimeFormatAnnotationFormatterFactory;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+@MetaInfServices(ConversionService.class)
+public class DefaultConversionService extends DefaultFormattingConversionService {
+
+	public DefaultConversionService() {
+		addFormatterForFieldAnnotation(new BytesFormatAnnotationFormatterFactory());
+		addFormatterForFieldAnnotation(new TimeFormatAnnotationFormatterFactory());
+	}
+
+}
