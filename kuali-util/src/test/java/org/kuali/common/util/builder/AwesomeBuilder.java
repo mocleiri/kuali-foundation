be97255131446e09fafa1d106f5aa337539d8848
jcaddel <jcaddel@kuali.org>
2014-01-17 19:12:55 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30554 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/builder/AwesomeBuilder.java
+++ kuali-util/src/test/java/org/kuali/common/util/builder/AwesomeBuilder.java
@@ -1,20 +1,28 @@
 package org.kuali.common.util.builder;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
+import org.kuali.common.util.bind.api.BinderService;
+import org.kuali.common.util.bind.api.Binding;
 import org.kuali.common.util.validate.Validation;
+import org.springframework.validation.BindingResult;
 
 public abstract class AwesomeBuilder<T> implements Builder<T> {
 
 	protected abstract T getInstance();
 
-	private final Validator validator = Validation.getDefaultValidator();
+	protected Validator validator = Validation.getDefaultValidator();
+	protected BinderService binder = Binding.getDefaultBinderService();
 
 	@Override
 	public final T build() {
+		BindingResult result = binder.bind(this);
+		checkState(!result.hasErrors(), "Binding failed with %s errors", result.getAllErrors().size());
 		T instance = getInstance();
 		Set<ConstraintViolation<T>> violations = validate(instance);
 		Validation.check(violations);
@@ -23,6 +31,22 @@ public abstract class AwesomeBuilder<T> implements Builder<T> {
 
 	public Set<ConstraintViolation<T>> validate(T instance) {
 		return validator.validate(instance);
+	}
+
+	public Validator getValidator() {
+		return validator;
+	}
+
+	public void setValidator(Validator validator) {
+		this.validator = validator;
+	}
+
+	public BinderService getBinder() {
+		return binder;
+	}
+
+	public void setBinder(BinderService binder) {
+		this.binder = binder;
 	}
 
 }
