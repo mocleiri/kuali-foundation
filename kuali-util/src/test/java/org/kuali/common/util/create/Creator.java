bbcf1e1f1beaab10401c70f9180ed2f5dab0940a
jcaddel <jcaddel@kuali.org>
2014-01-18 20:44:29 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30596 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/create/Creator.java
+++ kuali-util/src/test/java/org/kuali/common/util/create/Creator.java
@@ -1,9 +1,9 @@
 package org.kuali.common.util.create;
 
-import org.kuali.common.util.build.test.Builder;
+import org.kuali.common.util.build.test.InstanceBuilder;
 
 public interface Creator {
 
-	<T> T create(Builder<T> builder);
+	<T> T create(InstanceBuilder<T> builder);
 
 }
