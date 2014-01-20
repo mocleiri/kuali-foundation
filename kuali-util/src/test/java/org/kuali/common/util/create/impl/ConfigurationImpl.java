f9f56b1423249171b4968d1918ce75daf1cd1834
jcaddel <jcaddel@kuali.org>
2014-01-20 18:24:21 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30629 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/create/impl/ConfigurationImpl.java
+++ kuali-util/src/test/java/org/kuali/common/util/create/impl/ConfigurationImpl.java
@@ -5,6 +5,7 @@ import javax.validation.Validator;
 import org.kuali.common.util.bind.api.BinderService;
 import org.kuali.common.util.create.CreatorFactory;
 import org.kuali.common.util.create.spi.ConfigurationState;
+import org.kuali.common.util.validate.Validation;
 
 public class ConfigurationImpl implements KualiCreationConfiguration, ConfigurationState {
 
@@ -15,7 +16,7 @@ public class ConfigurationImpl implements KualiCreationConfiguration, Configurat
 
 	@Override
 	public Validator getValidator() {
-		return null;
+		return Validation.getDefaultValidator();
 	}
 
 	@Override
