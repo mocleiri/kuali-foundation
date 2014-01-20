194bd1e057067d2cc9f3d651680c64c36e15dc41
jcaddel <jcaddel@kuali.org>
2014-01-20 18:27:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30630 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/create/impl/ConfigurationImpl.java
+++ kuali-util/src/test/java/org/kuali/common/util/create/impl/ConfigurationImpl.java
@@ -1,13 +1,19 @@
 package org.kuali.common.util.create.impl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import javax.validation.Validator;
 
 import org.kuali.common.util.bind.api.BinderService;
+import org.kuali.common.util.bind.api.Binding;
 import org.kuali.common.util.create.CreatorFactory;
 import org.kuali.common.util.create.spi.ConfigurationState;
 import org.kuali.common.util.validate.Validation;
 
-public class ConfigurationImpl implements KualiCreationConfiguration, ConfigurationState {
+public final class ConfigurationImpl implements KualiCreationConfiguration, ConfigurationState {
+
+	private final Validator validator;
+	private final BinderService binder;
 
 	@Override
 	public CreatorFactory buildCreatorFactory() {
@@ -16,12 +22,45 @@ public class ConfigurationImpl implements KualiCreationConfiguration, Configurat
 
 	@Override
 	public Validator getValidator() {
-		return Validation.getDefaultValidator();
+		return validator;
 	}
 
 	@Override
 	public BinderService getBinderService() {
-		return null;
+		return binder;
+	}
+
+	private ConfigurationImpl(Builder builder) {
+		this.validator = builder.validator;
+		this.binder = builder.binder;
+	}
+
+	public static class Builder implements org.kuali.common.util.build.Builder<ConfigurationImpl> {
+
+		private Validator validator = Validation.getDefaultValidator();
+		private BinderService binder = Binding.getDefaultBinderService();
+
+		public Builder validator(Validator validator) {
+			this.validator = validator;
+			return this;
+		}
+
+		public Builder binder(BinderService binder) {
+			this.binder = binder;
+			return this;
+		}
+
+		@Override
+		public ConfigurationImpl build() {
+			ConfigurationImpl instance = new ConfigurationImpl(this);
+			validate(instance);
+			return instance;
+		}
+
+		private static void validate(ConfigurationImpl instance) {
+			checkNotNull(instance.validator, "'validator' cannot be null");
+			checkNotNull(instance.binder, "'binder' cannot be null");
+		}
 	}
 
 }
