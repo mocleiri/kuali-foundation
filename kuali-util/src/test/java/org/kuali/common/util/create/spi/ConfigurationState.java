f53bd5b2292c7333e081273c06fddd3d49b0e3ce
jcaddel <jcaddel@kuali.org>
2014-01-20 11:47:18 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30624 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/create/spi/ConfigurationState.java
+++ kuali-util/src/test/java/org/kuali/common/util/create/spi/ConfigurationState.java
@@ -1,5 +1,13 @@
 package org.kuali.common.util.create.spi;
 
+import javax.validation.Validator;
+
+import org.kuali.common.util.bind.api.BinderService;
+
 public interface ConfigurationState {
+
+	Validator getValidator();
+
+	BinderService getBinderService();
 
 }
