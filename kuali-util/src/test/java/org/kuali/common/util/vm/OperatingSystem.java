5dca3352f68a8765392ade8023213230fb83dfbb
jcaddel <jcaddel@kuali.org>
2014-01-21 18:58:08 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30656 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/vm/OperatingSystem.java
@@ -0,0 +1,63 @@
+package org.kuali.common.util.vm;
+
+import org.kuali.common.util.bind.api.BindAlias;
+import org.kuali.common.util.bind.api.Bound;
+import org.kuali.common.util.build.AwesomeBuilder;
+import org.kuali.common.util.validate.IdiotProofImmutable;
+
+@IdiotProofImmutable
+public class OperatingSystem {
+
+	private final String name;
+	private final String architecture;
+	private final String version;
+
+	private OperatingSystem(Builder builder) {
+		this.name = builder.name;
+		this.architecture = builder.architecture;
+		this.version = builder.version;
+	}
+
+	@Bound("os")
+	public static class Builder extends AwesomeBuilder<OperatingSystem> {
+
+		private String name;
+		@BindAlias("arch")
+		private String architecture;
+		private String version;
+
+		public Builder name(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder architecture(String architecture) {
+			this.architecture = architecture;
+			return this;
+		}
+
+		public Builder version(String version) {
+			this.version = version;
+			return this;
+		}
+
+		@Override
+		public OperatingSystem getInstance() {
+			return new OperatingSystem(this);
+		}
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getArchitecture() {
+		return architecture;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+}
