8d81f94fa537548e2d5a0505d67713c1c0ecb3b0
jcaddel <jcaddel@kuali.org>
2014-01-21 16:55:56 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30637 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/create/spring/CreatorFactoryConfig.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common.util.create.spring;
-
-import org.kuali.common.util.create.CreatorFactory;
-
-public interface CreatorFactoryConfig {
-
-	CreatorFactory creatorFactory();
-
-}
