feb6a16a38b39989126e15500de56c95ba519aee
jcaddel <jcaddel@kuali.org>
2014-01-22 10:52:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30723 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/system/spring/SystemConfig.java
@@ -0,0 +1,9 @@
+package org.kuali.common.util.system.spring;
+
+import org.kuali.common.util.system.SystemProperties;
+
+public interface SystemConfig {
+
+	SystemProperties systemProperties();
+
+}
