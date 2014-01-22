a0236ab52e7b4a07e2d69d6db9feada7bf4dfd1e
jcaddel <jcaddel@kuali.org>
2014-01-21 20:27:28 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30679 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/api/Bound.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/api/Bound.java
@@ -1,5 +1,6 @@
 package org.kuali.common.util.bind.api;
 
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -7,7 +8,7 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target({ TYPE })
+@Target({ TYPE, FIELD })
 @Retention(RUNTIME)
 @Documented
 public @interface Bound {
