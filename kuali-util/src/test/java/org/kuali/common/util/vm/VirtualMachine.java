28996bac94159cea7ade62906dec318da2d806eb
jcaddel <jcaddel@kuali.org>
2014-01-21 23:21:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30707 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/vm/VirtualMachine.java
+++ kuali-util/src/test/java/org/kuali/common/util/vm/VirtualMachine.java
@@ -7,17 +7,41 @@ import org.kuali.common.util.validate.IdiotProofImmutable;
 public final class VirtualMachine {
 
 	private final Specification specification;
+	private final String name;
+	private final String vendor;
+	private final String version;
 
 	private VirtualMachine(Builder builder) {
 		this.specification = builder.specification;
+		this.name = builder.name;
+		this.vendor = builder.vendor;
+		this.version = builder.version;
 	}
 
 	public static class Builder extends AwesomeBuilder<VirtualMachine> {
 
 		private Specification specification;
+		private String name;
+		private String vendor;
+		private String version;
 
 		public Builder specification(Specification specification) {
 			this.specification = specification;
+			return this;
+		}
+
+		public Builder name(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder vendor(String vendor) {
+			this.vendor = vendor;
+			return this;
+		}
+
+		public Builder version(String version) {
+			this.version = version;
 			return this;
 		}
 
@@ -34,10 +58,46 @@ public final class VirtualMachine {
 			this.specification = specification;
 		}
 
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getVendor() {
+			return vendor;
+		}
+
+		public void setVendor(String vendor) {
+			this.vendor = vendor;
+		}
+
+		public String getVersion() {
+			return version;
+		}
+
+		public void setVersion(String version) {
+			this.version = version;
+		}
+
 	}
 
 	public Specification getSpecification() {
 		return specification;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public String getVersion() {
+		return version;
 	}
 
 }
