756198e3f93366df3d3f90f35966ef396bda8649
jcaddel <jcaddel@kuali.org>
2014-01-22 10:54:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30725 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/spring/DefaultSystemConfig.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/spring/DefaultSystemConfig.java
@@ -1,12 +1,20 @@
 package org.kuali.common.util.system.spring;
 
 import org.kuali.common.util.system.SystemProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
+@Configuration
 public class DefaultSystemConfig implements SystemConfig {
 
+	@Autowired
+	Environment env;
+
 	@Override
+	@Bean
 	public SystemProperties systemProperties() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
