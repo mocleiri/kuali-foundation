be2517865d5c581272ff6069f0f7682a64067c30
jcaddel <jcaddel@kuali.org>
2014-01-21 19:15:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30662 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/vm/VirtualMachine.java
+++ kuali-util/src/test/java/org/kuali/common/util/vm/VirtualMachine.java
@@ -1,6 +1,11 @@
 package org.kuali.common.util.vm;
 
+import java.io.File;
+
+import org.kuali.common.util.bind.api.BindAlias;
+import org.kuali.common.util.bind.api.Bound;
 import org.kuali.common.util.build.AwesomeBuilder;
+import org.kuali.common.util.spring.binder.CanonicalFileFormat;
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
 @IdiotProofImmutable
@@ -8,10 +13,12 @@ public final class VirtualMachine {
 
 	private final User user;
 	private final OperatingSystem operatingSystem;
+	private final File tempDirectory;
 
 	private VirtualMachine(Builder builder) {
 		this.user = builder.user;
 		this.operatingSystem = builder.operatingSystem;
+		this.tempDirectory = builder.tempDirectory;
 	}
 
 	public static VirtualMachine build() {
@@ -22,14 +29,42 @@ public final class VirtualMachine {
 		return new Builder();
 	}
 
-	private static class Builder extends AwesomeBuilder<VirtualMachine> {
+	@Bound(prefix = false)
+	public static class Builder extends AwesomeBuilder<VirtualMachine> {
 
 		private User user = User.create();
 		private OperatingSystem operatingSystem = OperatingSystem.create();
+		@BindAlias("java.io.tmpdir")
+		@CanonicalFileFormat
+		private File tempDirectory;
 
 		@Override
 		public VirtualMachine getInstance() {
 			return new VirtualMachine(this);
+		}
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+		public OperatingSystem getOperatingSystem() {
+			return operatingSystem;
+		}
+
+		public void setOperatingSystem(OperatingSystem operatingSystem) {
+			this.operatingSystem = operatingSystem;
+		}
+
+		public File getTempDirectory() {
+			return tempDirectory;
+		}
+
+		public void setTempDirectory(File tempDirectory) {
+			this.tempDirectory = tempDirectory;
 		}
 
 	}
@@ -40,6 +75,10 @@ public final class VirtualMachine {
 
 	public User getUser() {
 		return user;
+	}
+
+	public File getTempDirectory() {
+		return tempDirectory;
 	}
 
 }
