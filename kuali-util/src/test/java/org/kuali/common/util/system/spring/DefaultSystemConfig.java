6d0efa2a9a36e3792ac93d010998a141c214e960
jcaddel <jcaddel@kuali.org>
2014-01-22 10:53:48 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30724 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/system/spring/DefaultSystemConfig.java
@@ -0,0 +1,13 @@
+package org.kuali.common.util.system.spring;
+
+import org.kuali.common.util.system.SystemProperties;
+
+public class DefaultSystemConfig implements SystemConfig {
+
+	@Override
+	public SystemProperties systemProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
