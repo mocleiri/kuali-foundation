8aa0459f25f12b5069741061164a3171f7e9291d
jcaddel <jcaddel@kuali.org>
2014-01-21 23:15:49 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30705 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/vm/Java.java
+++ kuali-util/src/test/java/org/kuali/common/util/vm/Java.java
@@ -1,8 +1,63 @@
 package org.kuali.common.util.vm;
 
+import org.kuali.common.util.build.AwesomeBuilder;
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
 @IdiotProofImmutable
 public final class Java {
+
+	private final RuntimeEnvironment runtimeEnvironment;
+	private final VirtualMachine virtualMachine;
+
+	private Java(Builder builder) {
+		this.runtimeEnvironment = builder.runtimeEnvironment;
+		this.virtualMachine = builder.virtualMachine;
+	}
+
+	public static class Builder extends AwesomeBuilder<Java> {
+
+		private RuntimeEnvironment runtimeEnvironment;
+		private VirtualMachine virtualMachine;
+
+		public Builder runtimeEnvironment(RuntimeEnvironment runtimeEnvironment) {
+			this.runtimeEnvironment = runtimeEnvironment;
+			return this;
+		}
+
+		public Builder virtualMachine(VirtualMachine virtualMachine) {
+			this.virtualMachine = virtualMachine;
+			return this;
+		}
+
+		@Override
+		public Java getInstance() {
+			return new Java(this);
+		}
+
+		public RuntimeEnvironment getRuntimeEnvironment() {
+			return runtimeEnvironment;
+		}
+
+		public void setRuntimeEnvironment(RuntimeEnvironment runtimeEnvironment) {
+			this.runtimeEnvironment = runtimeEnvironment;
+		}
+
+		public VirtualMachine getVirtualMachine() {
+			return virtualMachine;
+		}
+
+		public void setVirtualMachine(VirtualMachine virtualMachine) {
+			this.virtualMachine = virtualMachine;
+		}
+
+	}
+
+	public RuntimeEnvironment getRuntimeEnvironment() {
+		return runtimeEnvironment;
+	}
+
+	public VirtualMachine getVirtualMachine() {
+		return virtualMachine;
+	}
 
 }
