7aec644cd886e28678cab6a9be1cac6bdb182894
jcaddel <jcaddel@kuali.org>
2014-01-22 19:15:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30758 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/impl/BindUtilsTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/impl/BindUtilsTest.java
@@ -1,13 +1,18 @@
 package org.kuali.common.util.bind.impl;
 
-import static java.lang.String.format;
+import static com.google.common.base.Preconditions.checkState;
 
+import java.util.Set;
+
+import org.codehaus.plexus.util.StringUtils;
 import org.junit.Test;
+import org.kuali.common.util.bind.api.Bind;
 import org.kuali.common.util.log.LoggerUtils;
-import org.kuali.common.util.system.OperatingSystem;
 import org.kuali.common.util.system.SystemProperties;
-import org.kuali.common.util.system.User;
 import org.slf4j.Logger;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Sets;
 
 public class BindUtilsTest {
 
@@ -16,18 +21,52 @@ public class BindUtilsTest {
 	@Test
 	public void test() {
 		try {
-			SystemProperties vm = SystemProperties.builder().build();
-			User user = vm.getUser();
-			OperatingSystem os = vm.getOperatingSystem();
-			logger.info(format("user.name=[%s]", user.getName()));
-			logger.info(format("user.home=[%s]", user.getHome()));
-			logger.info(format("user.dir=[%s]", user.getDir()));
-			logger.info(format("os.name=[%s]", os.getName()));
-			logger.info(format("os.arch=[%s]", os.getArchitecture()));
-			logger.info(format("os.version=[%s]", os.getVersion()));
+			Set<String> keys = getKeys(SystemProperties.class);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public Set<String> getKeys(Class<?> type) {
+		return getKeys(Optional.<String> absent(), type);
+	}
+
+	public Set<String> getKeys(Optional<String> prefix, Class<?> type) {
+		if (!type.isAnnotationPresent(Bind.class)) {
+			return Sets.newHashSet();
+		}
+
+		return null;
+	}
+
+	protected Optional<String> getPrefix(Optional<String> prefix, Class<?> type, Bind bind) {
+		// Explicit prefix. This overrides everything
+		if (prefix.isPresent()) {
+			return prefix;
+		}
+
+		// They have specifically said, "don't use a prefix"
+		if (bind.noPrefix()) {
+			return Optional.absent();
+		}
+
+		// An explicit prefix class has been configured on the annotation. This overrides value()
+		if (!bind.prefix().equals(void.class)) {
+			return Optional.of(getPrefix(bind.prefix()));
+		}
+
+		// An explicit prefix value has been configured on the annotation.
+		if (!bind.value().equals("")) {
+			checkState(!StringUtils.isBlank(bind.value()), "[%s.value()] cannot be blank", bind.getClass().getCanonicalName());
+			return Optional.of(bind.value());
+		}
+
+		// Use a prefix derived from the class type
+		return Optional.of(getPrefix(type));
+	}
+
+	protected String getPrefix(Class<?> type) {
+		return StringUtils.uncapitalise(type.getSimpleName());
 	}
 
 }
