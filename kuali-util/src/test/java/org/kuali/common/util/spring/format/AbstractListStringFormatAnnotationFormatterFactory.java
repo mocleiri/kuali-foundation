a9092e93b91ab9561cd0c82418fbc18e50ca4382
jcaddel <jcaddel@kuali.org>
2014-01-23 16:49:29 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30830 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/format/AbstractListStringFormatAnnotationFormatterFactory.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/format/AbstractListStringFormatAnnotationFormatterFactory.java
@@ -1,5 +1,7 @@
 package org.kuali.common.util.spring.format;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.Set;
@@ -7,6 +9,7 @@ import java.util.Set;
 import org.springframework.format.AnnotationFormatterFactory;
 import org.springframework.format.Formatter;
 
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableSet;
 
 public abstract class AbstractListStringFormatAnnotationFormatterFactory<A extends Annotation> implements AnnotationFormatterFactory<A> {
@@ -20,7 +23,9 @@ public abstract class AbstractListStringFormatAnnotationFormatterFactory<A exten
 	}
 
 	protected Formatter<List<String>> getFormatter(char separator, boolean trim, boolean omitEmpty, String magicEmptyString) {
-		return ListStringFormatter.builder().separator(separator).trim(trim).omitEmpty(omitEmpty).magicEmptyString(magicEmptyString).build();
+		checkNotNull(magicEmptyString, "'magicEmptyString' cannot be null");
+		Optional<String> mes = magicEmptyString.equals("") ? Optional.<String> absent() : Optional.of(magicEmptyString);
+		return ListStringFormatter.builder().separator(separator).trim(trim).omitEmpty(omitEmpty).magicEmptyString(mes).build();
 	}
 
 }
