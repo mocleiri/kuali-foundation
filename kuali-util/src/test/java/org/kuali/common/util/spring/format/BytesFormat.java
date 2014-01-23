b37e58a3e6369892f79c40300e3183b996f24852
jcaddel <jcaddel@kuali.org>
2014-01-23 12:35:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30804 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/format/BytesFormat.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/format/BytesFormat.java
@@ -1,12 +1,15 @@
 package org.kuali.common.util.spring.format;
 
-import java.lang.annotation.ElementType;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
-@Retention(RetentionPolicy.RUNTIME)
+@Target({ METHOD, FIELD, PARAMETER })
+@Retention(RUNTIME)
 public @interface BytesFormat {
 
 	boolean printDecimalDigits() default true;
