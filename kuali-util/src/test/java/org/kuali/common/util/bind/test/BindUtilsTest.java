1164813006e1c2b265d6ffaa532fa502471a2b5e
jcaddel <jcaddel@kuali.org>
2014-01-22 20:30:21 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30769 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/test/BindUtilsTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/test/BindUtilsTest.java
@@ -2,16 +2,23 @@ package org.kuali.common.util.bind.test;
 
 import static com.google.common.base.Preconditions.checkState;
 
+import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
 
 import org.codehaus.plexus.util.StringUtils;
 import org.junit.Test;
+import org.kuali.common.util.ReflectionUtils;
+import org.kuali.common.util.bind.api.Alias;
 import org.kuali.common.util.bind.api.Bind;
 import org.kuali.common.util.log.LoggerUtils;
 import org.kuali.common.util.system.SystemProperties;
 import org.slf4j.Logger;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 public class BindUtilsTest {
@@ -22,6 +29,9 @@ public class BindUtilsTest {
 	public void test() {
 		try {
 			Set<String> keys = getKeys(SystemProperties.class);
+			for (String key : keys) {
+				logger.info("key={}", key);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -35,9 +45,31 @@ public class BindUtilsTest {
 		if (!type.isAnnotationPresent(Bind.class)) {
 			return Sets.newHashSet();
 		}
+		SortedSet<String> keys = Sets.newTreeSet();
 		Optional<String> actualPrefix = getPrefix(prefix, type, type.getAnnotation(Bind.class));
+		Set<Field> fields = ReflectionUtils.getAllFields(type);
+		for (Field field : fields) {
+			List<String> aliases = getAliases(field);
+			List<String> prefixed = Lists.transform(aliases, null);
+			String fieldName = field.getName();
+			if (actualPrefix.isPresent()) {
+				String key = actualPrefix.get() + "." + fieldName;
+				keys.add(key);
+			} else {
+				String key = fieldName;
+				keys.add(key);
+			}
+		}
+		return keys;
+	}
 
-		return null;
+	protected List<String> getAliases(Field field) {
+		Optional<Alias> alias = Optional.of(field.getAnnotation(Alias.class));
+		if (!alias.isPresent()) {
+			return ImmutableList.of();
+		} else {
+			return ImmutableList.copyOf(alias.get().value());
+		}
 	}
 
 	protected Optional<String> getPrefix(Optional<String> prefix, Class<?> type, Bind bind) {
---
+++ kuali-util/src/test/java/org/kuali/common/util/bind/test/PrefixFunction.java
@@ -0,0 +1,7 @@
+package org.kuali.common.util.bind.test;
+
+import com.google.common.base.Function;
+
+public class PrefixFunction implements Function<String, String> {
+
+}
