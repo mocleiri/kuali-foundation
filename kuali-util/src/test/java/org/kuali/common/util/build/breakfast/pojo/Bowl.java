b50abcdd093ae4f9140a006601243ecc3ff13afe
jcaddel <jcaddel@kuali.org>
2014-01-22 19:46:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30763 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/build/breakfast/pojo/Bowl.java
+++ kuali-util/src/test/java/org/kuali/common/util/build/breakfast/pojo/Bowl.java
@@ -4,15 +4,19 @@ public class Bowl {
 
 	Cereal cereal;
 	Milk milk;
+
 	public Cereal getCereal() {
 		return cereal;
 	}
+
 	public void setCereal(Cereal cereal) {
 		this.cereal = cereal;
 	}
+
 	public Milk getMilk() {
 		return milk;
 	}
+
 	public void setMilk(Milk milk) {
 		this.milk = milk;
 	}
