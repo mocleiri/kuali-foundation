2c4cc4a56ab9ee0bee22f5eb7ba41f12289a21be
jcaddel <jcaddel@kuali.org>
2014-01-23 16:21:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30822 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/RuntimeEnvironment.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/RuntimeEnvironment.java
@@ -1,7 +1,7 @@
 package org.kuali.common.util.system;
 
-import org.kuali.common.util.bind.api.BindingAlias;
 import org.kuali.common.util.bind.api.Bind;
+import org.kuali.common.util.bind.api.BindingAlias;
 import org.kuali.common.util.build.AwesomeBuilder;
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
@@ -10,8 +10,10 @@ public class RuntimeEnvironment {
 
 	private final String vendor;
 	private final String version;
+
 	@BindingAlias("vendor.url")
 	private final String url;
+
 	@Bind
 	private final Specification specification;
 
