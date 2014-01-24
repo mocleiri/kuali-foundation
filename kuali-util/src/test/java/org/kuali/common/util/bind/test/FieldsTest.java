1fc278d7edb4ba0a4663b8bc501f49e52b42edf9
jcaddel <jcaddel@kuali.org>
2014-01-23 22:10:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30864 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/test/FieldsTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/test/FieldsTest.java
@@ -1,12 +1,10 @@
 package org.kuali.common.util.bind.test;
 
 import java.io.File;
-import java.util.List;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.kuali.common.util.system.SystemProperties;
 
@@ -18,14 +16,7 @@ public class FieldsTest {
 			DefaultMutableTreeNode tree = Fields.assemble(SystemProperties.class);
 			String html = Trees.html(tree);
 			FileUtils.write(new File("/tmp/fields.htm"), html);
-			List<DefaultMutableTreeNode> nodes = Trees.depthFirst(tree);
-			for (DefaultMutableTreeNode node : nodes) {
-				int level = node.getLevel();
-				String prefix = StringUtils.repeat(" ", level);
-				Object object = node.getUserObject();
-				// System.out.println(prefix + object.toString());
-			}
-			System.out.println(nodes.size());
+			System.out.println(Trees.breadthFirst(tree).size());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
