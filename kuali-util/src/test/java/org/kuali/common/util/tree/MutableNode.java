4dfa71267b7939184c6929d12509675d83828fbf
jcaddel <jcaddel@kuali.org>
2014-01-25 14:12:29 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30922 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/tree/MutableNode.java
+++ kuali-util/src/test/java/org/kuali/common/util/tree/MutableNode.java
@@ -1,30 +1,70 @@
 package org.kuali.common.util.tree;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.List;
 
+import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 
 public class MutableNode<T> {
 
-	protected MutableNode<T> parent;
-	protected List<MutableNode<T>> children = Lists.newArrayList();
-	protected T element;
+	MutableNode<T> parent;
+	List<MutableNode<T>> children = Lists.newArrayList();
+	T userObject;
 
-	public MutableNode(T element) {
-		this.parent = null;
-		this.element = element;
+	public MutableNode(T userObject) {
+		this.userObject = userObject;
 	}
 
 	public void add(MutableNode<T> node) {
 		children.add(node);
 	}
 
-	public void setElement(T element) {
-		this.element = element;
+	/**
+	 * Returns the number of levels above this node -- the distance from the root to this node. If this node is the root, returns 0.
+	 * 
+	 * @see #getDepth
+	 * @return the number of levels above this node
+	 */
+	public int getLevel() {
+		int levels = 0;
+		MutableNode<T> ancestor = this;
+		while ((ancestor = ancestor.getParent()) != null) {
+			levels++;
+		}
+		return levels;
 	}
 
-	public T getElement() {
-		return element;
+	public List<MutableNode<T>> getPath() {
+		MutableNode<T> ancestor = this;
+		List<MutableNode<T>> list = Lists.newArrayList();
+		while ((ancestor = ancestor.getParent()) != null) {
+			list.add(ancestor);
+		}
+		return Lists.reverse(list);
+	}
+
+	public List<T> getUserObjectPath() {
+		return Lists.transform(getPath(), new UserObjectFunction());
+	}
+
+	private class UserObjectFunction implements Function<MutableNode<T>, T> {
+
+		@Override
+		public T apply(MutableNode<T> node) {
+			checkNotNull(node, "'node' cannot be null");
+			return node.getUserObject();
+		}
+
+	}
+
+	public void setUserObject(T element) {
+		this.userObject = element;
+	}
+
+	public T getUserObject() {
+		return userObject;
 	}
 
 	public MutableNode<T> getParent() {
