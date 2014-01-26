370bba92b1db0e9f5c3c5528ceb17c15fe3be229
jcaddel <jcaddel@kuali.org>
2014-01-25 22:35:09 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30951 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/tree/AbstractNode.java
+++ kuali-util/src/test/java/org/kuali/common/util/tree/AbstractNode.java
@@ -8,11 +8,17 @@ import com.google.common.collect.Lists;
 
 public abstract class AbstractNode<T> implements Node<T> {
 
+	/**
+	 * Return true if this is the root node (ie it has no parent)
+	 */
 	@Override
 	public boolean isRoot() {
 		return !getParent().isPresent();
 	}
 
+	/**
+	 * Return true if this is a leaf node (ie it has no children)
+	 */
 	@Override
 	public boolean isLeaf() {
 		return getChildren().isEmpty();
@@ -20,9 +26,6 @@ public abstract class AbstractNode<T> implements Node<T> {
 
 	/**
 	 * Returns the number of levels above this node -- the distance from the root to this node. If this node is the root, returns 0.
-	 * 
-	 * @see #getDepth
-	 * @return the number of levels above this node
 	 */
 	@Override
 	public int getLevel() {
@@ -50,16 +53,25 @@ public abstract class AbstractNode<T> implements Node<T> {
 		return Lists.reverse(list);
 	}
 
+	/**
+	 * Returns the user objects in the path from the root, to get to this node. The last element is the user object from this node
+	 */
 	@Override
 	public List<T> getUserObjectPath() {
 		return Lists.transform(getPath(), new UserObjectFunction<T>());
 	}
 
+	/**
+	 * Return true if this node is a parent of {@code child}
+	 */
 	public boolean isChild(AbstractNode<T> child) {
 		checkNotNull(child, "'child' cannot be null");
 		return child.getParent().isPresent() && child.getParent().get() == this;
 	}
 
+	/**
+	 * Return true if {@code node} is this node, a parent of this node, a grandparent of this node, etc
+	 */
 	public boolean isAncestor(AbstractNode<T> node) {
 		checkNotNull(node, "'node' cannot be null");
 		return getPath().contains(node);
