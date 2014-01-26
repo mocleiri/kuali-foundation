092627a689670b23ba421408c1b58374385b58ad
jcaddel <jcaddel@kuali.org>
2014-01-25 22:24:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@30947 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/tree/Trees.java
+++ kuali-util/src/test/java/org/kuali/common/util/tree/Trees.java
@@ -1,27 +1,21 @@
 package org.kuali.common.util.tree;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.util.Enumeration;
 import java.util.List;
-
-import javax.swing.tree.DefaultMutableTreeNode;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Function;
-import com.google.common.collect.Lists;
 
 public class Trees {
 
-	public static <T> String html2(String title, List<? extends Node<T>> nodes, Function<Node<T>, String> converter) {
+	public static <T> String html(String title, List<? extends Node<T>> nodes, Function<Node<T>, String> converter) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("<table border=\"1\">\n");
 		sb.append(" <th>" + title + "</th>\n");
 		sb.append(" <tr>\n");
 		sb.append("  <td>\n");
 		for (Node<T> node : nodes) {
-			sb.append(html2(node, 3, converter));
+			sb.append(html(node, 3, converter));
 		}
 		sb.append("  </td>\n");
 		sb.append(" </tr>\n");
@@ -29,7 +23,7 @@ public class Trees {
 		return sb.toString();
 	}
 
-	public static <T> String html2(Node<T> node, int indent, Function<Node<T>, String> converter) {
+	public static <T> String html(Node<T> node, int indent, Function<Node<T>, String> converter) {
 		StringBuilder sb = new StringBuilder();
 		String prefix = StringUtils.repeat(" ", indent);
 		sb.append(prefix + "<table border=\"1\">\n");
@@ -39,40 +33,6 @@ public class Trees {
 		if (!children.isEmpty()) {
 			sb.append(prefix + "  <td>\n");
 			for (Node<T> child : children) {
-				sb.append(html2(child, indent + 3, converter));
-			}
-			sb.append(prefix + "  </td>\n");
-		}
-		sb.append(prefix + " </tr>\n");
-		sb.append(prefix + "</table>\n");
-		return sb.toString();
-	}
-
-	public static String html(String title, List<DefaultMutableTreeNode> nodes, Function<DefaultMutableTreeNode, String> converter) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("<table border=\"1\">\n");
-		sb.append(" <th>" + title + "</th>\n");
-		sb.append(" <tr>\n");
-		sb.append("  <td>\n");
-		for (DefaultMutableTreeNode node : nodes) {
-			sb.append(html(node, 3, converter));
-		}
-		sb.append("  </td>\n");
-		sb.append(" </tr>\n");
-		sb.append("</table>\n");
-		return sb.toString();
-	}
-
-	public static String html(DefaultMutableTreeNode node, int indent, Function<DefaultMutableTreeNode, String> converter) {
-		StringBuilder sb = new StringBuilder();
-		String prefix = StringUtils.repeat(" ", indent);
-		sb.append(prefix + "<table border=\"1\">\n");
-		sb.append(prefix + " <tr>\n");
-		sb.append(prefix + "  <td>" + converter.apply(node) + "</td>\n");
-		List<DefaultMutableTreeNode> children = children(node);
-		if (!children.isEmpty()) {
-			sb.append(prefix + "  <td>\n");
-			for (DefaultMutableTreeNode child : children) {
 				sb.append(html(child, indent + 3, converter));
 			}
 			sb.append(prefix + "  </td>\n");
@@ -82,48 +42,4 @@ public class Trees {
 		return sb.toString();
 	}
 
-	@SuppressWarnings("unchecked")
-	public static List<DefaultMutableTreeNode> children(DefaultMutableTreeNode node) {
-		checkNotNull(node, "'node' cannot be null");
-		return asList(node.children());
-	}
-
-	@SuppressWarnings("unchecked")
-	public static List<DefaultMutableTreeNode> breadthFirst(DefaultMutableTreeNode node) {
-		checkNotNull(node, "'node' cannot be null");
-		return asList(node.breadthFirstEnumeration());
-	}
-
-	@SuppressWarnings("unchecked")
-	public static List<DefaultMutableTreeNode> depthFirst(DefaultMutableTreeNode node) {
-		checkNotNull(node, "'node' cannot be null");
-		return asList(node.depthFirstEnumeration());
-	}
-
-	public static List<DefaultMutableTreeNode> asList(Enumeration<DefaultMutableTreeNode> enumeration) {
-		checkNotNull(enumeration, "'enumeration' cannot be null");
-		List<DefaultMutableTreeNode> nodes = Lists.newArrayList();
-		while (enumeration.hasMoreElements()) {
-			nodes.add(enumeration.nextElement());
-		}
-		return nodes;
-	}
-
-	public static List<DefaultMutableTreeNode> getLeaves(List<DefaultMutableTreeNode> nodes) {
-		List<DefaultMutableTreeNode> leaves = Lists.newArrayList();
-		for (DefaultMutableTreeNode node : nodes) {
-			if (node.isLeaf()) {
-				leaves.add(node);
-			}
-		}
-		return leaves;
-	}
-
-	public static List<DefaultMutableTreeNode> combine(List<DefaultMutableTreeNode> nodes) {
-		List<DefaultMutableTreeNode> combined = Lists.newArrayList();
-		for (DefaultMutableTreeNode node : nodes) {
-			combined.addAll(breadthFirst(node));
-		}
-		return combined;
-	}
 }
