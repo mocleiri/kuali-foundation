8e814ecf9a170329c3a625963cecd756b8044baf
jcaddel <jcaddel@kuali.org>
2014-01-28 18:48:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31045 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/beans/RecordComparator.java
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/beans/RecordComparator.java
@@ -17,47 +17,47 @@ package org.kuali.maven.plugins.dnsme.beans;
 
 import java.util.Comparator;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class RecordComparator implements Comparator<Record> {
 
-    @Override
-    public int compare(Record record1, Record record2) {
-        String name1 = record1.getName();
-        String name2 = record2.getName();
+	@Override
+	public int compare(Record record1, Record record2) {
+		String name1 = record1.getName();
+		String name2 = record2.getName();
 
-        Integer nullCompare = getNullCompare(name1, name2);
-        if (nullCompare != null) {
-            return nullCompare;
-        }
+		Integer nullCompare = getNullCompare(name1, name2);
+		if (nullCompare != null) {
+			return nullCompare;
+		}
 
-        String[] tokens1 = StringUtils.split(name1, ".");
-        String[] tokens2 = StringUtils.split(name2, ".");
+		String[] tokens1 = StringUtils.split(name1, ".");
+		String[] tokens2 = StringUtils.split(name2, ".");
 
-        String s1 = getReversedString(tokens1);
-        String s2 = getReversedString(tokens2);
+		String s1 = getReversedString(tokens1);
+		String s2 = getReversedString(tokens2);
 
-        return s1.compareTo(s2);
-    }
+		return s1.compareTo(s2);
+	}
 
-    protected String getReversedString(String[] tokens) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = tokens.length - 1; i >= 0; i--) {
-            sb.append(tokens[i]);
-        }
-        return sb.toString();
-    }
+	protected String getReversedString(String[] tokens) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = tokens.length - 1; i >= 0; i--) {
+			sb.append(tokens[i]);
+		}
+		return sb.toString();
+	}
 
-    protected Integer getNullCompare(String s1, String s2) {
-        if (s1 == null && s2 == null) {
-            return 0;
-        } else if (s1 != null && s2 == null) {
-            return 1;
-        } else if (s1 == null && s2 != null) {
-            return -1;
-        } else {
-            return null;
-        }
-    }
+	protected Integer getNullCompare(String s1, String s2) {
+		if (s1 == null && s2 == null) {
+			return 0;
+		} else if (s1 != null && s2 == null) {
+			return 1;
+		} else if (s1 == null && s2 != null) {
+			return -1;
+		} else {
+			return null;
+		}
+	}
 
 }
