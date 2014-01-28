2938a9d8796b26d0f04e99687b9863ad00c393c5
jcaddel <jcaddel@kuali.org>
2014-01-28 16:17:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31034 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/test/KeyAssembler.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/test/KeyAssembler.java
@@ -2,112 +2,33 @@ package org.kuali.common.util.bind.test;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Set;
 
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
-
-import org.kuali.common.util.bind.api.BindingAlias;
-
-import com.google.common.base.Joiner;
-import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
+import org.kuali.common.util.bind.model.BoundTypeDescriptor;
 
 public final class KeyAssembler implements Assembler<Set<String>> {
 
-	private final Class<?> type;
-	private final List<DefaultMutableTreeNode> nodes;
-
-	private KeyAssembler(Builder builder) {
-		this.type = builder.type;
-		this.nodes = builder.nodes;
-	}
+	private final BoundTypeDescriptor descriptor;
 
 	@Override
 	public Set<String> assemble() {
-		/*
-		Optional<String> prefix = Prefixes.get(type);
-		List<DefaultMutableTreeNode> combined = Trees.combine(nodes);
-		List<DefaultMutableTreeNode> leaves = Trees.getLeaves(combined);
-		SortedSet<String> keys = Sets.newTreeSet();
-		for (DefaultMutableTreeNode leaf : leaves) {
-			List<Field> fields = getFieldPath(leaf);
-			keys.addAll(getKeys(prefix, fields));
-		}
-		return keys;
-		*/
 		return null;
 	}
 
-	protected List<String> getKeys(Optional<String> prefix, List<Field> fields) {
-		List<String> tokens = Lists.newArrayList();
-		if (prefix.isPresent()) {
-			tokens.add(prefix.get());
-		}
-		for (int i = 0; i < fields.size() - 1; i++) {
-			Optional<String> fieldPrefix = Prefixes.get(fields.get(i));
-			if (fieldPrefix.isPresent()) {
-				tokens.add(fieldPrefix.get());
-			}
-		}
-		Field field = fields.get(fields.size() - 1);
-		List<String> keys = Lists.newArrayList();
-		Joiner joiner = Joiner.on('.');
-		if (field.isAnnotationPresent(BindingAlias.class)) {
-			String[] aliases = field.getAnnotation(BindingAlias.class).value();
-			for (String alias : aliases) {
-				List<String> strings = Lists.newArrayList(tokens);
-				strings.add(alias);
-				keys.add(joiner.join(strings));
-			}
-			if (field.getAnnotation(BindingAlias.class).includeFieldName()) {
-				List<String> strings = Lists.newArrayList(tokens);
-				strings.add(field.getName());
-				keys.add(joiner.join(strings));
-			}
-		} else {
-			List<String> strings = Lists.newArrayList(tokens);
-			strings.add(field.getName());
-			keys.add(joiner.join(strings));
-		}
-		return keys;
+	private KeyAssembler(Builder builder) {
+		this.descriptor = builder.descriptor;
 	}
 
-	protected List<Field> getFieldPath(DefaultMutableTreeNode node) {
-		TreeNode[] path = node.getPath();
-		List<Field> fields = Lists.newArrayList();
-		for (TreeNode element : path) {
-			DefaultMutableTreeNode mutable = (DefaultMutableTreeNode) element;
-			Field field = (Field) mutable.getUserObject();
-			fields.add(field);
-		}
-		return fields;
-	}
+	public static class Builder implements org.kuali.common.util.build.Builder<KeyAssembler> {
 
-	protected String getKey(Optional<String> prefix, DefaultMutableTreeNode node) {
-		return null;
-	}
+		private BoundTypeDescriptor descriptor;
 
-	public static KeyAssembler make(Class<?> type, List<DefaultMutableTreeNode> nodes) {
-		return builder(type, nodes).build();
-	}
-
-	public static Builder builder(Class<?> type, List<DefaultMutableTreeNode> nodes) {
-		return new Builder(type, nodes);
-	}
-
-	public static class Builder {
-
-		private final Class<?> type;
-		private final List<DefaultMutableTreeNode> nodes;
-
-		public Builder(Class<?> type, List<DefaultMutableTreeNode> nodes) {
-			this.type = type;
-			this.nodes = nodes;
+		public Builder descriptor(BoundTypeDescriptor descriptor) {
+			this.descriptor = descriptor;
+			return this;
 		}
 
+		@Override
 		public KeyAssembler build() {
 			KeyAssembler instance = new KeyAssembler(this);
 			validate(instance);
@@ -115,8 +36,8 @@ public final class KeyAssembler implements Assembler<Set<String>> {
 		}
 
 		private static void validate(KeyAssembler instance) {
-			checkNotNull(instance.type, "'type' cannot be null");
-			checkNotNull(instance.nodes, "'nodes' cannot be null");
+			checkNotNull(instance.descriptor, "'descriptor' cannot be null");
 		}
 	}
+
 }
