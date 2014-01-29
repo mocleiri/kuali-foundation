a0c9f2dd460a96779977dddf492799ee96b1efdb
jcaddel <jcaddel@kuali.org>
2014-01-29 14:45:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31085 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/test/java/org/kuali/common/aws/status/GetStatusTest.java
+++ kuali-aws/src/test/java/org/kuali/common/aws/status/GetStatusTest.java
@@ -5,12 +5,18 @@ import java.util.List;
 import org.jasypt.util.text.TextEncryptor;
 import org.junit.Test;
 import org.kuali.common.aws.Credentials;
+import org.kuali.common.aws.ec2.api.EC2Service;
+import org.kuali.common.aws.ec2.impl.DefaultEC2Service;
+import org.kuali.common.aws.ec2.model.EC2ServiceContext;
 import org.kuali.common.util.enc.EncUtils;
 import org.kuali.common.util.log.LoggerUtils;
+import org.kuali.common.util.wait.DefaultWaitService;
+import org.kuali.common.util.wait.WaitService;
 import org.slf4j.Logger;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.ec2.model.Instance;
 import com.google.common.collect.Lists;
 
 public class GetStatusTest {
@@ -23,6 +29,13 @@ public class GetStatusTest {
 			String password = Passwords.getEncPassword();
 			List<AWSCredentials> creds = getCredentials(password);
 			logger.info(String.format("Located %s sets of credentials", creds.size()));
+			WaitService ws = new DefaultWaitService();
+			for (AWSCredentials credentials : creds) {
+				EC2ServiceContext context = EC2ServiceContext.create(credentials);
+				EC2Service service = new DefaultEC2Service(context, ws);
+				List<Instance> instances = service.getInstances();
+				logger.info(String.format("Located %s instances for %s", instances.size(), credentials.getAWSAccessKeyId()));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
