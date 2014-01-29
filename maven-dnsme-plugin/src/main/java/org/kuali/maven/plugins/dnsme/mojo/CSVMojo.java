d3555281468a4bbfcd5f848690433eb483bdef04
jcaddel <jcaddel@kuali.org>
2014-01-28 19:43:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31046 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/mojo/CSVMojo.java
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/mojo/CSVMojo.java
@@ -16,8 +16,10 @@
 package org.kuali.maven.plugins.dnsme.mojo;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.kuali.maven.plugins.dnsme.DNSMEClient;
@@ -27,107 +29,110 @@ import org.kuali.maven.plugins.dnsme.beans.Record;
 import org.kuali.maven.plugins.dnsme.beans.RecordType;
 import org.kuali.maven.plugins.dnsme.beans.Search;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
 /**
- * Show records for the domain indicated. By default, all records are shown. Search criteria can be used to restrict the
- * display
- *
+ * Show records for the domain indicated. By default, all records are shown. Search criteria can be used to restrict the display
+ * 
  * @author Jeff Caddel
  * @goal csv
  */
 public class CSVMojo extends BaseDNSMEMojo {
-    /**
-     * The domain to show records for
-     *
-     * @parameter expression="${dnsme.domainName}"
-     * @required
-     */
-    String domainName;
+	/**
+	 * The domain to show records for
+	 * 
+	 * @parameter expression="${dnsme.domainName}"
+	 * @required
+	 */
+	String domainName;
 
-    /**
-     * DEFAULT, US_EAST, US_WEST, ASIA
-     *
-     * @parameter expression="${dnsme.gtdLocation}"
-     */
-    GTDLocation gtdLocation;
+	/**
+	 * DEFAULT, US_EAST, US_WEST, ASIA
+	 * 
+	 * @parameter expression="${dnsme.gtdLocation}"
+	 */
+	GTDLocation gtdLocation;
 
-    /**
-     * A, CNAME, MX, NS, PTR, SRV, AAAA, HTTPRED, TXT
-     *
-     * @parameter expression="${dnsme.recordType}"
-     */
-    RecordType recordType;
+	/**
+	 * A, CNAME, MX, NS, PTR, SRV, AAAA, HTTPRED, TXT
+	 * 
+	 * @parameter expression="${dnsme.recordType}"
+	 */
+	RecordType recordType;
 
-    /**
-     * Matches a single record with this exact name
-     *
-     * @parameter expression="${dnsme.recordName}"
-     */
-    String recordName;
+	/**
+	 * Matches a single record with this exact name
+	 * 
+	 * @parameter expression="${dnsme.recordName}"
+	 */
+	String recordName;
 
-    /**
-     * Matches any record with a name that contains this value
-     *
-     * @parameter expression="${dnsme.recordNameContains}"
-     */
-    String recordNameContains;
+	/**
+	 * Matches any record with a name that contains this value
+	 * 
+	 * @parameter expression="${dnsme.recordNameContains}"
+	 */
+	String recordNameContains;
 
-    /**
-     * Matches any record with this exact value
-     *
-     * @parameter expression="${dnsme.recordValue}"
-     */
-    String recordValue;
+	/**
+	 * Matches any record with this exact value
+	 * 
+	 * @parameter expression="${dnsme.recordValue}"
+	 */
+	String recordValue;
 
-    /**
-     * Matches any record with a value that contains this value
-     *
-     * @parameter expression="${dnsme.recordValueContains}"
-     */
-    String recordValueContains;
+	/**
+	 * Matches any record with a value that contains this value
+	 * 
+	 * @parameter expression="${dnsme.recordValueContains}"
+	 */
+	String recordValueContains;
 
-    /**
-     * Matches any record with a value that contains this value
-     *
-     * @parameter expression="${dnsme.outputFile}"
-     */
-    File outputFile;
+	/**
+	 * Matches any record with a value that contains this value
+	 * 
+	 * @parameter expression="${dnsme.outputFile}"
+	 */
+	File outputFile;
 
-    @Override
-    public void performTasks(DNSMEClient client) throws MojoExecutionException, MojoFailureException {
-        Search search = new Search();
-        search.setGtdLocation(gtdLocation);
-        search.setType(recordType);
-        search.setName(recordName);
-        search.setNameContains(recordNameContains);
-        search.setValue(recordValue);
-        search.setValueContains(recordValueContains);
+	@Override
+	public void performTasks(DNSMEClient client) throws MojoExecutionException, MojoFailureException {
+		Search search = new Search();
+		search.setGtdLocation(gtdLocation);
+		search.setType(recordType);
+		search.setName(recordName);
+		search.setNameContains(recordNameContains);
+		search.setValue(recordValue);
+		search.setValueContains(recordValueContains);
 
-        Domain domain = client.getDomain(domainName);
-        List<Record> records = client.getRecords(domain, search);
-        getLog().info("Showing records for: " + domainName);
-        for (Record record : records) {
-            getLog().info(getCompactLog(record));
-        }
-    }
+		Domain domain = client.getDomain(domainName);
+		List<Record> records = client.getRecords(domain, search);
+		Joiner joiner = Joiner.on(',');
+		List<String> lines = Lists.newArrayList();
+		String header = "name,data,type,ttl";
+		lines.add(header);
+		for (Record record : records) {
+			List<String> tokens = getTokens(record);
+			String line = joiner.join(tokens.iterator());
+			lines.add(line);
+		}
+		try {
+			String path = outputFile.getCanonicalPath();
+			FileUtils.writeLines(outputFile, lines);
+			getLog().info("Created; " + path);
+		} catch (IOException e) {
+			throw new IllegalStateException("Unexpected IO error", e);
+		}
+	}
 
-    protected String getCompactLog(Record record) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(record.getName());
-        sb.append("->" + record.getData());
-        sb.append("," + record.getType());
-        sb.append(",ttl=" + record.getTtl() + "s");
-        return sb.toString();
-    }
-
-    protected String getLog(Record record) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Id:" + record.getId());
-        sb.append(" Name:" + record.getName());
-        sb.append(" Value:" + record.getData());
-        sb.append(" Type:" + record.getType());
-        sb.append(" TTL:" + record.getTtl());
-        sb.append(" GTD:" + record.getGtdLocation());
-        return sb.toString();
-    }
+	protected List<String> getTokens(Record record) {
+		List<String> strings = Lists.newArrayList();
+		strings.add(record.getName());
+		strings.add(record.getData());
+		strings.add(record.getType().name());
+		strings.add(record.getTtl() + "");
+		return strings;
+	}
 
 }
