bbc9a51baf0d026af62a76aaa1acb80f2bd070d8
jcaddel <jcaddel@kuali.org>
2014-01-29 18:26:49 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31109 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/test/java/org/kuali/common/aws/status/MergeTest.java
+++ kuali-aws/src/test/java/org/kuali/common/aws/status/MergeTest.java
@@ -8,8 +8,12 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.kuali.common.aws.status.model.AwsRecord;
 import org.kuali.common.aws.status.model.DnsmeRecord;
+import org.kuali.common.aws.status.model.Environment;
 import org.kuali.common.util.file.CanonicalFile;
+import org.kuali.common.util.log.LoggerUtils;
+import org.slf4j.Logger;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
@@ -20,6 +24,9 @@ public class MergeTest {
 
 	private static final String DNSME = "./target/dnsme/records.csv";
 	private static final String AWS = GetStatusTest.OUTPUT_PATH;
+	private static final String KUALI = "./target/kuali/envs.csv";
+
+	private static final Logger logger = LoggerUtils.make();
 
 	@Test
 	public void test() {
@@ -30,16 +37,60 @@ public class MergeTest {
 			checkArgument(dnsme.exists(), "[%s] does not exist", dnsme);
 			checkArgument(aws.exists(), "[%s] does not exist", aws);
 			List<DnsmeRecord> drecs = getDnsmeRecords(dnsme);
+			List<AwsRecord> arecs = getAwsRecords(aws);
+			List<Environment> envs = merge(drecs, arecs);
+			logger.info("Merged %s records", envs.size());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
 
+	protected List<Environment> merge(List<DnsmeRecord> drecs, List<AwsRecord> arecs) {
+		List<Environment> envs = Lists.newArrayList();
+		for (AwsRecord arec : arecs) {
+			DnsmeRecord drec = findRecord(arec, drecs);
+			Environment env = new Environment();
+			env.setProject(arec.getProject());
+			env.setId(arec.getEnv());
+			env.setDns(drec.getDns() + ".kuali.org");
+			env.setType(arec.getType());
+			envs.add(env);
+		}
+		return envs;
+	}
+
+	protected DnsmeRecord findRecord(AwsRecord arec, List<DnsmeRecord> drecs) {
+		for (DnsmeRecord drec : drecs) {
+			if (drec.getAlias().equals(arec.getDns())) {
+				return drec;
+			}
+		}
+		throw new IllegalStateException(String.format("No corresponding DNSME entry for [%s=%s]", arec.getEnv(), arec.getDns()));
+	}
+
+	protected List<AwsRecord> getAwsRecords(File file) throws IOException {
+		List<String> lines = FileUtils.readLines(file);
+		List<AwsRecord> records = Lists.newArrayList();
+		for (int i = 1; i < lines.size(); i++) {
+			String line = lines.get(i);
+			List<String> tokens = Lists.newArrayList(SPLITTER.split(line));
+			AwsRecord record = new AwsRecord();
+			record.setProject(tokens.get(0));
+			record.setEnv(tokens.get(1));
+			record.setDns(tokens.get(2));
+			record.setType(tokens.get(3));
+			records.add(record);
+		}
+		return records;
+
+	}
+
 	protected List<DnsmeRecord> getDnsmeRecords(File file) throws IOException {
 		List<String> lines = FileUtils.readLines(file);
 		List<DnsmeRecord> records = Lists.newArrayList();
-		for (String line : lines) {
+		for (int i = 1; i < lines.size(); i++) {
+			String line = lines.get(i);
 			List<String> tokens = Lists.newArrayList(SPLITTER.split(line));
 			DnsmeRecord record = new DnsmeRecord();
 			record.setDns(tokens.get(0));
