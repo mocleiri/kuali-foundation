4c5a7eba753f46c4de049a20fd2c154f679d080d
jcaddel <jcaddel@kuali.org>
2014-01-29 18:15:13 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31108 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/test/java/org/kuali/common/aws/status/MergeTest.java
+++ kuali-aws/src/test/java/org/kuali/common/aws/status/MergeTest.java
@@ -3,19 +3,20 @@ package org.kuali.common.aws.status;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.kuali.common.aws.status.model.DnsmeRecord;
 import org.kuali.common.util.file.CanonicalFile;
-import org.kuali.common.util.log.LoggerUtils;
-import org.slf4j.Logger;
 
-import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 
 public class MergeTest {
 
-	private static final Logger logger = LoggerUtils.make();
-	private static final Joiner JOINER = Joiner.on(',');
-	private static final Joiner LINES = Joiner.on('\n');
+	private static final Splitter SPLITTER = Splitter.on(',');
 
 	private static final String DNSME = "./target/dnsme/records.csv";
 	private static final String AWS = GetStatusTest.OUTPUT_PATH;
@@ -28,9 +29,27 @@ public class MergeTest {
 			File aws = new CanonicalFile(System.getProperty("aws.input", AWS));
 			checkArgument(dnsme.exists(), "[%s] does not exist", dnsme);
 			checkArgument(aws.exists(), "[%s] does not exist", aws);
+			List<DnsmeRecord> drecs = getDnsmeRecords(dnsme);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	protected List<DnsmeRecord> getDnsmeRecords(File file) throws IOException {
+		List<String> lines = FileUtils.readLines(file);
+		List<DnsmeRecord> records = Lists.newArrayList();
+		for (String line : lines) {
+			List<String> tokens = Lists.newArrayList(SPLITTER.split(line));
+			DnsmeRecord record = new DnsmeRecord();
+			record.setDns(tokens.get(0));
+			record.setAlias(tokens.get(1));
+			record.setType(tokens.get(2));
+			record.setTtl(tokens.get(3));
+			records.add(record);
+		}
+		return records;
+
 	}
 
 }
