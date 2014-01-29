0425e1de58d18bfe7526538a94763f109c6c0a29
jcaddel <jcaddel@kuali.org>
2014-01-29 11:41:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31066 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/beans/RecordNameComparator.java
+++ maven-dnsme-plugin/src/main/java/org/kuali/maven/plugins/dnsme/beans/RecordNameComparator.java
@@ -12,35 +12,33 @@ import com.google.common.collect.Lists;
 
 public final class RecordNameComparator implements Comparator<Record> {
 
-	private static final Comparator<String> COMPARATOR = new ReverseStringComparator();
-
 	@Override
 	public int compare(Record one, Record two) {
-		String n1 = one.getName();
-		String n2 = two.getName();
-		if (n1.startsWith("env") && n2.startsWith("env")) {
-			return compareEnvRecords(n1, n2);
-		} else {
-			return COMPARATOR.compare(one.getName(), two.getName());
-		}
+		return compareStrings(one.getName(), two.getName());
 	}
 
-	protected int compareEnvRecords(String one, String two) {
+	protected int compareStrings(String one, String two) {
 		Splitter splitter = Splitter.on('.');
 		Joiner joiner = Joiner.on('.');
 		List<String> t1 = Lists.newArrayList(splitter.split(one));
 		List<String> t2 = Lists.newArrayList(splitter.split(two));
-		Integer i1 = Integer.parseInt(t1.get(0).substring(3));
-		Integer i2 = Integer.parseInt(t2.get(0).substring(3));
-		String s1 = StringUtils.leftPad(i1.toString(), 10, "0");
-		String s2 = StringUtils.leftPad(i2.toString(), 10, "0");
-		t1.set(0, s1);
-		t2.set(0, s2);
+		padEnvString(t1);
+		padEnvString(t2);
 		Collections.reverse(t1);
 		Collections.reverse(t2);
 		String c1 = joiner.join(t1);
 		String c2 = joiner.join(t2);
 		return c1.compareTo(c2);
+	}
+
+	protected List<String> padEnvString(List<String> list) {
+		String first = list.get(0);
+		if (first.startsWith("env")) {
+			Integer i = Integer.parseInt(first.substring(3));
+			String s = StringUtils.leftPad(i.toString(), 10, "0");
+			list.set(0, s);
+		}
+		return list;
 	}
 
 }
