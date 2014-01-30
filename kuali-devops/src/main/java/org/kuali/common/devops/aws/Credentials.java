af95b73846ae23198360339a728027cf9bd5f570
jcaddel <jcaddel@kuali.org>
2014-01-29 21:31:31 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31155 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/Credentials.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/Credentials.java
@@ -7,9 +7,9 @@ import com.amazonaws.auth.AWSCredentials;
 public enum Credentials implements AWSCredentials {
 
 	FOUNDATION("AKIAJFD5IM7IPVVUEBNA", "ENC(uXNCzc6efcKz1zvp4t5Fj4wyR9oGw2GZ2VOB3SXZaoXaV1BA1Gao2d2vWXnjqUA1oKzg+0s9NAM=)"), //
-	STUDENT("AKIAJZ72UQ5ZCVEDMAPQ", "ENC(fXH7NKtbCDnn17aoTOPL707itdwlz6VKyHJaKCHAuBk//XGTyGef+2CeM89GqpIPZYk4ewKia2Y=)"), //
+	OLE("AKIAI453FI76LUZ7T7CA", "ENC(2UP9ztdMy32DdN62ZMjD+K0jMYCrKkYzI2xies0asEU4cc7sWOF47a2CDu314ojHoSrz/CtFaAs=)"), //
 	RICE("AKIAIZFPMJVCNOYYAZ2Q", "ENC(iDHNEii2oWjbwqJMaaT4f5SKhiNHyhWhcd9NIHTPIByNJlUg72B4czVFeIf/rNYJvp/gFXQkTj0=)"), //
-	OLE("AKIAI453FI76LUZ7T7CA", "ENC(2UP9ztdMy32DdN62ZMjD+K0jMYCrKkYzI2xies0asEU4cc7sWOF47a2CDu314ojHoSrz/CtFaAs=)");
+	STUDENT("AKIAJZ72UQ5ZCVEDMAPQ", "ENC(fXH7NKtbCDnn17aoTOPL707itdwlz6VKyHJaKCHAuBk//XGTyGef+2CeM89GqpIPZYk4ewKia2Y=)"); //
 
 	private final String accessKey;
 	private final String secretKey;
