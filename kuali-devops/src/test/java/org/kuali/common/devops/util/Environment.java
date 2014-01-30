061adff5c104e656ee57336307cfab43986166b4
jcaddel <jcaddel@kuali.org>
2014-01-29 20:33:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31135 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/util/Environment.java
@@ -0,0 +1,42 @@
+package org.kuali.common.devops.util;
+
+public class Environment {
+
+	String project;
+	String id;
+	String dns;
+	String type;
+
+	public String getProject() {
+		return project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDns() {
+		return dns;
+	}
+
+	public void setDns(String dns) {
+		this.dns = dns;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
