024113aacd47788864abfccaa04695b0ee61cfb9
jcaddel <jcaddel@kuali.org>
2014-01-29 20:46:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31141 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/status/StatusTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/StatusTest.java
@@ -6,15 +6,10 @@ import java.util.Map;
 import org.junit.Test;
 import org.kuali.common.devops.util.Fqdns;
 import org.kuali.common.devops.util.Instances;
-import org.kuali.common.util.log.LoggerUtils;
-import org.slf4j.Logger;
 
 import com.amazonaws.services.ec2.model.Instance;
 
 public class StatusTest {
-
-	private static final Logger logger = LoggerUtils.make();
-	private static final String DOMAIN = "kuali.org";
 
 	@Test
 	public void test() {
