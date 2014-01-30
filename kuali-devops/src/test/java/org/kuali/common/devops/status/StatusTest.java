06fac6c747607cc1bf5344a175084292857431e3
jcaddel <jcaddel@kuali.org>
2014-01-29 20:57:13 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31145 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/status/StatusTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/StatusTest.java
@@ -4,22 +4,27 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
-import org.kuali.common.devops.util.Fqdns;
+import org.kuali.common.devops.util.AwsRecord;
 import org.kuali.common.devops.util.Instances;
+import org.kuali.common.util.log.LoggerUtils;
+import org.slf4j.Logger;
 
 import com.amazonaws.services.ec2.model.Instance;
 
 public class StatusTest {
 
+	private static final Logger logger = LoggerUtils.make();
+
 	@Test
 	public void test() {
 		try {
-			Map<String, String> fqdns = Fqdns.getMap();
+			// Map<String, String> fqdns = Fqdns.getMap();
 			Map<String, List<Instance>> instances = Instances.getMap();
+			List<AwsRecord> records = Instances.getRecords(instances);
+			logger.info("Located {} records", records.size());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 
 }
