029a0eb9b50e0c029c25802cdad9922745a06648
jcaddel <jcaddel@kuali.org>
2014-01-29 23:10:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31160 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/status/StatusTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/StatusTest.java
@@ -2,19 +2,25 @@ package org.kuali.common.devops.status;
 
 import static com.google.common.base.Preconditions.checkState;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.kuali.common.devops.util.AwsRecord;
 import org.kuali.common.devops.util.Environment;
-import org.kuali.common.devops.util.Fqdns;
-import org.kuali.common.devops.util.Instances;
+import org.kuali.common.util.LocationUtils;
 import org.kuali.common.util.log.LoggerUtils;
 import org.slf4j.Logger;
 
-import com.amazonaws.services.ec2.model.Instance;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class StatusTest {
 
@@ -23,14 +29,47 @@ public class StatusTest {
 	@Test
 	public void test() {
 		try {
-			Map<String, String> fqdns = Fqdns.getMap();
-			Map<String, List<Instance>> instances = Instances.getMap();
-			List<AwsRecord> records = Instances.getRecords(instances);
-			List<Environment> envs = merge(records, fqdns);
-			logger.info("Located {} managed environments", envs.size());
+			// Map<String, String> fqdns = Fqdns.getMap();
+			// Map<String, List<Instance>> instances = Instances.getMap();
+			// List<AwsRecord> records = Instances.getRecords(instances);
+			// List<Environment> envs = merge(records, fqdns);
+			// logger.info("Located {} managed environments", envs.size());
+			String fqdn = "env1.ks.kuali.org";
+			Map<String, String> map = getManifestAttributes(fqdn);
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected Map<String, String> getManifestAttributes(String fqdn) {
+		String protocol = "http://";
+		String fragment = "/tomcat/webapps/ROOT/META-INF/MANIFEST.MF";
+		String location = protocol + fqdn + fragment;
+		Map<String, String> map = Maps.newHashMap();
+		InputStream in = null;
+		try {
+			in = LocationUtils.getInputStream(location);
+			Manifest manifest = new Manifest(in);
+			Attributes attributes = manifest.getMainAttributes();
+			SortedSet<String> keys = getKeys(attributes);
+			for (String key : keys) {
+				String value = attributes.getValue(key);
+				map.put(key, value);
+			}
+		} catch (IOException e) {
+			logger.warn(String.format("cannot access -> [%s]", location));
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+		return map;
+	}
+
+	protected SortedSet<String> getKeys(Attributes attributes) {
+		SortedSet<String> keys = Sets.newTreeSet();
+		for (Object key : attributes.keySet()) {
+			keys.add(key.toString());
+		}
+		return keys;
 	}
 
 	protected List<Environment> merge(List<AwsRecord> records, Map<String, String> fqdns) {
