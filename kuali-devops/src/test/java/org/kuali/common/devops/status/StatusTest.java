752816e1092d560b676b657973b73db1bf9b6a0b
jcaddel <jcaddel@kuali.org>
2014-01-29 20:44:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31138 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/status/StatusTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/StatusTest.java
@@ -1,10 +1,20 @@
 package org.kuali.common.devops.status;
 
+import static com.google.common.base.Preconditions.checkState;
+
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.codehaus.plexus.util.StringUtils;
 import org.junit.Test;
+import org.kuali.common.aws.Credentials;
+import org.kuali.common.aws.ec2.api.EC2Service;
+import org.kuali.common.aws.ec2.impl.DefaultEC2Service;
+import org.kuali.common.aws.ec2.model.EC2ServiceContext;
 import org.kuali.common.devops.util.Auth;
+import org.kuali.common.devops.util.Fqdns;
+import org.kuali.common.devops.util.Instances;
 import org.kuali.common.dns.api.DnsService;
 import org.kuali.common.dns.dnsme.DNSMadeEasyDnsService;
 import org.kuali.common.dns.dnsme.URLS;
@@ -13,9 +23,16 @@ import org.kuali.common.dns.model.DnsRecord;
 import org.kuali.common.dns.model.DnsRecordSearchCriteria;
 import org.kuali.common.dns.model.DnsRecordType;
 import org.kuali.common.util.log.LoggerUtils;
+import org.kuali.common.util.wait.DefaultWaitService;
+import org.kuali.common.util.wait.WaitService;
 import org.slf4j.Logger;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Tag;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class StatusTest {
@@ -26,13 +43,88 @@ public class StatusTest {
 	@Test
 	public void test() {
 		try {
-			Map<String, String> fqdns = getFQDNs();
+			Map<String, String> fqdns = Fqdns.getMap();
+			Map<String, List<Instance>> instances = Instances.getMap();
 			for (String key : fqdns.keySet()) {
 				logger.info(String.format("[%s]=[%s]", key, fqdns.get(key)));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected String getProjectName(String accessKey) {
+		for (Credentials credentials : Credentials.values()) {
+			if (accessKey.equals(credentials.getAWSAccessKeyId())) {
+				return credentials.name().toLowerCase();
+			}
+		}
+		throw new IllegalArgumentException(String.format("Unable to locate a name for [%s]", accessKey));
+	}
+
+	protected List<Instance> filter(List<Instance> instances) {
+		List<Instance> filtered = Lists.newArrayList();
+		for (Instance instance : instances) {
+			boolean env = isDeployEnvironment(instance);
+			if (env) {
+				filtered.add(instance);
+			}
+		}
+		return filtered;
+	}
+
+	protected static Tag getRequiredTag(Instance instance, String key) {
+		Optional<Tag> optional = getTag(instance, key);
+		checkState(optional.isPresent(), "Required tag [%s] is not present for instance [%s]", key, instance.getInstanceId());
+		return optional.get();
+	}
+
+	protected static Optional<Tag> getTag(Instance instance, String key) {
+		List<Tag> tags = instance.getTags();
+		for (Tag tag : tags) {
+			if (key.equals(tag.getKey())) {
+				return Optional.of(tag);
+			}
+		}
+		return Optional.absent();
+	}
+
+	protected boolean isDeployEnvironment(Instance instance) {
+		Optional<Tag> tag = getTag(instance, "Name");
+		if (!tag.isPresent()) {
+			return false;
+		}
+		String value = tag.get().getValue();
+		return value.startsWith("env");
+	}
+
+	protected Map<String, List<Instance>> getMap() {
+		List<AWSCredentials> creds = Auth.getCredentials();
+		logger.info(String.format("Using %s sets of AWS credentials", creds.size()));
+		WaitService ws = new DefaultWaitService();
+		Map<String, List<Instance>> instances = Maps.newHashMap();
+		for (AWSCredentials credentials : creds) {
+			EC2ServiceContext context = EC2ServiceContext.create(credentials);
+			EC2Service service = new DefaultEC2Service(context, ws);
+			List<Instance> list = Lists.newArrayList(service.getInstances());
+			Iterator<Instance> itr = list.iterator();
+			while (itr.hasNext()) {
+				Instance i = itr.next();
+				if (!service.isOnline(i.getInstanceId())) {
+					itr.remove();
+				}
+			}
+			String projectName = getProjectName(credentials.getAWSAccessKeyId());
+			instances.put(projectName, list);
+			logger.debug(String.format("Located %s instances for %s", list.size(), projectName));
+		}
+		for (String key : instances.keySet()) {
+			List<Instance> list = instances.get(key);
+			List<Instance> filtered = filter(list);
+			logger.info(String.format("%s -> %s environments", StringUtils.rightPad(key, 12), filtered.size()));
+			instances.put(key, filtered);
+		}
+		return instances;
 	}
 
 	/**
---
+++ kuali-devops/src/test/java/org/kuali/common/devops/util/Instances.java
@@ -0,0 +1,104 @@
+package org.kuali.common.devops.util;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.plexus.util.StringUtils;
+import org.kuali.common.aws.Credentials;
+import org.kuali.common.aws.ec2.api.EC2Service;
+import org.kuali.common.aws.ec2.impl.DefaultEC2Service;
+import org.kuali.common.aws.ec2.model.EC2ServiceContext;
+import org.kuali.common.util.log.LoggerUtils;
+import org.kuali.common.util.wait.DefaultWaitService;
+import org.kuali.common.util.wait.WaitService;
+import org.slf4j.Logger;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Tag;
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+public class Instances {
+
+	private static final Logger logger = LoggerUtils.make();
+
+	public static Map<String, List<Instance>> getMap() {
+		List<AWSCredentials> creds = Auth.getCredentials();
+		logger.info(String.format("Using %s sets of AWS credentials", creds.size()));
+		WaitService ws = new DefaultWaitService();
+		Map<String, List<Instance>> instances = Maps.newHashMap();
+		for (AWSCredentials credentials : creds) {
+			EC2ServiceContext context = EC2ServiceContext.create(credentials);
+			EC2Service service = new DefaultEC2Service(context, ws);
+			List<Instance> list = Lists.newArrayList(service.getInstances());
+			Iterator<Instance> itr = list.iterator();
+			while (itr.hasNext()) {
+				Instance i = itr.next();
+				if (!service.isOnline(i.getInstanceId())) {
+					itr.remove();
+				}
+			}
+			String projectName = getProjectName(credentials.getAWSAccessKeyId());
+			instances.put(projectName, list);
+			logger.debug(String.format("Located %s instances for %s", list.size(), projectName));
+		}
+		for (String key : instances.keySet()) {
+			List<Instance> list = instances.get(key);
+			List<Instance> filtered = filter(list);
+			logger.info(String.format("%s -> %s environments", StringUtils.rightPad(key, 12), filtered.size()));
+			instances.put(key, filtered);
+		}
+		return instances;
+	}
+
+	protected static String getProjectName(String accessKey) {
+		for (Credentials credentials : Credentials.values()) {
+			if (accessKey.equals(credentials.getAWSAccessKeyId())) {
+				return credentials.name().toLowerCase();
+			}
+		}
+		throw new IllegalArgumentException(String.format("Unable to locate a name for [%s]", accessKey));
+	}
+
+	protected static List<Instance> filter(List<Instance> instances) {
+		List<Instance> filtered = Lists.newArrayList();
+		for (Instance instance : instances) {
+			boolean env = isDeployEnvironment(instance);
+			if (env) {
+				filtered.add(instance);
+			}
+		}
+		return filtered;
+	}
+
+	protected static Tag getRequiredTag(Instance instance, String key) {
+		Optional<Tag> optional = getTag(instance, key);
+		checkState(optional.isPresent(), "Required tag [%s] is not present for instance [%s]", key, instance.getInstanceId());
+		return optional.get();
+	}
+
+	protected static Optional<Tag> getTag(Instance instance, String key) {
+		List<Tag> tags = instance.getTags();
+		for (Tag tag : tags) {
+			if (key.equals(tag.getKey())) {
+				return Optional.of(tag);
+			}
+		}
+		return Optional.absent();
+	}
+
+	protected static boolean isDeployEnvironment(Instance instance) {
+		Optional<Tag> tag = getTag(instance, "Name");
+		if (!tag.isPresent()) {
+			return false;
+		}
+		String value = tag.get().getValue();
+		return value.startsWith("env");
+	}
+
+}
