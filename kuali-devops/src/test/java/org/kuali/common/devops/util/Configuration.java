8aabf854b3c26d51ce2e47effc160df9d1c70559
jcaddel <jcaddel@kuali.org>
2014-01-30 11:16:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31203 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/util/Configuration.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/util/Configuration.java
@@ -1,15 +1,17 @@
 package org.kuali.common.devops.util;
 
+import java.util.Properties;
+
 import org.kuali.common.util.log.LoggerUtils;
 import org.kuali.common.util.project.model.Project;
 import org.slf4j.Logger;
 
-public class Databases {
+public class Configuration {
 
 	private static final Logger logger = LoggerUtils.make();
 
-	public static Database getDatabase(String fqdn, Project project) {
-		return new Database();
+	public static Properties getProperties(String fqdn, Project project) {
+		return new Properties();
 	}
 
 }
