dc5c93812a35a3452c368af5253a15a870718064
jcaddel <jcaddel@kuali.org>
2014-01-31 17:19:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31287 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Databases.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Databases.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 import org.kuali.common.devops.model.Application;
 import org.kuali.common.devops.model.Database;
 import org.kuali.common.devops.model.Environment;
+import org.kuali.common.util.project.KualiProjectConstants;
 
 import com.google.common.collect.ImmutableList;
 
@@ -26,9 +27,26 @@ public class Databases {
 
 	public static Database getDatabase(Application app) {
 		Properties config = app.getConfiguration();
-		String username = getProperty(config, "datasource.username", "jdbc.username");
+		String groupId = app.getProject().getGroupId();
+		if (groupId.equals(KualiProjectConstants.OLE_GROUP_ID)) {
+			return getOleDatabase(config);
+		}
+		String username = getProperty(config, "datasource.username");
 		String url = getProperty(config, "datasource.url");
 		String vendor = getVendor(url);
+		Database db = new Database();
+		db.setVendor(vendor);
+		db.setUsername(username);
+		db.setUrl(url);
+		return db;
+	}
+
+	protected static Database getOleDatabase(Properties config) {
+		String vendor = getProperty(config, "db.vendor");
+		String username = getProperty(config, "jdbc.username");
+		String urlKey = vendor + ".dba.url";
+		String url = getProperty(config, urlKey);
+
 		Database db = new Database();
 		db.setVendor(vendor);
 		db.setUsername(username);
