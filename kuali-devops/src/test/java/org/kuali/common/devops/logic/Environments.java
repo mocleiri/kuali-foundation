e0c2cb3f2a600d7fa74a12f0cd760ee96af9d4c7
jcaddel <jcaddel@kuali.org>
2014-01-31 18:00:37 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31294 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Environments.java
@@ -0,0 +1,41 @@
+package org.kuali.common.devops.logic;
+
+import java.util.Set;
+
+import org.kuali.common.devops.model.Environment;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+public class Environments {
+
+	public static Table<String, String, String> getTable(Environment env) {
+		Table<String, String, String> table = HashBasedTable.create();
+		table.put("row1", "col1", "a");
+		table.put("row1", "col2", "b");
+		table.put("row1", "col3", "c");
+		table.put("row1", "col4", "d");
+		table.put("row2", "col1", "1");
+		table.put("row2", "col2", "2");
+		table.put("row2", "col3", "3");
+		table.put("row2", "col4", "4");
+		return table;
+	}
+
+	public static String html(Table<?, ?, ?> table) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<table border=1>\n");
+		Set<?> rowKeys = table.rowKeySet();
+		Set<?> colKeys = table.columnKeySet();
+		for (Object rowKey : rowKeys) {
+			sb.append(" <tr>\n");
+			for (Object colKey : colKeys) {
+				Object value = table.get(rowKey, colKey);
+				sb.append(String.format("  <td>%s</td>", value.toString()));
+			}
+			sb.append(" </tr>\n");
+		}
+		sb.append("</table>\n");
+		return sb.toString();
+	}
+}
