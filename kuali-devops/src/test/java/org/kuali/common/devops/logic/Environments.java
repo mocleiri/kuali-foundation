78a0eb6bbdb1fb9f5114705341b25b2632ee2be9
jcaddel <jcaddel@kuali.org>
2014-01-31 21:18:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31330 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Environments.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Environments.java
@@ -3,7 +3,6 @@ package org.kuali.common.devops.logic;
 import static java.lang.String.format;
 
 import java.util.List;
-import java.util.Properties;
 import java.util.SortedSet;
 
 import org.apache.commons.lang3.StringUtils;
@@ -11,10 +10,10 @@ import org.kuali.common.devops.model.Application;
 import org.kuali.common.devops.model.Database;
 import org.kuali.common.devops.model.Environment;
 import org.kuali.common.devops.model.Tomcat;
-import org.kuali.common.util.project.model.ImmutableProject;
 import org.kuali.common.util.project.model.Project;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Lists;
@@ -27,16 +26,16 @@ public class Environments {
 		Table<Integer, Integer, Object> table = HashBasedTable.create();
 		for (int row = 0; row < envs.size(); row++) {
 			Environment env = envs.get(row);
-			Project project = getProject(env);
-			Database db = getDatabase(env);
+			Optional<Project> project = getProject(env);
+			Optional<Database> db = getDatabase(env);
 			table.put(Integer.valueOf(row), Integer.valueOf(0), env.getProject());
 			table.put(Integer.valueOf(row), Integer.valueOf(1), env.getId().substring(3));
 			table.put(Integer.valueOf(row), Integer.valueOf(2), env.getFqdn());
 			table.put(Integer.valueOf(row), Integer.valueOf(3), env.getJava());
 			table.put(Integer.valueOf(row), Integer.valueOf(4), env.getType());
 			table.put(Integer.valueOf(row), Integer.valueOf(5), getTable(env.getTomcat()));
-			table.put(Integer.valueOf(row), Integer.valueOf(8), getTable(project));
-			table.put(Integer.valueOf(row), Integer.valueOf(10), getTable(db));
+			table.put(Integer.valueOf(row), Integer.valueOf(8), project.isPresent() ? getTable(project.get()) : "na");
+			table.put(Integer.valueOf(row), Integer.valueOf(10), db.isPresent() ? getTable(db.get()) : "na");
 		}
 		return table;
 	}
@@ -52,7 +51,7 @@ public class Environments {
 		} else {
 			List<String> tokens = Lists.newArrayList(Splitter.on(':').splitToList(url));
 			tokens.remove(0); // scm
-			tokens.remove(0); // svn|git
+			tokens.remove(0); // svn
 			url = Joiner.on(':').join(tokens.iterator());
 			url = "<a href=\"" + url + "\">public url</a>";
 		}
@@ -86,21 +85,21 @@ public class Environments {
 		}
 	}
 
-	protected static Database getDatabase(Environment env) {
+	protected static Optional<Database> getDatabase(Environment env) {
 		if (env.getApplication().isPresent()) {
 			Application app = env.getApplication().get();
-			return app.getDatabase();
+			return Optional.of(app.getDatabase());
 		} else {
-			return new Database();
+			return Optional.absent();
 		}
 	}
 
-	protected static Project getProject(Environment env) {
+	protected static Optional<Project> getProject(Environment env) {
 		if (env.getApplication().isPresent()) {
 			Application app = env.getApplication().get();
-			return app.getProject();
+			return Optional.of(app.getProject());
 		} else {
-			return new ImmutableProject("na", "na", "na", new Properties());
+			return Optional.absent();
 		}
 	}
 
