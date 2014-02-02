f12308750791281d65b81779999b558abe603cd6
jcaddel <jcaddel@kuali.org>
2014-02-02 14:01:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31436 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Tables.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Tables.java
@@ -1,5 +1,8 @@
 package org.kuali.common.devops.logic;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Table;
 
 public class Tables {
@@ -12,9 +15,13 @@ public class Tables {
 	}
 
 	public static void addRow(Table<Integer, Integer, String> table, String... strings) {
+		addRow(table, ImmutableList.copyOf(strings));
+	}
+
+	public static void addRow(Table<Integer, Integer, String> table, List<String> strings) {
 		int row = table.rowKeySet().size();
-		for (int col = 0; col < strings.length; col++) {
-			table.put(Integer.valueOf(row), Integer.valueOf(col), strings[col]);
+		for (int col = 0; col < strings.size(); col++) {
+			table.put(Integer.valueOf(row), Integer.valueOf(col), strings.get(col));
 		}
 	}
 }
