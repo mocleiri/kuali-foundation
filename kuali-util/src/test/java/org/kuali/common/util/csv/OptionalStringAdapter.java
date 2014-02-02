1d5ff0b72d326e593549db053cb5f0354e4aad4c
jcaddel <jcaddel@kuali.org>
2014-02-02 18:05:45 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31451 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/csv/OptionalStringAdapter.java
+++ kuali-util/src/test/java/org/kuali/common/util/csv/OptionalStringAdapter.java
@@ -1,10 +1,12 @@
 package org.kuali.common.util.csv;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.google.common.base.Optional;
 
 public class OptionalStringAdapter extends CsvAdapter<Optional<String>> {
 
-	private final CsvAdapter<String> adapter = BasicStringAdapter.builder().build();
+	private final CsvAdapter<String> adapter;
 
 	@Override
 	public String format(Optional<String> instance) {
@@ -18,6 +20,39 @@ public class OptionalStringAdapter extends CsvAdapter<Optional<String>> {
 
 	public CsvAdapter<String> getAdapter() {
 		return adapter;
+	}
+
+	private OptionalStringAdapter(Builder builder) {
+		this.adapter = builder.adapter;
+	}
+
+	public static OptionalStringAdapter create() {
+		return builder().build();
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static class Builder implements org.kuali.common.util.build.Builder<OptionalStringAdapter> {
+
+		private CsvAdapter<String> adapter = BasicStringAdapter.builder().build();
+
+		public Builder adapter(CsvAdapter<String> adapter) {
+			this.adapter = adapter;
+			return this;
+		}
+
+		@Override
+		public OptionalStringAdapter build() {
+			OptionalStringAdapter instance = new OptionalStringAdapter(this);
+			validate(instance);
+			return instance;
+		}
+
+		private static void validate(OptionalStringAdapter instance) {
+			checkNotNull(instance.adapter, "'adapter' cannot be null");
+		}
 	}
 
 }
