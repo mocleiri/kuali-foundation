174aab721eb100525ebc1eea13701536368240c4
jcaddel <jcaddel@kuali.org>
2014-02-02 02:18:18 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31395 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Instances.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Instances.java
@@ -6,10 +6,8 @@ import org.kuali.common.aws.ec2.api.EC2Service;
 import org.kuali.common.aws.ec2.impl.DefaultEC2Service;
 import org.kuali.common.aws.ec2.model.EC2ServiceContext;
 import org.kuali.common.devops.model.EC2Instance;
-import org.kuali.common.util.log.LoggerUtils;
 import org.kuali.common.util.wait.DefaultWaitService;
 import org.kuali.common.util.wait.WaitService;
-import org.slf4j.Logger;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.services.ec2.model.Instance;
@@ -19,18 +17,14 @@ import com.google.common.collect.Lists;
 
 public class Instances {
 
-	private static final Logger logger = LoggerUtils.make();
-
 	public static List<EC2Instance> getInstances(AWSCredentials creds) {
 		WaitService ws = new DefaultWaitService();
 		EC2ServiceContext context = EC2ServiceContext.create(creds);
 		EC2Service service = new DefaultEC2Service(context, ws);
-		List<Instance> list = Lists.newArrayList(service.getInstances());
-		List<EC2Instance> instances = Lists.newArrayList();
-		return instances;
+		return convert(service.getInstances());
 	}
 
-	protected List<EC2Instance> convert(List<Instance> instances) {
+	protected static List<EC2Instance> convert(List<Instance> instances) {
 		List<EC2Instance> list = Lists.newArrayList();
 		for (Instance instance : instances) {
 			list.add(convert(instance));
@@ -38,7 +32,7 @@ public class Instances {
 		return list;
 	}
 
-	protected EC2Instance convert(Instance instance) {
+	protected static EC2Instance convert(Instance instance) {
 		String id = instance.getInstanceId();
 		Optional<String> name = getName(instance);
 		String publicDnsName = instance.getPublicDnsName();
