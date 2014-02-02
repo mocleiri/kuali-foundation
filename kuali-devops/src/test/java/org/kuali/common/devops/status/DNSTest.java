5da0c5a7757acef52286f660992bfc9019c27f96
jcaddel <jcaddel@kuali.org>
2014-02-01 23:43:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31373 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/DNSTest.java
@@ -0,0 +1,19 @@
+package org.kuali.common.devops.status;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.kuali.common.devops.logic.DNS;
+import org.kuali.common.util.log.LoggerUtils;
+import org.slf4j.Logger;
+
+public class DNSTest {
+
+	private static final Logger logger = LoggerUtils.make();
+
+	@Test
+	public void test() {
+		Map<String, String> dns = DNS.getMap(false);
+		logger.info(String.format("dns entries -> %s", dns.size()));
+	}
+}
