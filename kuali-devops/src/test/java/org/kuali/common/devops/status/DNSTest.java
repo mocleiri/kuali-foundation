5bbcd001b866ef81f08b3121979c6cf39e75a60c
jcaddel <jcaddel@kuali.org>
2014-02-02 00:43:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31385 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/status/DNSTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/DNSTest.java
@@ -7,10 +7,6 @@ import org.kuali.common.devops.logic.DNS;
 import org.kuali.common.util.log.LoggerUtils;
 import org.slf4j.Logger;
 
-import com.google.common.collect.Maps;
-import com.google.common.collect.Multiset;
-import com.google.common.collect.TreeMultiset;
-
 public class DNSTest {
 
 	private static final Logger logger = LoggerUtils.make();
@@ -18,20 +14,6 @@ public class DNSTest {
 	@Test
 	public void test() {
 		Map<String, String> records = DNS.getCanonicalNameRecords(false);
-		logger.info(String.format("records -> %s", records.size()));
-		Multiset<String> multi = TreeMultiset.create();
-		for (String cname : records.values()) {
-			multi.add(cname);
-		}
-		Map<String, Integer> duplicates = Maps.newTreeMap();
-		for (String element : multi) {
-			int count = multi.count(element);
-			if (count > 1) {
-				duplicates.put(element, multi.count(element));
-			}
-		}
-		for (String duplicate : duplicates.keySet()) {
-			System.out.println(duplicate + " -> " + duplicates.get(duplicate));
-		}
+		logger.info(String.format("cname records -> %s", records.size()));
 	}
 }
