ca55829fa6aa96db50a8a3bb0e238038e8aff46c
jcaddel <jcaddel@kuali.org>
2014-02-02 18:14:29 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31452 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/csv/DefaultAdapter.java
@@ -0,0 +1,74 @@
+package org.kuali.common.util.csv;
+
+import org.springframework.core.convert.ConversionService;
+
+import com.google.common.base.Preconditions;
+
+public class DefaultAdapter<T> extends CsvAdapter<T> {
+
+	private final CsvAdapter<String> adapter;
+	private final ConversionService converter;
+	private final Class<T> targetType;
+
+	@Override
+	public String format(T instance) {
+		if (instance == null) {
+			return adapter.format(null);
+		} else {
+			String converted = converter.convert(instance, String.class);
+			return adapter.format(converted);
+		}
+	}
+
+	@Override
+	public T parse(String string) {
+		if (string == null) {
+			return null;
+		} else {
+			String parsed = adapter.parse(string);
+			T converted = converter.convert(parsed, targetType);
+			return converted;
+		}
+	}
+
+	private DefaultAdapter(Builder<T> builder) {
+		this.adapter = builder.adapter;
+		this.converter = builder.converter;
+		this.targetType = builder.targetType;
+	}
+
+	public static class Builder<T> implements org.kuali.common.util.build.Builder<DefaultAdapter<T>> {
+
+		private CsvAdapter<String> adapter;
+		private ConversionService converter;
+		private Class<T> targetType;
+
+		public Builder<T> adapter(CsvAdapter<String> adapter) {
+			this.adapter = adapter;
+			return this;
+		}
+
+		public Builder<T> converter(ConversionService converter) {
+			this.converter = converter;
+			return this;
+		}
+
+		public Builder<T> targetType(Class<T> targetType) {
+			this.targetType = targetType;
+			return this;
+		}
+
+		@Override
+		public DefaultAdapter<T> build() {
+			DefaultAdapter<T> instance = new DefaultAdapter<T>(this);
+			validate(instance);
+			return instance;
+		}
+
+		private static <T> void validate(DefaultAdapter<T> instance) {
+			Preconditions.checkNotNull(instance.adapter, "'adapter' cannot be null");
+			Preconditions.checkNotNull(instance.converter, "'converter' cannot be null");
+			Preconditions.checkNotNull(instance.targetType, "'targetType' cannot be null");
+		}
+	}
+}
