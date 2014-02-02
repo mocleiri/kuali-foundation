455d620822803ed6f1a506a26eef581e091a9fcc
jcaddel <jcaddel@kuali.org>
2014-02-02 14:57:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31441 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/ToCsvFunction.java
@@ -0,0 +1,52 @@
+package org.kuali.common.devops.logic;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.List;
+import java.util.SortedSet;
+
+import org.kuali.common.util.base.Replacer;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.common.collect.Table;
+
+public final class ToCsvFunction<R, C> implements Function<Table<? extends Comparable<R>, ? extends Comparable<C>, String>, List<String>> {
+
+	private final String nullToken = "${csv.null}";
+	private final String emptyToken = "${csv.empty}";
+	private final Replacer replacer = Replacer.builder().add("\r", "${csv.cr}").add("\n", "${csv.lf}").add(",", "${csv.comma}").build();
+	private final Joiner joiner = Joiner.on(',');
+
+	@Override
+	public List<String> apply(Table<? extends Comparable<R>, ? extends Comparable<C>, String> table) {
+		checkNotNull(table, "'table' cannot be null");
+		SortedSet<Comparable<R>> rowKeys = Sets.newTreeSet(table.rowKeySet());
+		SortedSet<Comparable<C>> colKeys = Sets.newTreeSet(table.columnKeySet());
+		List<String> lines = Lists.newArrayList();
+		for (Comparable<R> rowKey : rowKeys) {
+			List<String> tokens = Lists.newArrayList();
+			for (Comparable<C> colKey : colKeys) {
+				String tableValue = table.get(rowKey, colKey);
+				String token = getToken(tableValue);
+				tokens.add(token);
+			}
+			String joined = joiner.join(tokens);
+			lines.add(joined);
+		}
+		return lines;
+	}
+
+	protected String getToken(String tableValue) {
+		if (tableValue == null) {
+			return nullToken;
+		}
+		if (tableValue.equals("")) {
+			return emptyToken;
+		}
+		return replacer.replace(tableValue);
+	}
+
+}
