da4a97bc8b69e8f75f7798356c5832b5216d135c
jcaddel <jcaddel@kuali.org>
2014-02-01 22:28:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31353 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Instances.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Instances.java
@@ -67,8 +67,8 @@ public class Instances {
 		String env = getName(instance);
 		String dns = instance.getPublicDnsName();
 		String type = instance.getInstanceType();
-		long launchTimeMillis = instance.getLaunchTime().getTime();
-		return AwsRecord.builder().dns(dns).project(project).type(type).env(env).launchTimeMillis(launchTimeMillis).build();
+		long launchTime = instance.getLaunchTime().getTime();
+		return AwsRecord.builder().dns(dns).project(project).type(type).env(env).launchTime(launchTime).build();
 	}
 
 	public static Map<String, List<Instance>> getMap() {
