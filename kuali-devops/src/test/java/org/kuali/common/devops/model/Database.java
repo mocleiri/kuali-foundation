fb36a1540de27594c3370acb7eac64735b321f58
jcaddel <jcaddel@kuali.org>
2014-02-03 18:19:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31549 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/model/Database.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/model/Database.java
@@ -1,32 +1,82 @@
 package org.kuali.common.devops.model;
 
-public class Database {
+import org.kuali.common.util.build.ValidatingBuilder;
+import org.kuali.common.util.validate.IdiotProofImmutable;
 
-	String vendor = "na";
-	String url = "na";
-	String username = "na";
+@IdiotProofImmutable
+public final class Database {
+
+	private final String vendor;
+	private final String url;
+	private final String username;
+
+	private Database(Builder builder) {
+		this.vendor = builder.vendor;
+		this.url = builder.url;
+		this.username = builder.username;
+	}
+
+	public static class Builder extends ValidatingBuilder<Database> {
+
+		private String vendor;
+		private String url;
+		private String username;
+
+		public Builder vendor(String vendor) {
+			this.vendor = vendor;
+			return this;
+		}
+
+		public Builder url(String url) {
+			this.url = url;
+			return this;
+		}
+
+		public Builder username(String username) {
+			this.username = username;
+			return this;
+		}
+
+		@Override
+		public Database getInstance() {
+			return new Database(this);
+		}
+
+		public String getVendor() {
+			return vendor;
+		}
+
+		public void setVendor(String vendor) {
+			this.vendor = vendor;
+		}
+
+		public String getUrl() {
+			return url;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+	}
 
 	public String getVendor() {
 		return vendor;
-	}
-
-	public void setVendor(String vendor) {
-		this.vendor = vendor;
 	}
 
 	public String getUrl() {
 		return url;
 	}
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
 	public String getUsername() {
 		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
 	}
 }
