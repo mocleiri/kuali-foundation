625021ce5760ff36af304ffc5c341c5734a40123
jcaddel <jcaddel@kuali.org>
2014-02-02 23:57:13 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31463 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/ToCsvFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/ToCsvFunction.java
@@ -5,10 +5,12 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.util.List;
 import java.util.SortedSet;
 
-import org.kuali.common.util.csv.DefaultAdapter;
+import org.kuali.common.util.csv.BasicStringAdapter;
+import org.kuali.common.util.csv.CsvAdapter;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -24,18 +26,35 @@ public final class ToCsvFunction<R, C> implements Function<Table<? extends Compa
 		SortedSet<Comparable<R>> rowKeys = Sets.newTreeSet(table.rowKeySet());
 		SortedSet<Comparable<C>> colKeys = Sets.newTreeSet(table.columnKeySet());
 		List<String> lines = Lists.newArrayList();
+		lines.add(getHeader(colKeys));
 		for (Comparable<R> rowKey : rowKeys) {
 			List<String> tokens = Lists.newArrayList();
 			for (Comparable<C> colKey : colKeys) {
 				Object value = table.get(rowKey, colKey);
-				DefaultAdapter adapter = DefaultAdapter.create(value.getClass());
-				String string = adapter.format(value);
+				if (value instanceof Optional) {
+					Optional<?> optional = (Optional<?>) value;
+					value = optional.orNull();
+				}
+				CsvAdapter<String> adapter = BasicStringAdapter.create();
+				String s = null;
+				if (value != null) {
+					s = value.toString();
+				}
+				String string = adapter.format(s);
 				tokens.add(string);
 			}
 			String joined = joiner.join(tokens);
 			lines.add(joined);
 		}
 		return ImmutableList.copyOf(lines);
+	}
+
+	protected String getHeader(SortedSet<Comparable<C>> colKeys) {
+		List<String> tokens = Lists.newArrayList();
+		for (Comparable<C> colKey : colKeys) {
+			tokens.add(colKey.toString());
+		}
+		return joiner.join(tokens);
 	}
 
 }
