24ba55f1c7f760ef98239d1bf255f2891811eb09
jcaddel <jcaddel@kuali.org>
2014-02-03 12:28:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31494 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/model/TableCellDescriptor.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/model/TableCellDescriptor.java
@@ -6,53 +6,72 @@ import java.lang.reflect.Field;
 
 import com.google.common.base.Optional;
 
-public final class TableCellDescriptor {
+public final class TableCellDescriptor<T> {
 
 	private final Field field;
-	private final Optional<?> fieldValue;
+	private final Optional<T> fieldValue;
 
-	private TableCellDescriptor(Builder builder) {
+	private TableCellDescriptor(Builder<T> builder) {
 		this.field = builder.field;
 		this.fieldValue = builder.fieldValue;
 	}
 
-	public static TableCellDescriptor create(Field field, Optional<?> fieldValue) {
-		return builder().field(field).fieldValue(fieldValue).build();
+	public static <T> TableCellDescriptor<T> create(Field field, Optional<T> fieldValue) {
+		Builder<T> builder = builder();
+		builder.setField(field);
+		builder.setFieldValue(fieldValue);
+		return builder.build();
 	}
 
-	public static Builder builder() {
-		return new Builder();
+	public static <T> Builder<T> builder() {
+		return new Builder<T>();
 	}
 
-	public static class Builder implements org.kuali.common.util.build.Builder<TableCellDescriptor> {
+	public static class Builder<T> implements org.kuali.common.util.build.Builder<TableCellDescriptor<T>> {
 
 		private Field field;
-		private Optional<?> fieldValue;
+		private Optional<T> fieldValue;
 
-		public Builder field(Field field) {
+		public Builder<T> field(Field field) {
 			this.field = field;
 			return this;
 		}
 
-		public Builder fieldValue(Optional<?> fieldValue) {
+		public Builder<T> fieldValue(Optional<T> fieldValue) {
 			this.fieldValue = fieldValue;
 			return this;
 		}
 
-		public Builder fieldValue(Object object) {
+		public Builder<T> fieldValue(Object object) {
 			return fieldValue(Optional.of(object));
 		}
 
 		@Override
-		public TableCellDescriptor build() {
-			TableCellDescriptor instance = new TableCellDescriptor(this);
+		public TableCellDescriptor<T> build() {
+			TableCellDescriptor<T> instance = new TableCellDescriptor<T>(this);
 			validate(instance);
 			return instance;
 		}
 
-		private static void validate(TableCellDescriptor instance) {
+		private static <T> void validate(TableCellDescriptor<T> instance) {
 			checkNotNull(instance.field, "'field' cannot be null");
-			checkNotNull(instance.fieldValue, "'value' cannot be null");
+			checkNotNull(instance.fieldValue, "'fieldValue' cannot be null");
+		}
+
+		public Field getField() {
+			return field;
+		}
+
+		public void setField(Field field) {
+			this.field = field;
+		}
+
+		public Optional<T> getFieldValue() {
+			return fieldValue;
+		}
+
+		public void setFieldValue(Optional<T> fieldValue) {
+			this.fieldValue = fieldValue;
 		}
 	}
 
@@ -60,7 +79,7 @@ public final class TableCellDescriptor {
 		return field;
 	}
 
-	public Optional<?> getFieldValue() {
+	public Optional<T> getFieldValue() {
 		return fieldValue;
 	}
 
