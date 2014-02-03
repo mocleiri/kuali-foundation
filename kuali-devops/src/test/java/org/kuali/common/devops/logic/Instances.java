2aa1f79847822a9f14afd97626fc5d0302840610
jcaddel <jcaddel@kuali.org>
2014-02-03 17:21:49 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31529 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Instances.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Instances.java
@@ -92,8 +92,8 @@ public class Instances {
 	protected static void store(File file, List<EC2Instance> instances) {
 		Table<Integer, String, TableCellDescriptor<Object>> table = Tables.getTable(instances, EC2Instance.class);
 		ToCsvFunction<Integer, String> function = new ToCsvFunction<Integer, String>();
-		List<String> csv = function.apply(table);
-		store(file, csv, ENCODING);
+		List<String> lines = function.apply(table);
+		store(file, lines, ENCODING);
 	}
 
 	protected static void store(File file, List<String> lines, String encoding) {
