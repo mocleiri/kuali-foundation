29d49c6e0328aec80e0d6468b2968252aeefe066
jcaddel <jcaddel@kuali.org>
2014-02-03 00:22:46 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31470 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/convert/DefaultConversionService.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/convert/DefaultConversionService.java
@@ -3,9 +3,12 @@ package org.kuali.common.util.spring.convert;
 import org.kohsuke.MetaInfServices;
 import org.kuali.common.util.spring.format.BytesFormatAnnotationFormatterFactory;
 import org.kuali.common.util.spring.format.CanonicalFileFormatAnnotationFormatterFactory;
+import org.kuali.common.util.spring.format.OptionalStringFormatter;
 import org.kuali.common.util.spring.format.TimeFormatAnnotationFormatterFactory;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.format.support.DefaultFormattingConversionService;
+
+import com.google.common.base.Optional;
 
 @MetaInfServices(ConversionService.class)
 public class DefaultConversionService extends DefaultFormattingConversionService {
@@ -14,7 +17,7 @@ public class DefaultConversionService extends DefaultFormattingConversionService
 		addFormatterForFieldAnnotation(new BytesFormatAnnotationFormatterFactory());
 		addFormatterForFieldAnnotation(new TimeFormatAnnotationFormatterFactory());
 		addFormatterForFieldAnnotation(new CanonicalFileFormatAnnotationFormatterFactory());
-		addFormatterForFieldType(fieldType, formatter)
+		addFormatterForFieldType(Optional.class, new OptionalStringFormatter());
 	}
 
 }
