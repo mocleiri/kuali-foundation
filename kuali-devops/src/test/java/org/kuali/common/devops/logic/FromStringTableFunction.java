33c63e0be2e5e8683d226b773025bce040526196
jcaddel <jcaddel@kuali.org>
2014-02-03 13:55:24 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31505 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/FromStringTableFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/FromStringTableFunction.java
@@ -3,18 +3,16 @@ package org.kuali.common.devops.logic;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.lang.reflect.Field;
-import java.text.ParseException;
 import java.util.List;
 import java.util.Locale;
 import java.util.SortedSet;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.kuali.common.devops.model.TableCellDescriptor;
 import org.kuali.common.util.ReflectionUtils;
 import org.kuali.common.util.spring.convert.DefaultConversionService;
-import org.kuali.common.util.spring.format.CsvStringFormatter;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.TypeDescriptor;
-import org.springframework.format.Formatter;
 
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
@@ -23,16 +21,15 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Table;
 
-public final class FromCsvFunction<R, C, V> implements Function<Table<? extends Comparable<R>, ? extends Comparable<C>, TableCellDescriptor>, List<V>> {
+public final class FromStringTableFunction<R, C, V> implements Function<Table<? extends Comparable<R>, ? extends Comparable<C>, TableCellDescriptor<String>>, List<V>> {
 
 	private final Class<V> targetType;
 	private final ConversionService converter;
-	private final Formatter<String> formatter;
 	private final TypeDescriptor sourceType;
 	private final Locale locale;
 
 	@Override
-	public List<V> apply(Table<? extends Comparable<R>, ? extends Comparable<C>, TableCellDescriptor> table) {
+	public List<V> apply(Table<? extends Comparable<R>, ? extends Comparable<C>, TableCellDescriptor<String>> table) {
 		try {
 			checkNotNull(table, "'table' cannot be null");
 			SortedSet<Comparable<R>> rowKeys = Sets.newTreeSet(table.rowKeySet());
@@ -43,17 +40,13 @@ public final class FromCsvFunction<R, C, V> implements Function<Table<? extends 
 			for (Comparable<R> rowKey : rowKeys) {
 				org.kuali.common.util.build.Builder<V> builder = ReflectionUtils.newInstance(builderClass);
 				for (Comparable<C> colKey : colKeys) {
-					TableCellDescriptor descriptor = table.get(rowKey, colKey);
+					TableCellDescriptor<String> descriptor = table.get(rowKey, colKey);
 					Field originalField = descriptor.getField();
 					TypeDescriptor targetType = new TypeDescriptor(originalField);
-					Optional<?> value = descriptor.getFieldValue();
-					if (value.get().toString().equals("${csv.null}")) {
-						System.out.println("yo");
-					}
-					String formatted = format(value.get().toString(), locale);
-					Object converted = converter.convert(formatted, sourceType, targetType);
+					Optional<String> value = descriptor.getFieldValue();
+					Object converted = converter.convert(value.orNull(), sourceType, targetType);
 					Field builderField = builderClass.getDeclaredField(originalField.getName());
-					ReflectionUtils.set(builder, builderField, converted);
+					PropertyUtils.setProperty(builder, builderField.getName(), converted);
 				}
 				V element = builder.build();
 				elements.add(element);
@@ -64,28 +57,17 @@ public final class FromCsvFunction<R, C, V> implements Function<Table<? extends 
 		}
 	}
 
-	protected String format(String string, Locale locale) {
-		try {
-			return formatter.parse(string, locale);
-		} catch (ParseException e) {
-			throw Exceptions.illegalState(e, "unexpected parse error -> [%s]", string);
-		}
-
-	}
-
-	private FromCsvFunction(Builder<R, C, V> builder) {
+	private FromStringTableFunction(Builder<R, C, V> builder) {
 		this.targetType = builder.targetType;
 		this.converter = builder.converter;
-		this.formatter = builder.formatter;
 		this.sourceType = builder.sourceType;
 		this.locale = builder.locale;
 	}
 
-	public static class Builder<R, C, V> implements org.kuali.common.util.build.Builder<FromCsvFunction<R, C, V>> {
+	public static class Builder<R, C, V> implements org.kuali.common.util.build.Builder<FromStringTableFunction<R, C, V>> {
 
 		private Class<V> targetType;
 		private ConversionService converter = new DefaultConversionService();
-		private Formatter<String> formatter = CsvStringFormatter.create();
 		private TypeDescriptor sourceType = TypeDescriptor.valueOf(String.class);
 		private Locale locale = Locale.getDefault();
 
@@ -96,11 +78,6 @@ public final class FromCsvFunction<R, C, V> implements Function<Table<? extends 
 
 		public Builder<R, C, V> converter(ConversionService converter) {
 			this.converter = converter;
-			return this;
-		}
-
-		public Builder<R, C, V> formatter(Formatter<String> formatter) {
-			this.formatter = formatter;
 			return this;
 		}
 
@@ -115,16 +92,15 @@ public final class FromCsvFunction<R, C, V> implements Function<Table<? extends 
 		}
 
 		@Override
-		public FromCsvFunction<R, C, V> build() {
-			FromCsvFunction<R, C, V> instance = new FromCsvFunction<R, C, V>(this);
+		public FromStringTableFunction<R, C, V> build() {
+			FromStringTableFunction<R, C, V> instance = new FromStringTableFunction<R, C, V>(this);
 			validate(instance);
 			return instance;
 		}
 
-		private static <R, C, V> void validate(FromCsvFunction<R, C, V> instance) {
+		private static <R, C, V> void validate(FromStringTableFunction<R, C, V> instance) {
 			checkNotNull(instance.targetType, "'targetType' cannot be null");
 			checkNotNull(instance.converter, "'converter' cannot be null");
-			checkNotNull(instance.formatter, "'formatter' cannot be null");
 			checkNotNull(instance.sourceType, "'sourceType' cannot be null");
 			checkNotNull(instance.locale, "'locale' cannot be null");
 		}
