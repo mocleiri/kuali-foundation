1de0422672b4ba319f5574755e5e8fd2a06bda4a
jcaddel <jcaddel@kuali.org>
2014-02-03 18:25:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31550 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/model/Environment.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/model/Environment.java
@@ -2,84 +2,178 @@ package org.kuali.common.devops.model;
 
 import java.util.Comparator;
 
+import org.kuali.common.util.build.ValidatingBuilder;
+import org.kuali.common.util.validate.IdiotProofImmutable;
+
 import com.google.common.base.Optional;
 
-public class Environment implements Comparable<Environment> {
+@IdiotProofImmutable
+public final class Environment implements Comparable<Environment> {
 
 	private static final Comparator<String> COMPARATOR = new EnvStringComparator();
 
-	String project;
-	String id;
-	String fqdn;
-	EC2Instance instance;
-	Tomcat tomcat;
-	String java;
-	Optional<Application> application;
+	private final String group;
+	private final String name;
+	private final String fqdn;
+	private final EC2Instance server;
+	private final Tomcat tomcat;
+	private final String java;
+	private final Optional<Application> application;
 
 	@Override
 	public int compareTo(Environment other) {
-		int compare = project.compareTo(other.getProject());
+		int compare = group.compareTo(other.getGroup());
 		if (compare != 0) {
 			return compare;
 		} else {
-			return COMPARATOR.compare(id, other.getId());
+			return COMPARATOR.compare(name, other.getName());
 		}
 	}
 
-	public String getProject() {
-		return project;
+	private Environment(Builder builder) {
+		this.group = builder.group;
+		this.name = builder.name;
+		this.fqdn = builder.fqdn;
+		this.server = builder.server;
+		this.tomcat = builder.tomcat;
+		this.java = builder.java;
+		this.application = builder.application;
 	}
 
-	public void setProject(String project) {
-		this.project = project;
+	public static class Builder extends ValidatingBuilder<Environment> {
+
+		private String group;
+		private String name;
+		private String fqdn;
+		private EC2Instance server;
+		private Tomcat tomcat;
+		private String java;
+		private Optional<Application> application;
+
+		public Builder group(String group) {
+			this.group = group;
+			return this;
+		}
+
+		public Builder name(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder fqdn(String fqdn) {
+			this.fqdn = fqdn;
+			return this;
+		}
+
+		public Builder server(EC2Instance server) {
+			this.server = server;
+			return this;
+		}
+
+		public Builder tomcat(Tomcat tomcat) {
+			this.tomcat = tomcat;
+			return this;
+		}
+
+		public Builder java(String java) {
+			this.java = java;
+			return this;
+		}
+
+		public Builder application(Optional<Application> application) {
+			this.application = application;
+			return this;
+		}
+
+		@Override
+		public Environment getInstance() {
+			return new Environment(this);
+		}
+
+		public String getGroup() {
+			return group;
+		}
+
+		public void setGroup(String group) {
+			this.group = group;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getFqdn() {
+			return fqdn;
+		}
+
+		public void setFqdn(String fqdn) {
+			this.fqdn = fqdn;
+		}
+
+		public EC2Instance getServer() {
+			return server;
+		}
+
+		public void setServer(EC2Instance server) {
+			this.server = server;
+		}
+
+		public Tomcat getTomcat() {
+			return tomcat;
+		}
+
+		public void setTomcat(Tomcat tomcat) {
+			this.tomcat = tomcat;
+		}
+
+		public String getJava() {
+			return java;
+		}
+
+		public void setJava(String java) {
+			this.java = java;
+		}
+
+		public Optional<Application> getApplication() {
+			return application;
+		}
+
+		public void setApplication(Optional<Application> application) {
+			this.application = application;
+		}
+
 	}
 
-	public String getId() {
-		return id;
+	public String getGroup() {
+		return group;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public String getName() {
+		return name;
 	}
 
 	public String getFqdn() {
 		return fqdn;
 	}
 
-	public void setFqdn(String dns) {
-		this.fqdn = dns;
-	}
-
-	public String getJava() {
-		return java;
-	}
-
-	public void setJava(String java) {
-		this.java = java;
+	public EC2Instance getServer() {
+		return server;
 	}
 
 	public Tomcat getTomcat() {
 		return tomcat;
 	}
 
-	public void setTomcat(Tomcat tomcat) {
-		this.tomcat = tomcat;
+	public String getJava() {
+		return java;
 	}
 
 	public Optional<Application> getApplication() {
 		return application;
-	}
-
-	public void setApplication(Optional<Application> application) {
-		this.application = application;
-	}
-
-	public EC2Instance getInstance() {
-		return instance;
-	}
-
-	public void setInstance(EC2Instance instance) {
-		this.instance = instance;
 	}
 
 }
