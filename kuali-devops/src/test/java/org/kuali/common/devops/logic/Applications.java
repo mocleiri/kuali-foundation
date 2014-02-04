70e211b686a90c43029f2bf11eeb84c77faaae05
jcaddel <jcaddel@kuali.org>
2014-02-03 21:49:31 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31583 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Applications.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Applications.java
@@ -1,13 +1,20 @@
 package org.kuali.common.devops.logic;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.kuali.common.devops.model.Environment;
 import org.kuali.common.util.LocationUtils;
 import org.kuali.common.util.PropertyUtils;
 import org.kuali.common.util.Str;
 import org.kuali.common.util.log.Loggers;
+import org.kuali.common.util.project.KualiProjectConstants;
+import org.kuali.common.util.project.ProjectUtils;
+import org.kuali.common.util.project.model.Project;
+import org.kuali.common.util.properties.rice.RiceLoader;
 import org.slf4j.Logger;
 
 import com.google.common.base.Optional;
@@ -16,6 +23,44 @@ import com.google.common.base.Splitter;
 public class Applications extends Examiner {
 
 	private static final Logger logger = Loggers.make();
+
+	public static Optional<Project> getProject(String fqdn) {
+		Map<String, String> manifest = Manifests.getManifest(fqdn);
+		Properties properties = getProjectProperties(fqdn, manifest);
+		if (properties.getProperty("project.artifactId") == null) {
+			return Optional.absent();
+		} else {
+			return Optional.of(ProjectUtils.getProject(properties));
+		}
+	}
+
+	protected static Properties getConfig(Environment env) {
+		if (!env.getApplication().isPresent()) {
+			return new Properties();
+		}
+		String fragment = getConfigFragment(env);
+		String location = PROTOCOL + env.getFqdn() + fragment;
+		try {
+			return RiceLoader.load(location);
+		} catch (Exception e) {
+			logger.info("error loading [%s] -> [%s]", location, e.getMessage());
+			return new Properties();
+		}
+	}
+
+	protected static String getConfigFragment(Environment env) {
+		Project project = env.getApplication().get().getProject();
+		String groupId = project.getGroupId();
+		if (groupId.equals(KualiProjectConstants.STUDENT_GROUP_ID)) {
+			return "/home/kuali/main/dev/" + project.getArtifactId() + "-config.xml";
+		} else if (groupId.equals(KualiProjectConstants.OLE_GROUP_ID)) {
+			Optional<String> environment = getSystemProperty(env.getFqdn(), "environment");
+			checkState(environment.isPresent(), "could not locate system property -> [%s]", environment);
+			return "/home/kuali/main/" + environment + "/common-config.xml";
+		} else {
+			return "/home/kuali/main/dev/common-config.xml";
+		}
+	}
 
 	public static Properties getProjectProperties(String fqdn, Map<String, String> manifest) {
 		Optional<String> bundleSymbolicName = Optional.fromNullable(manifest.get("Bundle-SymbolicName"));
