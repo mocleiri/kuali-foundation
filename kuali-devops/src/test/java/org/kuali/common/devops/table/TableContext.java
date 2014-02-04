5b46ff39b94f2cf37a7c40ae3e1dfc75f1c44d07
jcaddel <jcaddel@kuali.org>
2014-02-04 02:11:37 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31641 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/table/TableContext.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/table/TableContext.java
@@ -1,9 +1,12 @@
 package org.kuali.common.devops.table;
 
+import com.google.common.base.Optional;
+
 public class TableContext {
 
 	private int indent;
 	private boolean headers;
+	private Optional<Integer> border = Optional.of(1);
 
 	public int getIndent() {
 		return indent;
@@ -19,6 +22,14 @@ public class TableContext {
 
 	public void setHeaders(boolean headers) {
 		this.headers = headers;
+	}
+
+	public Optional<Integer> getBorder() {
+		return border;
+	}
+
+	public void setBorder(Optional<Integer> border) {
+		this.border = border;
 	}
 
 }
