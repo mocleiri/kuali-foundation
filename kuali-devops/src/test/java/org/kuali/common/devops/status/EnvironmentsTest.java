ac3f9d061dc3a2c4f288cbcede09f2541ce374b4
jcaddel <jcaddel@kuali.org>
2014-02-03 23:11:08 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31600 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/status/EnvironmentsTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/EnvironmentsTest.java
@@ -1,24 +1,41 @@
 package org.kuali.common.devops.status;
 
+import static java.lang.String.format;
+
+import java.io.File;
 import java.util.List;
 import java.util.SortedMap;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.kuali.common.devops.logic.Environments;
 import org.kuali.common.devops.logic.Environments2;
 import org.kuali.common.devops.model.Environment;
+import org.kuali.common.util.file.CanonicalFile;
 import org.kuali.common.util.log.Loggers;
 import org.kuali.common.util.validate.Validation;
 import org.slf4j.Logger;
 
+import com.google.common.collect.Table;
+
 public class EnvironmentsTest {
 
 	private static final Logger logger = Loggers.make();
+	private static final File HTML_DIR = new CanonicalFile("./target/envs");
 
 	@Test
 	public void test() {
 		try {
 			Validation.getDefaultValidator();
 			SortedMap<String, List<Environment>> maps = Environments2.getEnvironments(false);
+			for (String group : maps.keySet()) {
+				List<Environment> envs = maps.get(group);
+				Table<Integer, Integer, String> table = Environments.getTable(envs);
+				String html = Environments.html(table);
+				File outputFile = new CanonicalFile(HTML_DIR, group + ".htm");
+				FileUtils.write(outputFile, html);
+				logger.info(format("created -> %s", outputFile));
+			}
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
