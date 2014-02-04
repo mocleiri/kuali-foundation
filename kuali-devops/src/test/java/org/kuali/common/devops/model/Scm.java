fa1e06792a807a2249ec61ae1293a1faa26133c7
jcaddel <jcaddel@kuali.org>
2014-02-03 22:36:42 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31591 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/model/Scm.java
@@ -0,0 +1,54 @@
+package org.kuali.common.devops.model;
+
+import org.kuali.common.util.build.ValidatingBuilder;
+import org.kuali.common.util.validate.IdiotProofImmutable;
+
+@IdiotProofImmutable
+public final class Scm {
+
+	private final String url;
+	private final String revision;
+
+	private Scm(Builder builder) {
+		this.url = builder.url;
+		this.revision = builder.revision;
+	}
+
+	public static Scm create(String url, String revision) {
+		return builder().url(url).revision(revision).build();
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static class Builder extends ValidatingBuilder<Scm> {
+
+		private String url;
+		private String revision;
+
+		public Builder url(String url) {
+			this.url = url;
+			return this;
+		}
+
+		public Builder revision(String revision) {
+			this.revision = revision;
+			return this;
+		}
+
+		@Override
+		public Scm getInstance() {
+			return new Scm(this);
+		}
+
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getRevision() {
+		return revision;
+	}
+}
