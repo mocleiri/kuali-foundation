d0fe0ae58a551dfd758606f938cd91ff156baf61
jcaddel <jcaddel@kuali.org>
2014-02-03 19:34:32 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31562 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Exceptions.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Exceptions.java
@@ -1,6 +1,5 @@
 package org.kuali.common.devops.logic;
 
-import java.lang.reflect.Constructor;
 
 public class Exceptions {
 
@@ -9,30 +8,34 @@ public class Exceptions {
 	}
 
 	public static IllegalStateException illegalState(String msg) {
-		return illegalState(msg, (Object[]) null);
+		return new IllegalStateException(msg);
 	}
 
 	public static IllegalStateException illegalState(String msg, Object... args) {
-		return create(IllegalStateException.class, msg, args);
+		return new IllegalStateException(formattedMessage(msg, args));
 	}
 
 	public static IllegalStateException illegalState(Throwable cause, String msg, Object... args) {
-		return create(IllegalStateException.class, cause, msg, args);
+		return new IllegalStateException(formattedMessage(msg, args), cause);
+	}
+
+	public static IllegalArgumentException illegalArgument(Throwable cause) {
+		return new IllegalArgumentException(cause);
 	}
 
 	public static IllegalArgumentException illegalArgument(String msg) {
-		return illegalArgument(msg, (Object[]) null);
+		return new IllegalArgumentException(msg);
 	}
 
 	public static IllegalArgumentException illegalArgument(String msg, Object... args) {
-		return create(IllegalArgumentException.class, msg, args);
+		return new IllegalArgumentException(formattedMessage(msg, args));
 	}
 
 	public static IllegalArgumentException illegalArgument(Throwable cause, String msg, Object... args) {
-		return create(IllegalArgumentException.class, cause, msg, args);
+		return new IllegalArgumentException(formattedMessage(msg, args), cause);
 	}
 
-	protected static String formatMessage(String msg, Object... args) {
+	protected static String formattedMessage(String msg, Object... args) {
 		if (args == null || args.length == 0) {
 			return msg;
 		} else {
@@ -40,28 +43,4 @@ public class Exceptions {
 		}
 	}
 
-	public static <T extends Exception> T create(Class<T> type, String msg) {
-		return create(type, msg, (Object[]) null);
-	}
-
-	public static <T extends Exception> T create(Class<T> type, String msg, Object... args) {
-		Class<?>[] parameterTypes = { String.class };
-		Object[] initArgs = { formatMessage(msg, args) };
-		return create(type, parameterTypes, initArgs);
-	}
-
-	public static <T extends Exception> T create(Class<T> type, Throwable cause, String msg, Object... args) {
-		Class<?>[] parameterTypes = { String.class, Throwable.class };
-		Object[] initArgs = { formatMessage(msg, args), cause };
-		return create(type, parameterTypes, initArgs);
-	}
-
-	public static <T extends Exception> T create(Class<T> type, Class<?>[] parameterTypes, Object[] initArgs) {
-		try {
-			Constructor<T> constructor = type.getConstructor(parameterTypes);
-			return constructor.newInstance(initArgs);
-		} catch (Exception e) {
-			throw new IllegalStateException(e);
-		}
-	}
 }
