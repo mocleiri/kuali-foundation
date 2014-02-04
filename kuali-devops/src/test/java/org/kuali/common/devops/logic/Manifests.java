680c2457c6ad77371c86372cb36ffe291045645a
jcaddel <jcaddel@kuali.org>
2014-02-03 21:14:55 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31578 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Manifests.java
@@ -0,0 +1,52 @@
+package org.kuali.common.devops.logic;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+import org.apache.commons.io.IOUtils;
+import org.kuali.common.util.LocationUtils;
+import org.kuali.common.util.log.Loggers;
+import org.slf4j.Logger;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+public class Manifests extends Examiner {
+
+	private static final Logger logger = Loggers.make();
+
+	public static Map<String, String> getManifest(String fqdn) {
+		String fragment = "/tomcat/webapps/ROOT/META-INF/MANIFEST.MF";
+		String location = PROTOCOL + fqdn + fragment;
+		Map<String, String> map = Maps.newHashMap();
+		InputStream in = null;
+		try {
+			in = LocationUtils.getInputStream(location);
+			Manifest manifest = new Manifest(in);
+			Attributes attributes = manifest.getMainAttributes();
+			SortedSet<String> keys = getKeys(attributes);
+			for (String key : keys) {
+				String value = attributes.getValue(key);
+				map.put(key, value);
+			}
+		} catch (IOException e) {
+			logger.debug(String.format("error getting manifest -> [%s]", location));
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+		return map;
+	}
+
+	protected static SortedSet<String> getKeys(Attributes attributes) {
+		SortedSet<String> keys = Sets.newTreeSet();
+		for (Object key : attributes.keySet()) {
+			keys.add(key.toString());
+		}
+		return keys;
+	}
+
+}
