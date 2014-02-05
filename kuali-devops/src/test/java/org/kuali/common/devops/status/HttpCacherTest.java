51efeae210eb9a7491496609eb572121a77933b7
jcaddel <jcaddel@kuali.org>
2014-02-05 17:07:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31812 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/status/HttpCacherTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/HttpCacherTest.java
@@ -1,11 +1,18 @@
 package org.kuali.common.devops.status;
 
-import java.io.File;
+import static com.google.common.base.Preconditions.checkState;
+
+import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
 
 import org.junit.Test;
-import org.kuali.common.devops.logic.HttpCacher;
+import org.kuali.common.devops.logic.Environments2;
+import org.kuali.common.devops.model.Environment;
 import org.kuali.common.util.log.LoggerUtils;
 import org.slf4j.Logger;
+
+import com.google.common.collect.Sets;
 
 public class HttpCacherTest {
 
@@ -14,11 +21,23 @@ public class HttpCacherTest {
 	@Test
 	public void test() {
 		try {
-			String url = "http://env1.ks.kuali.org/tomcat/logs/env.jsp";
-			File file = HttpCacher.cache(url);
-			logger.info(String.format("file -> %s", file));
+			SortedMap<String, List<Environment.Builder>> map = Environments2.getBuilders(false);
+			Set<String> fqdns = getFqdns(map);
+			logger.info(String.format("fqdns=%s", fqdns.size()));
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
 	}
+
+	protected Set<String> getFqdns(SortedMap<String, List<Environment.Builder>> map) {
+		Set<String> strings = Sets.newHashSet();
+		for (List<Environment.Builder> builders : map.values()) {
+			for (Environment.Builder builder : builders) {
+				String fqdn = builder.getFqdn();
+				checkState(strings.add(fqdn), "duplicate fqdn -> %s", fqdn);
+			}
+		}
+		return strings;
+	}
+
 }
