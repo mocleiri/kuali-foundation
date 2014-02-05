152019630e603d5a78b4967310bb6583bfaafd33
jcaddel <jcaddel@kuali.org>
2014-02-05 15:16:09 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31788 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Http.java
@@ -0,0 +1,17 @@
+package org.kuali.common.devops.logic;
+
+import org.kuali.common.http.model.HttpContext;
+
+import com.google.common.base.Optional;
+
+public class Http {
+	
+	private static final HttpContext
+	private static final HttpService SERVICE = 
+	
+	
+	public static Optional<String> getContent(String url) {
+		
+	}
+
+}
