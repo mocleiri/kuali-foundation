d715609f0fa4dcfbf570fcf82574e40357228bed
jcaddel <jcaddel@kuali.org>
2014-02-05 15:39:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31795 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Http.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Http.java
@@ -1,17 +1,25 @@
 package org.kuali.common.devops.logic;
 
 import org.kuali.common.http.model.HttpContext;
+import org.kuali.common.http.model.HttpStatus;
+import org.kuali.common.http.model.HttpWaitResult;
+import org.kuali.common.http.service.DefaultHttpService;
+import org.kuali.common.http.service.HttpService;
 
 import com.google.common.base.Optional;
 
 public class Http {
-	
-	private static final HttpContext
-	private static final HttpService SERVICE = 
-	
-	
+
+	private static final HttpService SERVICE = new DefaultHttpService();
+
 	public static Optional<String> getContent(String url) {
-		
+		HttpContext context = HttpContext.builder(url).overallTimeout("10s").requestTimeout("3s").quiet(true).build();
+		HttpWaitResult result = SERVICE.wait(context);
+		if (result.getStatus().equals(HttpStatus.SUCCESS)) {
+			return result.getFinalRequestResult().getResponseBody();
+		} else {
+			return Optional.absent();
+		}
 	}
 
 }
