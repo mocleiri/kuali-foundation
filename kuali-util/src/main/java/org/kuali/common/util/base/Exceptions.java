6edb9ecc9c532efcb3a1ca7350543f783b50e4af
jcaddel <jcaddel@kuali.org>
2014-02-05 10:51:01 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31731 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/main/java/org/kuali/common/util/base/Exceptions.java
@@ -0,0 +1,60 @@
+package org.kuali.common.util.base;
+
+/**
+ * <p>
+ * Create {@code IllegalStateException's} and {@code IllegaArgumentException's} with richly formatted error messages.
+ * </p>
+ * 
+ * Typical usage:
+ * 
+ * <pre>
+ * try {
+ *   ...
+ * } catch (SomeException e) {
+ *   throw Exceptions.illegalArg(e, &quot;[%s] must be between %s and %s&quot; "port", 0, 65535);
+ * }
+ * </pre>
+ */
+public class Exceptions {
+
+	public static IllegalStateException illegalState(Throwable cause) {
+		return new IllegalStateException(cause);
+	}
+
+	public static IllegalStateException illegalState(String msg) {
+		return new IllegalStateException(msg);
+	}
+
+	public static IllegalStateException illegalState(String msg, Object... args) {
+		return new IllegalStateException(formattedMessage(msg, args));
+	}
+
+	public static IllegalStateException illegalState(Throwable cause, String msg, Object... args) {
+		return new IllegalStateException(formattedMessage(msg, args), cause);
+	}
+
+	public static IllegalArgumentException illegalArg(Throwable cause) {
+		return new IllegalArgumentException(cause);
+	}
+
+	public static IllegalArgumentException illegalArg(String msg) {
+		return new IllegalArgumentException(msg);
+	}
+
+	public static IllegalArgumentException illegalArg(String msg, Object... args) {
+		return new IllegalArgumentException(formattedMessage(msg, args));
+	}
+
+	public static IllegalArgumentException illegalArg(Throwable cause, String msg, Object... args) {
+		return new IllegalArgumentException(formattedMessage(msg, args), cause);
+	}
+
+	protected static String formattedMessage(String msg, Object... args) {
+		if (args == null || args.length == 0) {
+			return msg;
+		} else {
+			return String.format(msg, args);
+		}
+	}
+
+}
