3e8f56e96bb6ef13ab6c2fa34cb45314e8946c95
jcaddel <jcaddel@kuali.org>
2014-02-05 12:32:01 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31742 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-http/src/main/java/org/kuali/common/http/model/HttpContext.java
+++ kuali-http/src/main/java/org/kuali/common/http/model/HttpContext.java
@@ -15,10 +15,12 @@
  */
 package org.kuali.common.http.model;
 
-import java.util.List;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.kuali.common.util.FormatUtils.getMillisAsInt;
 
-import org.kuali.common.util.Assert;
-import org.kuali.common.util.FormatUtils;
+import java.util.List;
 
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
@@ -27,7 +29,7 @@ public final class HttpContext {
 
 	private final String url; // Url to contact
 	private final String logMsgPrefix; // Gets printed as a prefix to any log messages emitted by the HttpService
-	private final List<Integer> successCodes; // HTTP codes signifying success
+	private final ImmutableList<Integer> successCodes; // HTTP codes signifying success
 	private final int requestTimeoutMillis; // Millis to wait before an individual http request times out (15 seconds)
 	private final int sleepIntervalMillis; // Millis to wait in between http requests (15 seconds)
 	private final int overallTimeoutMillis; // Total number of millis to wait before timing out (30 minutes)
@@ -35,9 +37,9 @@ public final class HttpContext {
 
 	// If Tomcat is fronted by an Apache web server, and Apache is up and running but Tomcat is still starting, http 503 is returned by Apache
 	// We don't want to fail if we get a 503, just continue waiting
-	private final List<Integer> continueWaitingCodes;
+	private final ImmutableList<Integer> continueWaitingCodes;
 
-	public static class Builder {
+	public static class Builder implements org.apache.commons.lang3.builder.Builder<HttpContext> {
 
 		private static final int OK = 200;
 		private static final int SERVICE_UNAVAILABLE = 503;
@@ -49,9 +51,9 @@ public final class HttpContext {
 		private String logMsgPrefix = "Waiting for";
 		private List<Integer> successCodes = ImmutableList.of(OK);
 		private List<Integer> continueWaitingCodes = ImmutableList.of(SERVICE_UNAVAILABLE);
-		private int requestTimeoutMillis = getIntMillis("15s"); // 15 seconds
-		private int sleepIntervalMillis = getIntMillis("15s"); // 15 seconds
-		private int overallTimeoutMillis = getIntMillis("30m"); // 30 minutes
+		private int requestTimeoutMillis = getMillisAsInt("15s"); // 15 seconds
+		private int sleepIntervalMillis = getMillisAsInt("15s"); // 15 seconds
+		private int overallTimeoutMillis = getMillisAsInt("30m"); // 30 minutes
 		private String encoding = Charsets.UTF_8.name();
 
 		public Builder(String url) {
@@ -79,7 +81,7 @@ public final class HttpContext {
 		}
 
 		public Builder requestTimeout(String requestTimeout) {
-			this.requestTimeoutMillis = getIntMillis(requestTimeout);
+			this.requestTimeoutMillis = getMillisAsInt(requestTimeout);
 			return this;
 		}
 
@@ -89,7 +91,7 @@ public final class HttpContext {
 		}
 
 		public Builder sleepInterval(String sleepInterval) {
-			this.sleepIntervalMillis = getIntMillis(sleepInterval);
+			this.sleepIntervalMillis = getMillisAsInt(sleepInterval);
 			return this;
 		}
 
@@ -99,7 +101,7 @@ public final class HttpContext {
 		}
 
 		public Builder overallTimeout(String overallTimeout) {
-			this.overallTimeoutMillis = getIntMillis(overallTimeout);
+			this.overallTimeoutMillis = getMillisAsInt(overallTimeout);
 			return this;
 		}
 
@@ -108,20 +110,39 @@ public final class HttpContext {
 			return this;
 		}
 
+		@Override
 		public HttpContext build() {
-			Assert.noBlanks(url, encoding);
-			Assert.noNulls(successCodes, continueWaitingCodes, logMsgPrefix);
-			Assert.isTrue(requestTimeoutMillis > 0, "requestTimeoutMillis must be a positive integer");
-			Assert.isTrue(overallTimeoutMillis > 0, "overallTimeoutMillis must be a positive integer");
-			Assert.isTrue(sleepIntervalMillis >= 0, "sleepIntervalMillis is negative");
-			this.successCodes = ImmutableList.copyOf(successCodes);
-			this.continueWaitingCodes = ImmutableList.copyOf(continueWaitingCodes);
-			return new HttpContext(this);
+			HttpContext instance = new HttpContext(this);
+			validate(instance);
+			return instance;
 		}
 
-		private static int getIntMillis(String time) {
-			return new Long(FormatUtils.getMillis(time)).intValue();
+		private static void validate(HttpContext instance) {
+			checkBlank(instance.url, "url");
+			checkBlank(instance.encoding, "encoding");
+			checkNull(instance.successCodes, "successCodes");
+			checkNull(instance.continueWaitingCodes, "continueWaitingCodes");
+			checkNull(instance.logMsgPrefix, "logMsgPrefix");
+			checkPositive(instance.requestTimeoutMillis, "requestTimeoutMillis");
+			checkPositive(instance.overallTimeoutMillis, "overallTimeoutMillis");
+			checkPositive(instance.sleepIntervalMillis, "sleepIntervalMillis");
 		}
+
+		private static void checkNull(Object arg, String name) {
+			checkNotNull(arg, NONULLS, name);
+		}
+
+		private static void checkPositive(int arg, String name) {
+			checkNotNull(arg, POSITIVE_INTEGER, name);
+		}
+
+		private static void checkBlank(String arg, String name) {
+			checkArgument(!isBlank(arg), NOBLANKS, name);
+		}
+
+		private static final String NOBLANKS = "'%s' cannot be blank";
+		private static final String NONULLS = "'%s' cannot be blank";
+		private static final String POSITIVE_INTEGER = "'%s' must be a positive integer";
 
 	}
 
@@ -129,8 +150,8 @@ public final class HttpContext {
 		this.url = builder.url;
 		this.encoding = builder.encoding;
 		this.logMsgPrefix = builder.logMsgPrefix;
-		this.successCodes = builder.successCodes;
-		this.continueWaitingCodes = builder.continueWaitingCodes;
+		this.successCodes = ImmutableList.copyOf(builder.successCodes);
+		this.continueWaitingCodes = ImmutableList.copyOf(builder.continueWaitingCodes);
 		this.requestTimeoutMillis = builder.requestTimeoutMillis;
 		this.sleepIntervalMillis = builder.sleepIntervalMillis;
 		this.overallTimeoutMillis = builder.overallTimeoutMillis;
