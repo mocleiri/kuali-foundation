0d566595dfc19844ed810cc4e0df75b7d38dece3
jcaddel <jcaddel@kuali.org>
2014-02-05 15:26:52 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31789 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/base/Assertions.java
+++ kuali-util/src/main/java/org/kuali/common/util/base/Assertions.java
@@ -10,19 +10,21 @@ public class Assertions {
 	private static final String NOT_BLANK_MSG = "'%s' cannot be blank";
 	private static final String IS_POSITIVE_MSG = "%s not allowed. '%s' must be positive";
 
-	public static void assertNotNull(Object arg, String name) {
-		checkNotNull(arg, NOT_NULL_MSG, name);
+	public static <T> T assertNotNull(T arg, String name) {
+		return checkNotNull(arg, NOT_NULL_MSG, name);
 	}
 
-	public static void assertNotBlank(String arg, String name) {
+	public static String assertNotBlank(String arg, String name) {
 		checkArgument(isBlank(arg), NOT_BLANK_MSG, name);
+		return arg;
 	}
 
 	/**
 	 * Assert that arg is greater than zero. Zero itself does not count as positive.
 	 */
-	public static void assertPositive(int arg, String name) {
+	public static int assertPositive(int arg, String name) {
 		checkArgument(arg > 0, IS_POSITIVE_MSG, arg, name);
+		return arg;
 	}
 
 }
