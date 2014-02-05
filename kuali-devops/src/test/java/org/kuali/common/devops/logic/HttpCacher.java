76a3832eeb352a25a15acd2e2b2e47450c1448a6
jcaddel <jcaddel@kuali.org>
2014-02-05 16:27:01 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31798 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/HttpCacher.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/HttpCacher.java
@@ -1,18 +1,48 @@
 package org.kuali.common.devops.logic;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.kuali.common.http.model.HttpContext;
 import org.kuali.common.http.model.HttpStatus;
 import org.kuali.common.http.model.HttpWaitResult;
 import org.kuali.common.http.service.DefaultHttpService;
 import org.kuali.common.http.service.HttpService;
+import org.kuali.common.util.Encodings;
+import org.kuali.common.util.base.Exceptions;
+import org.kuali.common.util.file.CanonicalFile;
 
 import com.google.common.base.Optional;
 
 public class HttpCacher {
 
 	private static final HttpService SERVICE = new DefaultHttpService();
+	private static final File CACHE_DIR = new CanonicalFile("./target/http/cache");
 
-	public static Optional<String> getContent(String url) {
+	public static void cache(String url) {
+		File cacheFile = getCacheFile(url);
+		Optional<String> content = getContent(url);
+		cache(cacheFile, content);
+	}
+
+	public static File getCacheFile(String url) {
+		return new CanonicalFile(CACHE_DIR, url);
+	}
+
+	protected static void cache(File file, Optional<String> data) {
+		try {
+			if (!data.isPresent()) {
+				FileUtils.forceDelete(file);
+			} else {
+				FileUtils.write(file, data.get(), Encodings.UTF8);
+			}
+		} catch (IOException e) {
+			throw Exceptions.illegalState("unexpected io error -> %s", file);
+		}
+	}
+
+	protected static Optional<String> getContent(String url) {
 		HttpContext context = HttpContext.builder(url).overallTimeout("10s").requestTimeout("3s").quiet(true).build();
 		HttpWaitResult result = SERVICE.wait(context);
 		if (result.getStatus().equals(HttpStatus.SUCCESS)) {
--- kuali-devops/src/test/java/org/kuali/common/devops/status/HttpTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/HttpTest.java
@@ -1,13 +1,9 @@
 package org.kuali.common.devops.status;
 
-import static java.lang.String.format;
-
 import org.junit.Test;
 import org.kuali.common.devops.logic.HttpCacher;
 import org.kuali.common.util.log.LoggerUtils;
 import org.slf4j.Logger;
-
-import com.google.common.base.Optional;
 
 public class HttpTest {
 
@@ -15,12 +11,11 @@ public class HttpTest {
 
 	@Test
 	public void test() {
-		String url = "http://env1.ks.kuali.org/tomcat/logs/env.jsp";
-		Optional<String> content = HttpCacher.getContent(url);
-		if (content.isPresent()) {
-			logger.info(format("\n\n[%s]\n\n%s\n\n", url, content.get()));
-		} else {
-			logger.info(format("no content was located @ [%s]", url));
+		try {
+			String url = "http://env1.ks.kuali.org/tomcat/logs/env.jsp";
+			HttpCacher.cache(url);
+		} catch (Throwable e) {
+			e.printStackTrace();
 		}
 	}
 }
