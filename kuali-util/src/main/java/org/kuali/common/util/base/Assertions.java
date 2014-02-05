a18d63e9035a5dd3b0e62315c87d981de8a798c4
jcaddel <jcaddel@kuali.org>
2014-02-05 13:34:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31763 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/base/Assertions.java
+++ kuali-util/src/main/java/org/kuali/common/util/base/Assertions.java
@@ -4,26 +4,27 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
-public class Checks {
+public class Assertions {
 
 	private static final String NOT_NULL_MSG = "'%s' cannot be null";
 	private static final String NOT_BLANK_MSG = "'%s' cannot be blank";
 	private static final String IS_POSITIVE_MSG = "%s not allowed. '%s' must be positive";
 
-	public static void notNull(Object arg, String name) {
+	public static void assertNotNull(Object arg, String name) {
 		checkNotNull(arg, NOT_NULL_MSG, name);
 	}
 
-	public static void notBlank(String arg, String name) {
+	public static void assertNotBlank(String arg, String name) {
 		checkArgument(isBlank(arg), NOT_BLANK_MSG, name);
 	}
 
 	/**
 	 * Ass
+	 * 
 	 * @param arg
 	 * @param name
 	 */
-	public static void isPositive(int arg, String name) {
+	public static void assertPositive(int arg, String name) {
 		checkArgument(arg > 0, IS_POSITIVE_MSG, arg, name);
 	}
 
