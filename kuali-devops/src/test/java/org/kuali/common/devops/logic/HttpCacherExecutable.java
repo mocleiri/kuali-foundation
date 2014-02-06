ade8613b2f3b75b9cd42a5367732a676b0989034
jcaddel <jcaddel@kuali.org>
2014-02-05 19:27:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31846 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/HttpCacherExecutable.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/HttpCacherExecutable.java
@@ -3,9 +3,7 @@ package org.kuali.common.devops.logic;
 import static org.kuali.common.util.base.Assertions.assertNotBlank;
 
 import org.kuali.common.util.execute.Executable;
-import org.kuali.common.util.validate.IdiotProofImmutable;
 
-@IdiotProofImmutable
 public final class HttpCacherExecutable implements Executable {
 
 	public HttpCacherExecutable(String url) {
