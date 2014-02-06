3a206b0fac894e2b81f196cf43704cd183eb2273
jcaddel <jcaddel@kuali.org>
2014-02-06 12:54:39 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31931 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/cache/PropertiesFileCache.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/PropertiesFileCache.java
@@ -1,10 +1,19 @@
 package org.kuali.common.devops.cache;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
 import java.util.concurrent.ExecutionException;
 
 import com.google.common.cache.AbstractLoadingCache;
 
-public class PropertiesFileCache extends AbstractLoadingCache<String, String> {
+public final class PropertiesFileCache extends AbstractLoadingCache<String, String> {
+
+	public PropertiesFileCache(File file) {
+		this.file = checkNotNull(file);
+	}
+
+	private final File file;
 
 	@Override
 	public String get(String key) throws ExecutionException {
