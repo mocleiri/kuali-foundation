62b264ac4f017f8de19006ac1d521f33b546fc6a
jcaddel <jcaddel@kuali.org>
2014-02-06 02:19:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31913 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/base/Exceptions.java
+++ kuali-util/src/main/java/org/kuali/common/util/base/Exceptions.java
@@ -8,7 +8,7 @@ package org.kuali.common.util.base;
  * Typical usage:
  * 
  * <pre>
- *   throw Exceptions.illegalArg(e, &quot;Port [%s] must be between %s and %s&quot; port, 0, 65535);
+ *   throw Exceptions.illegalArg("Port [%s] must be between %s and %s" port, 0, 65535);
  * </pre>
  */
 public class Exceptions {
