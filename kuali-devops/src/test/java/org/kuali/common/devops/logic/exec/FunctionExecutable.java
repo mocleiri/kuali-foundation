733bf9cc0de4e5ceb325fbc119e81e41f0a79013
jcaddel <jcaddel@kuali.org>
2014-02-06 01:14:52 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31893 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/exec/FunctionExecutable.java
@@ -0,0 +1,30 @@
+package org.kuali.common.devops.logic.exec;
+
+import static org.kuali.common.util.base.Assertions.assertNotNull;
+
+import org.kuali.common.util.execute.Executable;
+
+import com.google.common.base.Function;
+
+public final class FunctionExecutable<F, T> implements Executable {
+
+	public FunctionExecutable(Function<F, T> function, F input) {
+		this.function = assertNotNull(function, "function");
+		this.input = assertNotNull(input, "input");
+	}
+
+	private final Function<F, T> function;
+	private final F input;
+
+	private T result;
+
+	@Override
+	public void execute() {
+		this.result = function.apply(input);
+	}
+
+	public T getResult() {
+		return result;
+	}
+
+}
