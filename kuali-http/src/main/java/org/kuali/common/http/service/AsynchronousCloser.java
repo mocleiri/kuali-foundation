2e39180902c7d34f4006ed41a0e514ec0a19f686
jcaddel <jcaddel@kuali.org>
2014-02-05 20:57:09 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31860 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-http/src/main/java/org/kuali/common/http/service/AsynchronousCloser.java
@@ -0,0 +1,28 @@
+package org.kuali.common.http.service;
+
+import static org.kuali.common.util.base.Assertions.assertNotNull;
+
+import java.io.InputStream;
+
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.io.IOUtils;
+
+public final class AsynchronousCloser implements Runnable {
+
+	public AsynchronousCloser(HttpMethod method, InputStream in) {
+		assertNotNull(method, "method");
+		assertNotNull(in, "in");
+		this.method = method;
+		this.in = in;
+	}
+
+	private final HttpMethod method;
+	private final InputStream in;
+
+	@Override
+	public void run() {
+		method.releaseConnection();
+		IOUtils.closeQuietly(in);
+	}
+
+}
