cb9f6a469ba248feba688cd44b397232c75fc0d5
jcaddel <jcaddel@kuali.org>
2014-02-06 13:36:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31933 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/cache/FileCacheLoader.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/FileCacheLoader.java
@@ -1,22 +1,33 @@
 package org.kuali.common.devops.cache;
 
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.kuali.common.util.base.Precondition.checkNotBlank;
+
 import java.io.File;
 import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
 
 import com.google.common.base.Optional;
 import com.google.common.cache.CacheLoader;
 
 public final class FileCacheLoader extends CacheLoader<File, Optional<String>> {
 
+	public FileCacheLoader(String encoding) {
+		this.encoding = checkNotBlank(encoding, "encoding");
+	}
+
+	private final String encoding;
+
 	@Override
 	public Optional<String> load(File file) throws IOException {
 		if (file.exists()) {
-			return Optional.of(FileUtils.readFileToString(file));
+			return Optional.of(readFileToString(file, encoding));
 		} else {
 			return Optional.absent();
 		}
+	}
+
+	public String getEncoding() {
+		return encoding;
 	}
 
 }
---
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/LocationLoader.java
@@ -0,0 +1,62 @@
+package org.kuali.common.devops.cache;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.kuali.common.util.base.Precondition.checkNotBlank;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.kuali.common.util.file.CanonicalFile;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import com.google.common.base.Optional;
+import com.google.common.cache.CacheLoader;
+
+public final class LocationLoader extends CacheLoader<String, Optional<String>> {
+
+	public LocationLoader(String encoding) {
+		this(new DefaultResourceLoader(), encoding);
+	}
+
+	public LocationLoader(ResourceLoader resourceLoader, String encoding) {
+		this.resourceLoader = checkNotNull(resourceLoader);
+		this.encoding = checkNotBlank(encoding, "encoding");
+	}
+
+	private final ResourceLoader resourceLoader;
+	private final String encoding;
+
+	@Override
+	public Optional<String> load(String location) throws IOException {
+		checkNotBlank(location, "location");
+		Resource resource = getResource(location);
+		if (!resource.exists()) {
+			return Optional.absent();
+		} else {
+			return Optional.of(IOUtils.toString(resource.getInputStream(), encoding));
+		}
+	}
+
+	protected Resource getResource(String location) {
+		File file = new File(location);
+		if (file.exists()) {
+			File canonical = new CanonicalFile(file);
+			return new FileSystemResource(canonical);
+		} else {
+			return resourceLoader.getResource(location);
+		}
+	}
+
+	public ResourceLoader getResourceLoader() {
+		return resourceLoader;
+	}
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+}
