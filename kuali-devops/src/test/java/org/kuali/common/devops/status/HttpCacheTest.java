eef22b8cf887775c6129268a821c9cae3a6ada90
jcaddel <jcaddel@kuali.org>
2014-02-06 18:34:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31982 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/status/HttpCacheTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/status/HttpCacheTest.java
@@ -2,12 +2,15 @@ package org.kuali.common.devops.status;
 
 import static java.lang.String.format;
 import static org.apache.commons.lang3.StringUtils.rightPad;
+import static org.kuali.common.util.Encodings.UTF8;
 import static org.kuali.common.util.FormatUtils.getTime;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
 import org.kuali.common.devops.cache.HttpLoader;
+import org.kuali.common.devops.cache.LocationLoader;
 import org.kuali.common.http.model.HttpContext;
 import org.kuali.common.util.log.Loggers;
 import org.slf4j.Logger;
@@ -25,17 +28,23 @@ public class HttpCacheTest {
 	@Test
 	public void test() {
 		try {
-			List<String> urls = ImmutableList.of("http://www.google.com/", "http://cnn.com", "http://www.microsoft.com", "http://www.yahoo.com/");
 			HttpContext context = HttpContext.builder().quiet(true).asynchronousClose(true).maxBytes(50 * 1024).maxRetries(0).overallTimeout("5s").build();
-			LoadingCache<String, Optional<String>> cache = CacheBuilder.newBuilder().build(HttpLoader.create(context));
-			for (String url : urls) {
-				Stopwatch stopwatch = Stopwatch.createStarted();
-				Optional<String> optional = cache.get(url);
-				String content = optional.isPresent() ? optional.get().length() + "" : "absent";
-				logger.info(format("%s content: %s - %s", rightPad(url, 25), content, getTime(stopwatch)));
-			}
+			LoadingCache<String, Optional<String>> cache1 = CacheBuilder.newBuilder().build(HttpLoader.create(context));
+			LoadingCache<String, Optional<String>> cache2 = CacheBuilder.newBuilder().build(new LocationLoader(UTF8));
+			cache(cache1);
+			cache(cache2);
 		} catch (Throwable e) {
 			e.printStackTrace();
+		}
+	}
+
+	protected static void cache(LoadingCache<String, Optional<String>> cache) throws ExecutionException {
+		List<String> urls = ImmutableList.of("http://www.google.com/", "http://cnn.com", "http://www.microsoft.com", "http://www.yahoo.com/");
+		for (String url : urls) {
+			Stopwatch stopwatch = Stopwatch.createStarted();
+			Optional<String> optional = cache.get(url);
+			String content = optional.isPresent() ? optional.get().length() + "" : "absent";
+			logger.info(format("%s content: %s - %s", rightPad(url, 25), content, getTime(stopwatch)));
 		}
 	}
 
