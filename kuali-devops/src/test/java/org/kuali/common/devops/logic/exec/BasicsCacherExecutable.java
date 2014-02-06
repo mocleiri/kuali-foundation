916df046cd03cd2c4f40f13f83d41bbcf6935331
jcaddel <jcaddel@kuali.org>
2014-02-05 19:30:37 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31848 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/exec/BasicsCacherExecutable.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/exec/BasicsCacherExecutable.java
@@ -2,21 +2,33 @@ package org.kuali.common.devops.logic.exec;
 
 import static org.kuali.common.util.base.Assertions.assertNotBlank;
 
+import java.util.List;
+
+import org.kuali.common.devops.logic.Examiner;
 import org.kuali.common.devops.logic.HttpCacher;
+import org.kuali.common.devops.logic.Manifests;
+import org.kuali.common.devops.logic.Tomcats;
 import org.kuali.common.util.execute.Executable;
+
+import com.google.common.collect.Lists;
 
 public final class BasicsCacherExecutable implements Executable {
 
-	public BasicsCacherExecutable(String url) {
-		assertNotBlank(url, "url");
-		this.url = url;
+	public BasicsCacherExecutable(String fqdn) {
+		assertNotBlank(fqdn, "fqdn");
+		this.fqdn = fqdn;
 	}
 
-	private final String url;
+	private final String fqdn;
 
 	@Override
 	public void execute() {
-		HttpCacher.cache(url);
+		List<String> urls = Lists.newArrayList();
+		urls.add(Manifests.getUrl(fqdn));
+		urls.add(Tomcats.getHeapUrl(fqdn));
+		urls.add(Tomcats.getReleaseNotesUrl(fqdn));
+		urls.add(Examiner.getEnvJspUrl(fqdn));
+		HttpCacher.cache(fqdn);
 	}
 
 }
