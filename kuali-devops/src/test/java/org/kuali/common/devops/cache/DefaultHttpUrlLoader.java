30a784d89c583be274ff90300a1498ff6663cb22
jcaddel <jcaddel@kuali.org>
2014-02-06 17:12:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31966 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/DefaultHttpUrlLoader.java
@@ -0,0 +1,77 @@
+package org.kuali.common.devops.cache;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.IOException;
+
+import org.kuali.common.http.model.HttpContext;
+import org.kuali.common.http.model.HttpWaitResult;
+import org.kuali.common.http.service.HttpService;
+import org.kuali.common.util.build.ValidatingBuilder;
+import org.kuali.common.util.validate.IdiotProofImmutable;
+import org.springframework.beans.BeanUtils;
+
+import com.google.common.base.Optional;
+import com.google.common.cache.CacheLoader;
+
+@IdiotProofImmutable
+public final class DefaultHttpUrlLoader extends CacheLoader<String, Optional<String>> {
+
+	private final HttpContext context;
+	private final HttpService service;
+
+	@Override
+	public Optional<String> load(String url) throws IOException {
+		checkNotNull(url);
+		HttpContext.Builder builder = HttpContext.builder(url);
+		BeanUtils.copyProperties(context, builder);
+		builder.setUrl(url);
+		HttpContext context = builder.build();
+		HttpWaitResult result = service.wait(context);
+		return result.getFinalRequestResult().getResponseBody();
+	}
+
+	private DefaultHttpUrlLoader(Builder builder) {
+		this.context = builder.context;
+		this.service = builder.service;
+	}
+
+	public static class Builder extends ValidatingBuilder<DefaultHttpUrlLoader> {
+
+		private HttpContext context;
+		private HttpService service;
+
+		public Builder context(HttpContext context) {
+			this.context = context;
+			return this;
+		}
+
+		public Builder service(HttpService service) {
+			this.service = service;
+			return this;
+		}
+
+		@Override
+		public DefaultHttpUrlLoader getInstance() {
+			return new DefaultHttpUrlLoader(this);
+		}
+
+		public HttpContext getContext() {
+			return context;
+		}
+
+		public void setContext(HttpContext context) {
+			this.context = context;
+		}
+
+		public HttpService getService() {
+			return service;
+		}
+
+		public void setService(HttpService service) {
+			this.service = service;
+		}
+
+	}
+
+}
