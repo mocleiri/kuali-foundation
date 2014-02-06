da60a4fadcc8e9b7656f676cb6dbb9d155e68386
jcaddel <jcaddel@kuali.org>
2014-02-06 18:57:21 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31988 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/cache/DualLayerCache.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/DualLayerCache.java
@@ -3,8 +3,6 @@ package org.kuali.common.devops.cache;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.kuali.common.util.Encodings.UTF8;
 
-import java.io.IOException;
-
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
 import com.google.common.base.Optional;
@@ -16,7 +14,7 @@ public final class DualLayerCache<T> extends CacheLoader<T, Optional<String>> {
 	private final FileLoader fileLoader = new FileLoader(UTF8);
 
 	@Override
-	public Optional<String> load(T key) throws IOException {
+	public Optional<String> load(T key) {
 		checkNotNull(key);
 		return null;
 	}
