355d7193b68f1c4a442eadd7ef4eef4b69c19fde
jcaddel <jcaddel@kuali.org>
2014-02-05 19:22:56 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31843 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/HttpCacherExecutable.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/HttpCacherExecutable.java
@@ -1,55 +1,23 @@
 package org.kuali.common.devops.logic;
 
-import java.util.List;
+import static org.kuali.common.util.base.Assertions.assertNotBlank;
 
-import org.kuali.common.util.build.ValidatingBuilder;
 import org.kuali.common.util.execute.Executable;
 import org.kuali.common.util.validate.IdiotProofImmutable;
-
-import com.google.common.collect.ImmutableList;
 
 @IdiotProofImmutable
 public final class HttpCacherExecutable implements Executable {
 
-	private final ImmutableList<String> urls;
+	public HttpCacherExecutable(String url) {
+		assertNotBlank(url, "url");
+		this.url = url;
+	}
+
+	private final String url;
 
 	@Override
 	public void execute() {
-		for (String url : urls) {
-			HttpCacher.cache(url);
-		}
-	}
-
-	private HttpCacherExecutable(Builder builder) {
-		this.urls = ImmutableList.copyOf(builder.urls);
-	}
-
-	public static class Builder extends ValidatingBuilder<HttpCacherExecutable> {
-
-		private List<String> urls;
-
-		public Builder urls(List<String> urls) {
-			this.urls = urls;
-			return this;
-		}
-
-		@Override
-		public HttpCacherExecutable getInstance() {
-			return new HttpCacherExecutable(this);
-		}
-
-		public List<String> getUrls() {
-			return urls;
-		}
-
-		public void setUrls(List<String> urls) {
-			this.urls = urls;
-		}
-
-	}
-
-	public List<String> getUrls() {
-		return urls;
+		HttpCacher.cache(url);
 	}
 
 }
