dd96477839a1a9aaf56fc1f44ab80ad82b2ccb95
jcaddel <jcaddel@kuali.org>
2014-02-07 13:06:35 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32041 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/cache/function/UrlToFileFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/function/UrlToFileFunction.java
@@ -1,36 +1,116 @@
 package org.kuali.common.devops.cache.function;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static org.kuali.common.util.base.Precondition.checkNotBlank;
 
 import java.io.File;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.kuali.common.util.build.ValidatingBuilder;
 import org.kuali.common.util.file.CanonicalFile;
+import org.kuali.common.util.validate.IdiotProofImmutable;
 
 import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
 
+@IdiotProofImmutable
 public final class UrlToFileFunction implements Function<String, File> {
 
-	public UrlToFileFunction() {
-		this(new CanonicalFile("./target/cache"));
-	}
-
-	public UrlToFileFunction(File basedir) {
-		this.basedir = checkNotNull(basedir);
-	}
-
 	private final File basedir;
+	private final ImmutableList<String> removeTokens;
+	private final ImmutableList<String> replaceTokens;
 
 	@Override
 	public File apply(String url) {
 		checkNotBlank(url, "url");
-		String path = StringUtils.removeEnd(url.replace(":", ""), "/");
+		String path = getPath(url);
 		return new CanonicalFile(basedir, path);
+	}
+
+	protected String getPath(String url) {
+		// Remove trailing slash (if any)
+		String path = StringUtils.removeEnd(url, "/");
+
+		// For each token in the remove tokens list, replace it with empty ("")
+		for (String removeToken : removeTokens) {
+			path.replace(removeToken, "");
+		}
+
+		// For each token in the replace tokens list, replace it with the file separator
+		for (String replaceToken : replaceTokens) {
+			path.replace(replaceToken, File.separator);
+		}
+		return path;
+	}
+
+	private UrlToFileFunction(Builder builder) {
+		this.basedir = builder.basedir;
+		this.removeTokens = ImmutableList.copyOf(builder.removeTokens);
+		this.replaceTokens = ImmutableList.copyOf(builder.replaceTokens);
+	}
+
+	public static class Builder extends ValidatingBuilder<UrlToFileFunction> {
+
+		private File basedir = new CanonicalFile("./target/cache");
+		private List<String> removeTokens = ImmutableList.of();
+		private List<String> replaceTokens = ImmutableList.of(":", "///", "?", "#", "=");
+
+		public Builder basedir(File basedir) {
+			this.basedir = basedir;
+			return this;
+		}
+
+		public Builder removeTokens(List<String> removeTokens) {
+			this.removeTokens = removeTokens;
+			return this;
+		}
+
+		public Builder replaceTokens(List<String> replaceTokens) {
+			this.replaceTokens = replaceTokens;
+			return this;
+		}
+
+		@Override
+		public UrlToFileFunction getInstance() {
+			return new UrlToFileFunction(this);
+		}
+
+		public File getBasedir() {
+			return basedir;
+		}
+
+		public void setBasedir(File basedir) {
+			this.basedir = basedir;
+		}
+
+		public List<String> getRemoveTokens() {
+			return removeTokens;
+		}
+
+		public void setRemoveTokens(List<String> removeTokens) {
+			this.removeTokens = removeTokens;
+		}
+
+		public List<String> getReplaceTokens() {
+			return replaceTokens;
+		}
+
+		public void setReplaceTokens(List<String> replaceTokens) {
+			this.replaceTokens = replaceTokens;
+		}
+
 	}
 
 	public File getBasedir() {
 		return basedir;
+	}
+
+	public List<String> getRemoveTokens() {
+		return removeTokens;
+	}
+
+	public List<String> getReplaceTokens() {
+		return replaceTokens;
 	}
 
 }
