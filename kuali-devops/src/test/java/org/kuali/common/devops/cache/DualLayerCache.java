8bb37bb1af5f3c430a6b9dbf7bb94d882a67a740
jcaddel <jcaddel@kuali.org>
2014-02-06 19:10:30 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31991 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/cache/DualLayerCache.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/DualLayerCache.java
@@ -1,7 +1,11 @@
 package org.kuali.common.devops.cache;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static org.apache.commons.io.FileUtils.writeStringToFile;
+import static org.codehaus.plexus.util.FileUtils.forceDelete;
 import static org.kuali.common.util.Encodings.UTF8;
+
+import java.io.File;
 
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
@@ -9,14 +13,30 @@ import com.google.common.base.Optional;
 import com.google.common.cache.CacheLoader;
 
 @IdiotProofImmutable
-public final class DualLayerCache<T> extends CacheLoader<T, Optional<String>> {
+public abstract class DualLayerCache<T> extends CacheLoader<T, Optional<String>> {
 
 	private final FileLoader fileLoader = new FileLoader(UTF8);
+	private final CacheLoader<T, Optional<String>> loader = null;
+
+	protected abstract File getFile(T key);
 
 	@Override
-	public Optional<String> load(T key) {
+	public Optional<String> load(T key) throws Exception {
 		checkNotNull(key);
-		return null;
+		File file = getFile(key);
+		Optional<String> data = fileLoader.load(file);
+		if (data.isPresent()) {
+			return data;
+		}
+		data = loader.load(key);
+		if (data.isPresent()) {
+			writeStringToFile(file, data.get(), UTF8);
+		} else {
+			if (file.exists()) {
+				forceDelete(file);
+			}
+		}
+		return data;
 	}
 
 }
