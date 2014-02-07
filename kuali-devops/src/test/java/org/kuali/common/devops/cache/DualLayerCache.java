6f0f8d70ad4934ce5c670ca9aee4712b03c221e4
jcaddel <jcaddel@kuali.org>
2014-02-06 19:27:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@31995 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/cache/DualLayerCache.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/DualLayerCache.java
@@ -1,28 +1,23 @@
 package org.kuali.common.devops.cache;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static org.apache.commons.io.FileUtils.writeStringToFile;
-import static org.codehaus.plexus.util.FileUtils.forceDelete;
 import static org.kuali.common.util.Encodings.UTF8;
 
 import java.io.File;
 
-import org.kuali.common.util.log.Loggers;
 import org.kuali.common.util.validate.IdiotProofImmutable;
-import org.slf4j.Logger;
 
 import com.google.common.base.Optional;
 import com.google.common.cache.CacheLoader;
 
 @IdiotProofImmutable
-public abstract class DualLayerCache<T> extends CacheLoader<T, Optional<String>> implements CacheWriter<T, Optional<String>> {
+public abstract class DualLayerCache<T> extends CacheLoader<T, Optional<String>> {
 
 	private final FileLoader fileLoader = new FileLoader(UTF8);
+	private final FileWriter fileWriter = new FileWriter(UTF8);
 	private final CacheLoader<T, Optional<String>> loader = null;
 
 	protected abstract File getFile(T key);
-
-	private static final Logger logger = Loggers.make();
 
 	@Override
 	public Optional<String> load(T key) throws Exception {
@@ -31,17 +26,11 @@ public abstract class DualLayerCache<T> extends CacheLoader<T, Optional<String>>
 		Optional<String> data = fileLoader.load(file);
 		if (data.isPresent()) {
 			return data;
-		}
-		data = loader.load(key);
-		if (data.isPresent()) {
-			logger.info(String.format("creating -> %s", file));
-			writeStringToFile(file, data.get(), UTF8);
 		} else {
-			if (file.exists()) {
-				forceDelete(file);
-			}
+			data = loader.load(key);
+			fileWriter.write(file, data);
+			return data;
 		}
-		return data;
 	}
 
 }
