386ee0d837767cb2235edc772b2370fc33d88512
jcaddel <jcaddel@kuali.org>
2014-02-06 20:41:39 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32008 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/cache/FileCache.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/FileCache.java
@@ -1,10 +1,10 @@
 package org.kuali.common.devops.cache;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static org.kuali.common.util.Encodings.UTF8;
 
 import java.io.File;
 
+import org.kuali.common.util.build.ValidatingBuilder;
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
 import com.google.common.base.Function;
@@ -12,29 +12,112 @@ import com.google.common.base.Optional;
 import com.google.common.cache.CacheLoader;
 
 @IdiotProofImmutable
-public abstract class FileCache<T> extends CacheLoader<T, Optional<String>> {
+public class FileCache<T, V> extends CacheLoader<T, Optional<V>> {
 
-	public FileCache(CacheLoader<T, Optional<String>> loader, Function<T, File> function) {
-		this.loader = checkNotNull(loader);
-		this.function = checkNotNull(function);
-	}
-
-	private final CacheLoader<File, Optional<String>> fileLoader = new FileLoader(UTF8);
-	private final CachePersister<File, Optional<String>> filePersister = new FilePersister(UTF8);
-
-	private final CacheLoader<T, Optional<String>> loader;
+	private final CacheLoader<File, Optional<V>> fileLoader;
+	private final CachePersister<File, Optional<V>> filePersister;
+	private final CacheLoader<T, Optional<V>> loader;
 	private final Function<T, File> function;
 
 	@Override
-	public Optional<String> load(T key) throws Exception {
+	public Optional<V> load(T key) throws Exception {
 		checkNotNull(key);
 		File file = function.apply(key);
-		Optional<String> data = fileLoader.load(file);
+		Optional<V> data = fileLoader.load(file);
 		if (!data.isPresent()) {
 			data = loader.load(key);
 			filePersister.persist(file, data);
 		}
 		return data;
+	}
+
+	private FileCache(Builder<T, V> builder) {
+		this.fileLoader = builder.fileLoader;
+		this.filePersister = builder.filePersister;
+		this.loader = builder.loader;
+		this.function = builder.function;
+	}
+
+	public static class Builder<T, V> extends ValidatingBuilder<FileCache<T, V>> {
+
+		private CacheLoader<File, Optional<V>> fileLoader;
+		private CachePersister<File, Optional<V>> filePersister;
+		private CacheLoader<T, Optional<V>> loader;
+		private Function<T, File> function;
+
+		public Builder<T, V> fileLoader(CacheLoader<File, Optional<V>> fileLoader) {
+			this.fileLoader = fileLoader;
+			return this;
+		}
+
+		public Builder<T, V> filePersister(CachePersister<File, Optional<V>> filePersister) {
+			this.filePersister = filePersister;
+			return this;
+		}
+
+		public Builder<T, V> loader(CacheLoader<T, Optional<V>> loader) {
+			this.loader = loader;
+			return this;
+		}
+
+		public Builder<T, V> function(Function<T, File> function) {
+			this.function = function;
+			return this;
+		}
+
+		@Override
+		public FileCache<T, V> getInstance() {
+			return new FileCache<T, V>(this);
+		}
+
+		public CacheLoader<File, Optional<V>> getFileLoader() {
+			return fileLoader;
+		}
+
+		public void setFileLoader(CacheLoader<File, Optional<V>> fileLoader) {
+			this.fileLoader = fileLoader;
+		}
+
+		public CachePersister<File, Optional<V>> getFilePersister() {
+			return filePersister;
+		}
+
+		public void setFilePersister(CachePersister<File, Optional<V>> filePersister) {
+			this.filePersister = filePersister;
+		}
+
+		public CacheLoader<T, Optional<V>> getLoader() {
+			return loader;
+		}
+
+		public void setLoader(CacheLoader<T, Optional<V>> loader) {
+			this.loader = loader;
+		}
+
+		public Function<T, File> getFunction() {
+			return function;
+		}
+
+		public void setFunction(Function<T, File> function) {
+			this.function = function;
+		}
+
+	}
+
+	public CacheLoader<File, Optional<V>> getFileLoader() {
+		return fileLoader;
+	}
+
+	public CachePersister<File, Optional<V>> getFilePersister() {
+		return filePersister;
+	}
+
+	public CacheLoader<T, Optional<V>> getLoader() {
+		return loader;
+	}
+
+	public Function<T, File> getFunction() {
+		return function;
 	}
 
 }
