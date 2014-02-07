d544cb6b34b70f05399a2f01bdde8f798ad37b26
jcaddel <jcaddel@kuali.org>
2014-02-07 16:13:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32078 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/Examiner.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/Examiner.java
@@ -1,8 +1,12 @@
 package org.kuali.common.devops.logic;
 
+import static com.google.common.base.Preconditions.checkState;
 import static java.lang.String.format;
+import static org.apache.commons.lang.StringUtils.substringBetween;
+import static org.apache.commons.lang.StringUtils.substringsBetween;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.common.http.model.HttpContext;
@@ -11,6 +15,7 @@ import org.kuali.common.http.service.DefaultHttpService;
 import org.kuali.common.http.service.HttpService;
 import org.kuali.common.util.base.Exceptions;
 import org.kuali.common.util.log.Loggers;
+import org.kuali.common.util.property.ImmutableProperties;
 import org.slf4j.Logger;
 
 import com.google.common.base.Optional;
@@ -24,6 +29,20 @@ public class Examiner {
 	protected static final String SCM_URL_KEY = "project.scm.url";
 	protected static final String SCM_REVISION_KEY = "project.scm.revision";
 	public static final String ENV_JSP_FRAGMENT = "/tomcat/logs/env.jsp";
+
+	public static Properties getSystemPropertiesFromString(String envJspContent) {
+		String content = substringBetween(envJspContent, "<th>System Property</th>", "</table>");
+		String[] rows = substringsBetween(content, "<tr>", "</tr>");
+		Properties properties = new Properties();
+		for (String row : rows) {
+			String[] tokens = substringsBetween(row, "<td>", "</td>");
+			checkState(tokens.length == 2, "expected exactly 2 tokens. found %s instead [%]", tokens.length, row);
+			String key = tokens[0];
+			String value = tokens[1];
+			properties.setProperty(key, value);
+		}
+		return ImmutableProperties.copyOf(properties);
+	}
 
 	public static final String getEnvJspUrl(String fqdn) {
 		return getUrl(fqdn, ENV_JSP_FRAGMENT);
