2b1bcd918ba3e86b439887e66765d6f69bf924d8
jcaddel <jcaddel@kuali.org>
2014-02-07 14:51:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32064 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/model/CanonicalNameRecord.java
@@ -0,0 +1,63 @@
+package org.kuali.common.devops.model;
+
+import org.kuali.common.util.build.ValidatingBuilder;
+import org.kuali.common.util.validate.IdiotProofImmutable;
+
+@IdiotProofImmutable
+public final class CanonicalNameRecord {
+
+	private final String alias;
+	private final String canonical;
+
+	private CanonicalNameRecord(Builder builder) {
+		this.alias = builder.alias;
+		this.canonical = builder.canonical;
+	}
+
+	public static class Builder extends ValidatingBuilder<CanonicalNameRecord> {
+
+		private String alias;
+		private String canonical;
+
+		@Override
+		public CanonicalNameRecord getInstance() {
+			return new CanonicalNameRecord(this);
+		}
+
+		public Builder alias(String alias) {
+			this.alias = alias;
+			return this;
+		}
+
+		public Builder canonical(String canonical) {
+			this.canonical = canonical;
+			return this;
+		}
+
+		public String getAlias() {
+			return alias;
+		}
+
+		public void setAlias(String alias) {
+			this.alias = alias;
+		}
+
+		public String getCanonical() {
+			return canonical;
+		}
+
+		public void setCanonical(String canonical) {
+			this.canonical = canonical;
+		}
+
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public String getCanonical() {
+		return canonical;
+	}
+
+}
