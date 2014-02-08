17a54c811b40f7b16d1afb56f2fa419bd152d909
jcaddel <jcaddel@kuali.org>
2014-02-08 15:57:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32163 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/metadata/function/RemoteEnvironmentFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/metadata/function/RemoteEnvironmentFunction.java
@@ -22,14 +22,15 @@ import com.google.common.base.Optional;
 public final class RemoteEnvironmentFunction implements Function<String, RemoteEnvironment> {
 
 	@Override
-	public RemoteEnvironment apply(String content) {
-		checkNotNull(content, "content");
-		Properties system = getProperties("System Property", content);
-		Properties environment = getProperties("Environment Variable", content);
-		return RemoteEnvironment.builder().system(system).environment(environment).build();
+	public RemoteEnvironment apply(String html) {
+		checkNotNull(html, "html");
+		Properties system = getProperties("System Property", html);
+		Properties environment = getProperties("Environment Variable", html);
+		Optional<Long> millis = getCurrentTimeMillis(html);
+		return RemoteEnvironment.builder().system(system).environment(environment).currentTimeMillis(millis).build();
 	}
 
-	protected Optional<Long> getTimestamp(String html) {
+	protected Optional<Long> getCurrentTimeMillis(String html) {
 		// <li>time: 2014-02-08 18:26:06.873 UTC</li>
 		Optional<String> token = fromNullable(substringBetween(html, "<li>time:", "</li>"));
 		if (token.isPresent()) {
