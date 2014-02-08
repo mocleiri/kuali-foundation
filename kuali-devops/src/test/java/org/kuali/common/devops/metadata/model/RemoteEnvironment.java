4ec5c92eeae3f69f558d01a56799a35d93bbee88
jcaddel <jcaddel@kuali.org>
2014-02-08 14:50:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32154 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/metadata/model/RemoteEnvironment.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/metadata/model/RemoteEnvironment.java
@@ -2,24 +2,20 @@ package org.kuali.common.devops.metadata.model;
 
 import java.util.Properties;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
-
 import org.kuali.common.util.build.ValidatingBuilder;
 import org.kuali.common.util.property.ImmutableProperties;
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
+import com.google.common.base.Optional;
+
 @IdiotProofImmutable
 public final class RemoteEnvironment {
 
-	@Min(1)
-	private final int processors;
+	private final Optional<Integer> processors;
 	private final ImmutableProperties system;
 	private final ImmutableProperties environment;
-	@Min(0)
-	private final long timestamp;
-	@Valid
-	private final Memory memory;
+	private final Optional<Long> timestamp;
+	private final Optional<Memory> memory;
 
 	private RemoteEnvironment(Builder builder) {
 		this.processors = builder.processors;
@@ -29,39 +25,35 @@ public final class RemoteEnvironment {
 		this.memory = builder.memory;
 	}
 
-	public static Builder builder() {
-		return new Builder();
-	}
-
 	public static class Builder extends ValidatingBuilder<RemoteEnvironment> {
 
-		private int processors;
+		private Optional<Integer> processors;
 		private Properties system;
 		private Properties environment;
-		private long timestamp;
-		private Memory memory;
+		private Optional<Long> timestamp;
+		private Optional<Memory> memory;
 
-		public Builder processors(int processors) {
+		public Builder processors(Optional<Integer> processors) {
 			this.processors = processors;
 			return this;
 		}
 
-		public Builder system(ImmutableProperties system) {
+		public Builder system(Properties system) {
 			this.system = system;
 			return this;
 		}
 
-		public Builder environment(ImmutableProperties environment) {
+		public Builder environment(Properties environment) {
 			this.environment = environment;
 			return this;
 		}
 
-		public Builder timestamp(long timestamp) {
+		public Builder timestamp(Optional<Long> timestamp) {
 			this.timestamp = timestamp;
 			return this;
 		}
 
-		public Builder memory(Memory memory) {
+		public Builder memory(Optional<Memory> memory) {
 			this.memory = memory;
 			return this;
 		}
@@ -70,50 +62,9 @@ public final class RemoteEnvironment {
 		public RemoteEnvironment getInstance() {
 			return new RemoteEnvironment(this);
 		}
-
-		public int getProcessors() {
-			return processors;
-		}
-
-		public void setProcessors(int processors) {
-			this.processors = processors;
-		}
-
-		public Properties getSystem() {
-			return system;
-		}
-
-		public void setSystem(Properties system) {
-			this.system = system;
-		}
-
-		public Properties getEnvironment() {
-			return environment;
-		}
-
-		public void setEnvironment(Properties environment) {
-			this.environment = environment;
-		}
-
-		public long getTimestamp() {
-			return timestamp;
-		}
-
-		public void setTimestamp(long timestamp) {
-			this.timestamp = timestamp;
-		}
-
-		public Memory getMemory() {
-			return memory;
-		}
-
-		public void setMemory(Memory memory) {
-			this.memory = memory;
-		}
-
 	}
 
-	public int getProcessors() {
+	public Optional<Integer> getProcessors() {
 		return processors;
 	}
 
@@ -125,11 +76,11 @@ public final class RemoteEnvironment {
 		return environment;
 	}
 
-	public long getTimestamp() {
+	public Optional<Long> getTimestamp() {
 		return timestamp;
 	}
 
-	public Memory getMemory() {
+	public Optional<Memory> getMemory() {
 		return memory;
 	}
 
