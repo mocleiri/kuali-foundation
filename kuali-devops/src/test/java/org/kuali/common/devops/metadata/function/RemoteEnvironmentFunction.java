788e20db404730cda452c804a3593b1c240d36fb
jcaddel <jcaddel@kuali.org>
2014-02-08 16:50:41 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32165 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/metadata/function/RemoteEnvironmentFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/metadata/function/RemoteEnvironmentFunction.java
@@ -2,21 +2,30 @@ package org.kuali.common.devops.metadata.function;
 
 import static com.google.common.base.Optional.absent;
 import static com.google.common.base.Optional.fromNullable;
+import static org.apache.commons.lang.StringUtils.substringAfter;
+import static org.apache.commons.lang.StringUtils.substringBefore;
 import static org.apache.commons.lang.StringUtils.substringBetween;
 import static org.apache.commons.lang.StringUtils.substringsBetween;
+import static org.kuali.common.util.base.Exceptions.illegalArg;
 import static org.kuali.common.util.base.Precondition.checkNotNull;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import org.kuali.common.devops.metadata.model.Memory;
 import org.kuali.common.devops.metadata.model.RemoteEnvironment;
+import org.kuali.common.util.FormatUtils;
 import org.kuali.common.util.property.ImmutableProperties;
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Maps;
 
 @IdiotProofImmutable
 public final class RemoteEnvironmentFunction implements Function<String, RemoteEnvironment> {
@@ -27,12 +36,30 @@ public final class RemoteEnvironmentFunction implements Function<String, RemoteE
 		Properties system = getProperties("System Property", html);
 		Properties environment = getProperties("Environment Variable", html);
 		Optional<Long> millis = getCurrentTimeMillis(html);
-		return RemoteEnvironment.builder().system(system).environment(environment).currentTimeMillis(millis).build();
+		Optional<Integer> processors = getProcessors(html);
+		return RemoteEnvironment.builder().system(system).environment(environment).currentTimeMillis(millis).processors(processors).build();
+	}
+
+	protected Optional<Memory> getMemory(String html) {
+		// <li>mem: [used=0.62g, free=1.16g, allocated=0.94g, max=1.78g]</li>
+		Optional<String> token = fromNullable(substringBetween(html, "<li>mem: [", "]</li>"));
+		if (token.isPresent()) {
+			List<String> tokens = Splitter.on(',').splitToList(token.get());
+			Map<String, Long> map = Maps.newHashMap();
+			for (String element : tokens) {
+				String key = substringBefore(element, "=");
+				Long value = FormatUtils.getMillis(substringAfter(element, "="));
+				map.put(key, value);
+			}
+			Memory memory = Memory.builder().used(map.get("used")).free(map.get("free")).allocated(map.get("allocated")).max(map.get("max")).build();
+			return Optional.of(memory);
+		}
+		return absent();
 	}
 
 	protected Optional<Integer> getProcessors(String html) {
 		// <li>processors: 2</li>
-		Optional<String> token = fromNullable(substringBetween(html, "<li>processors:", "</li>"));
+		Optional<String> token = getToken(html, "processors");
 		if (token.isPresent()) {
 			return Optional.of(Integer.parseInt(token.get()));
 		}
@@ -41,17 +68,18 @@ public final class RemoteEnvironmentFunction implements Function<String, RemoteE
 
 	protected Optional<Long> getCurrentTimeMillis(String html) {
 		// <li>time: 2014-02-08 18:26:06.873 UTC</li>
-		Optional<String> token = fromNullable(substringBetween(html, "<li>time:", "</li>"));
-		if (token.isPresent()) {
-			try {
-				SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S z");
-				Date date = parser.parse(token.get().trim());
-				return Optional.of(date.getTime());
-			} catch (ParseException e) {
-				return absent();
-			}
+		Optional<String> token = getToken(html, "time");
+		return token.isPresent() ? getTime(token.get()) : Optional.<Long> absent();
+	}
+
+	protected Optional<Long> getTime(String token) {
+		try {
+			SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S z");
+			Date date = parser.parse(token);
+			return Optional.of(date.getTime());
+		} catch (ParseException e) {
+			throw illegalArg(e, "unexpected parse error -> [%s]", token);
 		}
-		return absent();
 	}
 
 	protected Properties getProperties(String title, String html) {
@@ -75,4 +103,14 @@ public final class RemoteEnvironmentFunction implements Function<String, RemoteE
 		}
 		return ImmutableProperties.copyOf(properties);
 	}
+
+	protected Optional<String> getToken(String html, String item) {
+		String token = substringBetween(html, "<li>" + item + ":", "</li>");
+		if (token == null) {
+			return absent();
+		} else {
+			return Optional.of(token.trim());
+		}
+	}
+
 }
