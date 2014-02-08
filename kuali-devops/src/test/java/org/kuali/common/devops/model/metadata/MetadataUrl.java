78c89fc9c8c14e87ec029d48fbf737730265cb47
jcaddel <jcaddel@kuali.org>
2014-02-07 20:29:55 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32111 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/model/metadata/MetadataUrl.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/model/metadata/MetadataUrl.java
@@ -1,74 +1,45 @@
 package org.kuali.common.devops.model.metadata;
 
-import org.kuali.common.util.build.ValidatingBuilder;
-import org.kuali.common.util.validate.IdiotProofImmutable;
-
 import com.google.common.base.Function;
+import com.google.common.base.Optional;
 
-@IdiotProofImmutable
-public final class MetadataUrl<T> {
+public class MetadataUrl<T> {
 
-	private final String url;
-	private final Function<String, T> contentConverter;
-
-	private MetadataUrl(Builder<T> builder) {
-		this.url = builder.url;
-		this.contentConverter = builder.contentConverter;
-	}
-
-	public static <T> MetadataUrl<T> create(String url, Function<String, T> contentConverter) {
-		Builder<T> builder = builder();
-		return builder.url(url).contentConverter(contentConverter).build();
-	}
-
-	public static <T> Builder<T> builder() {
-		return new Builder<T>();
-	}
-
-	public static class Builder<T> extends ValidatingBuilder<MetadataUrl<T>> {
-
-		private String url;
-		private Function<String, T> contentConverter;
-
-		public Builder<T> url(String url) {
-			this.url = url;
-			return this;
-		}
-
-		public Builder<T> contentConverter(Function<String, T> contentConverter) {
-			this.contentConverter = contentConverter;
-			return this;
-		}
-
-		@Override
-		public MetadataUrl<T> getInstance() {
-			return new MetadataUrl<T>(this);
-		}
-
-		public String getUrl() {
-			return url;
-		}
-
-		public void setUrl(String url) {
-			this.url = url;
-		}
-
-		public Function<String, T> getContentConverter() {
-			return contentConverter;
-		}
-
-		public void setContentConverter(Function<String, T> converter) {
-			this.contentConverter = converter;
-		}
-
-	}
+	String url;
+	Optional<String> content;
+	Function<String, T> contentConverter;
+	T instance;
 
 	public String getUrl() {
 		return url;
 	}
 
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Optional<String> getContent() {
+		return content;
+	}
+
+	public void setContent(Optional<String> content) {
+		this.content = content;
+	}
+
 	public Function<String, T> getContentConverter() {
 		return contentConverter;
+	}
+
+	public void setContentConverter(Function<String, T> contentConverter) {
+		this.contentConverter = contentConverter;
+	}
+
+	public T getInstance() {
+		return instance;
+	}
+
+	public void setInstance(T instance) {
+		this.instance = instance;
 	}
 
 }
