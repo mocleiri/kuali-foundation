c8ed09a61b946398ba6d68a018870d906784c8f1
jcaddel <jcaddel@kuali.org>
2014-02-08 09:39:36 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32121 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/model/metadata/MetadataUrl.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/model/metadata/MetadataUrl.java
@@ -11,17 +11,20 @@ public final class MetadataUrl<T> {
 
 	private final String url;
 	private final Optional<String> content;
-	private final Function<Optional<String>, Optional<T>> converter;
+	private final Function<String, Optional<T>> converter;
+	private final Optional<T> metadata;
 
 	private MetadataUrl(Builder<T> builder) {
 		this.url = builder.url;
 		this.content = builder.content;
 		this.converter = builder.converter;
+		this.metadata = builder.metadata;
 	}
 
-	public static <T> MetadataUrl<T> create(String url, Optional<String> content, Function<Optional<String>, Optional<T>> converter) {
+	public static <T> MetadataUrl<T> create(String url, Optional<String> content, Function<String, Optional<T>> converter) {
 		Builder<T> builder = builder();
-		return builder.url(url).content(content).converter(converter).build();
+		Optional<T> metadata = content.isPresent() ? converter.apply(content.get()) : Optional.<T> absent();
+		return builder.url(url).content(content).converter(converter).metadata(metadata).build();
 	}
 
 	public static <T> Builder<T> builder() {
@@ -32,7 +35,13 @@ public final class MetadataUrl<T> {
 
 		private String url;
 		private Optional<String> content;
-		private Function<Optional<String>, Optional<T>> converter;
+		private Function<String, Optional<T>> converter;
+		private Optional<T> metadata;
+
+		public Builder<T> metadata(Optional<T> metadata) {
+			this.metadata = metadata;
+			return this;
+		}
 
 		public Builder<T> url(String url) {
 			this.url = url;
@@ -44,7 +53,7 @@ public final class MetadataUrl<T> {
 			return this;
 		}
 
-		public Builder<T> converter(Function<Optional<String>, Optional<T>> converter) {
+		public Builder<T> converter(Function<String, Optional<T>> converter) {
 			this.converter = converter;
 			return this;
 		}
@@ -64,8 +73,12 @@ public final class MetadataUrl<T> {
 		return content;
 	}
 
-	public Function<Optional<String>, Optional<T>> getConverter() {
+	public Function<String, Optional<T>> getConverter() {
 		return converter;
+	}
+
+	public Optional<T> getMetadata() {
+		return metadata;
 	}
 
 }
