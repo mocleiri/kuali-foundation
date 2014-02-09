9b800b19305d12ad8f844bef604d547dd2e052d8
jcaddel <jcaddel@kuali.org>
2014-02-08 21:47:21 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32214 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/metadata/function/ProjectConfigUrlFragmentFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/metadata/function/ProjectConfigUrlFragmentFunction.java
@@ -4,11 +4,12 @@ import static com.google.common.base.Optional.absent;
 import static com.google.common.base.Optional.fromNullable;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.kuali.common.util.base.Precondition.checkNotNull;
+import static org.kuali.common.util.project.KualiProjectConstants.OLE_GROUP_ID;
+import static org.kuali.common.util.project.KualiProjectConstants.STUDENT_GROUP_ID;
 
 import java.util.Properties;
 
 import org.kuali.common.devops.metadata.model.RemoteEnvironment;
-import org.kuali.common.util.project.KualiProjectConstants;
 import org.kuali.common.util.project.model.Project;
 
 import com.google.common.base.Function;
@@ -30,10 +31,10 @@ public final class ProjectConfigUrlFragmentFunction implements Function<Project,
 
 	protected Optional<String> getConfigFragment(Project project, Optional<RemoteEnvironment> remoteEnvironment) {
 		String groupId = project.getGroupId();
-		if (groupId.equals(KualiProjectConstants.STUDENT_GROUP_ID)) {
+		if (groupId.equals(STUDENT_GROUP_ID)) {
 			// KS specific logic
 			return Optional.of("/home/kuali/main/dev/" + project.getArtifactId() + "-config.xml");
-		} else if (groupId.equals(KualiProjectConstants.OLE_GROUP_ID)) {
+		} else if (groupId.equals(OLE_GROUP_ID)) {
 			// OLE specific logic
 			return getOleConfigFragment(remoteEnvironment);
 		} else {
