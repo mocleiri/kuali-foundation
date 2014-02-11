6f7a5cc6ee7e7d833c396259e2de5849bc82c457
jcaddel <jcaddel@kuali.org>
2014-02-11 09:14:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32415 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/function/ToCsvFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/function/ToCsvFunction.java
@@ -1,5 +1,6 @@
 package org.kuali.common.devops.logic.function;
 
+import static com.google.common.collect.Sets.newTreeSet;
 import static org.kuali.common.util.ReflectionUtils.isOptionalString;
 import static org.kuali.common.util.base.Precondition.checkNotNull;
 
@@ -20,7 +21,6 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.google.common.collect.Table;
 
 public final class ToCsvFunction<R, C> implements Function<Table<? extends Comparable<R>, ? extends Comparable<C>, TableCellDescriptor<Object>>, List<String>> {
@@ -40,8 +40,8 @@ public final class ToCsvFunction<R, C> implements Function<Table<? extends Compa
 	@Override
 	public List<String> apply(Table<? extends Comparable<R>, ? extends Comparable<C>, TableCellDescriptor<Object>> table) {
 		checkNotNull(table, "table");
-		SortedSet<Comparable<R>> rowKeys = Sets.newTreeSet(table.rowKeySet());
-		SortedSet<Comparable<C>> colKeys = Sets.newTreeSet(table.columnKeySet());
+		SortedSet<Comparable<R>> rowKeys = newTreeSet(table.rowKeySet());
+		SortedSet<Comparable<C>> colKeys = newTreeSet(table.columnKeySet());
 		List<String> lines = Lists.newArrayList();
 		lines.add(getHeader(colKeys));
 		for (Comparable<R> rowKey : rowKeys) {
