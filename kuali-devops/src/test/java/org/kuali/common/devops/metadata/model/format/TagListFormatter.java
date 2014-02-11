6e5e192ea49dd4a88f878de171b9c3af27b11917
jcaddel <jcaddel@kuali.org>
2014-02-11 08:04:23 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32395 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/metadata/model/format/TagListFormatter.java
@@ -0,0 +1,55 @@
+package org.kuali.common.devops.metadata.model.format;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.kuali.common.devops.metadata.model.EC2Tag;
+import org.kuali.common.util.base.string.Replacer;
+import org.springframework.format.Formatter;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+
+public class TagListFormatter implements Formatter<List<EC2Tag>> {
+
+	private static final Replacer REPLACER = Replacer.builder().add("|", "${format.separator}").add("=", "${format.equals}").build();
+	private static final Splitter SPLITTER = Splitter.on('|');
+	private static final Joiner JOINER = Joiner.on('|');
+
+	@Override
+	public String print(List<EC2Tag> tags, Locale locale) {
+		if (tags.isEmpty()) {
+			return null;
+		}
+		List<String> strings = newArrayList();
+		for (EC2Tag tag : tags) {
+			String string = REPLACER.replace(tag.getKey()) + "=" + REPLACER.replace(tag.getValue());
+			strings.add(string);
+		}
+		return JOINER.join(strings);
+	}
+
+	@Override
+	public List<EC2Tag> parse(String text, Locale locale) {
+		if (text == null) {
+			return ImmutableList.<EC2Tag> of();
+		}
+		List<String> strings = SPLITTER.splitToList(text);
+		List<EC2Tag> tags = newArrayList();
+		Splitter equals = Splitter.on('=');
+		for (String string : strings) {
+			List<String> tokens = equals.splitToList(string);
+			checkState(tokens.size() == 2, "expected exactly 2 tokens, but there were %s intead -> [%s]", tokens.size(), string);
+			String key = tokens.get(0);
+			String value = tokens.get(1);
+			EC2Tag tag = EC2Tag.create(key, value);
+			tags.add(tag);
+		}
+		return ImmutableList.copyOf(tags);
+	}
+
+}
