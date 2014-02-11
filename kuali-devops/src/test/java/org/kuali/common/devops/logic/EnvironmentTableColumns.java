2a7ae453e3259cdb54ac684b96613f8f2b23a4d9
jcaddel <jcaddel@kuali.org>
2014-02-11 07:15:30 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32388 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/logic/EnvironmentTableColumns.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/logic/EnvironmentTableColumns.java
@@ -1,6 +1,5 @@
 package org.kuali.common.devops.logic;
 
-import static org.kuali.common.devops.table.Label.create;
 import static org.kuali.common.util.base.Precondition.checkNotNull;
 
 import org.kuali.common.devops.table.Label;
@@ -8,18 +7,18 @@ import org.kuali.common.util.Counter;
 
 public enum EnvironmentTableColumns {
 
-	NAME(create(Int.next(), "#")), //
-	URL(create(Int.next(), "Url")), //
-	APP(create(Int.next(), "Application")), //
-	VERSION(create(Int.next(), "Version")), //
-	BUILD_DATE(create(Int.next(), "Build Date")), //
-	PURPOSE(create(Int.next(), "Purpose")), //
-	SCM(create(Int.next(), "SCM")), //
-	DATABASE(create(Int.next(), "Database")), //
-	SCHEMA(create(Int.next(), "Schema")), //
-	JAVA(create(Int.next(), "Java")), //
-	SERVER(create(Int.next(), "Server")), //
-	TOMCAT(create(Int.next(), "Tomcat")); //
+	NAME(Label.create(Int.next(), "#")), //
+	URL(Label.create(Int.next(), "Url")), //
+	APP(Label.create(Int.next(), "Application")), //
+	VERSION(Label.create(Int.next(), "Version")), //
+	BUILD_DATE(Label.create(Int.next(), "Build Date")), //
+	PURPOSE(Label.create(Int.next(), "Purpose")), //
+	SCM(Label.create(Int.next(), "SCM")), //
+	DATABASE(Label.create(Int.next(), "Database")), //
+	SCHEMA(Label.create(Int.next(), "Schema")), //
+	JAVA(Label.create(Int.next(), "Java")), //
+	SERVER(Label.create(Int.next(), "Server")), //
+	TOMCAT(Label.create(Int.next(), "Tomcat")); //
 
 	private final Label label;
 
