00e012f05ecee5e8f11600e3791ab0ac7756c9ec
jcaddel <jcaddel@kuali.org>
2014-02-12 20:00:38 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32602 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/base/string/IsBlankPredicate.java
+++ kuali-util/src/main/java/org/kuali/common/util/base/string/IsBlankPredicate.java
@@ -1,7 +1,8 @@
 package org.kuali.common.util.base.string;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
+
+import javax.annotation.Nullable;
 
 import com.google.common.base.Predicate;
 
@@ -11,8 +12,7 @@ public enum IsBlankPredicate implements Predicate<String> {
 	INSTANCE;
 
 	@Override
-	public boolean apply(String string) {
-		checkNotNull(string);
+	public boolean apply(@Nullable String string) {
 		return isBlank(string);
 	}
 
