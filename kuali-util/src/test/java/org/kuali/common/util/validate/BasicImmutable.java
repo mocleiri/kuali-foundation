51ce36c49421e4f4f743ee20ece5255ffa44236f
jcaddel <jcaddel@kuali.org>
2014-02-12 21:13:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32623 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/validate/BasicImmutable.java
@@ -0,0 +1,29 @@
+package org.kuali.common.util.validate;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+@StronglyImmutable
+@NoNullFields
+@ImmutableGuavaMaps
+@ImmutableGuavaCollections
+@Documented
+public @interface BasicImmutable {
+
+	String message() default "idiot proofing failed";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
