31a27a426e01059de6611453b83bca92168970ce
jcaddel <jcaddel@kuali.org>
2014-02-12 23:21:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32644 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/breakfast/immutable/DataBinderTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/breakfast/immutable/DataBinderTest.java
@@ -1,9 +1,11 @@
 package org.kuali.common.util.bind.breakfast.immutable;
 
 import static com.google.common.collect.Maps.newHashMap;
+import static org.kuali.common.util.ReflectionUtils.newInstance;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.builder.Builder;
 import org.junit.Test;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.validation.DataBinder;
@@ -16,30 +18,19 @@ public class DataBinderTest {
 			Map<String, Object> milkMap = newHashMap();
 			milkMap.put("type", "lowfat");
 			milkMap.put("price", "2.29");
-			MutablePropertyValues milkValues = new MutablePropertyValues(milkMap);
-			Milk.Builder milkBuilder = new Milk.Builder();
-			DataBinder milkBinder = new DataBinder(milkBuilder);
-			milkBinder.bind(milkValues);
-			Milk milk = milkBuilder.build();
+			Milk milk = getInstance(Milk.Builder.class, milkMap);
 			Map<String, Object> bowlMap = newHashMap();
 			bowlMap.put("milk", milk);
-			MutablePropertyValues bowlValues = new MutablePropertyValues(bowlMap);
-			Bowl.Builder bowlBuilder = new Bowl.Builder();
-			DataBinder bowlBinder = new DataBinder(bowlBuilder);
-			bowlBinder.bind(bowlValues);
-			Bowl bowl = bowlBuilder.build();
+			Bowl bowl = getInstance(Bowl.Builder.class, bowlMap);
 			System.out.println("milk.type=" + bowl.getMilk().getType());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	protected Milk getMilk() {
-		Map<String, Object> map = newHashMap();
-		map.put("type", "lowfat");
-		map.put("price", "2.29");
+	protected static <T> T getInstance(Class<? extends Builder<T>> type, Map<String, Object> map) {
 		MutablePropertyValues values = new MutablePropertyValues(map);
-		Milk.Builder builder = new Milk.Builder();
+		Builder<T> builder = newInstance(type);
 		DataBinder binder = new DataBinder(builder);
 		binder.bind(values);
 		return builder.build();
