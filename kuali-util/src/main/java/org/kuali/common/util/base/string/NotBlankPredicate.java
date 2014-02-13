b75b6609a34f0416bb98f7fea268b61d0da18c05
jcaddel <jcaddel@kuali.org>
2014-02-13 10:13:11 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32660 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/base/string/NotBlankPredicate.java
+++ kuali-util/src/main/java/org/kuali/common/util/base/string/NotBlankPredicate.java
@@ -7,13 +7,13 @@ import javax.annotation.Nullable;
 import com.google.common.base.Predicate;
 
 // enum singleton pattern
-public enum BlankStringPredicate implements Predicate<String> {
+public enum NotBlankPredicate implements Predicate<String> {
 
 	INSTANCE;
 
 	@Override
 	public boolean apply(@Nullable String string) {
-		return isBlank(string);
+		return !isBlank(string);
 	}
 
 }
