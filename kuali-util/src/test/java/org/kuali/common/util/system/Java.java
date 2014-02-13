64e39cfd8eefed06fd84d20efc7c723016fb10ce
jcaddel <jcaddel@kuali.org>
2014-02-12 21:10:57 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32622 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/Java.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/Java.java
@@ -35,7 +35,7 @@ public final class Java {
 	@Alias("ext.dirs")
 	private final ImmutableList<String> extensionDirectories;
 
-	@Bind
+	@Bind(Bind.ABSENT)
 	private final RuntimeEnvironment runtimeEnvironment;
 
 	@Bind(value = "vm")
