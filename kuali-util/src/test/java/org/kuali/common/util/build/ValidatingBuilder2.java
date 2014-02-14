ea155f5ce0fbdab238292de082b076456d561c9a
jcaddel <jcaddel@kuali.org>
2014-02-14 14:36:55 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32703 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/build/ValidatingBuilder2.java
+++ kuali-util/src/test/java/org/kuali/common/util/build/ValidatingBuilder2.java
@@ -3,7 +3,9 @@ package org.kuali.common.util.build;
 import static org.kuali.common.util.validate.Validation.checkConstraints;
 
 import java.util.List;
+import java.util.Set;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
 import org.apache.commons.lang3.builder.Builder;
@@ -11,21 +13,35 @@ import org.kuali.common.util.validate.Validation;
 
 import com.google.common.collect.ImmutableList;
 
-public abstract class ValidatingBuilder<T> implements Builder<T> {
+public abstract class ValidatingBuilder2<T> implements Builder<T> {
+
+	public static final Class<?>[] EMPTY_CLASS_ARRAY = {};
 
 	protected Validator validator = Validation.getDefaultValidator();
 	protected List<Class<?>> validationGroups = ImmutableList.of();
+
+	public abstract boolean isValid();
+
+	public abstract Set<ConstraintViolation<T>> getViolations();
+
+	public boolean isValid(T instance) {
+		return getViolations(instance).isEmpty();
+	}
+
+	public Set<ConstraintViolation<T>> getViolations(T instance) {
+		return validator.validate(instance, validationGroups.toArray(EMPTY_CLASS_ARRAY));
+	}
 
 	public T validate(T instance) {
 		return checkConstraints(instance, validator, validationGroups);
 	}
 
-	public ValidatingBuilder<T> validator(Validator validator) {
+	public ValidatingBuilder2<T> validator(Validator validator) {
 		this.validator = validator;
 		return this;
 	}
 
-	public ValidatingBuilder<T> validationGroups(List<Class<?>> validationGroups) {
+	public ValidatingBuilder2<T> validationGroups(List<Class<?>> validationGroups) {
 		this.validationGroups = validationGroups;
 		return this;
 	}
