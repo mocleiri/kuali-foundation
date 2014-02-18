53c3997d50949be2a93a25c623045749e8d98d6a
jcaddel <jcaddel@kuali.org>
2014-02-18 18:26:57 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32760 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/User.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/User.java
@@ -1,10 +1,15 @@
 package org.kuali.common.util.system;
 
+import static com.google.common.base.Optional.absent;
 import static org.kuali.common.util.validate.Validation.checkConstraints;
 
 import java.io.File;
+import java.util.TimeZone;
 
+import org.kuali.common.util.bind.api.Alias;
 import org.kuali.common.util.validate.IdiotProofImmutable;
+
+import com.google.common.base.Optional;
 
 @IdiotProofImmutable
 public final class User {
@@ -12,11 +17,18 @@ public final class User {
 	private final String name;
 	private final File home;
 	private final File dir;
+	private final Optional<String> language;
+	private final Optional<String> country;
+	@Alias("timezone")
+	private final Optional<TimeZone> timeZone;
 
 	private User(Builder builder) {
 		this.name = builder.name;
 		this.home = builder.home;
 		this.dir = builder.dir;
+		this.language = builder.language;
+		this.country = builder.country;
+		this.timeZone = builder.timeZone;
 	}
 
 	public static Builder builder() {
@@ -28,6 +40,28 @@ public final class User {
 		private String name;
 		private File home;
 		private File dir;
+		private Optional<String> language = absent();
+		private Optional<String> country = absent();
+		private Optional<TimeZone> timeZone = absent();
+
+		public Builder country(Optional<String> country) {
+			this.country = country;
+			return this;
+		}
+
+		public Builder language(Optional<String> language) {
+			this.language = language;
+			return this;
+		}
+
+		public Builder timeZone(Optional<TimeZone> timeZone) {
+			this.timeZone = timeZone;
+			return this;
+		}
+
+		public Builder timeZone(TimeZone timeZone) {
+			return timeZone(Optional.of(timeZone));
+		}
 
 		public Builder name(String name) {
 			this.name = name;
@@ -72,6 +106,30 @@ public final class User {
 		public void setDir(File dir) {
 			this.dir = dir;
 		}
+
+		public Optional<String> getLanguage() {
+			return language;
+		}
+
+		public void setLanguage(Optional<String> language) {
+			this.language = language;
+		}
+
+		public Optional<String> getCountry() {
+			return country;
+		}
+
+		public void setCountry(Optional<String> country) {
+			this.country = country;
+		}
+
+		public Optional<TimeZone> getTimeZone() {
+			return timeZone;
+		}
+
+		public void setTimeZone(Optional<TimeZone> timeZone) {
+			this.timeZone = timeZone;
+		}
 	}
 
 	public String getName() {
@@ -84,6 +142,18 @@ public final class User {
 
 	public File getDir() {
 		return dir;
+	}
+
+	public Optional<String> getLanguage() {
+		return language;
+	}
+
+	public Optional<String> getCountry() {
+		return country;
+	}
+
+	public Optional<TimeZone> getTimeZone() {
+		return timeZone;
 	}
 
 }
