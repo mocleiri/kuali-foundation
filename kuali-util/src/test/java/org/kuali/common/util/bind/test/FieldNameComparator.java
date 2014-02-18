d4c0656f2a7d96ef3b540fbc39aedef34b2a28e6
jcaddel <jcaddel@kuali.org>
2014-02-18 14:02:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32729 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/test/FieldNameComparator.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/test/FieldNameComparator.java
@@ -3,9 +3,9 @@ package org.kuali.common.util.bind.test;
 import java.lang.reflect.Field;
 import java.util.Comparator;
 
-public final class FieldNameComparator implements Comparator<Field> {
-
-	public static final FieldNameComparator INSTANCE = new FieldNameComparator();
+// singleton enum pattern
+public enum FieldNameComparator implements Comparator<Field> {
+	INSTANCE;
 
 	@Override
 	public int compare(Field one, Field two) {
