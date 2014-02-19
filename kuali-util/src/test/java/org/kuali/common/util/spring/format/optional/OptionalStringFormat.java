96d3e5a8b93c57c80b0f430f1f8e9634a22d496b
jcaddel <jcaddel@kuali.org>
2014-02-19 01:30:35 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32808 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/format/optional/OptionalStringFormat.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/format/optional/OptionalStringFormat.java
@@ -4,16 +4,15 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.kuali.common.util.base.Optionals.ABSENT_OPTIONAL_TOKEN;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-
-import org.kuali.common.util.base.Optionals;
 
 @Target({ METHOD, FIELD, PARAMETER })
 @Retention(RUNTIME)
 public @interface OptionalStringFormat {
 
-	String absentToken() default Optionals.ABSENT_OPTIONAL_TOKEN;
+	String absentToken() default ABSENT_OPTIONAL_TOKEN;
 
 }
