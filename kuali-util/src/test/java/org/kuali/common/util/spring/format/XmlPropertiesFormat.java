83e096e56ea8dff92e5d92a8c1681e7e168e227b
jcaddel <jcaddel@kuali.org>
2014-02-19 12:14:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32830 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/spring/format/XmlPropertiesFormat.java
+++ kuali-util/src/test/java/org/kuali/common/util/spring/format/XmlPropertiesFormat.java
@@ -4,6 +4,8 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.kuali.common.util.Encodings.UTF8;
+import static org.kuali.common.util.base.Optionals.EMPTY_PROPERTIES_TOKEN;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -11,4 +13,9 @@ import java.lang.annotation.Target;
 @Target({ METHOD, FIELD, PARAMETER })
 @Retention(RUNTIME)
 public @interface XmlPropertiesFormat {
+
+	String encoding() default UTF8;
+
+	String emptyPropertiesToken() default EMPTY_PROPERTIES_TOKEN;
+
 }
