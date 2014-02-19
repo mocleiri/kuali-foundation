09ada4a1dab8d75a7bca3811e783b4ecddcc5c4f
jcaddel <jcaddel@kuali.org>
2014-02-19 00:06:19 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32785 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/bind/breakfast/immutable/DataBinderTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/bind/breakfast/immutable/DataBinderTest.java
@@ -278,13 +278,13 @@ public class DataBinderTest {
 		return list;
 	}
 
-	public static <T> T getInstance(Class<T> type, Properties properties, Set<String> exclusions) {
+	public static <T> T getInstance(Class<T> type, Properties properties, Set<String> allowedBlanks) {
 		Map<String, String> map = PropertyUtils.convert(properties);
 		for (String key : newHashSet(map.keySet())) {
 			String value = map.get(key);
-			boolean remove = StringUtils.isBlank(value) && !exclusions.contains(key);
+			boolean remove = StringUtils.isBlank(value) && !allowedBlanks.contains(key);
 			if (remove) {
-				logger.info(String.format("remove [%s] because it is blank", key));
+				logger.info(String.format("removing [%s] because it is blank", key));
 				map.remove(key);
 			}
 		}
