3550abcf71930936bbec8abb341783c2812e153a
jcaddel <jcaddel@kuali.org>
2014-02-18 23:04:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32774 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/spring/format/optional/OptionalTimeZoneFormat.java
@@ -0,0 +1,19 @@
+package org.kuali.common.util.spring.format.optional;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.kuali.common.util.base.Optionals;
+
+@Target({ METHOD, FIELD, PARAMETER })
+@Retention(RUNTIME)
+public @interface OptionalTimeZoneFormat {
+
+	String value() default Optionals.ABSENT_OPTIONAL_TOKEN;
+
+}
