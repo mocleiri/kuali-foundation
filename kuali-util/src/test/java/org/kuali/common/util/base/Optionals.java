0986b570e71b66d1732ebc8c13f3656f23bd13b1
jcaddel <jcaddel@kuali.org>
2014-02-19 00:40:38 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32789 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/base/Optionals.java
+++ kuali-util/src/test/java/org/kuali/common/util/base/Optionals.java
@@ -3,6 +3,7 @@ package org.kuali.common.util.base;
 public class Optionals {
 
 	public static final String ABSENT_OPTIONAL_TOKEN = "${optional.absent}";
+
 	public static final String EMPTY_LIST_TOKEN = "${list.empty}";
 
 }
