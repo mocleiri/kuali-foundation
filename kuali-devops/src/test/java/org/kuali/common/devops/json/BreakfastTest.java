c53cd52e8ac82ecf7fbd15cd4fcf6d05a5b4d0cc
jcaddel <jcaddel@kuali.org>
2014-02-22 15:52:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32946 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/json/BreakfastTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/BreakfastTest.java
@@ -1,10 +1,14 @@
 package org.kuali.common.devops.json;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static org.kuali.common.util.base.Exceptions.illegalState;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.lang3.builder.Builder;
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 
@@ -15,15 +19,35 @@ public class BreakfastTest {
 	@Test
 	public void test() {
 		try {
-			ObjectMapper mapper = new ObjectMapper();
 			Milk milk = Milk.builder().price(2.29).type("low\"fat").build();
 			Bowl bowl = Bowl.builder().milk(milk).build();
+			ObjectMapper mapper = new ObjectMapper();
 			String json = mapper.writeValueAsString(bowl);
 			System.out.println(json);
 			JsonNode node = mapper.readTree(json);
 			print(node);
 		} catch (Throwable e) {
 			e.printStackTrace();
+		}
+	}
+
+	protected static <T> T read(Class<T> type, ObjectMapper mapper, String json) {
+		JsonNode node = readTree(mapper, json);
+		return null;
+	}
+	
+	protected static <T> Builder<T> getBuilder(Class<T> type) {
+		
+		return null;
+	}
+
+	protected static JsonNode readTree(ObjectMapper mapper, String json) {
+		try {
+			return mapper.readTree(json);
+		} catch (JsonProcessingException e) {
+			throw illegalState(e);
+		} catch (IOException e) {
+			throw illegalState(e);
 		}
 	}
 
