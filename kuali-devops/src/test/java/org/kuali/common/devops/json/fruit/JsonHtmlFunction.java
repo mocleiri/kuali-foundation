1fb1eb2e8633e9e0cdb3daf885c876fbe78ab364
jcaddel <jcaddel@kuali.org>
2014-02-23 15:41:39 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33016 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/json/fruit/JsonHtmlFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/fruit/JsonHtmlFunction.java
@@ -1,17 +1,30 @@
 package org.kuali.common.devops.json.fruit;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.kuali.common.util.base.Precondition.checkNotNull;
+
+import java.util.List;
 
 import org.kuali.common.util.tree.Node;
 
 import com.google.common.base.Function;
+import com.google.common.base.Joiner;
 
 public class JsonHtmlFunction implements Function<Node<JsonDescriptor>, String> {
 
 	@Override
 	public String apply(Node<JsonDescriptor> node) {
 		checkNotNull(node, "node");
-		return node.getElement().getType().getSimpleName();
+		JsonDescriptor desc = node.getElement();
+		List<String> strings = newArrayList();
+		strings.add("type: " + desc.getType().getSimpleName());
+		if (desc.getDescriptor().isPresent()) {
+			FieldDescriptor fd = desc.getDescriptor().get();
+			strings.add("name:" + fd.getField().getName());
+		} else {
+			strings.add("name:none");
+		}
+		return "<table border=1><tr><td>" + Joiner.on("</td></tr><tr><td>").join(strings) + "</td></tr></table>";
 	}
 
 }
