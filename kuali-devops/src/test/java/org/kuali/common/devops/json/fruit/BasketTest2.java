001f4d72031e06d4c086e29ca185b1ae423ca6b6
jcaddel <jcaddel@kuali.org>
2014-02-23 13:58:06 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33000 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/fruit/BasketTest2.java
@@ -0,0 +1,60 @@
+package org.kuali.common.devops.json.fruit;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.apache.commons.lang3.StringUtils.repeat;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+
+public class BasketTest2 {
+
+	@Test
+	public void test() {
+		try {
+			Apple a1 = Apple.builder().color("red").build();
+			Apple a2 = Apple.builder().color("green").build();
+			List<Apple> apples = ImmutableList.of(a1, a2);
+			Basket basket = Basket.builder().apples(apples).material("straw").build();
+			ObjectMapper mapper = new ObjectMapper();
+			String json = mapper.writeValueAsString(basket);
+			JsonNode node = mapper.readTree(json);
+			print(node);
+			System.out.println(json);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void print(JsonNode node) {
+		print(node, 0);
+	}
+
+	protected void print(final JsonNode node, final int indent) {
+		System.out.println(repeat(" ", indent) + toString(node));
+		int indentation = indent + 2;
+		for (JsonNode element : newArrayList(node.iterator())) {
+			print(element, indentation);
+		}
+	}
+
+	protected String toString(JsonNode node) {
+		List<String> strings = newArrayList();
+		strings.add("container=" + node.isContainerNode() + "");
+		strings.add("value=" + node.isValueNode() + "");
+		strings.add("array=" + node.isArray());
+		List<String> fields = newArrayList(node.fieldNames());
+		if (!fields.isEmpty()) {
+			strings.add("fields=" + Joiner.on(',').join(fields));
+		} else {
+			strings.add("fields=none");
+		}
+		return Joiner.on("::").join(strings);
+	}
+
+}
