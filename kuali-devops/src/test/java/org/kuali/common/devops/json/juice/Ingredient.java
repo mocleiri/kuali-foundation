24f3f38eb6789279e610cf3a0d3119fdbeb28789
jcaddel <jcaddel@kuali.org>
2014-02-22 20:37:44 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@32971 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/juice/Ingredient.java
@@ -0,0 +1,58 @@
+package org.kuali.common.devops.json.juice;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+
+import org.kuali.common.util.build.ValidatingBuilder;
+
+public final class Ingredient {
+
+	private final String name;
+	private final String description;
+
+	private Ingredient(Builder builder) {
+		this.name = builder.name;
+		this.description = builder.description;
+	}
+
+	public static class Builder extends ValidatingBuilder<Ingredient> {
+
+		private String name;
+		private String description;
+
+		@Override
+		public Ingredient build() {
+			return validate(make());
+		}
+
+		@Override
+		public Set<ConstraintViolation<Ingredient>> violations() {
+			return violations(make());
+		}
+
+		private Ingredient make() {
+			return new Ingredient(this);
+		}
+
+		public Builder name(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder description(String description) {
+			this.description = description;
+			return this;
+		}
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}
---
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/juice/Juice.java
@@ -0,0 +1,73 @@
+package org.kuali.common.devops.json.juice;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.constraints.Min;
+
+import org.kuali.common.util.build.ValidatingBuilder;
+import org.kuali.common.util.validate.IdiotProofImmutable;
+
+import com.google.common.collect.ImmutableList;
+
+@IdiotProofImmutable
+public final class Juice {
+
+	@Min(0)
+	private final double price;
+	private final ImmutableList<Ingredient> ingredients;
+
+	private Juice(Builder builder) {
+		this.price = builder.price;
+		this.ingredients = ImmutableList.copyOf(builder.ingredients);
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static class Builder extends ValidatingBuilder<Juice> {
+
+		private double price = -1;
+		private List<Ingredient> ingredients;
+
+		@Override
+		public Set<ConstraintViolation<Juice>> violations() {
+			return violations(make());
+		}
+
+		@Override
+		public Juice build() {
+			return validate(make());
+		}
+
+		private Juice make() {
+			return new Juice(this);
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+		public void setPrice(double price) {
+			this.price = price;
+		}
+
+		public List<Ingredient> getIngredients() {
+			return ingredients;
+		}
+
+		public void setIngredients(List<Ingredient> ingredients) {
+			this.ingredients = ingredients;
+		}
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public ImmutableList<Ingredient> getIngredients() {
+		return ingredients;
+	}
+}
