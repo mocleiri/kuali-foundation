b1263cee5a314a01ced0d8d85314d1a253fac8db
jcaddel <jcaddel@kuali.org>
2014-02-24 13:50:25 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33052 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/pojo/JsonService.java
@@ -0,0 +1,16 @@
+package org.kuali.common.devops.json.pojo;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface JsonService {
+
+	<T> T readString(String json, Class<T> type);
+
+	<T> T read(InputStream in, Class<T> type);
+
+	<T> String writeString(T reference);
+
+	<T> void write(OutputStream out, T reference);
+
+}
