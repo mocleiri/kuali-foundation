6235ad7976d9decb79a49ea669bc856957d30bdd
jcaddel <jcaddel@kuali.org>
2014-02-24 12:34:50 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33044 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/json/pojo/BasketTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/pojo/BasketTest.java
@@ -3,6 +3,7 @@ package org.kuali.common.devops.json.pojo;
 import static org.junit.Assert.assertEquals;
 import static org.kuali.common.devops.json.pojo.Apple.newApple;
 import static org.kuali.common.devops.json.pojo.Basket.newBasket;
+import static org.kuali.common.util.base.Exceptions.illegalState;
 
 import java.util.List;
 
@@ -21,14 +22,30 @@ public class BasketTest {
 			Basket basket1 = newBasket("straw", createApples(), Optional.of(newApple("green")));
 			ObjectMapper mapper = new ObjectMapper();
 			mapper.registerModule(new GuavaModule());
-			String json1 = mapper.writeValueAsString(basket1);
+			String json1 = write(mapper, basket1);
 			System.out.println(json1);
-			Basket basket2 = mapper.readValue(json1, Basket.class);
-			String json2 = mapper.writeValueAsString(basket2);
+			Basket basket2 = read(mapper, json1, Basket.class);
+			String json2 = write(mapper, basket2);
 			System.out.println(json2);
 			assertEquals(json1, json2);
 		} catch (Throwable e) {
 			e.printStackTrace();
+		}
+	}
+
+	protected static <T> T read(ObjectMapper mapper, String json, Class<T> type) {
+		try {
+			return mapper.readValue(json, type);
+		} catch (Exception e) {
+			throw illegalState(e);
+		}
+	}
+
+	protected static <T> String write(ObjectMapper mapper, T reference) {
+		try {
+			return mapper.writer().withDefaultPrettyPrinter().writeValueAsString(reference);
+		} catch (Exception e) {
+			throw illegalState(e);
 		}
 	}
 
