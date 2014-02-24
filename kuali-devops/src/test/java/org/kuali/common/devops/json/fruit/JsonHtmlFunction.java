74c0baa962eac4d9ada8afdaeec0120ded9e507e
jcaddel <jcaddel@kuali.org>
2014-02-23 19:05:11 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33024 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/json/fruit/JsonHtmlFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/fruit/JsonHtmlFunction.java
@@ -1,5 +1,6 @@
 package org.kuali.common.devops.json.fruit;
 
+import static com.google.common.base.Optional.absent;
 import static com.google.common.base.Optional.fromNullable;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Lists.newArrayList;
@@ -7,7 +8,11 @@ import static org.kuali.common.util.base.Exceptions.illegalState;
 import static org.kuali.common.util.base.Precondition.checkNotBlank;
 import static org.kuali.common.util.base.Precondition.checkNotNull;
 import static org.kuali.common.util.build.BuilderUtils.findPublicStaticBuilderClass;
+import static org.springframework.core.CollectionFactory.createCollection;
+import static org.springframework.util.ReflectionUtils.findField;
 
+import java.lang.reflect.Field;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.lang3.builder.Builder;
@@ -45,22 +50,70 @@ public class JsonHtmlFunction implements Function<Node<JsonDescriptor>, String> 
 		}
 		Class<?> typeToCreate = findTypeToCreate(desc);
 		strings.add(tr("type to create", typeToCreate.getSimpleName()));
-		Optional<?> builderClass = findPublicStaticBuilderClass(typeToCreate);
+		Optional<Class<Builder<?>>> builderClass = getBuilderClass(typeToCreate);
 		if (builderClass.isPresent()) {
-			@SuppressWarnings("unchecked")
-			Class<Builder<?>> builder = (Class<Builder<?>>) builderClass.get();
-			strings.add(tr("builder", builder.getCanonicalName()));
-		} else {
-			Object instance = getInstance(desc);
+			strings.add(tr("builder", builderClass.get().getCanonicalName()));
+		}
+
+		if (node.isLeaf()) {
+			Object instance = getInstance(desc, builderClass);
 			strings.add(tr("instance", instance));
 		}
+
 		strings.add(tr("count", count++));
 		return "<table border=0>" + Joiner.on("").join(strings) + "</table>";
 	}
 
-	protected Object getInstance(JsonDescriptor desc) {
+	protected Optional<Class<Builder<?>>> getBuilderClass(Class<?> typeToCreate) {
+		Optional<?> builderClass = findPublicStaticBuilderClass(typeToCreate);
+		if (builderClass.isPresent()) {
+			@SuppressWarnings("unchecked")
+			Class<Builder<?>> builder = (Class<Builder<?>>) builderClass.get();
+			return Optional.of(builder);
+		} else {
+			return absent();
+		}
+	}
+
+	protected Object getInstance(JsonDescriptor desc, Optional<Class<Builder<?>>> builderClass) {
+		if (!builderClass.isPresent()) {
+			return readValue(desc.getType(), desc.getNode());
+		} else {
+			if (desc.getNode().isArray()) {
+				return getCollection(desc, builderClass);
+			} else {
+				Builder<?> builder = (Builder<?>) readValue(builderClass.get(), desc.getNode());
+				return builder.build();
+			}
+		}
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	protected Collection getCollection(JsonDescriptor desc, Optional<Class<Builder<?>>> builderClass) {
+		Class<?> type = desc.getType();
+		if (builderClass.isPresent()) {
+			Field field = desc.getDescriptor().get().getField();
+			String name = field.getName();
+			Class<?> actualBuilderClass = builderClass.get();
+			Field builderField = findField(actualBuilderClass, name);
+			Optional<Field> builderFieldType = fromNullable(builderField);
+			if (builderFieldType.isPresent()) {
+				type = builderFieldType.get().getType();
+			}
+		}
+		FieldDescriptor fd = desc.getDescriptor().get();
+		List<JsonNode> children = newArrayList(desc.getNode().elements());
+		Collection c = createCollection(type, children.size());
+		for (JsonNode child : children) {
+			Object instance = readValue(fd.getElementTypeDescriptor().getType(), child);
+			c.add(instance);
+		}
+		return c;
+	}
+
+	protected Object readValue(Class<?> type, JsonNode node) {
 		try {
-			return mapper.readValue(desc.getNode().toString(), desc.getType());
+			return mapper.readValue(node.toString(), type);
 		} catch (Exception e) {
 			throw illegalState(e);
 		}
@@ -98,8 +151,7 @@ public class JsonHtmlFunction implements Function<Node<JsonDescriptor>, String> 
 
 	protected String tr(String label, Object value) {
 		checkNotBlank(label, "label");
-		checkNotNull(value, "value");
-		return "<tr><td align=right>" + label + "</td><td>&nbsp;</td><td>" + value.toString() + "</td></tr>";
+		return "<tr><td align=right>" + label + "</td><td>&nbsp;</td><td>" + value + "</td></tr>";
 	}
 
 }
