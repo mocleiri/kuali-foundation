cf0389a9b9be12b8d2211ddf37c86a7111138ad9
jcaddel <jcaddel@kuali.org>
2014-02-24 22:45:39 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33116 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/cache/Caches.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/cache/Caches.java
@@ -1,7 +1,7 @@
 package org.kuali.common.devops.cache;
 
 import static org.kuali.common.devops.cache.UrlLoader.newUrlLoader;
-import static org.kuali.common.devops.cache.function.UrlToFileFunction.newUrlToFileFunction;
+import static org.kuali.common.devops.cache.function.UrlToFileFunction2.newUrlToFileFunction2;
 import static org.kuali.common.http.model.HttpContext.newHttpContext;
 
 import java.io.File;
@@ -30,7 +30,7 @@ public class Caches {
 	public static <T, V> FileSystemCache<String, HttpWaitResult> buildUrlCache(HttpContext context, File basedir, String encoding) {
 		CacheLoader<String, HttpWaitResult> loader = newUrlLoader(context);
 		PersistentCache<File, HttpWaitResult> fileCache = new UrlFileCache();
-		Function<String, File> keyConverter = newUrlToFileFunction(basedir);
+		Function<String, File> keyConverter = newUrlToFileFunction2(basedir);
 
 		FileSystemCache.Builder<String, HttpWaitResult> builder = FileSystemCache.builder();
 		builder.keyConverter(keyConverter);
