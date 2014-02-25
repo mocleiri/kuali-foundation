c8e17bde35cd1f66553d9bbda111a3d5ebe2e3d0
jcaddel <jcaddel@kuali.org>
2014-02-24 20:03:50 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33098 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-httpclient/src/test/java/org/kuali/common/http/model/HttpWaitResultTest.java
+++ kuali-httpclient/src/test/java/org/kuali/common/http/model/HttpWaitResultTest.java
@@ -3,6 +3,7 @@ package org.kuali.common.http.model;
 import static java.lang.System.currentTimeMillis;
 import static org.junit.Assert.assertEquals;
 import static org.kuali.common.http.model.HttpRequestResult.newHttpRequestResult;
+import static org.kuali.common.http.model.HttpStatus.IO_EXCEPTION;
 import static org.kuali.common.http.model.HttpStatus.SUCCESS;
 import static org.kuali.common.http.model.HttpWaitResult.newHttpWaitResult;
 
@@ -12,27 +13,38 @@ import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.google.common.base.Optional;
 
 public class HttpWaitResultTest {
 
 	private final boolean pretty = false;
 
 	@Test
-	public void test() throws Exception {
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			mapper.registerModule(new GuavaModule());
-			HttpRequestResult hrr = newHttpRequestResult(new IOException("uhoh"), currentTimeMillis());
-			HttpWaitResult result1 = newHttpWaitResult(SUCCESS, hrr, currentTimeMillis());
-			String json1 = writeString(mapper, result1);
-			System.out.println(json1);
-			HttpWaitResult result2 = mapper.readValue(json1, HttpWaitResult.class);
-			String json2 = writeString(mapper, result2);
-			System.out.println(json2);
-			assertEquals(json1, json2);
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+	public void testWithException() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new GuavaModule());
+		HttpRequestResult hrr = newHttpRequestResult(new IOException("uhoh"), currentTimeMillis());
+		HttpWaitResult result1 = newHttpWaitResult(IO_EXCEPTION, hrr, currentTimeMillis());
+		String json1 = writeString(mapper, result1);
+		System.out.println(json1);
+		HttpWaitResult result2 = mapper.readValue(json1, HttpWaitResult.class);
+		String json2 = writeString(mapper, result2);
+		System.out.println(json2);
+		assertEquals(json1, json2);
+	}
+
+	@Test
+	public void testWithoutException() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new GuavaModule());
+		HttpRequestResult hrr = newHttpRequestResult("OK", 200, Optional.of("Hello World"), currentTimeMillis());
+		HttpWaitResult result1 = newHttpWaitResult(SUCCESS, hrr, currentTimeMillis());
+		String json1 = writeString(mapper, result1);
+		System.out.println(json1);
+		HttpWaitResult result2 = mapper.readValue(json1, HttpWaitResult.class);
+		String json2 = writeString(mapper, result2);
+		System.out.println(json2);
+		assertEquals(json1, json2);
 	}
 
 	protected <T> String writeString(ObjectMapper mapper, T reference) throws IOException {
