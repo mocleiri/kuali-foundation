100f7604a83b4851b3083411e38acc056cbb84b9
jcaddel <jcaddel@kuali.org>
2014-02-26 19:53:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33226 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/build/DefaultValidatingBuilder.java
@@ -0,0 +1,49 @@
+package org.kuali.common.util.build;
+
+import static org.kuali.common.util.validate.Validation.checkConstraints;
+
+import java.util.List;
+
+import javax.validation.Validator;
+
+import org.apache.commons.lang3.builder.Builder;
+import org.kuali.common.util.validate.Validation;
+
+import com.google.common.collect.ImmutableList;
+
+public abstract class DefaultValidatingBuilder<T> implements Builder<T> {
+
+	protected Validator validator = Validation.getDefaultValidator();
+	protected List<Class<?>> validationGroups = ImmutableList.of();
+
+	protected T validate(T instance) {
+		return checkConstraints(instance, validator, validationGroups);
+	}
+
+	public DefaultValidatingBuilder<T> validator(Validator validator) {
+		this.validator = validator;
+		return this;
+	}
+
+	public DefaultValidatingBuilder<T> validationGroups(List<Class<?>> validationGroups) {
+		this.validationGroups = validationGroups;
+		return this;
+	}
+
+	public Validator getValidator() {
+		return validator;
+	}
+
+	public void setValidator(Validator validator) {
+		this.validator = validator;
+	}
+
+	public List<Class<?>> getValidationGroups() {
+		return validationGroups;
+	}
+
+	public void setValidationGroups(List<Class<?>> validationGroups) {
+		this.validationGroups = validationGroups;
+	}
+
+}
--- kuali-util/src/test/java/org/kuali/common/util/build/ValidatingBuilder.java
+++ kuali-util/src/test/java/org/kuali/common/util/build/ValidatingBuilder.java
@@ -1,59 +1,17 @@
 package org.kuali.common.util.build;
 
-import static org.kuali.common.util.validate.Validation.checkConstraints;
-
-import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
 
-import org.apache.commons.lang3.builder.Builder;
-import org.kuali.common.util.validate.Validation;
-
-import com.google.common.collect.ImmutableList;
-
-public abstract class ValidatingBuilder<T> implements Builder<T> {
+public abstract class ValidatingBuilder<T> extends DefaultValidatingBuilder<T> {
 
 	private static final Class<?>[] EMPTY_CLASS_ARRAY = {};
-
-	protected Validator validator = Validation.getDefaultValidator();
-	protected List<Class<?>> validationGroups = ImmutableList.of();
 
 	public abstract Set<ConstraintViolation<T>> violations();
 
 	protected Set<ConstraintViolation<T>> violations(T instance) {
 		return validator.validate(instance, validationGroups.toArray(EMPTY_CLASS_ARRAY));
-	}
-
-	protected T validate(T instance) {
-		return checkConstraints(instance, validator, validationGroups);
-	}
-
-	public ValidatingBuilder<T> validator(Validator validator) {
-		this.validator = validator;
-		return this;
-	}
-
-	public ValidatingBuilder<T> validationGroups(List<Class<?>> validationGroups) {
-		this.validationGroups = validationGroups;
-		return this;
-	}
-
-	public Validator getValidator() {
-		return validator;
-	}
-
-	public void setValidator(Validator validator) {
-		this.validator = validator;
-	}
-
-	public List<Class<?>> getValidationGroups() {
-		return validationGroups;
-	}
-
-	public void setValidationGroups(List<Class<?>> validationGroups) {
-		this.validationGroups = validationGroups;
 	}
 
 }
