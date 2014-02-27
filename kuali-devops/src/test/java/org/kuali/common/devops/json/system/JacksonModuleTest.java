2c9031f6d39f290641d0ba7909fb1cf243ca4483
jcaddel <jcaddel@kuali.org>
2014-02-27 11:40:49 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33261 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/json/system/JacksonModuleTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/system/JacksonModuleTest.java
@@ -9,7 +9,6 @@ import org.kuali.common.devops.json.pojo.JacksonJsonService;
 import org.kuali.common.devops.json.pojo.JsonService;
 
 import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.google.common.collect.ImmutableList;
@@ -21,18 +20,14 @@ public class JacksonModuleTest {
 		try {
 			Properties props = new Properties();
 			props.setProperty("foo.bar", "baz");
-			props.setProperty("a", "1");
-			props.setProperty("b", "2");
-			props.setProperty("c", "3");
+			props.setProperty("a", "b");
 
-			ObjectMapper mapper = new ObjectMapper();
-			mapper.registerModule(getModule());
 			List<Module> modules = ImmutableList.of(new GuavaModule(), getModule());
 			JacksonContext context = JacksonContext.builder().withModules(modules).build();
 			JsonService service1 = new JacksonJsonService(context);
 			System.out.println(service1.writeString(props));
-			JsonService service2 = new JacksonJsonService();
-			System.out.println(service2.writeString(props));
+			// JsonService service2 = new JacksonJsonService();
+			// System.out.println(service2.writeString(props));
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
