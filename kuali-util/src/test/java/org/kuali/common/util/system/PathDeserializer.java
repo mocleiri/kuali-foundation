66281d2d3a723e3647034e3ebc3dcf220ab7f65c
jcaddel <jcaddel@kuali.org>
2014-02-27 21:12:09 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33300 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/PathDeserializer.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/PathDeserializer.java
@@ -1,9 +1,13 @@
 package org.kuali.common.util.system;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static java.io.File.pathSeparatorChar;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
+import org.kuali.common.util.file.CanonicalFile;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,13 +15,19 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.google.common.base.Splitter;
 
-public class PathDeserializer extends JsonDeserializer<List<String>> {
+public class PathDeserializer extends JsonDeserializer<List<File>> {
 
 	private final Splitter splitter = Splitter.on(pathSeparatorChar);
 
 	@Override
-	public List<String> deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-		return splitter.splitToList(jp.getText());
+	public List<File> deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+		List<String> tokens = splitter.splitToList(jp.getText());
+		List<File> files = newArrayList();
+		for (String token : tokens) {
+			String canonical = new CanonicalFile(token).getPath();
+			files.add(new File(canonical));
+		}
+		return files;
 	}
 
 }
