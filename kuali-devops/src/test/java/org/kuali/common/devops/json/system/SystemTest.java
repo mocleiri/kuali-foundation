76b190723782a195ba2272b51934075234eeeb82
jcaddel <jcaddel@kuali.org>
2014-02-28 14:29:56 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33334 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/json/system/SystemTest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/system/SystemTest.java
@@ -32,6 +32,7 @@ public class SystemTest {
 	@Test
 	public void test() {
 		try {
+			Properties yo = new Properties();
 			String separator = ".";
 			Properties system = System.getProperties();
 			Properties props = new SystemPropertiesFunction().apply(system);
