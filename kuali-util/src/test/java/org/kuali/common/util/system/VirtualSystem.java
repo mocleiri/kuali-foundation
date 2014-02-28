0ffb4d59947f90b2817629e6bcbf46f649b83a95
jcaddel <jcaddel@kuali.org>
2014-02-27 20:03:21 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33282 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/VirtualSystem.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/VirtualSystem.java
@@ -33,10 +33,7 @@ public final class VirtualSystem {
 		this.fileSeparator = builder.fileSeparator;
 		this.properties = ImmutableProperties.copyOf(builder.properties);
 		this.environment = ImmutableProperties.copyOf(builder.environment);
-	}
 
-	public static Builder builder() {
-		return new Builder();
 	}
 
 	public static class Builder extends SimpleValidatingBuilder<VirtualSystem> {
@@ -44,57 +41,56 @@ public final class VirtualSystem {
 		private User user;
 		private OperatingSystem os;
 		private Java java;
-		private String pathSeparator;
 		private String lineSeparator;
+		private String pathSeparator;
 		private String fileSeparator;
 		private Properties properties;
 		private Properties environment;
+
+		public Builder withUser(User user) {
+			this.user = user;
+			return this;
+		}
+
+		public Builder withOs(OperatingSystem os) {
+			this.os = os;
+			return this;
+		}
+
+		public Builder withJava(Java java) {
+			this.java = java;
+			return this;
+		}
+
+		public Builder withLineSeparator(String lineSeparator) {
+			this.lineSeparator = lineSeparator;
+			return this;
+		}
+
+		public Builder withPathSeparator(String pathSeparator) {
+			this.pathSeparator = pathSeparator;
+			return this;
+		}
+
+		public Builder withFileSeparator(String fileSeparator) {
+			this.fileSeparator = fileSeparator;
+			return this;
+		}
+
+		public Builder withProperties(Properties properties) {
+			this.properties = properties;
+			return this;
+		}
+
+		public Builder withEnvironment(Properties environment) {
+			this.environment = environment;
+			return this;
+		}
 
 		@Override
 		public VirtualSystem build() {
 			return validate(new VirtualSystem(this));
 		}
-
-		public Builder properties(Properties properties) {
-			this.properties = properties;
-			return this;
-		}
-
-		public Builder environment(Properties environment) {
-			this.environment = environment;
-			return this;
-		}
-
-		public Builder user(User user) {
-			this.user = user;
-			return this;
-		}
-
-		public Builder os(OperatingSystem os) {
-			this.os = os;
-			return this;
-		}
-
-		public Builder java(Java java) {
-			this.java = java;
-			return this;
-		}
-
-		public Builder lineSeparator(String lineSeparator) {
-			this.lineSeparator = lineSeparator;
-			return this;
-		}
-
-		public Builder pathSeparator(String pathSeparator) {
-			this.pathSeparator = pathSeparator;
-			return this;
-		}
-
-		public Builder fileSeparator(String fileSeparator) {
-			this.fileSeparator = fileSeparator;
-			return this;
-		}
-
 	}
 
 	public User getUser() {
