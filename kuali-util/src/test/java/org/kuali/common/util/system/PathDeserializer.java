475a38afbd854bd9859e549f7d61d1f9a919b7cd
jcaddel <jcaddel@kuali.org>
2014-02-27 20:52:27 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33295 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/PathDeserializer.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/PathDeserializer.java
@@ -7,17 +7,12 @@ import java.util.List;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.google.common.base.Splitter;
 
-public class PathDeserializer extends StdDeserializer<List<String>> {
+public class PathDeserializer extends JsonDeserializer<List<String>> {
 
-	private static final long serialVersionUID = 1L;
 	private final Splitter splitter = Splitter.on(File.pathSeparatorChar);
-
-	public PathDeserializer(Class<List<String>> type) {
-		super(type);
-	}
 
 	@Override
 	public List<String> deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
