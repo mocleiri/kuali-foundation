cb426db288cadd2300f2ca5d319552fa94f396ca
jcaddel <jcaddel@kuali.org>
2014-02-28 14:34:59 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33338 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/json/system/SystemPropertiesFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/system/SystemPropertiesFunction.java
@@ -43,7 +43,7 @@ public final class SystemPropertiesFunction implements Function<Properties, Prop
 
 	protected void removeBlanks(Properties properties, Set<String> exceptions) {
 		for (String key : newTreeSet(properties.stringPropertyNames())) {
-			if (!blanksAllowed.contains(key) && isBlank(properties.getProperty(key))) {
+			if (isBlank(properties.getProperty(key)) && !blanksAllowed.contains(key)) {
 				properties.remove(key);
 			}
 		}
