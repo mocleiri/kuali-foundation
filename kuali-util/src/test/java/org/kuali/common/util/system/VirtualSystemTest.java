729efbcbdaf139bb68b94eafd4bace2db9b41412
jcaddel <jcaddel@kuali.org>
2014-03-02 13:50:08 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33434 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/VirtualSystemTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/VirtualSystemTest.java
@@ -1,21 +1,27 @@
 package org.kuali.common.util.system;
 
+import static com.google.common.base.Stopwatch.createStarted;
 import static org.junit.Assert.assertEquals;
 import static org.kuali.common.util.system.VirtualSystemFactory.newVirtualSystem;
 
 import org.junit.Test;
+import org.kuali.common.util.FormatUtils;
 import org.kuali.common.util.json.api.JsonService;
 import org.kuali.common.util.json.jackson.JacksonJsonService;
+
+import com.google.common.base.Stopwatch;
 
 public class VirtualSystemTest {
 
 	@Test
 	public void test() {
+		Stopwatch sw = createStarted();
 		VirtualSystem vs = newVirtualSystem();
 		JsonService service = new JacksonJsonService();
 		String expected = service.writeString(vs);
 		String actual = service.writeString(service.readString(expected, VirtualSystem.class));
 		assertEquals(expected, actual);
+		System.out.println(FormatUtils.getTime(sw));
 	}
 
 }
