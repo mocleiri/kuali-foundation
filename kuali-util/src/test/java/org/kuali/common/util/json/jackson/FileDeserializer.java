47479d23ca920bdf621517c84e166a464b760ad6
jcaddel <jcaddel@kuali.org>
2014-03-02 10:56:24 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33402 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/json/jackson/FileDeserializer.java
@@ -0,0 +1,19 @@
+package org.kuali.common.util.json.jackson;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+public class FileDeserializer extends JsonDeserializer<File> {
+
+	@Override
+	public File deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+		String canonical = new File(jp.getText()).getCanonicalPath();
+		return new File(canonical);
+	}
+
+}
