2231b73dd5196b4edaef8190450ecf02c1278e58
jcaddel <jcaddel@kuali.org>
2014-03-01 22:00:15 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33365 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/json/system/SystemPropertiesFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/json/system/SystemPropertiesFunction.java
@@ -1,88 +1,43 @@
 package org.kuali.common.devops.json.system;
 
-import static com.google.common.collect.Maps.newHashMap;
-import static com.google.common.collect.Sets.newTreeSet;
-import static org.apache.commons.lang3.StringUtils.isBlank;
+import static com.google.common.base.Optional.fromNullable;
+import static org.kuali.common.util.base.Precondition.checkNotBlank;
 import static org.kuali.common.util.base.Precondition.checkNotNull;
+import static org.kuali.common.util.system.VirtualSystem.PROPERTY_MAPPINGS;
+import static org.kuali.common.util.system.VirtualSystem.UNIVERSAL_SYSTEM_PROPERTY_KEYS;
 
-import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
 import org.kuali.common.util.validate.IdiotProofImmutable;
 
 import com.google.common.base.Function;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
+import com.google.common.base.Optional;
 
 /**
- * Convert default JDK system properties into the properties needed by {@code VirtualSystem}, and in the process remove all blank properties except for {@code line.separator}
+ * Convert default JDK system properties into the properties needed by {@code VirtualSystem}
  */
 @IdiotProofImmutable
 public final class SystemPropertiesFunction implements Function<Properties, Properties> {
 
-	private static final Map<String, String> DEFAULT_MAPPINGS = getDefaultMappings();
-
-	public SystemPropertiesFunction() {
-		this(ImmutableSet.of("line.separator"), DEFAULT_MAPPINGS);
-	}
-
-	public SystemPropertiesFunction(Set<String> blanksAllowed, Map<String, String> mappings) {
-		this.blanksAllowed = ImmutableSet.copyOf(blanksAllowed);
-		this.mappings = ImmutableMap.copyOf(mappings);
-	}
-
-	private final ImmutableSet<String> blanksAllowed;
-	private final ImmutableMap<String, String> mappings;
+	private static final String LINE_SEPARATOR_KEY = "line.separator";
 
 	@Override
 	public Properties apply(Properties oldProperties) {
 		checkNotNull(oldProperties, "oldProperties");
 		Properties newProperties = new Properties();
-		newProperties.putAll(oldProperties);
-		removeBlanks(newProperties, blanksAllowed);
-		for (String newKey : newTreeSet(mappings.keySet())) {
-			String oldKey = mappings.get(newKey);
-			newProperties.setProperty(newKey, newProperties.getProperty(oldKey));
-			newProperties.remove(oldKey);
+		for (String key : UNIVERSAL_SYSTEM_PROPERTY_KEYS) {
+			String value = checkNotNull(oldProperties.getProperty(key), key);
+			Optional<String> mappedKey = fromNullable(PROPERTY_MAPPINGS.get(key));
+			String actualKey = mappedKey.isPresent() ? mappedKey.get() : key;
+			if (LINE_SEPARATOR_KEY.equals(key)) {
+				// The only universal system property allowed to be blank is the line separator
+				newProperties.setProperty(actualKey, value);
+			} else {
+				// All other properties cannot be blank
+				newProperties.setProperty(actualKey, checkNotBlank(value, key));
+			}
 		}
 		return newProperties;
 	}
 
-	protected void removeBlanks(Properties properties, Set<String> exceptions) {
-		for (String key : newTreeSet(properties.stringPropertyNames())) {
-			if (isBlank(properties.getProperty(key)) && !blanksAllowed.contains(key)) {
-				properties.remove(key);
-			}
-		}
-	}
-
-	public static Map<String, String> getDefaultMappings() {
-		Map<String, String> mappings = newHashMap();
-		mappings.put("pathSeparator", "path.separator");
-		mappings.put("fileSeparator", "file.separator");
-		mappings.put("lineSeparator", "line.separator");
-		mappings.put("java.classpath", "java.class.path");
-		mappings.put("java.classVersion", "java.class.version");
-		mappings.put("java.tmpDir", "java.io.tmpdir");
-		mappings.put("java.extensionDirs", "java.ext.dirs");
-		mappings.put("java.endorsedDirs", "java.endorsed.dirs");
-		mappings.put("java.libraryPaths", "java.library.path");
-		mappings.put("java.runtime.version", "java.version");
-		mappings.put("java.runtime.vendor", "java.vendor");
-		mappings.put("java.runtime.vendorUrl", "java.vendor.url");
-		mappings.put("java.runtime.specification.version", "java.specification.version");
-		mappings.put("java.runtime.specification.vendor", "java.specification.vendor");
-		mappings.put("java.runtime.specification.name", "java.specification.name");
-		return ImmutableMap.copyOf(mappings);
-
-	}
-
-	public Set<String> getBlanksAllowed() {
-		return blanksAllowed;
-	}
-
-	public Map<String, String> getMappings() {
-		return mappings;
-	}
 }
