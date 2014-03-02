fa60703e4e1b5fbd1271aa32438b3d29dbf06d7f
jcaddel <jcaddel@kuali.org>
2014-03-01 21:33:16 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33362 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/system/VirtualSystem.java
+++ kuali-util/src/test/java/org/kuali/common/util/system/VirtualSystem.java
@@ -1,17 +1,37 @@
 package org.kuali.common.util.system;
 
+import static com.google.common.collect.Maps.newHashMap;
+import static com.google.common.collect.Sets.newHashSet;
+
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import org.kuali.common.util.build.ValidatingBuilder;
 import org.kuali.common.util.property.ImmutableProperties;
 import org.kuali.common.util.validate.IdiotProofImmutable;
 import org.kuali.common.util.validate.IgnoreBlanks;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
 @IdiotProofImmutable
 @JsonDeserialize(builder = VirtualSystem.Builder.class)
 public final class VirtualSystem {
+
+	/**
+	 * Set of system property keys required to be present on every JVM
+	 */
+	@JsonIgnore
+	public static final ImmutableSet<String> UNIVERSAL_PROPERTY_KEYS = getUniversalPropertyKeys();
+
+	/**
+	 * Mappings between universal property keys and the strongly typed field they correspond to in the VirtualSystem object
+	 */
+	@JsonIgnore
+	public static final ImmutableMap<String, String> PROPERTY_MAPPINGS = getPropertyMappings();
 
 	private final User user;
 	private final OperatingSystem os;
@@ -93,6 +113,60 @@ public final class VirtualSystem {
 		public VirtualSystem build() {
 			return validate(new VirtualSystem(this));
 		}
+	}
+
+	private static final ImmutableSet<String> getUniversalPropertyKeys() {
+		Set<String> keys = newHashSet();
+		keys.add("java.version");
+		keys.add("java.vendor");
+		keys.add("java.vendor.url");
+		keys.add("java.home");
+		keys.add("java.vm.specification.version");
+		keys.add("java.vm.specification.vendor");
+		keys.add("java.vm.specification.name");
+		keys.add("java.vm.version");
+		keys.add("java.vm.vendor");
+		keys.add("java.vm.name");
+		keys.add("java.specification.version");
+		keys.add("java.specification.vendor");
+		keys.add("java.specification.name");
+		keys.add("java.class.version");
+		keys.add("java.class.path");
+		keys.add("java.library.path");
+		keys.add("java.io.tmpdir");
+		keys.add("java.compiler");
+		keys.add("java.ext.dirs");
+		keys.add("os.name");
+		keys.add("os.arch");
+		keys.add("os.version");
+		keys.add("file.separator");
+		keys.add("path.separator");
+		keys.add("line.separator");
+		keys.add("user.name");
+		keys.add("user.home");
+		keys.add("user.dir");
+		return ImmutableSet.copyOf(keys);
+	}
+
+	private static final ImmutableMap<String, String> getPropertyMappings() {
+		Map<String, String> mappings = newHashMap();
+		mappings.put("pathSeparator", "path.separator");
+		mappings.put("fileSeparator", "file.separator");
+		mappings.put("lineSeparator", "line.separator");
+		mappings.put("java.classpath", "java.class.path");
+		mappings.put("java.classVersion", "java.class.version");
+		mappings.put("java.tmpDir", "java.io.tmpdir");
+		mappings.put("java.extensionDirs", "java.ext.dirs");
+		mappings.put("java.endorsedDirs", "java.endorsed.dirs");
+		mappings.put("java.libraryPaths", "java.library.path");
+		mappings.put("java.runtime.version", "java.version");
+		mappings.put("java.runtime.vendor", "java.vendor");
+		mappings.put("java.runtime.vendorUrl", "java.vendor.url");
+		mappings.put("java.runtime.specification.version", "java.specification.version");
+		mappings.put("java.runtime.specification.vendor", "java.specification.vendor");
+		mappings.put("java.runtime.specification.name", "java.specification.name");
+		return ImmutableMap.copyOf(mappings);
+
 	}
 
 	public User getUser() {
