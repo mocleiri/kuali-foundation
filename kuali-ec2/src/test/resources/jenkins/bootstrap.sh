c380d922f3d656a68d603bde623ebde06eac6a6c
jcaddel <jcaddel@kuali.org>
2014-03-03 15:54:39 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33538 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/jenkins/bootstrap.sh
+++ kuali-ec2/src/test/resources/jenkins/bootstrap.sh
@@ -1,8 +1,11 @@
-HOSTNAME=jeff.ci
-DOMAIN=kuali.org
+# Subversion
 SVN_REPO=https://svn.kuali.org/repos/foundation
 SVN_PATH=trunk/kuali-ec2
 SVN_URL=$SVN_REPO/$SVN_PATH
+
+# DNS
+HOSTNAME=jeff.ci
+DOMAIN=kuali.org
 FQDN=$HOSTNAME.$DOMAIN
 
 ssh ubuntu@$FQDN 'sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys'
