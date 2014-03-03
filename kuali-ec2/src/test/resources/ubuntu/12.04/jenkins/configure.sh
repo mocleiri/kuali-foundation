af36616d9453f80930e838a66987c976ca660a6d
jcaddel <jcaddel@kuali.org>
2014-03-03 18:43:54 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33590 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/configure.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/configure.sh
@@ -4,9 +4,16 @@ TOMCAT7=tomcat7
 TOMCAT6=tomcat6
 TOMCAT=$TOMCAT7
 
+JENKINS_VERSION=1.532.2
+
 TOMCAT_DIR=/var/lib/$TOMCAT
 TOMCAT_ROOT=$TOMCAT_DIR/webapps/ROOT
+TOMCAT_ROOT_WAR=$TOMCAT_DIR/webapps/ROOT.war
 
 service $TOMCAT stop
 
-rm -rf $TOMCAT_ROOT; mkdir -p $TOMCAT_ROOT;
+rm -rf $TOMCAT_ROOT TOMCAT_ROOT_WAR
+
+wget http://maven.kuali.org/external/org/jenkins/jenkins/$JENKINS_VERSION/jenkins-$JENKINS_VERSION.war -O $TOMCAT_ROOT_WAR
+
+service $TOMCAT start
