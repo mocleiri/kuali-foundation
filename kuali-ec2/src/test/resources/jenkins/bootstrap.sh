66d1b06f868a90099aeb3de459329ae3eda84745
jcaddel <jcaddel@kuali.org>
2014-03-03 13:51:56 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33509 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/jenkins/bootstrap.sh
+++ kuali-ec2/src/test/resources/jenkins/bootstrap.sh
@@ -6,4 +6,4 @@ FQDN=$HOSTNAME.$DOMAIN
 ssh ubuntu@$FQDN 'sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys'
 
 ssh root@$FQDN 'wget https://svn.kuali.org/repos/foundation/trunk/kuali-ec2/src/test/resources/jenkins/initial_setup.sh; chmod 755 ./initial_setup.sh'
-ssh root@$FQDN './initial_setup.sh $PWD $HOSTNAME $DOMAIN'
+ssh root@$FQDN './initial_setup.sh silent $PWD $HOSTNAME $DOMAIN'
