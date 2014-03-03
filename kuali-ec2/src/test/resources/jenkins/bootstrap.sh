a6b5da25d48ed594cdbe16c3b97cbd9b59850866
jcaddel <jcaddel@kuali.org>
2014-03-03 16:31:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33550 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/jenkins/bootstrap.sh
+++ kuali-ec2/src/test/resources/jenkins/bootstrap.sh
@@ -2,6 +2,7 @@
 SVN_REPO=https://svn.kuali.org/repos/foundation
 SVN_PATH=trunk/kuali-ec2
 SVN_URL=$SVN_REPO/$SVN_PATH
+SVN_DIR=/mnt/kuali-ec2
 
 # DNS
 HOSTNAME=jeff.ci
@@ -10,7 +11,8 @@ FQDN=$HOSTNAME.$DOMAIN
 
 # Nexus
 NEXUS_PASSWORD=$1
+BOOTSTRAP=$SVN_DIR/bootstrap.sh
 
 ssh ubuntu@$FQDN 'sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys'
-ssh root@$FQDN 'apt-get install subversion -y; rm -rf /mnt/kuali-ec2; svn --quiet checkout '$SVN_URL' /mnt/kuali-ec2'
-ssh root@$FQDN 'echo "Server1" >> /mnt/kuali-ec2/foo.txt'
+ssh root@$FQDN 'apt-get install subversion -y; rm -rf /mnt/kuali-ec2; svn --quiet checkout '$SVN_URL' '$SVN_DIR''
+ssh root@$FQDN 'rm -rf '$BOOTSTRAP'; echo '$SVN_DIR'/src/test/resources/jenkins/initial_setup.sh '$NEXUS_PASSWORD' > '$BOOTSTRAP'; chmod 744 '$BOOTSTRAP''
