51579f1295026eef24d4e30466db30d47cfdc405
jcaddel <jcaddel@kuali.org>
2014-03-03 16:58:02 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33558 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/jenkins/bootstrap.sh
+++ kuali-ec2/src/test/resources/jenkins/bootstrap.sh
@@ -15,19 +15,12 @@ NEXUS_PASSWORD=$1
 # Bash
 BOOTSTRAP=$SVN_DIR/bootstrap.sh
 SETUP=$SVN_DIR/src/test/resources/jenkins/initial_setup.sh
-BASH1="rm -rf '$BOOTSTRAP'"
-BASH2="echo '$SETUP' silent '$NEXUS_PASSWORD' '$HOSTNAME' '$DOMAIN' > '$BOOTSTRAP'"
-BASH3="chmod 755 '$BOOTSTRAP'"
-BASH="$BASH1; $BASH2; $BASH3"
-
-SVN1="apt-get install subversion -y"
-SVN2="rm -rf $SVN_DIR"
-SVN3="svn --quiet checkout '$SVN_URL' '$SVN_DIR'"
-SVN="$SVN1; $SVN2; $SVN3"
 
 # Enable root ssh
 ssh ubuntu@$FQDN 'sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys'
 
-#
-ssh root@$FQDN '$SVN; $BASH'
-#ssh root@$FQDN 'rm -rf '$BOOTSTRAP'; echo '$SETUP' silent '$NEXUS_PASSWORD' '$DOMAIN' '$HOSTNAME' > '$BOOTSTRAP'; chmod 755 '$BOOTSTRAP''
+# Checkout kuali-ec2
+ssh root@$FQDN 'apt-get install subversion -y; rm -rf $SVN_DIR; svn --quiet checkout '$SVN_URL' '$SVN_DIR''
+
+# Create the bootstrap.sh script
+ssh root@$FQDN 'rm -rf '$BOOTSTRAP'; echo '$SETUP' silent '$NEXUS_PASSWORD' '$HOSTNAME' '$DOMAIN' > '$BOOTSTRAP'; chmod 755 '$BOOTSTRAP''
