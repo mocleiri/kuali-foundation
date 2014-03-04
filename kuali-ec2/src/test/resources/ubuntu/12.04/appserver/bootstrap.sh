9bb440afe0ef676a473dacc3c86578df05c8f6a2
jcaddel <jcaddel@kuali.org>
2014-03-04 02:43:50 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33785 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/appserver/bootstrap.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/appserver/bootstrap.sh
@@ -57,11 +57,7 @@ ssh ubuntu@$FQDN 'sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorize
 # Checkout kuali-ec2
 ssh root@$FQDN 'apt-get install subversion -y -qq; rm -rf $SVN_DIR; svn --quiet checkout '$SVN_URL' '$SVN_DIR''
 
-# Create the bootstrap.sh script on the remote server
-#ssh root@$FQDN 'rm -rf '$BOOTSTRAP'; echo '$CONFIGURE' '$NEXUS_PASSWORD' '$HOSTNAME' '$JDK' '$TOMCAT' '$MAX_HEAP' '$MAX_PERM' > '$BOOTSTRAP'; chmod 755 '$BOOTSTRAP''
+# Run configure.sh on the remote server
 ssh root@$FQDN ''$CONFIGURE' '$NEXUS_PASSWORD' '$HOSTNAME' '$JDK' '$TOMCAT' '$MAX_HEAP' '$MAX_PERM''
-
-# Run the bootstrap script on the remote server
-#ssh root@$FQDN $BOOTSTRAP
 
 echo $(date)
