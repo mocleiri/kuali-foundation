c51928549a0f76676214b6a3e779b7ba5fba0184
jcaddel <jcaddel@kuali.org>
2014-03-04 15:04:23 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33854 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/master.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/master.sh
@@ -45,7 +45,8 @@ function install_plugin {
 	echo $PLUGIN_URL
 	PLUGIN_DIR=$JENKINS_HOME/plugins
 	PLUGIN_FILE=$PLUGIN_DIR/$PLUGIN_NAME.jpi
-	curl $PLUGIN_URL --silent --location --create-dirs --output $PLUGIN_FILE 
+	curl $PLUGIN_URL --silent --location --create-dirs --output $PLUGIN_FILE
+	touch $PLUGIN_FILE.pinned 
 	chown -R $TOMCAT:$TOMCAT $TOMCAT_HOME  
   
 }
