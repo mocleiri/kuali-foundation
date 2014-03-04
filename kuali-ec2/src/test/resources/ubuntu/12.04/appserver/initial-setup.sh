cbf9e9b18fd517803e55b3384df1a720bec7e273
jcaddel <jcaddel@kuali.org>
2014-03-03 21:05:35 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33654 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/appserver/initial-setup.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/appserver/initial-setup.sh
@@ -137,7 +137,9 @@ if [ $NEXUS_PASSWORD == "NOTDEFINED" ]; then
 get_nexus_password
 fi;
 
-wget $QUIET --user $NEXUS_USER --password $NEXUS_PASSWORD $NEXUS_URL/$NEXUS_JDK_LOCATION/$JDK_ZIP_FILE --output-document $DOWNLOADS/$JDK_ZIP_FILE
+URL=$NEXUS_URL/$NEXUS_JDK_LOCATION/$JDK_ZIP_FILE
+OUTPUT_FILE=$DOWNLOADS/$JDK_ZIP_FILE
+wget $QUIET --user $NEXUS_USER --password $NEXUS_PASSWORD $URL --output-document $OUTPUT_FILE
 
 if [ ! -f $DOWNLOADS/$JDK_ZIP_FILE ]; then
   echo "$DOWNLOADS/$JDK_ZIP_FILE does not exist!"
