c9e1b79bb03fc555b831d021038e69d6d9712378
jcaddel <jcaddel@kuali.org>
2014-03-03 21:06:42 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33655 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/appserver/initial-setup.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/appserver/initial-setup.sh
@@ -139,7 +139,9 @@ fi;
 
 URL=$NEXUS_URL/$NEXUS_JDK_LOCATION/$JDK_ZIP_FILE
 OUTPUT_FILE=$DOWNLOADS/$JDK_ZIP_FILE
+echo "downloading -> $URL"
 wget $QUIET --user $NEXUS_USER --password $NEXUS_PASSWORD $URL --output-document $OUTPUT_FILE
+echo "created     -> $OUTPUT_FILE"
 
 if [ ! -f $DOWNLOADS/$JDK_ZIP_FILE ]; then
   echo "$DOWNLOADS/$JDK_ZIP_FILE does not exist!"
