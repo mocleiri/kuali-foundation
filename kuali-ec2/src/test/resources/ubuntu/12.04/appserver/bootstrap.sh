ee02eadf8442c8d515cae0228a832631bf11af3a
jcaddel <jcaddel@kuali.org>
2014-03-04 13:03:43 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33815 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/appserver/bootstrap.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/appserver/bootstrap.sh
@@ -48,7 +48,7 @@ MAX_HEAP=${5-5g}
 MAX_PERM=${6-512m}
 QUIET=${7-""}
 
-CONFIGURE=$SCRIPTS_DIR/configure.sh
+CONFIGURE="$SCRIPTS_DIR/configure.sh $NEXUS_PASSWORD $HOSTNAME $JDK $TOMCAT $MAX_HEAP $MAX_PERM $QUIET"
 
 SVN1="apt-get install subversion -y $QUIET"
 SVN2="rm -rf $SVN_DIR"
@@ -61,6 +61,6 @@ ssh ubuntu@$FQDN 'sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorize
 
 # Checkout kuali-ec2 on the remote server and run configure.sh
 echo "configure -> application server"
-ssh root@$FQDN "$SVN; '$CONFIGURE' '$NEXUS_PASSWORD' '$HOSTNAME' '$JDK' '$TOMCAT' '$MAX_HEAP' '$MAX_PERM' '$QUIET'"
+ssh root@$FQDN "$SVN; $CONFIGURE"
 
 echo $(date)
