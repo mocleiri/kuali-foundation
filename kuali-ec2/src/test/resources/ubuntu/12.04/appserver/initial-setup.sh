bdabf2e70d476e7595e350f11f82864cb27b5aed
jcaddel <jcaddel@kuali.org>
2014-03-03 21:08:42 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33656 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/appserver/initial-setup.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/appserver/initial-setup.sh
@@ -97,7 +97,7 @@ $SCRIPTS_DIR/unattended-upgrades.sh
 # Setup port redirect rules
 function redirect_rules {
 echo
-echo "Setting up port re-direct rules."
+echo "redirecting all internal/external traffic on port 80 -> 8080"
 echo
 iptables --table nat --append PREROUTING --protocol tcp --dport 80 --jump REDIRECT --to-port 8080
 iptables -t nat -A OUTPUT -p tcp -o lo --dport 80 -j DNAT --to 127.0.0.1:8080
