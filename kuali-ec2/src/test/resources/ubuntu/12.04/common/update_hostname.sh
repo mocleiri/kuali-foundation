940457b4ca7f7986dc4864415f7f66d57ff32b4f
jcaddel <jcaddel@kuali.org>
2014-03-05 15:45:55 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34134 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-ec2/src/test/resources/ubuntu/12.04/common/update_hostname.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright 2004-2014 The Kuali Foundation
+#
+# Licensed under the Educational Community License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.opensource.org/licenses/ecl2.php
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Set hostname and FQDN
+function set_hostname {
+  MYIP=$(ifconfig eth0 |grep inet | awk '{ print $2 }' | awk 'BEGIN { FS=":" } { print $2 }')
+
+  ETC_HOSTS="$MYIP $HOSTNAME.$DOMAIN $HOSTNAME"
+  echo "hostname  -> $HOSTNAME"
+  echo "hosts     -> $ETC_HOSTS"
+
+  echo "$HOSTNAME" > /etc/hostname
+  hostname $HOSTNAME
+
+  echo '127.0.0.1 localhost' > /etc/hosts
+  echo $ETC_HOSTS >> /etc/hosts
+ 
+  # Mike's magic line here always appends to /etc/hosts
+  # This breaks with the concept of idempotence
+  # And also if for some reason we gave the machine a new DNS name I think we'd have issues 
+  #eval "sed -i -e '/127.0.0.1/a$ETC_HOSTS' /etc/hosts"
+}
