00e26856899e48ecc072fc618c33a3f0987fabfa
jcaddel <jcaddel@kuali.org>
2014-03-05 16:37:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34145 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-ec2/src/test/resources/ubuntu/12.04/appserver/simple.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright 2004-2014 The Kuali Foundation
+#
+# Licensed under the Educational Community License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.opensource.org/licenses/ecl2.php
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+echo $(date)
+
+# generic functions
+function check_not_blank {
+  if ! [ -n "$2" ]; then 
+    echo $1 cannot be blank
+    show_usage
+  fi
+}
+
+# module specific functions
+function show_usage {
+  echo
+  echo requires HOSTNAME NEXUS_PASSWORD
+  echo usage: bootstrap.sh hostname password
+  echo
+  exit 1
+}
+
+function check_args {
+  check_not_blank HOSTNAME $HOSTNAME
+  check_not_blank NEXUS_PASSWORD $NEXUS_PASSWORD
+}
+
+echo $(date)
+echo $(date)
+
+
