e63ad79fb562b3a7b55f89428ba59ade3ab8aec8
jcaddel <jcaddel@kuali.org>
2014-03-05 17:22:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34163 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/appserver/simple.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/appserver/simple.sh
@@ -61,7 +61,7 @@ function checkout_module {
 
 function configure_application_server {
   BASICS="$MODULES/common/install_basics.sh $BASEDIR"
-  JAVA="$MODULES/common/install_java.sh $JDK"
+  JAVA="$MODULES/common/install_java.sh $BASEDIR $JDK $NEXUS_PASSOWORD"
   DNS="$MODULES/common/update_hostname.sh $SUBDOMAIN $DOMAIN"
   SSH="$BASICS; $JAVA; $DNS"
   ssh root@$FQDN "$SSH"
