464813c3427f02e266d2bcdb8497f51cd3e2f29c
jcaddel <jcaddel@kuali.org>
2014-03-05 11:47:45 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34028 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/common/simple.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/common/simple.sh
@@ -15,80 +15,12 @@
 # limitations under the License.
 #
 
-# Install JDK
-function install_jdk {
-
-  URL=$NEXUS_URL/$NEXUS_JDK_LOCATION/$JDK_ZIP_FILE
-  OUTPUT_FILE=$DOWNLOADS/$JDK_ZIP_FILE
-  echo "download  -> $URL"
-  wget $QUIET --user $NEXUS_USER --password $NEXUS_PASSWORD $URL --output-document $OUTPUT_FILE
-  echo "to        -> $OUTPUT_FILE"
-
-  # Make sure the JDK and the symbolic link are both gone
-  JDK_TARGET=$JDK_BASEDIR/$JDK_UNZIP_DIR
-  JDK_LINK=$JDK_BASEDIR/$JDK_ARTIFACT_ID
-  echo "install   -> $JDK_LINK -> $JDK_TARGET"
-  rm -rf $JDK_LINK $JDK_TARGET
-
-  # Unpack the JDK into /usr/java
-  unzip $QUIET -o $DOWNLOADS/$JDK_ZIP_FILE -d $JDK_BASEDIR
-
-  # Create a symbolic link for /usr/java/jdk7 -> /usr/java/jdk7-1.7.0-u51
-  ln -s $JDK_TARGET $JDK_LINK
-}
-
 function check_args {
-  echo yo1
   if [ -n "$1" ]; then
-    echo yo2
+    echo "bad  juju"
+  else
+    echo "good juju"
   fi
 }
-
-function check_arg {
-  echo yo
-  echo $1 $2
-  ARG=$1; 
-  ARG_NAME=$2
-  echo $ARG
-  if [ $ARG == "" ]; then
-    echo must provide $ARG_NAME
-    show_usage
-  fi
-  exit 1
-}
-
-function show_usage {
-  echo
-  echo requires JDK BASEDIR NEXUS_PASSWORD
-  echo usage: install_java.sh jdk6/jdk7
-  echo
-}
-
-JDK6_VERSION=1.6.0-u45
-JDK7_VERSION=1.7.0-u51
-
-JDK6=jdk6
-JDK7=jdk7
-
-#JDK=$(eval echo \${JDK${JDK_LEVEL}})
-#JDK_VERSION=$(eval echo \${JDK${JDK_LEVEL}_VERSION})
-
-JDK_GROUP_ID=com/oracle
-JDK_ARTIFACT_ID=$JDK
-JDK_CLASSIFIER=linux-x64
-
-JDK_UNZIP_DIR=$JDK_ARTIFACT_ID-$JDK_VERSION
-JDK_ZIP_FILE=$JDK_ARTIFACT_ID-$JDK_VERSION-$JDK_CLASSIFIER.zip
-JDK_BASEDIR=/usr/java
-
-NEXUS_URL=http://nexus.kuali.org/content/groups/developer
-NEXUS_JDK_LOCATION=$JDK_GROUP_ID/$JDK_ARTIFACT_ID/$JDK_VERSION
-NEXUS_USER=developer
-NEXUS_JDK_DOWNLOAD_FILE=$DOWNLOADS/$JDK_ZIP_FILE
-
-# Directory for the JDK download
-DOWNLOADS=$BASEDIR/target/downloads/jdk
-# rm -rf $DOWNLOADS; mkdir -p $DOWNLOADS
 
 check_args
-#install_jdk
