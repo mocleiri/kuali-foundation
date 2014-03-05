f8e1feb6ce19194dc69a750289cb3051c84d2279
jcaddel <jcaddel@kuali.org>
2014-03-04 21:46:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33911 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/common.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Copyright 2004-2014 The Kuali Foundation
+#
+# Licensed under the Educational Community License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.opensource.org/licenses/ecl2.php
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+MAVEN_BASEDIR=/usr/maven
+
+function install_packages {
+  echo "install   -> custom packages"
+  apt-get install subversion git graphviz -y -qq > /dev/null 2>&1
+}
+
+function install_maven {
+  MAVEN_VERSION=$1
+  MAVEN_ABBR=$2
+
+  echo "install   -> maven $MAVEN_VERSION"
+
+  MAVEN_ARTIFACT_ID=apache-maven
+  MAVEN_ZIP=$MAVEN_ARTIFACT_ID-$MAVEN_VERSION-bin.zip
+  MAVEN_URL=http://search.maven.org/remotecontent?filepath=org/apache/maven/apache-maven/$MAVEN_VERSION/$MAVEN_ZIP
+  MAVEN_DIR=$MAVEN_BASEDIR/$MAVEN_ARTIFACT_ID-$MAVEN_VERSION
+  MAVEN_FILE=$MAVEN_BASEDIR/$MAVEN_ZIP
+  curl $MAVEN_URL --silent --location --create-dirs --output $MAVEN_FILE
+  MAVEN_TARGET=$MAVEN_DIR
+  MAVEN_LINK=$MAVEN_BASEDIR/mvn$MAVEN_ABBR
+  MAVEN_USR_BIN=/usr/bin/mvn$MAVEN_ABBR
+
+  rm -rf $MAVEN_DIR $MAVEN_LINK $MAVEN_USR_BIN
+  unzip -qq $MAVEN_FILE -d $MAVEN_BASEDIR
+  chmod -R 755 $MAVEN_DIR
+  ln -s $MAVEN_TARGET $MAVEN_LINK
+  ln -s $MAVEN_BASEDIR/mvn$MAVEN_ABBR/bin/mvn $MAVEN_USR_BIN
+}
+
+function install_default_maven {
+  MAVEN_ABBR=$1
+
+  echo "install   -> default maven mvn$MAVEN_ABBR"
+
+  MAVEN_TARGET=$MAVEN_BASEDIR/mvn$MAVEN_ABBR/bin/mvn
+  MAVEN_USR_BIN=/usr/bin/mvn
+
+  rm -rf $MAVEN_USR_BIN
+  ln -s $MAVEN_TARGET $MAVEN_USR_BIN
+}
+
+function configure_java {
+
+  echo "configure -> default java"
+  cp /mnt/kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/root/.bashrc /root/.bashrc
+  rm -rf /usr/bin/java
+  ln -s /usr/java/jdk7/bin/java /usr/bin/java
+  
+}
+
+configure_java
+install_packages
+install_maven 3.2.1 32
+install_maven 3.1.0 31
+install_maven 3.0.5 30
+install_default_maven 30
