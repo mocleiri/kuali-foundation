bf90b876063e61e40361572747ba56db53769f16
jcaddel <jcaddel@kuali.org>
2014-03-05 14:25:56 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34092 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/common/simple.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/common/simple.sh
@@ -45,12 +45,12 @@ JDK=${2-$JDK}
 NEXUS_PASSWORD=${3-$NEXUS_PASSWORD}
 JDK_LEVEL=${JDK:3:1}
 
-# Make sure we have what we need to continue
-check_args
-
 # Change these as new versions become available
 JDK6_VERSION=1.6.0-u45
 JDK7_VERSION=1.7.0-u51
+
+# Make sure we have what we need to continue
+check_args
 
 echo $JDK
 echo $JDK_LEVEL
