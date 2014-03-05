f7e6ec11234274e4dd36385dab22c69fb26bada5
jcaddel <jcaddel@kuali.org>
2014-03-04 22:51:18 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@33937 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/appserver/configure.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/appserver/configure.sh
@@ -61,7 +61,7 @@ TOMCAT_GROUP=$TOMCAT
 TOMCAT_DIR=/var/lib/$TOMCAT
 TOMCAT_LOGS=$TOMCAT_DIR/logs
 TOMCAT_HOME=/home/$TOMCAT
-JAVA_OPTS="\"-Duser.home=$TOMCAT_HOME -Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xms512m -Xmx$MAX_HEAP -XX:MaxPermSize=$MAX_PERM -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+PrintTenuringDistribution -Xloggc:$TOMCAT_LOGS/heap.log -XX:HeapDumpPath=$TOMCAT_LOGS -XX:+HeapDumpOnOutOfMemoryError"\"
+JAVA_OPTS="\"-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xms512m -Xmx$MAX_HEAP -XX:MaxPermSize=$MAX_PERM -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+PrintTenuringDistribution -Xloggc:$TOMCAT_LOGS/heap.log -XX:HeapDumpPath=$TOMCAT_LOGS -XX:+HeapDumpOnOutOfMemoryError"\"
 JAVA_HOME=/usr/java/$JDK
 
 
