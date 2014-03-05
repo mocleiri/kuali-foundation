b4311ab5f04d07eacdcd310fa94bf2c4dd8d594f
jcaddel <jcaddel@kuali.org>
2014-03-05 12:16:00 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34060 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/common/simple.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/common/simple.sh
@@ -16,7 +16,7 @@
 #
 
 function check_args {
-  # If this is true, JDK is not blank.  It is set and it is not the empty string
+  # Make sure JDK is set and is not the empty string
   if ! [ -n "$JDK" ]; then 
     show_usage
   fi
