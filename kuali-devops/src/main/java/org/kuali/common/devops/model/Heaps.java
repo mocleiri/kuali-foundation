a423b0e81e03aa3a9fe4f3eb6028355c171a542e
jcaddel <jcaddel@kuali.org>
2014-03-06 18:20:34 -0500

    updates
    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34500 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/model/Heaps.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.common.devops.model;
-
-
-/**
- * A mapping of recommended heap settings to Amazon EC2 instance types.
- */
-public enum Heaps {
-}
--- kuali-jdbc/src/main/java/org/kuali/common/jdbc/reset/JdbcResetConfig.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common.jdbc.reset;
-
-import org.kuali.common.util.execute.Executable;
-
-public interface JdbcResetConfig {
-
-	Executable jdbcResetExecutable();
-
-}
