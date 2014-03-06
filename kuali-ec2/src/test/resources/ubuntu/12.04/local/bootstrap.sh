fb37a2ba6359d031935abb72bbf0ea9cab1e51a7
jcaddel <jcaddel@kuali.org>
2014-03-05 19:35:30 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34204 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/local/bootstrap.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/local/bootstrap.sh
@@ -15,18 +15,6 @@
 # limitations under the License.
 #
 
-# generic functions
-function check_not_blank {
-  if ! [ -n "$2" ]; then 
-    echo $1 cannot be blank
-    show_usage
-  fi
-}
-
-function check_args {
-  check_not_blank SUBDOMAIN $SUBDOMAIN
-}
-
 function enable_root_ssh {
   echo "enable    -> root ssh"
   SSH="sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys"
@@ -46,10 +34,6 @@ function checkout_module {
   ssh root@$FQDN "$SSH"
 }
 
-# Make sure we have what we need to continue
-check_args
-
-LOCAL=/Users
 DOMAIN=kuali.org
 BASEDIR=/mnt/kuali-ec2
 MODULES=$BASEDIR/src/test/resources/ubuntu/12.04
