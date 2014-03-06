b4ee92111b75b45c54cf1309c3b170229d26a0d2
jcaddel <jcaddel@kuali.org>
2014-03-05 20:43:22 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34245 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/local/installers.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/local/installers.sh
@@ -1,7 +1,7 @@
 function check_installer_args {
   check_not_blank MODULE $MODULE
   check_not_blank BASEDIR $BASEDIR
-  check_not_blank JDK $JDK
+  check_not_blank JDKK $JDKK
   check_not_blank NEXUS_PASSWORD $NEXUS_PASSWORD
   check_not_blank SUBDOMAIN $SUBDOMAIN
   check_not_blank DOMAIN $DOMAIN
