9c4acf38f37f4373fbd90b3e30e81181dc6244bb
jcaddel <jcaddel@kuali.org>
2014-03-06 01:29:58 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34355 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/common.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/common.sh
@@ -137,7 +137,7 @@ function touch_subversion_repo {
   SVN_REPO=$1
   check_not_blank SVN_REPO $SVN_REPO
   SVN_MESSAGE="Automated auth check"
-  SVN_TOUCH_URL="https://svn.kuali.org/repos/$SVN_REPO/automated-auth-check"
+  SVN_TOUCH_URL="https://svn.kuali.org/repos/$SVN_REPO/automated-auth-check-touch"
   SVN_AUTH="--username=$SVN_USERNAME --password=$SVN_PASSWORD"
   SVN_CREATE="svn mkdir  $SVN_TOUCH_URL -m \"$SVN_MESSAGE\" $SVN_AUTH"
   SVN_DELETE="svn delete $SVN_TOUCH_URL -m \"$SVN_MESSAGE\""
