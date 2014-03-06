f5a786a00a1315994ee12523e8f94baa64d08369
jcaddel <jcaddel@kuali.org>
2014-03-06 17:29:33 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34484 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/local/installers.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/local/installers.sh
@@ -1,13 +1,12 @@
 function show_usage {
   echo
-  echo requires MODULES BASEDIR JDK NEXUS_PASSWORD SUBDOMAIN DOMAIN
+  echo requires MODULES JDK NEXUS_PASSWORD SUBDOMAIN DOMAIN
   echo
   exit 1
 }
 
 function check_args {
   check_not_blank MODULES $MODULES
-  check_not_blank BASEDIR $BASEDIR
   check_not_blank JDK $JDK
   check_not_blank NEXUS_PASSWORD $NEXUS_PASSWORD
   check_not_blank SUBDOMAIN $SUBDOMAIN
