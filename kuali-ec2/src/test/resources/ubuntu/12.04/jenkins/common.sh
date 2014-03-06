db4b21fd9db5edc1bfc39a2742a227d37177dded
jcaddel <jcaddel@kuali.org>
2014-03-06 01:08:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34331 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/common.sh
+++ kuali-ec2/src/test/resources/ubuntu/12.04/jenkins/common.sh
@@ -131,6 +131,7 @@ function configure_subversion {
   echo "[global]"                      >  $SVN_SERVERS
   echo "store-plaintext-passwords=yes" >> $SVN_SERVERS
   
+  SVN_MESSAGE="Automated Jenkins auth check"
   SVN_TOUCH_URL="https://svn.kuali.org/repos/foundation/sandbox/kuali-devops/temp/nightly/touch"
   SVN_AUTH="--username=$SVN_USERNAME --password=$SVN_PASSWORD"
   SVN_CREATE="svn mkdir --parents $SVN_TOUCH_URL $SVN_AUTH"
