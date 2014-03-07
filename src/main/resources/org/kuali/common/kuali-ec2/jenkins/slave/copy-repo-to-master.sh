8abd15362255b1fb0874b53e49c07faf0eaab77a
jcaddel <jcaddel@kuali.org>
2014-03-07 02:53:20 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34647 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/resources/org/kuali/common/kuali-ec2/jenkins/slave/copy-repo-to-master.sh
+++ src/main/resources/org/kuali/common/kuali-ec2/jenkins/slave/copy-repo-to-master.sh
@@ -3,20 +3,10 @@
 echo $(date)
 set -xe
 
-function check_not_blank {
-  if [ ! -n "$2" ]; then 
-    echo $1 cannot be blank
-    exit 1
-  fi
-}
-
-JENKINS_MASTER=$1
-check_not_blank JENKINS_MASTER $JENKINS_MASTER
-
 # The trailing slash is significant here
 REPO=/root/.m2/repository
 SRC=$REPO/
-DST=root@$JENKINS_MASTER:$REPO
+DST=root@${jenkins.master}:$REPO
 EXCLUDEFILE=/usr/share/ec2slave/rsync-excludes
 
 # Make the rsync call
