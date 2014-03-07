69bee10ddd9484f3ad3755a0039fd266dd9432e8
jcaddel <jcaddel@kuali.org>
2014-03-06 19:58:29 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34514 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/local/bootstrap.sh
+++ src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/local/bootstrap.sh
@@ -37,10 +37,9 @@ function copy_jar {
   LOCAL="$HOME/.m2/repository/${project.groupId.path}/${project.artifactId}/${project.version}/${project.artifactId}-${project.version}.jar"
   REMOTE="/mnt/${project.artifactId}.jar"
   
-  echo "copy      -> $LOCAL"
+  echo "copy      -> $REMOTE"
 
   SCP="$LOCAL root@$FQDN:$REMOTE"
-  echo $SCP
   scp $SCP
 }
 
