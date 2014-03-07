7a2d73f0093c0b36d664819d75ad3b5c4f9b90ac
jcaddel <jcaddel@kuali.org>
2014-03-07 12:35:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34707 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/jenkins/slave/copy_repo_to_master.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo $(date)
+set -xe
+
+REPO=/root/.m2/repository
+# The trailing slash is significant here
+SRC=$REPO/
+JENKINS_MASTER=${jenkins.master}
+DST=root@$JENKINS_MASTER:$REPO
+EXCLUDEFILE=/usr/share/ec2slave/rsync.excludes
+
+# Make the rsync call
+rsync --archive --stats -v $SRC $DST --exclude-from $EXCLUDEFILE
+
+set +x
+echo $(date)
