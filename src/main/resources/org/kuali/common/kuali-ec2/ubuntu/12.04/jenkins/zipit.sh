dc9e621ee1fa30e641f8d4cadda68e48efd1485f
jcaddel <jcaddel@kuali.org>
2014-03-07 18:06:09 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34790 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/jenkins/zipit.sh
+++ src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/jenkins/zipit.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 function check_not_blank {
   if [ ! -n "$2" ]; then 
     echo $1 cannot be blank
