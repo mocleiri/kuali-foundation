0564604f91d028a5dcdefc9fa4fcd2ef1babea7b
jcaddel <jcaddel@kuali.org>
2014-03-07 22:39:05 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34842 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/jenkins/master.sh
+++ src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/jenkins/master.sh
@@ -121,7 +121,7 @@ function configure_jenkins {
   
   echo "configure -> jenkins:application"
   MASTER_CONFIG_ZIP=$BASEDIR/${project.groupId.path}/${project.artifactId}/jenkins/master_config.zip
-  unzip $QUIET -o -e -r --password $ZIP_PASSWORD $MASTER_CONFIG_ZIP -d $TOMCAT_HOME
+  unzip $QUIET -o -P $ZIP_PASSWORD $MASTER_CONFIG_ZIP -d $TOMCAT_HOME
   chown -R $TOMCAT:$TOMCAT $TOMCAT_HOME
   
 }
