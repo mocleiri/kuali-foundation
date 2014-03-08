d9e358eb27164c936a35c75ee49c1e862f03f362
jcaddel <jcaddel@kuali.org>
2014-03-08 00:08:45 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34881 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/local/zipjenkinsconfig.sh
+++ src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/local/zipjenkinsconfig.sh
@@ -35,7 +35,7 @@ function scp_zip_file {
   
   SSH1="rm -f $SERVER_ZIP"
   SSH2="cd /home/tomcat7"
-  SSH3="zip -qq -e --password $ZIP_PASSWORD -r $SERVER_ZIP .jenkins -x '**/plugins/*' '**/jobs/*'"
+  SSH3="zip -qq -e --password $ZIP_PASSWORD -r $SERVER_ZIP .jenkins -x '**/plugins/*'"
   SSH="$SSH1; $SSH2; $SSH3"
 
   echo "zip -> root@$FQDN:$SERVER_ZIP"
