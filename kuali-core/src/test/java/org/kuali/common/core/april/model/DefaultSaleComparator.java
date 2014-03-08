466fdd94f307fb41827cea27815dbfffd6d1534e
jcaddel <jcaddel@kuali.org>
2014-03-08 17:04:03 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34941 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/test/java/org/kuali/common/core/april/model/DefaultSaleComparator.java
+++ kuali-core/src/test/java/org/kuali/common/core/april/model/DefaultSaleComparator.java
@@ -4,12 +4,14 @@ import java.util.Comparator;
 
 import com.google.common.collect.ComparisonChain;
 
+// Singleton enum pattern
 public enum DefaultSaleComparator implements Comparator<Sale> {
 	INSTANCE;
 
 	@Override
 	public int compare(Sale one, Sale two) {
-		return ComparisonChain.start().compare(one.getLevel(), two.getLevel()).compare(one.getArea(), two.getArea()).result();
+		return ComparisonChain.start().compare(one.getLevel(), two.getLevel()).compare(one.getArea(), two.getArea()).compare(one.getSection(), two.getSection())
+				.compare(one.getRow(), two.getRow()).compare(one.getPrice(), two.getPrice()).result();
 	}
 
 }
