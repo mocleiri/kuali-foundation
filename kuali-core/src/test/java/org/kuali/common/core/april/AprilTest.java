a20a0c1c6bc8b9be09cbc1d1f79129e71b327aa3
jcaddel <jcaddel@kuali.org>
2014-03-08 13:30:10 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34902 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/test/java/org/kuali/common/core/april/AprilTest.java
+++ kuali-core/src/test/java/org/kuali/common/core/april/AprilTest.java
@@ -1,11 +1,14 @@
 package org.kuali.common.core.april;
 
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.String.format;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
 import java.util.List;
 
 import org.junit.Test;
+import org.kuali.common.core.april.model.SaleLines;
 import org.kuali.common.util.LocationUtils;
 import org.slf4j.Logger;
 
@@ -19,8 +22,18 @@ public class AprilTest {
 			logger.info(format("hello world"));
 			List<String> lines = LocationUtils.readLines("classpath:json/april-01.txt");
 			logger.info(format("line count: %s", lines.size()));
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected List<SaleLines> getSaleLines(List<String> lines) {
+		checkState(lines.size() == 300, "expected exactly 100 sales (3 lines per sale)");
+		List<SaleLines> sales = newArrayList();
+		for (int i=0;i<100;i+=3) {
+			
+		}
+		return null;
 	}
 }
