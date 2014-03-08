ce11e5dcaae7eecffaf939e51881fc3885a35284
jcaddel <jcaddel@kuali.org>
2014-03-08 15:02:34 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34922 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/main/java/org/kuali/common/core/system/VirtualSystem.java
+++ kuali-core/src/main/java/org/kuali/common/core/system/VirtualSystem.java
@@ -24,7 +24,7 @@ public final class VirtualSystem {
 	private final ImmutableProperties environment;
 
 	/**
-	 * Produce an immutable VirtualSystem representing the current state of the system we are running on.
+	 * Create a new immutable VirtualSystem instance representing the current state of the system we are running on.
 	 */
 	public static VirtualSystem create() {
 		return VirtualSystemHelper.createVirtualSystem();
