09a57cb4cc61a0875cf8acae4f013e23603f0e9f
jcaddel <jcaddel@kuali.org>
2014-03-07 21:39:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34805 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/jenkins/master.sh
+++ src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/jenkins/master.sh
@@ -119,7 +119,7 @@ function configure_tomcat_user {
 
 function configure_jenkins {
   
-  echo "configure -> jenkins:"
+  echo "configure -> jenkins:application"
   
   MASTER_SRC_DIR=$BASEDIR/${project.groupId.path}/${project.artifactId}/jenkins/master
   
