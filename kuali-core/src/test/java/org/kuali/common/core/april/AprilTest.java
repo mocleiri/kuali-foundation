cdbfd52b14d64d5e8140363e2a3bf159f775c7ee
jcaddel <jcaddel@kuali.org>
2014-03-08 13:06:42 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34898 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/test/java/org/kuali/common/core/april/AprilTest.java
+++ kuali-core/src/test/java/org/kuali/common/core/april/AprilTest.java
@@ -3,7 +3,10 @@ package org.kuali.common.core.april;
 import static java.lang.String.format;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
+import java.util.List;
+
 import org.junit.Test;
+import org.kuali.common.util.LocationUtils;
 import org.slf4j.Logger;
 
 public class AprilTest {
@@ -14,6 +17,8 @@ public class AprilTest {
 	public void test() {
 		try {
 			logger.info(format("hello world"));
+			List<String> lines = LocationUtils.readLines("classpath:json/april-01.txt");
+			logger.info("line count: %s", lines.size());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
