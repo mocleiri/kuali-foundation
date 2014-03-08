c475b7ebe487b2e166b6899373512af75a49dc2b
jcaddel <jcaddel@kuali.org>
2014-03-08 18:05:04 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34956 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/test/java/org/kuali/common/core/april/model/Sale.java
+++ kuali-core/src/test/java/org/kuali/common/core/april/model/Sale.java
@@ -17,7 +17,7 @@ import com.google.common.base.Objects;
 
 @IdiotProofImmutable
 @JsonDeserialize(builder = Sale.Builder.class)
-@JsonPropertyOrder(value = { "date", "level", "area", "section", "row", "price", "quantity" }, alphabetic = true)
+@JsonPropertyOrder(value = { "date", "level", "area", "price", "section", "row", "quantity" }, alphabetic = true)
 public final class Sale {
 
 	private final Level level;
