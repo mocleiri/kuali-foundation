3816cd64018673b62ef6306f6ec2a13acbc836ad
jcaddel <jcaddel@kuali.org>
2014-03-07 22:46:57 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-ec2-2.x@34852 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/local/jenkins.sh
+++ src/main/resources/org/kuali/common/kuali-ec2/ubuntu/12.04/local/jenkins.sh
@@ -39,7 +39,7 @@ function configure_master {
   echo "configure -> jenkins:master"
   JDK=jdk6
   source installers.sh
-  MASTER="$MODULES/jenkins/master.sh $BASEDIR $QUIET"
+  MASTER="$MODULES/jenkins/master.sh $ZIP_PASSWORD $BASEDIR $QUIET"
   SSH="$JAVA; $MASTER"
   ssh root@$FQDN "$SSH"
 }
