b50570ec1de25502067eb1c5dac7de739b37a3f6
jcaddel <jcaddel@kuali.org>
2014-03-08 14:26:57 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34915 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/test/java/org/kuali/common/core/april/AprilTest.java
+++ kuali-core/src/test/java/org/kuali/common/core/april/AprilTest.java
@@ -3,14 +3,24 @@ package org.kuali.common.core.april;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.String.format;
+import static org.kuali.common.util.base.Exceptions.illegalState;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
+import org.kuali.common.core.april.model.Sale;
 import org.kuali.common.core.april.model.SaleLines;
 import org.kuali.common.util.LocationUtils;
 import org.slf4j.Logger;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
 
 public class AprilTest {
 
@@ -26,6 +36,61 @@ public class AprilTest {
 			logger.info(format("sales: %s", lines.size()));
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	protected List<Sale> getSales(List<SaleLines> list) {
+		List<Sale> sales = newArrayList();
+		for (SaleLines element : list) {
+			Sale sale = getSale(element);
+			sales.add(sale);
+		}
+		return sales;
+	}
+
+	protected Sale getSale(SaleLines saleLines) {
+		char separator = ' ';
+		Splitter splitter = Splitter.on(separator).trimResults().omitEmptyStrings();
+		Joiner joiner = Joiner.on(separator);
+
+		List<String> tokens1 = splitter.splitToList(saleLines.getLine1());
+		checkState(tokens1.size() == 4, "expected 4 tokens");
+
+		List<String> tokens3 = splitter.splitToList(saleLines.getLine3());
+		checkState(tokens3.size() == 3, "expected 3 tokens");
+
+		String section = tokens1.get(tokens1.size() - 1);
+		String area = tokens1.get(tokens1.size() - 2);
+		String level = joiner.join(ImmutableList.of(tokens1.get(0), tokens1.get(1)));
+		double price = getPrice(saleLines.getLine2().trim());
+		int row = Integer.parseInt(tokens3.get(0));
+		int quantity = Integer.parseInt(tokens3.get(1));
+		Date date = getDate(tokens3.get(2));
+
+		Sale.Builder builder = Sale.builder();
+		builder.withLevel(level).withArea(area).withSection(section);
+		builder.withPrice(price);
+		builder.withRow(row).withQuantity(quantity).withDate(date.getTime());
+		return builder.build();
+	}
+
+	protected Date getDate(String source) {
+		// 03/08/14
+		SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/YY");
+		try {
+			return sdf.parse(source);
+		} catch (ParseException e) {
+			throw illegalState("unexpected parse error", e);
+		}
+	}
+
+	protected double getPrice(String source) {
+		NumberFormat nf = NumberFormat.getCurrencyInstance();
+		try {
+			Number number = nf.parse(source);
+			return number.doubleValue();
+		} catch (ParseException e) {
+			throw illegalState("unexpected parse error", e);
 		}
 	}
 
