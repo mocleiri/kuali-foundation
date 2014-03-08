d52ad0d95d29f66b307d38de2109472f3dd68448
jcaddel <jcaddel@kuali.org>
2014-03-08 14:00:24 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34913 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/main/java/org/kuali/common/core/validate/annotation/IdiotProofImmutable.java
+++ kuali-core/src/main/java/org/kuali/common/core/validate/annotation/IdiotProofImmutable.java
@@ -10,17 +10,6 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-/**
- * Aggregate of the annotations:
- * 
- * <pre>
- *  StronglyImmutable
- *  NoNullFields
- *  NoBlanks
- *  ImmutableGuavaMaps
- *  ImmutableGuavaCollections
- * </pre>
- */
 @Target({ TYPE })
 @Retention(RUNTIME)
 @Constraint(validatedBy = {})
