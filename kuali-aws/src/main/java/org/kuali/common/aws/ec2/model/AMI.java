f9f9863dd35480cd93ab6d444a02a1a06ec1b1c5
jcaddel <jcaddel@kuali.org>
2014-03-09 16:12:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35114 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMI.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMI.java
@@ -1,23 +1,22 @@
 package org.kuali.common.aws.ec2.model;
 
-import org.kuali.common.util.Assert;
-
 public enum AMI {
 
 	// This is a bare bones Amazon Linux box with virtually nothing except yum installed on it
 	// amzn-ami-minimal-pv-2013.09.0.x86_64-ebs
-	AMAZON_LINUX_64_BIT_MINIMAL_AMI_2013_09(Distro.AMAZON, "2013.09", "ami-65792c0c");
+	AMAZON_LINUX_64_BIT_MINIMAL_AMI_2013_09(Distro.AMAZON, "2013.09", "ami-65792c0c", "amzn-ami-minimal-pv-2013.09.0.x86_64-ebs"), //
+	UBUNTU_64_BIT_PRECISE_LTS(Distro.UBUNTU, "12.04", "ami-0b9c9f62", "ubuntu/images/ebs/ubuntu-precise-12.04-amd64-server-20140227");
 
 	private final Distro distro;
 	private final String version;
 	private final String id;
+	private final String code;
 
-	private AMI(Distro distro, String version, String id) {
-		Assert.noBlanks(version, id);
-		Assert.noNulls(distro);
+	private AMI(Distro distro, String version, String id, String code) {
 		this.distro = distro;
 		this.version = version;
 		this.id = id;
+		this.code = code;
 	}
 
 	public Distro getDistro() {
@@ -30,6 +29,10 @@ public enum AMI {
 
 	public String getId() {
 		return id;
+	}
+
+	public String getCode() {
+		return code;
 	}
 
 }
