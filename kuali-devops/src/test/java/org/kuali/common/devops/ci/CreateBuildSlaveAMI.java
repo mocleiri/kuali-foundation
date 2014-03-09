0d499fb1afd2d86f84aaf88d11d8f7bc5ae97952
jcaddel <jcaddel@kuali.org>
2014-03-08 20:59:07 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34979 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/CreateBuildSlaveAMI.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/CreateBuildSlaveAMI.java
@@ -1,10 +1,16 @@
 package org.kuali.common.devops.ci;
 
-import static java.lang.String.format;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
 import org.junit.Test;
+import org.kuali.common.aws.ec2.api.EC2Service;
+import org.kuali.common.aws.ec2.impl.DefaultEC2Service;
+import org.kuali.common.aws.ec2.model.EC2ServiceContext;
 import org.kuali.common.devops.logic.Auth;
+import org.kuali.common.util.FormatUtils;
+import org.kuali.common.util.wait.DefaultWaitService;
+import org.kuali.common.util.wait.WaitContext;
+import org.kuali.common.util.wait.WaitService;
 import org.slf4j.Logger;
 
 import com.amazonaws.auth.AWSCredentials;
@@ -12,15 +18,23 @@ import com.amazonaws.auth.AWSCredentials;
 public class CreateBuildSlaveAMI {
 
 	private static final Logger logger = newLogger();
+	private final String ami = "ami-83dee0ea";
 
 	@Test
 	public void test() {
 		try {
-			AWSCredentials creds = Auth.getAwsCredentials("foundation");
-			logger.info(format("access key id: %s", creds.getAWSAccessKeyId()));
+			WaitContext wc = new WaitContext.Builder(FormatUtils.getMillis("15m")).build();
+			EC2Service service = getEC2Service();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected EC2Service getEC2Service() {
+		AWSCredentials creds = Auth.getAwsCredentials("foundation");
+		WaitService ws = new DefaultWaitService();
+		EC2ServiceContext ec = EC2ServiceContext.create(creds);
+		return new DefaultEC2Service(ec, ws);
 	}
 
 }
