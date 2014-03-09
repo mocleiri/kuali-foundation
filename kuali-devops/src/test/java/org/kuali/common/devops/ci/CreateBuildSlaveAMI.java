373a751236073cd52804697a5d70f663ffbb1f7c
jcaddel <jcaddel@kuali.org>
2014-03-08 20:43:55 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@34974 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/CreateBuildSlaveAMI.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/CreateBuildSlaveAMI.java
@@ -1,13 +1,23 @@
 package org.kuali.common.devops.ci;
 
+import static java.lang.String.format;
+import static org.kuali.common.util.log.Loggers.newLogger;
+
 import org.junit.Test;
+import org.kuali.common.devops.logic.Auth;
+import org.slf4j.Logger;
+
+import com.amazonaws.auth.AWSCredentials;
 
 public class CreateBuildSlaveAMI {
+
+	private static final Logger logger = newLogger();
 
 	@Test
 	public void test() {
 		try {
-
+			AWSCredentials creds = Auth.getAwsCredentials("foundation");
+			logger.info(format("access key id: %s", creds.getAWSAccessKeyId()));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
