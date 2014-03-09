834754aafaf43c3c8f31d6743ccd323aa00a4cab
jcaddel <jcaddel@kuali.org>
2014-03-09 00:11:40 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35009 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/NamedSecurityGroup.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/NamedSecurityGroup.java
@@ -1,10 +1,12 @@
 package org.kuali.common.devops.aws;
 
+import static org.kuali.common.aws.ec2.model.security.NamedPermission.APPLICATION_SERVER_PERMISSIONS;
+
 import java.util.List;
 
 import org.kuali.common.aws.ec2.model.security.KualiSecurityGroup;
 import org.kuali.common.aws.ec2.model.security.Permission;
-import org.kuali.common.aws.ec2.model.security.Permissions;
+import org.kuali.common.aws.ec2.model.security.NamedPermission;
 
 import com.google.common.collect.ImmutableList;
 
@@ -13,9 +15,9 @@ public enum NamedSecurityGroup {
 	// TODO Remove the Permissions.WEB_SERVER from the CI security group here
 	// That is just a group with no perm's so that the slaves and master belong to the same group and thus
 	// have unrestricted network access to each other from Amazon's perspective
-	CI("ci", "Continuous Integration", Permissions.APPLICATION_SERVER_PERMISSIONS), //
-	CI_MASTER("ci.master", "Jenkins CI Server - Master", Permissions.APPLICATION_SERVER_PERMISSIONS), //
-	CI_BUILD_SLAVE("ci.slave", "Jenkins CI Server - Slave", Permissions.SSH.getPermission());
+	CI("ci", "Continuous Integration", APPLICATION_SERVER_PERMISSIONS), //
+	CI_MASTER("ci.master", "Jenkins CI Server - Master", APPLICATION_SERVER_PERMISSIONS), //
+	CI_BUILD_SLAVE("ci.slave", "Jenkins CI Server - Slave", NamedPermission.SSH_PERMISSION.getPermission());
 
 	private final KualiSecurityGroup group;
 
