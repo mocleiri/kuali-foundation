dd2cc00bf350eab103c5477c9333b5e559dcce3d
jcaddel <jcaddel@kuali.org>
2014-03-09 00:01:31 -0500

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35002 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/RootVolume.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/RootVolume.java
@@ -5,6 +5,7 @@ import static com.google.common.base.Optional.absent;
 import org.kuali.common.core.build.ValidatingBuilder;
 import org.kuali.common.core.validate.annotation.IdiotProofImmutable;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
 import com.google.common.base.Optional;
 
 @IdiotProofImmutable
@@ -23,14 +24,24 @@ public final class RootVolume {
 		private Optional<Integer> sizeInGigabytes = absent();
 		private Optional<Boolean> deleteOnTermination = absent();
 
+		@JsonSetter
 		public Builder withSizeInGigabytes(Optional<Integer> sizeInGigabytes) {
 			this.sizeInGigabytes = sizeInGigabytes;
 			return this;
 		}
 
+		@JsonSetter
 		public Builder withDeleteOnTermination(Optional<Boolean> deleteOnTermination) {
 			this.deleteOnTermination = deleteOnTermination;
 			return this;
+		}
+
+		public Builder withSizeInGigabytes(int sizeInGigabytes) {
+			return withSizeInGigabytes(Optional.of(sizeInGigabytes));
+		}
+
+		public Builder withDeleteOnTermination(boolean deleteOnTermination) {
+			return withDeleteOnTermination(Optional.of(deleteOnTermination));
 		}
 
 		@Override
