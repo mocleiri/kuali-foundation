5b34440ff68ecc932070ee7c56dc35b6967241ed
jcaddel <jcaddel@kuali.org>
2014-03-09 11:51:49 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35054 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/CreateBuildSlaveAMI.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/CreateBuildSlaveAMI.java
@@ -1,5 +1,6 @@
 package org.kuali.common.devops.ci;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.String.format;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
@@ -9,8 +10,11 @@ import org.junit.Test;
 import org.kuali.common.aws.ec2.api.EC2Service;
 import org.kuali.common.aws.ec2.impl.DefaultEC2Service;
 import org.kuali.common.aws.ec2.model.EC2ServiceContext;
+import org.kuali.common.aws.ec2.model.LaunchInstanceContext;
 import org.kuali.common.aws.ec2.model.RootVolume;
 import org.kuali.common.aws.ec2.model.security.KualiSecurityGroup;
+import org.kuali.common.core.ssh.KeyPair;
+import org.kuali.common.devops.aws.KeyPairBuilders;
 import org.kuali.common.devops.aws.NamedSecurityGroups;
 import org.kuali.common.devops.aws.Tags;
 import org.kuali.common.devops.logic.Auth;
@@ -31,25 +35,41 @@ public class CreateBuildSlaveAMI {
 	private final InstanceType type = InstanceType.C3Xlarge;
 	private final RootVolume rootVolume = RootVolume.builder().withDeleteOnTermination(true).withSizeInGigabytes(64).build();
 	private final List<KualiSecurityGroup> securityGroups = ImmutableList.of(NamedSecurityGroups.CI.getGroup(), NamedSecurityGroups.CI_BUILD_SLAVE.getGroup());
-	private final List<Tag> tags = ImmutableList.of(Tags.Name.SLAVE.getTag(), Tags.Team.DEVOPS.getTag(), Tags.Stack.TESTING.getTag());
+	private final List<Tag> tags = getTags();
 
 	@Test
 	public void test() {
 		try {
-			Instance instance = getNewSlaveInstance();
+			// Instance instance = getNewSlaveInstance();
+			Instance instance = getRunningSlaveInstance("i-385fa21b");
 			logger.info(format("public dns: %s", instance.getPublicDnsName()));
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
 	}
 
+	protected List<Tag> getTags() {
+		List<Tag> tags = newArrayList();
+		// TODO Change this to PRODUCTION when ready
+		tags.add(Tags.Stack.TESTING.getTag());
+		tags.add(Tags.Name.SLAVE.getTag());
+		tags.add(Tags.Team.DEVOPS.getTag());
+		tags.add(Tags.Vendor.JENKINS.getTag());
+		tags.add(Tags.Project.SHARED.getTag());
+		return ImmutableList.copyOf(tags);
+	}
+
+	protected Instance getRunningSlaveInstance(String instanceId) {
+		EC2Service service = getEC2Service();
+		return service.getInstance(instanceId);
+	}
+
 	protected Instance getNewSlaveInstance() {
 		EC2Service service = getEC2Service();
-		return service.getInstance("i-385fa21b");
-		// KeyPair keyPair = Auth.getKeyPair(KeyPairBuilders.FOUNDATION.getBuilder());
-		// LaunchInstanceContext context = LaunchInstanceContext.builder(ami, keyPair).withType(type).withRootVolume(rootVolume).withSecurityGroups(securityGroups).withTags(tags)
-		// .build();
-		// return service.launchInstance(context);
+		KeyPair keyPair = Auth.getKeyPair(KeyPairBuilders.FOUNDATION.getBuilder());
+		LaunchInstanceContext context = LaunchInstanceContext.builder(ami, keyPair).withType(type).withRootVolume(rootVolume).withSecurityGroups(securityGroups).withTags(tags)
+				.build();
+		return service.launchInstance(context);
 	}
 
 	protected EC2Service getEC2Service() {
