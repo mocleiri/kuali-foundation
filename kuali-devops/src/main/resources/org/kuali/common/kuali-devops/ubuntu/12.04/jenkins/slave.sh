9ad1650cd2449308178f760a491204d83d650b91
jcaddel <jcaddel@kuali.org>
2014-03-10 19:05:22 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35266 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/slave.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/slave.sh
@@ -95,12 +95,12 @@ function install_mysql_server {
 }
 
 function copy_repo_from_master {
-  echo "copy      -> maven repo"
   M2_REPO=/root/.m2/repository
   mkdir -p $M2_REPO
-  echo "rsync     -> $(date)"
+  echo "copy      -> maven repo"
+  echo "rsync     -> start $(date)"
   execute_quietly "rsync -av --stats root@$JENKINS_MASTER:$M2_REPO/ $M2_REPO" 
-  echo "rsync     -> $(date)"
+  echo "rsync     -> stop  $(date)"
 }
 
 BASEDIR=${1-BASEDIR}
