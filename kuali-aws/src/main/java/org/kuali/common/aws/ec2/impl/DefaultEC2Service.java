219a17251de03f509b7fa9854034055000797315
jcaddel <jcaddel@kuali.org>
2014-03-10 17:00:08 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35216 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/impl/DefaultEC2Service.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/impl/DefaultEC2Service.java
@@ -125,7 +125,6 @@ public final class DefaultEC2Service implements EC2Service {
 
 	@Override
 	public void deleteSnapshot(String snapshotId) {
-		logger.info(format("snapshot -> delete -> %s", snapshotId));
 		DeleteSnapshotRequest request = new DeleteSnapshotRequest();
 		request.setSnapshotId(snapshotId);
 		client.deleteSnapshot(request);
