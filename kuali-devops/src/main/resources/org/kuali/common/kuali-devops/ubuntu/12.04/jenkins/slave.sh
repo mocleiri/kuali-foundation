05a91ffbf3d286d28ca4986ad7822af4f52cae94
jcaddel <jcaddel@kuali.org>
2014-03-10 19:12:23 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35269 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/slave.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/slave.sh
@@ -98,9 +98,9 @@ function copy_repo_from_master {
   M2_REPO=/root/.m2/repository
   mkdir -p $M2_REPO
   echo "copy      -> maven repo"
-  echo "rsync     -> start $(date)"
+  echo "rsync     -> start -> $(date)"
   execute_quietly "rsync -av --stats root@$JENKINS_MASTER:$M2_REPO/ $M2_REPO" 
-  echo "rsync     -> stop  $(date)"
+  echo "rsync     -> stop  -> $(date)"
 }
 
 BASEDIR=${1-BASEDIR}
