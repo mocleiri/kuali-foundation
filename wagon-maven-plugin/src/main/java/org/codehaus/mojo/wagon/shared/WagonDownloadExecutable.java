b9b1f4111f8c3c62c1c7c8bd31954f599f9eeef0
jcaddel <jcaddel@kuali.org>
2014-03-11 02:35:46 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35360 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- wagon-maven-plugin/src/main/java/org/codehaus/mojo/wagon/shared/WagonDownloadExecutable.java
+++ wagon-maven-plugin/src/main/java/org/codehaus/mojo/wagon/shared/WagonDownloadExecutable.java
@@ -61,7 +61,7 @@ public final class WagonDownloadExecutable implements Executable {
 		long timeRemaining = millisPerFile * filesRemaining;
 		// int percent = new Double((count / (total * 1D)) * 100).intValue();
 		String amount = lpad(getSize(bytesCounter.getValue(), numberFormatter), 6);
-		Object[] args = { lpad(count, 5), total, ltime(elapsed), ltime(timeRemaining), lpad(rate, 7), amount };
+		Object[] args = { lpad(count, 5), total, ltime(elapsed), ltime(timeRemaining), lpad(rate, 8), amount };
 		logger.info(format("%s of %s [elapsed:%s  remaining:%s  rate:%s  amount: %s]", args));
 	}
 
