d06ba5107bc21cc0c88f7ebc43705102a1abdeef
jcaddel <jcaddel@kuali.org>
2014-03-11 21:56:30 -0400

    [maven-release-plugin]  copy for tag spring-maven-plugin-3.1.0
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/spring-maven-plugin-3.1.0@35441 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/test/resources/modules/simple/manifests/java6-openjdk.pp
+++
@@ -1,3 +0,0 @@
-package { "java-1.6.0-openjdk":
-  ensure => present,
-}
--- kuali-util/src/main/java/org/kuali/common/util/metainf/spring/MetaInfGroup.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.common.util.metainf.spring;
-
-public enum MetaInfGroup {
-
-	SCHEMA, DATA, CONSTRAINTS, OTHER;
-
-}
