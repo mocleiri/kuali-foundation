916d9cdd5695f8d7c2b5a6bd6ca7cac0461bdd94
jcaddel <jcaddel@kuali.org>
2014-03-12 18:14:58 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35582 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/install_tomcat.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/install_tomcat.sh
@@ -114,7 +114,7 @@ function configure_tomcat_ssl {
   unzip $QUIET -P $ZIP_PASSWORD -o $ZIP -d $TOMCAT_SSL_DIR
   
   SSL_KEYSTORE=$TOMCAT_SSL_DIR/wildcard.$DOMAIN.keystore.pksc12
-  check_exists SSL_KEYSTORE $SSL_KEYSTORE
+  check_exists $SSL_KEYSTORE
 
   openssl pkcs12 -in $SSL_KEYSTORE -out $SSL_KEYSTORE/SSLCertificateFile.pem      -clcerts -nokeys -passin pass:"$SSL_PASSWORD"
   openssl pkcs12 -in $SSL_KEYSTORE -out $SSL_KEYSTORE/SSLCertificateChainFile.pem -clcerts -nokeys -passin pass:"$SSL_PASSWORD"
