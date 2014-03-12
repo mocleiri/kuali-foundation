4a3087c72c5d3fcc49f977f04954178ceb881f9e
jcaddel <jcaddel@kuali.org>
2014-03-12 03:05:16 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35494 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/CreateBuildSlaveAMI.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/CreateBuildSlaveAMI.java
@@ -94,7 +94,7 @@ public class CreateBuildSlaveAMI {
 		// The amount of time to wait before timing out on: instance creation, snapshot creation, ami creation
 		int timeoutMillis = getMillisAsInt(System.getProperty("slave.timeout", "30m"));
 		// The amount of time to sleep after creating a brand new instance (gives DNS a few seconds to figure itself out)
-		String sleep = System.getProperty("slave.sleep", "15s");
+		String sleep = System.getProperty("slave.sleep", "1s");
 
 		EC2Service service = getEC2Service();
 		// Instance instance = getNewSlaveInstance(service, ami, type, rootVolume, timeoutMillis);
