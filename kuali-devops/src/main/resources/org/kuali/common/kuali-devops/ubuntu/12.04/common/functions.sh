fa46920b9bd6367494c1b3120cf649c8fd04e26f
jcaddel <jcaddel@kuali.org>
2014-03-13 19:47:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35852 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/functions.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/functions.sh
@@ -36,10 +36,10 @@ function check_exists {
   if [ ! -f $FILENAME   ]; then echo "file [$FILENAME] does not exist"; exit 1; fi
 }
 
-function decrypt_password {
+function decrypt_file {
+  check_not_blank GPG_PASSPHRASE $GPG_PASSPHRASE
   GPG_INPUT_FILE=$1
   GPG_OUTPUT_FILE=$2
-  GPG_PASSPHRASE=$3
   check_exists $GPG_INPUT_FILE
   execute_quietly "gpg --batch --yes --passphrase $GPG_PASSPHRASE --decrypt --output $GPG_OUTPUT_FILE $GPG_INPUT_FILE"
 }
