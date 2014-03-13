cdfae7ad0da149598240dcb12be8f1c9415baa93
jcaddel <jcaddel@kuali.org>
2014-03-13 13:31:39 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35691 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/functions.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/functions.sh
@@ -43,6 +43,7 @@ function decrypt_password {
   check_exists $1
   GPG_INPUT_FILE=$1
   GPG_OUTPUT_FILE=$(basename $1 .gpg)
+  echo "decrypt   -> $GPG_OUTPUT_FILE"
   execute_quietly "gpg --batch --yes --passphrase $GPG_PASSPHRASE --decrypt --output $GPG_OUTPUT_FILE $GPG_INPUT_FILE"
   PASSWORD=$(<$GPG_OUTPUT_FILE)
   rm $GPG_OUTPUT_FILE
