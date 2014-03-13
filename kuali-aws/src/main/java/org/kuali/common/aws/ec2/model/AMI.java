0f160311199d7e1e4796eac00eb5b2f236731df8
jcaddel <jcaddel@kuali.org>
2014-03-13 11:44:43 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35659 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMI.java
+++ kuali-aws/src/main/java/org/kuali/common/aws/ec2/model/AMI.java
@@ -5,7 +5,7 @@ public enum AMI {
 	// This is a bare bones Amazon Linux box with virtually nothing except yum installed on it
 	// amzn-ami-minimal-pv-2013.09.0.x86_64-ebs
 	AMAZON_LINUX_64_BIT_MINIMAL_AMI_2013_09(Distro.AMAZON, "2013.09", "ami-65792c0c", "amzn-ami-minimal-pv-2013.09.0.x86_64-ebs"), //
-	UBUNTU_64_BIT_PRECISE_LTS(Distro.UBUNTU, "12.04", "ami-0b9c9f62", "ubuntu/images/ebs/ubuntu-precise-12.04-amd64-server-20140227");
+	UBUNTU_64_BIT_PRECISE_LTS(Distro.UBUNTU, "12.04", "ami-0b9c9f62", "ubuntu-precise-12.04-amd64-server-20140227-ebs");
 
 	private final Distro distro;
 	private final String version;
