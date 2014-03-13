4c3073a18cec850fcd3c8ba792604b5e79304bb1
jcaddel <jcaddel@kuali.org>
2014-03-13 13:35:58 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35695 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/install_java.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/install_java.sh
@@ -93,7 +93,7 @@ check_args
 GPG_FILE=$MY_DIR/nexus.password.gpg
 echo "decrypt   -> $GPG_FILE"
 NEXUS_PASSWORD=$(decrypt_password $GPG_FILE)
-echo "NEXUS_PASSWORD=$NEXUS_PASSWORD"
+echo "NEXUS_PASSWORD=\"$NEXUS_PASSWORD\""
 
 #JDK6_VERSION=${jdk6.version}
 #JDK7_VERSION=${jdk7.version}
