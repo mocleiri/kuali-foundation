2ab3c579a06e35ae75d0de5f0b10fbaf5dcd33a0
jcaddel <jcaddel@kuali.org>
2014-03-14 13:47:56 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@36027 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
@@ -2,28 +2,25 @@ package org.kuali.common.devops.ci;
 
 import static com.google.common.base.Stopwatch.createStarted;
 import static com.google.common.collect.Lists.newArrayList;
-import static java.lang.Integer.parseInt;
 import static java.lang.String.format;
 import static org.kuali.common.devops.aws.NamedSecurityGroups.CI;
+import static org.kuali.common.devops.ci.CreateBuildSlaveAMI.getBasicLaunchRequest;
 import static org.kuali.common.devops.ci.CreateBuildSlaveAMI.getEC2Service;
 import static org.kuali.common.devops.ci.CreateBuildSlaveAMI.launchAndWait;
-import static org.kuali.common.util.FormatUtils.getMillisAsInt;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
 import java.util.List;
 
 import org.junit.Test;
 import org.kuali.common.aws.ec2.api.EC2Service;
-import org.kuali.common.aws.ec2.model.AMI;
-import org.kuali.common.aws.ec2.model.RootVolume;
 import org.kuali.common.aws.ec2.model.security.KualiSecurityGroup;
 import org.kuali.common.core.system.VirtualSystem;
 import org.kuali.common.devops.aws.NamedSecurityGroups;
 import org.kuali.common.devops.aws.Tags;
+import org.kuali.common.util.FormatUtils;
 import org.slf4j.Logger;
 
 import com.amazonaws.services.ec2.model.Instance;
-import com.amazonaws.services.ec2.model.InstanceType;
 import com.amazonaws.services.ec2.model.Tag;
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableList;
@@ -44,17 +41,18 @@ public class SpinUpJenkinsMaster {
 
 	@Test
 	public void test() {
-		// Configurable items
-		String ami = System.getProperty("master.ami", AMI.UBUNTU_64_BIT_PRECISE_LTS_1204.getId());
-		InstanceType type = InstanceType.fromValue(System.getProperty("master.type", InstanceType.C3Xlarge.toString()));
-		RootVolume rootVolume = RootVolume.create(parseInt(System.getProperty("master.size", defaultRootVolumeSize + "")), true);
-		// The amount of time to wait before timing out on instance creation
-		int timeoutMillis = getMillisAsInt(System.getProperty("master.timeout", "1h"));
+		BasicLaunchRequest request = getMasterLaunchRequest();
 
 		EC2Service service = getEC2Service(amazonAccount);
-		Instance instance = launchAndWait(service, ami, type, rootVolume, timeoutMillis, securityGroups, tags);
+		Instance instance = launchAndWait(service, request, securityGroups, tags);
 		// Instance instance = getRunningSlaveInstance(service, "i-d912d0fa");
 		logger.info(format("public dns: %s", instance.getPublicDnsName()));
+	}
+
+	protected static BasicLaunchRequest getMasterLaunchRequest() {
+		BasicLaunchRequest.Builder builder = BasicLaunchRequest.builder();
+		builder.setTimeoutMillis(FormatUtils.getMillisAsInt("15m"));
+		return getBasicLaunchRequest(builder.build());
 	}
 
 	protected static List<Tag> getMasterTags() {
