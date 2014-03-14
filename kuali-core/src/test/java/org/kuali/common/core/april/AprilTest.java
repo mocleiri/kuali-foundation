c9387964955e50ed469d8d7177148b684fc95b1e
jcaddel <jcaddel@kuali.org>
2014-03-13 22:43:16 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@35938 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/test/java/org/kuali/common/core/april/AprilTest.java
+++ kuali-core/src/test/java/org/kuali/common/core/april/AprilTest.java
@@ -141,8 +141,9 @@ public class AprilTest {
 		char separator = ' ';
 		Splitter splitter = Splitter.on(separator).trimResults().omitEmptyStrings();
 
-		String line1 = saleLines.getLine1().replace("Ring Of Honor", "RingOfHonor Level");
+		String line1 = saleLines.getLine1().replace("Ring Of Honor", "RingOfHonor Level").replace("- No Alcohol", "");
 		List<String> tokens1 = splitter.splitToList(line1);
+		System.out.println(saleLines.getLine1());
 		checkState(tokens1.size() == 4, "expected 4 tokens");
 
 		List<String> tokens3 = splitter.splitToList(saleLines.getLine3().replace('\t', separator));
