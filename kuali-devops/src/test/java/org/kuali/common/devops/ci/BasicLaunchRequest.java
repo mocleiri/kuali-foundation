2342c0242fe445d3a08528da470589826c245fa2
jcaddel <jcaddel@kuali.org>
2014-03-14 13:26:42 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@36021 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/BasicLaunchRequest.java
@@ -0,0 +1,79 @@
+package org.kuali.common.devops.ci;
+
+import static org.kuali.common.aws.ec2.model.AMI.UBUNTU_64_BIT_PRECISE_LTS;
+import static org.kuali.common.util.FormatUtils.getMillisAsInt;
+
+import org.kuali.common.core.build.ValidatingBuilder;
+import org.kuali.common.core.validate.annotation.IdiotProofImmutable;
+
+import com.amazonaws.services.ec2.model.InstanceType;
+
+@IdiotProofImmutable
+public final class BasicLaunchRequest {
+
+	private final String ami;
+	private final InstanceType type;
+	private final int sizeInGigabytes;
+	private final int timeoutMillis;
+
+	private BasicLaunchRequest(Builder builder) {
+		this.ami = builder.ami;
+		this.type = builder.type;
+		this.sizeInGigabytes = builder.sizeInGigabytes;
+		this.timeoutMillis = builder.timeoutMillis;
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static class Builder extends ValidatingBuilder<BasicLaunchRequest> {
+
+		private String ami = UBUNTU_64_BIT_PRECISE_LTS.getId();
+		private InstanceType type = InstanceType.C3Xlarge;
+		private int sizeInGigabytes = 32;
+		private int timeoutMillis = getMillisAsInt("1h");
+
+		public Builder withAmi(String ami) {
+			this.ami = ami;
+			return this;
+		}
+
+		public Builder withType(InstanceType type) {
+			this.type = type;
+			return this;
+		}
+
+		public Builder withSizeInGigabytes(int sizeInGigabytes) {
+			this.sizeInGigabytes = sizeInGigabytes;
+			return this;
+		}
+
+		public Builder withTimeoutMillis(int timeoutMillis) {
+			this.timeoutMillis = timeoutMillis;
+			return this;
+		}
+
+		@Override
+		public BasicLaunchRequest build() {
+			return validate(new BasicLaunchRequest(this));
+		}
+	}
+
+	public String getAmi() {
+		return ami;
+	}
+
+	public InstanceType getType() {
+		return type;
+	}
+
+	public int getSizeInGigabytes() {
+		return sizeInGigabytes;
+	}
+
+	public int getTimeoutMillis() {
+		return timeoutMillis;
+	}
+
+}
