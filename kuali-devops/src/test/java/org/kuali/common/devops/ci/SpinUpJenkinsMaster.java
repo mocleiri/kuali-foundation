a6bb812c77e64929c0e5913ea97dbf0f2d56512c
jcaddel <jcaddel@kuali.org>
2014-03-15 21:30:30 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@36333 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
@@ -87,9 +87,9 @@ public class SpinUpJenkinsMaster {
 	private final String distroVersion = "12.04";
 	private static final String ROOT = "root";
 	private static final String UBUNTU = "ubuntu";
-
-	// TODO Change these when ready
-	private static final int DEFAULT_ROOT_VOLUME_SIZE = 32;
+	private static final int DEFAULT_ROOT_VOLUME_SIZE = 256;
+	
+	// TODO east or west?
 	private static final AMI DEFAULT_AMI = AMI.UBUNTU_64_BIT_PRECISE_LTS_1204_US_WEST;
 
 	private Map<String, JenkinsContext> contexts = getJenkinsContexts();
