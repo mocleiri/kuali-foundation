59a11a6d760ed5a16f7dffd790a5870219f12be8
jcaddel <jcaddel@kuali.org>
2014-03-16 20:37:45 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@36545 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/NamedSecurityGroups.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/NamedSecurityGroups.java
@@ -12,10 +12,9 @@ import com.google.common.collect.ImmutableList;
 
 public enum NamedSecurityGroups {
 
-	// TODO Remove APPLICATION_SERVER_PERMISSIONS from the CI security group here
 	// That is just a group with no perm's so that the slaves and master belong to the same group and thus
 	// have unrestricted network access to each other from Amazon's perspective
-	CI("ci", "Continuous Integration", APPLICATION_SERVER_PERMISSIONS), //
+	CI("ci", "Continuous Integration", ImmutableList.<Permission> of()), //
 	CI_MASTER("ci.master", "Jenkins CI Server - Master", APPLICATION_SERVER_PERMISSIONS), //
 	CI_BUILD_SLAVE("ci.slave", "Jenkins CI Server - Slave", ALLOW_SSH_FROM_ANYWHERE.getPermission());
 
