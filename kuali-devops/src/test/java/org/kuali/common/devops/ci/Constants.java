e93fa22bf5e253d900adb74e8365d0b785c3b147
jcaddel <jcaddel@kuali.org>
2014-03-17 22:17:23 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@36639 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/Constants.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/Constants.java
@@ -1,7 +1,13 @@
 package org.kuali.common.devops.ci;
 
+import static com.google.common.base.Optional.absent;
+
 import org.kuali.common.aws.ec2.model.AMI;
 import org.kuali.common.aws.ec2.model.Distro;
+import org.kuali.common.aws.ec2.model.ImmutableBlockDeviceMapping;
+import org.kuali.common.aws.ec2.model.ImmutableEbsBlockDevice;
+
+import com.google.common.base.Optional;
 
 public final class Constants {
 
@@ -16,5 +22,12 @@ public final class Constants {
 	public static final String ROOT = "root";
 	public static final String UBUNTU = "ubuntu";
 	public static final AMI DEFAULT_AMI = AMI.UBUNTU_64_BIT_PRECISE_LTS_1204_US_WEST;
+
+	private static final String SSD_DEVICE_NAME = "/dev/sdb";
+	private static final String SSD_VIRTUAL_NAME = "ephemeral0";
+	private static final Optional<ImmutableEbsBlockDevice> NO_EBS = absent();
+	private static final Optional<String> ABSENT = absent();
+
+	public static final ImmutableBlockDeviceMapping SSD = new ImmutableBlockDeviceMapping(SSD_DEVICE_NAME, NO_EBS, Optional.of(SSD_VIRTUAL_NAME), ABSENT);
 
 }
