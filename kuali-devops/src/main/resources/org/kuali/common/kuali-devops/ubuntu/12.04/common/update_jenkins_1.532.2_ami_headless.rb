ffdc758846ae3fdac5023dbc7413c54b1d2ed39c
jcaddel <jcaddel@kuali.org>
2014-03-20 02:29:22 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@36891 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/update_jenkins_1.532.2_ami_headless.rb
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/update_jenkins_1.532.2_ami_headless.rb
@@ -28,35 +28,39 @@ puts "3"
 puts "4"
 
 #@browser.goto("http://beta.ci.kuali.org")
-puts "Attempting to go to #{ci_url}"
+puts "going to #{ci_url}"
 @browser.goto(ci_url)
 
 @browser.link(:text => "log in").wait_until_present
+puts "log in link detected"
 @browser.link(:text => "log in").click
  
+
 @browser.text_field(:id => "username").wait_until_present
+puts "logging in"
 @browser.text_field(:id => "username").set username
-
 @browser.text_field(:id => "password").set password
-
 @browser.button(:name => "submit").click
 
 @browser.link(:text => "Manage Jenkins").wait_until_present
+puts "manage jenkins"
 @browser.link(:text => "Manage Jenkins").click
-
 @browser.td(:id => "main-panel").h1(:text => "Manage Jenkins").wait_until_present
 
+puts "configure system"
 @browser.table(:id => "management-links").link(:text => "Configure System").click
-
 @browser.text_field(:name => "_.ami").wait_until_present
 
 puts @browser.text_field(:name => "_.ami").value
-puts "updating ami value to #{ami_value}"
+puts "update ami -> #{ami_value}"
 
 @browser.text_field(:name => "_.ami").set ami_value
 
 # This is the id for the "Save" button
 #@browser.button(:id => "yui-gen76-button").click
+
+puts "save configuration"
 @browser.button(:value => "Save").click
 
+puts "log out"
 @browser.link(:text => "log out").click
