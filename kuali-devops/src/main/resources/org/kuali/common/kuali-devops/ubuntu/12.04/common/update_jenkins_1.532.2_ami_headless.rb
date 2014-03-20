d803fb1303f66062b6fa735c975d8b5a924b6aa2
jcaddel <jcaddel@kuali.org>
2014-03-20 02:21:58 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@36888 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/update_jenkins_1.532.2_ami_headless.rb
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/update_jenkins_1.532.2_ami_headless.rb
@@ -14,8 +14,8 @@ ami_value = ARGV[2]
 ci_url = ARGV[3]
 
 puts "username: #{username}"
-puts "ami: #{ami_value}"
-puts "url: #{ci_url}"
+puts "     ami: #{ami_value}"
+puts "     url: #{ci_url}"
 
 headless = Headless.new #:destroy_at_exit => false
 headless.start
