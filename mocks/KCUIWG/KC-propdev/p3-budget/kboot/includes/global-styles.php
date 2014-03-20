21e5c12c47aed974d0042cd1c8432ca364aede1d
clrux <clrux@kuali.org>
2014-03-20 16:30:13 -0400

    Starting PHP and includes for budget work
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@36991 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/KC-propdev/p3-budget/kboot/includes/global-styles.php
@@ -0,0 +1,2 @@
+<link href="/bootstrap/stylesheets/bootstrap.css" rel="stylesheet" type="text/css"/>
+<link href="/kboot/stylesheets/kboot.css" rel="stylesheet" type="text/css"/>
---
+++ mocks/KCUIWG/KC-propdev/p3-budget/kboot/includes/navigation.php
@@ -0,0 +1,31 @@
+<nav id="Uif-Navigation">
+    <div id="LabsProposal-Menu" class="uif-menuNavigationGroup" >
+        <div class="sidebar-collapse">
+            <span class="icon-angle-left"></span>
+        </div>
+
+        <!-- NAVIGATION -->
+        <ul class="nav nav-list">
+            <li><a class="uif-navigationActionLink" tabindex="0" href="versions.php"><span class="icon-file-alt"></span><span class="uif-innerText">Budget Versions</span></a></li>
+            <li><a class="uif-navigationActionLink" tabindex="0" href="periods-and-totals.php"><span class="icon-sort-by-attributes-alt"></span><span class="uif-innerText">Budget Periods &amp; Totals</span></a></li>
+            <li><a class="uif-navigationActionLink" tabindex="0" href="rates.php"><span class="icon-tasks"></span><span class="uif-innerText">Rates</span></a></li>
+            <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <span class="icon-user3"></span>
+                    <span class="uif-innerText" >Personnel Costs</span>
+                    <span class="arrow icon-angle-right"></span>
+                </a>
+                <ul class="submenu uif-listLayout">
+                    <li><a class="uif-actionLink" tabindex="0" href="personnel-roster.php">Project Personnel</a></li>
+                    <li><a class="uif-actionLink" tabindex="0" href="personnel-assign.php">Assign Personnel to Periods</a></li>
+                </ul>
+            </li>
+            <li><a class="uif-navigationActionLink" tabindex="0" href="non-personnel.php"><span class="icon-desktop"></span><span class="uif-innerText">Non-Personnel Costs</span></a></li>
+            <li><a class="uif-navigationActionLink" tabindex="0" href="subawards.php"><span class="icon-file"></span><span class="uif-innerText">Subawards</span></a></li>
+            <li><a class="uif-navigationActionLink" tabindex="0" href="cost-sharing.php"><span class="icon-random"></span><span class="uif-innerText">Cost Sharing &amp; Unrecovered F&amp;A</span></a></li>
+            <li><a class="uif-navigationActionLink" tabindex="0" href="project-income.php"><span class="icon-money"></span><span class="uif-innerText">Project Income</span></a></li>
+            <li><a class="uif-navigationActionLink" tabindex="0" href="modular.php"><span class="icon-ok"></span><span class="uif-innerText">Modular</span></a></li>
+            <li><a class="uif-navigationActionLink" tabindex="0" href="notes.php"><span class="icon-th-list"></span><span class="uif-note">Budget Notes</span></a></li>
+        </ul>
+    </div>
+</nav>
