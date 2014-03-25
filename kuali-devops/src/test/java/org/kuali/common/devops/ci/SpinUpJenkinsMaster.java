c678d3ecca604dca117294eb06c6a7737b1418c6
jcaddel <jcaddel@kuali.org>
2014-03-24 20:24:01 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@37071 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
@@ -249,7 +249,7 @@ public class SpinUpJenkinsMaster {
 
 	protected static SecureChannel openSecureChannel(String username, String hostname, String privateKey, boolean quiet) throws IOException {
 		ChannelContext context = getSilentContextBuilder(hostname).echo(!quiet).requestPseudoTerminal(true).debug(!quiet).username(username).privateKey(privateKey)
-				.connectTimeout(getMillisAsInt("5s")).build();
+				.connectTimeout(getMillisAsInt("15s")).build();
 		ChannelService service = new DefaultChannelService();
 		return service.openChannel(context);
 	}
