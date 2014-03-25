688c16a6212e2bad1dd03e44f9e94a60553b78b4
ag725 <ag725@kuali.org>
2014-03-25 10:54:22 -0400

    Add guided mode start screen
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@37120 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/KC-propdev/p3-budget/guided-start.php
@@ -0,0 +1,116 @@
+<?php
+$page = 'periods-and-totals';
+$section = '';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kuali Coeus Budget POC</title>
+
+    <!-- GLOBAL STYLES -->
+    <?php include ('includes/global-styles.php') ?>
+</head>
+<body id="Uif-Application">
+
+    <!-- STICKY HEADER AND NAV -->
+    <?php include ('includes/header-sticky.php') ?>
+
+    <form id="kualiForm" action="#" method="post" accept-charset="UTF-8">
+        <div id="LabsProposal" class="clearfix uif-formView">
+            <?php include ('includes/header-docinfo.php') ?>
+    
+            <!-- VIEW CONTENT -->
+            <div id="Uif-ViewContentWrapper" class="uif-viewContentWrapper container">
+    
+                <!-- VIEW NAVIGATION -->
+                <div class="col-md-3">
+                    <?php include ('includes/navigation.php') ?>
+                </div>
+
+                <div class="col-md-9">
+                    <main id="LabsProposal-Page" class="uif-page">
+                        <header class="clearfix uif-header-contentWrapper">
+                            <div class="uif-pageHeader clearfix">
+                                <h2 class="uif-headerText">
+                                    <span class="uif-headerText-span">Guided Mode</span>
+                                </h2>
+                            </div>
+                            <div class="uif-verticalBoxGroup uif-header-lowerGroup">
+                                <div class="uif-boxLayoutVerticalItem clearfix">
+                                    <p>Would you like to create a detailed budget or enter a summary only?</p>
+                                </div>
+                            </div>
+                            <div class="uif-cssGridSection uif-boxLayoutVerticalItem clearfix">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <a class="btn btn-default btn-large" href="#">Start a detailed budget</a>
+                                    <a class="btn btn-default btn-large" href="#">Start a summary-only budget</a>
+                                </div>
+                            </div>
+                        </div>
+                        </header>
+                    </main>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <!-- MODALS -->
+    <!-- <div id="modal-create-version" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true" aria-labbeledby="create-new-title">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title" id="create-new-label">Create a budget version</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="uif-cssGridSection uif-boxLayoutVerticalItem clearfix">
+                        <div class="row">
+                            <div class="col-md-3 uif-cssGridLabelCol">
+                                <label for="new-version-name" class="uif-label displayWith-uk9itqu uif-labelBlock uif-required">Name: *</label>
+                            </div>
+                            <div class="col-md-9">
+                                <div class="uif-inputField" data-role="InputField" data-label="Name">
+                                    <input id="new-version-name" type="text" name="new-version-name" value="" size="30" class="form-control input-sm uif-textControl required" data-role="Control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-9">
+                                <div class="well">
+                                    <fieldset>
+                                        <legend>Does this NIH proposal allow modular budgets?</legend>
+                                        <label for="create-with-modular-true" class="clearfix"><input type="radio" name="create-with-modular" id="create-with-modular-true"> Yes, allow for modular budgets</label>
+                                        <label for="create-with-modular-false" class="clearfix"><input type="radio" name="create-with-modular" id="create-with-modular-false"> No, there will be no modular budget</label>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-9">
+                                <div class="well">
+                                    <fieldset>
+                                        <legend>Would you like guided assistance to complete this budget?</legend>
+                                        <label for="create-with-guided-asst-true" class="clearfix"><input type="radio" name="create-with-guided-asst" id="create-with-guided-asst-true"> No, show me everything</label>
+                                        <label for="create-with-guided-asst-false" class="clearfix"><input type="radio" name="create-with-guided-asst" id="create-with-guided-asst-false"> Yes, please use guided assistance</label>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a role="button" class="btn btn-link" data-dismiss="modal" href="#">Cancel</a>
+                    <a role="button" class="btn btn-primary" href="periods-and-totals.php">Create and open</a>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+    <?php include ('includes/footer-scripts.php') ?>
+</body>
+</html>
