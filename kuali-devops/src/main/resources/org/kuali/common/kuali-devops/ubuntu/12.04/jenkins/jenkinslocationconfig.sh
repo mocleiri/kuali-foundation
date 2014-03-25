aa3d9fff9c722e31461d3b3bb7d5bd6e4683f73c
jcaddel <jcaddel@kuali.org>
2014-03-25 12:08:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@37145 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/jenkinslocationconfig.sh
+++
@@ -1,9 +0,0 @@
-#
-# Used to override the DNS name bundled into the master.zip.gpg file
-#
-
-JENKINS_LOCATION_XML_LINE1="<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-JENKINS_LOCATION_XML_LINE2="<jenkins.model.JenkinsLocationConfiguration>"
-JENKINS_LOCATION_XML_LINE3="<adminAddress>Kuali CI &lt;$JENKINS_ADMIN_EMAIL_ADDRESS&gt;</adminAddress>"
-JENKINS_LOCATION_XML_LINE4="<jenkinsUrl>https://$JENKINS_MASTER/</jenkinsUrl>"
-JENKINS_LOCATION_XML_LINE5="</jenkins.model.JenkinsLocationConfiguration>"
