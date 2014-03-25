358d94bf224af5596059b179114db41fae51c00c
jcaddel <jcaddel@kuali.org>
2014-03-25 12:12:32 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@37148 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
@@ -93,6 +93,7 @@ public class SpinUpJenkinsMaster {
 	private final String distroVersion = Constants.DISTRO_VERSION;
 	private static final String ROOT = Constants.ROOT;
 	private static final String UBUNTU = Constants.UBUNTU;
+	private static final String JENKINS_VERSION = "1.532.2";
 
 	// What should we go with for default root volume size, 256?)
 	private static final int DEFAULT_ROOT_VOLUME_SIZE = 256;
@@ -155,7 +156,7 @@ public class SpinUpJenkinsMaster {
 			String common = getResource(basedir, pid, distro, distroVersion, "jenkins/configurecommon");
 			String jenkins = getResource(basedir, pid, distro, distroVersion, "jenkins/installjenkins");
 			exec(channel, common, quietFlag, jenkinsMaster, gpgPassphrase);
-			exec(channel, jenkins, quietFlag, jenkinsMaster, "1.532.2", gpgPassphrase);
+			exec(channel, jenkins, quietFlag, jenkinsMaster, jenkinsContext.getRegion().getName(), JENKINS_VERSION, gpgPassphrase);
 
 			// The spin up process should have given DNS enough time to settle down
 			info("Verifying SSH to -> [%s]", jenkinsMaster);
