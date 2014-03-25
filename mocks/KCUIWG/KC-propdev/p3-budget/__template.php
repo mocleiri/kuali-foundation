f34992e86c42e0ab76fb185a8727037630a7a5b1
clrux <clrux@kuali.org>
2014-03-25 10:33:56 -0400

    Cleanup __template.php
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@37118 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KC-propdev/p3-budget/__template.php
+++ mocks/KCUIWG/KC-propdev/p3-budget/__template.php
@@ -12,7 +12,7 @@
     <!-- STICKY HEADER AND NAV -->
     <?php include ('includes/header-sticky.php') ?>
 
-    <form id="kualiForm" action="#" method="post" accept-charset="UTF-8">
+    <form action="#" method="post" accept-charset="UTF-8">
         <div id="LabsProposal" class="clearfix uif-formView">
             <?php include ('includes/header-docinfo.php') ?>
     
@@ -25,7 +25,7 @@
                 </div>
 
                 <div class="col-md-9">
-                    <main id="LabsProposal-Page" class="uif-page">
+                    <main class="uif-page">
                         <header class="clearfix uif-header-contentWrapper">
                             <div class="uif-pageHeader clearfix">
                                 <h2 class="uif-headerText">
@@ -42,7 +42,7 @@
                             <div class="uif-cssGridSection uif-boxLayoutVerticalItem clearfix">
                                 <div class="row">
                                     <div class="col-md-3 uif-cssGridLabelCol">
-                                        <label for="new-version-name" class="uif-label displayWith-uk9itqu uif-labelBlock uif-required">Name: *</label>
+                                        <label for="new-version-name" class="uif-label uif-labelBlock uif-required">Name: *</label>
                                     </div>
                                     <div class="col-md-9">
                                         <div class="uif-inputField" data-role="InputField" data-label="Name">
@@ -81,7 +81,7 @@
             </div>
             
             <!-- VIEW FOOTER -->
-            <div class="uif-footer clearfix" data-sticky_footer="true" data-parent="LabsProposal">
+            <div class="uif-footer clearfix">
                 <button class="btn btn-default btn btn-default uif-boxLayoutHorizontalItem">Save</button>
                 <button class="btn btn-primary btn btn-primary uif-boxLayoutHorizontalItem">Save and Continue</button>
             </div>
