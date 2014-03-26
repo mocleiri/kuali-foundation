d74dc4f2fe34885e29c82e6febdca30d8c5c7c61
jcaddel <jcaddel@kuali.org>
2014-03-25 20:58:50 -0400

    [maven-release-plugin]  copy for tag kuali-pom-3.4.4
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-pom-3.4.4@37196 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-puppet/src/test/resources/modules/simple/manifests/jdk7.pp
+++
@@ -1,3 +0,0 @@
-package { "jdk":
-  ensure => "1.7.0_11-fcs",
-}
--- kuali-util/src/test/java/org/kuali/common/util/xml/jaxb/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Testing JAXB + Immutability
- */
-@XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
-package org.kuali.common.util.xml.jaxb;
-
-import javax.xml.bind.annotation.XmlAccessorType;
-
