eb73b3ca0e3de30212d05a950c5ef8cce6b75806
jcaddel <jcaddel@kuali.org>
2014-03-26 17:53:18 -0400

    [maven-release-plugin]  copy for tag kuali-aws-1.0.0
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-aws-1.0.0@37254 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/spring/event/ApplicationEventListenerConfig.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.common.util.spring.event;
-
-import org.springframework.context.event.SmartApplicationListener;
-
-public interface ApplicationEventListenerConfig {
-	
-	SmartApplicationListener applicationEventListener();
-
-}
