e912abbd51d839e1747ad3455d8f76b396a15f46
thrclark <thrclark@kuali.org>
2014-03-28 10:15:03 -0400

    Navigation edits; restoring budget summary screen to KRAD based prototype
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@37418 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/uif-navigation-budget.php
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/uif-navigation-budget.php
@@ -5,9 +5,9 @@
         <!-- NAVIGATION -->
         <ul class="nav nav-list">
          
-            <li <?php if ($page == 'summary') { echo 'class="active"'; } ?>><a class="uif-navigationActionLink" tabindex="0" href="budget-summary.php"><span class="icon-file"></span><span class="uif-innerText">Budget Summary</span></a></li>
-            <li <?php if ($page == 'periods-and-totals') { echo 'class="active"'; } ?>><a class="uif-navigationActionLink" tabindex="0" href="periods-and-totals.php"><span class="icon-sort-by-attributes-alt"></span><span class="uif-innerText">Periods &amp; Totals</span></a></li>
-            <li <?php if ($page == 'rates') { echo 'class="active"'; } ?>><a class="uif-navigationActionLink" tabindex="0" href="rates.php"><span class="icon-tasks"></span><span class="uif-innerText">Rates</span></a></li>
+            <li <?php if ($page == 'summary') { echo 'class="active"'; } ?>><a class="uif-navigationActionLink" tabindex="0" href="budget-ng-summary.php"><span class="icon-file"></span><span class="uif-innerText">Budget Summary</span></a></li>
+            <li <?php if ($page == 'periods-and-totals') { echo 'class="active"'; } ?>><a class="uif-navigationActionLink" tabindex="0" href="budget-ng-periods.php"><span class="icon-sort-by-attributes-alt"></span><span class="uif-innerText">Periods &amp; Totals</span></a></li>
+            <li <?php if ($page == 'rates') { echo 'class="active"'; } ?>><a class="uif-navigationActionLink" tabindex="0" href="budget-ng-rates.php"><span class="icon-tasks"></span><span class="uif-innerText">Rates</span></a></li>
             <li <?php if ($section == 'personnel') { echo 'class="active in"'; } ?>> <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <span class="icon-user3"></span> <span class="uif-innerText" >Personnel Costs</span> <span class="arrow icon-angle-right"></span> </a>
                 <ul class="submenu uif-listLayout">
                     <li <?php if ($page == 'personnel-costs') { echo 'class="active"'; } ?>><a class="uif-actionLink" tabindex="0" href="personnel-roster.php">Project Personnel</a></li>
