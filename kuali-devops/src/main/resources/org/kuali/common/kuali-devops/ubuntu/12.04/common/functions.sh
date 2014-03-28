fd9ec9e08616a1e5d7085f80100b2e8060726bfa
jcaddel <jcaddel@kuali.org>
2014-03-28 13:03:34 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@37439 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/functions.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/functions.sh
@@ -157,10 +157,3 @@ function echo_lines {
     echo "$LINE"
   done < $FILENAME
 }
-
-trim() {
-  local var=$@
-  var="${var#"${var%%[![:space:]]*}"}"   # remove leading whitespace characters
-  var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespace characters
-  echo -n "$var"
-}
