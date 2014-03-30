25026728c123be2108d373f341f09e77a974b049
jcaddel <jcaddel@kuali.org>
2014-03-30 09:20:07 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@37526 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/metadata/function/ProjectConfigUrlFragmentFunction.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/metadata/function/ProjectConfigUrlFragmentFunction.java
@@ -18,10 +18,16 @@ import com.google.common.base.Optional;
 public final class ProjectConfigUrlFragmentFunction implements Function<Project, Optional<String>> {
 
 	public ProjectConfigUrlFragmentFunction(Optional<RemoteEnvironment> remoteEnvironment) {
+		this(remoteEnvironment, Optional.<String> absent());
+	}
+
+	public ProjectConfigUrlFragmentFunction(Optional<RemoteEnvironment> remoteEnvironment, Optional<String> prefix) {
 		this.remoteEnvironment = checkNotNull(remoteEnvironment, "remoteEnvironment");
+		this.prefix = prefix;
 	}
 
 	private final Optional<RemoteEnvironment> remoteEnvironment;
+	private final Optional<String> prefix;
 
 	@Override
 	public Optional<String> apply(Project project) {
@@ -33,28 +39,48 @@ public final class ProjectConfigUrlFragmentFunction implements Function<Project,
 		String groupId = project.getGroupId();
 		if (groupId.equals(STUDENT_GROUP_ID)) {
 			// KS specific logic
-			return Optional.of("/home/kuali/main/dev/" + project.getArtifactId() + "-config.xml");
+			if (prefix.isPresent()) {
+				return Optional.of("/" + prefix.get() + "/home/kuali/main/dev/" + project.getArtifactId() + "-config.xml");
+			} else {
+				return Optional.of("/home/kuali/main/dev/" + project.getArtifactId() + "-config.xml");
+			}
 		} else if (groupId.equals(OLE_GROUP_ID)) {
 			// OLE specific logic
-			return getOleConfigFragment(remoteEnvironment);
+			return getOleConfigFragment(remoteEnvironment, prefix);
 		} else {
 			// Everyone else
-			return Optional.of("/home/kuali/main/dev/common-config.xml");
+			if (prefix.isPresent()) {
+				return Optional.of("/" + prefix.get() + "/home/kuali/main/dev/common-config.xml");
+			} else {
+				return Optional.of("/home/kuali/main/dev/common-config.xml");
+			}
 		}
 	}
 
-	protected Optional<String> getOleConfigFragment(Optional<RemoteEnvironment> remoteEnvironment) {
+	protected Optional<String> getOleConfigFragment(Optional<RemoteEnvironment> remoteEnvironment, Optional<String> prefix) {
 		// OLE specific logic
 		if (!remoteEnvironment.isPresent()) {
 			return absent();
 		}
 		Properties systemProperties = remoteEnvironment.get().getSystem();
-		Optional<String> value = fromNullable(systemProperties.getProperty( "environment"));
+		Optional<String> value = fromNullable(systemProperties.getProperty("environment"));
 		if (!value.isPresent() || isBlank(value.get().trim())) {
 			return absent();
 		} else {
-			return Optional.of("/home/kuali/main/" + value.get() + "/common-config.xml");
+			if (prefix.isPresent()) {
+				return Optional.of("/" + prefix.get() + "/home/kuali/main/" + value.get() + "/common-config.xml");
+			} else {
+				return Optional.of("/home/kuali/main/" + value.get() + "/common-config.xml");
+			}
 		}
+	}
+
+	public Optional<RemoteEnvironment> getRemoteEnvironment() {
+		return remoteEnvironment;
+	}
+
+	public Optional<String> getPrefix() {
+		return prefix;
 	}
 
 }
