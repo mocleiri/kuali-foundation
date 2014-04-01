a54bc119e0a90460de314f68ed702cd45289da81
mfitzwater <mfitzwater@kuali.org>
2014-04-01 11:50:21 -0400

    Added a style block to increase the icon size
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@37735 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/styles.php
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/styles.php
@@ -10,3 +10,11 @@
 <link href="../../plugins/tooltip/jquery.bubblepopup.v2.3.1.css" rel="stylesheet" type="text/css"/>
 <!--<link href="../css/kradSampleApp.css" rel="stylesheet" type="text/css"/>
    <link href="../css/labsProposal.css" rel="stylesheet" type="text/css"/>  -->
+   <!-- icon size position-->
+<style type="text/css">
+.edit-table .icon, .edit-table .icon-edit, .edit-table .icon-save{ font-size:140%;}
+.edit-table .icon-edit:hover, .edit-table .icon-save:hover{text-decoration:none;}
+.edit-table tr:hover{background:#EEF7FB;; color:#000;}
+.edit-table .icon-edit{padding:0; padding-top:5px;vertical-align:bottom; text-decoration:none; }
+.edit-table tr  td:last-of-type{  text-align:center; }
+</style>
