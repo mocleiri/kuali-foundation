14e8f57b659dd05d180527aa19a572bcaac1f596
jcaddel <jcaddel@kuali.org>
2014-04-01 15:45:38 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@37798 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-devops/src/test/java/org/kuali/common/devops/presigned/CreateBuildSlaveAMI.java
@@ -0,0 +1,21 @@
+package org.kuali.common.devops.presigned;
+
+import static org.kuali.common.util.log.Loggers.newLogger;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+
+public class CreateBuildSlaveAMI {
+
+	private static final Logger logger = newLogger();
+
+	@Test
+	public void test() {
+		try {
+			logger.info("hello world");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+}
