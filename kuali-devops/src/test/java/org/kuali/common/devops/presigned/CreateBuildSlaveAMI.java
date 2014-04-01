3a1dad157c030375e0ae2c5064e3330fb66ef2b1
jcaddel <jcaddel@kuali.org>
2014-04-01 15:54:33 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@37800 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/presigned/CreateBuildSlaveAMI.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/presigned/CreateBuildSlaveAMI.java
@@ -2,8 +2,18 @@ package org.kuali.common.devops.presigned;
 
 import static org.kuali.common.util.log.Loggers.newLogger;
 
+import java.net.URL;
+
+import org.jasypt.util.text.TextEncryptor;
 import org.junit.Test;
+import org.kuali.common.devops.logic.Passwords;
+import org.kuali.common.util.enc.EncUtils;
 import org.slf4j.Logger;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 
 public class CreateBuildSlaveAMI {
 
@@ -12,10 +22,24 @@ public class CreateBuildSlaveAMI {
 	@Test
 	public void test() {
 		try {
-			logger.info("hello world");
+			String bucket = "maven.kuali.org";
+			String key = "private/com/oracle/jdk6/1.6.0-u43/jdk6-1.6.0-u43.pom";
+			GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucket, key);
+			AWSCredentials foundation = getFoundationCreds();
+			AmazonS3Client client = new AmazonS3Client(foundation);
+			URL url = client.generatePresignedUrl(request);
+			logger.info(url.toString());
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
+	}
+
+	private AWSCredentials getFoundationCreds() {
+		String password = Passwords.getEncPassword();
+		TextEncryptor enc = EncUtils.getTextEncryptor(password);
+		String accessKey = "AKIAJFD5IM7IPVVUEBNA";
+		String secretKey = enc.decrypt("uXNCzc6efcKz1zvp4t5Fj4wyR9oGw2GZ2VOB3SXZaoXaV1BA1Gao2d2vWXnjqUA1oKzg+0s9NAM=");
+		return new BasicAWSCredentials(accessKey, secretKey);
 	}
 
 }
