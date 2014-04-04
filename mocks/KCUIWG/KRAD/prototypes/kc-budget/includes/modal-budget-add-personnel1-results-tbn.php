2974c79f058969bf646304d8d45884f24e6ee3c1
thrclark <thrclark@kuali.org>
2014-04-04 13:29:17 -0400

    UXI-167 TBN personnel process updated
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@38111 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/modal-budget-add-personnel1-results-tbn.php
@@ -0,0 +1,79 @@
+
+<div id="modal-add-personnel1-results-tbn" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="myModalLabel"> Personnel Search Results</h4>
+            </div>
+            <div class="modal-body uif-cssGridGroup">
+                
+                <h4>Results</h4>
+                
+                
+<table id="u569ish_line0" class="table table-condensed table-bordered table-hover" role="presentation">
+							<tbody>
+								<tr>
+									<th scope="row">&nbsp;</th>
+									<th scope="row">TBN ID</th>
+									<th class="uif-gridLayoutCell">Person Name </th>
+									<th class="uif-gridLayoutCell">Job</th>
+								</tr>
+								<tr class="">
+								    <td><input type="checkbox" name="checkbox" id="checkbox" />
+							        <label for="checkbox"></label></td>
+								    <td   title="DeptAward  Viewer">1</td>
+								    <td  title="TBA Research"> TBA Research
+								        &nbsp; </td>
+									<td>Default</td>
+								</tr>
+								<tr>
+								    <td class=""><input type="checkbox" name="checkbox" id="checkbox" />
+								        <label for="checkbox"></label></td>
+								    <td   title="DOMINIC  NUGENT">2</td>
+								    <td  title="Research Associate"> Research Associate
+								        &nbsp; </td>
+								    <td>Default</td>
+								</tr>
+								<tr>
+								    <td class=""><input type="checkbox" name="checkbox" id="checkbox" />
+								        <label for="checkbox"></label></td>
+								    <td   title="TREVOR  DUGGINS">3</td>
+								    <td  title="Data Entry Assistant"> Data Entry Assistant
+								        &nbsp; </td>
+								    <td>Default</td>
+								</tr>
+								<tr>
+								    <td class=""><input type="checkbox" name="checkbox" id="checkbox" />
+								        <label for="checkbox"></label></td>
+								    <td   title="DARREN  DEVANEY">4</td>
+								    <td  title="Computer Programmer"> Computer Programmer
+								        &nbsp; </td>
+								    <td>Default</td>
+								</tr>
+								<tr>
+								    <td class=""><input type="checkbox" name="checkbox" id="checkbox" />
+								        <label for="checkbox"></label></td>
+								    <td   title="EMORY  EAGLE">5</td>
+								    <td  title="Data Manager"> Data Manager
+								        &nbsp; </td>
+								    <td>Default</td>
+								</tr>
+								<tr>
+								    <td class=""><input type="checkbox" name="checkbox" id="checkbox" />
+								        <label for="checkbox"></label></td>
+								    <td   title="OLIVER  ELGIN">6</td>
+								    <td  title="Project Director"> Project Director
+								        &nbsp; </td>
+								    <td>Default</td>
+								</tr>
+							</tbody>
+						</table>                
+                
+                
+            </div>
+        <div class="modal-footer"><a href="" class="btn btn-default"  data-dismiss="modal" data-toggle="modal" data-target="#modal-add-personnel1"><span aria-hidden="true" class="icon-arrow-left"></span> Refine Search</a> <a href="budget-ng-personnelCosts-projPersonnel2.php" class="btn btn-primary" data-dismiss=""> Return Selected</a></div></div>
+    </div>
+</div>
+
+<!-- //  MODAL ADD PERSONNEL --> 
---
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/modal-budget-add-personnel1.php
@@ -0,0 +1,169 @@
+
+<div id="modal-add-personnel1" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="myModalLabel">Personnel Lookup</h4>
+            </div>
+            <div class="modal-body uif-cssGridGroup">
+                <div class="form-horizontal" role="form"> <br />
+                    <div class="row ">
+                        <div class="col-md-12"> 
+                            
+                            <!--  Employee Search-->
+                            
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-md-3 control-label">Personnel Type</label>
+                                <div class="col-md-6">
+                                    <select name="privileges" id="personType" class="form-control" onclick="craateUserJsObject.ShowPrivileges();">
+                                        <option id="" value="">select</option>
+                                        <option id="some" value="emp">Employee</option>
+                                        <option id="some" value="nonemp">Non-Employee</option>
+                                        <option id="all" value="tbn">To Be Named</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">KcPerson Id</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Last Name</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">First Name</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">User Name</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Email Address</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Office Phone</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Active</label>
+                                <div class="col-md-6">
+                                    <select class="form-control">
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Home Unit</label>
+                                <div class="col-md-6">
+                                    <select class="form-control">
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group employee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Campus Code</label>
+                                <div class="col-md-6">
+                                    <select class="form-control">
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                        <option value="">select</option>
+                                    </select>
+                                </div>
+                            </div>
+                            
+                            <!--  Non Employee Search-->
+                            
+                            <div class="form-group nonemployee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Address Book Id</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group nonemployee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Sponsor</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group nonemployee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Organization</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group nonemployee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Owned By Unit</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group nonemployee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Sponsor Address Flag</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group nonemployee" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Title</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            
+                            <!--  To Be Named-->
+                            
+                            <div class="form-group tbn" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">TBN ID</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group tbn" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Person Name</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group tbn" style="display:none;">
+                                <label for="inputEmail3" class="col-md-3 control-label">Job Code</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" placeholder="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+            <a href="" class="btn btn-default" data-dismiss="modal">Cancel</a> 
+            <a href="" style="display:none;" data-dismiss="modal" class="btn btn-primary employee" data-toggle="modal" data-target="#modal-add-personnel1-results">Search</a>
+            <a href="" style="display:none;" data-dismiss="modal" class="btn btn-primary nonemployee" data-toggle="modal" data-target="#modal-add-personnel1-results">Search</a>
+            <a href="" style="display:none;" data-dismiss="modal" class="btn btn-primary tbn" data-toggle="modal" data-target="#modal-add-personnel1-results-tbn">Search</a> </div>
+        </div>
+    </div>
+</div>
+
+<!-- //  MODAL ADD PERSONNEL --> 
