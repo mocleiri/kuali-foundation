56f75a4850a4f9a4b91b7529db8a1e2dfd58e2ef
mfitzwater <mfitzwater@kuali.org>
2014-04-04 11:50:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@38068 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/modal-budget-versions.php
@@ -0,0 +1,142 @@
+<!-- Modal -->
+<div class="modal fade" id="modal-budget-versions" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Budget Versions</h4>
+            </div>
+            <div class="modal-body">
+              <!--  <p>Here's a summary of your current budget.</p>-->
+                   <!-- BUDGET VERSIONS -->
+                    <table id="u569ish_line0" class="table table-condensed table-bordered" role="presentation">
+                        <tbody>
+                            <tr>
+                                <th scope="row" style="width:20px">&nbsp;</th>
+                                <th scope="row">Name</th>
+                                <th class="uif-gridLayoutCell">Direct Cost</th>
+                                <th class="uif-gridLayoutCell">F&amp;A</th>
+                                <th class="uif-gridLayoutCell">Total</th>
+                                <th class="uif-gridLayoutCell">Status</th>
+                                <th class="uif-gridLayoutCell">Actions</th>
+                            </tr>
+                            <tr>
+                                <td scope="row"><input type="radio" value="radio" name="version-list" id="radio-version-1">
+                                    <label for="radio"></label></td>
+                                <td scope="row"><a href="" data-toggle="modal" data-target="#switchdoc"  class="versions">Version 1</a> <small>(for submission)</small></td>
+                                <td class="">  22,835.00</td>
+                                <td class="">  6,170.00</td>
+                                <td class="">29,005.00</td>
+                                <td class="">Complete</td>
+                                <td class=""><!-- Single button -->
+                                    
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"> Action <span class="caret"></span> </button>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a id="uotglr8" class="uif-actionLink" data-toggle="modal" data-target="#summary"><span aria-hidden="true" class="icon-eye-open"></span> View Summary</a> </li>
+                                            <li><a href=""  data-toggle="modal" data-target="#copyNew" class=""><span aria-hidden="true" class="icon-copy"></span> Copy</a></li>
+                                            <li><a href="#"><span aria-hidden="true" class="icon-print"></span> Print</a></li>
+                                            <li class="divider"></li>
+                                            <li><a href="#"><span aria-hidden="true" class="icon-ok"></span> Submit with Proposal</a></li>
+                                        </ul>
+                                    </div></td>
+                            </tr>
+                            <tr>
+                                <td scope="row"  class="success"><input type="radio"  value="radio" name="version-list" id="radio-version-2" checked>
+                                    <label for="radio"></label></td>
+                                <td class="showMe" scope="row"><a href="" data-toggle="modal" data-target="#switchdoc"   class="versions">Version 2</a> <small>(for submission)</small></td>
+                                <td class="">  22,835.00</td>
+                                <td class="">  6,170.00</td>
+                                <td class="">29,005.00</td>
+                                <td class="">Complete</td>
+                                <td class=""><!-- Single button -->
+                                    
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"> Action <span class="caret"></span></button>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a id="uotglr8" class="uif-actionLink" data-toggle="modal" data-target="#summary"><span aria-hidden="true" class="icon-eye-open"></span> View Summary</a></li>
+                                            <li><a href=""  data-toggle="modal" data-target="#copyNew" class=""><span aria-hidden="true" class="icon-copy"></span> Copy</a></li>
+                                            <li><a href="#"><span aria-hidden="true" class="icon-print"></span> Print</a></li>
+                                            <li class="divider"></li>
+                                            <li><a href="#"><span aria-hidden="true" class="icon-ok"></span> Submit with Proposal</a></li>
+                                        </ul>
+                                    </div></td>
+                            </tr>
+                            <tr>
+                                <td scope="row"><input type="radio" value="radio" name="version-list" id="radio-version-3">
+                                    <label for="radio"></label></td>
+                                <td scope="row"><a href="" data-toggle="modal" data-target="#switchdoc"   class="versions">Version 3</a> <small>(for submission)</small></td>
+                                <td class="">  22,835.00</td>
+                                <td class="">  6,170.00</td>
+                                <td class="">29,005.00</td>
+                                <td class="">Incomplete</td>
+                                <td class=""><!-- Single button -->
+                                    
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"> Action <span class="caret"></span></button>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a id="uotglr8" class="uif-actionLink" data-toggle="modal" data-target="#summary"><span aria-hidden="true" class="icon-eye-open"></span> View Summary</a></li>
+                                            <li><a href=""  data-toggle="modal" data-target="#copyNew" class=""><span aria-hidden="true" class="icon-copy"></span> Copy</a></li>
+                                            <li><a href="#"><span aria-hidden="true" class="icon-print"></span> Print</a></li>
+                                            <li class="divider"></li>
+                                            <li><a href="#"><span aria-hidden="true" class="icon-ok"></span> Submit with Proposal</a></li>
+                                        </ul>
+                                    </div></td>
+                            </tr>
+                            <tr>
+                                <td scope="row"><input type="radio" value="radio" name="version-list" id="radio-version-4">
+                                    <label for="radio"></label></td>
+                                <td scope="row"><a href="" data-toggle="modal" data-target="#switchdoc"  class="versions">Version 4</a> <small>(for submission)</small></td>
+                                <td class="">  22,835.00</td>
+                                <td class="">  6,170.00</td>
+                                <td class="">29,005.00</td>
+                                <td class="">Complete</td>
+                                <td class=""><!-- Single button -->
+                                    
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"> Action <span class="caret"></span></button>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a id="uotglr8" class="uif-actionLink" data-toggle="modal" data-target="#summary"><span aria-hidden="true" class="icon-eye-open"></span> View Summary</a></li>
+                                            <li><a href=""  data-toggle="modal" data-target="#copyNew" class=""><span aria-hidden="true" class="icon-copy"></span> Copy</a></li>
+                                            <li><a href="#"><span aria-hidden="true" class="icon-print"></span> Print</a></li>
+                                            <li class="divider"></li>
+                                            <li><a href="#"><span aria-hidden="true" class="icon-ok"></span> Submit with Proposal</a></li>
+                                        </ul>
+                                    </div></td>
+                            </tr>
+                            <tr>
+                                <td scope="row"><input type="radio"  value="radio" name="version-list" id="radio-version-5">
+                                    <label for="radio"></label></td>
+                                <td scope="row" colspan="6" class="versions">NONE <small>(for submission)</small></td>
+                                    </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                        <!-- //BUDGET VERSIONS -->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<style type="text/css">
+ #u569ish_line0 td:nth-child(2){min-width:160px;}
+ small{display:none;}
+.showMe small { display:inline;}
+</style>
+<script>
+(function($){
+    var $radioButtons = $('#u569ish_line0  input[type="radio"]');
+    $radioButtons.click(function() {
+        $radioButtons.each(function() {
+            $(this).parent().toggleClass("success", this.checked);
+        });
+       $("#u569ish_line0  td").removeClass("showMe");
+       $(".success").next().addClass("showMe");
+
+
+    });
+}(jQuery))
+</script> 
+<!-- end Modal --> 
