bf8bc09f947eae83572295b52d4c06b71bafd041
jcaddel <jcaddel@kuali.org>
2014-04-06 15:35:05 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38204 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/slave/copy_repo_to_master.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/slave/copy_repo_to_master.sh
@@ -6,11 +6,10 @@ set -xe
 REPO=/root/.m2/repository
 # The trailing slash is significant here
 SRC=$REPO/
-JENKINS_MASTER=FILL_ME_IN_PLEASE
 DST=root@$JENKINS_MASTER:$REPO
 
 # Make the rsync call
-rsync --archive --stats -v $SRC $DST --exclude="**/org/kuali/**" --exclude="**/SNAPSHOT*.*
+rsync --archive --stats -v $SRC $DST --exclude='**/org/kuali/**' --exclude='**/SNAPSHOT*.*'
 
 set +x
 date
