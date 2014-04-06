d9d230256688d646969c58258ce4516f9f82b9cc
jcaddel <jcaddel@kuali.org>
2014-04-06 15:39:22 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38206 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/slave/copy_repo_to_master.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/jenkins/slave/copy_repo_to_master.sh
@@ -6,9 +6,15 @@ set -xe
 REPO=/root/.m2/repository
 # The trailing slash is significant here
 SRC=$REPO/
+
+# This gets replaced by a real value during the AMI creation
+# Cannot rely on an environment variable provided by the master because this script only executes when the instance is shutting down
+JENKINS_MASTER=FILL_ME_IN_PLEASE
 DST=root@$JENKINS_MASTER:$REPO
 
 # Make the rsync call
+# Ignore all Kuali artifacts, ignore all SNAPSHOT's
+# Kuali artifacts get pre-cached on the build slaves by a different process
 rsync --archive --stats --verbose $SRC $DST --exclude='**/org/kuali/**' --exclude='**/SNAPSHOT*.*'
 
 set +x
