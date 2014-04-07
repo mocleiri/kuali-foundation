a848dc640fa02fb2c21c6df0a0d4860b04a1d237
thrclark <thrclark@kuali.org>
2014-04-07 15:05:01 -0400

    Budget version creation workflow updates
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@38285 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/modal-budget-createnew.php
@@ -0,0 +1,41 @@
+    
+                    
+                    <!-- Modal -->
+                    <div class="modal fade" id="createNew" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    <h4 class="modal-title" id="myModalLabel">Create a Budget Version</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="form-horizontal" role="form">
+                                        <div class="form-group">
+                                            <label for="inputEmail3" class="col-sm-3 control-label">Parent Proposal</label>
+                                            <div class="col-sm-9">
+                                                <p class="form-control-static">#23533 </p>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputPassword3" class="col-sm-3 control-label">Budget Name</label>
+                                            <div class="col-sm-9">
+                                                <input type=" text" class="form-control" id="" placeholder="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Create <span class="caret"></span> </button>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a href="#">Create and stay on Proposal</a></li>
+                                            <li><a href="budget-start.php">Create and go to Budget</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- /Modal -->
