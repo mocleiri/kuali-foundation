eda787c8cc20ffb21976571bf2a9e19e6227271a
jcaddel <jcaddel@kuali.org>
2014-04-06 23:45:55 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38227 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/functions.sh
+++ kuali-devops/src/main/resources/org/kuali/common/kuali-devops/ubuntu/12.04/common/functions.sh
@@ -235,3 +235,17 @@ function check_jenkins_mode {
     usage
   fi
 }
+
+function get_next_line_number {
+  local FILE=$1
+  local TOKEN=$2
+  local LINE_NUMBER=$(get_line_number "$FILE" "$TOKEN")
+  echo $(($LINE_NUMBER + 1))
+}
+
+function get_line_number {
+  local FILE=$1
+  local TOKEN=$2
+  echo $(cat -n $FILE | grep "$TOKEN" | cut -f 1 | xargs)
+}
+
