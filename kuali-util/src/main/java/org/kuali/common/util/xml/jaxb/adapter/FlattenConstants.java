f2ac3f5d2a1bf2c7449254e4596db529af594d84
jcaddel <jcaddel@kuali.org>
2014-04-07 12:55:50 -0400

    [maven-release-plugin]  copy for tag kuali-aws-1.0.1
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-aws-1.0.1@38255 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/xml/jaxb/adapter/FlattenConstants.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.common.util.xml.jaxb.adapter;
-
-public abstract class FlattenConstants {
-
-	public static final String DEFAULT_CR_REPLACEMENT = "${jaxb.cr}";
-	public static final String DEFAULT_LF_REPLACEMENT = "${jaxb.lf}";
-
-}
