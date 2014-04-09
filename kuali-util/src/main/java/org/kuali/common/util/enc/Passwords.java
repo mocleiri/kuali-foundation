37674468653e0b0c4a9c64af3fd198dabeac411c
jcaddel <jcaddel@kuali.org>
2014-04-09 12:38:39 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38395 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/enc/Passwords.java
+++ kuali-util/src/main/java/org/kuali/common/util/enc/Passwords.java
@@ -7,24 +7,27 @@ import static org.apache.commons.io.FileUtils.readFileToString;
 import static org.apache.commons.lang3.StringUtils.substringBetween;
 import static org.apache.commons.lang3.StringUtils.trimToNull;
 import static org.kuali.common.util.base.Exceptions.illegalState;
+import static org.kuali.common.util.base.Precondition.checkNotBlank;
+import static org.kuali.common.util.log.Loggers.newLogger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.kuali.common.util.Encodings;
 import org.kuali.common.util.Str;
 import org.kuali.common.util.file.CanonicalFile;
-import org.kuali.common.util.log.LoggerUtils;
 import org.slf4j.Logger;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 
 public final class Passwords {
 
-	private static final String SYS_KEY = "enc.password";
+	private static final List<String> SYS_KEYS = ImmutableList.of("enc.password", "properties.enc.password");
 	private static final String ENV_KEY = "ENC_PASSWORD";
 	private static final File SETTINGS = getSettingsXmlFile();
-	private static final Logger logger = LoggerUtils.make();
+	private static final Logger logger = newLogger();
 
 	private static String password;
 
@@ -36,27 +39,33 @@ public final class Passwords {
 	}
 
 	protected static String initPassword() {
-		Optional<String> sys = getSystemPassword();
+		Optional<Property> sys = getSystemPassword();
 		if (sys.isPresent()) {
-			logger.info(format("Located [%s] in system properties", SYS_KEY));
-			return Str.reveal(sys.get());
+			logger.info(format("Located [%s] in system properties", sys.get().getKey()));
+			return Str.reveal(sys.get().getValue());
 		}
 		Optional<String> env = getEnvPassword();
 		if (env.isPresent()) {
 			logger.info(format("Located [%s] in environment variables", ENV_KEY));
 			return Str.reveal(env.get());
 		}
-		Optional<String> settings = getSettingsXmlPassword();
+		Optional<Property> settings = getSettingsXmlPassword();
 		if (settings.isPresent()) {
-			logger.info(format("Located [%s] in [%s]", SYS_KEY, SETTINGS));
-			return Str.reveal(settings.get());
+			logger.info(format("Located [%s] in [%s]", settings.get().getKey(), SETTINGS));
+			return Str.reveal(settings.get().getValue());
 		} else {
 			throw illegalState("encryption password could not be found in system properties, environment variables, or [%s]", SETTINGS);
 		}
 	}
 
-	protected static Optional<String> getSystemPassword() {
-		return fromNullable(System.getProperty(SYS_KEY));
+	protected static Optional<Property> getSystemPassword() {
+		for (String key : SYS_KEYS) {
+			Optional<String> password = fromNullable(trimToNull(System.getProperty(key)));
+			if (password.isPresent()) {
+				return Optional.of(new Property(key, password.get()));
+			}
+		}
+		return absent();
 	}
 
 	protected static Optional<String> getEnvPassword() {
@@ -68,16 +77,45 @@ public final class Passwords {
 		return new CanonicalFile(path);
 	}
 
-	protected static Optional<String> getSettingsXmlPassword() {
+	protected static Optional<Property> getSettingsXmlPassword() {
 		if (!SETTINGS.exists()) {
 			return absent();
 		}
+		String content = getSettingsXmlContent();
+		for (String key : SYS_KEYS) {
+			String nullable = substringBetween(content, "<" + key + ">", "</" + key + ">");
+			Optional<String> password = fromNullable(trimToNull(nullable));
+			if (password.isPresent()) {
+				return Optional.of(new Property(key, password.get()));
+			}
+		}
+		return absent();
+	}
+
+	protected static String getSettingsXmlContent() {
 		try {
-			String contents = readFileToString(SETTINGS, Encodings.UTF8);
-			String password = substringBetween(contents, "<" + SYS_KEY + ">", "</" + SYS_KEY + ">");
-			return fromNullable(trimToNull(password));
+			return readFileToString(SETTINGS, Encodings.UTF8);
 		} catch (IOException e) {
 			throw illegalState(e, "unexpected io error -> [%s]", SETTINGS);
+		}
+	}
+
+	private static final class Property {
+
+		private final String key;
+		private final String value;
+
+		public Property(String key, String value) {
+			this.key = checkNotBlank(key, "key");
+			this.value = checkNotBlank(value, "value");
+		}
+
+		public String getKey() {
+			return key;
+		}
+
+		public String getValue() {
+			return value;
 		}
 	}
 
