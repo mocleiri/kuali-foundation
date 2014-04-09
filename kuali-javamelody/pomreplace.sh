693c5a0f61fcc96b2ab69287e38e67b764f029c0
jcaddel <jcaddel@kuali.org>
2014-04-09 19:40:46 -0400

    [maven-release-plugin]  copy for tag kuali-pom-3.4.6
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-pom-3.4.6@38461 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-javamelody/pomreplace.sh
+++
@@ -1,2 +0,0 @@
-find . -name 'pom.xml' -print0 | xargs -0 perl -pi -e 's|1.43.0-SNAPSHOT|1.43-patch1|g'
-
--- kuali-util/src/test/java/org/kuali/common/util/taxpayer/SomeOtherType.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.common.util.taxpayer;
-
-public interface SomeOtherType<T> {
-
-	void myMethod(T argument);
-
-}
