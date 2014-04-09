fe72ea5f64fcd421b9c6cc7acbd523f7114399c2
jcaddel <jcaddel@kuali.org>
2014-04-09 15:57:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38430 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/provider/DefaultEncryptionContextProviderChain.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/provider/DefaultEncryptionContextProviderChain.java
@@ -1,6 +1,6 @@
 package org.kuali.common.util.encrypt.provider;
 
-import static com.google.common.base.Optional.absent;
+import static org.kuali.common.util.base.Exceptions.illegalState;
 
 import java.util.List;
 
@@ -37,7 +37,7 @@ public final class DefaultEncryptionContextProviderChain implements EncryptionCo
 				return context;
 			}
 		}
-		return absent();
+		throw illegalState("unable to locate encryption password in any of the %s providers", providers.size());
 	}
 
 }
