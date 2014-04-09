60674338a8d3afb8c07d43ce124ded328a3a4348
jcaddel <jcaddel@kuali.org>
2014-04-09 16:03:10 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38432 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
@@ -5,7 +5,7 @@ import org.kuali.common.util.encrypt.jasypt.DefaultJasyptEncryptor;
 public final class Encryption {
 
 	public static final String ENCRYPTION_PASSWORD_KEY = "enc.password";
-	public static final String ENCRYPTION_STRENGTH_KEY = "enc.password.strength";
+	public static final String ENCRYPTION_STRENGTH_KEY = "enc.strength";
 
 	private static Encryptor encryptor;
 
