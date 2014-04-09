ccb10d369bb5e1d9aeb20cd3de299308eaa7b4c5
jcaddel <jcaddel@kuali.org>
2014-04-09 16:01:55 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38431 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
@@ -1,141 +1,18 @@
 package org.kuali.common.util.encrypt;
 
-import static com.google.common.base.Optional.absent;
-import static com.google.common.base.Optional.fromNullable;
-import static java.lang.String.format;
-import static org.apache.commons.io.FileUtils.readFileToString;
-import static org.apache.commons.lang3.StringUtils.substringBetween;
-import static org.apache.commons.lang3.StringUtils.trimToNull;
-import static org.kuali.common.util.Encodings.UTF8;
-import static org.kuali.common.util.base.Exceptions.illegalState;
-import static org.kuali.common.util.base.Precondition.checkNotBlank;
-import static org.kuali.common.util.log.Loggers.newLogger;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-import org.kuali.common.util.Str;
 import org.kuali.common.util.encrypt.jasypt.DefaultJasyptEncryptor;
-import org.kuali.common.util.file.CanonicalFile;
-import org.slf4j.Logger;
-
-import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableList;
 
 public final class Encryption {
 
 	public static final String ENCRYPTION_PASSWORD_KEY = "enc.password";
 	public static final String ENCRYPTION_STRENGTH_KEY = "enc.password.strength";
 
-	// If this is set to "true" any passwords stored as system properties get removed from the system properties
-	private static final String REMOVE_SYSTEM_PROPERTY_KEY = "enc.password.remove";
-	private static final List<String> SYS_KEYS = ImmutableList.of("enc.password", "properties.enc.password");
-	private static final String ENV_KEY = "ENC_PASSWORD";
-	private static final File SETTINGS = getSettingsXmlFile();
-	private static final Logger logger = newLogger();
-
 	private static Encryptor encryptor;
 
 	public synchronized static Encryptor getDefaultEncryptor() {
 		if (encryptor == null) {
-			encryptor = new DefaultJasyptEncryptor(getDefaultEncryptionPassword());
+			encryptor = new DefaultJasyptEncryptor();
 		}
 		return encryptor;
 	}
-
-	protected static String getDefaultEncryptionPassword() {
-		Optional<Property> sys = getSystemPassword();
-		if (sys.isPresent()) {
-			logger.info(format("located [%s] in system properties", sys.get().getKey()));
-			if (Boolean.getBoolean(REMOVE_SYSTEM_PROPERTY_KEY)) {
-				removeSystemProperties();
-			}
-			return Str.reveal(sys.get().getValue());
-		}
-		Optional<String> env = getEnvPassword();
-		if (env.isPresent()) {
-			logger.info(format("located [%s] in environment variables", ENV_KEY));
-			return Str.reveal(env.get());
-		}
-		Optional<Property> settings = getSettingsXmlPassword();
-		if (settings.isPresent()) {
-			logger.info(format("located [%s] in [%s]", settings.get().getKey(), SETTINGS));
-			return Str.reveal(settings.get().getValue());
-		} else {
-			throw illegalState("encryption password could not be found in system properties, environment variables, or [%s]", SETTINGS);
-		}
-	}
-
-	protected static void removeSystemProperties() {
-		for (String key : SYS_KEYS) {
-			String value = System.getProperty(key);
-			if (value != null) {
-				logger.info(format("removing system property [%s]", key));
-				System.getProperties().remove(key);
-			}
-		}
-	}
-
-	protected static Optional<Property> getSystemPassword() {
-		for (String key : SYS_KEYS) {
-			Optional<String> password = fromNullable(trimToNull(System.getProperty(key)));
-			if (password.isPresent()) {
-				return Optional.of(new Property(key, password.get()));
-			}
-		}
-		return absent();
-	}
-
-	protected static Optional<String> getEnvPassword() {
-		return fromNullable(System.getenv(ENV_KEY));
-	}
-
-	protected static File getSettingsXmlFile() {
-		String path = System.getProperty("user.home") + "/.m2/settings.xml";
-		return new CanonicalFile(path);
-	}
-
-	protected static Optional<Property> getSettingsXmlPassword() {
-		if (!SETTINGS.exists()) {
-			return absent();
-		}
-		String content = getSettingsXmlContent();
-		for (String key : SYS_KEYS) {
-			String nullable = substringBetween(content, "<" + key + ">", "</" + key + ">");
-			Optional<String> password = fromNullable(trimToNull(nullable));
-			if (password.isPresent()) {
-				return Optional.of(new Property(key, password.get()));
-			}
-		}
-		return absent();
-	}
-
-	protected static String getSettingsXmlContent() {
-		try {
-			return readFileToString(SETTINGS, UTF8);
-		} catch (IOException e) {
-			throw illegalState(e, "unexpected io error -> [%s]", SETTINGS);
-		}
-	}
-
-	private static final class Property {
-
-		private final String key;
-		private final String value;
-
-		public Property(String key, String value) {
-			this.key = checkNotBlank(key, "key");
-			this.value = checkNotBlank(value, "value");
-		}
-
-		public String getKey() {
-			return key;
-		}
-
-		public String getValue() {
-			return value;
-		}
-	}
-
 }
