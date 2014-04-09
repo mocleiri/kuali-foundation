e62548e71e98332271fa1c0e6217af8fed5f2051
jcaddel <jcaddel@kuali.org>
2014-04-09 17:42:44 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38443 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
@@ -13,6 +13,8 @@ public final class Encryption {
 
 	public static final String ENCRYPTION_PASSWORD_KEY = "enc.password";
 	public static final String ENCRYPTION_STRENGTH_KEY = "enc.strength";
+
+	private static final String ENCRYPTION_PASSWORD_REMOVE_KEY = "enc.password.remove";
 	private static final Logger logger = newLogger();
 
 	private static Encryptor encryptor;
@@ -26,6 +28,12 @@ public final class Encryption {
 			} else {
 				encryptor = NoOpEncryptor.INSTANCE;
 				logger.info(format("encryption disabled - [%s] is not set", ENCRYPTION_PASSWORD_KEY));
+			}
+			if (Boolean.getBoolean(ENCRYPTION_PASSWORD_REMOVE_KEY)) {
+				if (System.getProperty(ENCRYPTION_PASSWORD_KEY) != null) {
+					logger.info(format("removing system property [%s]", ENCRYPTION_PASSWORD_KEY));
+					System.getProperties().remove(ENCRYPTION_PASSWORD_KEY);
+				}
 			}
 		}
 		return encryptor;
