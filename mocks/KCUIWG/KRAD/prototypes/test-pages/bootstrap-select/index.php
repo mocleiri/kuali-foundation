b9405d75c0c5d4608535a0c3ab4fc6d8a1dece81
thrclark <thrclark@kuali.org>
2014-04-09 09:49:17 -0400

    small html fix
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@38391 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KRAD/prototypes/test-pages/bootstrap-select/index.php
+++ mocks/KCUIWG/KRAD/prototypes/test-pages/bootstrap-select/index.php
@@ -11,18 +11,18 @@ function randomText($numchar)
 ?>
 <!DOCTYPE html>
 <html>
-<head>
+    <head>
     <title></title>
     <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="bootstrap-select.css">
-    
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-   	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="bootstrap-select.js"></script>
-
     <style>
-	.container { margin: 20px;}
-	</style>
+.container {
+	margin: 20px;
+}
+</style>
     <script type="text/javascript">
         jQuery(document).ready(function () {
 
@@ -43,25 +43,24 @@ function randomText($numchar)
 			});
         });
     </script>
-</head>
-<body>
-
-    <div class="container">
-    <h1>Bootstrap Select Demo</h1>
-        <form class="form-horizontal" role="form">
-            <div class="form-group">
-                <label for="bs3Select" class="col-lg-2 control-label">Test bootstrap 3 form</label>
-                <div class="col-lg-3">
-                    <select id="bs3Select" class="selectpicker show-tick form-control" multiple data-live-search="true">
-                        <option>cow</option>
-                        <option>bull</option>
-                        <option class="get-class" disabled>ox</option>
-                        <optgroup label="test" data-subtext="another test" data-icon="icon-ok">
-                            <option>ASD</option>
-                            <option selected>Bla</option>
-                            <option>Ble</option>
-                        </optgroup>
-                        <?php 
+    </head>
+    <body>
+<div class="container">
+<h1>Bootstrap Select Demo</h1>
+<form class="form-horizontal" role="form">
+        <div class="form-group">
+        <label for="bs3Select" class="col-lg-2 control-label">Test bootstrap 3 form</label>
+        <div class="col-lg-3">
+                <select id="bs3Select" class="selectpicker show-tick form-control" multiple data-live-search="true">
+                <option>cow</option>
+                <option>bull</option>
+                <option class="get-class" disabled>ox</option>
+                <optgroup label="test" data-subtext="another test" data-icon="icon-ok">
+                    <option>ASD</option>
+                    <option selected>Bla</option>
+                    <option>Ble</option>
+                    </optgroup>
+                <?php 
 							foreach (range(1, 100) as $number) { 
 								echo optGroup($number);
 								foreach (range(0, rand(3, 15)) as $n) { 
@@ -79,12 +78,9 @@ function randomText($numchar)
 								return "<option>" . randomText(7) . "</option>";
 							}
 						?>
-                        
-                    </select>
-                </div>
-              </div>
-        <form>
+            </select>
+            </div>
     </div>
-
+    </form>
 </body>
 </html>
