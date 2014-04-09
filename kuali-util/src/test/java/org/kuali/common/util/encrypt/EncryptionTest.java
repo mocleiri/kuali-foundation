fcf1626bbcf6c1a655d0cfe49a7b6bf3a0549681
jcaddel <jcaddel@kuali.org>
2014-04-09 16:11:58 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38434 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/encrypt/EncryptionTest.java
@@ -0,0 +1,21 @@
+package org.kuali.common.util.encrypt;
+
+import org.junit.Test;
+
+public class EncryptionTest {
+
+	@Test
+	public void test() {
+		try {
+			Encryptor encryptor = Encryption.getDefaultEncryptor();
+			String text = "foo";
+			String encrypted = encryptor.encrypt(text);
+			String decrypted = encryptor.decrypt(encrypted);
+			System.out.println(text);
+			System.out.println(decrypted);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+}
