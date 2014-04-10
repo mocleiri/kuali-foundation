91d7398cf5dd0e52d282b9e01bf132145d42aef0
jcaddel <jcaddel@kuali.org>
2014-04-09 23:56:54 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38472 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
@@ -19,32 +19,15 @@ public final class Encryption {
 
 	private static Encryptor encryptor;
 
-	public synchronized static Encryptor buildEncryptor(String encryptionPasswordKey) {
-		if (encryptor == null) {
-			Optional<EncryptionContext> context = new DefaultEncryptionContextProviderChain().getEncryptionContext();
-			if (context.isPresent()) {
-				encryptor = new DefaultJasyptEncryptor(context.get());
-				logger.info(format("encryption enabled [strength=%s]", context.get().getStrength()));
-			} else {
-				encryptor = NoOpEncryptor.INSTANCE;
-				logger.info(format("encryption disabled - [%s] is not set", ENCRYPTION_PASSWORD_KEY));
-			}
-			if (Boolean.getBoolean(ENCRYPTION_PASSWORD_REMOVE_KEY) && System.getProperty(ENCRYPTION_PASSWORD_KEY) != null) {
-				logger.info(format("removing system property [%s]", ENCRYPTION_PASSWORD_KEY));
-				System.getProperties().remove(ENCRYPTION_PASSWORD_KEY);
-			}
-		}
-		return encryptor;
-	}
 	public synchronized static Encryptor buildDefaultEncryptor() {
 		if (encryptor == null) {
-			Optional<EncryptionContext> context = new DefaultEncryptionContextProviderChain().getEncryptionContext();
+			Optional<EncryptionContext> context = new DefaultEncryptionContextProviderChain(ENCRYPTION_PASSWORD_KEY).getEncryptionContext();
 			if (context.isPresent()) {
-				encryptor = new DefaultJasyptEncryptor(context.get());
 				logger.info(format("encryption enabled [strength=%s]", context.get().getStrength()));
+				encryptor = new DefaultJasyptEncryptor(context.get());
 			} else {
-				encryptor = NoOpEncryptor.INSTANCE;
 				logger.info(format("encryption disabled - [%s] is not set", ENCRYPTION_PASSWORD_KEY));
+				encryptor = NoOpEncryptor.INSTANCE;
 			}
 			if (Boolean.getBoolean(ENCRYPTION_PASSWORD_REMOVE_KEY) && System.getProperty(ENCRYPTION_PASSWORD_KEY) != null) {
 				logger.info(format("removing system property [%s]", ENCRYPTION_PASSWORD_KEY));
