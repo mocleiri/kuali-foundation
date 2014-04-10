1b6ce260f03372309158db3ef106318a2d764846
jcaddel <jcaddel@kuali.org>
2014-04-10 17:57:44 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38538 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/dnsme/ProductionDNSMEContextConfig.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/dnsme/ProductionDNSMEContextConfig.java
@@ -1,12 +1,11 @@
 package org.kuali.common.devops.dnsme;
 
 import org.kuali.common.dns.dnsme.URLS;
+import org.kuali.common.dns.dnsme.model.DNSMadeEasyCredentials;
 import org.kuali.common.dns.dnsme.model.DNSMadeEasyServiceContext;
 import org.kuali.common.dns.dnsme.spring.DNSMEServiceContextConfig;
 import org.kuali.common.dns.dnsme.spring.DNSMadeEasyUtils;
 import org.kuali.common.dns.spring.DomainNameConfig;
-import org.kuali.common.util.enc.EncryptionService;
-import org.kuali.common.util.enc.spring.DefaultEncryptionServiceConfig;
 import org.kuali.common.util.spring.env.EnvironmentService;
 import org.kuali.common.util.spring.service.SpringServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +14,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ SpringServiceConfig.class, DefaultEncryptionServiceConfig.class })
+@Import({ SpringServiceConfig.class })
 public class ProductionDNSMEContextConfig implements DNSMEServiceContextConfig {
 
 	@Autowired
 	EnvironmentService env;
-
-	@Autowired
-	EncryptionService enc;
 
 	@Autowired
 	DomainNameConfig config;
@@ -30,9 +26,9 @@ public class ProductionDNSMEContextConfig implements DNSMEServiceContextConfig {
 	@Override
 	@Bean
 	public DNSMadeEasyServiceContext dnsMadeEasyServiceContext() {
-		EncryptedDNSMECredentials encrypted = EncryptedDNSMECredentials.PRODUCTION.getCredentials();
+		DNSMadeEasyCredentials encrypted = EncryptedDNSMECredentials.PRODUCTION.getCredentials();
 		String restApiURL = URLS.PRODUCTION;
-		return DNSMadeEasyUtils.getServiceContext(env, enc, restApiURL, config.domainName(), encrypted);
+		return DNSMadeEasyUtils.getServiceContext(env, restApiURL, config.domainName(), encrypted);
 	}
 
 }
