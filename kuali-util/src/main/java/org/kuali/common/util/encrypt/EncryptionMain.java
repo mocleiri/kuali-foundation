2fac8138e6b1135b8e3c900ae4e6ffd4da638812
jcaddel <jcaddel@kuali.org>
2014-04-10 10:12:45 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38489 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/EncryptionMain.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/EncryptionMain.java
@@ -1,5 +1,16 @@
 package org.kuali.common.util.encrypt;
 
+import static com.google.common.base.Optional.absent;
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.kuali.common.util.base.Exceptions.illegalState;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.kuali.common.util.file.CanonicalFile;
+
+import com.google.common.base.Optional;
+
 public final class EncryptionMain {
 
 	public static void main(String[] args) {
@@ -7,12 +18,49 @@ public final class EncryptionMain {
 			usage();
 			System.exit(1);
 		}
-		Encryptor encryptor = Encryption.buildDefaultEncryptor();
-		String encrypted = encryptor.encrypt(args[0]);
-		System.out.println(encrypted);
+		boolean encrypt = matches(args, "-e", "--encrypt");
+		boolean decrypt = matches(args, "-d", "--decrypt");
+		String text = getText(args);
+		if (encrypt) {
+			System.out.println(Encryption.buildDefaultEncryptor().encrypt(text));
+		}
+		if (decrypt) {
+			System.out.println(Encryption.buildDefaultEncryptor().decrypt(text));
+		}
+	}
+
+	private static String getText(String[] args) {
+		Optional<File> file = getFile(args);
+		if (file.isPresent()) {
+			try {
+				return readFileToString(file.get());
+			} catch (IOException e) {
+				throw illegalState(e);
+			}
+		} else {
+			return args[args.length - 1];
+		}
+	}
+
+	private static Optional<File> getFile(String[] args) {
+		for (String arg : args) {
+			if (arg.equals("-f") || arg.equals("--file")) {
+				return Optional.<File> of(new CanonicalFile(arg));
+			}
+		}
+		return absent();
+	}
+
+	private static boolean matches(String[] args, String option, String longoption) {
+		for (String arg : args) {
+			if (arg.equals(option) || arg.equals(longoption)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private static void usage() {
-		System.out.println("Usage: EncryptionMain text");
+		System.out.println("Usage: EncryptionMain --encrypt --decrypt [--file=filename] text");
 	}
 }
