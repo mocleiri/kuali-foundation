4f382ce535e84b939d7f193f3fe2f1a134155020
mfitzwater <mfitzwater@kuali.org>
2014-04-10 14:22:30 -0400

    Removed the collapsible box, changed the placement of the updated by, updated time to get the submit action above the fold
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@38508 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KRAD/prototypes/kc-budget/budget-ng-notes.php
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/budget-ng-notes.php
@@ -2,7 +2,6 @@
 $page = 'notes';
 $section = '';
 ?>
-
 <!DOCTYPE HTML>
 <html lang=en>
 <head>
@@ -12,69 +11,73 @@ $section = '';
     <?php include ('includes/styles.php') ?>
     <style type="text/css">
 
-
+#budget-notes-area{width:100%; min-height:355px; margin-bottom: 1em;;}
         .data-row-table th{ padding:5px; text-align:right; }
     </style>
 </head>
 <body id="Uif-Application" style="padding-bottom: 570px;">
 <!-- APPLICATION HEADER -->
 <header id="Uif-ApplicationHeader-Wrapper" data-sticky=true class="uif-sticky" style="position:fixed; left: 0; top: 0px;">
-    <header id="u1xj79g4" class="uif-applicationHeader">
-        <div class="container">
-            <nav id="u1osy4lo" class="navbar" role=navigation>
-                <div class="navbar-header">
-                    <button type=button class="navbar-toggle" data-toggle=collapse data-target=.navbar-ex1-collapse><span class="sr-only">Toggle navigation</span> <span class="icon-reorder"></span></button>
-                    <a class="navbar-brand" href="index.php">
-                        <div class="logoBrand">
-                            <h1><img id="u2elq10" src="http://ux.kuali.org/prototypes/kc/p2-devbranch/themes/kc/img/logo.png" alt="" class="uif-image"> </h1>
-                        </div>
-                    </a></div>
-                <div id="u1gk19wq" class="collapse navbar-collapse navbar-ex1-collapse uif-listGroup">
-                    <ul class="nav navbar-nav navbar-right uif-listLayout">
-                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle=dropdown> Researcher </a>
-                            <div id="uyedx8c" class="dropdown-menu uif-cssGridGroup" style="min-width: 300px; right: -180px;">
-                                <div class="row ">
-                                    <div class="col-md-12">
-                                        <section id="u9tj3ss" class="uif-listGroup">
-                                            <h3 class="uif-headerText">Proposal Development</h3>
-                                            <ul class="uif-listLayout">
-                                                <li><a href="prop-start.php" class="uif-actionLink" id="umdwwyj" tabindex=0 data-role=Action> Create a Proposal </a></li>
-                                                <li><a href="#" class="uif-actionLink" id="umdwwze" tabindex=0 data-role=Action> Created a Proposal Budget</a></li>
-                                            </ul>
-                                        </section>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle=dropdown> Unit </a></li>
-                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle=dropdown> Central Admin </a></li>
-                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle=dropdown> Maintenance </a></li>
-                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle=dropdown> System Admin </a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-        <div id="upils8b" class="uif-cssGridGroup toolbar">
-            <div class="container">
-                <div class="row ">
-                    <div class="col-md-12">
-                        <div id="u1i3m5yh" class="uif-listGroup" data-parent=upils8b>
-                            <ul class="uif-listLayout nav pull-right">
-                                <li class="pull-right"><a href="#" class="uif-actionLink" id="u1o09qku" tabindex=0 data-role=Action data-onclick="redirect(&quot;../kr-krad/labs?methodToCall=start&amp;viewId=Lab-ActionListView&quot;);"> Action List </a></li>
-                                <li class="pull-right"><a href="#" class="uif-actionLink" id="u1o09qlp" tabindex=0 data-role=Action data-onclick="redirect(&quot;../kr-krad/labs?methodToCall=start&amp;viewId=Lab-DocSearchView&quot;);"> Doc Search </a></li>
-                                <li class="dropdown pull-right"><a href="#" class="dropdown-toggle" data-toggle=dropdown> Backdoor Login <span class="caret"></span> </a>
-                                    <ul class="dropdown-menu uif-listLayout">
-                                        <li><a href="#" class="uif-actionLink" id="u101xf6k" tabindex=0 data-role=Action data-onclick="redirect(&quot;../kr-krad/labs?methodToCall=start&amp;viewId=Lab-BootstrapComponents&quot;);"> Preferences </a></li>
-                                        <li><a href="#" class="uif-actionLink" id="u101xf7f" tabindex=0 data-role=Action data-onclick="redirect(&quot;../kr-krad/labs?methodToCall=start&amp;viewId=Lab-BootstrapComponents&quot;);"> Logout </a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
+    <header id="u1xj79g4" class="uif-applicationHeader"> 
+<div class="container">
+	<nav id="u1osy4lo" class="navbar" role="navigation"> 
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"><span class="sr-only">Toggle navigation</span> <span class="icon-reorder"></span></button> <a class="navbar-brand" href="index.php"> 
+	<div class="logoBrand">
+		<h1>
+			<img id="u2elq10" src="http://ux.kuali.org/prototypes/kc/p2-devbranch/themes/kc/img/logo.png" alt="" class="uif-image"> 
+		</h1>
+	</div>
+	</a>
+</div>
+<div id="u1gk19wq" class="collapse navbar-collapse navbar-ex1-collapse uif-listGroup">
+	<ul class="nav navbar-nav navbar-right uif-listLayout">
+		<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> Researcher </a> 
+		<div id="uyedx8c" class="dropdown-menu uif-cssGridGroup" style="min-width: 300px; right: -180px;">
+			<div class="row ">
+				<div class="col-md-12">
+					<section id="u9tj3ss" class="uif-listGroup">
+						<h3 class="uif-headerText">
+							Proposal Development
+						</h3>
+						<ul class="uif-listLayout">
+							<li><a href="prop-start.php" class="uif-actionLink" id="umdwwyj" tabindex="0" data-role="Action"> Create a Proposal </a></li>
+							<li><a href="#" class="uif-actionLink" id="umdwwze" tabindex="0" data-role="Action"> Created a Proposal Budget</a></li>
+						</ul>
+					</section>
+				</div>
+			</div>
+		</div>
+		</li>
+		<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> Unit </a></li>
+		<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> Central Admin </a></li>
+		<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> Maintenance </a></li>
+		<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> System Admin </a></li>
+	</ul>
+</div>
+</nav> 
+</div>
+<div id="upils8b" class="uif-cssGridGroup toolbar">
+	<div class="container">
+		<div class="row ">
+			<div class="col-md-12">
+				<div id="u1i3m5yh" class="uif-listGroup" data-parent="upils8b">
+					<ul class="uif-listLayout nav pull-right">
+						<li class="pull-right"><a href="#" class="uif-actionLink" id="u1o09qku" tabindex="0" data-role="Action" data-onclick="redirect(&quot;../kr-krad/labs?methodToCall=start&amp;viewId=Lab-ActionListView&quot;);"> Action List </a></li>
+						<li class="pull-right"><a href="#" class="uif-actionLink" id="u1o09qlp" tabindex="0" data-role="Action" data-onclick="redirect(&quot;../kr-krad/labs?methodToCall=start&amp;viewId=Lab-DocSearchView&quot;);"> Doc Search </a></li>
+						<li class="dropdown pull-right"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> Backdoor Login <span class="caret"></span> </a> 
+						<ul class="dropdown-menu uif-listLayout">
+							<li><a href="#" class="uif-actionLink" id="u101xf6k" tabindex="0" data-role="Action" data-onclick="redirect(&quot;../kr-krad/labs?methodToCall=start&amp;viewId=Lab-BootstrapComponents&quot;);"> Preferences </a></li>
+							<li><a href="#" class="uif-actionLink" id="u101xf7f" tabindex="0" data-role="Action" data-onclick="redirect(&quot;../kr-krad/labs?methodToCall=start&amp;viewId=Lab-BootstrapComponents&quot;);"> Logout </a></li>
+						</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</header>
     <!-- Backdoor info (here to inherit stickyness with the header, if set) --></header>
 <form id="kualiForm" action="../kr-krad/uicomponents" method=post accept-charset=UTF-8>
     <!-- VIEW -->
@@ -91,43 +94,27 @@ $section = '';
                         <h2 class="uif-headerText"><span class="uif-headerText-span">Budget Notes</span></h2>
                     </div>
                 </header>
-
-                <br>
-                <section id="u1qq592w" class="uif-disclosure uif-boxLayoutVerticalItem clearfix" data-parent=Demo-Disclosure-Example1>
-                    <header id="u1l3ufy3" class="uif-sectionHeader" data-header_for=u1qq592w>
-                        <h3 class="uif-headerText"><a data-role=disclosureLink data-linkfor="projectNotes" href="#" id="u1qq592w_toggle" data-open=true data-widgetid=u1vpenbn data-speed=500 data-ajax=false><span class="uif-headerText-span"><span id="u1qq592w_toggle_exp" class="icon-caret-down"></span><span style="display:none;" id="u1qq592w_toggle_col" class="icon-caret-right"></span> Notes</span></a></h3>
-                    </header>
-                    <div id="projectNotes" data-role=disclosureContent data-open=true class="uif-disclosureContent"> <br>
+             <section>
+                
                         <div class="row">
-                            <div class="col-md-3">
-                                <strong>Last updated:</strong>
-                            </div>
-                            <div class="col-md-9">
-                                01/22/2014
-                            </div>
+                          
                         </div>
                         <div class="row">
-                            <div class="col-md-3">
-                                <strong>Updated by:</strong>
-                            </div>
-                            <div class="col-md-9">
-                                quickstart
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3">
-                                <strong>Notes:</strong>
-                            </div>
-                            <div class="col-md-9">
-                                Praesent laoreet dui nec arcu rhoncus pellentesque. Pellentesque quam nulla, congue in velit et, varius blandit risus. Integer pretium velit velit, et ultrices quam rhoncus sed. Praesent pretium velit a egestas bibendum. Fusce commodo justo quis lacus sollicitudin varius. In in ipsum ut justo tristique tincidunt id quis quam. Vivamus mattis tempus urna a ornare. Vestibulum lectus velit, dapibus quis mauris a, porta luctus ante. In hac habitasse platea dictumst.
-                            </div>
+                          
+                            <div class="col-md-12 clearfix">
+                              <div class="col-md-3 pull-right text-right"><strong>Last updated:</strong> 01/22/2014</div>
+                      
+                            <div class="col-md-3 pull-right text-right"><strong>Updated by:</strong>  quickstart  </div>
+                           
+                             <label for="budget-notes-area" class=""><strong>Notes</strong></label><br><textarea id="budget-notes-area"  class="form-control input">Praesent laoreet dui nec arcu rhoncus pellentesque. Pellentesque quam nulla, congue in velit et, varius blandit risus. Integer pretium velit velit, et ultrices quam rhoncus sed. Praesent pretium velit a egestas bibendum. Fusce commodo justo quis lacus sollicitudin varius. In in ipsum ut justo tristique tincidunt id quis quam. Vivamus mattis tempus urna a ornare. Vestibulum lectus velit, dapibus quis mauris a, porta luctus ante. In hac habitasse platea dictumst.
+                           </textarea> </div>
                         </div>
                         <div class="row">
                             <div class="col-md-12">
                                 <button class="btn btn-default btn-sm pull-right">Consolidate expense justifications</button>
                             </div>
                         </div>
-                    </div>
+               
                 </section>
             </main>
         </div>
