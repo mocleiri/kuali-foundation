2136ff0790677b9441a7b0df18881e5649d9ed3a
jcaddel <jcaddel@kuali.org>
2014-04-10 00:13:05 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38475 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/encrypt/EncryptionTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/encrypt/EncryptionTest.java
@@ -1,22 +1,21 @@
 package org.kuali.common.util.encrypt;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.kuali.common.util.encrypt.Encryption.buildDefaultEncryptor;
+
 import org.junit.Test;
 
 public class EncryptionTest {
 
 	@Test
 	public void test() {
-		try {
-			Encryptor encryptor = Encryption.buildDefaultEncryptor();
-			String plaintext = "foo";
-			String encrypted = encryptor.encrypt(plaintext);
-			String decrypted = encryptor.decrypt(encrypted);
-			System.out.println(plaintext);
-			System.out.println(encrypted);
-			System.out.println(decrypted);
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+		Encryptor encryptor = buildDefaultEncryptor();
+		String plaintext = "foo";
+		String encrypted = encryptor.encrypt(plaintext);
+		String decrypted = encryptor.decrypt(encrypted);
+		assertNotEquals(plaintext, encrypted);
+		assertEquals(plaintext, decrypted);
 	}
 
 }
