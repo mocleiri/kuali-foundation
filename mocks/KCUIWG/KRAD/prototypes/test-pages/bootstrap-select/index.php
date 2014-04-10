14cefdd935cab73d3e43515a742f35a19949fc0d
tpaegle <tpaegle@kuali.org>
2014-04-10 14:30:06 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@38511 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KRAD/prototypes/test-pages/bootstrap-select/index.php
+++ mocks/KCUIWG/KRAD/prototypes/test-pages/bootstrap-select/index.php
@@ -15,7 +15,7 @@ function randomText($numchar)
     <title></title>
     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="bootstrap-select.css">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
    	<script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="bootstrap-select.js"></script>
 
