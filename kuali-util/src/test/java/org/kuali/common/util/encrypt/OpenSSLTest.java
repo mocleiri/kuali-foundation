3a6f851eae744a98dc6ae031d64e31b90c9eb0a2
jcaddel <jcaddel@kuali.org>
2014-04-12 18:44:28 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38634 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/encrypt/OpenSSLTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/encrypt/OpenSSLTest.java
@@ -4,18 +4,20 @@ import static java.lang.String.format;
 import static javax.crypto.Cipher.DECRYPT_MODE;
 import static org.kuali.common.util.Encodings.UTF8;
 import static org.kuali.common.util.HexUtils.getBytesFromHexString;
+import static org.kuali.common.util.HexUtils.toHexStringLower;
+import static org.kuali.common.util.HexUtils.toStringFromHex;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
+import java.io.IOException;
 import java.security.Key;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.codehaus.plexus.util.Base64;
 import org.junit.Test;
 import org.slf4j.Logger;
-
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 public class OpenSSLTest {
 
@@ -31,7 +33,11 @@ public class OpenSSLTest {
 		try {
 			byte[] secret = getBytesFromHexString("5f4dcc3b5aa765d61d8327deb882cf99");
 			byte[] iv = getBytesFromHexString("2b95990a9151374abd8ff8c5a7a0fe08");
-			byte[] data = Base64.decode("16GRjUXjW5vCqs9bQrAEEQ==");
+			byte[] data = decodeBase64("16GRjUXjW5vCqs9bQrAEEQ==");
+			String hex = toHexStringLower(data);
+			System.out.println(hex);
+			String plain = toStringFromHex(hex, UTF8);
+			System.out.println(plain);
 			Cipher cipher = Cipher.getInstance(cipherTransformation);
 			Key key = new SecretKeySpec(secret, secretKeySpecAlgorithm);
 			cipher.init(DECRYPT_MODE, key, new IvParameterSpec(iv));
@@ -40,6 +46,10 @@ public class OpenSSLTest {
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static byte[] decodeBase64(String s) throws IOException {
+		return Base64.decodeBase64(s.getBytes(UTF8));
 	}
 
 	protected static void info(String msg, Object... args) {
