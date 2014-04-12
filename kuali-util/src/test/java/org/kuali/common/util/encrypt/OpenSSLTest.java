9bbc6bebf326d7ce77e3fc4e9bdfb58e87401a8e
jcaddel <jcaddel@kuali.org>
2014-04-12 14:42:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38626 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/encrypt/OpenSSLTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/encrypt/OpenSSLTest.java
@@ -2,23 +2,14 @@ package org.kuali.common.util.encrypt;
 
 import static java.lang.String.format;
 import static javax.crypto.Cipher.DECRYPT_MODE;
-import static javax.crypto.Cipher.ENCRYPT_MODE;
 import static org.kuali.common.util.Encodings.UTF8;
 import static org.kuali.common.util.HexUtils.getBytesFromHexString;
-import static org.kuali.common.util.HexUtils.toHexString;
-import static org.kuali.common.util.base.Exceptions.illegalState;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
-import java.security.AlgorithmParameters;
 import java.security.Key;
-import java.security.SecureRandom;
-import java.security.spec.KeySpec;
 
 import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.junit.Test;
@@ -33,11 +24,7 @@ public class OpenSSLTest {
 	// openssl enc -nosalt -k password -in test -aes-128-cbc -p -out test.enc | tr '[:upper:]' '[:lower:]'; echo "data=$(base64 test.enc)"
 
 	String cipherTransformation = "AES/CBC/PKCS5Padding";
-	String secretKeyFactoryAlgorithm = "PBKDF2WithHmacSHA1";
 	String secretKeySpecAlgorithm = "AES";
-	int bits = 128;
-	int saltLength = 8;
-	int iterationCount = 1024 * 64;
 
 	@Test
 	public void test() {
@@ -61,50 +48,5 @@ public class OpenSSLTest {
 		} else {
 			logger.info(format(msg, args));
 		}
-	}
-
-	protected String decrypt(EncryptionResult encrypted, String password, String salt) {
-		try {
-			byte[] iv = getBytesFromHexString(encrypted.getInitializationVector());
-			byte[] ciphertext = Base64.decode(encrypted.getEncryptedText());
-			SecretKey secret = getSecretKey(password, salt);
-			Cipher cipher = Cipher.getInstance(cipherTransformation);
-			cipher.init(DECRYPT_MODE, secret, new IvParameterSpec(iv));
-			return new String(cipher.doFinal(ciphertext), UTF8);
-		} catch (Exception e) {
-			throw illegalState(e);
-		}
-	}
-
-	protected SecretKey getSecretKey(String password, String salt) {
-		try {
-			byte[] saltBytes = getBytesFromHexString(salt);
-			SecretKeyFactory factory = SecretKeyFactory.getInstance(secretKeyFactoryAlgorithm);
-			KeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, iterationCount, bits);
-			SecretKey tmp = factory.generateSecret(spec);
-			return new SecretKeySpec(tmp.getEncoded(), secretKeySpecAlgorithm);
-		} catch (Exception e) {
-			throw illegalState(e);
-		}
-	}
-
-	protected EncryptionResult encrypt(String plaintext, String password, String salt) {
-		try {
-			SecretKey secret = getSecretKey(password, salt);
-			Cipher cipher = Cipher.getInstance(cipherTransformation);
-			cipher.init(ENCRYPT_MODE, secret);
-			AlgorithmParameters params = cipher.getParameters();
-			byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
-			byte[] ciphertext = cipher.doFinal(plaintext.getBytes(UTF8));
-			return new EncryptionResult(Base64.encode(ciphertext).replace("\n", "").replace("\r", ""), toHexString(iv).toLowerCase());
-		} catch (Exception e) {
-			throw illegalState(e);
-		}
-	}
-
-	protected byte[] getSalt(int length) {
-		byte[] salt = new byte[length];
-		new SecureRandom().nextBytes(salt);
-		return salt;
 	}
 }
