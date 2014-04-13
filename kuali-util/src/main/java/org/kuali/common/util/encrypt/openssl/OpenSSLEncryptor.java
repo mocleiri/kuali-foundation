1e4912939e1de51e6a2a75172891535a1e79b4d9
jcaddel <jcaddel@kuali.org>
2014-04-13 10:59:28 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38672 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/openssl/OpenSSLEncryptor.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/openssl/OpenSSLEncryptor.java
@@ -1,17 +1,63 @@
 package org.kuali.common.util.encrypt.openssl;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.codehaus.plexus.util.Base64.decodeBase64;
+import static org.kuali.common.util.Ascii.isDigit;
+import static org.kuali.common.util.Ascii.isLetter;
+import static org.kuali.common.util.Encodings.ASCII;
+import static org.kuali.common.util.base.Precondition.checkNotBlank;
+import static org.kuali.common.util.base.Precondition.checkNotNull;
+import static org.kuali.common.util.encrypt.openssl.OpenSSLContext.buildDefaultOpenSSLContext;
+
+import org.kuali.common.util.Str;
 import org.kuali.common.util.encrypt.Encryptor;
 
-public class OpenSSLEncryptor implements Encryptor {
+public final class OpenSSLEncryptor implements Encryptor {
+
+	private static final char[] OTHER_BASE64_CHARS = { '+', '/', '=' };
+	private final char[] password;
+	private final OpenSSLContext context;
+
+	public OpenSSLEncryptor(String password) {
+		this(buildDefaultOpenSSLContext(), password);
+	}
+
+	public OpenSSLEncryptor(OpenSSLContext context, String password) {
+		this.password = checkNotBlank(password, "password").toCharArray();
+		this.context = checkNotNull(context, "context");
+	}
 
 	@Override
 	public String encrypt(String text) {
+		checkNotBlank(text, "text");
 		return null;
 	}
 
 	@Override
 	public String decrypt(String text) {
+		checkNotBlank(text, "text");
 		return null;
+	}
+
+	protected OpenSSLEncryptedContext getEncryptedContext(String text) {
+		byte[] bytes = decodeBase64(Str.getBytes(checkBase64(text), ASCII));
+		return null;
+	}
+
+	protected String checkBase64(String text) {
+		checkNotBlank(text, "text");
+		for (char c : text.toCharArray()) {
+			checkArgument(isBase64(c), "'%s' is not a base 64 character", c);
+		}
+		return text;
+	}
+
+	protected boolean isBase64(char c) {
+		if (isLetter(c) || isDigit(c)) {
+			return true;
+		} else {
+			return c == '/' || c == '+' || c == '=';
+		}
 	}
 
 }
