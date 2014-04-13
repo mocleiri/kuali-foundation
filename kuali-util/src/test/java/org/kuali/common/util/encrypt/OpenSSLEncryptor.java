8bfe9c1742343e36eb0064c28c9accaaafbc8d0b
jcaddel <jcaddel@kuali.org>
2014-04-12 21:14:52 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38662 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-util/src/test/java/org/kuali/common/util/encrypt/OpenSSLEncryptor.java
@@ -0,0 +1,15 @@
+package org.kuali.common.util.encrypt;
+
+public class OpenSSLEncryptor implements Encryptor {
+
+	@Override
+	public String encrypt(String text) {
+		return null;
+	}
+
+	@Override
+	public String decrypt(String text) {
+		return null;
+	}
+
+}
