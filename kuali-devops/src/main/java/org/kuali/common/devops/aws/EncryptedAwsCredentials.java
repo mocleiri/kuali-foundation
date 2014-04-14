6ff21774962a1503a7f0292ff06da783fd89cb11
jcaddel <jcaddel@kuali.org>
2014-04-14 09:47:21 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38743 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/main/java/org/kuali/common/devops/aws/EncryptedAwsCredentials.java
+++ kuali-devops/src/main/java/org/kuali/common/devops/aws/EncryptedAwsCredentials.java
@@ -21,8 +21,8 @@ import com.amazonaws.auth.AWSCredentials;
 
 public enum EncryptedAwsCredentials implements AWSCredentials {
 
-	FOUNDATION("YbHpITe4Q2m8uIbISkgv7pzKDQRNHitmXq688vneT+E=", "Lq924d5rBEel47mOwFTUl429ADZPI9dgKU+WJF253PStruafXbofMAfWC+dGTjGXykyCFVcT0os="), //
-	STUDENT("1qX8qo6+OPWttFNuvrRN5bTUoxncnTeQr87hhE3UWUs=", "NERgtnAql47LomO7wa06+rU2AI3pbYSpOYZZDVdAXJP7vWtzki6jTB4Vys8pzmM4h/qAXXbgmqs="), //
+	FOUNDATION("U2FsdGVkX1+8Ztcpl1oE1zvItuWZ+U/B1m1Y5ZfnPL1/RL3ytfTW0GPQ0T2I6f3x", "U2FsdGVkX19iFvmWrQbvBA+aoUjCW37k8hFzcRrDzjc6KDOfEYUd7m8/nyvy/YBZghfgT88GJhlQVk4aD02eWg=="), //
+	STUDENT("U2FsdGVkX19hBXKp9N3JwJMV3n7AMldCEv3BMRKVngUNmsfIk7QfRRxgJ7bSnX1O", "U2FsdGVkX18dh07mozYEQfy5uzzLZ2gEf8hWlkSViADnBBfsgeHUd3as46GSFGr/z3UKbaYDC6UR/kaK44a14g=="), //
 	RICE("gD0RPJ6Q4qnHVqvcy05gPTzgQyCfPrdEz9aivyh0tk8=", "n6ZFYgj1tbVkhPvvTD/QltvFicvag65OJSPdT1eOW3sKu4nBbfLHunyGRK1pFFJafuePnvSOUAA="), //
 	OLE("DsNWIZtKF7XRlgoM7D6H3ywghJw5y+yaz9zgEPsH4IY=", "regjgo+ZtWY187I0TFNFSNO0DsjU7qDzCWLcwIEWwsr2zijUW5imm4WxPSfB+c7lsep/Yf64cH4=");
 
