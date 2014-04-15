9aef36c596fb300463c78c31211f1019ed74411b
jcaddel <jcaddel@kuali.org>
2014-04-14 20:17:17 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38797 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/ci/SpinUpJenkinsMaster.java
@@ -121,8 +121,8 @@ public class SpinUpJenkinsMaster {
 			ProjectIdentifier pid = KUALI_DEVOPS_PROJECT_IDENTIFIER;
 
 			EC2Service service = getEC2Service(amazonAccount, jenkinsContext.getRegion());
-			Instance instance = CreateBuildSlaveAMI.launchAndWait(service, request, securityGroups, tags, jenkinsContext.getRegion().getName());
-			// Instance instance = service.getInstance("i-9c89fac0");
+			// Instance instance = CreateBuildSlaveAMI.launchAndWait(service, request, securityGroups, tags, jenkinsContext.getRegion().getName());
+			Instance instance = service.getInstance("i-90b47298");
 			info("public dns: %s", instance.getPublicDnsName());
 			updateDns(instance, jenkinsMaster);
 			String dns = instance.getPublicDnsName();
