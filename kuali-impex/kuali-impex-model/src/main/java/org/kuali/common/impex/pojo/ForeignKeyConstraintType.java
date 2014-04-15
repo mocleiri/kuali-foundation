06be6f14e7ac1e5abc89b1851648b5a199512d12
jcaddel <jcaddel@kuali.org>
2014-04-14 21:42:20 -0400

    [maven-release-plugin]  copy for tag kuali-sql-2.1.4
    git-svn-id: https://svn.kuali.org/repos/foundation/tags/kuali-sql-2.1.4@38810 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-impex/kuali-impex-model/src/main/java/org/kuali/common/impex/pojo/ForeignKeyConstraintType.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.common.impex.pojo;
-
-public enum ForeignKeyConstraintType {
-
-	CASCADE, SET_NULL, SET_DEFAULT, RESTRICT, NO_ACTION
-
-}
--- kuali-puppet/src/test/resources/modules/simple/manifests/1.file.pp
+++
@@ -1,6 +0,0 @@
- file {'/tmp/helloworld.txt':
-    path    => '/tmp/helloworld.txt',
-    ensure  => present,
-    mode    => 0644,
-    content => "Hello World",
- }
