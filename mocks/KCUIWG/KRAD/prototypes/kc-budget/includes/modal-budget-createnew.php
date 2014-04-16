2926ffca5a955892175dde2ad8cde3c68e8a00fe
mfitzwater <mfitzwater@kuali.org>
2014-04-16 10:35:38 -0400

    UXI-228 Add "casual user" task flows, changed link https://jira.kuali.org/browse/UXI-228
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@38875 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/modal-budget-createnew.php
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/modal-budget-createnew.php
@@ -1,12 +1,10 @@
-    
-                    
-                    <!-- Modal -->
+  <!-- Modal -->
                     <div class="modal fade" id="createNew" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                    <h4 class="modal-title" id="myModalLabel">Create a Budget Version</h4>
+                                    <h4 class="modal-title" id="myModalLabel">Create a Budget Version TODAY</h4>
                                 </div>
                                 <div class="modal-body">
                                     <div class="form-horizontal" role="form">
@@ -27,15 +25,15 @@
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
                                     <div class="btn-group">
-                                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Create <span class="caret"></span> </button>
+                                    <a  href="budget-ng-start.php" type="button" class="btn btn-primary"> Create </a>
+                                        <!-- <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Create <span class="caret"></span> </button>
                                         <ul class="dropdown-menu" role="menu">
                                             <li><a href="#">Create and stay on Proposal</a></li>
-                                            <li><a href="budget-start.php">Create and go to Budget</a></li>
-                                        </ul>
+                                            <li><a href="budget-ng-start.php">Create and go to Budget</a></li>
+                                        </ul> -->
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     
-                    <!-- /Modal -->
