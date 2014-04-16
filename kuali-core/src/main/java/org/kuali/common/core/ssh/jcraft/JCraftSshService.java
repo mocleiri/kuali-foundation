dbbdde74a4a417f03b0a19c933eee66c98c32b83
jcaddel <jcaddel@kuali.org>
2014-04-16 11:36:38 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@38879 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-core/src/main/java/org/kuali/common/core/ssh/jcraft/JCraftSshService.java
+++ kuali-core/src/main/java/org/kuali/common/core/ssh/jcraft/JCraftSshService.java
@@ -15,6 +15,9 @@
  */
 package org.kuali.common.core.ssh.jcraft;
 
+import static com.jcraft.jsch.KeyPair.DSA;
+import static com.jcraft.jsch.KeyPair.RSA;
+import static org.kuali.common.util.Encodings.UTF8;
 import static org.kuali.common.util.base.Precondition.checkNotNull;
 
 import java.io.ByteArrayOutputStream;
@@ -24,21 +27,16 @@ import org.kuali.common.core.ssh.Algorithm;
 import org.kuali.common.core.ssh.GenerateKeyPairContext;
 import org.kuali.common.core.ssh.KeyPair;
 import org.kuali.common.core.ssh.SshService;
-import org.kuali.common.util.Encodings;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 
 public class JCraftSshService implements SshService {
 
-	private static final String UTF8 = Encodings.UTF8;
-	private static final int DSA = com.jcraft.jsch.KeyPair.DSA;
-	private static final int RSA = com.jcraft.jsch.KeyPair.RSA;
-
 	@Override
 	public KeyPair generateKeyPair(GenerateKeyPairContext context) {
 		checkNotNull(context, "context");
-		int algorithm = (Algorithm.DSA == context.getAlgorithm()) ? DSA : RSA;
+		int algorithm = (Algorithm.DSA.equals(context.getAlgorithm())) ? DSA : RSA;
 		JSch jsch = new JSch();
 		com.jcraft.jsch.KeyPair keyPair = getKeyPair(jsch, algorithm, context.getSize());
 		String publicKey = getPublicKey(keyPair, context.getName()).trim();
@@ -58,16 +56,16 @@ public class JCraftSshService implements SshService {
 	protected String getPrivateKey(com.jcraft.jsch.KeyPair keyPair) {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		keyPair.writePrivateKey(out);
-		return toUTF8String(out);
+		return getUTF8String(out);
 	}
 
 	protected String getPublicKey(com.jcraft.jsch.KeyPair keyPair, String name) {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		keyPair.writePublicKey(out, name);
-		return toUTF8String(out);
+		return getUTF8String(out);
 	}
 
-	protected String toUTF8String(ByteArrayOutputStream out) {
+	protected String getUTF8String(ByteArrayOutputStream out) {
 		try {
 			return out.toString(UTF8);
 		} catch (UnsupportedEncodingException e) {
