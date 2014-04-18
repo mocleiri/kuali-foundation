498582395a94a7ec8ab730448430d5c7d102deb7
mfitzwater <mfitzwater@kuali.org>
2014-04-18 11:45:20 -0400

    UXI-226 - Add a persistent link (on all pages) that goes to the CMS for user assistance. UXI-233 - In usability testing users have had difficulty finding links in the "action bar"
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@39004 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/styles.php
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/styles.php
@@ -20,8 +20,40 @@
 #u19v7dpm {padding-bottom: 1em; }/* pad the bottom fixed bar */
 /*  relocating the action bar */
 main{margin-top:1.3em}
-#ueqbqhn{padding-bottom: 1.3em;}
+
 header{position:relative;}
-#LabsProposal-DocActionBar{padding:5px 0 5px 1em; background:#F5F5F5; position: absolute;right:0;bottom:-31px; border:1px solid #ccc;border-bottom-right-radius:10px;border-bottom-left-radius:10px}
-/*  // relocating the action bar */
+
+
+
+#LabsProposal-DocActionBar{ position:relative;padding:5px 0 5px 15px;margin-top:1em; background:#fff; border:1px solid #ccc;;border-top-right-radius:10px;border-bottom:none;border-top-left-radius:10px;float:right;}
+
+
+/* CURRENT action bar white background positioned on the right
+main,header{position:relative}
+
+#LabsProposal-DocActionBar{padding:5px 0 5px 1em;margin-top:1em; background:#fff; border:1px solid #ccc;;border-top-right-radius:10px;width:680px;border-bottom:none;border-top-left-radius:10px;float:right;}
+
+*/
+/*
+action bar gray positioned underneath the line
+#ueqbqhn{padding-bottom: 1.3em;}
+#LabsProposal-DocActionBar{padding:5px 0 5px 1em; background:#F5F5F5; position: absolute;right:0;bottom:-30px; border:1px solid #ccc;border-bottom-right-radius:10px;border-bottom-left-radius:10px}
+*/
+/* Gray background - inline right document status 
+
+#LabsProposal-DocInfo{width:600px; margin:22px 0 0;right:51px;}
+#LabsProposal-DocActionBar{padding:5px 0 5px 1em; background:#F5F5F5; position: absolute;right:0;bottom:-1px; border:1px solid #ccc;;border-top-right-radius:10px;border-top-left-radius:10px}
+#u1f206ki, #u1f206ld,#u1f206jn,#LabsProposal-MoreDocInfo{ float:left; clear:none;width:110px; }
+#u1f206jn{width:160px;}
+
+// relocating the action bar - */
+/* action bar page help */
+#page-help{line-height:0; position:relative; margin-left:5px; }
+#page-help .icon-question-sign{ color:#959595;font-size:20px; cursor:pointer; position:absolute; bottom:-3px; left:0 px;padding:0 3px;color:#0055CC}
+#page-help .caret{margin-left:23px;  }
+#page-help .dropdown-menu {left:-139px; padding:0;}
+#page-help .dropdown-menu li {width:100%; float:left;}
+#page-help .dropdown-menu li a{ width:100%;min-width:150px;padding:5px;}
+/* - page help - */
+
 </style>
--- mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/uif-viewHeader.php
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/uif-viewHeader.php
@@ -60,6 +60,7 @@
             <li> <a id="uotglr8" class="uif-actionLink"  data-toggle="modal" data-target="#budgetSettings"><span class="icon-cog"></span>Budget Settings</a> </li>
             <li> <a id="uotglr8" class="uif-actionLink" data-toggle="modal" data-target="#summary"><span class="icon-eye"></span>Summary</a> </li>
             <li><a  class="uif-actionLink" data-toggle="modal" data-target="#modal-budget-versions"><span class="icon-money"></span>Budget Versions</a> </li>
+                    
             <?php
             if ($_COOKIE['pg'] || $_COOKIE['pg'] == 1) {
                 echo '<li><span class="uif-actionLink uif-disabled"><span class="icon-stack"></span>Generate All Periods </span></li>';
@@ -67,6 +68,28 @@
                 echo '<li><a class="uif-actionLink" data-toggle="modal" data-target="#modal-generate-versions"><span class="icon-stack"></span>Generate All Periods </a></li>';
             }
             ?>
+     
+                 <li id="page-help"> <a  class="dropdown-toggle" data-toggle="dropdown"><span class="icon-question-sign"><span class="sr-only">help</span></span><span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                           
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Periods &amp; Totals</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Rates</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Personnel Cost</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Non-Personnel</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Institutional Commitments</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Project Income</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Modular</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Budget Notes</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Summary</a></li>
+<li><a href="https://testdrive.kc.kuali.org/kc-ptd/static/help/default.htm?turl=Documents/requiredfieldsforsavingdocument.htm">Glossary of Terms</a></li>
+                           
+                           
+                           
+                           
+                        </ul>
+                        </li>
+                 
+            
         </ul>
     </div>
 </header>
