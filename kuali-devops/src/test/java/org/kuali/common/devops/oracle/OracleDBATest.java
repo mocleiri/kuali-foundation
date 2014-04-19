29458d5253c93f54d5c043195b33a2e664e6345b
jcaddel <jcaddel@kuali.org>
2014-04-19 12:21:39 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@39016 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-devops/src/test/java/org/kuali/common/devops/oracle/OracleDBATest.java
+++ kuali-devops/src/test/java/org/kuali/common/devops/oracle/OracleDBATest.java
@@ -1,7 +1,11 @@
 package org.kuali.common.devops.oracle;
 
+import static java.lang.String.format;
+import static java.net.InetAddress.getByName;
 import static org.junit.Assert.fail;
 import static org.kuali.common.util.log.Loggers.newLogger;
+
+import java.net.InetAddress;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -13,9 +17,22 @@ public class OracleDBATest {
 	@Test
 	public void test() throws Exception {
 		try {
+			String host = "env8.ks.kuali.org";
+			InetAddress addr = getByName(host);
+			String ip = addr.getHostAddress();
+			info("host=%s", host);
+			info("ip=%s", ip);
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail(e.getMessage());
+		}
+	}
+
+	protected static void info(String msg, Object... args) {
+		if (args == null) {
+			logger.info(msg);
+		} else {
+			logger.info(format(msg, args));
 		}
 	}
 }
