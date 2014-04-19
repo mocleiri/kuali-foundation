9c42b3cf3c522b4dc8c48fbe176e285e62cb3e69
jcaddel <jcaddel@kuali.org>
2014-04-19 12:08:33 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@39014 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/resources/oracle/dba.sql
+++ kuali-util/src/test/resources/oracle/dba.sql
@@ -6,4 +6,10 @@ select substr(sys_context('USERENV','IP_ADDRESS'),1,15) as my_ip, substr(sys_con
 select substr(sys_context('USERENV','TERMINAL','IP_ADDRESS','SERVER_HOST'),1,25) as terminal from dual;
 
 
-select machine from v$session where username is not null order by machine; 
+select username, machine from v$session where username = 'OLEENV4' order by machine; 
+
+select distinct machine from v$session where username = 'OLEENV4';
+ 
+select distinct username, machine from v$session order by machine, username;
+
+select v.username, v.machine, UTL_INADDR.get_host_address(v.machine) as ip from v$session v where username = 'OLEENV4' order by v.machine; 
