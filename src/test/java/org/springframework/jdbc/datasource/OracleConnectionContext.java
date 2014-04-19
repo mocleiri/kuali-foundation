19cd7d73471709263be879ef3aa56fd8d9dc153a
jcaddel <jcaddel@kuali.org>
2014-04-19 12:53:00 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/branches/kuali-jdbc-2.0@39021 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- src/test/java/org/springframework/jdbc/datasource/OracleConnectionContext.java
+++ src/test/java/org/springframework/jdbc/datasource/OracleConnectionContext.java
@@ -39,7 +39,7 @@ public final class OracleConnectionContext {
 		private String host;
 		private String driver = "oracle.jdbc.driver.OracleDriver";
 		private int port = 1521;
-		private String sid = "ORACLE";
+		private String sid = "XE";
 
 		public Builder withUsername(String username) {
 			this.username = username;
