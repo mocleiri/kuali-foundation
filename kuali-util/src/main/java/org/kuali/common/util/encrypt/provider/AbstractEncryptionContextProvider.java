f3770150055b22f0e31f94397a59e50f6888239c
jcaddel <jcaddel@kuali.org>
2014-04-20 19:46:40 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@39082 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/provider/AbstractEncryptionContextProvider.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/provider/AbstractEncryptionContextProvider.java
@@ -2,32 +2,39 @@ package org.kuali.common.util.encrypt.provider;
 
 import static com.google.common.base.Optional.absent;
 import static com.google.common.base.Optional.fromNullable;
+import static java.lang.String.format;
 import static org.apache.commons.lang3.StringUtils.trimToNull;
 import static org.kuali.common.util.base.Precondition.checkNotBlank;
 import static org.kuali.common.util.encrypt.EncryptionStrength.DEFAULT_ENCRYPTION_STRENGTH;
+import static org.kuali.common.util.log.Loggers.newLogger;
 
 import org.kuali.common.util.encrypt.EncryptionContext;
 import org.kuali.common.util.encrypt.EncryptionStrength;
+import org.slf4j.Logger;
 
 import com.google.common.base.Optional;
 
 public abstract class AbstractEncryptionContextProvider implements EncryptionContextProvider {
 
-	public AbstractEncryptionContextProvider(String encryptionPasswordKey, String encryptionStrengthKey) {
-		this.encryptionPasswordKey = checkNotBlank(encryptionPasswordKey, "encryptionPasswordKey");
-		this.encryptionStrengthKey = checkNotBlank(encryptionStrengthKey, "encryptionStrengthKey");
+	private static final Logger logger = newLogger();
+
+	public AbstractEncryptionContextProvider(String passwordKey, String strengthKey) {
+		this.passwordKey = checkNotBlank(passwordKey, "passwordKey");
+		this.strengthKey = checkNotBlank(passwordKey, "strengthKey");
 	}
 
-	private final String encryptionPasswordKey;
-	private final String encryptionStrengthKey;
+	private final String passwordKey;
+	private final String strengthKey;
 
 	@Override
 	public Optional<EncryptionContext> getEncryptionContext() {
-		Optional<String> password = getOptionalString(encryptionPasswordKey);
+		Optional<String> password = getOptionalString(passwordKey);
 		if (!password.isPresent()) {
+			logger.info(format("no encryption password found [%s, key=%s]", this.getClass().getSimpleName(), passwordKey));
 			return absent();
 		} else {
 			EncryptionStrength strength = getEncryptionStrength();
+			logger.info(format("encryption password located [%s, key=%s %s]", this.getClass().getSimpleName(), passwordKey, strength));
 			return Optional.of(new EncryptionContext(password.get(), strength));
 		}
 	}
@@ -39,7 +46,7 @@ public abstract class AbstractEncryptionContextProvider implements EncryptionCon
 	protected abstract String getValueFromSource(String key);
 
 	protected EncryptionStrength getEncryptionStrength() {
-		Optional<String> optional = getOptionalString(encryptionStrengthKey);
+		Optional<String> optional = getOptionalString(strengthKey);
 		if (optional.isPresent()) {
 			return EncryptionStrength.valueOf(optional.get().toUpperCase());
 		} else {
