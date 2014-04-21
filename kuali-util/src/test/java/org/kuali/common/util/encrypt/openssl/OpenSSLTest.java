b371eec29ab727139917460448686dc6fede6c09
jcaddel <jcaddel@kuali.org>
2014-04-21 11:52:51 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@39122 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/test/java/org/kuali/common/util/encrypt/openssl/OpenSSLTest.java
+++ kuali-util/src/test/java/org/kuali/common/util/encrypt/openssl/OpenSSLTest.java
@@ -3,6 +3,9 @@ package org.kuali.common.util.encrypt.openssl;
 import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.Math.pow;
 import static java.lang.String.format;
+import static org.codehaus.plexus.util.Base64.decodeBase64;
+import static org.kuali.common.util.Str.getAsciiBytes;
+import static org.kuali.common.util.encrypt.openssl.OpenSSL.generatePassword;
 import static org.kuali.common.util.log.LoggerUtils.logTable;
 import static org.kuali.common.util.log.Loggers.newLogger;
 
@@ -26,30 +29,39 @@ public class OpenSSLTest {
 	@Test
 	public void testGeneratePassword() {
 		List<MathPow> list = newArrayList();
-		list.add(new MathPow(2, 32)); // Java integers are 32 bit
-		list.add(new MathPow(2, 64)); // Java longs are 64 bit
-		list.add(new MathPow(2, 128)); // 128 bit based encryption techniques are pretty standard
-		list.add(new MathPow(62, 21)); // There are 62 alphanumeric characters A-Z, a-z, and 0-9
-		list.add(new MathPow(62, 22)); // There are 62 alphanumeric characters A-Z, a-z, and 0-9
-		list.add(new MathPow(95, 19)); // There are 95 printable ASCII characters 32-126 (inclusive)
-		list.add(new MathPow(95, 20)); // There are 95 printable ASCII characters 32-126 (inclusive)
-		list.add(new MathPow(2, 256));
-		list.add(new MathPow(62, 42)); // There are 62 alphanumeric characters A-Z, a-z, and 0-9
-		list.add(new MathPow(62, 43)); // There are 62 alphanumeric characters A-Z, a-z, and 0-9
-		list.add(new MathPow(95, 39)); // There are 62 alphanumeric characters A-Z, a-z, and 0-9
-		list.add(new MathPow(95, 40)); // There are 62 alphanumeric characters A-Z, a-z, and 0-9
-		list.add(new MathPow(36, 20)); // AWS uses 20 character access keys A-Z and 0-9
-		list.add(new MathPow(64, 40)); // AWS uses 40 character base64 secret keys
+		list.add(new MathPow("java int", 2, 32));
+		list.add(new MathPow("java long", 2, 64));
+		list.add(new MathPow("128 bit", 2, 128));
+		list.add(new MathPow("alphanumeric", 62, 21));
+		list.add(new MathPow("alphanumeric", 62, 22));
+		list.add(new MathPow("printable ascii", 95, 19));
+		list.add(new MathPow("printable ascii", 95, 20));
+		list.add(new MathPow("256 bit", 2, 256));
+		list.add(new MathPow("alphanumeric", 62, 42));
+		list.add(new MathPow("alphanumeric", 62, 43));
+		list.add(new MathPow("printable ascii", 95, 38));
+		list.add(new MathPow("printable ascii", 95, 39));
+		// AWS uses 20 character access keys A-Z and 0-9
+		list.add(new MathPow("aws access key", 36, 20));
+		// AWS uses 40 character base64 secret keys which equates to 30 bytes which equates to 230 bits
+		list.add(new MathPow("aws secret key", 2, 230));
 		show(list);
+		String password = generatePassword(40);
+		byte[] asciiBytes = getAsciiBytes(password);
+		byte[] base64Bytes = decodeBase64(asciiBytes);
+		info("password.length()=%s", password.length());
+		info("asciiBytes.length=%s", asciiBytes.length);
+		info("base64Bytes.length=%s", base64Bytes.length);
 	}
 
 	protected static void show(List<MathPow> list) {
-		List<String> columns = ImmutableList.of("function", "value");
+		List<String> columns = ImmutableList.of("label", "function", "value");
 		List<Object[]> rows = newArrayList();
 		for (MathPow element : list) {
+			String label = element.getLabel();
 			String function = format("Math.pow(%s, %s)", element.getBase(), element.getPow());
 			String value = NF.format(Math.pow(element.getBase(), element.getPow()));
-			Object[] row = { function, value };
+			Object[] row = { label, function, value };
 			rows.add(row);
 		}
 		logTable(columns, rows);
