726544e3f825d0293e9c5cba048dbc6f735af906
jcaddel <jcaddel@kuali.org>
2014-04-21 09:40:12 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@39111 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
+++ kuali-util/src/main/java/org/kuali/common/util/encrypt/Encryption.java
@@ -1,13 +1,18 @@
 package org.kuali.common.util.encrypt;
 
+import static java.lang.String.format;
 import static org.kuali.common.util.encrypt.openssl.OpenSSL.buildOpenSSLEncryptor;
+import static org.kuali.common.util.log.Loggers.newLogger;
 
 import org.kuali.common.util.encrypt.provider.DefaultEncryptionContextProviderChain;
 import org.kuali.common.util.encrypt.provider.EncryptionContextProvider;
+import org.slf4j.Logger;
 
 import com.google.common.base.Optional;
 
 public final class Encryption {
+
+	private static final Logger logger = newLogger();
 
 	private static Encryptor encryptor;
 
@@ -17,6 +22,7 @@ public final class Encryption {
 		if (context.isPresent()) {
 			return buildOpenSSLEncryptor(context.get());
 		} else {
+			logger.info(format("encryption disabled. no encryption password was located"));
 			return NoOpEncryptor.INSTANCE;
 		}
 	}
