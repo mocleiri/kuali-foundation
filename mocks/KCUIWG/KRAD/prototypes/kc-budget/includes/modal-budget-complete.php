1b3fadd4c4378760e4d1e48e0f65faef6a83f0b1
mfitzwater <mfitzwater@kuali.org>
2014-04-24 14:45:13 -0400

    UXI-228 Added modal for complete budget https://jira.kuali.org/browse/UXI-228
    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@39242 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/includes/modal-budget-complete.php
@@ -0,0 +1,22 @@
+<!-- Modal -->
+<div class="modal fade" id="modal-budget-complete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="myModalLabel">Complete Budget</h4>
+            </div>
+            <div class="modal-body">
+               
+                <p>Is this Budget ready to be sent to the sponsor?</p>
+        
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                 <a href="http://subkrad/prototypes/kc-budget/prop-basics-details.php?complete=yes" class="btn btn-default">Yes</a>
+             
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end Modal --> 
