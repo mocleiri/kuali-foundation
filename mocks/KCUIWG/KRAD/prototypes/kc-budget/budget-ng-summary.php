752cc39daab300b40aaab5114a4ab621a5d7deab
mfitzwater <mfitzwater@kuali.org>
2014-04-24 14:42:56 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/legacy@39240 eb505253-de9c-4f3b-bf87-3aad84f29c0f

--- mocks/KCUIWG/KRAD/prototypes/kc-budget/budget-ng-summary.php
+++ mocks/KCUIWG/KRAD/prototypes/kc-budget/budget-ng-summary.php
@@ -325,7 +325,12 @@ $section = '';
                 </table>
             </main>
         </div> 
-<!-- VIEW FOOTER --> <div id="" class="uif-footer clearfix uif-stickyFooter uif-stickyButtonFooter" data-sticky_footer="true" data-parent="LabsProposal" style="position:fixed; left: 0; bottom: 0px;"> <div class="global-actions"> <a id="" class="btn btn-default uif-secondaryActionButton uif-boxLayoutHorizontalItem">Save </a> <a id="" class="btn btn-default uif-secondaryActionButton uif-boxLayoutHorizontalItem">Reload </a> <a id="ufuknl9" class="btn btn-primary uif-secondaryActionButton uif-boxLayoutHorizontalItem">Complete Budget </a> </div>
+<!-- VIEW FOOTER --> <div id="" class="uif-footer clearfix uif-stickyFooter uif-stickyButtonFooter" data-sticky_footer="true" data-parent="LabsProposal" style="position:fixed; left: 0; bottom: 0px;">
+ <div class="global-actions"> <a id="" class="btn btn-default uif-secondaryActionButton uif-boxLayoutHorizontalItem">Save </a>
+ <a id="" class="btn btn-default uif-secondaryActionButton uif-boxLayoutHorizontalItem">Reload </a> 
+		 <a id="ufuknl9" class="btn btn-default uif-secondaryActionButton uif-boxLayoutHorizontalItem"   data-target="#modal-budget-complete" data-toggle="modal">Complete Budget </a> 
+		
+ </div>
             
             <div class="global-navigate">
             
@@ -359,6 +364,7 @@ $section = '';
 <!-- MODAL BUDGET VERSIONS -->
 <?php include ('includes/modal-budget-versions.php') ?>
 
-
+<!-- MODAL budget complete  -->
+<?php include ('includes/modal-budget-complete.php') ?>
 </body>
 </html>
