fe30c070a2de8ec9fe58c165107057e0992cd5cb
jcaddel <jcaddel@kuali.org>
2014-05-02 19:30:04 -0400

    git-svn-id: https://svn.kuali.org/repos/foundation/trunk@39328 eb505253-de9c-4f3b-bf87-3aad84f29c0f

---
+++ kuali-core/src/test/java/org/kuali/common/core/json/AwsAccount.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.common.core.json;
+
+import org.kuali.common.core.build.ValidatingBuilder;
+import org.kuali.common.core.validate.annotation.IdiotProofImmutable;
+
+import com.google.common.base.Optional;
+
+@IdiotProofImmutable
+public final class AwsAccount {
+
+	private final String name;
+	private final String accountNumber;
+	private final Optional<String> description;
+
+	private AwsAccount(Builder builder) {
+		this.name = builder.name;
+		this.accountNumber = builder.accountNumber;
+		this.description = builder.description;
+	}
+
+	public static class Builder extends ValidatingBuilder<AwsAccount> {
+
+		private String name;
+		private String accountNumber;
+		private Optional<String> description;
+
+		public Builder withName(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder withAccountNumber(String accountNumber) {
+			this.accountNumber = accountNumber;
+			return this;
+		}
+
+		public Builder withDescription(Optional<String> description) {
+			this.description = description;
+			return this;
+		}
+
+		@Override
+		public AwsAccount build() {
+			return validate(new AwsAccount(this));
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getAccountNumber() {
+			return accountNumber;
+		}
+
+		public void setAccountNumber(String accountNumber) {
+			this.accountNumber = accountNumber;
+		}
+
+		public Optional<String> getDescription() {
+			return description;
+		}
+
+		public void setDescription(Optional<String> description) {
+			this.description = description;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public Optional<String> getDescription() {
+		return description;
+	}
+}
